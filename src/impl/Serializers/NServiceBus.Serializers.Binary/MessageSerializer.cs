47504c9a0168228488c78ec0e8ae5293637dd774
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2008-05-24 08:37:44 +0000

    Binary implementation of IMessageSerializer.

---
+++ src/impl/Serializers/NServiceBus.Serializers.Binary/MessageSerializer.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using NServiceBus.Serialization;
+
+namespace NServiceBus.Serializers.Binary
+{
+    public class MessageSerializer : IMessageSerializer
+    {
+        public void Initialize(params Type[] types)
+        {
+        }
+
+        public void Serialize(IMessage[] messages, Stream stream)
+        {
+            this.binaryFormatter.Serialize(stream, new List<object>(messages));
+        }
+
+        public IMessage[] Deserialize(Stream stream)
+        {
+            List<object> body = this.binaryFormatter.Deserialize(stream) as List<object>;
+
+            if (body == null)
+                return null;
+
+            IMessage[] result = new IMessage[body.Count];
+
+            int i = 0;
+            foreach (IMessage m in body)
+                result[i++] = m;
+
+            return result;
+        }
+
+        private readonly BinaryFormatter binaryFormatter = new BinaryFormatter();
+    }
+}
