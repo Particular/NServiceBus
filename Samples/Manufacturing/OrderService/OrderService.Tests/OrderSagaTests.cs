0ee693c638fc91404c47f980fef621a3251addf8
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2008-11-28 22:15:27 +0000

    Updated Manufacturing unit tests to use lambdas.

--- Samples/Manufacturing/OrderService/OrderService.Tests/OrderSagaTests.cs
+++ Samples/Manufacturing/OrderService/OrderService.Tests/OrderSagaTests.cs
@@ -11,6 +11,8 @@ namespace OrderService.Tests
     [TestClass]
     public class OrderSagaTests
     {
+        #region members
+
         private OrderSaga orderSaga;
         private Saga Saga;
         string partnerAddress;
@@ -20,6 +22,7 @@ namespace OrderService.Tests
         string purchaseOrderNumber;
         List<Messages.OrderLine> orderLines;
 
+        #endregion
 
         [TestInitialize]
         public void Setup()
@@ -39,108 +42,57 @@ namespace OrderService.Tests
         [TestMethod]
         public void OrderSagaTest()
         {
-            OrderMessage message = new OrderMessage(purchaseOrderNumber, partnerId, true, DateTime.Now, orderLines);
-
             Saga.WhenReceivesMessageFrom(partnerAddress)
                 .ExpectSendToDestination<OrderStatusChangedMessage>(
-                delegate(string destination, OrderStatusChangedMessage m)
-                    {
-                        return (destination == partnerAddress && Check(m, OrderStatusEnum.Recieved));
-                    }
-                )
-                .ExpectPublish<OrderStatusChangedMessage>(
-                delegate(OrderStatusChangedMessage m)
-                    {
-                        return Check(m, OrderStatusEnum.Recieved);
-                    }
-                )
-                .ExpectSend<RequestOrderAuthorizationMessage>(
-                delegate(RequestOrderAuthorizationMessage m)
-                    {
-                        return (
-                                   m.PartnerId == partnerId &&
-                                   m.OrderLines.Count == 1 &&
-                                   m.OrderLines[0].ProductId == productId &&
-                                   m.OrderLines[0].Quantity == quantity
-                               );
-                    }
-                )
-                .When(delegate { orderSaga.Handle(message); });
-
-            List<HR.Messages.OrderLine> hrLines = new List<HR.Messages.OrderLine>(1);
-            hrLines.Add(new HR.Messages.OrderLine(productId, quantity));
+                    (dest, m) => (dest == partnerAddress && Check(m, OrderStatusEnum.Recieved)))
+                .ExpectPublish<OrderStatusChangedMessage>(m => Check(m, OrderStatusEnum.Recieved))
+                .ExpectSend<RequestOrderAuthorizationMessage>(m => Check(m))
+                .When(() => orderSaga.Handle(CreateRequest()));
 
             Saga.ExpectSendToDestination<OrderStatusChangedMessage>(
-                delegate(string destination, OrderStatusChangedMessage m)
-                {
-                    return (destination == partnerAddress && Check(m, OrderStatusEnum.Accepted));
-                }
-                )
-                .ExpectPublish<OrderStatusChangedMessage>(
-                delegate(OrderStatusChangedMessage m)
-                {
-                    return Check(m, OrderStatusEnum.Accepted);
-                }
-                )
-            .When(delegate { orderSaga.Handle(new OrderAuthorizationResponseMessage(orderSaga.Entity.Id, true, hrLines)); });
+                    (dest, m) => (dest == partnerAddress && Check(m, OrderStatusEnum.Accepted)))
+                .ExpectPublish<OrderStatusChangedMessage>(m => Check(m, OrderStatusEnum.Accepted))
+            .When(() => orderSaga.Handle(CreateResponse()));
         }
 
         [TestMethod]
         public void TimeoutTest()
         {
-            OrderMessage message = new OrderMessage(purchaseOrderNumber, partnerId, true, DateTime.Now, orderLines);
             object state = null;
 
             Saga.WhenReceivesMessageFrom(partnerAddress)
                 .ExpectSendToDestination<OrderStatusChangedMessage>(
-                delegate(string destination, OrderStatusChangedMessage m)
-                {
-                    return (destination == partnerAddress && Check(m, OrderStatusEnum.Recieved));
-                }
-                )
-                .ExpectPublish<OrderStatusChangedMessage>(
-                delegate(OrderStatusChangedMessage m)
-                {
-                    return Check(m, OrderStatusEnum.Recieved);
-                }
-                )
-                .ExpectSend<RequestOrderAuthorizationMessage>(
-                delegate(RequestOrderAuthorizationMessage m)
-                {
-                    return (
-                               m.PartnerId == partnerId &&
-                               m.OrderLines.Count == 1 &&
-                               m.OrderLines[0].ProductId == productId &&
-                               m.OrderLines[0].Quantity == quantity
-                           );
-                }
-                )
-                .ExpectSend<TimeoutMessage>(
-                delegate(TimeoutMessage m)
-                    {
-                        state = m.State;
-
-                        return m.SagaId == orderSaga.Entity.Id;
-                    }
-                )
-                .When(delegate { orderSaga.Handle(message); });
+                    (dest, m) => (dest == partnerAddress && Check(m, OrderStatusEnum.Recieved)))
+                .ExpectPublish<OrderStatusChangedMessage>(m => Check(m, OrderStatusEnum.Recieved))
+                .ExpectSend<RequestOrderAuthorizationMessage>(m => Check(m))
+                .ExpectSend<TimeoutMessage>(m => { state = m.State; return m.SagaId == orderSaga.Entity.Id; })
+                .When(() => orderSaga.Handle(CreateRequest()));
 
             Saga.ExpectSendToDestination<OrderStatusChangedMessage>(
-                delegate(string destination, OrderStatusChangedMessage m)
-                {
-                    return (destination == partnerAddress && BasicCheck(m, OrderStatusEnum.Accepted));
-                }
-                )
-                .ExpectPublish<OrderStatusChangedMessage>(
-                delegate(OrderStatusChangedMessage m)
-                {
-                    return BasicCheck(m, OrderStatusEnum.Accepted);
-                }
-                )
-            .When(delegate { orderSaga.Timeout(state); });
+                    (dest, m) => (dest == partnerAddress && Check(m, OrderStatusEnum.Accepted)))
+                .ExpectPublish<OrderStatusChangedMessage>(m => BasicCheck(m, OrderStatusEnum.Accepted))
+            .When(() => orderSaga.Timeout(state));
 
             Assert.IsTrue(orderSaga.Completed);
 
+        }
+
+        #region helper methods
+
+        private OrderMessage CreateRequest()
+        {
+            return new OrderMessage(purchaseOrderNumber, partnerId, true, DateTime.Now, orderLines);
+        }
+
+        private OrderAuthorizationResponseMessage CreateResponse()
+        {
+            List<HR.Messages.OrderLine> hrLines = new List<HR.Messages.OrderLine>(1);
+            hrLines.Add(new HR.Messages.OrderLine(productId, quantity));
+
+            return new OrderAuthorizationResponseMessage(
+                orderSaga.Entity.Id, 
+                true, 
+                hrLines);
         }
 
         private bool Check(OrderStatusChangedMessage m, OrderStatusEnum status)
@@ -155,6 +107,16 @@ namespace OrderService.Tests
                    );
         }
 
+        private bool Check(RequestOrderAuthorizationMessage m)
+        {
+            return (
+                       m.PartnerId == partnerId &&
+                       m.OrderLines.Count == 1 &&
+                       m.OrderLines[0].ProductId == productId &&
+                       m.OrderLines[0].Quantity == quantity
+                   );
+        }
+
         private bool BasicCheck(OrderStatusChangedMessage m, OrderStatusEnum status)
         {
             return (
@@ -163,6 +125,7 @@ namespace OrderService.Tests
                        m.Status == status
                    );
         }
+        #endregion
 
     }
 }
