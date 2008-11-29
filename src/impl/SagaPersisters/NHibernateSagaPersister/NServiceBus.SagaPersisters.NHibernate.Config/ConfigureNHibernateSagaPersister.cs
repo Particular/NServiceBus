ca548a9f5ca5667028f99b18523a6489abad1771
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2008-11-29 21:09:08 +0000

    Added files in previously forgotten.

---
+++ src/impl/SagaPersisters/NHibernateSagaPersister/NServiceBus.SagaPersisters.NHibernate.Config/ConfigureNHibernateSagaPersister.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using ObjectBuilder;
+using NServiceBus.SagaPersisters.NHibernate;
+
+namespace NServiceBus.Config
+{
+    public static class ConfigureNHibernateSagaPersister
+    {
+        public static Configure NHibernateSagaPersister(this Configure config, ISessionFactory sessionFactory)
+        {
+            config.builder.ConfigureComponent<SagaPersister>(ComponentCallModelEnum.Singlecall)
+                .SessionFactory = sessionFactory;
+
+            config.builder.ConfigureComponent<NHibernateMessageModule>(ComponentCallModelEnum.Singleton)
+                .SessionFactory = sessionFactory;
+
+            return config;
+        }
+    }
+}
---
+++ src/unicast/NServiceBus.Unicast.Config/ConfigureUnicastBus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NServiceBus.Unicast.Config;
+
+namespace NServiceBus.Config
+{
+    public static class ConfigureUnicastBus
+    {
+        public static ConfigUnicastBus UnicastBus(this NServiceBus.Config.Configure config)
+        {
+            ConfigUnicastBus cfg = new ConfigUnicastBus();
+            cfg.Configure(config.builder);
+
+            return cfg;
+        } 
+    }
+}
