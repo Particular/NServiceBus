38447c525186434d02e8a77de7e988914d8516b6
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2008-12-01 22:24:56 +0000

    Shortened some test code.

--- src/impl/Serializers/NServiceBus.Serializers.InterfacesToXML.Test/Class1.cs
+++ src/impl/Serializers/NServiceBus.Serializers.InterfacesToXML.Test/Class1.cs
@@ -40,7 +40,7 @@ namespace NServiceBus.Serializers.InterfacesToXML.Test
             o.Address = Guid.NewGuid().ToString();
             o.Int = 7;
             o.Name = "udi";
-            o.Risk = new Risk(0.15D, true);
+            o.Risk = new Risk { Percent = 0.15D, Annum = true };
             o.Some = SomeEnum.B;
             o.Start = DateTime.Now;
             o.Duration = TimeSpan.Parse("01:15:27.123");
@@ -51,7 +51,7 @@ namespace NServiceBus.Serializers.InterfacesToXML.Test
             o.Parent.Address = Guid.NewGuid().ToString();
             o.Parent.Int = 7;
             o.Parent.Name = "-1";
-            o.Parent.Risk = new Risk(0.15D, true);
+            o.Parent.Risk = new Risk { Percent = 0.15D, Annum = true };
 
             o.Names = new List<IM1>();
             for (int i = 0; i < number; i++)
@@ -62,7 +62,7 @@ namespace NServiceBus.Serializers.InterfacesToXML.Test
                 m1.Address = Guid.NewGuid().ToString();
                 m1.Int = 7;
                 m1.Name = i.ToString();
-                m1.Risk = new Risk(0.15D, true);
+                m1.Risk = new Risk { Percent = 0.15D, Annum = true };
             }
 
             IMessage[] messages = new IMessage[] {o, o, o};
@@ -137,7 +137,7 @@ namespace NServiceBus.Serializers.InterfacesToXML.Test
             o.Address = Guid.NewGuid().ToString();
             o.Int = 7;
             o.Name = "udi";
-            o.Risk = new Risk(0.15D, true);
+            o.Risk = new Risk { Percent = 0.15D, Annum = true };
             o.Some = SomeEnum.B;
 
             o.Parent = new M1();
@@ -146,7 +146,7 @@ namespace NServiceBus.Serializers.InterfacesToXML.Test
             o.Parent.Address = Guid.NewGuid().ToString();
             o.Parent.Int = 7;
             o.Parent.Name = "-1";
-            o.Parent.Risk = new Risk(0.15D, true);
+            o.Parent.Risk = new Risk { Percent = 0.15D, Annum = true };
 
             o.Names = new List<M1>();
             for (int i = 0; i < number; i++)
@@ -157,7 +157,7 @@ namespace NServiceBus.Serializers.InterfacesToXML.Test
                 m1.Address = Guid.NewGuid().ToString();
                 m1.Int = 7;
                 m1.Name = i.ToString();
-                m1.Risk = new Risk(0.15D, true);
+                m1.Risk = new Risk { Percent = 0.15D, Annum = true };
             }
 
             return o;
@@ -219,115 +219,29 @@ namespace NServiceBus.Serializers.InterfacesToXML.Test
     [Serializable]
     public class M2 : M1
     {
-        private Guid id;
-        private List<M1> names;
-        private M1 parent;
-        private SomeEnum some;
-        private DateTime start;
-        private TimeSpan duration;
-
-        public Guid Id
-        {
-            get { return id; }
-            set { id = value; }
-        }
-
-        public List<M1> Names
-        {
-            get { return names; }
-            set { names = value; }
-        }
-
-        public M1 Parent
-        {
-            get { return parent; }
-            set { parent = value; }
-        }
-
-        public SomeEnum Some
-        {
-            get { return some; }
-            set { some = value; }
-        }
-
-        public DateTime Start
-        {
-            get { return start; }
-            set { start = value; }
-        }
-
-        public TimeSpan Duration
-        {
-            get { return duration; }
-            set { duration = value; }
-        }
+        public Guid Id { get; set; }
+        public List<M1> Names { get; set; }
+        public M1 Parent { get; set; }
+        public SomeEnum Some { get; set; }
+        public DateTime Start { get; set; }
+        public TimeSpan Duration { get; set; }
     }
 
     [Serializable]
     public class M1 : IM1
     {
-        private float age;
-        private int intt;
-        private string name;
-        private string address;
-        private Risk risk;
-
-        public float Age
-        {
-            get { return age; }
-            set { age = value; }
-        }
-
-        public int Int
-        {
-            get { return intt; }
-            set { intt = value; }
-        }
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        public string Address
-        {
-            get { return address; }
-            set { address = value; }
-        }
-
-        public Risk Risk
-        {
-            get { return risk; }
-            set { risk = value; }
-        }
+        public float Age { get; set; }
+        public int Int { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public Risk Risk { get; set; }
     }
 
     [Serializable]
     public class Risk
     {
-        public Risk() { }
-        public Risk(double percent, bool annnum)
-        {
-            this.percent = percent;
-            this.annum = annnum;
-        }
-
-        private double percent;
-
-        public bool Annum
-        {
-            get { return annum; }
-            set { annum = value; }
-        }
-
-        public double Percent
-        {
-            get { return percent; }
-            set { percent = value; }
-        }
-
-        private bool annum;
+        public bool Annum { get; set; }
+        public double Percent { get; set; }
     }
 
     public enum SomeEnum
