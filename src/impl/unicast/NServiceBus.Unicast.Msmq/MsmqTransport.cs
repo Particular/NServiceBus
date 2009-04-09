256a2cb587571af17598398b7f010e31a60a662b
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-04-09 22:08:11 +0000

    Rephrased exception message.

--- src/impl/unicast/NServiceBus.Unicast.Msmq/MsmqTransport.cs
+++ src/impl/unicast/NServiceBus.Unicast.Msmq/MsmqTransport.cs
@@ -256,7 +256,7 @@ namespace NServiceBus.Unicast.Transport.Msmq
             string machine = GetMachineNameFromLogicalName(InputQueue);
 
             if (machine.ToLower() != Environment.MachineName.ToLower())
-                throw new InvalidOperationException("Input queue is located on a different machine. This is only supported on MSMQ 4 (Windows Server 2008 or Vista).");
+                throw new InvalidOperationException("Input queue must be on the same machine as this process.");
         }
 
         private void CreateQueuesIfNecessary()
