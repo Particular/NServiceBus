bdf8959eccfa612fc3317064088cd804b4583c86
Ohlund, Andreas <andreas_ohlund@hotmail.com>
2009-05-14 19:46:56 +0000

    Changed .bat files to use nant for consistent CI and local builds

--- build_samples.bat
+++ build_samples.bat
@@ -1,11 +1,1 @@
-cd Samples\AsyncPages
-msbuild
-cd ..\..\Samples\WebServiceBridge
-msbuild
-cd ..\..\Samples\FullDuplex
-msbuild
-cd ..\..\Samples\PubSub
-msbuild
-cd ..\..\Samples\Manufacturing
-msbuild
-cd ..\..\
+.\tools\nant\nant compile_samples
