33f2ffeb9c8608bdc4afba7cf08d7dffe1ce2c93
Ohlund, Andreas <andreas_ohlund@hotmail.com>
2009-05-14 18:19:29 +0000

    Added support for StructureMap

---
+++ src/impl/ObjectBuilder/ObjectBuilder.StructureMap/Config/ConfigureStructureMapBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NServiceBus.ObjectBuilder;
+using NServiceBus.ObjectBuilder.Common.Config;
+using NServiceBus.ObjectBuilder.StructureMap;
+using StructureMap;
+
+namespace NServiceBus
+{
+    public static class ConfigureStructureMapBuilder
+    {
+        public static Configure StructureMapBuilder(this Configure config, params Action<IConfigureComponents>[] configActions)
+        {
+            ConfigureCommon.With(config, new StructureMapObjectBuilder(), configActions);
+
+            return config;
+        }
+
+        public static Configure CastleWindsorBuilder(this Configure config, IContainer container, params Action<IConfigureComponents>[] configActions)
+        {
+            ConfigureCommon.With(config, new StructureMapObjectBuilder(container), configActions);
+
+            return config;
+        }
+
+
+    }
+}
+
---
+++ src/impl/ObjectBuilder/ObjectBuilder.StructureMap/StructureMapObjectBuilder.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Logging;
+using StructureMap;
+using StructureMap.Attributes;
+using StructureMap.Graph;
+using StructureMap.Interceptors;
+using StructureMap.Pipeline;
+using IContainer = StructureMap.IContainer;
+
+namespace NServiceBus.ObjectBuilder.StructureMap
+{
+    public class StructureMapObjectBuilder : Common.IContainer
+    {
+
+        private readonly IContainer container;
+        private readonly Dictionary<Type, ConfiguredInstance> configuredInstances = new Dictionary<Type, ConfiguredInstance>();
+
+        public StructureMapObjectBuilder()
+        {
+            container = ObjectFactory.GetInstance<IContainer>();
+        }
+
+        public StructureMapObjectBuilder(IContainer container)
+        {
+            this.container = container;
+        }
+
+
+        public object Build(Type typeToBuild)
+        {
+            return container.GetInstance(typeToBuild);
+        }
+
+        public IEnumerable<object> BuildAll(Type typeToBuild)
+        {
+            return container.GetAllInstances(typeToBuild).Cast<object>();
+        }
+
+
+        public void ConfigureProperty(Type component, string property, object value)
+        {
+            if (value == null)
+            {
+                return;
+            }
+
+
+            LogManager.GetLogger("ObjectBuilder").Debug("Configuring property for " + component.Name +
+                                                                ", propertyName: " + property +
+                                                                " withvalue: " + value);
+            lock (configuredInstances)
+            {
+                ConfiguredInstance configuredInstance;
+                configuredInstances.TryGetValue(component, out configuredInstance);
+
+                if (configuredInstance == null)
+                    throw new InvalidOperationException("Cannot configure property before the component has been configured. Please call 'Configure' first.");
+
+                if (value.GetType().IsPrimitive || value is string)
+                    configuredInstance.WithProperty(property).EqualTo(value);
+                else
+                    configuredInstance.Child(property).Is(value);
+
+            }
+        }
+
+        public void Configure(Type component, ComponentCallModelEnum callModel)
+        {
+            var scope = GetInstanceScopeFrom(callModel);
+
+            ConfiguredInstance configuredInstance = null;
+
+            container.Configure(x =>
+            {
+                configuredInstance = x.ForRequestedType(component)
+                     .CacheBy(scope)
+                     .TheDefaultIsConcreteType(component);
+
+                foreach (var serviceType in GetAllInterfacesImplementedBy(component))
+                {
+                    x.ForRequestedType(serviceType)
+                        .TheDefaultIsConcreteType(component)
+                        .Interceptor = new RedirectToConcreteTypeInterceptor(component, container);
+
+                    PluginCache.AddFilledType(serviceType);
+                }
+
+
+            });
+
+            lock (configuredInstances)
+                configuredInstances.Add(component, configuredInstance);
+        }
+
+        private static InstanceScope GetInstanceScopeFrom(ComponentCallModelEnum callModel)
+        {
+            switch (callModel)
+            {
+                case ComponentCallModelEnum.Singlecall: return InstanceScope.PerRequest;
+                case ComponentCallModelEnum.Singleton: return InstanceScope.Singleton;
+            }
+
+            throw new ArgumentException("Unhandled call model:" + callModel);
+        }
+
+        private static IEnumerable<Type> GetAllInterfacesImplementedBy(Type t)
+        {
+            var result = new List<Type>();
+
+            if (t == null)
+                return result;
+
+            var interfaces = t.GetInterfaces().Where(x => (
+                !x.IsGenericType
+                ));
+            result.AddRange(interfaces);
+
+            foreach (Type interfaceType in interfaces)
+                result.AddRange(GetAllInterfacesImplementedBy(interfaceType));
+
+            return result;
+        }
+    }
+
+    public class RedirectToConcreteTypeInterceptor : InstanceInterceptor
+    {
+        private readonly Type baseType;
+        private readonly IContainer container;
+
+
+
+
+        public RedirectToConcreteTypeInterceptor(Type baseType, IContainer container)
+        {
+            this.baseType = baseType;
+            this.container = container;
+        }
+
+
+        public object Process(object target, IContext context)
+        {
+            return container.GetInstance(baseType);
+        }
+
+    }
+
+}
