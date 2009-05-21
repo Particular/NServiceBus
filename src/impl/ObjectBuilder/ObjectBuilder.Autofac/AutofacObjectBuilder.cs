34b0aa2b7a86173853d1f1a52ade52aa99ac5d9a
Burton, Matt <matt.burton@gmail.com>
2009-05-21 07:33:38 +0000

    - committing Autofac support

---
+++ src/impl/ObjectBuilder/ObjectBuilder.Autofac/AutofacObjectBuilder.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using Autofac.Builder;
+using Autofac.Modules;
+
+namespace NServiceBus.ObjectBuilder.Autofac
+{
+    ///<summary>
+    /// Autofac implementation of IBuilderInternal.
+    ///</summary>
+    public class AutofacObjectBuilder : Common.IContainer
+    {
+        ///<summary>
+        /// Instantiates the class saving the given container.
+        ///</summary>
+        ///<param name="container"></param>
+        public AutofacObjectBuilder(IContainer container)
+        {
+            this.Container = container;
+            this.Initialize();
+        }
+
+        ///<summary>
+        /// Instantites the class with a new Autofac container.
+        ///</summary>
+        public AutofacObjectBuilder() : this(new Container())
+        {
+        }
+
+        /// <summary>
+        /// The container itself.
+        /// </summary>
+        public IContainer Container { get; set; }
+
+        public object Build(Type typeToBuild)
+        {
+            return this.Container.Resolve(typeToBuild);
+        }
+
+        public IEnumerable<object> BuildAll(Type typeToBuild)
+        {
+            return this.Container.ResolveAll(typeToBuild);
+        }
+
+        void Common.IContainer.Configure(Type component, ComponentCallModelEnum callModel)
+        {
+            IComponentRegistration registration = this.GetComponentRegistration(component);
+
+            if (registration == null)
+            {
+                var builder = new ContainerBuilder();
+                InstanceScope instanceScope = GetInstanceScopeFrom(callModel);
+                Type[] services = component.GetAllServices().ToArray();
+                builder.Register(component).As(services).WithScope(instanceScope).OnActivating(ActivatingHandler.InjectUnsetProperties);
+                builder.Build(this.Container);
+            }
+        }
+
+        public void ConfigureProperty(Type component, string property, object value)
+        {
+            var registration = this.GetComponentRegistration(component);
+
+            if (registration == null)
+            {
+                return;
+            }
+
+            registration.Activating += (sender, e) => e.Instance.SetPropertyValue(property, value);
+        }
+
+        public void RegisterSingleton(Type lookupType, object instance)
+        {
+            var builder = new ContainerBuilder();
+            builder.Register(instance).As(lookupType).ExternallyOwned().OnActivating(ActivatingHandler.InjectUnsetProperties);
+            builder.Build(this.Container);
+        }
+
+        private void Initialize()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new ImplicitCollectionSupportModule());
+            builder.Build(this.Container);
+        }
+
+        private static InstanceScope GetInstanceScopeFrom(ComponentCallModelEnum callModel)
+        {
+            switch (callModel)
+            {
+                case ComponentCallModelEnum.Singlecall:
+                    return InstanceScope.Factory;
+                case ComponentCallModelEnum.Singleton:
+                    return InstanceScope.Singleton;
+            }
+
+            return InstanceScope.Container;
+        }
+
+        private IComponentRegistration GetComponentRegistration(Type concreteComponent)
+        {
+            return this.Container.GetAllRegistrations().Where(x => x.Descriptor.BestKnownImplementationType == concreteComponent).FirstOrDefault();
+        }
+    }
+}
---
+++ src/impl/ObjectBuilder/ObjectBuilder.Autofac/ContainerExtensions.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+
+namespace NServiceBus.ObjectBuilder.Autofac
+{
+    ///<summary>
+    /// Autofac IContainer extensions
+    ///</summary>
+    public static class ContainerExtensions
+    {
+        ///<summary>
+        /// Retrieve all registrations across container hierarchies as a single sequence
+        ///</summary>
+        ///<param name="container"></param>
+        ///<returns></returns>
+        public static IEnumerable<IComponentRegistration> GetAllRegistrations(this IContainer container)
+        {
+            var registrations = container.ComponentRegistrations;
+
+            while (container.OuterContainer != null)
+            {
+                container = container.OuterContainer;
+                registrations = registrations.Union(container.ComponentRegistrations);
+            }
+
+            return registrations;
+        }
+
+        ///<summary>
+        /// Resolve all components registered for the type.
+        ///</summary>
+        ///<param name="container"></param>
+        ///<param name="componentType"></param>
+        ///<returns></returns>
+        public static IEnumerable<object> ResolveAll(this IContainer container, Type componentType)
+        {
+            return container.Resolve(typeof(IEnumerable<>).MakeGenericType(componentType)) as IEnumerable<object>;
+        }
+    }
+}
