5cf48a1ab708e2e70ce56cde7dd1fb1067c96a96
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-06-16 13:39:51 +0000

    Cleaned up usings (back to Resharper after DevExpress Refactor didn't do it for me).

--- src/impl/unicast/NServiceBus.Unicast.Msmq/NServiceBus.Unicast.Transport.Msmq.Config/ConfigureMsmqTransport.cs
+++ src/impl/unicast/NServiceBus.Unicast.Msmq/NServiceBus.Unicast.Transport.Msmq.Config/ConfigureMsmqTransport.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NServiceBus.Unicast.Transport.Msmq.Config;
+﻿using NServiceBus.Unicast.Transport.Msmq.Config;
 
 namespace NServiceBus
 {
