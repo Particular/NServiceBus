f2f50cc0991dd11183c697c83c2456844bee263c
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-06-21 09:13:15 +0000

    Cleaned up unused "usings"

--- Samples/Manufacturing/OrderService/OrderService.Host/Program.cs
+++ Samples/Manufacturing/OrderService/OrderService.Host/Program.cs
@@ -1,11 +1,8 @@
 using System;
 using Common.Logging;
-using NHibernate.Cfg;
 using NServiceBus;
 using NServiceBus.Grid.MessageHandlers;
 using NServiceBus.Saga;
-using NHibernate;
-using NServiceBus.ObjectBuilder;
 
 
 namespace OrderService.Host
