19fee96948403c5db77c197cc0299e7ad7440e07
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-07-01 08:49:34 +0000

    Re-ordered build so that host will be built before tools (as tools are now starting to use the host).

---
+++ src/timeout/Timeout.MessageHandlers/Endpoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using NServiceBus;
+using NServiceBus.Host;
+
+namespace Timeout.MessageHandlers
+{
+    class Endpoint : IConfigureThisEndpoint,
+        As.aServer,
+        ISpecify.MyOwnSerialization
+    {
+        public void Init(Configure configure)
+        {
+            string nameSpace = ConfigurationManager.AppSettings["NameSpace"];
+            string serialization = ConfigurationManager.AppSettings["Serialization"];
+
+            switch (serialization)
+            {
+                case "xml":
+                    configure.XmlSerializer(nameSpace);
+                    break;
+                case "binary":
+                    configure.BinarySerializer();
+                    break;
+                default:
+                    throw new ConfigurationErrorsException("Serialization can only be one of 'interfaces', 'xml', or 'binary'.");
+            }
+        }
+    }
+}
