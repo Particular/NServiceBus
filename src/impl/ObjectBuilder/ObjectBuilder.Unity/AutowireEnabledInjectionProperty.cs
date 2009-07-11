fd264c3532b579b5062c4b452a325bf2e6e04d3d
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-07-11 08:17:12 +0000

    Brought in UnityBuilder again, this time with autowire functionality inside the same project.

---
+++ src/impl/ObjectBuilder/ObjectBuilder.Unity/AutowireEnabledInjectionProperty.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Microsoft.Practices.ObjectBuilder2;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.ObjectBuilder;
+
+namespace NServiceBus.ObjectBuilder.Unity
+{
+   /// <summary>
+   /// Specialy prepared derivative of <see cref="InjectionProperty"/> adjusted to cooperate with 
+   /// autowire infrastructure. Does not replace property selector policy for 
+   /// <see cref="SpecifiedPropertiesSelectorPolicy"/> but, instead, uses 
+   /// <see cref="FullAutowirePropertySelectorPolicy"/>'s
+   /// <see cref="FullAutowirePropertySelectorPolicy.SpecifiedPropertiesPolicy"/>.
+   /// TODO: This class needs cleaning up the exception throwing code to include more information
+   /// for users.
+   /// </summary>
+   /// <remarks>
+   /// Based on Unity's <see cref="InjectionProperty"/> class.
+   /// </remarks>   
+   public class AutowireEnabledInjectionProperty : InjectionMember
+   {
+      private readonly string propertyName;
+      private InjectionParameterValue parameterValue;
+
+      /// <summary>
+      /// Configure the container to inject the given property name,
+      /// resolving the value via the container.
+      /// </summary>
+      /// <param name="propertyName">Name of the property to inject.</param>
+      public AutowireEnabledInjectionProperty(string propertyName)
+      {
+         this.propertyName = propertyName;
+      }
+
+      /// <summary>
+      /// Configure the container to inject the given property name,
+      /// using the value supplied. This value is converted to an
+      /// <see cref="InjectionParameterValue"/> object using the
+      /// rules defined by the <see cref="InjectionParameterValue.ToParameters"/>
+      /// method.
+      /// </summary>
+      /// <param name="propertyName">Name of property to inject.</param>
+      /// <param name="propertyValue">Value for property.</param>
+      public AutowireEnabledInjectionProperty(string propertyName, object propertyValue)
+      {
+         this.propertyName = propertyName;
+         parameterValue = InjectionParameterValue.ToParameter(propertyValue);
+      }
+      /// <summary>
+      /// Add policies to the <paramref name="policies"/> to configure the
+      /// container to call this constructor with the appropriate parameter values.
+      /// </summary>
+      /// <param name="typeToCreate">Type to register.</param>
+      /// <param name="name">Name used to resolve the type object.</param>
+      /// <param name="policies">Policy list to add policies to.</param>
+      [SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods",
+          Justification = "Validation is done via Guard class")]
+      public override void AddPolicies(Type typeToCreate, string name, IPolicyList policies)
+      {
+         Guard.ArgumentNotNull(typeToCreate, "typeToCreate");
+         PropertyInfo propInfo = typeToCreate.GetProperty(propertyName);
+         GuardPropertyExists(propInfo, typeToCreate, propertyName);
+         GuardPropertyIsSettable(propInfo);
+         GuardPropertyIsNotIndexer(propInfo);
+         InitializeParameterValue(propInfo);
+         GuardPropertyValueIsCompatible(propInfo, parameterValue);
+
+         SpecifiedPropertiesSelectorPolicy selector =
+             GetSelectorPolicy(policies, typeToCreate, name);
+
+         selector.AddPropertyAndValue(propInfo, parameterValue);
+      }
+
+      private InjectionParameterValue InitializeParameterValue(PropertyInfo propInfo)
+      {
+         if (parameterValue == null)
+         {
+            parameterValue = new ResolvedParameter(propInfo.PropertyType);
+         }
+         return parameterValue;
+      }
+      
+      private static SpecifiedPropertiesSelectorPolicy GetSelectorPolicy(IPolicyList policies, Type typeToInject, string name)
+      {
+         NamedTypeBuildKey key = new NamedTypeBuildKey(typeToInject, name);
+         IPropertySelectorPolicy selector =
+             policies.GetNoDefault<IPropertySelectorPolicy>(key, false);
+
+         if (selector == null)
+         {
+            FullAutowirePropertySelectorPolicy defaultSelector =
+               policies.Get<IPropertySelectorPolicy>(key, false) as FullAutowirePropertySelectorPolicy;
+            if (defaultSelector != null)
+            {
+               selector = defaultSelector.Clone();
+               policies.Set(selector, key);
+            }
+            else
+            {
+               throw new InvalidOperationException("Cannot use AutiviewEnabledInjectionProperty without FullAutowireContainerExtension. Please register FullAutowireContainerExtension extension in the container.");
+            }
+         }
+         return ((FullAutowirePropertySelectorPolicy)selector).SpecifiedPropertiesPolicy;
+      }
+
+      private static void GuardPropertyExists(PropertyInfo propInfo, Type typeToCreate, string propertyName)
+      {
+         if (propInfo == null)
+         {
+            throw new InvalidOperationException();
+         }
+      }
+
+      private static void GuardPropertyIsSettable(PropertyInfo propInfo)
+      {
+         if (!propInfo.CanWrite)
+         {
+            throw new InvalidOperationException();
+         }
+      }
+
+      private static void GuardPropertyIsNotIndexer(PropertyInfo property)
+      {
+         if (property.GetIndexParameters().Length > 0)
+         {
+            throw new InvalidOperationException();
+         }
+      }
+      private static void GuardPropertyValueIsCompatible(PropertyInfo property, InjectionParameterValue value)
+      {
+         if (!value.MatchesType(property.PropertyType))
+         {
+            throw new InvalidOperationException();
+         }
+      }     
+   }
+}
---
+++ src/impl/ObjectBuilder/ObjectBuilder.Unity/UnityObjectBuilder.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Practices.Unity;
+using NServiceBus.ObjectBuilder.Common;
+
+namespace NServiceBus.ObjectBuilder.Unity
+{
+   public class UnityObjectBuilder : IContainer
+   {
+      /// <summary>
+      /// The container itself.
+      /// </summary>
+      private readonly IUnityContainer container;
+
+      /// <summary>
+      /// Instantites the class with a new UnityContainer.
+      /// </summary>
+      public UnityObjectBuilder()
+         : this(new UnityContainer())
+      {
+      }
+
+      /// <summary>
+      /// Instantiates the class saving the given container.
+      /// </summary>
+      /// <param name="container"></param>
+      public UnityObjectBuilder(IUnityContainer container)
+      {
+         this.container = container;
+         var autowireContainerExtension = this.container.Configure<FullAutowireContainerExtension>();
+         if (autowireContainerExtension == null)
+         {
+            this.container.AddNewExtension<FullAutowireContainerExtension>();
+         }
+      }
+
+      public object Build(Type typeToBuild)
+      {
+         return container.Resolve(typeToBuild);
+      }
+
+      public IEnumerable<object> BuildAll(Type typeToBuild)
+      {
+         foreach (var component in container.ResolveAll(typeToBuild))
+         {
+            yield return component;
+         }
+      }
+
+      public void Configure(Type concreteComponent, ComponentCallModelEnum callModel)
+      {
+         ConfigureComponentAdapter config =
+            container.ResolveAll<ConfigureComponentAdapter>().Where(x => x.ConfiguredType == concreteComponent).
+               FirstOrDefault();
+         if (config == null)
+         {
+            IEnumerable<Type> interfaces = GetAllServiceTypesFor(concreteComponent);
+            config = new ConfigureComponentAdapter(container, concreteComponent);
+            container.RegisterInstance(Guid.NewGuid().ToString(), config);
+
+            foreach (Type t in interfaces)
+            {
+               container.RegisterType(t, concreteComponent, GetLifetimeManager(callModel));
+            }
+         }          
+      }
+
+      public void ConfigureProperty(Type concreteComponent, string property, object value)
+      {
+         ConfigureComponentAdapter config =
+            container.ResolveAll<ConfigureComponentAdapter>().Where(x => x.ConfiguredType == concreteComponent).
+               First();
+         config.ConfigureProperty(property, value);
+      }
+
+      public void RegisterSingleton(Type lookupType, object instance)
+      {
+         container.RegisterInstance(lookupType, instance);
+      }
+
+      private static IEnumerable<Type> GetAllServiceTypesFor(Type t)
+      {
+         if (t == null)
+         {
+            return new List<Type>();
+         }
+
+         var result = new List<Type>(t.GetInterfaces());
+         result.Add(t);
+
+         foreach (Type interfaceType in t.GetInterfaces())
+         {
+            result.AddRange(GetAllServiceTypesFor(interfaceType));
+         }
+
+         return result;
+      }
+
+      private static LifetimeManager GetLifetimeManager(ComponentCallModelEnum callModel)
+      {
+         switch (callModel)
+         {
+            case ComponentCallModelEnum.Singlecall:
+               return new TransientLifetimeManager();
+            case ComponentCallModelEnum.Singleton:
+               return new ContainerControlledLifetimeManager();
+            default:
+               return new TransientLifetimeManager();
+         }
+
+      }
+   }
+}
