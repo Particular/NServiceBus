c757985e12ecc0b9c566906a17f3092ea6e20b41
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-07-15 06:29:16 +0000

    Made subscriber2 in the pub/sub sample use the Unity container just to show we can mix and match containers.

--- Samples/PubSub/Subscriber2/EndpointConfig.cs
+++ Samples/PubSub/Subscriber2/EndpointConfig.cs
@@ -1,10 +1,12 @@
 ﻿using NServiceBus.Host;
+using NServiceBus.ObjectBuilder.Unity;
 
 namespace Subscriber2
 {
     class EndpointConfig : IConfigureThisEndpoint, 
         As.aServer,
-        ISpecify.ToUseXmlSerialization
+        ISpecify.ToUseXmlSerialization, 
+        ISpecify.ContainerTypeToUse<UnityObjectBuilder> //just to show we can mix and match containers
     {
     }
 }
