a2d0cd21dfa07a437a5d9e762b5b627045723f2d
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-07-17 22:15:46 +0000

    Added Rijndael implementation of encryption.

---
+++ src/impl/encryption/NServiceBus.Encryption.Rijndael/EncryptionService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace NServiceBus.Encryption.Rijndael
+{
+    /// <summary>
+    /// Implementation of the encryption capability using Rijndael.
+    /// Blatantly copied from https://rhino-tools.svn.sourceforge.net/svnroot/rhino-tools/trunk/esb/Rhino.ServiceBus/Impl/RijndaelEncryptionService.cs
+    /// allowable under the Apache 2.0 license.
+    /// </summary>
+    public class EncryptionService : IEncryptionService
+    {
+        /// <summary>
+        /// Symmetric key used for encryption.
+        /// </summary>
+        public byte[] Key { get; set; }
+
+        string IEncryptionService.Decrypt(EncryptedValue encryptedValue)
+        {
+            var encrypted = Convert.FromBase64String(encryptedValue.EncryptedBase64Value);
+            using (var rijndael = new RijndaelManaged())
+            {
+                rijndael.Key = Key;
+                rijndael.IV = Convert.FromBase64String(encryptedValue.Base64Iv);
+                rijndael.Mode = CipherMode.CBC;
+
+                using (var decryptor = rijndael.CreateDecryptor())
+                using (var memoryStream = new MemoryStream(encrypted))
+                using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                using (var reader = new StreamReader(cryptoStream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        EncryptedValue IEncryptionService.Encrypt(string value)
+        {
+            using (var rijndael = new RijndaelManaged())
+            {
+                rijndael.Key = Key;
+                rijndael.Mode = CipherMode.CBC;
+                rijndael.GenerateIV();
+
+                using (var encryptor = rijndael.CreateEncryptor())
+                using (var memoryStream = new MemoryStream())
+                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                using (var writer = new StreamWriter(cryptoStream))
+                {
+                    writer.Write(value);
+                    writer.Flush();
+                    cryptoStream.Flush();
+                    cryptoStream.FlushFinalBlock();
+                    return new EncryptedValue
+                    {
+                        EncryptedBase64Value = Convert.ToBase64String(memoryStream.ToArray()),
+                        Base64Iv = Convert.ToBase64String(rijndael.IV)
+                    };
+                }
+            }
+        }
+    }
+}
