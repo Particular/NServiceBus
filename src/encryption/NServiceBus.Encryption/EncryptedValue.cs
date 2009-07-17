abc2ae1da6f8281b026fa910c2f5197abb5b357f
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-07-17 22:02:41 +0000

    Added encryption abstraction.

---
+++ src/encryption/NServiceBus.Encryption/EncryptedValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NServiceBus.Encryption
+{
+    /// <summary>
+    /// Class used to represent an encrypted value with an initialization vector.
+    /// </summary>
+    [Serializable]
+    public class EncryptedValue
+    {
+        /// <summary>
+        /// The encrypted value represented as a Base64 string.
+        /// </summary>
+        public string EncryptedBase64Value { get; set; }
+
+        /// <summary>
+        /// The initialization vector represented as a Base64 string.
+        /// </summary>
+        public string Base64Iv { get; set; }
+    }
+}
---
+++ src/encryption/NServiceBus.Encryption/IEncryptionService.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus.Encryption
+{
+    /// <summary>
+    /// Abstraction for encryption capabilities.
+    /// </summary>
+    public interface IEncryptionService
+    {
+        /// <summary>
+        /// Encrypts the given value returning an EncryptedValue.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        EncryptedValue Encrypt(string value);
+
+        /// <summary>
+        /// Decrypts the given EncryptedValue object returning the source string.
+        /// </summary>
+        /// <param name="encryptedValue"></param>
+        /// <returns></returns>
+        string Decrypt(EncryptedValue encryptedValue);
+    }
+}
