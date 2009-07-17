fd18769c2db4a785b7b9f4cc5d59faee80506d90
Ohlund, Andreas <andreas_ohlund@hotmail.com>
2009-07-17 21:54:58 +0000

    Refactored the bus configuration out of the generic host to make it more readable

---
+++ src/host/NServiceBus.Host/Internal/ConfigurationBuilder.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using NServiceBus.ObjectBuilder;
+using NServiceBus.ObjectBuilder.Common;
+using NServiceBus.Unicast.Config;
+using NServiceBus.Unicast.Subscriptions.Msmq;
+
+namespace NServiceBus.Host.Internal
+{
+    public class ConfigurationBuilder
+    {
+        public Configure BuildConfigurationFrom(IConfigureThisEndpoint specifier, Type endpointType)
+        {
+
+            Configure busConfiguration;
+
+            if (specifier is ISpecify.TypesToScan)
+                busConfiguration = Configure.With((specifier as ISpecify.TypesToScan).TypesToScan);
+            else
+                if (specifier is ISpecify.AssembliesToScan)
+                    busConfiguration = Configure.With(new List<Assembly>((specifier as ISpecify.AssembliesToScan).AssembliesToScan).ToArray());
+                else
+                    if (specifier is ISpecify.ProbeDirectory)
+                        busConfiguration = Configure.With((specifier as ISpecify.ProbeDirectory).ProbeDirectory);
+                    else
+                        busConfiguration = Configure.With();
+
+            IContainer container = null;
+
+
+            if (specifier is ISpecify.ContainerInstanceToUse)
+                container = (specifier as ISpecify.ContainerInstanceToUse).ContainerInstance;
+
+            Type containerType = null;
+            Type messageEndpointType = null;
+
+            foreach (var t in endpointType.GetInterfaces())
+            {
+                var args = t.GetGenericArguments();
+                if (args.Length == 1)
+                {
+                    if (typeof(IContainer).IsAssignableFrom(args[0]))
+                        if (typeof(ISpecify.ContainerTypeToUse<>).MakeGenericType(args[0]).IsAssignableFrom(endpointType))
+                            containerType = args[0];
+
+                    if (typeof(IMessageEndpoint).IsAssignableFrom(args[0]))
+                        if (typeof(ISpecify.ToRun<>).MakeGenericType(args[0]).IsAssignableFrom(endpointType))
+                            messageEndpointType = args[0];
+                }
+            }
+
+            if (container != null)
+            {
+                ObjectBuilder.Common.Config.ConfigureCommon.With(busConfiguration, container);
+            }
+            else if (containerType != null)
+                ObjectBuilder.Common.Config.ConfigureCommon.With(
+                    busConfiguration,
+                    Activator.CreateInstance(containerType) as IContainer
+                    );
+            else
+                busConfiguration.SpringBuilder();
+
+            if (specifier is As.aClient && specifier is As.aServer)
+                throw new InvalidOperationException("Cannot specify endpoint both as a client and as a server.");
+
+            ConfigUnicastBus configUnicastBus = null;
+
+            if (specifier is As.aClient)
+                configUnicastBus = busConfiguration
+                    .MsmqTransport()
+                    .IsTransactional(false)
+                    .PurgeOnStartup(true)
+                    .UnicastBus()
+                    .ImpersonateSender(false);
+
+            if (specifier is As.aServer)
+            {
+                configUnicastBus = busConfiguration
+                    .MsmqTransport()
+                    .IsTransactional(true)
+                    .PurgeOnStartup(false)
+                    .Sagas()
+                    .UnicastBus()
+                    .ImpersonateSender(true);
+
+                if (!(specifier is ISpecify.MyOwnSagaPersistence))
+                    busConfiguration.Configurer.ConfigureComponent<InMemorySagaPersister>(ComponentCallModelEnum.Singleton);
+
+                if (specifier is As.aPublisher)
+                {
+                    var subscriptionConfig =
+                        Configure.GetConfigSection<Config.DbSubscriptionStorageConfig>();
+
+                    if (subscriptionConfig == null)
+                    {
+                        string q = Program.GetEndpointId(endpointType) + "_subscriptions";
+                        busConfiguration.Configurer.ConfigureComponent<MsmqSubscriptionStorage>(ComponentCallModelEnum.Singleton)
+                            .ConfigureProperty(s => s.Queue, q);
+                    }
+                    else
+                        busConfiguration.DbSubscriptionStorage();
+                }
+            }
+
+            if (configUnicastBus != null)
+            {
+                if (specifier is ISpecify.MessageHandlerOrdering)
+                    (specifier as ISpecify.MessageHandlerOrdering).SpecifyOrder(new Order { config = configUnicastBus });
+                else
+                    configUnicastBus.LoadMessageHandlers();
+
+                if (specifier is IDontWantToSubscribeAutomatically)
+                    configUnicastBus.DoNotAutoSubscribe();
+            }
+
+            if (specifier is ISpecify.ToUseXmlSerialization)
+            {
+                if (specifier is ISpecify.XmlSerializationNamespace)
+                    busConfiguration.XmlSerializer((specifier as ISpecify.XmlSerializationNamespace).Namespace);
+                else
+                    busConfiguration.XmlSerializer();
+            }
+            else
+                if (!(specifier is ISpecify.MyOwnSerialization))
+                    busConfiguration.BinarySerializer();
+
+            if (specifier is IWantCustomInitialization)
+                (specifier as IWantCustomInitialization).Init(busConfiguration);
+
+            if (messageEndpointType != null)
+                Configure.TypeConfigurer.ConfigureComponent(messageEndpointType, ComponentCallModelEnum.Singleton);
+            return busConfiguration;           
+        }
+    }
+}
--- src/host/NServiceBus.Host/Internal/GenericHost.cs
+++ src/host/NServiceBus.Host/Internal/GenericHost.cs
@@ -1,13 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Reflection;
 using Common.Logging;
-using NServiceBus.ObjectBuilder.Common;
-using NServiceBus.ObjectBuilder;
-using NServiceBus.Unicast.Subscriptions.Msmq;
 using System.Collections.Specialized;
-using NServiceBus.Unicast.Config;
 
 namespace NServiceBus.Host.Internal
 {
@@ -17,9 +11,31 @@ namespace NServiceBus.Host.Internal
         {
             Trace.WriteLine("Starting host for " + endpointType.FullName);
 
-            var specifier = (IConfigureThisEndpoint)Activator.CreateInstance(endpointType);
-            Configure cfg;
+            var configurationSpecifier = (IConfigureThisEndpoint)Activator.CreateInstance(endpointType);
+          
+            ConfigureLogging(configurationSpecifier);
 
+          
+            var busConfiguration = new ConfigurationBuilder()
+                                    .BuildConfigurationFrom(configurationSpecifier,endpointType);
+          
+            Action startupAction = null;
+
+            if (configurationSpecifier is ISpecify.StartupAction)
+                startupAction = (configurationSpecifier as ISpecify.StartupAction).StartupAction;
+
+            messageEndpoint = Configure.ObjectBuilder.Build<IMessageEndpoint>();
+
+            if (!(configurationSpecifier is IDontWantTheBusStartedAutomatically))
+                busConfiguration.CreateBus().Start(startupAction);
+
+            if (messageEndpoint != null)
+                messageEndpoint.OnStart();
+        }
+
+       
+        private static void ConfigureLogging(IConfigureThisEndpoint specifier)
+        {
             if (!(specifier is IDontWantLog4Net))
             {
                 var props = new NameValueCollection();
@@ -39,134 +55,6 @@ namespace NServiceBus.Host.Internal
                     log4net.Config.BasicConfigurator.Configure(appender);
                 }
             }
-
-            if (specifier is ISpecify.TypesToScan)
-                cfg = Configure.With((specifier as ISpecify.TypesToScan).TypesToScan);
-            else
-                if (specifier is ISpecify.AssembliesToScan)
-                    cfg = Configure.With(new List<Assembly>((specifier as ISpecify.AssembliesToScan).AssembliesToScan).ToArray());
-                else
-                    if (specifier is ISpecify.ProbeDirectory)
-                        cfg = Configure.With((specifier as ISpecify.ProbeDirectory).ProbeDirectory);
-                    else
-                        cfg = Configure.With();
-
-            Action startupAction = null;
-            IContainer container = null;
-
-            if (specifier is ISpecify.StartupAction)
-                startupAction = (specifier as ISpecify.StartupAction).StartupAction;
-
-            if (specifier is ISpecify.ContainerInstanceToUse)
-                container = (specifier as ISpecify.ContainerInstanceToUse).ContainerInstance;
-            
-            Type containerType = null;
-            Type messageEndpointType = null;
-
-            foreach (var t in endpointType.GetInterfaces())
-            {
-                var args = t.GetGenericArguments();
-                if (args.Length == 1)
-                {
-                    if (typeof(IContainer).IsAssignableFrom(args[0]))
-                        if (typeof(ISpecify.ContainerTypeToUse<>).MakeGenericType(args[0]).IsAssignableFrom(endpointType))
-                            containerType = args[0];
-
-                    if (typeof(IMessageEndpoint).IsAssignableFrom(args[0]))
-                        if (typeof(ISpecify.ToRun<>).MakeGenericType(args[0]).IsAssignableFrom(endpointType))
-                            messageEndpointType = args[0];
-                }
-            }
-
-            if (container != null)
-            {
-                ObjectBuilder.Common.Config.ConfigureCommon.With(cfg, container);
-            }
-            else if (containerType != null)
-                ObjectBuilder.Common.Config.ConfigureCommon.With(
-                                cfg,
-                                Activator.CreateInstance(containerType) as IContainer
-                                );
-            else
-                cfg.SpringBuilder();
-
-            if (specifier is As.aClient && specifier is As.aServer)
-                throw new InvalidOperationException("Cannot specify endpoint both as a client and as a server.");
-
-            ConfigUnicastBus configUnicastBus = null;
-
-            if (specifier is As.aClient)
-                configUnicastBus = cfg
-                    .MsmqTransport()
-                        .IsTransactional(false)
-                        .PurgeOnStartup(true)
-                    .UnicastBus()
-                        .ImpersonateSender(false);
-
-            if (specifier is As.aServer)
-            {
-                configUnicastBus = cfg
-                    .MsmqTransport()
-                        .IsTransactional(true)
-                        .PurgeOnStartup(false)
-                    .Sagas()
-                    .UnicastBus()
-                        .ImpersonateSender(true);
-
-                if (!(specifier is ISpecify.MyOwnSagaPersistence))
-                    cfg.Configurer.ConfigureComponent<InMemorySagaPersister>(ComponentCallModelEnum.Singleton);
-
-                if (specifier is As.aPublisher)
-                {
-                    var subscriptionConfig =
-                        Configure.GetConfigSection<Config.DbSubscriptionStorageConfig>();
-
-                    if (subscriptionConfig == null)
-                    {
-                        string q = Program.GetEndpointId(endpointType) + "_subscriptions";
-                        cfg.Configurer.ConfigureComponent<MsmqSubscriptionStorage>(ComponentCallModelEnum.Singleton)
-                            .ConfigureProperty(s => s.Queue, q);
-                    }
-                    else
-                        cfg.DbSubscriptionStorage();
-                }
-            }
-
-            if (configUnicastBus != null)
-            {
-                if (specifier is ISpecify.MessageHandlerOrdering)
-                    (specifier as ISpecify.MessageHandlerOrdering).SpecifyOrder(new Order {config = configUnicastBus});
-                else
-                    configUnicastBus.LoadMessageHandlers();
-
-                if (specifier is IDontWantToSubscribeAutomatically)
-                    configUnicastBus.DoNotAutoSubscribe();
-            }
-
-            if (specifier is ISpecify.ToUseXmlSerialization)
-            {
-                if (specifier is ISpecify.XmlSerializationNamespace)
-                    cfg.XmlSerializer((specifier as ISpecify.XmlSerializationNamespace).Namespace);
-                else
-                    cfg.XmlSerializer();
-            }
-            else
-                if (!(specifier is ISpecify.MyOwnSerialization))
-                    cfg.BinarySerializer();
-
-            if (specifier is IWantCustomInitialization)
-                (specifier as IWantCustomInitialization).Init(cfg);
-
-            if (messageEndpointType != null)
-                Configure.TypeConfigurer.ConfigureComponent(messageEndpointType, ComponentCallModelEnum.Singleton);
-
-            messageEndpoint = Configure.ObjectBuilder.Build<IMessageEndpoint>();
-
-            if (!(specifier is IDontWantTheBusStartedAutomatically))
-                cfg.CreateBus().Start(startupAction);
-
-            if (messageEndpoint != null)
-                messageEndpoint.OnStart();
         }
 
         public void Stop()
