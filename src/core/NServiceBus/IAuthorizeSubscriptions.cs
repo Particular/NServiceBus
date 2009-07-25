5c2cc013d681d2f5cd12cbd8df9a5893a82bd235
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-07-25 11:54:10 +0000

    Added IAuthorizeSubscriptions, not yet plugged into infrastructure.

---
+++ src/core/NServiceBus/IAuthorizeSubscriptions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace NServiceBus
+{
+    /// <summary>
+    /// Implementer will be called by the infrastructure in order to authorize
+    /// subscribe and unsubscribe requests from other endpoints.
+    /// </summary>
+    public interface IAuthorizeSubscriptions
+    {
+        /// <summary>
+        /// Return true if the client endpoint is to be allowed to subscribe to the given message type.
+        /// </summary>
+        /// <param name="messageType"></param>
+        /// <param name="clientEndpoint"></param>
+        /// <param name="clientWindowsIdentity"></param>
+        /// <param name="headers"></param>
+        /// <returns></returns>
+        bool AuthorizeSubscribe(string messageType, string clientEndpoint, string clientWindowsIdentity,
+                                IDictionary<string, string> headers);
+
+        /// <summary>
+        /// Return true if the client endpoint is to be allowed to unsubscribe to the given message type.
+        /// </summary>
+        /// <param name="messageType"></param>
+        /// <param name="clientEndpoint"></param>
+        /// <param name="clientWindowsIdentity"></param>
+        /// <param name="headers"></param>
+        /// <returns></returns>
+        bool AuthorizeUnsubscribe(string messageType, string clientEndpoint, string clientWindowsIdentity,
+                        IDictionary<string, string> headers);
+
+    }
+}
