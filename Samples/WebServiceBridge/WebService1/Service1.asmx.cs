c4dc18e6b1fe014b58b0a55de6711d07749d5f1d
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-08-17 05:15:33 +0000

    Added base webservice implementation greatly simplifying writing web service bridges to messaging endpoints.

--- Samples/WebServiceBridge/WebService1/Service1.asmx.cs
+++ Samples/WebServiceBridge/WebService1/Service1.asmx.cs
@@ -2,35 +2,14 @@ using System.Web.Services;
 using System.ComponentModel;
 using Messages;
 using NServiceBus;
-using System;
 
 namespace WebService1
 {
-    /// <summary>
-    /// Summary description for Service1
-    /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [ToolboxItem(false)]
-    public class Service1 : WebService
+    public class Service1 : NServiceBus.Webservice<Command, ErrorCodes>
     {
-
-        [WebMethod]
-        public ErrorCodes Process(Command request)
-        {
-            object result = ErrorCodes.None;
-
-            IAsyncResult sync = Global.Bus.Send(request).Register(asyncResult =>
-                                {
-                                    CompletionResult completionResult = asyncResult.AsyncState as CompletionResult;
-                                    if (completionResult != null)
-                                        result = (ErrorCodes)completionResult.ErrorCode;
-                                }, null);
-
-            sync.AsyncWaitHandle.WaitOne();
-
-            return (ErrorCodes)result;
-        }
     }
 }
 
