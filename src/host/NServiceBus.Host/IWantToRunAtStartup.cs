3d94d2eb16d8d922edd005b21e0d09ab6479fe67
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-09-08 11:15:48 +0000

    Forgot to add IWantToRunAtStartup.

---
+++ src/host/NServiceBus.Host/IWantToRunAtStartup.cs
@@ -0,0 +1,18 @@
+﻿namespace NServiceBus.Host
+{
+    /// <summary>
+    /// Implementers will be invoked when the endpoint starts up.
+    /// </summary>
+    public interface IWantToRunAtStartup
+    {
+        /// <summary>
+        /// Method called at startup.
+        /// </summary>
+        void Run();
+
+        /// <summary>
+        /// Method called on shutdown.
+        /// </summary>
+        void Stop();
+    }
+}
