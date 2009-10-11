5cdc926f26709286e962e4b78cee8700bdf681e1
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-10-11 20:23:15 +0000

    Provides better exception in cases where IMessageCreator wasn't set for the ExtensionMethods.

--- src/core/NServiceBus/ExtensionMethods.cs
+++ src/core/NServiceBus/ExtensionMethods.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace NServiceBus
 {
@@ -15,9 +14,12 @@ namespace NServiceBus
         /// <typeparam name="T">The type to instantiate.</typeparam>
         /// <param name="list">The list to which to add the new element</param>
         /// <param name="constructor">An action for setting properties of the created instance.</param>
-        public static void Add<T>(this System.Collections.Generic.IList<T> list, Action<T> constructor) where T : IMessage
+        public static void Add<T>(this IList<T> list, Action<T> constructor) where T : IMessage
         {
-            list.Add(MessageCreator.CreateInstance<T>(constructor));
+            if (MessageCreator == null)
+                throw new InvalidOperationException("MessageCreator has not been set.");
+
+            list.Add(MessageCreator.CreateInstance(constructor));
         }
 
         /// <summary>
