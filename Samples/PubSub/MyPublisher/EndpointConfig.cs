2ae58ac38902fb504334f9b37cee4b4c5de622d7
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2009-10-15 15:07:23 +0000

    Renamed Server in pub/sub sample to MyPublisher.

---
+++ Samples/PubSub/MyPublisher/EndpointConfig.cs
@@ -0,0 +1,16 @@
+﻿using NServiceBus;
+using NServiceBus.Grid.MessageHandlers;
+using NServiceBus.Host;
+using NServiceBus.Sagas.Impl;
+
+namespace MyPublisher
+{
+    class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher, 
+        ISpecifyMessageHandlerOrdering
+    {
+        public void SpecifyOrder(Order order)
+        {
+            order.Specify(First<GridInterceptingMessageHandler>.Then<SagaMessageHandler>());
+        }
+    }
+}
--- Samples/PubSub/MyPublisher/Properties/AssemblyInfo.cs
+++ Samples/PubSub/MyPublisher/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyMessages")]
+[assembly: AssemblyTitle("MyPublisher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyMessages")]
+[assembly: AssemblyProduct("MyPublisher")]
 [assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5e94a240-87cd-470f-9633-adffc9532a22")]
+[assembly: Guid("6d0a1864-1cf8-4f8a-90ae-bc3fda6179d9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
