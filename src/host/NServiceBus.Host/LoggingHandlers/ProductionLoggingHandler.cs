d1549111a1681785e356c2c6d22461ad34df42ec
Ohlund, Andreas <andreas_ohlund@hotmail.com>
2009-12-09 22:14:31 +0000

    Another try with the mysterious missing files for the host

---
+++ src/host/NServiceBus.Host/LoggingHandlers/ProductionLoggingHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Specialized;
+using Common.Logging;
+
+namespace NServiceBus.Host.LoggingHandlers
+{
+    /// <summary>
+    /// Handles logging configuration for the production profile
+    /// </summary>
+    public class ProductionLoggingHandler : IConfigureLoggingForProfile<Production>
+    {
+        void IConfigureLogging.Configure(IConfigureThisEndpoint specifier)
+        {
+            var props = new NameValueCollection();
+            props["configType"] = "EXTERNAL";
+            LogManager.Adapter = new Common.Logging.Log4Net.Log4NetLoggerFactoryAdapter(props);
+
+            var layout = new log4net.Layout.PatternLayout("%d [%t] %-5p %c [%x] <%X{auth}> - %m%n");
+            var level = log4net.Core.Level.Warn;
+
+            var appender = new log4net.Appender.RollingFileAppender
+                               {
+                                   Layout = layout,
+                                   Threshold = level, 
+                                   CountDirection = 1,
+                                   DatePattern = "yyyy-MM-dd",
+                                   RollingStyle = log4net.Appender.RollingFileAppender.RollingMode.Composite,
+                                   MaxFileSize = 1024*1024,
+                                   MaxSizeRollBackups = 10,
+                                   LockingModel = new log4net.Appender.FileAppender.MinimalLock(),
+                                   StaticLogFileName = true,
+                                   File = "logfile",
+                                   AppendToFile = true
+                               };
+            appender.ActivateOptions();
+
+            log4net.Config.BasicConfigurator.Configure(appender);
+        }
+    }
+}
