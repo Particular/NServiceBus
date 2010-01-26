2cccb6685a094e7abbebeb2063d3c84623931223
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2010-01-26 08:43:20 +0000

    Added logging for the AccessViolationException in the IntegrationProfileHandler to tell the user that this is a common occurrence for the SQLite DB and that the production profile should be used under load.

--- src/hosting/NServiceBus.Hosting.Windows/Profiles/Handlers/IntegrationProfileHandler.cs
+++ src/hosting/NServiceBus.Hosting.Windows/Profiles/Handlers/IntegrationProfileHandler.cs
@@ -1,7 +1,9 @@
-﻿using NServiceBus.Config;
+﻿using System;
+using NServiceBus.Config;
 using NServiceBus.Hosting.Profiles;
 using NServiceBus.ObjectBuilder;
 using NServiceBus.Unicast.Subscriptions.Msmq;
+using Common.Logging;
 
 namespace NServiceBus.Hosting.Windows.Profiles.Handlers
 {
@@ -13,6 +15,14 @@ namespace NServiceBus.Hosting.Windows.Profiles.Handlers
                 .NHibernateSagaPersisterWithSQLiteAndAutomaticSchemaGeneration();
 
             Configure.Instance.Configurer.ConfigureComponent<Faults.InMemory.FaultManager>(ComponentCallModelEnum.Singleton);
+
+            AppDomain.CurrentDomain.UnhandledException += (o, e) =>
+            {
+                if (e.ExceptionObject.GetType() ==
+                    typeof(AccessViolationException))
+                    logger.Fatal(
+                        "NServiceBus has detected an error in the operation of SQLite. SQLite is the database used to store sagas from NServiceBus when running under the 'Integration' profile. This error usually occurs only under load. If you wish to use sagas under load, it is recommended to run NServiceBus under the 'Production' profile. This can be done by passing the value 'NServiceBus.Production' on the command line to the NServiceBus.Host.exe process. For more information see http://www.NServiceBus.com/Profiles.aspx .");
+            };
 
             if (Config is AsA_Publisher)
             {
@@ -26,5 +36,6 @@ namespace NServiceBus.Hosting.Windows.Profiles.Handlers
         }
 
         public IConfigureThisEndpoint Config { get; set; }
+        private static ILog logger = LogManager.GetLogger("System.Data.SQLite");
     }
 }
