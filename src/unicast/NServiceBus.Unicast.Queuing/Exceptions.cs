c46ec6c378171463b560e3376d41bf29173197bd
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2010-02-23 09:13:22 +0000

    Made QueueNotFoundException serializable.

--- src/unicast/NServiceBus.Unicast.Queuing/Exceptions.cs
+++ src/unicast/NServiceBus.Unicast.Queuing/Exceptions.cs
@@ -2,8 +2,30 @@
 
 namespace NServiceBus.Unicast.Queuing
 {
+    [Serializable]
     public class QueueNotFoundException : Exception
     {
         public string Queue { get; set; }
+
+        public QueueNotFoundException()
+        {
+        }
+
+        protected QueueNotFoundException(
+            System.Runtime.Serialization.SerializationInfo info, 
+            System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        {
+            if (info != null)
+                Queue = info.GetString("Queue");
+        }
+
+        public override void GetObjectData(
+            System.Runtime.Serialization.SerializationInfo info, 
+            System.Runtime.Serialization.StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            info.AddValue("Queue", Queue);
+        }
     }
 }
