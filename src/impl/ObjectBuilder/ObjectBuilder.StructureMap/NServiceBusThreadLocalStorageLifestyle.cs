65edf9f39955c537683682917ae4812dc6766e6c
Ohlund, Andreas <andreas_ohlund@hotmail.com>
2010-03-25 10:15:11 +0000

    Added custom lifestyle to handle thread local storage correctly

---
+++ src/impl/ObjectBuilder/ObjectBuilder.StructureMap/NServiceBusThreadLocalStorageLifestyle.cs
@@ -0,0 +1,16 @@
+﻿using StructureMap.Pipeline;
+
+namespace NServiceBus.ObjectBuilder.StructureMap
+{
+    public class NServiceBusThreadLocalStorageLifestyle : ThreadLocalStorageLifecycle,  IMessageModule
+    {
+        public void HandleBeginMessage(){}
+
+        public void HandleEndMessage()
+        {
+            EjectAll();
+        }
+
+        public void HandleError(){}
+    }
+}
