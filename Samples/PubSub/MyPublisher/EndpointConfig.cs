b0f454f73e3a0d5e836074f0e50f825080836187
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2010-05-12 16:57:40 +0000

    Simplified publisher config - removed ISpecifyMessageHandlerOrdering as it was meaningless in the sample.

--- Samples/PubSub/MyPublisher/EndpointConfig.cs
+++ Samples/PubSub/MyPublisher/EndpointConfig.cs
@@ -1,15 +1,6 @@
 ﻿using NServiceBus;
-using NServiceBus.Grid.MessageHandlers;
-using NServiceBus.Sagas.Impl;
 
 namespace MyPublisher
 {
-    class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher, 
-        ISpecifyMessageHandlerOrdering
-    {
-        public void SpecifyOrder(Order order)
-        {
-            order.Specify(First<GridInterceptingMessageHandler>.Then<SagaMessageHandler>());
-        }
-    }
+    class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher { }
 }
