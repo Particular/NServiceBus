429f055b365e896e71317846243ca6aa3b5fef34
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2010-06-20 08:34:04 +0000

    Fixed bug with gateway where it sent back the wrong hash.

--- src/gateway/NServiceBus.Gateway/HttpRequestHandler.cs
+++ src/gateway/NServiceBus.Gateway/HttpRequestHandler.cs
@@ -85,7 +85,7 @@ namespace NServiceBus.Gateway
                 {
                     Logger.Debug("Sending HTTP response.");
 
-                    ctx.Response.AddHeader(Headers.ContentMd5Key, hash);
+                    ctx.Response.AddHeader(Headers.ContentMd5Key, myHash);
                     ctx.Response.StatusCode = 200;
                     ctx.Response.StatusDescription = "OK";
 
