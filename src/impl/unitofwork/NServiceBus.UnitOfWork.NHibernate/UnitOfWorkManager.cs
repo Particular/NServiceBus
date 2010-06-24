febaa00ba92eee347abc7eee901cc6cb84994cf9
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2010-06-24 16:36:29 +0000

    Started introducing explicit unit of work management.

---
+++ src/impl/unitofwork/NServiceBus.UnitOfWork.NHibernate/UnitOfWorkManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Transactions;
+using NHibernate;
+using NHibernate.Context;
+
+namespace NServiceBus.UnitOfWork.NHibernate
+{
+    /// <summary>
+    /// Implementation of unit of work management with NHibernate
+    /// </summary>
+    public class UnitOfWorkManager : IManageUnitsOfWork
+    {
+        void IManageUnitsOfWork.Begin()
+        {
+            if (SessionFactory == null) return;
+
+            var session = SessionFactory.OpenSession();
+
+            CurrentSessionContext.Bind(session);
+
+            if (NoAmbientTransaction())
+            {
+                session.BeginTransaction();
+            }
+        }
+
+        void IManageUnitsOfWork.End()
+        {
+            if (SessionFactory == null) return;
+
+            if (!NoAmbientTransaction()) return;
+
+            var session = SessionFactory.GetCurrentSession();
+
+            session.Transaction.Commit();
+            session.Transaction.Dispose();
+
+            session.Close();
+        }
+
+        void IManageUnitsOfWork.Error()
+        {
+            if (SessionFactory == null) return;
+
+            //HandleError always run after the transactionscope so we can't check for ambient trans here
+            var session = SessionFactory.GetCurrentSession();
+
+            if (session.Transaction.IsActive)
+            {
+                session.Transaction.Rollback();
+                session.Transaction.Dispose();
+            }
+
+            if (session.IsOpen)
+                session.Close();
+        }
+
+        /// <summary>
+        /// Injected NHibernate session factory.
+        /// </summary>
+        public ISessionFactory SessionFactory { get; set; }
+
+
+        private static bool NoAmbientTransaction()
+        {
+            return Transaction.Current == null;
+        }
+    }
+}
---
+++ src/unitofwork/NServiceBus.UnitOfWork/IManageUnitsOfWork.cs
@@ -0,0 +1,24 @@
+﻿namespace NServiceBus.UnitOfWork
+{
+    /// <summary>
+    /// Interface used by NServiceBus to manage units of work as a part of the
+    /// message processing pipeline.
+    /// </summary>
+    public interface IManageUnitsOfWork
+    {
+        /// <summary>
+        /// Called before all message handlers and modules
+        /// </summary>
+        void Begin();
+
+        /// <summary>
+        /// Called after all message handlers and modules
+        /// </summary>
+        void End();
+
+        /// <summary>
+        /// Called in the case there was an exception during the message processing
+        /// </summary>
+        void Error();
+    }
+}
