5f52cb90da31e62cf4b14a8d192b5e8fec07692a
Ohlund, Andreas <andreas_ohlund@hotmail.com>
2010-06-28 21:03:15 +0000

    Added the messagemutator interface

---
+++ src/core/NServiceBus/IMessageMutator.cs
@@ -0,0 +1,33 @@
+namespace NServiceBus
+{
+    /// <summary>
+    /// Use this interface to hook into the seralization pipeline
+    /// </summary>
+    public interface IMessageMutator : IMutateOutgoingMessages, IMutateIncomingMessages{}
+
+    /// <summary>
+    /// Mutaor for incoming messages
+    /// </summary>
+    public interface IMutateIncomingMessages
+    {
+        /// <summary>
+        /// Mutates the given message right after it has been deseralized
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        IMessage MutateIncoming(IMessage message);
+    }
+
+    /// <summary>
+    /// Mutates outgoing messages
+    /// </summary>
+    public interface IMutateOutgoingMessages
+    {
+        /// <summary>
+        /// Mutates the given message just before it's serialized
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        IMessage MutateOutgoing(IMessage message);
+    }
+}
