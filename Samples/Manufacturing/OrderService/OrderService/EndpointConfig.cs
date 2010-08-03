b0a7195c89dc9a910393fb06974404b6791c19c2
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2010-08-03 18:37:17 +0000

    Removed unnecessary specification of handler ordering from manufacturing sample.

--- Samples/Manufacturing/OrderService/OrderService/EndpointConfig.cs
+++ Samples/Manufacturing/OrderService/OrderService/EndpointConfig.cs
@@ -1,16 +1,6 @@
 using NServiceBus;
-using NServiceBus.Grid.MessageHandlers;
-using NServiceBus.Sagas.Impl;
 
 namespace OrderService
 {
-    public class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher,
-                                    ISpecifyMessageHandlerOrdering
-    {
-        public void SpecifyOrder(Order order)
-        {
-            order.Specify(First<GridInterceptingMessageHandler>
-              .Then<SagaMessageHandler>());
-        }
-    }
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher { }
 }
