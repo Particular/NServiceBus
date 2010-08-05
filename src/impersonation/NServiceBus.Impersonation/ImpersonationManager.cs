327ae191395985b4ce04306afdab6c13278781bf
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2010-08-05 17:02:24 +0000

    Fixed bug with impersonation management which assumed it had dependency injection when it didn't.

--- src/impersonation/NServiceBus.Impersonation/ImpersonationManager.cs
+++ src/impersonation/NServiceBus.Impersonation/ImpersonationManager.cs
@@ -15,26 +15,20 @@ namespace NServiceBus.Impersonation
     /// </summary>
     public class ImpersonationManager : INeedInitialization, IMapOutgoingTransportMessages
     {
-        /// <summary>
-        /// Injected transport
-        /// </summary>
-        public ITransport Transport { get; set; }
-
-        /// <summary>
-        /// Configuration on whether incoming messages should be processed using the permissions
-        /// of the client who sent those messages.
-        /// </summary>
-        public virtual bool ImpersonateSender { get; set; }
-
         void INeedInitialization.Init()
         {
-            Transport.TransportMessageReceived += Transport_TransportMessageReceived;
+            Configure.ConfigurationComplete +=
+                (o, a) =>
+                    {
+                        Configure.Instance.Builder.Build<ITransport>().TransportMessageReceived +=
+                            Transport_TransportMessageReceived;
+                    };
         }
 
         void Transport_TransportMessageReceived(object sender, TransportMessageReceivedEventArgs e)
         {
             if (e.Message.Headers.ContainsKey(WINDOWSIDENTITYNAME))
-                Thread.CurrentPrincipal = ImpersonateSender ? new GenericPrincipal(new GenericIdentity(e.Message.Headers[WINDOWSIDENTITYNAME]), new string[0]) : null;
+                Thread.CurrentPrincipal = ConfigureImpersonation.Impersonate ? new GenericPrincipal(new GenericIdentity(e.Message.Headers[WINDOWSIDENTITYNAME]), new string[0]) : null;
         }
 
         void IMapOutgoingTransportMessages.MapOutgoing(IMessage[] messages, TransportMessage transportMessage)
@@ -62,10 +56,11 @@ namespace NServiceBus
         /// <returns></returns>
         public static ConfigUnicastBus ImpersonateSender(this ConfigUnicastBus config, bool value)
         {
-            config.Configurer.ConfigureComponent<ImpersonationManager>(ComponentCallModelEnum.Singleton)
-                .ConfigureProperty(m => m.ImpersonateSender, value);
+            Impersonate = true;
 
             return config;
         }
+
+        public static bool Impersonate { get; private set; }
     }
 }
