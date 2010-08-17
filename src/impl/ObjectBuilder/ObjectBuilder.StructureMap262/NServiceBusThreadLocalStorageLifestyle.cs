567f2f04a117be4ae06a01606903e98c08ffd8a9
Ohlund, Andreas <andreas_ohlund@hotmail.com>
2010-08-17 11:35:30 +0000

    Added a the special thread static lifecycle for structuremap that is compatible with the way that nservicebus manages its worker threads

---
+++ src/impl/ObjectBuilder/ObjectBuilder.StructureMap262/NServiceBusThreadLocalStorageLifestyle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using StructureMap.Pipeline;
+
+namespace NServiceBus.ObjectBuilder.StructureMap262
+{
+    public class NServiceBusThreadLocalStorageLifestyle : IMessageModule,ILifecycle
+    {
+        public void EjectAll()
+        {
+            FindCache().DisposeAndClear();
+        }
+
+        public IObjectCache FindCache()
+        {
+            guaranteeHashExists();
+            return cache;
+        }
+
+        public void HandleBeginMessage() { }
+
+        public void HandleEndMessage()
+        {
+            EjectAll();
+        }
+
+        public void HandleError() { }
+
+
+        public string Scope
+        {
+            get
+            {
+                return typeof (NServiceBusThreadLocalStorageLifestyle).Name;
+            }
+        }
+
+        private void guaranteeHashExists()
+        {
+            if (cache == null)
+            {
+                lock (locker)
+                {
+                    if (cache == null)
+                    {
+                        cache = new MainObjectCache();
+                    }
+                }
+            }
+        }
+
+        [ThreadStatic]
+        private static MainObjectCache cache;
+        private readonly object locker = new object();
+
+    }
+    
+       
+}
