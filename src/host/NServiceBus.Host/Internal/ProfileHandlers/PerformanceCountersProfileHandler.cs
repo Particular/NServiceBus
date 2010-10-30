5ad0cdd88fbada7e9252c49c4648ba45d589d5a1
Dahan, Udi <thesoftwaresimplist@udidahan.com>
2010-10-30 14:32:58 +0000

    Fixed bug with performance counters where they didn't return to zero when no messages were being processed.

--- src/host/NServiceBus.Host/Internal/ProfileHandlers/PerformanceCountersProfileHandler.cs
+++ src/host/NServiceBus.Host/Internal/ProfileHandlers/PerformanceCountersProfileHandler.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Diagnostics;
-using NServiceBus.Unicast.Transport.Msmq;
+using System.Threading;
+using NServiceBus.Unicast.Transport;
 
 namespace NServiceBus.Host.Internal.ProfileHandlers
 {
     /// <summary>
     /// Handles the PerformanceCounters profile.
     /// </summary>
-    public class PerformanceCountersProfileHandler : IHandleProfile<PerformanceCounters>
+    public class PerformanceCountersProfileHandler : IHandleProfile<PerformanceCounters>, IDisposable
     {
         void IHandleProfile.ProfileActivated()
         {
             var categoryName = "NServiceBus";
             var counterName = "Critical Time";
-            PerformanceCounter counter = null;
-
+            
             try
             {
                 counter = new PerformanceCounter(categoryName, counterName, Program.EndpointId, false);
@@ -24,16 +24,39 @@ namespace NServiceBus.Host.Internal.ProfileHandlers
                 throw new InvalidOperationException("NServiceBus performance counters not set up correctly. Running this process with the flag NServiceBus.InstallPerformanceCounters once should rectify this problem.", e);
             }
 
-            GenericHost.ConfigurationComplete += (o, e) =>
-                                                     {
-                     var msmqTransport = Configure.Instance.Builder.Build<MsmqTransport>();
-                     msmqTransport.TransportMessageReceived += (obj, args) =>
-                       {
-                           counter.RawValue =
-                               Convert.ToInt32((DateTime.Now - args.Message.TimeSent).TotalSeconds);
+            GenericHost.ConfigurationComplete += 
+                (o, e) =>
+                {
+                    var transport = Configure.Instance.Builder.Build<ITransport>();
+                    transport.TransportMessageReceived += HandleTransportMessageReceived;
+                };
 
-                       };
-                 };
-    }
+            timer = new Timer(ClearPerfCounter, null, 0, 2000);
+        }
+
+        private void ClearPerfCounter(object state)
+        {
+            var delta = DateTime.Now - timeOfLastCounter;
+
+            if (delta > maxDelta)
+                counter.RawValue = 0;
+        }
+
+        private void HandleTransportMessageReceived(object sender, TransportMessageReceivedEventArgs e)
+        {
+            counter.RawValue = Convert.ToInt32((DateTime.Now - e.Message.TimeSent).TotalSeconds);
+
+            timeOfLastCounter = DateTime.Now;
+        }
+
+        public void Dispose()
+        {
+            timer.Dispose();
+        }
+
+        PerformanceCounter counter;
+        Timer timer;
+        private DateTime timeOfLastCounter;
+        private readonly TimeSpan maxDelta = TimeSpan.FromSeconds(2);
     }
 }
