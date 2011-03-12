5f7493669d51ea2ffdc37e9c91d78e201a85828b
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2011-03-12 06:03:01 -0700

    Now using SQLite binaries compiled against CLR v2.0.507272; Mixed-mode assembly failures now contain a more detailed exception message; If a SQLite assembly exists in the application runtime directory, it will be automatically loaded.

--- src/impl/sqlite/NServiceBus.SQLite/AssemblyResolver.cs
+++ src/impl/sqlite/NServiceBus.SQLite/AssemblyResolver.cs
@@ -1,50 +1,99 @@
 namespace NServiceBus.SQLite
 {
 	using System;
+	using System.Configuration;
 	using System.IO;
+	using System.Linq;
 	using System.Reflection;
+	using System.Web;
 	using Config;
 
-	internal class AssemblyResolver : INeedInitialization
+	public class AssemblyResolver : INeedInitialization
 	{
 		private const string SqliteAssemblyNamespace = "System.Data.SQLite";
+		private const string SqliteAssemblyMask = "*.dll";
 		private const string SqliteAssemblyFilename = SqliteAssemblyNamespace + ".dll";
 		private const int X86 = 4;
 
 		public void Init()
 		{
-			AppDomain.CurrentDomain.AssemblyResolve += (s, e) =>
+			if (ContainsAssembly())
+				LoadAssembly();
+
+			AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
 			{
-				if (!e.Name.Contains(SqliteAssemblyNamespace))
+				if (!args.Name.Contains(SqliteAssemblyNamespace))
 					return null;
 
-				DeleteAssembly();
 				WriteAssembly();
-
-				try
-				{
-					return LoadAssembly();
-				}
-				catch (BadImageFormatException)
-				{
-					DeleteAssembly();
-					return null;
-				}
+				return LoadAssembly();
 			};
 		}
+
+		private static bool ContainsAssembly()
+		{
+			var runtimeDirectory = (HttpContext.Current != null)
+				? HttpRuntime.BinDirectory : AppDomain.CurrentDomain.BaseDirectory;
+
+			try
+			{
+				return Directory.GetFiles(runtimeDirectory, SqliteAssemblyMask).Where(IsSqliteAssembly).Any();
+			}
+			catch (IOException)
+			{
+				return false;
+			}
+			catch (UnauthorizedAccessException)
+			{
+				return false;
+			}
+		}
+		private static bool IsSqliteAssembly(string filename)
+		{
+			return (filename ?? string.Empty).EndsWith(
+				SqliteAssemblyFilename, StringComparison.InvariantCultureIgnoreCase);
+		}
+
 		private static void WriteAssembly()
 		{
-			var bytes = IntPtr.Size == X86 ? Binaries.SQLite32 : Binaries.SQLite64;
+			var bytes = IntPtr.Size == X86 ? Resources.SQLite32 : Resources.SQLite64;
 			using (var writer = File.OpenWrite(SqliteAssemblyFilename))
 				writer.Write(bytes, 0, bytes.Length);
 		}
-		private static void DeleteAssembly()
-		{
-			File.Delete(SqliteAssemblyFilename);
-		}
 		private static Assembly LoadAssembly()
 		{
-			return Assembly.LoadFrom(SqliteAssemblyFilename);
+			try
+			{
+				return Assembly.LoadFrom(SqliteAssemblyFilename);
+			}
+			catch (BadImageFormatException)
+			{
+				DeleteAssembly();
+
+				return null;
+			}
+			catch (FileLoadException e)
+			{
+				DeleteAssembly();
+
+				if (e.Message.Contains(Resources.MixedModeAssembly))
+					throw new ConfigurationErrorsException(Resources.ConfigurationErrorsException);
+
+				throw;
+			}
+		}
+		private static void DeleteAssembly()
+		{
+			try
+			{
+				File.Delete(SqliteAssemblyFilename);
+			}
+			catch (IOException)
+			{
+			}
+			catch (UnauthorizedAccessException)
+			{
+			}
 		}
 	}
 }
