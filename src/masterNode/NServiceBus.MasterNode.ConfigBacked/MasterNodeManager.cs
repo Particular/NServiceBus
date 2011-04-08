92ab09be98dccc5122cd8969c9bc7ce4a7da7f8b
udidahan <udi@udidahan.com>
2011-04-08 18:19:35 +0300

    Added interface for getting master node info, as well as config backed impl.

---
+++ src/masterNode/NServiceBus.MasterNode.ConfigBacked/MasterNodeManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using NServiceBus.Config;
+
+namespace NServiceBus.MasterNode.ConfigBacked
+{
+    public class MasterNodeManager : IManageTheMasterNode
+    {
+        public string GetMasterNode()
+        {
+            if (masterNode == null)
+            {
+                var section = Configure.GetConfigSection<MasterNodeLocatorConfig>();
+                if (section != null)
+                    masterNode = section.Node.ToLower();
+            }
+
+            return masterNode;
+        }
+
+        public bool IsCurrentNodeTheMaster
+        {
+            get
+            {
+                if (GetMasterNode() == null)
+                    return false;
+
+                if (Environment.MachineName.ToLower() == masterNode)
+                    return true;
+
+                if (Dns.GetHostName().ToLower() == masterNode)
+                    return true;
+
+                IPAddress ip;
+                IPAddress.TryParse(masterNode, out ip);
+                if (ip != null)
+                    if (Dns.GetHostAddresses(Dns.GetHostName()).ToList().Contains(ip))
+                        return true;
+
+                return false;
+            }
+        }
+
+        private string masterNode; //lower case; cached output of GetMasterNode()
+    }
+}
