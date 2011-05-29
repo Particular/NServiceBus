40f2e6a14a962bcb08e9348ef0314c3f60936234
udidahan <udi@udidahan.com>
2011-05-29 13:09:36 +0300

    Added discovery-based master node detection.

---
+++ src/masterNode/NServiceBus.MasterNode.Discovery.Tests/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace NServiceBus.MasterNode.Discovery.Tests
+{
+    [TestFixture]
+    public class When_Two_Nodes_Are_Online
+    {
+        [Test]
+        public void They_should_identify_the_one_that_is_the_master()
+        {
+            Address.InitializeLocalAddress("test");
+
+            var m = new MasterNodeManager();
+            string a = null, b = null;
+
+            m.YieldMasterNode("test@A", true, s => a = s);
+            m.YieldMasterNode("test@B", false, s => b = s);
+
+            Thread.Sleep(MasterNodeManager.presenceInterval + TimeSpan.FromSeconds(1));
+
+            Assert.AreEqual(a, b);
+            Assert.AreEqual("test@A", a);
+        }
+    }
+}
