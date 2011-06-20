ba6b0de53255072764f9aaf433c6487e12bc41ed
yvesgoeleven@github.com <yvesgoeleven@yahoo.com>
2011-06-20 19:53:47 +0200

    Added databus for azure

---
+++ Samples/AzureBlobStorageDataBus/Receiver/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Config;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BootstrapNServiceBus();
+
+            Console.WriteLine("Press enter to stop receiving");
+            Console.ReadLine();
+
+        }
+
+        private static void BootstrapNServiceBus()
+        {
+            Configure.With()
+               .DefaultBuilder()
+               .AzureConfigurationSource()
+               .AzureMessageQueue()
+                    .BinarySerializer()
+               .AzureDataBus()
+               .UnicastBus()
+                    .LoadMessageHandlers()
+                    .IsTransactional(true)
+               .CreateBus()
+               .Start();
+        }
+    }
+}
