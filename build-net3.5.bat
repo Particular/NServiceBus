8748bd6ca2be9b09ecd3f318cc8ee9d4f4a68f87
andreasohlund <andreas_ohlund@hotmail.com>
2011-07-02 08:12:28 +0200

    Moved the raven dependency over to "packages", packages are also downloaded on demand using nuget

--- build-net3.5.bat
+++ build-net3.5.bat
@@ -1,3 +1,1 @@
-.\tools\nuget\nuget.exe install packages.config -o packages
-
 .\tools\nant\nant.exe -D:targetframework=net-3.5
--- build.bat
+++ build.bat
@@ -1,3 +1,1 @@
-.\tools\nuget\nuget.exe install packages.config -o packages
-
 .\tools\nant\nant %1
