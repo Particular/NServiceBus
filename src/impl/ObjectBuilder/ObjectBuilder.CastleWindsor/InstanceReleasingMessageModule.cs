acf9fccee9046d474ca9f5d3a730c23ad1202a65
Udi Dahan <udi@udidahan.com>
2011-07-26 19:03:33 +0300

    Fixed WindsorObjectBuilder so that it would explicitly release all transient instances that were explicitly resolved.

---
+++ src/impl/ObjectBuilder/ObjectBuilder.CastleWindsor/InstanceReleasingMessageModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NServiceBus.ObjectBuilder.CastleWindsor
+{
+    class InstanceReleasingMessageModule : IMessageModule
+    {
+        void IMessageModule.HandleBeginMessage()
+        {
+            
+        }
+
+        void IMessageModule.HandleEndMessage()
+        {
+            Builder.ReleaseResolvedInstances();
+        }
+
+        void IMessageModule.HandleError()
+        {
+            Builder.ReleaseResolvedInstances();
+        }
+
+        public static WindsorObjectBuilder Builder { get; set; }
+    }
+}
