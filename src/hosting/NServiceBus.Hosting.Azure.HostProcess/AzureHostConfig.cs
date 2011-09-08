960d22fd14585b95cce129303fb63df808f0e46f
Udi Dahan <udi@udidahan.com>
2011-09-08 09:46:38 +0300

    Added missing AzureHostConfig.

---
+++ src/hosting/NServiceBus.Hosting.Azure.HostProcess/AzureHostConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+
+namespace NServiceBus.Config
+{
+    /// <summary>
+    /// Configuration section for Azure host.
+    /// </summary>
+    public class AzureHostConfig : ConfigurationSection
+    {
+        /// <summary>
+        /// A comma separated list of profile names
+        /// </summary>
+        [ConfigurationProperty("Profiles", IsRequired = false)]
+        public string Profiles
+        {
+            get
+            {
+                return this["Profiles"] as string;
+            }
+            set
+            {
+                this["Profiles"] = value;
+            }
+        }
+    }
+}
--- src/hosting/NServiceBus.Hosting.Azure.HostProcess/WindowsHost.cs
+++ src/hosting/NServiceBus.Hosting.Azure.HostProcess/WindowsHost.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using NServiceBus.Config;
 
 namespace NServiceBus.Hosting.Azure.HostProcess
 {
