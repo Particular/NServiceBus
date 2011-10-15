5d93981b702918dab31fc922ffefdc90ef4d975c
Joseph Thomas <joseph.pattom@gmailcom>
2011-10-15 12:31:54 +0530

    Added arequired packages

--- default.ps1
+++ default.ps1
@@ -19,7 +19,7 @@ task default -depends CreatePackages
 task CreatePackages {
 	import-module ./NuGet\packit.psm1
 	Write-Output "Loding the moduele for packing.............."
-	$packit.push_to_nuget = $false 
+	$packit.push_to_nuget = $true 
 	
 	
 	$packit.framework_Isolated_Binaries_Loc = ".\release"
@@ -47,23 +47,69 @@ task CreatePackages {
 	#region Packing NServiceBus.Host
 	$packageName = "NServiceBus.Host" + $packageNameSuffix
 	$packit.package_description = "The hosting template for the nservicebus, The most popular open-source service bus for .net"
-	invoke-packit $packageName $productVersion @{$packageNameNsb=$productVersion} "binaries\NServiceBus.Host.exe" 
+	invoke-packit $packageName $productVersion @{$packageNameNsb=$productVersion} "" @{".\release\net40\binaries\NServiceBus.Host32.exe"="lib\net40\x86";".\release\net40\binaries\NServiceBus.Host.exe"="lib\net40\x64"} 
 	#endregion
 	
-#	#region Packing NServiceBus.Testing
-#	$packit.package_description = "The testing for the nservicebus, The most popular open-source service bus for .net"
-#	invoke-packit "NServiceBus.Testing" $productVersion @{NServiceBus=$productVersion} "NServiceBus.Testing.dll"
-#	#endregion
-#	
-#	#region Packing NServiceBus.Tools
-#	$packit.package_description = "The tools for configure the nservicebus, The most popular open-source service bus for .net"
-#	invoke-packit "NServiceBus.Tools" $version @{} "" @{".\tools\msmqutils\*.*"="tools\msmqutils";".\tools\RunMeFirst.bat"="tools";".\tools\install.ps1"="tools"}
-#	#endregion
-#	
-#	#region Packing NServiceBus.Autofac2
-#	$packit.package_description = "The Autofac Container for the nservicebus, The most popular open-source service bus for .net"
-#	invoke-packit "NServiceBus.Autofac2" $productVersion @{Autofac="2.3.2.632"} "containers\autofac\NServiceBus.ObjectBuilder.Autofac.dll"
-#	#endregion
+	#region Packing NServiceBus.Testing
+	$packageName = "NServiceBus.Testing" + $packageNameSuffix
+	$packit.package_description = "The testing for the nservicebus, The most popular open-source service bus for .net"
+	invoke-packit $packageName $productVersion @{$packageNameNsb=$productVersion} "binaries\NServiceBus.Testing.dll"
+	#endregion
+	
+	#region Packing NServiceBus.Integration.WebServices
+	$packageName = "NServiceBus.Integration.WebServices" + $packageNameSuffix
+	$packit.package_description = "The WebServices Integration for the nservicebus, The most popular open-source service bus for .net"
+	invoke-packit $packageName $productVersion @{$packageNameNsb=$productVersion} "binaries\NServiceBus.Integration.WebServices.dll"
+	#endregion
+
+	#region Packing NServiceBus.Autofac
+	$packageName = "NServiceBus.Autofac" + $packageNameSuffix
+	$packit.package_description = "The Autofac Container for the nservicebus"
+	invoke-packit $packageName $productVersion @{"Autofac"="2.5.2.830"} "" @{".\release\net40\binaries\containers\autofac\*.*"="lib\net40"}
+	#endregion
+		
+	#region Packing NServiceBus.CastleWindsor
+	$packageName = "NServiceBus.CastleWindsor" + $packageNameSuffix
+	$packit.package_description = "The CastleWindsor Container for the nservicebus"
+	invoke-packit $packageName $productVersion @{"Castle.Core"="3.0.0.2001";"Castle.Windsor"="3.0.0.2001"} "" @{".\release\net40\binaries\containers\castle\*.*"="lib\net40"}
+	#endregion
+	
+	#region Packing NServiceBus.StructureMap
+	$packageName = "NServiceBus.StructureMap" + $packageNameSuffix
+	$packit.package_description = "The StructureMap Container for the nservicebus"
+	invoke-packit $packageName $productVersion @{"structuremap"="2.6.3"} "" @{".\release\net40\binaries\containers\StructureMap\*.*"="lib\net40"}
+	#endregion		
+	
+	#region Packing NServiceBus.Unity
+	$packageName = "NServiceBus.Unity" + $packageNameSuffix
+	$packit.package_description = "The Unity Container for the nservicebus"
+	invoke-packit $packageName $productVersion @{"CommonServiceLocator"="1.0";"Unity"="2.1.505.0";"Unity.Interception"="2.1.505.0"} "" @{".\release\net40\binaries\containers\Unity\*.*"="lib\net40"}
+	#endregion
+	
+	#region Packing NServiceBus.Ninject
+	$packageName = "NServiceBus.Ninject" + $packageNameSuffix
+	$packit.package_description = "The Ninject Container for the nservicebus"
+	invoke-packit $packageName $productVersion @{"Ninject"="2.2.1.4";"Ninject.Extensions.ChildKernel"="2.2.0.5"} "" @{".\release\net40\binaries\containers\Ninject\*.*"="lib\net40"}
+	#endregion
+	
+	#region Packing NServiceBus.Spring
+	$packageName = "NServiceBus.Spring" + $packageNameSuffix
+	$packit.package_description = "The Spring Container for the nservicebus"
+	invoke-packit $packageName $productVersion @{"Common.Logging"="2.0.0";"Spring.Core"="1.3.2"} "" @{".\release\net40\binaries\containers\spring\*.*"="lib\net40"}
+	#endregion	
+	
+	#region Packing NServiceBus.NHibernate
+	$packageNameNHibernate = "NServiceBus.NHibernate" + $packageNameSuffix
+	$packit.package_description = "The NHibernate for the NServicebus"
+	invoke-packit $packageNameNHibernate $productVersion @{"Iesi.Collections"="3.2.0.4000";"NHibernate"="3.2.0.4000"} "binaries\NServiceBus.NHibernate.dll"
+	#endregion	
+	
+	
+	#region Packing NServiceBus.Azure
+	$packageName = "NServiceBus.Azure" + $packageNameSuffix
+	$packit.package_description = "The Azure for the NServicebus"
+	invoke-packit $packageName $productVersion @{$packageNameNsb=$productVersion; $packageNameNHibernate=$productVersion} "binaries\NServiceBus.Azure.dll"
+	#endregion	
 		
 	remove-module packit
  }
@@ -119,6 +165,21 @@ task ZipOutput {
 	if($env:BUILD_NUMBER -ne $null) {
     	$buildNumber = $env:BUILD_NUMBER
 	}
+	
+	$PackagingArtefactsRoot = ".\release\PackagingArtefacts"
+	$packageOutPutDir = ".\release\packages"
+	if((Test-Path -Path $PackagingArtefactsRoot) -eq $true)
+	{
+		rmdir $PackagingArtefactsRoot -Force
+	
+	}
+	
+	if((Test-Path -Path $packageOutPutDir) -eq $true)
+	{
+		rmdir $packageOutPutDir -Force
+	
+	}
+	
 	$productVersion = $buildNumber
 	
 	if((Test-Path -Path $artifacts_dir) -eq $true)
@@ -128,7 +189,7 @@ task ZipOutput {
 	
     mkdir $artifacts_dir
 	
-	$archive = "$artifacts_dir\NServiceBus$productVersion.zip"
+	$archive = "$artifacts_dir\NServiceBus.$productVersion.zip"
 	exec { 
 		& $tools_dir\zip\7za.exe a -tzip $archive $release_dir\**
 	}
