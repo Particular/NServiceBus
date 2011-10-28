5747082767593959ac30b8507c61cde054022fd4
andreasohlund <andreasohlund2@gmail.com>
2011-10-28 10:22:04 +0200

    Returning the configuration object when message conventions are set in order to make fluent configuration possible

---
+++ src/config/NServiceBus.Config/MessageConventions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace NServiceBus
+{
+    /// <summary>
+    /// Static extension methods to Configure.
+    /// </summary>
+    public static class MessageConventions
+    {
+        /// <summary>
+        /// Sets the function to be used to evaluate whether a type is a message.
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="definesMessageType"></param>
+        public static Configure DefiningMessagesAs(this Configure config, Func<Type, bool> definesMessageType)
+        {
+            ExtensionMethods.IsMessageTypeAction = definesMessageType;
+            return config;
+        }
+
+        /// <summary>
+        /// Sets the function to be used to evaluate whether a type is a commands.
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="definesCommandType"></param>
+        public static Configure DefiningCommandsAs(this Configure config, Func<Type, bool> definesCommandType)
+        {
+            ExtensionMethods.IsCommandTypeAction = definesCommandType;
+            return config;
+        }
+
+        /// <summary>
+        /// Sets the function to be used to evaluate whether a type is a event.
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="definesEventType"></param>
+        public static Configure DefiningEventsAs(this Configure config, Func<Type, bool> definesEventType)
+        {
+            ExtensionMethods.IsCommandTypeAction = definesEventType;
+            return config;
+        }
+    }
+}
