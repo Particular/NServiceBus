8537931726cfe1c63dbe3f160424ff0ac8d3125f
andreasohlund <andreasohlund2@gmail.com>
2011-11-24 13:16:41 +0100

    Modified the distributor sample to use the endpoint naming feature to name the input queues

--- Samples/Distributor/Worker1/EndpointConfig.cs
+++ Samples/Distributor/Worker1/EndpointConfig.cs
@@ -1,7 +1,9 @@
 ﻿namespace Worker1
 {
     using NServiceBus;
+    using NServiceBus.Hosting;
 
+    [EndpointName("Worker1")]
     internal class EndpointConfig : IConfigureThisEndpoint, AsA_Server
     {
     }
--- Samples/Distributor/Worker2/EndpointConfig.cs
+++ Samples/Distributor/Worker2/EndpointConfig.cs
@@ -1,7 +1,9 @@
 ﻿namespace Worker2
 {
     using NServiceBus;
+    using NServiceBus.Hosting;
 
+    [EndpointName("Worker2")]
     internal class EndpointConfig : IConfigureThisEndpoint, AsA_Server
     {
     }
