9592d2b5e1c3d16f944ec03b6350ea7b7896f9e2
andreasohlund <andreasohlund2@gmail.com>
2011-11-25 15:55:08 +0100

    Only start the gateway if it's actually configured to run

--- src/gateway/NServiceBus.Gateway/Config/GatewayConfiguration.cs
+++ src/gateway/NServiceBus.Gateway/Config/GatewayConfiguration.cs
@@ -112,6 +112,9 @@
             Configure.Instance.Builder.Build<IStartableBus>()
                             .Started += (s, e) =>
                                             {
+                                                if (!Configure.Instance.Configurer.HasComponent<GatewaySender>())
+                                                    return;
+
                                                 Configure.Instance.Builder.Build<GatewaySender>().Start(GatewayConfiguration.GatewayInputAddress);
                                                 Configure.Instance.Builder.Build<GatewayReceiver>().Start(GatewayConfiguration.GatewayInputAddress);
                                             };
---
+++ src/unicast/NServiceBus.Unicast.Tests/MessageModules.cs
@@ -0,0 +1,55 @@
+﻿namespace NServiceBus.Unicast.Tests
+{
+    using System;
+    using NUnit.Framework;
+    using SomeUserNamespace;
+    using Transport;
+
+    [TestFixture]
+    public class When_a_message_if_forwarded_via_the_fault_manager : using_the_unicastbus
+    {
+        [Test]
+        public void Should_invoke_begin_and_end_message()
+        {
+            var messageModule = new StubMessageModule();
+            bool beginCalled = false;
+
+            messageModule.OnBegin = () => { beginCalled = true; };
+
+            MessageModules.Add(messageModule);
+
+            RegisterMessageType<CommandMessage>();
+
+            Transport.SimulateMessageReceived(new TransportMessage());
+            
+            
+            
+
+            Assert.True(beginCalled);
+        }
+
+        [Test]
+        public void Should_not_invoke_handle_error()
+        {
+        }
+
+    }
+
+    public class StubMessageModule:IMessageModule
+    {
+        public Action OnBegin = () => { };
+        public void HandleBeginMessage()
+        {
+            OnBegin();
+        }
+
+        public void HandleEndMessage()
+        {
+            
+        }
+
+        public void HandleError()
+        {
+        }
+    }
+}
