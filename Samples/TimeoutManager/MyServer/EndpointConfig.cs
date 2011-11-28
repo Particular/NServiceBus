25ec0fae967acc876b91b585d3aea652ed92f324
andreasohlund <andreasohlund2@gmail.com>
2011-11-28 15:27:55 +0100

    First draft of a sample for the timeout manager

---
+++ Samples/TimeoutManager/MyServer/EndpointConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace MyServer
+{
+    using NServiceBus;
+    using NServiceBus.Timeout.Hosting.Windows.Config;
+
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization
+    {
+        public void Init()
+        {
+            Configure.With()
+                .DefaultBuilder()
+                .UseTimeoutManager();
+        }
+    }
+}
