fd46bc949a386ad0c261bb72410708160f75b0b2
shlomii <shlomi.izikovich@gmail.com>
2011-12-14 11:57:49 +0200

    Rename MasterNodeConfiguration to match ConfigureUnicastBus naming convention.

---
+++ src/masterNode/NServiceBus.MasterNode/ConfigureMasterNode.cs
@@ -0,0 +1,40 @@
+﻿namespace NServiceBus
+{
+    using Config;
+
+    public static class ConfigureMasterNode
+    {
+        public static Configure AsMasterNode(this Configure config)
+        {
+            isMasterNode = true;
+            return config;
+        }
+
+        public static bool IsConfiguredAsMasterNode(this Configure config)
+        {
+            return isMasterNode;
+        }
+
+        public static string GetMasterNode(this Configure config)
+        {
+            var section = Configure.GetConfigSection<MasterNodeConfig>();
+            if (section != null)
+                return section.Node;
+
+            return null;
+        }
+
+        public static Address GetMasterNodeAddress(this Configure config)
+        {
+            var masterNode = GetMasterNode(config);
+            
+            if (string.IsNullOrWhiteSpace(masterNode))
+                return Address.Parse(Configure.EndpointName);
+
+            return new Address(Configure.EndpointName,GetMasterNode(config));
+        }
+
+
+        static bool isMasterNode;
+    }
+}
