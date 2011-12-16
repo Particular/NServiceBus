5c41d1099b230f28593ddc2bbe6606a7d2d3e9dd
yvesgoeleven <yves.goeleven@capgemini.com>
2011-12-16 12:32:43 +0100

    Readd servicebus samples

---
+++ Samples/Azure/ServiceBusFullDuplex/MyMessages/SubmitOrderRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NServiceBus;
+
+namespace MyMessages
+{
+    [Serializable]
+    public class SubmitOrderRequest :IMessage
+    {
+        public Guid Id { get; set; }
+        public int Quantity { get; set; }
+    }
+}
