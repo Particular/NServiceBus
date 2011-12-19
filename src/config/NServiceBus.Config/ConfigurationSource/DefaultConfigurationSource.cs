1e30bb268b1c1d9424a02ae0cdf94b7a32da5235
andreasohlund <andreasohlund2@gmail.com>
2011-12-19 12:34:24 +0100

    Removed the validation that a app.config exists since we no longer require a app.config

--- src/config/NServiceBus.Config/ConfigurationSource/DefaultConfigurationSource.cs
+++ src/config/NServiceBus.Config/ConfigurationSource/DefaultConfigurationSource.cs
@@ -1,38 +1,19 @@
-using System;
-using System.Configuration;
-using System.IO;
-
 namespace NServiceBus.Config.ConfigurationSource
 {
+    using System;
+    using System.Configuration;
+
     /// <summary>
     /// A configuration source implementation on top of ConfigurationManager.
     /// </summary>
     public class DefaultConfigurationSource : IConfigurationSource
     {
-        static bool configurationIsValid;
-
         T IConfigurationSource.GetConfiguration<T>()
         {
-            ValidateConfiguration();
             if (!typeof(ConfigurationSection).IsAssignableFrom(typeof(T)))
                 throw new ArgumentException("DefaultConfigurationSource only supports .Net ConfigurationSections");
 
             return ConfigurationManager.GetSection(typeof(T).Name) as T;
-        }
-
-        void ValidateConfiguration()
-        {
-            if (configurationIsValid)
-                return;
-
-            var endpointConfigurationFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile;
-
-            if (!File.Exists(endpointConfigurationFile))
-            {
-                throw new InvalidOperationException(string.Format("No configuration file found at: {0}, the default configuration for nservicebus requires an app.config to be present. Please add one!",endpointConfigurationFile));
-            }
-
-            configurationIsValid = true;
         }
     }
 }
