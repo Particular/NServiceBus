d977f038db39de69e7786b635a17a74ded456c9b
yvesgoeleven <yves.goeleven@capgemini.com>
2011-12-20 19:47:12 +0100

    Not using raven persistence anymore if the gateway is not initialized

---
+++ Samples/Azure/AzureFullDuplex/MyMessages/IDefineMessages.cs
@@ -0,0 +1,6 @@
+namespace MyMessages
+{
+    public interface IDefineMessages
+    {
+    }
+}
--- src/gateway/NServiceBus.Gateway/Config/GatewayDefaults.cs
+++ src/gateway/NServiceBus.Gateway/Config/GatewayDefaults.cs
@@ -1,9 +1,11 @@
+
 namespace NServiceBus
 {
     using ObjectBuilder;
     using Config;
     using Gateway.Persistence;
     using Gateway.Persistence.Raven;
+    using Gateway.Sending;
     using Raven.Client;
 
     class GatewayDefaults : IWantToRunWhenConfigurationIsComplete
@@ -11,6 +13,9 @@ namespace NServiceBus
         public IConfigureComponents Configurer { get; set; }
         public void Run()
         {
+			if (!Configurer.HasComponent<GatewaySender>())
+                return;
+				
             if (!Configurer.HasComponent<IPersistMessages>())
             {
                 if (!Configurer.HasComponent<IDocumentStore>())
