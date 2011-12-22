c1f5f492b6bd49276e6a65d3b8b56294c23ee43a
andreasohlund <andreasohlund2@gmail.com>
2011-12-22 08:58:17 +0100

    Adjusting the messages convention in order to exclude the nservicebus types

--- Samples/Unobtrusive/Server/EndpointConfig.cs
+++ Samples/Unobtrusive/Server/EndpointConfig.cs
@@ -9,7 +9,7 @@
             Configure.With()
                 .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith("Commands"))
                 .DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith("Events"))
-                .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.EndsWith("Messages"))
+                .DefiningMessagesAs(t => t.Namespace != null && t.Namespace == "Messages")
                 .DefiningEncryptedPropertiesAs(p => p.Name.StartsWith("Encrypted"));
         }
     }
