ba1447337ed3251545c7dba08197875f95f5fed5
andreasohlund <andreasohlund2@gmail.com>
2011-12-23 10:53:18 +0100

    The raven resourcemanager id is now generated from the {endpointname}@{machinename} to avoid conflicting registrations when running multiple endpoints on the same machine

--- src/impl/Persistence/RavenPersistence/NServiceBus.Persistence.Raven/RavenPersistenceConstants.cs
+++ src/impl/Persistence/RavenPersistence/NServiceBus.Persistence.Raven/RavenPersistenceConstants.cs
@@ -2,10 +2,33 @@ using System;
 
 namespace NServiceBus.Persistence.Raven
 {
+    using System.Diagnostics;
+    using System.Security.Cryptography;
+    using System.Text;
+
     public static class RavenPersistenceConstants
     {
         public const string DefaultDataDirectory = @".\NServiceBusData";
         public const string DefaultUrl = "http://localhost:8080";
-        public static readonly Guid DefaultResourceManagerId = new Guid("2806A786-CAC5-404B-B6FA-B3780B4DDCBE");
+        public static Guid DefaultResourceManagerId
+        {
+            get
+            {
+                var resourceManagerId = Configure.EndpointName + "@" + Environment.MachineName;
+
+                return DeterministicGuidBuilder(resourceManagerId);
+            }
+        }
+
+        [DebuggerNonUserCode]
+        static Guid DeterministicGuidBuilder(string input)
+        {
+            //use MD5 hash to get a 16-byte hash of the string
+            var provider = new MD5CryptoServiceProvider();
+            byte[] inputBytes = Encoding.Default.GetBytes(input);
+            byte[] hashBytes = provider.ComputeHash(inputBytes);
+            //generate a guid from the hash:
+            return new Guid(hashBytes);
+        }
     }
 }
