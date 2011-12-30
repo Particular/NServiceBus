7fafc901ec1ae67b69ae8ca0f6d721668f223ccb
Jonathan Matheus <jmatheus@gmail.com>
2011-12-29 23:19:04 -0600

    Fixed raven subscription storage to only take the major version of a message into account

--- src/impl/unicast/NServiceBus.Unicast.Subscriptions.Raven/Subscription.cs
+++ src/impl/unicast/NServiceBus.Unicast.Subscriptions.Raven/Subscription.cs
@@ -20,7 +20,7 @@ namespace NServiceBus.Unicast.Subscriptions.Raven
         {
             //use MD5 hash to get a 16-byte hash of the string
             var provider = new MD5CryptoServiceProvider();
-            var inputBytes = Encoding.Default.GetBytes(messageType.TypeName + "/" + messageType.Version);
+            var inputBytes = Encoding.Default.GetBytes(messageType.TypeName + "/" + messageType.Version.Major);
             var hashBytes = provider.ComputeHash(inputBytes);
             //generate a guid from the hash:
             var id = new Guid(hashBytes);
