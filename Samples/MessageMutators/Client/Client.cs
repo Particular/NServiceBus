56a5efa8cc6bdb9b0efe6290240b262f924cfb6f
shlomii <shlomi.izikovich@gmail.com>
2012-01-12 09:44:10 +0200

    Allow the sample to run after invalid message exception is thrown

--- Samples/MessageMutators/Client/Client.cs
+++ Samples/MessageMutators/Client/Client.cs
@@ -16,13 +16,13 @@ namespace Client
 
             while ((cmd = Console.ReadKey().Key.ToString().ToLower()) != "q")
             {
-                switch(cmd)
+                switch (cmd)
                 {
                     case "s":
                         Bus.Send<CreateProductCommand>(m =>
                         {
                             m.ProductId = "XJ128";
-                            m.ProductName= "Milk";
+                            m.ProductName = "Milk";
                             m.ListPrice = 4;
                             m.SellEndDate = new DateTime(2012, 1, 3);
                             // 7MB. MSMQ should throw an exception, but it will not since the buffer will be compressed 
@@ -31,16 +31,21 @@ namespace Client
                         });
                         break;
                     case "e":
-                        Bus.Send<CreateProductCommand>(m =>
+                        try
                         {
-                            m.ProductId = "XJ128";
-                            m.ProductName = "Milk Milk Milk Milk Milk";
-                            m.ListPrice = 15;
-                            m.SellEndDate = new DateTime(2011, 1, 3);
-                            // 7MB. MSMQ should throw an exception, but it will not since the buffer will be compressed 
-                            // before it reaches MSMQ.
-                            m.Image = new byte[1024 * 1024 * 7];
-                        });
+                            Bus.Send<CreateProductCommand>(m =>
+                                                               {
+                                                                   m.ProductId = "XJ128";
+                                                                   m.ProductName = "Milk Milk Milk Milk Milk";
+                                                                   m.ListPrice = 15;
+                                                                   m.SellEndDate = new DateTime(2011, 1, 3);
+                                                                   // 7MB. MSMQ should throw an exception, but it will not since the buffer will be compressed 
+                                                                   // before it reaches MSMQ.
+                                                                   m.Image = new byte[1024 * 1024 * 7];
+                                                               });
+                        }
+                        //Just to allow the sample to keep running.
+                        catch (Exception exception){}
                         break;
                 }
             }
