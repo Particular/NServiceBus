9676718edd7530b73555fc30c7528cc377190fb5
andreasohlund <andreasohlund2@gmail.com>
2012-02-07 14:58:15 +0100

    Moved the dtc installer down to the transactional transport where it belongs

---
+++ src/impl/unicast/transport/NServiceBus.Unicast.Transport.Transactional/Installers/DtcInstaller.cs
@@ -0,0 +1,25 @@
+﻿namespace NServiceBus.Unicast.Transport.Transactional.Installers
+{
+    using System.Security.Principal;
+    using Installation;
+    using Installation.Environments;
+    using Utils;
+
+    public class DtcInstaller:INeedToInstallInfrastructure<Windows>
+    {
+        static DtcInstaller()
+        {
+            IsEnabled = true;
+        }
+
+        public static bool IsEnabled { get; set; }
+
+        public void Install(WindowsIdentity identity)
+        {
+            if(!IsEnabled)
+                return;
+            
+            DtcUtil.StartDtcIfNecessary();
+        }
+    }
+}
