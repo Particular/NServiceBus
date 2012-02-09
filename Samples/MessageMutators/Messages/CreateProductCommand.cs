e3ccaea222a124a86e7ea9d5eb799270be65b67d
shlomii <shlomi.izikovich@gmail.com>
2012-02-09 14:53:22 +0200

    Some spelling mistakes in MessageMutators sample

--- Samples/MessageMutators/Messages/CreateProductCommand.cs
+++ Samples/MessageMutators/Messages/CreateProductCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using NServiceBus;
 
 namespace Messages
@@ -15,22 +16,7 @@ namespace Messages
         [Range(1, 5)]
         public decimal ListPrice { get; set; }
 
-        /// <summary>
-        /// Custom Range Attribute for Date to avoid the problem with date format
-        /// </summary>
-        internal class DateRageAttribute : RangeAttribute
-        {
-            /// <summary>
-            /// Constructor
-            /// </summary>
-            public DateRageAttribute() :
-                base(typeof(DateTime), (new DateTime(2012, 1, 1)).ToString(), (new DateTime(2012, 1, 15)).ToString())
-            {
-
-            }
-        }
-
-        [DateRageAttribute(ErrorMessage = "Value for {0} must be between {1} and {2}")]
+        [DateRange(ErrorMessage = "Value for {0} must be between {1} and {2}")]
         public DateTime SellEndDate { get; set; }
 
         public byte[] Image { get; set; }
@@ -40,6 +26,20 @@ namespace Messages
             return string.Format(
                 "CreateProductCommand: ProductId={0}, ProductName={1}, ListPrice={2} SellEndDate={3} Image (length)={4}",
                 ProductId, ProductName, ListPrice, SellEndDate, (Image == null ? 0: Image.Length));
+        }
+    }
+    /// <summary>
+    /// Custom Range Attribute for Dates range.
+    /// </summary>
+    internal class DateRangeAttribute : RangeAttribute
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public DateRangeAttribute() :
+            base(typeof(DateTime), (new DateTime(2012, 1, 1)).ToString(CultureInfo.InvariantCulture), (new DateTime(2012, 5, 1)).ToString(CultureInfo.InvariantCulture))
+        {
+            
         }
     }
 }
