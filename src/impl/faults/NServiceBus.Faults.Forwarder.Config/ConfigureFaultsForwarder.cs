95b516d7092fc58abd4ce1d09d48ce7ffa85e1bf
shlomii <shlomi.izikovich@gmail.com>
2012-03-07 12:51:16 +0200

    Throw a better exception message if MessageForwardingInCaseOfFaultConfig section is missing and MsmqTransportConfig does not contain an Error queue value.

--- src/impl/faults/NServiceBus.Faults.Forwarder.Config/ConfigureFaultsForwarder.cs
+++ src/impl/faults/NServiceBus.Faults.Forwarder.Config/ConfigureFaultsForwarder.cs
@@ -22,20 +22,29 @@ namespace NServiceBus
         public static Configure MessageForwardingInCaseOfFault(this Configure config)
         {
             var section = Configure.GetConfigSection<MessageForwardingInCaseOfFaultConfig>();
-            if (section == null)
+            if (section != null)
+            {
+                if (string.IsNullOrWhiteSpace(section.ErrorQueue))
+                    throw new ConfigurationErrorsException(
+                        "'MessageForwardingInCaseOfFaultConfig' configuration section is found but 'ErrorQueue' value is missing." +
+                        "\n The following is an example for adding such a value to your app config: " +
+                        "\n <MessageForwardingInCaseOfFaultConfig ErrorQueue=\"error\"/> \n");
+                
+                ErrorQueue = Address.Parse(section.ErrorQueue);
+            }
+            else
             {
                 Logger.Warn("Could not find configuration section 'MessageForwardingInCaseOfFaultConfig'. Going to try to find the error queue defined in 'MsmqTransportConfig'.");
 
                 var msmq = Configure.GetConfigSection<MsmqTransportConfig>();
+
                 if ((msmq == null) || (string.IsNullOrWhiteSpace(msmq.ErrorQueue)))
-                    throw new ConfigurationErrorsException("Could not find backup configuration section 'MsmqTransportConfig' in order to locate the error queue.");
+                    throw new ConfigurationErrorsException("'MessageForwardingInCaseOfFaultConfig' configuration section is missing and could not find backup configuration section 'MsmqTransportConfig' in order to locate the error queue.");
 
                 ErrorQueue = Address.Parse(msmq.ErrorQueue);
             }
-            else
-                ErrorQueue =  Address.Parse(section.ErrorQueue);
 
-			if(ErrorQueue == Address.Undefined)
+            if(ErrorQueue == Address.Undefined)
 				throw new ConfigurationErrorsException("Faults forwarding requires an error queue to be specified. Please add a 'MessageForwardingInCaseOfFaultConfig' section to your app.config");
              
             config.Configurer.ConfigureComponent<FaultManager>(DependencyLifecycle.InstancePerCall)
