02f6271b87a05ccd60b75e907dedaaa471a54429
andreasohlund <andreasohlund2@gmail.com>
2012-03-09 14:17:11 +0100

    Added mutators to promote saga id from incoming timeout messages. This allows us to be backwards compatible with 2.X timeoutmessages

--- src/timeout/NServiceBus.Timeout.Core/TimeoutMessageConverter.cs
+++ src/timeout/NServiceBus.Timeout.Core/TimeoutMessageConverter.cs
@@ -5,19 +5,46 @@ namespace NServiceBus.Timeout.Core
     using MessageMutator;
     using Saga;
 
-    public class TimeoutMessageConverter : IMutateOutgoingMessages,INeedInitialization
+    public class TimeoutMessageConverter : IMessageMutator, INeedInitialization
     {
+        public IBus Bus { get; set; }
+
+        /// <summary>
+        /// Set the headers for outgoing messages to make sure that the 3.0 timeout manager will work correctly
+        /// </summary>
+        /// <param name="message"></param>
         public object MutateOutgoing(object message)
+        {
+            PromoteHeaders(message);
+
+            return message;
+        }
+
+   
+        void PromoteHeaders(object message)
         {
             var timeoutMessage = message as TimeoutMessage;
 
-            if (timeoutMessage != null)
-            {
-                message.SetHeader(Headers.Expire, timeoutMessage.Expires.ToWireFormattedString());
+            if (timeoutMessage == null)
+                return;
 
-                if (timeoutMessage.ClearTimeout)
-                    message.SetHeader(Headers.ClearTimeouts, true.ToString(CultureInfo.InvariantCulture));
-            }
+            Bus.CurrentMessageContext.Headers[Headers.SagaId] = timeoutMessage.SagaId.ToString();
+            Bus.CurrentMessageContext.Headers[Headers.Expire] = timeoutMessage.Expires.ToWireFormattedString();
+
+            if (timeoutMessage.ClearTimeout)
+                Bus.CurrentMessageContext.Headers[Headers.ClearTimeouts] = true.ToString(CultureInfo.InvariantCulture);
+
+        }
+
+
+        /// <summary>
+        /// We set the values for incoming messages as well so that we are backwards compatible with 2.6 timeouts
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public object MutateIncoming(object message)
+        {
+            PromoteHeaders(message);
 
             return message;
         }
@@ -26,5 +53,6 @@ namespace NServiceBus.Timeout.Core
         {
             Configure.Instance.Configurer.ConfigureComponent<TimeoutMessageConverter>(DependencyLifecycle.InstancePerCall);
         }
+
     }
 }
