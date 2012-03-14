fa5fde43c0183d20f41e97650f2a6048b3d3a0e4
shlomii <shlomi.izikovich@gmail.com>
2012-03-14 11:38:35 +0200

    Mutate subscription messages from endpoints running NServiceBus version 2.6 Closes #183

---
+++ src/unicast/NServiceBus.Unicast.BackwardCompatibility/MutateSubscriptionMessage.cs
@@ -0,0 +1,27 @@
+﻿using NServiceBus.Config;
+using NServiceBus.MessageMutator;
+using NServiceBus.Unicast.Transport;
+
+namespace NServiceBus.Unicast.BackwardCompatibility 
+{
+    class MutateSubscriptionMessage : IMutateIncomingTransportMessages, INeedInitialization
+    {
+        /// <summary>
+        /// Mutate incoming version 2.6 subscription messages.
+        /// If the message is a completion message that contains the "EnclosedMessageTypes" key, then change the Message Intent.
+        /// </summary>
+        /// <param name="transportMessage"></param>
+        public void MutateIncoming(TransportMessage transportMessage)
+        {
+            if (!transportMessage.Headers.ContainsKey("EnclosedMessageTypes"))
+                return;
+
+            transportMessage.MessageIntent--;
+        }
+
+        public void Init()
+        {
+            Configure.Instance.Configurer.ConfigureComponent<MutateSubscriptionMessage>(DependencyLifecycle.InstancePerCall);
+        }
+    }
+}
---
+++ src/unicast/NServiceBus.Unicast.BackwardCompatibility/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NServiceBus.Unicast.BackwardCompatibility")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NServiceBus.Unicast.BackwardCompatibility")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c41eb30d-991e-433c-a0f3-b121aacdbe73")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
