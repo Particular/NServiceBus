1686509356813af847ea298e97294d9091d9a091
yvesgoeleven <yves.goeleven@capgemini.com>
2012-03-30 14:16:01 +0200

    triggering build, it failed without giving an error very early in the process?

--- default.ps1
+++ default.ps1
@@ -739,7 +739,7 @@ else{
 	invoke-packit $packageNameHostingAzure $script:packageVersion @{$packageNameAzure=$script:packageVersion; } "binaries\NServiceBus.Hosting.Azure.dll" @{}
 	#endregion
 	
-	#region Packing NServiceBus.Hosting.Azure
+	#region Packing NServiceBus.Timeout.Hosting.Azure
 	$packageNameTimeoutHostingAzure = "NServiceBus.Timeout.Hosting.Azure" + $PackageNameSuffix
 	$packit.package_description = "The Azure Host for the timeout manager on NServicebus"
 	invoke-packit $packageNameTimeoutHostingAzure $script:packageVersion @{$packageNameHostingAzure=$script:packageVersion; } "binaries\NServiceBus.Timeout.Hosting.Azure.dll" @{}
