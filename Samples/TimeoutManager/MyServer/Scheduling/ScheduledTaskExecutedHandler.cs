9d8e32126d0b5117b21e7e836d320c990fb65439
Mikael Nordberg <nordbergmikael@hotmail.com>
2012-04-24 15:55:24 +0200

    Added new project, NServiceBus.Scheduling, including sample code in the Timeout manager sample:

---
+++ Samples/TimeoutManager/MyServer/Scheduling/ScheduledTaskExecutedHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NServiceBus;
+
+namespace MyServer.Scheduling
+{
+    public class ScheduledTaskExecutedHandler : IHandleMessages<ScheduledTaskExecuted>
+    {
+        public void Handle(ScheduledTaskExecuted message)
+        {
+            Console.WriteLine("ScheduledTaskExecuted handler executed");
+        }
+    }
+}
---
+++ src/scheduling/NServiceBus.Scheduling/IScheduledTaskStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NServiceBus.Scheduling
+{
+    public interface IScheduledTaskStorage
+    {
+        void Add(ScheduledTask scheduledTask);
+        ScheduledTask Get(Guid taskId);
+        IDictionary<Guid, ScheduledTask> Tasks { get; }
+    }
+}
