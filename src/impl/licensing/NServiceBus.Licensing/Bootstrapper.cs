56e0427434a6af5f4900dca8f314423d705b7ce8
shlomii <shlomi.izikovich@gmail.com>
2012-05-01 19:18:40 +0300

    Mute Rhino.Licensing LicenseValidator INFO message (by accepting from the validator log level of WARN). Closes #272

--- src/impl/licensing/NServiceBus.Licensing/Bootstrapper.cs
+++ src/impl/licensing/NServiceBus.Licensing/Bootstrapper.cs
@@ -9,18 +9,36 @@ namespace NServiceBus.Licensing
         public void Run()
         {
             var validated = Configure.Instance.HasValidLicense();
-
-            if (!validated)
+            if (validated)
             {
-                var transport = Configure.Instance.Builder.Build<TransactionalTransport>();
-                var numWorkerThreadsInfo = typeof (TransactionalTransport).GetField("numberOfWorkerThreads",
-                                                         BindingFlags.Instance | BindingFlags.NonPublic);
-
-                //intentionally don't check for null so that this will blow up if there are changes
-                numWorkerThreadsInfo.SetValue(transport, 1);
+                ChangeRhinoLicensingLogLevelToWarn();
+                return;
             }
-        }
 
+            var transport = Configure.Instance.Builder.Build<TransactionalTransport>();
+            var numWorkerThreadsInfo = typeof (TransactionalTransport).GetField("numberOfWorkerThreads",
+                                                        BindingFlags.Instance | BindingFlags.NonPublic);
+
+            //intentionally don't check for null so that this will blow up if there are changes
+            numWorkerThreadsInfo.SetValue(transport, 1);
+        }
+        
+        private void ChangeRhinoLicensingLogLevelToWarn()
+        {
+            Assembly rhinoLicensingAssembly = Assembly.GetAssembly(typeof(Rhino.Licensing.LicenseValidator));
+            if (rhinoLicensingAssembly == null) return;
+            log4net.Repository.ILoggerRepository rhinoLicensingRepository =
+                log4net.LogManager.GetRepository(rhinoLicensingAssembly);
+            
+            if (rhinoLicensingRepository == null) return;
+
+            var hier = (log4net.Repository.Hierarchy.Hierarchy)rhinoLicensingRepository;
+            var licenseValidatorLogger = hier.GetLogger("Rhino.Licensing.LicenseValidator");
+            if (licenseValidatorLogger == null) return;
+
+            ((log4net.Repository.Hierarchy.Logger)licenseValidatorLogger).Level = hier.LevelMap["WARN"];
+
+        }
         public void Init()
         {
             Configure.Instance.Configurer.RegisterSingleton<LicenseManager>(new LicenseManager());
