99ff62b5bbe7f108ba5a56cbf6aaadbe1efee86d
John Simons <johnsimons007@yahoo.com.au>
2012-05-18 16:49:10 +1000

    Fixed copyright symbol

--- default.ps1
+++ default.ps1
@@ -954,7 +954,7 @@ task GenerateAssemblyInfo -description "Generates assembly info for all the proj
 		"release" `
 		"NServiceBus" `
 		$assemblyProduct `
-		"Copyright � NServiceBus 2007-2012" `
+		"Copyright (C) NServiceBus 2007-2012" `
 		$asmVersion `
 		$fileVersion `
 		$infoVersion `
