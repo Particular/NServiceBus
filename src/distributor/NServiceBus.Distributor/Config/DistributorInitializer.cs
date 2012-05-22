834522bd62b5e3731890c697b491837bdd9e8036
andreasohlund <andreasohlund2@gmail.com>
2012-05-22 14:34:19 +0200

    Fixing the distributor to do its initialization in the correct order relating to the profiles.

--- src/distributor/NServiceBus.Distributor/Config/DistributorInitializer.cs
+++ src/distributor/NServiceBus.Distributor/Config/DistributorInitializer.cs
@@ -4,13 +4,10 @@ namespace NServiceBus.Distributor.Config
     using Unicast;
     using log4net;
 
-    public class DistributorInitializer : INeedInitialization
+    public class DistributorInitializer
     {
-        public void Init()
+        public static void Init(bool withWorker)
         {
-            if (!Configure.Instance.DistributorConfiguredToRunOnThisEndpoint())
-                return;
-
             var config = Configure.Instance;
 
             var masterNodeAddress = config.GetMasterNodeAddress();
@@ -18,7 +15,7 @@ namespace NServiceBus.Distributor.Config
 
             config.Configurer.ConfigureComponent<UnicastBus>(DependencyLifecycle.SingleInstance)
                 .ConfigureProperty(r => r.InputAddress, masterNodeAddress.SubScope("worker"))
-                .ConfigureProperty(r => r.DoNotStartTransport, !config.WorkerShouldRunOnDistributorEndpoint());
+                .ConfigureProperty(r => r.DoNotStartTransport, !withWorker);
 
             config.Configurer.ConfigureComponent<MsmqWorkerAvailabilityManager.MsmqWorkerAvailabilityManager>(
                 DependencyLifecycle.SingleInstance)
