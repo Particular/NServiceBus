f346bd1370bb09889fcc266e88ef90da6b8cf07f
Mikael Nordberg <nordbergmikael@hotmail.com>
2012-06-25 16:22:52 +0200

    Init

---
+++ src/installation/NServiceBus.Installation/IWantQueuesCreated.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Principal;
+
+namespace NServiceBus.Installation
+{
+    public interface IWantQueuesCreated
+    {
+        void Create(WindowsIdentity identity);
+    }
+
+    public interface IWantQueuesCreated<T> : IWantQueuesCreated where T : IEnvironment
+    {
+    }
+}
