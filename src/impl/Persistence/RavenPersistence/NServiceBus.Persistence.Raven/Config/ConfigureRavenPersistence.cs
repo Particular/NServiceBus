c3dcec7267cdd38272ea8c54ff9bdd6be93fa409
John Simons <johnsimons007@yahoo.com.au>
2012-07-02 18:52:34 +1000

    Trying to access ravendb before install finished installing, throws WebException, closes #509
    No need to create RavenDB database if user called DisableRavenInstall()

--- src/impl/Persistence/RavenPersistence/NServiceBus.Persistence.Raven/Config/ConfigureRavenPersistence.cs
+++ src/impl/Persistence/RavenPersistence/NServiceBus.Persistence.Raven/Config/ConfigureRavenPersistence.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq.Expressions;
 using Raven.Client;
 using Raven.Client.Document;
 
@@ -144,14 +143,22 @@ namespace NServiceBus
                 //and the turn the compression off
                 dummyStore.JsonRequestFactory.DisableRequestCompression = !enableRequestCompression;
 
-                //and then make sure that the database the user asked for is created
-                dummyStore.DatabaseCommands.EnsureDatabaseExists(store.DefaultDatabase);
+                try
+                {
+                    //and then make sure that the database the user asked for is created
+                    dummyStore.DatabaseCommands.EnsureDatabaseExists(store.DefaultDatabase);    
+                }
+                catch (System.Net.WebException)
+                {
+                    //Ignore since this could be running as part of an install
+                }
             }
         }
 
         public static Configure DisableRavenInstall(this Configure config)
         {
             ravenInstallEnabled = false;
+            AutoCreateDatabase = false;
 
             return config;
         }
