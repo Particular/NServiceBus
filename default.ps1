bb81466feea9c0d22e784ed40b6c92351f69b63e
shlomii <shlomiizikovich@gmail.com>
2012-07-09 09:18:06 +0300

    Change patch number to 5

--- default.ps1
+++ default.ps1
@@ -1,7 +1,7 @@
 properties {
 	$ProductVersion = "3.2"
 	$BuildNumber = "0";
-	$PatchVersion = "4"
+	$PatchVersion = "5"
 	$PreRelease = "-build"	
 	$PackageNameSuffix = ""
 	$TargetFramework = "net-4.0"
