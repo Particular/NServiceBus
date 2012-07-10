bb8aaaeba5908809bc84e6976a7750c776d9f435
SimonCropp <simon.cropp@gmail.com>
2012-07-10 13:58:22 +1000

    Dont error when cant create queue on remote machine.
    But on this point why bother even trying to create queues on remote machines?

--- src/utils/MsmqUtilities.cs
+++ src/utils/MsmqUtilities.cs
@@ -44,7 +44,8 @@ namespace NServiceBus.Utils
 
             var q = GetFullPathWithoutPrefix(address);
 
-            if (address.Machine != Environment.MachineName.ToLower())
+            var isRemote = address.Machine != Environment.MachineName.ToLower();
+            if (isRemote)
             {
                 Logger.Debug("Queue is on remote machine.");
                 Logger.Debug("If this does not succeed (like if the remote machine is disconnected), processing will continue.");
@@ -65,6 +66,14 @@ namespace NServiceBus.Utils
                 Logger.Debug("Going to create queue: " + q);
 
                 CreateQueue(q, account);
+            }
+            catch (MessageQueueException ex)
+            {
+                if (isRemote && (ex.MessageQueueErrorCode == MessageQueueErrorCode.IllegalQueuePathName))
+                {
+                    return;
+                }
+                Logger.Error(string.Format("Could not create queue {0} or check its existence. Processing will still continue.", address), ex);
             }
             catch (Exception ex)
             {
