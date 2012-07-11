9cd04ad448a4998ac246817d57ff9d4bab5ac4b9
John Simons <johnsimons007@yahoo.com.au>
2012-07-11 17:54:08 +1000

    SLR set the wrong 'Response' value in MSMQ message, fixes #525

--- src/management/retries/NServiceBus.Management.Retries.Tests/SecondLevelRetriesTests.cs
+++ src/management/retries/NServiceBus.Management.Retries.Tests/SecondLevelRetriesTests.cs
@@ -35,6 +35,34 @@ namespace NServiceBus.Management.Retries.Tests
         }
 
         [Test]
+        public void Message_should_have_ReplyToAddress_set_to_original_sender_when_sent_to_real_errorq()
+        {
+            var expected = new Address("clientQ", "myMachine");
+            _message.ReplyToAddress = expected;
+            SecondLevelRetries.RetryPolicy = _ => TimeSpan.MinValue;
+
+            _satellite.Handle(_message);
+
+            Assert.AreEqual(expected, _message.ReplyToAddress);
+        }
+
+        [Test]
+        public void Message_should_have_ReplyToAddress_set_to_original_sender_when_sent_to_real_errorq_after_retries()
+        {
+            TransportMessageHelpers.SetHeader(_message, Faults.FaultsHeaderKeys.FailedQ, "reply@address");            
+
+            var expected = new Address("clientQ", "myMachine");
+            _message.ReplyToAddress = expected;
+
+            for (var i = 0; i < DefaultRetryPolicy.NumberOfRetries + 1; i++)
+            {
+                _satellite.Handle(_message);
+            }
+
+            Assert.AreEqual(expected, _message.ReplyToAddress);
+        }
+
+        [Test]
         public void Message_should_be_sent_to_real_errorQ_if_defer_timespan_is_less_than_zero()
         {
             TransportMessageHelpers.SetHeader(_message, Faults.FaultsHeaderKeys.FailedQ, "reply@address");
