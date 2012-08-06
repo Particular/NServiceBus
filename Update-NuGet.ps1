cec8354c795ba37c09e6cbbefd489e898f9f5658
John Simons <johnsimons007@yahoo.com.au>
2012-08-06 12:40:07 +1000

    Updated nuget to 2.0. closes #488
    Added script to update nuget.exe to latest.

---
+++ Update-NuGet.ps1
@@ -0,0 +1,9 @@
+﻿$scriptPath = Split-Path (Resolve-Path $MyInvocation.MyCommand.Path)
+Get-ChildItem -Filter nuget.exe -Name -Recurse -Path $scriptPath | ForEach-Object {
+	$nugetExePath = Join-Path $scriptPath $_
+    echo "Checking $_"
+    Start-Process -NoNewWindow -Wait -FilePath $nugetExePath -ArgumentList 'update -Self'
+    if(Test-Path "${nugetExePath}.old") {
+		Remove-Item -Path "${nugetExePath}.old"
+    }
+}
