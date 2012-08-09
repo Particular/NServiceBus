22c94cf958c4832ef5018bdaf3e476aef1b5a742
John Simons <john.simons@nservicebus.com>
2012-08-09 19:03:58 +1000

    Powershell to upgrade nuget packages

---
+++ Update-NuGet-Package.ps1
@@ -0,0 +1,14 @@
+﻿$ids = $args
+$scriptPath = Split-Path (Resolve-Path $MyInvocation.MyCommand.Path)
+Get-ChildItem -Filter packages.config -Name -Recurse -Path $scriptPath | ForEach-Object {
+	$nugetExePath = Join-Path $scriptPath 'tools\NuGet\nuget.exe'
+    $packagePath = Join-Path $scriptPath $_
+    $nugetRepositoryPath = Join-Path $scriptPath 'packages'
+    
+    echo "Checking if $packagePath contains ""$ids"" package(s) and updating if required..."
+    
+    $arguments = "update ""$packagePath"" -RepositoryPath ""$nugetRepositoryPath"" -Id ""$ids"""
+       
+    Start-Process -NoNewWindow -Wait -FilePath $nugetExePath -ArgumentList $arguments
+    
+}
