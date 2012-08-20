53529f8efa35f93c35f68322556e61b5e50d05be
andreasohlund <andreasohlund2@gmail.com>
2012-08-20 19:42:22 +0200

    The gateway notifier is now registered as singlecall so that users can actually register for notifications. Closes #580

---
+++ Samples/Gateway/Headquarter/Notifications/MessageForwarded.cs
@@ -0,0 +1,22 @@
+﻿namespace Headquarter.Notifications
+{
+    using System;
+    using NServiceBus.Gateway.Notifications;
+    using NServiceBus.Unicast;
+
+    public class MessageForwarded:IWantToRunWhenTheBusStarts
+    {
+        public IMessageNotifier MessageNotifier { get; set; }
+ 
+        void MessageNotifier_MessageForwarded(object sender, MessageReceivedOnChannelArgs e)
+        {
+            Console.WriteLine(string.Format("Message with id {0} arrived on channel {1} and was forwarded onto {2}",e.Message.Id,e.FromChannel,e.ToChannel));
+        }
+
+        public void Run()
+        {
+            MessageNotifier.MessageForwarded += MessageNotifier_MessageForwarded;           
+        }
+
+    }
+}
