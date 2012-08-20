787987feebb737bb2fac77219e33b7c073f69c58
shlomii <shlomiizikovich@gmail.com>
2012-08-20 10:00:21 +0300

    Fixed #601: Better message when ConnectionString is not configured properly.

--- src/impl/unicast/queuing/NServiceBus.Unicast.Queuing.SQLServer.Config/ConfigureSqlServerTransport.cs
+++ src/impl/unicast/queuing/NServiceBus.Unicast.Queuing.SQLServer.Config/ConfigureSqlServerTransport.cs
@@ -3,6 +3,7 @@ using NServiceBus.Unicast.Queuing.Installers;
 
 namespace NServiceBus
 {
+    using System.Configuration;
     using Unicast.Queuing.SQLServer;
     using Config;
 
@@ -11,6 +12,13 @@ namespace NServiceBus
         public static Configure SqlServerTransport(this Configure configure)
         {
             var cfg = Configure.GetConfigSection<SqlServerTransportConfig>();
+            if (cfg == null)
+            {
+                throw new ConfigurationErrorsException(
+                    string.Format("Failed to load SqlServerTransportConfig section from app.config, please add the following section: \n[{0}]\n\n and its value, for example: \n[{1}]\n",
+                    "<section name=\"SqlServerTransportConfig\" type=\"NServiceBus.Config.SqlServerTransportConfig, NServiceBus.Core\"/>",
+                    "<SqlServerTransportConfig ConnectionString=\"Data Source=localhost\\SQLEXPRESS;Initial Catalog=SiteB;Integrated Security=True;Pooling=False\"/>"));
+            }
             return SqlServerTransport(configure, cfg.ConnectionString);
         }
 
