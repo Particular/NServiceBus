21a551669c83db7f5d064fe5c40d7e06ae4aeb57
andreasohlund <andreasohlund2@gmail.com>
2012-08-21 13:08:16 +0200

    Databus properties are now transmitted correctly when using the gateway Closes #584

--- Samples/Gateway/SiteA/EndpointConfig.cs
+++ Samples/Gateway/SiteA/EndpointConfig.cs
@@ -4,5 +4,14 @@ namespace SiteA
 {
     // The endpoint is started with the RunGateway profile which turns it on. The Lite profile is also
     // active which will configure the persistence to be InMemory
-    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server{}
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server,IWantCustomInitialization
+    {
+        public void Init()
+        {
+            Configure.With()
+                .DefaultBuilder()
+                .XmlSerializer()
+                .FileShareDataBus(".\\databus");
+        }
+    }
 }
