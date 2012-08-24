cd7d75cdbf0e118af6f070b2fe9ed491fca75928
John Simons <john.simons@nservicebus.com>
2012-08-24 11:36:17 +1000

    Updated nuget packages dependencies.
    Deleted tools package.

--- NuGet/NServiceBus.Tools/tools/init.ps1
+++
@@ -1,3 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-    Write-Host "The Tools Path ($toolsPath) has been added to the env:PATH. Please use RunMeFirst.bat and returntosourcequeue.exe directly in Package Manager Console"
-
