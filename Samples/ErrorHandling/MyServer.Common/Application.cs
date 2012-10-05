efe6fa1ffb70b0d134cce8c959ebe7a30f958168
John Simons <john.simons@nservicebus.com>
2012-10-05 12:06:40 +1000

    Removed stress test code.

--- Samples/ErrorHandling/MyServer.Common/Application.cs
+++ Samples/ErrorHandling/MyServer.Common/Application.cs
@@ -3,33 +3,28 @@ using NServiceBus;
 
 namespace MyServer.Common
 {
-    using System.Threading.Tasks;
-
     public class Application : IWantToRunAtStartup
     {
-        public IBus Bus { get; set; }        
+        public IBus Bus { get; set; }
 
         public void Run()
-        {            
-            Console.WriteLine("Press 'S' to send a message that will throw an exception.");            
-            Console.WriteLine("Press 'Q' to exit.");            
-                        
+        {
+            Console.WriteLine("Press 'S' to send a message that will throw an exception.");
+            Console.WriteLine("Press 'Q' to exit.");
+
             string cmd;
 
-            while ((cmd = Console.ReadKey().Key.ToString().ToLower()) != "q")            
+            while ((cmd = Console.ReadKey().Key.ToString().ToLower()) != "q")
             {
-                Console.WriteLine("");
-
                 switch (cmd)
                 {
                     case "s":
-                        Parallel.For(0, 100, (i, s) =>
-                            {
-                                var m = new MyMessage { Id = Guid.NewGuid() };
-                                Bus.Send("myserver", m);
-                            });
+
+                        var m = new MyMessage {Id = Guid.NewGuid()};
+
+                        Bus.Send("myserver", m);
                         break;
-                }                
+                }
             }
         }
 
