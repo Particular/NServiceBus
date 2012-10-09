322b68e2d2ddf375cde4e88a8672ae792d101280
Jason Duffett <github@jasonduffett.org>
2012-10-09 14:54:51 +0100

    NRE on IEnumerable<T> members with null elements
    XmlSerializer couldn't work out the type to serialize for elements of
    IEnumerable which were null resulting in a NullReferenceException

---
+++ src/impl/Serializers/NServiceBus.Serializers.XML.Test/SerializingEnumerableTests.cs
@@ -0,0 +1,37 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NServiceBus.Serializers.XML.Test
+{
+    [Serializable]
+    public class MessageWithEnumerableOfString
+    {
+        public Guid SagaId { get; set; }
+        public IEnumerable<string> SomeStrings { get; set; }
+    }
+
+    [TestFixture]
+    public class SerializingEnumerableTests
+    {
+        [Test]
+        public void CanSerializeNullElements()
+        {
+            var message = new MessageWithEnumerableOfString
+                {
+                    SomeStrings = new[]
+                        {
+                            "element 1",
+                            null,
+                            null,
+                           "element 2"
+                        }
+                };
+
+            var result = ExecuteSerializer.ForMessage<MessageWithEnumerableOfString>(message);
+            Assert.IsNotNull(result.SomeStrings);
+            Assert.AreEqual(4, result.SomeStrings.Count());
+        }
+    }
+}
