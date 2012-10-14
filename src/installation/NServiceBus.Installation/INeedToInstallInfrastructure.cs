6dd9b7790eecee482564c42222a784df049de2b6
shlomii <shlomiizikovich@gmail.com>
2012-10-14 11:01:11 +0200

    Fixed #683: Deprecate INeedToInstallInfrastructure

--- src/installation/NServiceBus.Installation/INeedToInstallInfrastructure.cs
+++ src/installation/NServiceBus.Installation/INeedToInstallInfrastructure.cs
@@ -4,6 +4,7 @@
     /// Interface invoked by the infrastructure when going to install an endpoint.
     /// Implementors should not implement this type directly but rather the generic version of it.
     /// </summary>
+    [ObsoleteEx(Message = "It is possible to use NServiceBus installer or powershell support to accomplish the same thing. Please see http://www.nservicebus.com/powershell.aspx for more information.", RemoveInVersion = "5.0")]
     public interface INeedToInstallInfrastructure : INeedToInstallSomething
     {
         
@@ -14,6 +15,7 @@
     /// Implementors invoked before <see cref="INeedToInstallSomething"/>.
     /// </summary>
     /// <typeparam name="T"></typeparam>
+    [ObsoleteEx(Message = "It is possible to use NServiceBus installer or powershell support to accomplish the same thing. Please see http://www.nservicebus.com/powershell.aspx for more information.", RemoveInVersion = "5.0")]
     public interface INeedToInstallInfrastructure<T> : INeedToInstallInfrastructure where T : IEnvironment
     {
         
