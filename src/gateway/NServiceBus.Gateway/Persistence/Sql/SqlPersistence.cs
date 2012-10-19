8c2d88d6ac4979c6be8c2125b411add6f413006e
Chris Bednarski <chris.bednarski@outlook.com>
2012-10-19 15:43:16 +1100

    added UpdateHeader implementation

--- src/gateway/NServiceBus.Gateway/Persistence/Sql/SqlPersistence.cs
+++ src/gateway/NServiceBus.Gateway/Persistence/Sql/SqlPersistence.cs
@@ -16,6 +16,7 @@
 
             var stream = new MemoryStream();
             serializer.Serialize(stream, headers);
+            stream.Position = 0;
             
             using (stream)
             using (var cn = new SqlConnection(ConnectionString))
@@ -28,30 +29,10 @@
                     cmd.CommandText =
                         "IF NOT EXISTS (SELECT Status FROM Messages WHERE (ClientId = @ClientId)) INSERT INTO Messages  (DateTime, ClientId, Status, Message, Headers) VALUES (@DateTime, @ClientId, 0, @Message, @Headers)";
 
-                    var datetimeIdParam = cmd.CreateParameter();
-                    datetimeIdParam.ParameterName = "@DateTime";
-                    datetimeIdParam.Value = timeReceived;
-                    cmd.Parameters.Add(datetimeIdParam);
-
-                    var clientIdParam = cmd.CreateParameter();
-                    clientIdParam.ParameterName = "@ClientId";
-                    clientIdParam.Value = clientId;
-                    cmd.Parameters.Add(clientIdParam);
-
-                    var messageParam = cmd.CreateParameter();
-                    messageParam.ParameterName = "@Message";
-                    var ms = message as MemoryStream;
-                    if (ms == null)
-                        messageParam.Value = message;
-                    else
-                        messageParam.Value = ms.GetBuffer();
-                    cmd.Parameters.Add(messageParam);
-
-                    var headersParam = cmd.CreateParameter();
-                    headersParam.ParameterName = "@Headers";
-                    headersParam.Value = stream.GetBuffer();
-                    cmd.Parameters.Add(headersParam);
-
+                    cmd.Parameters.AddWithValue("DateTime", timeReceived);
+                    cmd.Parameters.AddWithValue("ClientId", clientId);
+                    cmd.Parameters.AddWithValue("Message", message);
+                    cmd.Parameters.AddWithValue("Headers", stream);
                     results = cmd.ExecuteNonQuery();
 
                     tx.Commit();
@@ -75,15 +56,7 @@
                     cmd.CommandText =
                         "UPDATE Messages SET Status=1 WHERE (Status=0) AND (ClientId=@ClientId); SELECT Message, Headers FROM Messages WHERE (ClientId = @ClientId) AND (@@ROWCOUNT = 1)";
 
-                    var clientIdParam = cmd.CreateParameter();
-                    clientIdParam.ParameterName = "@ClientId";
-                    clientIdParam.Value = clientId;
-                    cmd.Parameters.Add(clientIdParam);
-
-                    var statusParam = cmd.CreateParameter();
-                    statusParam.ParameterName = "@Status";
-                    statusParam.Value = 0;
-                    cmd.Parameters.Add(statusParam);
+                    cmd.Parameters.AddWithValue("ClientId", clientId);
 
                     var ackOk = false;
 
@@ -110,7 +83,34 @@
 
         public void UpdateHeader(string clientId, string headerKey, string newValue)
         {
-            throw new NotImplementedException();
+            using (var cn = new SqlConnection(ConnectionString))
+            using (var writeStream = new MemoryStream())
+            {
+                cn.Open();
+                using (var tx = cn.BeginTransaction())
+                {
+                    var read = cn.CreateCommand();
+                    read.CommandText = "SELECT Headers FROM Messages WHERE ClientId = @ClientId";
+                    read.Parameters.AddWithValue("ClientId", clientId);
+
+                    IDictionary<string, string> o = null;
+                    using (var reader = read.ExecuteReader())
+                        if (reader.Read())
+                            using (var readStream = new MemoryStream((byte[])reader.GetValue(0)))
+                                o = serializer.Deserialize(readStream) as IDictionary<string, string>;
+
+                    o[headerKey] = newValue;
+                    serializer.Serialize(writeStream, o);
+                    writeStream.Position = 0;
+
+                    var write = cn.CreateCommand();
+                    write.CommandText = "UPDATE Messages SET Headers=@Headers WHERE ClientId=@ClientId";
+                    write.Parameters.AddWithValue("ClientId", clientId);
+                    write.Parameters.AddWithValue("Headers", writeStream);
+                    var result = write.ExecuteNonQuery();
+                    tx.Commit();
+                }
+            }
         }
 
         public int DeleteDeliveredMessages(DateTime until)
@@ -126,10 +126,7 @@
                     cmd.CommandText =
                         "DELETE FROM Messages WHERE (DATEDIFF(second, @DateTime, DateTime) < 0) AND (STATUS=1)";
 
-                    var dateParam = cmd.CreateParameter();
-                    dateParam.ParameterName = "@DateTime";
-                    dateParam.Value = until;
-                    cmd.Parameters.Add(dateParam);
+                    cmd.Parameters.AddWithValue("DateTime", until);
 
                     result = cmd.ExecuteNonQuery();
 
