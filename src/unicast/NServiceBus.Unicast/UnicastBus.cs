efb4ec7c3c5a90463579cdd3fb7ab6be3eb75b89
Ramon Smits <ramon@ramonsmits.com>
2012-10-19 22:13:40 +0200

    Method DispatchMessageToHandlersBasedOnType catch called GetInnermostException and passed its result to the TransportMessageHandlingFailedException resulting in possible information loss as the cause for the exception. Unknown why GetInnermostException is used at all in this method.

--- src/unicast/NServiceBus.Unicast/UnicastBus.cs
+++ src/unicast/NServiceBus.Unicast/UnicastBus.cs
@@ -1195,10 +1195,8 @@ namespace NServiceBus.Unicast
                 }
                 catch (Exception e)
                 {
-                    var innerEx = GetInnermostException(e);
-                    Log.Warn(handlerType.Name + " failed handling message.", innerEx);
-
-                    throw new TransportMessageHandlingFailedException(innerEx);
+                    Log.Warn(handlerType.Name + " failed handling message.", GetInnermostException(e));
+                    throw new TransportMessageHandlingFailedException(e);
                 }
             }
             return invokedHandlers;
