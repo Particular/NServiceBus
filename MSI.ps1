4c5088354daae49031a25e1a121fca5bad171a3c
John Simons <john.simons@nservicebus.com>
2012-10-23 17:07:12 +1100

    Signing MSI

--- MSI.ps1
+++ MSI.ps1
@@ -3,6 +3,7 @@ properties {
 	$PatchVersion = "0"
 	$BuildNumber = "0"
 	$VsixFilePath = if($env:VSIX_PATH -ne $null) { $env:VSIX_PATH } else { "C:\Projects" }
+	$SignFile = if($env:SIGN_CER_PATH -ne $null) { $env:SIGN_CER_PATH } else { "" }
 }
 
 $baseDir = Split-Path (Resolve-Path $MyInvocation.MyCommand.Path)
@@ -12,7 +13,7 @@ $buildWixPath = "$baseDir\build\wix\"
 
 include $toolsDir\psake\buildutils.ps1
 
-task default -depends Build
+task default -depends Build, Sign
 
 task Clean { 
 	if ( -Not (Test-Path $packageOutPutDir))
@@ -25,17 +26,25 @@ task Clean {
 
 task Init {
 	
+		$sdkInstallRoot = Get-RegistryValue "HKLM:\SOFTWARE\Microsoft\Microsoft SDKs\Windows\v7.0A" "InstallationFolder"
 		$netfxInstallroot = "" 
-		$netfxInstallroot =	Get-RegistryValue 'HKLM:\SOFTWARE\Microsoft\.NETFramework\' 'InstallRoot' 
+		$netfxInstallroot =	Get-RegistryValue "HKLM:\SOFTWARE\Microsoft\.NETFramework\" "InstallRoot" 
 			
 		$netfxCurrent = $netfxInstallroot + "v4.0.30319"
 			
 		$script:msBuild = $netfxCurrent + "\msbuild.exe"
-			
+		$script:signTool = $sdkInstallRoot + "Bin\signtool.exe"
+
 		echo ".Net 4.0 build requested - $script:msBuild" 
 }
 
 task Build -depends Clean, Init {
 	exec { &$script:msBuild $baseDir\src\wix\WixSolution.sln /t:"Clean,Build" /p:OutDir="$buildWixPath" /p:Configuration=Release /p:ProductVersion="$ProductVersion.$PatchVersion.$BuildNumber" /p:VsixPath="$VsixFilePath" }
 	copy $buildWixPath*.msi $packageOutPutDir\
+}
+
+task Sign -depends Init {
+	if($SignFile -ne "") {
+		exec { &$script:signTool sign /f "$SignFile" /p "$env:SIGN_CER_PASSWORD" /d "NServiceBus Installer" /du "http://www.nservicebus.com" /q  $packageOutPutDir\*.msi }
+	}
 }
