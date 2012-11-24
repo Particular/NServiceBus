fd86990bf2b474af84a76d9bca267acd22cf8bb8
Yves Goeleven <yves@goeleven.com>
2012-11-24 14:22:30 +0100

    performance counter warning now logged as info

--- src/NServiceBus.Core/Unicast/Transport/Transactional/Monitoring/ThroughputPerformanceCounter.cs
+++ src/NServiceBus.Core/Unicast/Transport/Transactional/Monitoring/ThroughputPerformanceCounter.cs
@@ -49,7 +49,7 @@ namespace NServiceBus.Unicast.Transport.Transactional.Monitoring
             }
             catch (Exception)
             {
-                Logger.WarnFormat("NServiceBus performance counter for CurrentThroughput not set up correctly, no statistics will be emitted for the {0} queue. Execute the Install-PerformanceCounters powershell command to create the counter", receiveAddress.Queue);
+                Logger.InfoFormat("NServiceBus performance counter for CurrentThroughput not set up correctly, no statistics will be emitted for the {0} queue. Execute the Install-PerformanceCounters powershell command to create the counter", receiveAddress.Queue);
                 return false;
             }
             Logger.InfoFormat("Throughput counter initialized for transport: {0}", receiveAddress);
