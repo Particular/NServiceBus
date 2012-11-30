58a9ed58d660863bfe95cf56a311fb565db50350
John Simons <john.simons@nservicebus.com>
2012-11-30 16:38:01 +1100

    Marked INameThisEndpoint as obsolete, relates to #277
    Updated Fody to latest

--- src/hosting/NServiceBus.Hosting.Windows/INameThisEndpoint.cs
+++ src/hosting/NServiceBus.Hosting.Windows/INameThisEndpoint.cs
@@ -3,6 +3,7 @@ namespace NServiceBus
     /// <summary>
     /// Implement this interface in the endpoint configuration class if you want to give this endpoint your own name
     /// </summary>
+    [ObsoleteEx(Replacement = "Configure.DefineEndpointName()", TreatAsErrorFromVersion = "3.4", RemoveInVersion = "4.0")]    
     public interface INameThisEndpoint
     {
         /// <summary>
