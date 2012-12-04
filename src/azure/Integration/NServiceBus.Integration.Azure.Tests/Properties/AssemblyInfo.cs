174dd72bc7dfae0c5ab2b3b04e144cae710964e9
andreasohlund <andreasohlund2@gmail.com>
2012-12-04 12:59:19 +0100

    Regenerating property files with clscompliant true so that we can catch build errors earlier

--- src/azure/Integration/NServiceBus.Integration.Azure.Tests/Properties/AssemblyInfo.cs
+++ src/azure/Integration/NServiceBus.Integration.Azure.Tests/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("NServiceBus.Integration.Azure.Tests")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("3.4.0.0")]
+[assembly: AssemblyFileVersion("3.4.0.0")]
 [assembly: AssemblyCopyright("Copyright (C) NServiceBus 2010-2012")]
 [assembly: AssemblyProduct("NServiceBus")]
 [assembly: AssemblyCompany("NServiceBus")]
 [assembly: AssemblyConfiguration("release")]
-[assembly: AssemblyInformationalVersion("4.0.0-alpha0")]
+[assembly: AssemblyInformationalVersion("3.4.0-alpha0")]
 [assembly: ComVisible(false)]		
 [assembly: CLSCompliantAttribute(true)]
 
--- src/nhibernate/NServiceBus.NHibernate/Properties/AssemblyInfo.cs
+++ src/nhibernate/NServiceBus.NHibernate/Properties/AssemblyInfo.cs
@@ -1,36 +1,18 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
+using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("NServiceBus.NHibernate")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyVersion("3.4.0.0")]
+[assembly: AssemblyFileVersion("3.4.0.0")]
+[assembly: AssemblyCopyright("Copyright (C) NServiceBus 2010-2012")]
 [assembly: AssemblyProduct("NServiceBus.NHibernate")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("NServiceBus")]
+[assembly: AssemblyConfiguration("release")]
+[assembly: AssemblyInformationalVersion("3.4.0-alpha0")]
+[assembly: ComVisible(false)]		
+[assembly: CLSCompliantAttribute(true)]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aaf5cf08-0622-43ec-ab20-932a1fa75e0c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
