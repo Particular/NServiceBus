4d31a075b7715680401ef84a9846fe6721926bf8
andreasohlund <andreasohlund2@gmail.com>
2012-12-04 13:23:46 +0100

    Supressed the logging in the AMQ sample to avoid distracting the users

--- Samples/Messaging/Messaging.ActiveMQ/MyServer/EndpointConfig.cs
+++ Samples/Messaging/Messaging.ActiveMQ/MyServer/EndpointConfig.cs
@@ -1,5 +1,6 @@
 ﻿namespace MyServer
 {
+    using System;
     using NServiceBus;
 
     public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization
@@ -10,6 +11,19 @@
                      .DefaultBuilder()
                      .XmlSerializer(dontWrapSingleMessages: true)
                      .ActiveMqTransport();
+        }
+    }
+
+    public class MyClass:IWantToRunWhenBusStartsAndStops
+    {
+        public void Start()
+        {
+            Console.Out.WriteLine("The MyServer endpoint is now started and ready to accept messages");
+        }
+
+        public void Stop()
+        {
+            
         }
     }
 }
