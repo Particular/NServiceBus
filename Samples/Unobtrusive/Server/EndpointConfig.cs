cb4537aa8811dd0c1cf8dc189b9c6f40118919e2
andreasohlund <andreasohlund2@gmail.com>
2012-12-06 08:45:07 +0100

    Refactored the conventions in the unobtrusive sample to be in a single location

--- Samples/Unobtrusive/Server/EndpointConfig.cs
+++ Samples/Unobtrusive/Server/EndpointConfig.cs
@@ -1,6 +1,5 @@
 ﻿namespace Server
 {
-    using System;
     using NServiceBus;
 
     public class EndpointConfig: IConfigureThisEndpoint, AsA_Publisher, IWantCustomInitialization
@@ -9,17 +8,7 @@
         {
             Configure.With()
                 .CastleWindsorBuilder()
-                .FileShareDataBus(@"..\..\..\DataBusShare\")
-                .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith("Commands"))
-                .DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith("Events"))
-                .DefiningMessagesAs(t => t.Namespace == "Messages")
-                .DefiningEncryptedPropertiesAs(p => p.Name.StartsWith("Encrypted"))
-                .DefiningDataBusPropertiesAs(p => p.Name.EndsWith("DataBus"))
-                .DefiningExpressMessagesAs(t => t.Name.EndsWith("Express"))
-                .DefiningTimeToBeReceivedAs(t => t.Name.EndsWith("Expires")
-                    ? TimeSpan.FromSeconds(30)
-                    : TimeSpan.MaxValue
-                    );
+                .FileShareDataBus(@"..\..\..\DataBusShare\");
         }
     }
 
