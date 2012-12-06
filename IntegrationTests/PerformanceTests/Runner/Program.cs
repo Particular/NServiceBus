b9f3b622605c0b0041c864b6339d6822eb9aca2c
andreasohlund <andreasohlund2@gmail.com>
2012-12-06 16:55:06 +0100

    Adding a suite of performance tests

---
+++ IntegrationTests/PerformanceTests/Runner/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Runner
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Config;
+    using NServiceBus.Config.ConfigurationSource;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfThreads = int.Parse(args[0]);
+
+            TransportConfigOverride.MaxDegreeOfParallelism = numberOfThreads;
+
+            var numberOfMessages = int.Parse(args[1]);
+
+
+            var config = Configure.With()
+                .DefineEndpointName("PerformanceTest")
+                     .DefaultBuilder();
+
+
+            switch (args[2].ToLower())
+            {
+                case "xml":
+                    config.XmlSerializer();
+                    break;
+                    
+                case "json":
+                    config.JsonSerializer();
+                    break;
+                default:
+                    throw new InvalidOperationException("Illegal seralization format " + args[2]);
+
+            }
+    
+            
+            var startableBus = config
+                     .MsmqTransport()
+                     .InMemoryFaultManagement()
+                     .UnicastBus()
+                     .CreateBus();
+
+            Configure.Instance.ForInstallationOn<NServiceBus.Installation.Environments.Windows>().Install();
+
+            var bus = Configure.Instance.Builder.Build<IBus>();
+
+            Parallel.For(0, 10, (x, y) =>
+                {
+                    for (int i = 0; i < numberOfMessages/10; i++)
+                    {
+                        bus.Send("PerformanceTest", new TestMessage());
+                    }
+
+                });
+
+
+            
+            startableBus.Start();
+            
+            while(Interlocked.Read(ref TestMessageHandler.NumberOfMessages)< numberOfMessages)
+                Thread.Sleep(1000);
+
+            var durationSeconds = (TestMessageHandler.Last - TestMessageHandler.First.Value).TotalSeconds;
+            Console.Out.WriteLine("Threads: {0},NumMessages: {1},Serialization: {2}, Throughput: {3} msg/s", numberOfThreads, numberOfMessages, args[2], Convert.ToDouble(numberOfMessages) / durationSeconds);
+
+            Environment.Exit(0);
+        }
+    }
+
+    class TransportConfigOverride : IProvideConfiguration<TransportConfig>
+    {
+        public static int MaxDegreeOfParallelism;
+        public TransportConfig GetConfiguration()
+        {
+            return new TransportConfig
+                {
+                    MaxDegreeOfParallelism = MaxDegreeOfParallelism,
+                    MaxRetries = 5,
+                    MaxMessageThroughputPerSecond = 0
+
+                };
+        }
+    }
+
+
+
+    class TestMessageHandler:IHandleMessages<TestMessage>
+    {
+        public static DateTime? First;
+        public static DateTime Last;
+        public static Int64 NumberOfMessages;
+        
+
+        public void Handle(TestMessage message)
+        {
+            if (!First.HasValue)
+            {
+                First = DateTime.Now;
+            }
+            Interlocked.Increment(ref NumberOfMessages);
+                
+            Last = DateTime.Now;
+        }
+    }
+
+    public class TestMessage:IMessage
+    {
+        
+    }
+}
---
+++ IntegrationTests/PerformanceTests/Runner/TestSuite.ps1
@@ -0,0 +1,7 @@
+﻿.\bin\debug\Runner.exe 1 10000 Xml
+.\bin\debug\Runner.exe 10 10000 Xml
+.\bin\debug\Runner.exe 15 10000 Xml
+.\bin\debug\Runner.exe 30 10000 Xml
+.\bin\debug\Runner.exe 60 10000 Xml
+
+.\bin\debug\Runner.exe 30 10000 Json
