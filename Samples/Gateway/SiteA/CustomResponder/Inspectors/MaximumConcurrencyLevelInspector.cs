efbcd39b1b6b0abd2771d9068acc7d4818433b19
John Simons <john.simons@nservicebus.com>
2012-12-12 16:11:32 +1100

    Updated Gateway to use new MaximumConcurrencyLevel property.

---
+++ Samples/Gateway/SiteA/CustomResponder/Inspectors/MaximumConcurrencyLevelInspector.cs
@@ -0,0 +1,19 @@
+﻿namespace SiteA.CustomResponder.Inspectors
+{
+    using NServiceBus.Unicast.Transport;
+
+    public class MaximumConcurrencyLevelInspector : IEndpointInspector
+    {
+        private readonly ITransport transport;
+
+        public MaximumConcurrencyLevelInspector(ITransport transport)
+        {
+            this.transport = transport;
+        }
+
+        public string GetStatusAsHtml()
+        {
+            return string.Format("Maximum concurrency level is: " + transport.MaximumConcurrencyLevel);
+        }
+    }
+}
