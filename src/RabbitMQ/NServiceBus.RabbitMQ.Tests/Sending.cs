ca97640dc816c273d63312744035c7b8991b30c7
andreasohlund <andreasohlund2@gmail.com>
2012-12-13 08:42:01 +0100

    Basic properties are now sent with the Rabbit message

--- src/RabbitMQ/NServiceBus.RabbitMQ.Tests/Sending.cs
+++ src/RabbitMQ/NServiceBus.RabbitMQ.Tests/Sending.cs
@@ -1,46 +1,58 @@
 ﻿namespace NServiceBus.RabbitMQ.Tests
 {
     using NUnit.Framework;
+    using Unicast.Queuing;
     using global::RabbitMQ.Client;
 
 
-    [TestFixture,Explicit("Integration tests")]
+    [TestFixture, Explicit("Integration tests")]
     public class Sending
     {
-        [SetUp]
-        public void SetUp()
-        {
-        }
-
         [Test]
         public void Should_send_a_message_to_the_given_queue()
         {
-            var factory = new ConnectionFactory {HostName = "localhost"};
+            MakeSureQueueExists(connection);
 
-
-            using (var connection = factory.CreateConnection())
-            {
-
-                MakeSureQueueExists(connection);
-
-                var sender = new RabbitMqMessageSender
+            sender.Send(new TransportMessage
                 {
-                    Connection = connection
-                };
-
-                sender.Send(new TransportMessage
-                    {
-                        Body =  System.Text.Encoding.UTF8.GetBytes("<TestMessage/>")
-                    }, Address.Parse("TestEndpoint@localhost"));
-            }
-
-
+                    Body = System.Text.Encoding.UTF8.GetBytes("<TestMessage/>")
+                }, Address.Parse("TestEndpoint@localhost"));
         }
+
+
+        [Test]
+        public void Should_throw_when_sending_to_a_nonexisting_queue()
+        {
+            Assert.Throws<QueueNotFoundException>(() =>
+                 sender.Send(new TransportMessage(), Address.Parse("NonExistingQueue@localhost")));
+        }
+
 
         static void MakeSureQueueExists(IConnection connection)
         {
             using (var channel = connection.CreateModel())
-                channel.QueueDeclare("testendpoint", false, false, false, null);
+                channel.QueueDeclare("testendpoint", true, false, false, null);
         }
+
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            factory = new ConnectionFactory { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            sender = new RabbitMqMessageSender { Connection = connection };
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            connection.Close();
+            connection.Dispose();
+        }
+        ConnectionFactory factory;
+        IConnection connection;
+        RabbitMqMessageSender sender;
+
     }
 }
--- src/RabbitMQ/NServiceBus.RabbitMQ/RabbitMqMessageSender.cs
+++ src/RabbitMQ/NServiceBus.RabbitMQ/RabbitMqMessageSender.cs
@@ -2,17 +2,38 @@
 {
     using Unicast.Queuing;
     using global::RabbitMQ.Client;
+    using global::RabbitMQ.Client.Impl;
 
-    public class RabbitMqMessageSender:ISendMessages
+    public class RabbitMqMessageSender : ISendMessages
     {
         public IConnection Connection { get; set; }
-     
+
         public void Send(TransportMessage message, Address address)
         {
-            using (IModel channel = Connection.CreateModel())
+
+            using (var channel = Connection.CreateModel())
             {
-                channel.BasicPublish("", address.Queue, null, message.Body);
+                var properties = message.RabbitMqProperties(channel);
+
+                channel.BasicPublish("", address.Queue, true, false, properties, message.Body);
             }
+        }
+
+      
+    }
+
+    public static class RabbitMQTransportMessageExtensions
+    {
+        public static IBasicProperties RabbitMqProperties(this TransportMessage message,IModel channel)
+        {
+            
+            var properties = channel.CreateBasicProperties();
+
+            //props.ContentType = "text/plain";
+            properties.ReplyTo = message.ReplyToAddress.Queue;
+            properties.DeliveryMode = 2;
+
+            return properties;
         }
     }
 }
