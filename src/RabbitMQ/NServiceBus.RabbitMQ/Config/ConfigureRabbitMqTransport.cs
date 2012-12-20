6e7bbf3c17369f5dec00c801a6b20e124c3cfb29
andreasohlund <andreasohlund2@gmail.com>
2012-12-20 10:03:41 +0100

    Added and option to pass the connection factory when configuring rabbit mq

--- src/RabbitMQ/NServiceBus.RabbitMQ/Config/ConfigureRabbitMqTransport.cs
+++ src/RabbitMQ/NServiceBus.RabbitMQ/Config/ConfigureRabbitMqTransport.cs
@@ -4,32 +4,44 @@
     using RabbitMQ;
     using RabbitMQ.Config;
     using Unicast.Queuing.Installers;
+    using global::RabbitMQ.Client;
 
     public static class ConfigureRabbitMqTransport
     {
-       
-         /// <summary>
-         /// Configures Rabbit as the transport. Settings are read from the configuration
-         /// </summary>
-         /// <param name="config"></param>
-         /// <returns></returns>
-         public static Configure RabbitMqTransport(this Configure config)
-         {
-             var connectionString = ConfigurationManager.ConnectionStrings["NServiceBus.RabbitMQ"].ConnectionString;
 
-             return RabbitMqTransport(config, connectionString);
-         }
         /// <summary>
         /// Configures Rabbit as the transport. Settings are read from the configuration
         /// </summary>
         /// <param name="config"></param>
         /// <returns></returns>
-         public static Configure RabbitMqTransport(this Configure config, string connectionString)
+        public static Configure RabbitMqTransport(this Configure config)
         {
-           var builder = new RabbitMqConnectionStringBuilder(connectionString);
+            var connectionString = ConfigurationManager.ConnectionStrings["NServiceBus/Transports/RabbitMQ"].ConnectionString;
+
+            return RabbitMqTransport(config, connectionString);
+        }
+        /// <summary>
+        /// Configures Rabbit as the transport. Settings are read from the configuration
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        public static Configure RabbitMqTransport(this Configure config, string connectionString)
+        {
+            var builder = new RabbitMqConnectionStringBuilder(connectionString);
 
             var connectionFactory = builder.BuildConnectionFactory();
 
+            return RabbitMqTransport(config, connectionFactory);
+        }
+
+        /// <summary>
+        /// Configures the transport and connecting using the given connection factory
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="connectionFactory"></param>
+        /// <returns></returns>
+        public static Configure RabbitMqTransport(this Configure config, ConnectionFactory connectionFactory)
+        {
             config.Configurer.ConfigureComponent(connectionFactory.CreateConnection, DependencyLifecycle.SingleInstance);
             config.Configurer.ConfigureComponent<RabbitMqDequeueStrategy>(DependencyLifecycle.InstancePerCall);
             config.Configurer.ConfigureComponent<RabbitMqMessageSender>(DependencyLifecycle.InstancePerCall);
@@ -40,6 +52,6 @@
 
             return config;
         }
- 
+
     }
 }
