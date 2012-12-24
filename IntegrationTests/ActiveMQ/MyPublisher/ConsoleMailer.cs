03c99aa5206fbf7086efcf13fbc0f24c2c0d31fc
danielmarbach <daniel.marbach@openplace.net>
2012-12-24 15:20:38 +0100

    Notifications working with ActiveMq. Using new pluggable mail sender

---
+++ IntegrationTests/ActiveMQ/MyPublisher/ConsoleMailer.cs
@@ -0,0 +1,15 @@
+﻿namespace MyPublisher
+{
+    using System;
+    using System.Net.Mail;
+
+    using NServiceBus.Notifications;
+
+    public class ConsoleMailer : ISendMail
+    {
+        public void Send(MailMessage message)
+        {
+            Console.WriteLine("New email from {0} to {1}.", message.From, message.To);
+        }
+    }
+}
---
+++ IntegrationTests/ActiveMQ/MyPublisher/CustomConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace MyPublisher
+{
+    using NServiceBus;
+
+    class CustomConfiguration : IWantCustomInitialization
+    {
+        public void Init()
+        {
+            Configure.Instance.Configurer.ConfigureComponent<ConsoleMailer>(DependencyLifecycle.SingleInstance);
+        }
+    }
+}
