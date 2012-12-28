abcf65b8fa6483959ba9300f544fe044af86bde9
John Simons <john.simons@nservicebus.com>
2012-12-28 16:42:19 +1100

    RunTimeoutManager removed when it should be marked as obsolete, closes #811

--- src/NServiceBus.Core/ConfigureTimeoutManager.cs
+++ src/NServiceBus.Core/ConfigureTimeoutManager.cs
@@ -38,17 +38,6 @@ namespace NServiceBus
         }
 
 
-        /// <summary>
-        /// As Timeout manager is turned on by default for server roles, use DisableTimeoutManager method to turn off Timeout manager
-        /// </summary>
-        /// <param name="config"></param>
-        /// <returns></returns>
-        [ObsoleteEx(Message = "As Timeout manager is a core functionality of NServiceBus it will be impossible to disable it beginning version 4.0.", TreatAsErrorFromVersion = "4.0", RemoveInVersion = "5.0")]        
-        public static Configure DisableTimeoutManager(this Configure config)
-        {
-            return config;
-        }
-
         public static Address TimeoutManagerAddress { get; set; }
 
         private static void SetupTimeoutManager(Configure config)
@@ -57,6 +46,51 @@ namespace NServiceBus
 
             config.Configurer.ConfigureComponent<TimeoutPersisterReceiver>(DependencyLifecycle.SingleInstance);
             config.Configurer.ConfigureComponent<DefaultTimeoutManager>(DependencyLifecycle.SingleInstance);
+        }
+
+        /// <summary>
+        /// As Timeout manager is turned on by default for server roles, use DisableTimeoutManager method to turn off Timeout manager
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        [ObsoleteEx(Message = "As Timeout Manager is part of the core NServiceBus functionality, it is impossible to disable it.", TreatAsErrorFromVersion = "4.0", RemoveInVersion = "5.0")]        
+        public static Configure DisableTimeoutManager(this Configure config)
+        {
+            return config;
+        }
+
+        [ObsoleteEx(Message = "As Timeout Manager is part of the core NServiceBus functionality, this method will always return true.", TreatAsErrorFromVersion = "4.0", RemoveInVersion = "5.0")]        
+        public static bool IsTimeoutManagerEnabled(this Configure config)
+        {
+            return true;
+        }
+
+        [ObsoleteEx(Message = "As Timeout Manager is part of the core NServiceBus functionality, it is not required to call this method any longer.", TreatAsErrorFromVersion = "4.0", RemoveInVersion = "5.0")]
+        public static Configure RunTimeoutManager(this Configure config)
+        {
+            return config;
+        }
+
+        /// <summary>
+        /// Sets the default persistence to InMemory.
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        [ObsoleteEx(Replacement = "UseInMemoryTimeoutPersister()", TreatAsErrorFromVersion = "4.0", RemoveInVersion = "5.0")]
+        public static Configure RunTimeoutManagerWithInMemoryPersistence(this Configure config)
+        {
+            return config;
+        }
+
+        /// <summary>
+        /// Sets the default persistence to InMemory.
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        [ObsoleteEx(Replacement = "UseInMemoryTimeoutPersister()", TreatAsErrorFromVersion = "4.0", RemoveInVersion = "5.0")]
+        public static Configure DefaultToInMemoryTimeoutPersistence(this Configure config)
+        {
+            return config;
         }
     }
 }
