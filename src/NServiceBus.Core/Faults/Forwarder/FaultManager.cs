6bcd8ba984f461b04ae29f129b9dafd383dd9aac
John Simons <john.simons@nservicebus.com>
2013-01-04 08:09:07 +1100

    Removed unnecessary exception headers.

--- src/NServiceBus.Core/Faults/Forwarder/FaultManager.cs
+++ src/NServiceBus.Core/Faults/Forwarder/FaultManager.cs
@@ -12,12 +12,12 @@ namespace NServiceBus.Faults.Forwarder
     {
         void IManageMessageFailures.SerializationFailedForMessage(TransportMessage message, Exception e)
         {
-            SendFailureMessage(message, e, "SerializationFailed");
+            SendFailureMessage(message, e, true);
         }
 
         void IManageMessageFailures.ProcessingAlwaysFailsForMessage(TransportMessage message, Exception e)
         {
-            SendFailureMessage(message, e, "ProcessingFailed");
+            SendFailureMessage(message, e);
         }
 
         void IManageMessageFailures.Init(Address address)
@@ -25,9 +25,10 @@ namespace NServiceBus.Faults.Forwarder
             localAddress = address;
         }
 
-        void SendFailureMessage(TransportMessage message, Exception e, string reason)
+        void SendFailureMessage(TransportMessage message, Exception e, bool serializationException = false)
         {
-            SetExceptionHeaders(message, e, reason);
+            SetExceptionHeaders(message, e);
+
             try
             {
                 var destinationQ = RetriesErrorQueue ?? ErrorQueue;
@@ -36,7 +37,7 @@ namespace NServiceBus.Faults.Forwarder
                 // resolution problem in the container
                 var sender = Configure.Instance.Builder.Build<ISendMessages>();
 
-                if (MessageWasSentFromSLR(message) || reason == "SerializationFailed")
+                if (serializationException || MessageWasSentFromSLR(message))
                 {
                     sender.Send(message, ErrorQueue);
                     return;
@@ -76,9 +77,8 @@ namespace NServiceBus.Faults.Forwarder
             return TransportMessageHelpers.GetReplyToAddress(message) == RetriesErrorQueue;
         }
 
-        void SetExceptionHeaders(TransportMessage message, Exception e, string reason)
+        void SetExceptionHeaders(TransportMessage message, Exception e)
         {
-            message.Headers["NServiceBus.ExceptionInfo.Reason"] = reason;
             message.Headers["NServiceBus.ExceptionInfo.ExceptionType"] = e.GetType().FullName;
 
             if (e.InnerException != null)
@@ -86,7 +86,6 @@ namespace NServiceBus.Faults.Forwarder
                 message.Headers["NServiceBus.ExceptionInfo.InnerExceptionType"] = e.InnerException.GetType().FullName;
             }
 
-            message.Headers["NServiceBus.ExceptionInfo.HelpLink"] = e.HelpLink;
             message.Headers["NServiceBus.ExceptionInfo.Message"] = e.Message;
             message.Headers["NServiceBus.ExceptionInfo.Source"] = e.Source;
             message.Headers["NServiceBus.ExceptionInfo.StackTrace"] = e.ToString();
