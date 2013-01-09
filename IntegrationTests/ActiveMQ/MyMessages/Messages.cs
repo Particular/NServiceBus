816475b818b7691cb2e4b9d7f9dd65a95674fce3
danielmarbach <daniel.marbach@openplace.net>
2013-01-09 16:56:53 +0100

    Databus working with ActiveMQ

--- IntegrationTests/ActiveMQ/MyMessages/Messages.cs
+++ IntegrationTests/ActiveMQ/MyMessages/Messages.cs
@@ -64,6 +64,16 @@ namespace MyMessages.SubscriberNMS
     }
 }
 
+namespace MyMessages.DataBus
+{
+    [TimeToBeReceived("00:01:00")]//the data bus is allowed to clean up transmitted properties older than the TTBR
+    public class MessageWithLargePayload : ICommand
+    {
+        public string SomeProperty { get; set; }
+        public DataBusProperty<byte[]> LargeBlob { get; set; }
+    }
+}
+
 namespace MyMessages.Publisher
 {
     public enum ResponseCode
