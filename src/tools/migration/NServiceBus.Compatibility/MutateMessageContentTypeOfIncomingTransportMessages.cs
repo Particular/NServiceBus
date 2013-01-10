2299773f823318e58c986f679e63b8287f837c3c
danielmarbach <daniel.marbach@openplace.net>
2013-01-10 12:28:15 +0100

    Mutate incoming transport messages with content type header if not available.

---
+++ src/tools/migration/NServiceBus.Compatibility/MutateMessageContentTypeOfIncomingTransportMessages.cs
@@ -0,0 +1,27 @@
+﻿namespace NServiceBus.Compatibility
+{
+    using NServiceBus.MessageMutator;
+    using NServiceBus.Serialization;
+
+    public class MutateMessageContentTypeOfIncomingTransportMessages : IMutateIncomingTransportMessages, INeedInitialization
+    {
+        public IMessageSerializer Serializer { get; set; }
+
+        /// <summary>
+        /// Ensure that the content type which is introduced in V4.0.0 and later versions is present in the header.
+        /// </summary>
+        /// <param name="transportMessage">Transport Message to mutate.</param>
+        public void MutateIncoming(TransportMessage transportMessage)
+        {
+            if (!transportMessage.Headers.ContainsKey(Headers.ContentType))
+            {
+                transportMessage.Headers[Headers.ContentType] = this.Serializer.ContentType;
+            }
+        }
+
+        public void Init()
+        {
+            Configure.Instance.Configurer.ConfigureComponent<MutateMessageContentTypeOfIncomingTransportMessages>(DependencyLifecycle.InstancePerCall);
+        }
+    }
+}
