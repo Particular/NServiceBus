a4db52cd7c219798568c1a6c3b4d2747a58f1627
John Simons <john.simons@nservicebus.com>
2013-01-21 14:10:51 +1100

    Added scripts to sync samples

---
+++ tools/scripts/SyncSamples/SynchronizeSamples.ps1
@@ -0,0 +1,63 @@
+﻿function Sync (
+    [Parameter(Position=0,Mandatory=1)]
+    [string]$transport = "MSMQ",
+    [Parameter(Position=1,Mandatory=1)]
+    [string]$connectionString = "MSMQ"
+)
+{
+    robocopy Messaging.SqlServer Messaging.$transport /MIR /FFT /Z /XA:H /W:5
+	Rename-Item Messaging.$transport\Messaging.SqlServer.sln Messaging.$transport.sln
+    
+	(Get-Content Messaging.$transport\MyWebClient\Web.config) | 
+    Foreach-Object {
+        $_ -replace "Data Source=\.\\SQLEXPRESS;Initial Catalog=nservicebus;Integrated Security=True", $connectionString
+    } | 
+    Set-Content Messaging.$transport\MyWebClient\Web.config
+	
+	(Get-Content Messaging.$transport\MySubscriber\App.config) | 
+    Foreach-Object {
+        $_ -replace "Data Source=\.\\SQLEXPRESS;Initial Catalog=nservicebus;Integrated Security=True", $connectionString
+    } | 
+    Set-Content Messaging.$transport\MySubscriber\App.config
+	
+	(Get-Content Messaging.$transport\MyServer\App.config) | 
+    Foreach-Object {
+        $_ -replace "Data Source=\.\\SQLEXPRESS;Initial Catalog=nservicebus;Integrated Security=True", $connectionString
+    } | 
+    Set-Content Messaging.$transport\MyServer\App.config
+	
+	(Get-Content Messaging.$transport\MyRequestResponseEndpoint\App.config) | 
+    Foreach-Object {
+        $_ -replace "Data Source=\.\\SQLEXPRESS;Initial Catalog=nservicebus;Integrated Security=True", $connectionString
+    } | 
+    Set-Content Messaging.$transport\MyRequestResponseEndpoint\App.config
+    
+    (Get-Content Messaging.$transport\MyWebClient\Global.asax.cs) | 
+    Foreach-Object {
+        $_ -replace "UseTransport<SqlServer>", "UseTransport<$transport>"
+    } | 
+    Set-Content Messaging.$transport\MyWebClient\Global.asax.cs
+    
+    (Get-Content Messaging.$transport\MySubscriber\EndpointConfig.cs) | 
+    Foreach-Object {
+        $_ -replace "SqlServer", $transport
+    } | 
+    Set-Content Messaging.$transport\MySubscriber\EndpointConfig.cs
+    
+    (Get-Content Messaging.$transport\MyServer\EndpointConfig.cs) | 
+    Foreach-Object {
+        $_ -replace "SqlServer", $transport
+    } | 
+    Set-Content Messaging.$transport\MyServer\EndpointConfig.cs
+    
+    (Get-Content Messaging.$transport\MyRequestResponseEndpoint\EndpointConfig.cs) | 
+    Foreach-Object {
+        $_ -replace "SqlServer", $transport
+    } | 
+    Set-Content Messaging.$transport\MyRequestResponseEndpoint\EndpointConfig.cs
+}
+
+
+Sync -transport "RabbitMQ" -connectionString "host=localhost"
+Sync -transport "Msmq" -connectionString "cache=true"
+Sync -transport "ActiveMQ" -connectionString "activemq:tcp://localhost:61616"
---
+++ tools/scripts/SyncSamples/TransformProj.xslt
@@ -0,0 +1,28 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl"
+>
+  <xsl:output method="xml" indent="yes" encoding="utf-8"/>
+  <xsl:variable name="references" select="document('MSMQ.xml')"/>
+
+  <xsl:template name="AddReferences">
+    <xsl:for-each select="$references/ItemGroup/*">
+        <xsl:copy-of select="." />
+    </xsl:for-each>
+  </xsl:template>
+
+  <xsl:template match="@*|node()">
+    <xsl:choose>
+      <xsl:when test="local-name() = 'Reference' and ./@Include='NServiceBus.SqlServer'">
+        <xsl:call-template name="AddReferences"/>
+      </xsl:when>
+      <xsl:otherwise>
+        <xsl:copy>
+          <xsl:apply-templates select="@*|node()"/>
+        </xsl:copy>
+      </xsl:otherwise>
+    </xsl:choose>
+    
+  </xsl:template>
+
+</xsl:stylesheet>
