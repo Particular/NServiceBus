90c154290c2962a5ed0439525464a7468b384f04
andreasohlund <andreasohlund2@gmail.com>
2013-02-01 12:55:05 +0100

    Fixing the idisposable builder test to expose the issues with the AutoFac, Castle and Spring builders

--- src/impl/ObjectBuilder/ObjectBuilder.Tests/When_disposing_the_builder.cs
+++ src/impl/ObjectBuilder/ObjectBuilder.Tests/When_disposing_the_builder.cs
@@ -1,6 +1,10 @@
 namespace ObjectBuilder.Tests
 {
+    using System;
     using NServiceBus;
+    using NServiceBus.ObjectBuilder.Autofac;
+    using NServiceBus.ObjectBuilder.CastleWindsor;
+    using NServiceBus.ObjectBuilder.Spring;
     using NUnit.Framework;
 
     [TestFixture]
@@ -11,6 +15,9 @@ namespace ObjectBuilder.Tests
         {
             ForAllBuilders(builder =>
                 {
+                    SingletonComponent.DisposeCalled = false;
+                    AnotherSingletonComponent.DisposeCalled = false;
+
                     builder.Configure(typeof(SingletonComponent), DependencyLifecycle.SingleInstance);
                     builder.RegisterSingleton(typeof(AnotherSingletonComponent),new AnotherSingletonComponent());
 
@@ -20,7 +27,29 @@ namespace ObjectBuilder.Tests
 
                     Assert.True(SingletonComponent.DisposeCalled, "Dispose should be called on SingletonComponent");
                     Assert.True(AnotherSingletonComponent.DisposeCalled, "Dispose should be called on AnotherSingletonComponent");
-                });
+                }, typeof(AutofacObjectBuilder), typeof(WindsorObjectBuilder), typeof(SpringObjectBuilder));
+
+
+        }
+
+        public class SingletonComponent :  IDisposable
+        {
+            public static bool DisposeCalled;
+
+            public void Dispose()
+            {
+                DisposeCalled = true;
+            }
+        }
+
+        public class AnotherSingletonComponent : IDisposable
+        {
+            public static bool DisposeCalled;
+
+            public void Dispose()
+            {
+                DisposeCalled = true;
+            }
         }
     }
 }
