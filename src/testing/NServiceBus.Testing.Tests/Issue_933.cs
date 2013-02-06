1c1ad7e2a68761d433abcfa910905e6425917ef8
John Simons <john.simons@nservicebus.com>
2013-02-06 15:49:55 +1100

    Testing framework was not setting message headers, closes #933

---
+++ src/testing/NServiceBus.Testing.Tests/Issue_933.cs
@@ -0,0 +1,43 @@
+﻿namespace NServiceBus.Testing.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Issue_933
+    {
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            Test.Initialize();
+        }
+
+        [Test]
+        public void SendMessageWithMultiIncomingHeaders()
+        {
+            var command = new MyCommand();
+
+            Test.Handler<MyCommandHandler>()
+                .SetIncomingHeader("Key1", "Header1")
+                .SetIncomingHeader("Key2", "Header2")
+                .OnMessage(command);
+
+            Assert.AreEqual("Header1", command.Header1);
+            Assert.AreEqual("Header2", command.Header2);
+        }
+
+        public class MyCommand : ICommand
+        {
+            public string Header1 { get; set; }
+            public string Header2 { get; set; }
+        }
+
+        public class MyCommandHandler : IHandleMessages<MyCommand>
+        {
+            public void Handle(MyCommand message)
+            {
+                message.Header1 = message.GetHeader("Key1");
+                message.Header2 = message.GetHeader("Key2");
+            }
+        }
+    }
+}
