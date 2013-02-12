463c83e754700137a4d574738521f286da549e6f
John Simons <john.simons@nservicebus.com>
2013-02-12 18:11:05 +1100

    Add Unique attribute to saga data

--- AcceptanceTests/NServiceBus.AcceptanceTests/Sagas/When_receiving_a_message_that_should_complete_saga.cs
+++ AcceptanceTests/NServiceBus.AcceptanceTests/Sagas/When_receiving_a_message_that_should_complete_saga.cs
@@ -140,6 +140,7 @@
                 public Guid Id { get; set; }
                 public string Originator { get; set; }
                 public string OriginalMessageId { get; set; }
+                [Unique]
                 public Guid SomeId { get; set; }
             }
         }
