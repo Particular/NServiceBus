b34965d187d5c43c3d943bfb39992da65e45dc1e
danielmarbach <daniel.marbach@openplace.net>
2013-02-18 08:24:34 +0100

    Proper disposal of MD5CryptoServiceProvider

--- src/NServiceBus.Core/SagaPersisters/Raven/RavenSagaPersister.cs
+++ src/NServiceBus.Core/SagaPersisters/Raven/RavenSagaPersister.cs
@@ -178,24 +178,30 @@ namespace NServiceBus.SagaPersisters.Raven
             if (uniqueProperty.Value == null)
                 throw new ArgumentNullException("uniqueProperty", string.Format("Property {0} is marked with the [Unique] attribute on {1} but contains a null value. Please make sure that all unique properties are set on your SagaData and/or that you have marked the correct properties as unique.", uniqueProperty.Key, sagaType.Name));
 
-            //use MD5 hash to get a 16-byte hash of the string
-            var provider = new MD5CryptoServiceProvider();
-            var inputBytes = Encoding.Default.GetBytes(uniqueProperty.Value.ToString());
-            var hashBytes = provider.ComputeHash(inputBytes);
-            //generate a guid from the hash:
-            var value = new Guid(hashBytes);
-
-            var id  = string.Format("{0}/{1}/{2}", sagaType.FullName, uniqueProperty.Key, value);
-
-            //raven has a size limit of 255 bytes == 127 unicode chars
-            if(id.Length > 127)
+            // use MD5 hash to get a 16-byte hash of the string
+            using (var provider = new MD5CryptoServiceProvider())
             {
-                //generate a guid from the hash:
-                var key = new Guid(provider.ComputeHash(Encoding.Default.GetBytes(sagaType.FullName + uniqueProperty.Key)));
+                var inputBytes = Encoding.Default.GetBytes(uniqueProperty.Value.ToString());
+                byte[] hashBytes = provider.ComputeHash(inputBytes);
 
-                id = string.Format("MoreThan127/{0}/{1}", key, value);
+                // generate a guid from the hash:
+                var value = new Guid(hashBytes);
+
+                var id = string.Format("{0}/{1}/{2}", sagaType.FullName, uniqueProperty.Key, value);
+
+                // raven has a size limit of 255 bytes == 127 unicode chars
+                if (id.Length > 127)
+                {
+                    // generate a guid from the hash:
+                    var key =
+                        new Guid(
+                            provider.ComputeHash(Encoding.Default.GetBytes(sagaType.FullName + uniqueProperty.Key)));
+
+                    id = string.Format("MoreThan127/{0}/{1}", key, value);
+                }
+
+                return id;
             }
-            return id;
         }
     }
 }
