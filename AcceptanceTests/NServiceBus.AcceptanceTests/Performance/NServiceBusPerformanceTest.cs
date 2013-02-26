2e0bfb0d178e7fcbb4db4a8bc1339d0075d9367b
andreasohlund <andreasohlund2@gmail.com>
2013-02-26 09:41:53 +0100

    Fixed the references for the acceptance tests to match the new names of the transport assemblies.

--- AcceptanceTests/NServiceBus.AcceptanceTests/Performance/NServiceBusPerformanceTest.cs
+++ AcceptanceTests/NServiceBus.AcceptanceTests/Performance/NServiceBusPerformanceTest.cs
@@ -30,7 +30,7 @@
                 file.WriteLine(string.Join(";", summary.RunDescriptor.Key, testCategory,testCase, c.NumberOfTestMessages, messagesPerSecondsProcessed));
             }
 
-            Console.Out.WriteLine("##teamcity[buildStatisticValue key='{0}' value='{1}']", summary.RunDescriptor.Key + "." + testCategory + "." + testCase, messagesPerSecondsProcessed);
+            Console.Out.WriteLine("##teamcity[buildStatisticValue key='{0}' value='{1}']", summary.RunDescriptor.Key + "." + testCategory + "." + testCase, Convert.ToInt16(messagesPerSecondsProcessed));
         }
 
 
