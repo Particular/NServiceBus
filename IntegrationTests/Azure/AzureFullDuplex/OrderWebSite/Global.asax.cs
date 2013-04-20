95a3790c0534242927f7b0f0e4bbff4cfdf5b570
yvesgoeleven <yves@goeleven.com>
2013-04-20 19:34:42 +0200

    diagnostics

--- IntegrationTests/Azure/AzureFullDuplex/OrderWebSite/Global.asax.cs
+++ IntegrationTests/Azure/AzureFullDuplex/OrderWebSite/Global.asax.cs
@@ -55,10 +55,7 @@ namespace OrderWebSite
 
         protected void Application_Error(object sender, EventArgs e)
         {
-            //get reference to the source of the exception chain
-            var ex = Server.GetLastError().GetBaseException();
-
-            LogManager.GetLogger(typeof(Global)).Error(ex.ToString());
+            
         }
 
         protected void Session_End(object sender, EventArgs e)
