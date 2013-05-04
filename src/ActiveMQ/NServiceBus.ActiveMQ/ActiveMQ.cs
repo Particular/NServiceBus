1ca7b8ec3142612972a3d0d05f6493fe95d9bbb5
danielmarbach <daniel.marbach@openplace.net>
2013-05-04 18:29:15 +0200

    Removed virtual property on TransportDefinition

--- src/ActiveMQ/NServiceBus.ActiveMQ/ActiveMQ.cs
+++ src/ActiveMQ/NServiceBus.ActiveMQ/ActiveMQ.cs
@@ -4,7 +4,10 @@
 
     public class ActiveMQ : TransportDefinition
     {
-        public override bool HasNativePubSubSupport { get { return true; } }
-        public override bool HasSupportForCentralizedPubSub { get { return true; } }
+        public ActiveMQ()
+        {
+            HasNativePubSubSupport = true;
+            HasSupportForCentralizedPubSub = true;
+        }
     }
 }
