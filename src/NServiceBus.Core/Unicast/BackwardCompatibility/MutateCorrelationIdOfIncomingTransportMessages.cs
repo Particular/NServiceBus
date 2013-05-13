eb47a3ce1dc1a6f0d991122a6e77261f4a1370b4
danielmarbach <daniel.marbach@openplace.net>
2013-05-13 13:44:04 +0200

    Added incoming and outgoing message mutators for the IdForCorrelation header

---
+++ src/NServiceBus.Core/Unicast/BackwardCompatibility/MutateCorrelationIdOfIncomingTransportMessages.cs
@@ -0,0 +1,24 @@
+﻿namespace NServiceBus.Unicast.BackwardCompatibility
+{
+    using NServiceBus.MessageMutator;
+
+    public class MutateCorrelationIdOfIncomingTransportMessages : IMutateIncomingTransportMessages, INeedInitialization
+    {
+        /// <summary>
+        /// Unsure that the IdForCorrelation header is copied over to the correlation id when set.
+        /// </summary>
+        /// <param name="transportMessage">Transport Message to mutate.</param>
+        public void MutateIncoming(TransportMessage transportMessage)
+        {
+            if (transportMessage.Headers.ContainsKey(Headers.IdForCorrelation))
+            {
+                transportMessage.CorrelationId = transportMessage.Headers[Headers.IdForCorrelation];
+            }
+        }
+
+        public void Init()
+        {
+            Configure.Instance.Configurer.ConfigureComponent<MutateCorrelationIdOfIncomingTransportMessages>(DependencyLifecycle.InstancePerCall);
+        }
+    }
+}
