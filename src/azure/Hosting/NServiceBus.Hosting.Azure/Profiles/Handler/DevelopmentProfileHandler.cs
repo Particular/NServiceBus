658ccf4271ff950e2fca2f891e9635d5e82d6dd9
yvesgoeleven <yves@goeleven.com>
2013-05-21 10:15:59 +0200

    changed check for existing logger before initializing azure logger

--- src/azure/Hosting/NServiceBus.Hosting.Azure/Profiles/Handler/DevelopmentProfileHandler.cs
+++ src/azure/Hosting/NServiceBus.Hosting.Azure/Profiles/Handler/DevelopmentProfileHandler.cs
@@ -4,11 +4,13 @@ using NServiceBus.Logging;
 
 namespace NServiceBus.Hosting.Azure.Profiles.Handlers
 {
+    using Logging.Loggers;
+
     internal class DevelopmentProfileHandler : IHandleProfile<Development>
     {
         void IHandleProfile.ProfileActivated()
         {
-            if (LogManager.LoggerFactory == null)
+            if (!(LogManager.LoggerFactory is NullLoggerFactory))
                 Configure.Instance.AzureDiagnosticsLogger(false, !IsHostedIn.ChildHostProcess());
         }
     }
