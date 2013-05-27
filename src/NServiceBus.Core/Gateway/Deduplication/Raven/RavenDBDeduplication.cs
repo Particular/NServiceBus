2a4894084bc76b6681eef894de59c2549fc1edca
andreasohlund <andreasohlund2@gmail.com>
2013-05-27 22:00:56 +0200

    Fixing a bug where the raven gateway deduplication would wrongly consider a message to be a duplicate

--- src/NServiceBus.Core/Gateway/Deduplication/Raven/RavenDBDeduplication.cs
+++ src/NServiceBus.Core/Gateway/Deduplication/Raven/RavenDBDeduplication.cs
@@ -14,13 +14,11 @@
 
         public bool DeduplicateMessage(string clientId, DateTime timeReceived)
         {
-            using (var session = OpenSession())
+            using (var session = store.OpenSession())
             {
-                var storedMessage = session.Load<GatewayMessage>(EscapeClientId(clientId));
-                if (storedMessage != null)
-                    return false;
-
                 session.Advanced.UseOptimisticConcurrency = true;
+                session.Advanced.AllowNonAuthoritativeInformation = false;
+
                 session.Store(new GatewayMessage { Id = EscapeClientId(clientId), TimeReceived = timeReceived });
 
                 try
@@ -31,22 +29,17 @@
                 {
                     return false;
                 }
+
+                return true;
             }
-            return false;
         }
 
-        private static string EscapeClientId(string clientId)
+        static string EscapeClientId(string clientId)
         {
             return clientId.Replace("\\", "_");
         }
 
-        private IDocumentSession OpenSession()
-        {
-            var session = store.OpenSession();
-            session.Advanced.AllowNonAuthoritativeInformation = false;
-            return session;
-        }
 
-        private readonly IDocumentStore store;
+        readonly IDocumentStore store;
     }
 }
