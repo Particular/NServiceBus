96e37b2a4395afc85163b882f1b7a17d1df49c7e
Chris Bednarski <chris.bednarski@outlook.com>
2013-06-04 20:52:20 +1000

    Added NHibernate deduplication
    updated sample

---
+++ src/nhibernate/NServiceBus.NHibernate/Deduplication/NHibernate/Config/DeduplicationMessage.cs
@@ -0,0 +1,20 @@
+﻿namespace NServiceBus.Deduplication.NHibernate.Config
+{
+    using System;
+
+    /// <summary>
+    /// The Gateway message
+    /// </summary>
+    public class DeduplicationMessage
+    {
+        /// <summary>
+        /// Id of this message.
+        /// </summary>
+        public virtual string Id { get; set; }
+
+        /// <summary>
+        /// The time at which the message was recived.
+        /// </summary>
+        public virtual DateTime TimeReceived { get; set; }
+    }
+}
