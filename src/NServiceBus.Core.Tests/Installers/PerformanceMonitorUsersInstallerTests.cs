c0d35e96d94fda5787c00f7b4e3254b47983e277
Simon Cropp <simon.cropp@gmail.com>
2013-06-07 20:58:17 +1000

    add service user to "Performance Monitor Users"

---
+++ src/NServiceBus.Core.Tests/Installers/PerformanceMonitorUsersInstallerTests.cs
@@ -0,0 +1,17 @@
+﻿namespace NServiceBus.Core.Tests.Installers
+{
+    using Installation;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PerformanceMonitorUsersInstallerTests
+    {
+        [Test]
+        [Explicit]
+        public void Integration()
+        {
+            var installer = new PerformanceMonitorUsersInstaller();
+            installer.Install(@"retina\nsbtest");
+        }
+    }
+}
