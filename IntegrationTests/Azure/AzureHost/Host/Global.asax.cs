3bf6b7324db9da97379abe5416898b7a7867f99d
Alistair Bush <alistair.bush@gmail.com>
2013-06-17 19:39:24 +1200

    Cleanup of the AzureHost integration test to make it easier to deploy to the cloud
    Add AzureTimeoutPersisterConfig as it is also initilized.
    
    Remove Plugins as they stop users from generating their own.
    
    Update build directory for Azure HostProcess
    
    Remove unused namespaces.
    
    Further configuration additions which aims to demonstrate what has to be configured.

--- IntegrationTests/Azure/AzureHost/Host/Global.asax.cs
+++ IntegrationTests/Azure/AzureHost/Host/Global.asax.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using NServiceBus;
-using NServiceBus.Config;
-using NServiceBus.Integration.Azure;
 
 namespace Host
 {
