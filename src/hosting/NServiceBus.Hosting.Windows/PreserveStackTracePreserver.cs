d1c2203442e44da742f6e55480ef668319c1b85a
Simon Cropp <simon.cropp@gmail.com>
2013-06-18 21:40:47 +1000

    fixes 1137
    re-throw and preserve stack trace

---
+++ src/hosting/NServiceBus.Hosting.Windows/PreserveStackTracePreserver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace NServiceBus.Hosting.Windows
+{
+    public static class PreserveStackTracePreserver
+    {
+        public static void PreserveStackTrace(this Exception e)
+        {
+            var context = new StreamingContext(StreamingContextStates.CrossAppDomain);
+            var objectManager = new ObjectManager(null, context);
+            var serializationInfo = new SerializationInfo(e.GetType(), new FormatterConverter());
+
+            e.GetObjectData(serializationInfo, context);
+            objectManager.RegisterObject(e, 1, serializationInfo); // prepare for SetObjectData
+            objectManager.DoFixups(); // ObjectManager calls SetObjectData
+        }
+    }
+}
