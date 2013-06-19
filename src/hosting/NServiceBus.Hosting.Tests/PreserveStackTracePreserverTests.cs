9d94da490e9b6f676005c0e9a90920e452cc9816
Simon Cropp <simon.cropp@gmail.com>
2013-06-19 13:38:57 +1000

    stop the JIT from optimizing the stack trace

--- src/hosting/NServiceBus.Hosting.Tests/PreserveStackTracePreserverTests.cs
+++ src/hosting/NServiceBus.Hosting.Tests/PreserveStackTracePreserverTests.cs
@@ -1,6 +1,7 @@
 namespace NServiceBus.Hosting.Tests
 {
     using System;
+    using System.Runtime.CompilerServices;
     using Windows;
     using NUnit.Framework;
 
@@ -11,12 +12,15 @@ namespace NServiceBus.Hosting.Tests
         [Test]
         public void PreservedStackTraceShouldInclude()
         {
+            //Note the NoInlining below is to prevent the JIT from optimizing out the stack trace
             var preservedException = Assert.Throws<Exception>(MethodThatReThrowsInnerExceptionWithPreserve);
             var actual = "actual: " + preservedException.StackTrace;
             Assert.IsTrue(preservedException.StackTrace.Contains("MethodThatThrows2"), actual);
             Assert.IsTrue(preservedException.StackTrace.Contains("MethodThatThrows1"), actual);
         }
 
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
         void MethodThatReThrowsInnerExceptionWithPreserve()
         {
             try
@@ -31,9 +35,9 @@ namespace NServiceBus.Hosting.Tests
             }
         }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         void MethodThatThrowsWithInnerException()
         {
-
             try
             {
                 MethodThatThrows1();
@@ -42,12 +46,14 @@ namespace NServiceBus.Hosting.Tests
             {
                 throw new Exception("bar", exception);
             }
-
         }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
         void MethodThatThrows1()
         {
             MethodThatThrows2();
         }
+        [MethodImpl(MethodImplOptions.NoInlining)]
         void MethodThatThrows2()
         {
             throw new Exception("Foo");
--- src/hosting/NServiceBus.Hosting.Windows/PreserveStackTracePreserver.cs
+++ src/hosting/NServiceBus.Hosting.Windows/PreserveStackTracePreserver.cs
@@ -1,28 +1,19 @@
-﻿using System;
-using System.Runtime.Serialization;
-
-
-namespace NServiceBus.Hosting.Windows
+﻿namespace NServiceBus.Hosting.Windows
 {
-    using System.Reflection;
+    using System;
+    using System.Runtime.Serialization;
 
     static class PreserveStackTracePreserver
     {
         public static void PreserveStackTrace(this Exception exception)
         {
+            var context = new StreamingContext(StreamingContextStates.CrossAppDomain);
+            var objectManager = new ObjectManager(null, context);
+            var serializationInfo = new SerializationInfo(exception.GetType(), new FormatterConverter());
 
-            typeof(Exception).GetMethod("PrepForRemoting",
-                BindingFlags.NonPublic | BindingFlags.Instance)
-                              .Invoke(exception, new object[0]);
-
-
-            //var context = new StreamingContext(StreamingContextStates.CrossAppDomain);
-            //var objectManager = new ObjectManager(null, context);
-            //var serializationInfo = new SerializationInfo(exception.GetType(), new FormatterConverter());
-
-            //exception.GetObjectData(serializationInfo, context);
-            //objectManager.RegisterObject(exception, 1, serializationInfo); // prepare for SetObjectData
-            //objectManager.DoFixups(); // ObjectManager calls SetObjectData
+            exception.GetObjectData(serializationInfo, context);
+            objectManager.RegisterObject(exception, 1, serializationInfo); // prepare for SetObjectData
+            objectManager.DoFixups(); // ObjectManager calls SetObjectData
         }
     }
 }
