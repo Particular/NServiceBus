86aef0101bb1364a05e4d2be6b968300f06242f2
Simon Cropp <simon.cropp@gmail.com>
2013-06-20 22:14:11 +1000

    add ravenuserinstaller

---
+++ src/NServiceBus.Core.Tests/Installers/RavenUserInstallerTests.cs
@@ -0,0 +1,66 @@
+﻿namespace NServiceBus.Core.Tests.Installers
+{
+    using System;
+    using Installation;
+    using NUnit.Framework;
+    using Raven.Client.Document;
+    using Raven.Client.Embedded;
+
+    [TestFixture]
+    public class RavenUserInstallerTests
+    {
+        [Test]
+        [Explicit]
+        //this will edit your current installed RavenDB
+        public void Integration()
+        {
+            using (var documentStore = new DocumentStore
+                                       {
+                                           Url = "http://localhost:8080"
+                                       })
+            {
+                documentStore.Initialize();
+                var identity = Environment.MachineName + @"\nsbtest";
+                RavenUserInstaller.AddUserToDatabase(identity, documentStore, "Test");
+            }
+        }
+
+        [Test]
+        public void EndureUserIsAddedToWindowsSettings()
+        {
+            using (var documentStore = new EmbeddableDocumentStore
+                                       {
+                                           RunInMemory = true
+                                       })
+            {
+                documentStore.Initialize();
+                RavenUserInstaller.AddUserToDatabase(@"domain\user", documentStore, "FakeDatabase");
+                var systemCommands = documentStore
+                    .DatabaseCommands
+                    .ForSystemDatabase();
+                var existing = systemCommands.Get("Raven/Authorization/WindowsSettings");
+
+                var expected = @"{
+  ""RequiredGroups"": [],
+  ""RequiredUsers"": [
+    {
+      ""Name"": ""domain\\user"",
+      ""Enabled"": true,
+      ""Databases"": [
+        {
+          ""Admin"": true,
+          ""ReadOnly"": false,
+          ""TenantId"": ""FakeDatabase""
+        }
+      ]
+    }
+  ]
+}";
+                var actual = existing.DataAsJson.ToString();
+                Assert.AreEqual(expected, actual);
+            }
+
+        }
+    }
+
+}
---
+++ src/NServiceBus.Core/Installation/RavenUserInstaller.cs
@@ -0,0 +1,119 @@
+namespace NServiceBus.Installation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Environments;
+    using Persistence.Raven;
+    using Raven.Abstractions.Extensions;
+    using Raven.Client;
+    using Raven.Json.Linq;
+    using Logging;
+
+    /// <summary>
+    /// Add the identity to the Raven users group 
+    /// </summary>
+    public class RavenUserInstaller : INeedToInstallSomething<Windows>
+    {
+
+        static ILog logger = LogManager.GetLogger(typeof(RavenUserInstaller));
+
+        public StoreAccessor StoreAccessor { get; set; }
+
+        public void Install(string identity)
+        {
+            var store = StoreAccessor.Store;
+            var dbName = Configure.EndpointName;
+            logger.InfoFormat(string.Format("Adding user '{0}' to raven. Instance:'{1}', Database:'{2}'.", identity, store.Url, dbName));
+            try
+            {
+                AddUserToDatabase(identity, store, dbName);
+            }
+            catch (Exception exception)
+            {
+                logger.Warn("Failed to add user to raven. Processing will continue", exception);
+            }
+        }
+
+        internal static void AddUserToDatabase(string identity, IDocumentStore documentStore, string databaseName)
+        {
+            var systemCommands = documentStore
+                .DatabaseCommands
+                .ForSystemDatabase();
+            var existing = systemCommands.Get("Raven/Authorization/WindowsSettings");
+
+            WindowsAuthDocument windowsAuthDocument;
+            if (existing == null)
+            {
+                windowsAuthDocument = new WindowsAuthDocument();
+            }
+            else
+            {
+                windowsAuthDocument = existing
+                    .DataAsJson
+                    .JsonDeserialization<WindowsAuthDocument>();
+            }
+            AddOrUpdateAuthUser(windowsAuthDocument, identity, databaseName);
+
+            var ravenJObject = RavenJObject.FromObject(windowsAuthDocument);
+
+            systemCommands.Put("Raven/Authorization/WindowsSettings", null, ravenJObject, new RavenJObject());
+        }
+
+        static void AddOrUpdateAuthUser(WindowsAuthDocument windowsAuthDocument, string identity, string tenantId)
+        {
+            var windowsAuthForUser = windowsAuthDocument
+                .RequiredUsers
+                .FirstOrDefault(x => x.Name == identity);
+            if (windowsAuthForUser == null)
+            {
+                windowsAuthForUser = new WindowsAuthData
+                                     {
+                                         Name = identity
+                                     };
+                windowsAuthDocument.RequiredUsers.Add(windowsAuthForUser);
+            }
+            windowsAuthForUser.Enabled = true;
+
+            AddOrUpdateDataAccess(windowsAuthForUser, tenantId);
+        }
+
+        static void AddOrUpdateDataAccess(WindowsAuthData windowsAuthForUser, string tenantId)
+        {
+            var dataAccess = windowsAuthForUser
+                .Databases
+                .FirstOrDefault(x => x.TenantId == tenantId);
+            if (dataAccess == null)
+            {
+                dataAccess = new DatabaseAccess
+                             {
+                                 TenantId = tenantId
+                             };
+                windowsAuthForUser.Databases.Add(dataAccess);
+            }
+            dataAccess.ReadOnly = false;
+            dataAccess.Admin = true;
+        }
+
+        internal class WindowsAuthDocument
+        {
+            public List<WindowsAuthData> RequiredGroups = new List<WindowsAuthData>();
+            public List<WindowsAuthData> RequiredUsers = new List<WindowsAuthData>();
+        }
+
+        internal class WindowsAuthData
+        {
+            public string Name;
+            public bool Enabled;
+            public List<DatabaseAccess> Databases = new List<DatabaseAccess>();
+        }
+
+        internal class DatabaseAccess
+        {
+            public bool Admin;
+            public bool ReadOnly;
+            public string TenantId;
+        }
+    }
+
+}
