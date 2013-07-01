855971beba8691379c2c96adb6d164218eac6053
John Simons <john.simons@nservicebus.com>
2013-07-01 15:40:06 +1000

    Fixed obsolete message for serializers, #SB-181 done

--- src/NServiceBus.Core/Serializers/Json/Config/ConfigureJsonSerializer.cs
+++ src/NServiceBus.Core/Serializers/Json/Config/ConfigureJsonSerializer.cs
@@ -2,7 +2,7 @@ namespace NServiceBus
 {
     public static class ConfigureJsonSerializer
     {
-        [ObsoleteEx(Replacement = "Configure.Serializers.Json()", RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0")]
+        [ObsoleteEx(Replacement = "Configure.Serialization.Json()", RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0")]
         public static Configure JsonSerializer(this Configure config)
         {
             Configure.Serialization.Json();
@@ -10,7 +10,7 @@ namespace NServiceBus
             return config;
         }
 
-        [ObsoleteEx(Replacement = "Configure.Serializers.Bson()", RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0")]
+        [ObsoleteEx(Replacement = "Configure.Serialization.Bson()", RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0")]
         public static Configure BsonSerializer(this Configure config)
         {
             Configure.Serialization.Bson();
