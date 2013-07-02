a1260db6bf1e5ebbd392126cf0eab0c2427c78f3
indualagarsamy <indu@bitsnbytes.us>
2013-07-01 18:30:54 -0700

    added another unit test for 1277 to make sure that reply messages to saga's get correlated properly without requiring an explicit context mapping

---
+++ AcceptanceTests/NServiceBus.AcceptanceTests/Sagas/When_an_endpoint_replies_to_a_saga.cs
@@ -0,0 +1,113 @@
+﻿using Remotion.Linq.Parsing.Structure.IntermediateModel;
+
+namespace NServiceBus.AcceptanceTests.Sagas
+{
+    using System;
+    using EndpointTemplates;
+    using AcceptanceTesting;
+    using NUnit.Framework;
+    using Saga;
+    using ScenarioDescriptors;
+
+    // Repro for issue  https://github.com/NServiceBus/NServiceBus/issues/1277 to test the fix
+    // making sure that the saga co-rrelation still works.
+    public class When_an_endpoint_replies_to_a_saga : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_correlate_all_saga_messages_properly()
+        {
+            Scenario.Define<Context>()
+                    .WithEndpoint<EndpointThatHostsASaga>(b => b.Given(bus => bus.SendLocal(new StartSaga { DataId = Guid.NewGuid() })))
+                    .WithEndpoint<EndpointThatHandlesAMessageFromSagaAndReplies>()
+                    .Done(c => c.DidSagaReplyMessageGetCorrelated)
+                    .Repeat(r => r.For(Transports.Default))
+                    .Should(c =>
+                    {
+                        Assert.True(c.DidSagaReplyMessageGetCorrelated);
+                    })
+                    .Run();
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool DidSagaReplyMessageGetCorrelated { get; set; }
+        }
+
+        public class EndpointThatHandlesAMessageFromSagaAndReplies : EndpointConfigurationBuilder
+        {
+            public EndpointThatHandlesAMessageFromSagaAndReplies()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            class DoSomethingHandler : IHandleMessages<DoSomething>
+            {
+                public IBus Bus { get; set; }
+
+                public void Handle(DoSomething message)
+                {
+                    Console.WriteLine("Received DoSomething command for DataId:{0} ... and responding with a reply", message.DataId);
+                    Bus.Reply(new DoSomethingResponse { DataId = message.DataId });
+                }
+            }
+        }
+
+        public class EndpointThatHostsASaga : EndpointConfigurationBuilder
+        {
+            public EndpointThatHostsASaga()
+            {
+                EndpointSetup<DefaultServer>()
+                    .AddMapping<DoSomething>(typeof (EndpointThatHandlesAMessageFromSagaAndReplies));
+
+            }
+
+            public class Saga2 : Saga<Saga2.MySaga2Data>, IAmStartedByMessages<StartSaga>, IHandleMessages<DoSomethingResponse>
+            {
+                public Context Context { get; set; }
+
+                public void Handle(StartSaga message)
+                {
+                    var dataId = Guid.NewGuid();
+                    Console.Out.WriteLine("Saga2 sending DoSomething for DataId: {0}", dataId);
+                    Data.DataId = dataId;
+                    Bus.Send(new DoSomething { DataId = dataId });
+                }
+
+                public void Handle(DoSomethingResponse message)
+                {
+                    Context.DidSagaReplyMessageGetCorrelated = message.DataId == Data.DataId;
+                    Console.Out.WriteLine("Saga received DoSomethingResponse for DataId: {0} and MarkAsComplete", message.DataId);
+                    MarkAsComplete();
+                }
+
+                public override void ConfigureHowToFindSaga()
+                {
+                    ConfigureMapping<StartSaga>(m => m.DataId).ToSaga(s => s.DataId);
+                }
+
+                public class MySaga2Data : ContainSagaData
+                {
+                    [Unique]
+                    public Guid DataId { get; set; }
+                }
+            }
+        }
+        
+
+        [Serializable]
+        public class StartSaga : ICommand
+        {
+            public Guid DataId { get; set; }
+        }
+
+        public class DoSomething : ICommand
+        {
+            public Guid DataId { get; set; }
+        }
+
+        public class DoSomethingResponse : IMessage
+        {
+            public Guid DataId { get; set; }
+        }
+    }
+}
