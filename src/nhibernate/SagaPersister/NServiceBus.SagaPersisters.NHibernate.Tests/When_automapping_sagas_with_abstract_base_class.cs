309347870868e3bd8074d317586e1bcaa84ad67f
John Simons <john.simons@nservicebus.com>
2013-07-04 15:00:20 +1000

    NHibernate saga data not working with abstract class, #SB-191 done

--- src/nhibernate/SagaPersister/NServiceBus.SagaPersisters.NHibernate.Tests/When_automapping_sagas_with_abstract_base_class.cs
+++ src/nhibernate/SagaPersister/NServiceBus.SagaPersisters.NHibernate.Tests/When_automapping_sagas_with_abstract_base_class.cs
@@ -20,18 +20,16 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
             var properties = SQLiteConfiguration.InMemory();
 
             sessionFactory = builder.Build(new Configuration().AddProperties(properties)) as SessionFactoryImpl;
-
         }
 
         [Test]
         public void Should_not_generate_join_table_for_base_class()
         {
-
             var persister = sessionFactory.GetEntityPersister(typeof(SagaWithAbstractBaseClass).FullName).
                    ClassMetadata as global::NHibernate.Persister.Entity.UnionSubclassEntityPersister;
 
-            Assert.NotNull(persister, "The persister should be single table");
-        }
+            Assert.IsNotNull(persister);
+        }       
     }
 
     public class SagaWithAbstractBaseClass : MyOwnAbstractBase
