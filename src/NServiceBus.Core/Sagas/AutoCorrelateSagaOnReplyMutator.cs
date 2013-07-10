6449104dcab6ea11908146cfa967b8e57360e3ac
John Simons <john.simons@nservicebus.com>
2013-07-10 12:00:50 +1000

    Added backwards compatibility code to support #1277

--- src/NServiceBus.Core/Sagas/AutoCorrelateSagaOnReplyMutator.cs
+++ src/NServiceBus.Core/Sagas/AutoCorrelateSagaOnReplyMutator.cs
@@ -18,6 +18,20 @@ namespace NServiceBus.Sagas
             originatingSagaId = null;
             originatingSagaType = null;
 
+            // We need this for backwards compatibility because in v4.0.0 we still have this headers being sent as part of the message even if MessageIntent == MessageIntentEnum.Publish
+            if (transportMessage.MessageIntent == MessageIntentEnum.Publish)
+            {
+                if (transportMessage.Headers.ContainsKey(Headers.SagaId))
+                {
+                    transportMessage.Headers.Remove(Headers.SagaId);
+                }
+
+                if (transportMessage.Headers.ContainsKey(Headers.SagaType))
+                {
+                    transportMessage.Headers.Remove(Headers.SagaType);
+                }
+            }
+
             if (transportMessage.Headers.ContainsKey(Headers.OriginatingSagaId))
             {
                 originatingSagaId = transportMessage.Headers[Headers.OriginatingSagaId];
