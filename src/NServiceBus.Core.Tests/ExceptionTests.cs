05dca05c4af6f928e61c53ce6caa2e4f82487b58
Simon Cropp <simon.cropp@gmail.com>
2013-07-17 13:50:06 +1000

    add serialization support to exceptions

--- src/NServiceBus.Core.Tests/ExceptionTests.cs
+++ src/NServiceBus.Core.Tests/ExceptionTests.cs
@@ -22,10 +22,10 @@
             foreach (var exceptionType in exceptionTypes)
             {
                 var constructor = exceptionType.GetConstructor(BindingFlags.NonPublic | BindingFlags.CreateInstance | BindingFlags.Instance, null, new[] { typeof(SerializationInfo), typeof(StreamingContext) }, null);
-                Assert.IsNotNull(constructor, string.Format("Exception '{0}' should implement 'ctor(SerializationInfo info, StreamingContext context)'", exceptionType.Name));
+                Assert.IsNotNull(constructor, string.Format("Exception '{0}' should implement 'protected {0}(SerializationInfo info, StreamingContext context){{}}'", exceptionType.Name));
                 var serializableAttribute = exceptionType.GetCustomAttributes(typeof(SerializableAttribute), false).FirstOrDefault();
                 Assert.IsNotNull(serializableAttribute, string.Format("Exception '{0}' should have a 'SerializableAttribute'", exceptionType.Name));
-                var propertyInfos = exceptionType.GetProperties(BindingFlags.Public|BindingFlags.Instance);
+                var propertyInfos = exceptionType.GetProperties(BindingFlags.Public|BindingFlags.Instance|BindingFlags.DeclaredOnly);
                 if (propertyInfos.Length > 0)
                 {
                     var getObjectDataMethod = exceptionType.GetMethod("GetObjectData");
--- src/NServiceBus.Core/Rhino.Licensing/LicenseExpiredException.cs
+++ src/NServiceBus.Core/Rhino.Licensing/LicenseExpiredException.cs
@@ -6,6 +6,7 @@ namespace Rhino.Licensing
     ///<summary>
     /// Thrown when license is found but is past it's expiration date
     ///</summary>
+    [Serializable]
     public class LicenseExpiredException : RhinoLicensingException
     {
         /// <summary>
@@ -37,7 +38,7 @@ namespace Rhino.Licensing
         /// </summary>
         /// <param name="info">serialization information</param>
         /// <param name="context">streaming context</param>
-        public LicenseExpiredException(SerializationInfo info, StreamingContext context) : base(info, context)
+        protected LicenseExpiredException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
