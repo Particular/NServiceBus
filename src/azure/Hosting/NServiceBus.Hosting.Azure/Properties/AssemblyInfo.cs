3071ef45ad2d2a6ab3162f135e425265ebf32ecb
Simon Cropp <simon.cropp@gmail.com>
2013-07-29 16:04:46 +1000

    Add tests for killing a process
    fixes #1264

--- src/azure/Hosting/NServiceBus.Hosting.Azure/Properties/AssemblyInfo.cs
+++ src/azure/Hosting/NServiceBus.Hosting.Azure/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Security;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Generic Host Process for running NServiceBus on windows azeure")]
@@ -13,6 +12,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany("NServiceBus")]
 [assembly: AssemblyConfiguration("release")]
 [assembly: AssemblyInformationalVersion("4.0.0-alpha0")]
-[assembly: ComVisible(false)]		
+[assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("NServiceBus.Integration.Azure.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100dde965e6172e019ac82c2639ffe494dd2e7dd16347c34762a05732b492e110f2e4e2e1b5ef2d85c848ccfb671ee20a47c8d1376276708dc30a90ff1121b647ba3b7259a6bc383b2034938ef0e275b58b920375ac605076178123693c6c4f1331661a62eba28c249386855637780e3ff5f23a6d854700eaa6803ef48907513b92")]		
 [assembly: CLSCompliantAttribute(true)]
 
