166fe9f597ba3e14f9eb9a4b994eb16df1d35a6c
John Simons <john.simons@nservicebus.com>
2013-08-01 14:48:51 +1000

    Code format

--- src/NServiceBus.Core/Gateway/HeaderManagement/HeaderMapper.cs
+++ src/NServiceBus.Core/Gateway/HeaderManagement/HeaderMapper.cs
@@ -10,7 +10,9 @@ namespace NServiceBus.Gateway.HeaderManagement
         public static TransportMessage Map(IDictionary<string, string> from)
         {
             if (!from.ContainsKey(GatewayHeaders.IsGatewayMessage))
+            {
                 return new TransportMessage();
+            }
 
             var headers = ExtractHeaders(from);
             var to = new TransportMessage(from[NServiceBus + Id], headers);
@@ -18,20 +20,24 @@ namespace NServiceBus.Gateway.HeaderManagement
             to.CorrelationId = @from[NServiceBus + CorrelationId] ?? to.Id;
 
             bool recoverable;
-            if(bool.TryParse(from[NServiceBus + Recoverable], out recoverable))
+            if (bool.TryParse(from[NServiceBus + Recoverable], out recoverable))
+            {
                 to.Recoverable = recoverable;
+            }
 
             TimeSpan timeToBeReceived;
             TimeSpan.TryParse(from[NServiceBus + TimeToBeReceived], out timeToBeReceived);
             to.TimeToBeReceived = timeToBeReceived;
 
             if (to.TimeToBeReceived < TimeSpan.FromSeconds(1))
+            {
                 to.TimeToBeReceived = TimeSpan.FromSeconds(1);
+            }
 
             return to;
         }
 
-        static Dictionary<string,string> ExtractHeaders(IDictionary<string, string> from)
+        static Dictionary<string, string> ExtractHeaders(IDictionary<string, string> from)
         {
             var result = new Dictionary<string, string>();
 
@@ -39,14 +45,14 @@ namespace NServiceBus.Gateway.HeaderManagement
             {
                 if (header.Contains(NServiceBus + Headers.HeaderName))
                 {
-                    result.Add(header.Replace(NServiceBus + Headers.HeaderName + ".", String.Empty),from[header]);
+                    result.Add(header.Replace(NServiceBus + Headers.HeaderName + ".", String.Empty), from[header]);
                 }
             }
 
             return result;
         }
 
-        public static void Map(TransportMessage from, IDictionary<string,string> to)
+        public static void Map(TransportMessage from, IDictionary<string, string> to)
         {
             to[NServiceBus + Id] = from.Id;
             to[NServiceBus + CorrelationId] = GetCorrelationForBackwardsCompatibility(from);
@@ -57,10 +63,12 @@ namespace NServiceBus.Gateway.HeaderManagement
             SetBackwardsCompatibilityHeaders(to);
 
             if (from.Headers.ContainsKey(ReplyToAddress))
+            {
                 to[Headers.RouteTo] = from.Headers[ReplyToAddress];
+            }
 
             from.Headers.ToList()
-                .ForEach(header =>to[NServiceBus + Headers.HeaderName + "." + header.Key] = header.Value);
+                .ForEach(header => to[NServiceBus + Headers.HeaderName + "." + header.Key] = header.Value);
         }
 
         [ObsoleteEx(RemoveInVersion = "5.0")]
@@ -75,7 +83,7 @@ namespace NServiceBus.Gateway.HeaderManagement
         [ObsoleteEx(RemoveInVersion = "5.0")]
         static string GetCorrelationForBackwardsCompatibility(TransportMessage message)
         {
-            var correlationIdToStore = message.CorrelationId;
+            string correlationIdToStore = message.CorrelationId;
 
             if (Configure.HasComponent<MsmqMessageSender>())
             {
@@ -83,7 +91,8 @@ namespace NServiceBus.Gateway.HeaderManagement
 
                 if (Guid.TryParse(message.CorrelationId, out correlationId))
                 {
-                    correlationIdToStore = message.CorrelationId + "\\0";//msmq required the id's to be in the {guid}\{incrementing number} format so we need to fake a \0 at the end to make it compatible                
+                    correlationIdToStore = message.CorrelationId + "\\0";
+                        //msmq required the id's to be in the {guid}\{incrementing number} format so we need to fake a \0 at the end to make it compatible                
                 }
             }
 
@@ -97,8 +106,6 @@ namespace NServiceBus.Gateway.HeaderManagement
         const string Recoverable = "Recoverable";
         const string ReplyToAddress = "ReplyToAddress";
         const string TimeToBeReceived = "TimeToBeReceived";
-
-        const string CorrIdHeader = "CorrId";
         const string IdForCorrelation = "IdForCorrelation";
     }
 }
