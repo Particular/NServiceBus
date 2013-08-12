1eef8f0a4cdcb01cb611894e504becbdb966df2c
John Simons <john.simons@nservicebus.com>
2013-08-12 12:09:06 +1000

    Request/Response over Gateway fails to correlate, fixes #1411

--- src/NServiceBus.Core/Gateway/HeaderManagement/HeaderMapper.cs
+++ src/NServiceBus.Core/Gateway/HeaderManagement/HeaderMapper.cs
@@ -10,43 +10,49 @@ namespace NServiceBus.Gateway.HeaderManagement
         public static TransportMessage Map(IDictionary<string, string> from)
         {
             if (!from.ContainsKey(GatewayHeaders.IsGatewayMessage))
+            {
                 return new TransportMessage();
+            }
 
             var headers = ExtractHeaders(from);
             var to = new TransportMessage(from[NServiceBus + Id], headers);
 
-            to.CorrelationId = @from[NServiceBus + CorrelationId] ?? to.Id;
+            to.CorrelationId = StripSlashZeroFromCorrelationId(from[NServiceBus + CorrelationId]) ?? to.Id;
 
             bool recoverable;
-            if(bool.TryParse(from[NServiceBus + Recoverable], out recoverable))
+            if (bool.TryParse(from[NServiceBus + Recoverable], out recoverable))
+            {
                 to.Recoverable = recoverable;
+            }
 
             TimeSpan timeToBeReceived;
             TimeSpan.TryParse(from[NServiceBus + TimeToBeReceived], out timeToBeReceived);
             to.TimeToBeReceived = timeToBeReceived;
 
-            if (to.TimeToBeReceived < TimeSpan.FromSeconds(1))
-                to.TimeToBeReceived = TimeSpan.FromSeconds(1);
+            if (to.TimeToBeReceived < MinimumTimeToBeReceived)
+            {
+                to.TimeToBeReceived = MinimumTimeToBeReceived;
+            }
 
             return to;
         }
 
-        static Dictionary<string,string> ExtractHeaders(IDictionary<string, string> from)
+        static Dictionary<string, string> ExtractHeaders(IDictionary<string, string> from)
         {
             var result = new Dictionary<string, string>();
 
-            foreach (string header in from.Keys)
+            foreach (var header in from.Keys)
             {
                 if (header.Contains(NServiceBus + Headers.HeaderName))
                 {
-                    result.Add(header.Replace(NServiceBus + Headers.HeaderName + ".", String.Empty),from[header]);
+                    result.Add(header.Replace(NServiceBus + Headers.HeaderName + ".", String.Empty), from[header]);
                 }
             }
 
             return result;
         }
 
-        public static void Map(TransportMessage from, IDictionary<string,string> to)
+        public static void Map(TransportMessage from, IDictionary<string, string> to)
         {
             to[NServiceBus + Id] = from.Id;
             to[NServiceBus + CorrelationId] = GetCorrelationForBackwardsCompatibility(from);
@@ -57,10 +63,31 @@ namespace NServiceBus.Gateway.HeaderManagement
             SetBackwardsCompatibilityHeaders(to);
 
             if (from.Headers.ContainsKey(ReplyToAddress))
+            {
                 to[Headers.RouteTo] = from.Headers[ReplyToAddress];
+            }
 
             from.Headers.ToList()
-                .ForEach(header =>to[NServiceBus + Headers.HeaderName + "." + header.Key] = header.Value);
+                .ForEach(header => to[NServiceBus + Headers.HeaderName + "." + header.Key] = header.Value);
+        }
+
+        [ObsoleteEx(RemoveInVersion = "5.0", TreatAsErrorFromVersion = "5.0")]
+        static string StripSlashZeroFromCorrelationId(string corrId)
+        {
+            if (corrId == null)
+            {
+                return null;
+            }
+
+            if (Configure.HasComponent<MsmqMessageSender>())
+            {
+                if (corrId.EndsWith("\\0"))
+                {
+                    return corrId.Replace("\\0", String.Empty);
+                }
+            }
+
+            return corrId;
         }
 
         [ObsoleteEx(RemoveInVersion = "5.0", TreatAsErrorFromVersion = "5.0")]
@@ -83,7 +110,8 @@ namespace NServiceBus.Gateway.HeaderManagement
 
                 if (Guid.TryParse(message.CorrelationId, out correlationId))
                 {
-                    correlationIdToStore = message.CorrelationId + "\\0";//msmq required the id's to be in the {guid}\{incrementing number} format so we need to fake a \0 at the end to make it compatible                
+                    correlationIdToStore = message.CorrelationId + "\\0";
+                        //msmq required the id's to be in the {guid}\{incrementing number} format so we need to fake a \0 at the end to make it compatible                
                 }
             }
 
@@ -97,8 +125,7 @@ namespace NServiceBus.Gateway.HeaderManagement
         const string Recoverable = "Recoverable";
         const string ReplyToAddress = "ReplyToAddress";
         const string TimeToBeReceived = "TimeToBeReceived";
-
-        const string CorrIdHeader = "CorrId";
         const string IdForCorrelation = "IdForCorrelation";
+        static readonly TimeSpan MinimumTimeToBeReceived = TimeSpan.FromSeconds(1);
     }
 }
