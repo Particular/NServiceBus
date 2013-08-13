e74ee672f40c0dd60b2c829f95ace03b83771e9f
Simon Cropp <simon.cropp@gmail.com>
2013-08-13 13:41:28 +1000

    clean up doco on UniqueAttribute

--- src/NServiceBus/Saga/UniqueAttribute.cs
+++ src/NServiceBus/Saga/UniqueAttribute.cs
@@ -13,26 +13,28 @@ namespace NServiceBus.Saga
     public sealed class UniqueAttribute : Attribute
     {
         /// <summary>
-        /// Gets a single property that is marked with the UniqueAttribute for a saga entity
+        /// Gets a single property that is marked with the <see cref="UniqueAttribute"/> for a <see cref="IContainSagaData"/>.
         /// </summary>
-        /// <param name="type">The type to evaluate</param>
-        /// <returns>A PropertyInfo of the property marked with a UniqAttribute or null if not used</returns>
+        /// <param name="type">The <see cref="Type"/> to evaluate.</param>
+        /// <returns>A <see cref="PropertyInfo"/> of the property marked with a <see cref="UniqueAttribute"/> or null if not used.</returns>
         public static PropertyInfo GetUniqueProperty(Type type)
         {
             var properties = GetUniqueProperties(type);
 
             if (properties.Count() > 1)
-                throw new InvalidOperationException(
-                    string.Format("More than one UniqueAttribute property was found on the type '{0}'. However, only one property is supported.", type.FullName));
+            {
+                var message = string.Format("More than one UniqueAttribute property was found on the type '{0}'. However, only one property is supported.", type.FullName);
+                throw new InvalidOperationException(message);
+            }
 
             return properties.SingleOrDefault();
         }
 
         /// <summary>
-        /// Gets a single property that is marked with the UniqueAttribute for a saga entity
+        /// Gets a single property that is marked with the <see cref="UniqueAttribute"/> for a saga entity.
         /// </summary>
-        /// <param name="entity">A saga entity</param>
-        /// <returns>A PropertyInfo of the property marked with a UniqAttribute or null if not used</returns>
+        /// <param name="entity">A saga entity.</param>
+        /// <returns>A <see cref="PropertyInfo"/> of the property marked with a <see cref="UniqueAttribute"/> or null if not used.</returns>
         public static KeyValuePair<string, object>? GetUniqueProperty(IContainSagaData entity)
         {
             var prop = GetUniqueProperty(entity.GetType());
@@ -43,24 +45,26 @@ namespace NServiceBus.Saga
         }
 
         /// <summary>
-        /// Gets all the properties that are marked with the UniqueAttribute for a saga entity
+        /// Gets all the properties that are marked with the <see cref="UniqueAttribute"/> for a saga entity.
         /// </summary>
-        /// <param name="entity">A saga entity</param>
-        /// <returns>A dictionary of property names and their values</returns>
+        /// <param name="entity">A <see cref="IContainSagaData"/>.</param>
+        /// <returns>A <see cref="IDictionary{TKey,TValue}"/> of property names and their values.</returns>
         public static IDictionary<string, object> GetUniqueProperties(IContainSagaData entity)
         {
-            return GetUniqueProperties(entity.GetType()).ToDictionary(p => p.Name, p => p.GetValue(entity, null));
+            return GetUniqueProperties(entity.GetType())
+                .ToDictionary(p => p.Name, p => p.GetValue(entity, null));
         }
 
         /// <summary>
-        /// Gets all the properties that are marked with the UniqueAttribute for the given Type
+        /// Gets all the properties that are marked with the <see cref="UniqueAttribute"/> for the given <paramref name="Type"/>.
         /// </summary>
-        /// <param name="type">The type to evaluate</param>
-        /// <returns>A queryable of PropertyInfo</returns>
+        /// <param name="type">The type to evaluate.</param>
+        /// <returns>A <see cref="IQueryable"/> of <see cref="PropertyInfo"/>.</returns>
         public static IQueryable<PropertyInfo> GetUniqueProperties(Type type)
         {
             return type.GetProperties()
-                .Where(p => p.CanRead && p.GetCustomAttributes(typeof (UniqueAttribute), false).Length > 0).AsQueryable();
+                .Where(p => p.CanRead && p.GetCustomAttributes(typeof (UniqueAttribute), false).Length > 0)
+                .AsQueryable();
         }
     }
 }
