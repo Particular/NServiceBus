9b3a021668b9c32145d87ad11e79eb4f1069cbc5
Simon Cropp <simon.cropp@gmail.com>
2013-08-14 13:57:36 +1000

    use enumerable not iqueryable
    fixes #1421

---
+++ src/NServiceBus.Core.Tests/Sagas/UniqueAttributeTests.cs
@@ -0,0 +1,71 @@
+﻿namespace NServiceBus.Core.Tests.Sagas
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+    using Saga;
+
+    [TestFixture]
+    public class UniqueAttributeTests
+    {
+
+        [Test]
+        public void Ensure_property_is_returned_when_attribute_exists()
+        {
+            var uniqueProperties = UniqueAttribute.GetUniqueProperties(typeof(ModelWithUniqueProperty))
+                .ToList();
+            Assert.AreEqual(1,uniqueProperties.Count);
+            Assert.AreEqual("PropertyWithAttribute", uniqueProperties.First().Name);
+        }
+
+        [Test]
+        public void Ensure_multiple_properties_are_returned_when_multiple_attributes_exists()
+        {
+            var uniqueProperties = UniqueAttribute.GetUniqueProperties(typeof(ModelWithMultipleUniqueProperty))
+                .ToList();
+            Assert.AreEqual(2,uniqueProperties.Count);
+            Assert.AreEqual("PropertyWithAttribute1", uniqueProperties.First().Name);
+            Assert.AreEqual("PropertyWithAttribute2", uniqueProperties.Skip(1).First().Name);
+        }
+
+        [Test]
+        public void Ensure_exception_is_thrown_when_multiple_attributes_exists()
+        {
+            Assert.Throws<InvalidOperationException>(() => UniqueAttribute.GetUniqueProperty(typeof(ModelWithMultipleUniqueProperty)));
+        }
+
+        [Test]
+        public void Ensure_single_property_is_returned_when_attribute_exists()
+        {
+            var uniqueProperty = UniqueAttribute.GetUniqueProperty(typeof(ModelWithUniqueProperty));
+            Assert.IsNotNull(uniqueProperty);
+        }
+
+        [Test]
+        public void Ensure_property_is_returned_when_no_attribute_exists()
+        {
+            var uniqueProperties = UniqueAttribute.GetUniqueProperties(typeof(ModelWithNoUniqueProperty));
+            Assert.IsEmpty(uniqueProperties);
+        }
+
+        public class ModelWithMultipleUniqueProperty
+        {
+            [UniqueAttribute]
+            public string PropertyWithAttribute1 { get; set; }   
+            [UniqueAttribute]
+            public string PropertyWithAttribute2 { get; set; }   
+        }
+
+        public class ModelWithUniqueProperty
+        {
+            [UniqueAttribute]
+            public string PropertyWithAttribute { get; set; }   
+            public string PropertyWithNoAttribute { get; set; }   
+        }
+        public class ModelWithNoUniqueProperty
+        {
+            public string PropertyWithAttribute { get; set; }   
+            public string PropertyWithNoAttribute { get; set; }   
+        }
+    }
+}
--- src/NServiceBus/Saga/UniqueAttribute.cs
+++ src/NServiceBus/Saga/UniqueAttribute.cs
@@ -19,19 +19,20 @@ namespace NServiceBus.Saga
         /// <returns>A <see cref="PropertyInfo"/> of the property marked with a <see cref="UniqueAttribute"/> or null if not used.</returns>
         public static PropertyInfo GetUniqueProperty(Type type)
         {
-            var properties = GetUniqueProperties(type);
+            var properties = GetUniqueProperties(type)
+                .ToList();
 
-            if (properties.Count() > 1)
+            if (properties.Count > 1)
             {
                 var message = string.Format("More than one UniqueAttribute property was found on the type '{0}'. However, only one property is supported.", type.FullName);
                 throw new InvalidOperationException(message);
             }
 
-            return properties.SingleOrDefault();
+            return properties.First();
         }
 
         /// <summary>
-        /// Gets a single property that is marked with the <see cref="UniqueAttribute"/> for a saga entity.
+        /// Gets a single property that is marked with the <see cref="UniqueAttribute"/> for a <see cref="IContainSagaData"/>.
         /// </summary>
         /// <param name="entity">A saga entity.</param>
         /// <returns>A <see cref="PropertyInfo"/> of the property marked with a <see cref="UniqueAttribute"/> or null if not used.</returns>
@@ -45,7 +46,7 @@ namespace NServiceBus.Saga
         }
 
         /// <summary>
-        /// Gets all the properties that are marked with the <see cref="UniqueAttribute"/> for a saga entity.
+        /// Gets all the properties that are marked with the <see cref="UniqueAttribute"/> for a <see cref="IContainSagaData"/>.
         /// </summary>
         /// <param name="entity">A <see cref="IContainSagaData"/>.</param>
         /// <returns>A <see cref="IDictionary{TKey,TValue}"/> of property names and their values.</returns>
@@ -60,11 +61,10 @@ namespace NServiceBus.Saga
         /// </summary>
         /// <param name="type">The type to evaluate.</param>
         /// <returns>A <see cref="IQueryable"/> of <see cref="PropertyInfo"/>.</returns>
-        public static IQueryable<PropertyInfo> GetUniqueProperties(Type type)
+        public static IEnumerable<PropertyInfo> GetUniqueProperties(Type type)
         {
             return type.GetProperties()
-                .Where(p => p.CanRead && p.GetCustomAttributes(typeof (UniqueAttribute), false).Length > 0)
-                .AsQueryable();
+                .Where(p => p.CanRead && p.GetCustomAttributes(typeof (UniqueAttribute),false).Length > 0);
         }
     }
 }
