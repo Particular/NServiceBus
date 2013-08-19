f3d93a4e9f0a843867548574b44ff9910591c566
Simon Cropp <simon.cropp@gmail.com>
2013-08-19 11:25:38 +1000

    ConnectionStringParser  swallows stack trace
    the catch was redundant. the message can be derived from the stack trace
    also it did not add the inner exception which would obscure the
    debugging experience

--- src/RabbitMQ/NServiceBus.RabbitMQ/Config/ConnectionStringParser.cs
+++ src/RabbitMQ/NServiceBus.RabbitMQ/Config/ConnectionStringParser.cs
@@ -1,43 +1,38 @@
-﻿using IHostConfiguration = EasyNetQ.IHostConfiguration;
-
-namespace NServiceBus.Transports.RabbitMQ.Config
+﻿namespace NServiceBus.Transports.RabbitMQ.Config
 {
-    using System;
-    using System.Collections.Generic;
     using System.ComponentModel;
     using System.Data.Common;
     using System.Text.RegularExpressions;
-    using EasyNetQ;
     using System.Linq;
 
     public class ConnectionStringParser : DbConnectionStringBuilder, IConnectionStringParser
     {
         ConnectionConfiguration connectionConfiguration;
+
         public IConnectionConfiguration Parse(string connectionString)
         {
             ConnectionString = connectionString;
 
-            try
+            connectionConfiguration = new ConnectionConfiguration();
+
+            foreach (var pair in 
+                (from property in typeof(ConnectionConfiguration).GetProperties()
+                    let match = Regex.Match(connectionString, string.Format("[^\\w]*{0}=(?<{0}>[^;]+)", property.Name), RegexOptions.IgnoreCase)
+                    where match.Success
+                    select new
+                           {
+                               Property = property,
+                               match.Groups[property.Name].Value
+                           }))
+                pair.Property.SetValue(connectionConfiguration, TypeDescriptor.GetConverter(pair.Property.PropertyType).ConvertFromString(pair.Value), null);
+
+            if (ContainsKey("host"))
             {
-                connectionConfiguration = new ConnectionConfiguration();
-
-                foreach(var pair in 
-                    (from property in typeof(ConnectionConfiguration).GetProperties()
-                     let match = Regex.Match(connectionString, string.Format("[^\\w]*{0}=(?<{0}>[^;]+)", property.Name), RegexOptions.IgnoreCase)
-                     where match.Success
-                     select new { Property = property, match.Groups[property.Name].Value }))
-                    pair.Property.SetValue(connectionConfiguration, TypeDescriptor.GetConverter(pair.Property.PropertyType).ConvertFromString(pair.Value),null);
-
-                if (ContainsKey("host"))
-                    connectionConfiguration.ParseHosts(this["host"] as string);
-
-                connectionConfiguration.Validate();
-                return connectionConfiguration;
+                connectionConfiguration.ParseHosts(this["host"] as string);
             }
-            catch (Exception parseException)
-            {
-                throw new Exception(string.Format("Connection String parsing exception {0}", parseException.Message));
-            }
+
+            connectionConfiguration.Validate();
+            return connectionConfiguration;
         }
     }
 }
