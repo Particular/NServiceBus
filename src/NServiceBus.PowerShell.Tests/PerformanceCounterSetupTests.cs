a62120a3dae17e363e24b775867a3c1f21aa458e
Simon Cropp <simon.cropp@gmail.com>
2013-09-20 14:26:55 +1000

    Dont delete perf counters on install
    fixes #1488

---
+++ src/NServiceBus.PowerShell.Tests/PerformanceCounterSetupTests.cs
@@ -0,0 +1,72 @@
+﻿namespace NServiceBus.PowerShell.Tests
+{
+    using System.Diagnostics;
+    using NUnit.Framework;
+    using Setup.Windows.PerformanceCounters;
+
+    [TestFixture]
+    public class PerformanceCounterSetupTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            PerformanceCounter.CloseSharedResources();
+            if (PerformanceCounterCategory.Exists("NServiceBus"))
+            {
+                PerformanceCounterSetup.DeleteCategory();
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            PerformanceCounter.CloseSharedResources();
+            if (PerformanceCounterCategory.Exists("NServiceBus"))
+            {
+                PerformanceCounterSetup.DeleteCategory();
+            }
+        }
+
+
+        [Explicit]
+        [Test]
+        public void DoAllCountersExist_returns_false_when_category_missing()
+        {
+            Assert.IsFalse(PerformanceCounterSetup.DoAllCountersExist());
+        }
+
+        [Explicit]
+        [Test]
+        public void DoAllCountersExist_returns_true_when_all_exist()
+        {
+            PerformanceCounterSetup.SetupCounters();
+            PerformanceCounter.CloseSharedResources();
+            Assert.IsTrue(PerformanceCounterSetup.DoAllCountersExist());
+        }
+
+        [Explicit]
+        [Test]
+        public void DoAllCountersExist_returns_false_when_some_counters_are_missing_exist()
+        {
+            var counters = new CounterCreationDataCollection
+                           {
+                               new CounterCreationData("Critical Time","Age of the oldest message in the queue.",PerformanceCounterType.NumberOfItems32),
+                           };
+            PerformanceCounterCategory.Create("NServiceBus", "NServiceBus statistics", PerformanceCounterCategoryType.MultiInstance, counters);
+            PerformanceCounter.CloseSharedResources();
+            Assert.IsFalse(PerformanceCounterSetup.DoAllCountersExist());
+        }
+
+        [Explicit]
+        [Test]
+        public void CreateBadCounters()
+        {
+            var counters = new CounterCreationDataCollection
+                           {
+                               new CounterCreationData("Critical Time","Age of the oldest message in the queue.",PerformanceCounterType.NumberOfItems32),
+                           };
+            PerformanceCounterCategory.Create("NServiceBus", "NServiceBus statistics", PerformanceCounterCategoryType.MultiInstance, counters);
+        }
+    }
+}
