70832821d21e1f1bd7e0d4a7160a9bd54cf20402
Simon Cropp <simon.cropp@gmail.com>
2013-09-27 18:57:34 +1000

    Re-enable the forwarding feature
    for #1591

--- src/NServiceBus.Core/Audit/Audit.cs
+++ src/NServiceBus.Core/Audit/Audit.cs
@@ -10,12 +10,11 @@
 
     public class Audit : Feature
     {
-        readonly static ILog Logger = LogManager.GetLogger(typeof(Audit));
+        static ILog Logger = LogManager.GetLogger(typeof(Audit));
 
         public override void Initialize()
         {
             base.Initialize();
-            WarnUserIfUsingUnicastBusConfigForAudit();
                 
             // Check to see if this entry is specified either in the app.config 
             // (in the new config section or existing unicastBus config section or the registry)
@@ -24,11 +23,6 @@
 
             // Check the AuditConfig section
             UseSettingsFromAuditConfigIfDefined(ref forwardAddress, ref timeToBeReceivedOnForwardedMessages);
-            if (forwardAddress == null)
-            {
-                // See if its defined in UnicastBusConfig section
-                UseSettingsFromUnicastBusForBackwardsCompatibility(ref forwardAddress, ref timeToBeReceivedOnForwardedMessages);
-            }
 
             if (forwardAddress == null)
             {
@@ -54,11 +48,13 @@
             }
         }
 
-        private Address ReadAuditQueueNameFromRegistry()
+        Address ReadAuditQueueNameFromRegistry()
         {
             var forwardQueue = RegistryReader<string>.Read("AuditQueue");
             if (string.IsNullOrWhiteSpace(forwardQueue))
+            {
                 return Address.Undefined;
+            }
             
             var forwardAddress = Address.Parse(forwardQueue);
 
@@ -71,7 +67,7 @@
             return forwardAddress;
         }
 
-        private void ThrowIfForwardAddressIsStillUndefined(Address forwardAddress)
+        void ThrowIfForwardAddressIsStillUndefined(Address forwardAddress)
         {
             // If the forwardAddress is still null, then it hasn't been specified either in the config or registry
             // and the user hasn't turned off the auditing feature -- throw exception in that case.
@@ -86,26 +82,7 @@ To configure it in registry, run the Set-NServiceBusLocalMachineSettings Powersh
             }
         }
 
-        private void WarnUserIfUsingUnicastBusConfigForAudit()
-        {
-            var unicastConfig = Configure.GetConfigSection<UnicastBusConfig>();
-            if (unicastConfig != null && !string.IsNullOrWhiteSpace(unicastConfig.ForwardReceivedMessagesTo))
-            {
-                var msg = @"Please use AuditConfig instead of UnicastBusConfig to specify audit related configuration.  Please remove the audit related attributes such as ForwardReceivedMessagesTo from UnicastBusConfig. 
-To quickly add the AuditConfig section to your app.config, in Package Manager Console type: add-NServiceBusAuditConfig. The audit related configuration will be removed from UnicastBusConfig in v5.0";
-
-                // Throw an exception when running within the debugger, to ask user to use the new config section
-                if (Debugger.IsAttached)
-                {
-                    throw new ConfigurationErrorsException(msg);
-                }
-
-                Logger.Warn(msg);
-            }
-        }
-
-        private void UseSettingsFromAuditConfigIfDefined(ref Address forwardAddress ,
-            ref TimeSpan timeToBeReceivedOnForwardedMessages )
+        void UseSettingsFromAuditConfigIfDefined(ref Address forwardAddress, ref TimeSpan timeToBeReceivedOnForwardedMessages )
         {
             // Get the auditing configuration - This could be specified either in the new MessageAuditingConfig or still in UnicastBusConfig
             // Check the message auditing config section first for the auditing configuration
@@ -117,17 +94,5 @@ To quickly add the AuditConfig section to your app.config, in Package Manager Co
             }
         }
 
-        private void UseSettingsFromUnicastBusForBackwardsCompatibility(ref Address forwardAddress,
-            ref TimeSpan timeToBeReceivedOnForwardedMessages)
-        {
-            // Check to see if the audit values are also being defined in the UnicastBusConfig section. If so log a warning in production, throw in development.
-            var unicastConfig = Configure.GetConfigSection<UnicastBusConfig>();
-            if (unicastConfig != null && !string.IsNullOrWhiteSpace(unicastConfig.ForwardReceivedMessagesTo))
-            {
-                Logger.Warn(@"Using the audit configuration values defined in the UnicastBusConfig for now.");
-                forwardAddress = Address.Parse(unicastConfig.ForwardReceivedMessagesTo);
-                timeToBeReceivedOnForwardedMessages = unicastConfig.TimeToBeReceivedOnForwardedMessages;
-             }
-        }
     }
 }
