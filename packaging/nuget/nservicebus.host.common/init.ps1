aa7fce18af2e88d1672313b2d3ec20781838d43d
John Simons <john.simons@nservicebus.com>
2013-10-10 12:06:43 +1100

    Fixed bug in T4Scaffolding init.ps1, see https://mvcscaffolding.codeplex.com/workitem/60
    Included T4Scaffolding as part of our hosts packages with patched version.

---
+++ packaging/nuget/nservicebus.host.common/init.ps1
@@ -0,0 +1,21 @@
+﻿param($rootPath, $toolsPath, $package, $project)
+
+if (Get-Module T4Scaffolding) {
+	# Disable scaffolding as much as possible until VS is restarted
+	Remove-Module T4Scaffolding
+}
+
+# OK, we've got the correct .NET assembly version or none at all (in which case we can load the correct version)
+$dllPath = Join-Path $toolsPath T4Scaffolding.dll
+$packagesRoot = [System.IO.Path]::GetDirectoryName($rootPath)
+
+if (Test-Path $dllPath) {
+	# Load the .NET PowerShell module and set up aliases, tab expansions, etc.
+	Import-Module $dllPath
+	[T4Scaffolding.NuGetServices.Services.ScaffoldingPackagePathResolver]::SetPackagesRootDirectory($packagesRoot)
+	Set-Alias Scaffold Invoke-Scaffolder -Option AllScope -scope Global
+} 
+else 
+{
+	Write-Warning ("Could not find T4Scaffolding module. Looked for: " + $dllPath)
+}
