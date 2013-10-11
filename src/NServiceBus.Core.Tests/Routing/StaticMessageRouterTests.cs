b7a9d00a1434b867e71e7ec6bdedd0d6bcdac0d9
Simon Cropp <simon.cropp@gmail.com>
2013-10-11 14:43:08 +1100

    add failing test for message mapping order

---
+++ src/NServiceBus.Core.Tests/Routing/StaticMessageRouterTests.cs
@@ -0,0 +1,80 @@
+﻿namespace NServiceBus.Core.Tests.Sagas
+{
+    using NUnit.Framework;
+    using Unicast.Routing;
+
+    [TestFixture]
+    public class StaticMessageRouterTests
+    {
+
+        [Test]
+        public void When_initialized_known_message_returns_undefined()
+        {
+            var router = new StaticMessageRouter(new[] { typeof(Message1) });
+            Assert.AreEqual(Address.Undefined, router.GetDestinationFor(typeof(Message1)));
+        }
+
+        [Test]
+        public void When_initialized_unknown_message_returns_undefined()
+        {
+            var router = new StaticMessageRouter(new[] { typeof(Message1) });
+            Assert.AreEqual(Address.Undefined, router.GetDestinationFor(typeof(Message2)));
+        }
+
+        [Test]
+        public void When_getting_route_correct_address_is_returned()
+        {
+            var router = new StaticMessageRouter(new[] { typeof(Message1) });
+            var address = new Address("a","b");
+            router.RegisterRoute(typeof(Message1), address);
+            Assert.AreSame(address, router.GetDestinationFor(typeof(Message1)));
+        }
+
+        [Test]
+        public void When_inherited_registered_after_base_correct_address_is_returned_for_both()
+        {
+            var baseType = typeof(BaseMessage);
+            var inheritedType = typeof(InheritedMessage);
+            var router = new StaticMessageRouter(new[] { baseType, inheritedType });
+            var baseAddress = new Address("baseAddress", "b");
+            router.RegisterRoute(baseType, baseAddress);
+            var inheritedAddress = new Address("inheritedAddress", "b");
+            router.RegisterRoute(inheritedType, inheritedAddress);
+            Assert.AreSame(baseAddress, router.GetDestinationFor(baseType));
+            Assert.AreSame(inheritedAddress, router.GetDestinationFor(inheritedType));
+        }
+
+        [Test]
+        public void When_inherited_base_after_registered_correct_address_is_returned_for_both()
+        {
+            var baseType = typeof(BaseMessage);
+            var inheritedType = typeof(InheritedMessage);
+            var router = new StaticMessageRouter(new[] { baseType, inheritedType });
+            var inheritedAddress = new Address("inheritedAddress", "b");
+            router.RegisterRoute(inheritedType, inheritedAddress);
+            var baseAddress = new Address("baseAddress", "b");
+            router.RegisterRoute(baseType, baseAddress);
+            Assert.AreSame(baseAddress, router.GetDestinationFor(baseType));
+            Assert.AreSame(inheritedAddress, router.GetDestinationFor(inheritedType));
+        }
+
+        public class Message1
+        {
+            
+        }
+        public class Message2
+        {
+            
+        }
+
+        public class BaseMessage
+        {
+            
+        }
+
+        public class InheritedMessage : BaseMessage
+        {
+            
+        }
+    }
+}
