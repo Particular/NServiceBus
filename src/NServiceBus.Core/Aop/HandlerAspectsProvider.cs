cdd49c9b72524c6b47c850fe9af6eadf80ba2d44
Marco Encarnacao <MeTitus@gmail.com>
2013-10-16 18:28:00 +0100

    Implemented AOP capabilities into the handlers. In order to be able to keep up with the changes in the main branch I opted to implement it without changing the underlying logic, which of course translates in some messy code. Hopefully NServiceBus guys will introduce AOP in the future (missing unit tests, will add them later)

---
+++ src/NServiceBus.Core/Aop/HandlerAspectsProvider.cs
@@ -0,0 +1,20 @@
+﻿using NServiceBus.Aop.Args;
+using System;
+
+namespace NServiceBus.Aop
+{
+    public class HandlerAspectsProvider
+    {
+        public virtual void OnEntry(HandlerAspectEntryArgs args)
+        { }
+
+        public virtual void OnExit(HandlerAspectArgs args)
+        { }
+
+        public virtual void OnSuccess(HandlerAspectArgs args)
+        { }
+
+        public virtual void OnException(HandlerAspectExceptionArgs args)
+        { }
+    }
+}
---
+++ src/NServiceBus.Core/Aop/IUseHandlerAspectsAttribute.cs
@@ -0,0 +1,32 @@
+﻿using NServiceBus.Aop.Args;
+using System;
+
+namespace NServiceBus.Aop
+{
+    public class IUseHandlerAspectsAttribute : Attribute
+    {
+        public IUseHandlerAspectsAttribute(Type aspectProvider)
+        {
+            AspectProvider = aspectProvider;
+        }
+
+
+        public Type AspectProvider
+        {
+            get; private set;
+        }
+
+
+        public virtual void OnEntry(HandlerAspectEntryArgs args)
+        { }
+
+        public virtual void OnExit(HandlerAspectArgs args)
+        { }
+
+        public virtual void OnSuccess(HandlerAspectArgs args)
+        { }
+
+        public virtual void OnException(HandlerAspectExceptionArgs args)
+        { }
+    }
+}
