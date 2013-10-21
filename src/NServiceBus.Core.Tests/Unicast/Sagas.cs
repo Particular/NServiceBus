9bc40439d4b4717a111ef3b5b58152b23b8b1ce4
andreasohlund <andreasohlund2@gmail.com>
2013-10-21 16:50:34 +0200

    Added tests to verify that saga completion works as expected

--- src/NServiceBus.Core.Tests/Unicast/Sagas.cs
+++ src/NServiceBus.Core.Tests/Unicast/Sagas.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using NUnit.Framework;
+    using Pipeline;
     using Saga;
 
     [TestFixture]
@@ -188,6 +189,46 @@
         class MessageThatMissesSaga : IMessage { }
     }
 
+    [TestFixture]
+    public class When_completing_a_saga : with_sagas
+    {
+        [Test]
+        public void Should_not_be_persisted_if_completed_right_away()
+        {
+            RegisterSaga<MySaga>();
+         
+            ReceiveMessage(new MessageThatStartsSaga());
+
+            Assert.AreEqual(0, persister.CurrentSagaEntities.Count(), "No saga should be stored");
+        }
+
+        [Test]
+        public void Should_be_removed_from_storage_if_persistent()
+        {
+            var sagaId = Guid.NewGuid();
+
+            RegisterSaga<MySaga>(new MySagaData { Id = sagaId });
+
+            ReceiveMessage(new MessageThatStartsSaga(), new Dictionary<string, string> { { Headers.SagaId, sagaId.ToString() } });
+
+            Assert.AreEqual(0, persister.CurrentSagaEntities.Count(), "No saga should be stored");
+        }
+
+
+        class MySaga : Saga<MySagaData>, IAmStartedByMessages<MessageThatStartsSaga>
+        {
+            public void Handle(MessageThatStartsSaga message)
+            {
+                MarkAsComplete();
+            }
+        }
+
+        class MySagaData : ContainSagaData
+        {
+        }
+
+        class MessageThatStartsSaga : IMessage { }
+    }
 
     [TestFixture]
     public class When_receiving_a_message_that_hits_multiple_sagas_of_different_types : with_sagas
