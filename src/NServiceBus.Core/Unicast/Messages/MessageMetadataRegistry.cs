9a167ad70379b8dac1f96e299472e28274922271
Tom Ferguson <tom.ferguson@lazook.com>
2013-10-25 11:10:22 +0100

    Changed \n to Environment.NewLine in MessageMetadata exception message to provide better cross-platform support

--- src/NServiceBus.Core/Unicast/Messages/MessageMetadataRegistry.cs
+++ src/NServiceBus.Core/Unicast/Messages/MessageMetadataRegistry.cs
@@ -14,7 +14,7 @@
             {
                 return metadata;
             }
-            var message = string.Format("Could not find Metadata for '{0}'.\nPlease ensure the following:\n1. '{0}' is included in initial scanning see File Scanning: http://particular.net/articles/the-nservicebus-host\n2. '{0}' implements either 'IMessage', 'IEvent' or 'ICommand' or alternatively, if you don't want to implement an interface, you can use 'Unobtrusive Mode' see: http://particular.net/articles/unobtrusive-mode-messages", messageType.FullName);
+            var message = string.Format("Could not find Metadata for '{0}'.{1}Please ensure the following:{1}1. '{0}' is included in initial scanning see File Scanning: http://particular.net/articles/the-nservicebus-host{1}2. '{0}' implements either 'IMessage', 'IEvent' or 'ICommand' or alternatively, if you don't want to implement an interface, you can use 'Unobtrusive Mode' see: http://particular.net/articles/unobtrusive-mode-messages", messageType.FullName, Environment.NewLine);
             throw new Exception(message);
         }
 
