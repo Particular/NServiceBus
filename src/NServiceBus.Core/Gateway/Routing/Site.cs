72ff1d02439b69f377a7288b9495d49719a8a4b3
Chris Bednarski <chris.bednarski@outlook.com>
2013-11-03 09:33:13 +1100

    Fix merge -X theirs of upstream/develop
    Readded ReplyChannel property after merge

--- src/NServiceBus.Core/Gateway/Routing/Site.cs
+++ src/NServiceBus.Core/Gateway/Routing/Site.cs
@@ -12,6 +12,7 @@ namespace NServiceBus.Gateway.Routing
         public Channel Channel { get; set; }
         public string Key { get; set; }
         public bool LegacyMode { get; set; }
+        public string ReplyChannel { get; set; }
 
         [ObsoleteEx(RemoveInVersion = "5.0", Message = "From v5 we need to set the legacy mode to false.")]
         void DefaultLegacyModeToEnabled()
