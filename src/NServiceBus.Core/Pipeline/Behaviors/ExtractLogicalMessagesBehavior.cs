648156274a8abfd2a35f84e71e99edd6c456b011
Simon Cropp <simon.cropp@gmail.com>
2013-11-11 15:09:56 +1100

    if control message dont deserialize
    but sill invoke CallbackInvocationBehavior
    fixes #1752

--- src/NServiceBus.Core/Pipeline/Behaviors/ExtractLogicalMessagesBehavior.cs
+++ src/NServiceBus.Core/Pipeline/Behaviors/ExtractLogicalMessagesBehavior.cs
@@ -42,6 +42,13 @@
 
             var transportMessage = context.PhysicalMessage;
 
+            if (transportMessage.IsControlMessage())
+            {
+                log.Info("Received a control message. Skipping deserilization as control message data is contained in the header.");
+                next();
+                return;
+            }
+
             object[] rawMessages;
 
             try
@@ -53,7 +60,7 @@
                 throw new SerializationException(string.Format("An error occurred while attempting to extract logical messages from transport message {0}", transportMessage), exception);
             }
 
-            if (!transportMessage.IsControlMessage() && !rawMessages.Any())
+            if (!rawMessages.Any())
             {
                 log.Warn("Received an empty message - ignoring.");
                 return;
