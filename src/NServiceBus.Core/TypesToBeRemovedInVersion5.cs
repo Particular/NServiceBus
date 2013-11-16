7bdfa3d882511d6709fb66b4505157b61c586fc3
andreasohlund <andreasohlund2@gmail.com>
2013-11-16 11:50:05 +0100

    Adding a few types that got removed prematurely

---
+++ src/NServiceBus.Core/TypesToBeRemovedInVersion5.cs
@@ -0,0 +1,68 @@
+﻿namespace NServiceBus.Sagas
+{
+    using System;
+    using MessageMutator;
+    using Saga;
+
+    [ObsoleteEx(TreatAsErrorFromVersion="4.3",RemoveInVersion = "5.0")]
+    public class AutoCorrelateSagaOnReplyMutator : IMutateTransportMessages, INeedInitialization
+    {
+        public void MutateIncoming(TransportMessage transportMessage)
+        {
+        }
+
+        public void MutateOutgoing(object[] messages, TransportMessage transportMessage)
+        {
+        }
+
+        public void Init()
+        {
+        }
+    }
+
+    [ObsoleteEx(TreatAsErrorFromVersion = "4.3", RemoveInVersion = "5.0")]
+    public class ReplyingToNullOriginatorDispatcher : IHandleReplyingToNullOriginator
+    {
+        public void TriedToReplyToNullOriginator()
+        {
+            
+        }
+    }
+
+    [ObsoleteEx(TreatAsErrorFromVersion = "4.3", RemoveInVersion = "5.0")]
+    public class SagaContext
+    {
+        [ThreadStatic]
+        public static ISaga Current;
+    }
+
+     [ObsoleteEx(TreatAsErrorFromVersion = "4.3", RemoveInVersion = "5.0")]
+    public class OriginatingSagaHeaderMutator : IMutateOutgoingTransportMessages, INeedInitialization
+    {
+        /// <summary>
+        /// Set the header if we run in the context of a saga
+        /// </summary>
+        public void MutateOutgoing(object[] messages, TransportMessage transportMessage)
+        {
+        }
+
+        public void Init()
+        {
+        }
+    }
+}
+
+
+namespace NServiceBus.Sagas.Finders
+{
+    using Saga;
+
+    [ObsoleteEx(Message = "Not used since 4.2", TreatAsErrorFromVersion = "4.3", RemoveInVersion = "5.0")]
+    public class NullSagaFinder<T> : IFindSagas<T>.Using<object> where T : IContainSagaData
+    {
+        public T FindBy(object message)
+        {
+            return default(T);
+        }
+    }
+}
