1b0d00db01e820e67cafe740c1b9f063efbcd18f
Andreas Öhlund <andreas.ohlund@particular.net>
2013-11-22 15:41:19 +0100

    Updated with latest pipeline changes

---
+++ src/NServiceBus.Core.Tests/Outbox/OutboxReceiveBehaviourTests.cs
@@ -0,0 +1,60 @@
+﻿namespace NServiceBus.Core.Tests.Pipeline
+{
+    using NServiceBus.Pipeline.Contexts;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class OutboxReceiveBehaviourTests
+    {
+        FakeOutboxStorage fakeOutbox;
+        OutboxReceiveBehaviour behavior;
+
+        [SetUp]
+        public void SetUp()
+        {
+            fakeOutbox = new FakeOutboxStorage();
+         
+            behavior = new OutboxReceiveBehaviour
+            {
+                OutboxStorage = fakeOutbox
+            };
+
+        }
+
+        void Invoke(TransportMessage message,bool shouldAbort = false)
+        {
+            var context = new PhysicalMessageContext(null, message);
+            behavior.Invoke(context, () =>
+            {
+                if (shouldAbort)
+                {
+                    Assert.Fail("Pipeline should be aborted");
+                }
+            });
+        }
+
+
+        [Test]
+        public void Should_mark_outbox_message_as_stored_when_successfully_processing_a_message()
+        {
+            var incomingTransportMessage = new TransportMessage();
+
+            Invoke(incomingTransportMessage);
+
+            Assert.True(fakeOutbox.StoredMessage.Dispatched);
+        }
+
+        [Test]
+        public void Should_shortcut_the_pipeline_if_existing_message_is_found()
+        {
+            var incomingTransportMessage = new TransportMessage();
+
+            fakeOutbox.ExistingMessage = new OutboxMessage { Id = incomingTransportMessage.Id };
+
+            Invoke(incomingTransportMessage);
+
+            Assert.True(fakeOutbox.ExistingMessage.Dispatched);
+            Assert.Null(fakeOutbox.StoredMessage);
+        }
+    }
+}
---
+++ src/NServiceBus.Core.Tests/Outbox/TransportOperation.cs
@@ -0,0 +1,10 @@
+﻿namespace NServiceBus.Core.Tests.Pipeline
+{
+    using Unicast;
+
+    class TransportOperation
+    {
+        public SendOptions SendOptions { get; set; }
+        public TransportMessage PhysicalMessage { get; set; }
+    }
+}
