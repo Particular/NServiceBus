ca36db1af183567468556354cbb4eda10b4f1ee0
Simon Cropp <simon.cropp@gmail.com>
2013-11-27 10:21:03 +1100

    forgot to pass the value

--- src/NServiceBus.Core/Unicast/UnicastBus.cs
+++ src/NServiceBus.Core/Unicast/UnicastBus.cs
@@ -42,7 +42,7 @@ namespace NServiceBus.Unicast
         {
             set
             {
-                messageHandlingDisabled = true;
+                messageHandlingDisabled = value;
             }
         }
 
