791768306dfb6a721598f8a6b66f9064a91c282e
Simon Cropp <simon.cropp@gmail.com>
2013-12-17 12:09:24 +1100

    split pipeline override

--- src/NServiceBus.Core/Pipeline/PipelineOverride.cs
+++ src/NServiceBus.Core/Pipeline/PipelineOverride.cs
@@ -28,17 +28,4 @@
         {
         }
     }
-
-   
-    [Obsolete("This is a prototype API. May change in minor version releases.")]
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public interface IPipelineOverride
-    {
-        void Override(BehaviorList<HandlerInvocationContext> behaviorList);
-        void Override(BehaviorList<ReceiveLogicalMessageContext> behaviorList);
-        void Override(BehaviorList<ReceivePhysicalMessageContext> behaviorList);
-        void Override(BehaviorList<SendLogicalMessageContext> behaviorList);
-        void Override(BehaviorList<SendLogicalMessagesContext> behaviorList);
-        void Override(BehaviorList<SendPhysicalMessageContext> behaviorList);
-    }
 }
