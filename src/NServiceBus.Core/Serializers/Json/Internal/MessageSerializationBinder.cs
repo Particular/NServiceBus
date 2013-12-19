cf221d48eb444556a0ca6cef21e3f18e598de200
Yves Goeleven <yves@goeleven.com>
2013-12-19 10:52:11 +0100

    Changed deserialization behavior to be less strict

--- src/NServiceBus.Core/Serializers/Json/Internal/MessageSerializationBinder.cs
+++ src/NServiceBus.Core/Serializers/Json/Internal/MessageSerializationBinder.cs
@@ -1,16 +1,20 @@
 namespace NServiceBus.Serializers.Json.Internal
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Runtime.Serialization;
     using MessageInterfaces;
 
     public class MessageSerializationBinder : SerializationBinder
     {
         private readonly IMessageMapper _messageMapper;
+        private readonly IList<Type> messageTypes;
 
-        public MessageSerializationBinder(IMessageMapper messageMapper)
+        public MessageSerializationBinder(IMessageMapper messageMapper, IList<Type> messageTypes = null)
         {
             _messageMapper = messageMapper;
+            this.messageTypes = messageTypes;
         }
 
         public override void BindToName(Type serializedType, out string assemblyName, out string typeName)
@@ -23,9 +27,22 @@ namespace NServiceBus.Serializers.Json.Internal
 
         public override Type BindToType(string assemblyName, string typeName)
         {
-          throw new NotImplementedException();
-          //string resolvedTypeName = typeName + ", " + assemblyName;
-          //return Type.GetType(resolvedTypeName, true);
+            Type resolved = null;
+            if (messageTypes != null) // usually the requested message types are provided, so this should be fast
+            {
+                resolved = messageTypes.FirstOrDefault(t => t.Name.Contains(typeName));
+            }
+            if (resolved == null) // if the type has been used before it should be resolvable like this
+            {
+                resolved = Type.GetType(typeName);
+            }
+            if (resolved == null) // if the type has not been used before, we need to find it brute force
+            {
+                resolved = AppDomain.CurrentDomain.GetAssemblies()
+                             .Select(a => a.GetType(typeName))
+                             .FirstOrDefault(t => t != null); 
+            }
+            return resolved;
         }
     }
 }
