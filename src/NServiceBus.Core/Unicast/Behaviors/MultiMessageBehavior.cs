278a4684dc48e9cd855951670a6483e6f1d50c02
Simon Cropp <simon.cropp@gmail.com>
2014-01-19 16:54:07 +1100

    fix the build

--- src/NServiceBus.Core/Unicast/Behaviors/MultiMessageBehavior.cs
+++ src/NServiceBus.Core/Unicast/Behaviors/MultiMessageBehavior.cs
@@ -5,18 +5,18 @@
     using Pipeline;
     using Pipeline.Contexts;
 
-    [ObsoleteEx(RemoveInVersion = "5.0", TreatAsErrorFromVersion = "5.0")]
     [Obsolete("This is a prototype API. May change in minor version releases.")]
     [EditorBrowsable(EditorBrowsableState.Never)]
     public class MultiMessageBehavior : IBehavior<SendLogicalMessagesContext>
     {
         public PipelineExecutor PipelineExecutor { get; set; }
 
+        [ObsoleteEx(RemoveInVersion = "5.0", TreatAsErrorFromVersion = "5.0")]
         public void Invoke(SendLogicalMessagesContext context, Action next)
         {
             foreach (var logicalMessage in context.LogicalMessages)
             {
-                PipelineExecutor.InvokeSendPipeline(context.SendOptions,logicalMessage);
+                PipelineExecutor.InvokeSendPipeline(context.SendOptions, logicalMessage);
             }
             next();
         }
