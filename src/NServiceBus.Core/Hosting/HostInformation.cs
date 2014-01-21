481f0e58a83eaaccd877fb935550ec3bab593af0
Andreas Öhlund <andreas.ohlund@particular.net>
2014-01-21 15:12:21 +0100

    Switch the hsotinfo over to be instance based and resolved from the container

--- src/NServiceBus.Core/Hosting/HostInformation.cs
+++ src/NServiceBus.Core/Hosting/HostInformation.cs
@@ -1,21 +1,67 @@
 namespace NServiceBus.Hosting
 {
+    using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Reflection;
+    using Settings;
+    using Support;
+    using Utils;
 
-    public static class HostInformation
+    public class HostInformation
     {
-        public static string HostId { get; set; }
+        public Guid HostId { get; set; }
 
-        public static Dictionary<string, string> Properties { get; set; }
+        public string DisplayName { get; set; }
 
-        //new Dictionary<string, string>
-        //    {
-        //        {"WinService","Sales-1.0.0"},
-        //        {"Machine","server2045"},
-        //        {"IISSite","acme.com"},
-        //        {"RoleInstanceId","2"},
-        //        {"PID","1045"},
-        //        {"Path","C:/somepath"}
-        //    }
+        public Dictionary<string, string> Properties { get; set; }
     }
+
+    class DefaultHostInformation
+    {
+        public static HostInformation GetCurrent()
+        {
+            var assembly = Assembly.GetEntryAssembly();
+
+            if (assembly == null)
+            {
+                assembly = Assembly.GetCallingAssembly();
+            }
+
+            return new HostInformation
+            {
+                HostId = DeterministicGuid.Create(assembly.Location), //todo is this what we need?
+                DisplayName = string.Format("{0}@{1}", assembly.Location, RuntimeEnvironment.MachineName),
+                Properties = new Dictionary<string, string>
+                {
+                   {"Machine", RuntimeEnvironment.MachineName},
+                   {"ProcessID",Process.GetCurrentProcess().Id.ToString()},
+                   {"WorkingDirectory",Environment.CurrentDirectory},
+                   {"UserName",Environment.UserName},
+                   {"CommandLine",Environment.CommandLine},
+                   {"InDebugMode",Debugger.IsAttached.ToString()},
+
+                }
+            };
+        }
+    }
+    class Defaults : ISetDefaultSettings
+    {
+        public Defaults()
+        {
+            SettingsHolder.SetDefault<HostInformation>(DefaultHostInformation.GetCurrent());
+        }
+    }
+
+    class RegisterHostInformation:IWantToRunBeforeConfigurationIsFinalized
+    {
+        public void Run()
+        {
+            var hostInformation = SettingsHolder.Get<HostInformation>(typeof(HostInformation).FullName);
+
+            Configure.Component(() => hostInformation, DependencyLifecycle.InstancePerCall);
+        }
+    }
+
+    
 }
---
+++ src/NServiceBus.Core/Utils/DeterministicGuid.cs
@@ -0,0 +1,21 @@
+﻿namespace NServiceBus.Utils
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class DeterministicGuid
+    {
+        public static Guid Create(params string[] data)
+        {
+            // use MD5 hash to get a 16-byte hash of the string
+            using (var provider = new MD5CryptoServiceProvider())
+            {
+                var inputBytes = Encoding.Default.GetBytes(string.Concat(data));
+                var hashBytes = provider.ComputeHash(inputBytes);
+                // generate a guid from the hash:
+                return new Guid(hashBytes);
+            }
+        } 
+    }
+}
