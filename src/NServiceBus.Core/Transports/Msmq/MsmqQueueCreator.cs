17eab0d09b5ebb3b921e2f1a19545535b98afab3
Yves Goeleven <yves@goeleven.com>
2014-01-22 14:56:52 +0100

    dealing with too long queue names in msmq

--- src/NServiceBus.Core/Transports/Msmq/MsmqQueueCreator.cs
+++ src/NServiceBus.Core/Transports/Msmq/MsmqQueueCreator.cs
@@ -2,7 +2,9 @@ namespace NServiceBus.Transports.Msmq
 {
     using System;
     using System.Messaging;
+    using System.Security.Cryptography;
     using System.Security.Principal;
+    using System.Text;
     using Config;
     using Logging;
     using Support;
@@ -104,9 +106,27 @@ namespace NServiceBus.Transports.Msmq
         /// </summary>
         public static string GetFullPathWithoutPrefix(Address address)
         {
-            return address.Machine + MsmqUtilities.PRIVATE + address.Queue;
+            var queueName = address.Queue;
+
+            if (queueName.Length >= 115)
+            {
+                queueName = DeterministicGuidBuilder(queueName).ToString();
+            }
+
+            return address.Machine + MsmqUtilities.PRIVATE + queueName;
         }
-        
+
+        private static Guid DeterministicGuidBuilder(string input)
+        {
+            // use MD5 hash to get a 16-byte hash of the string
+            using (var provider = new MD5CryptoServiceProvider())
+            {
+                var inputBytes = Encoding.Default.GetBytes(input);
+                var hashBytes = provider.ComputeHash(inputBytes);
+                // generate a guid from the hash:
+                return new Guid(hashBytes);
+            }
+        }
     }
 
 }
