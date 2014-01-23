d6235de14b5f8320ab4a858a312d4aec4ce5792b
Simon Cropp <simon.cropp@gmail.com>
2014-01-23 20:32:13 +1100

    add tests for NSBVersion

---
+++ src/NServiceBus.Core.Tests/NServiceBusVersionTests.cs
@@ -0,0 +1,14 @@
+﻿namespace NServiceBus
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class NServiceBusVersionTests
+    {
+        [Test]
+        public void Verify_has_a_version_and_can_be_parsed()
+        {
+            Assert.IsNotNullOrEmpty(NServiceBusVersion.Version);
+        }
+    }
+}
--- src/NServiceBus/NServiceBusVersion.cs
+++ src/NServiceBus/NServiceBusVersion.cs
@@ -3,12 +3,18 @@ namespace NServiceBus
     /// <summary>
     /// The semver version of NServiceBus
     /// </summary>
-    [ObsoleteEx(RemoveInVersion = "5.0")]
+    [ObsoleteEx(RemoveInVersion = "5.0", TreatAsErrorFromVersion = "5.0")]
     public static class NServiceBusVersion
     {
-        /// <summary>
-        /// The semver version of NServiceBus
-        /// </summary>
-        public static string Version = GitFlowVersion.MajorMinorPatch;
+        static NServiceBusVersion()
+        {
+            var assembly = typeof(NServiceBusVersion).Assembly;
+            var gitFlowVersionInformationType = assembly.GetType("NServiceBus.GitFlowVersionInformation", true);
+            var fieldInfo = gitFlowVersionInformationType.GetField("AssemblyFileVersion");
+            var assemblyFileVersion = System.Version.Parse((string)fieldInfo.GetValue(null));
+            Version = assemblyFileVersion.ToString(3);
+        }
+
+        public static readonly string Version;
     }
 }
