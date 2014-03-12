61d8dd3f1437191588036b6e04b6ef390e8ba7d3
Andreas Öhlund <andreas.ohlund@particular.net>
2014-03-12 13:55:22 +0100

    Only invoking the guidance page if this is a new platform user

--- packaging/nuget/tools/init.ps1
+++ packaging/nuget/tools/init.ps1
@@ -1,107 +1,73 @@
 param($installPath, $toolsPath, $package, $project)
 
-#default to build dir when debugging
-if(!$toolsPath){
-	$toolsPath = ""
+if($toolsPath){
+	if (Get-Module NServiceBus.Powershell) {
+		Remove-Module NServiceBus.Powershell
+	}
+
+	$pathToNServiceBusPSCmdLets = Join-Path $toolsPath NServiceBus.Powershell.dll
+
+	if(Test-Path $pathToNServiceBusPSCmdLets){
+		Import-Module $pathToNServiceBusPSCmdLets
+
+		Write-Host ""
+		Write-Host "Type 'get-help about_NServiceBus' to see all available NServiceBus commands."
+		Write-Host ""
+	}
+	else {
+		Write-Host "NServiceBus powershell module could not be found, no powershell commands will be available"	
+	}
 }
 
-if (Get-Module NServiceBus.Powershell) {
-	Remove-Module NServiceBus.Powershell
-}
 
-Import-Module (Join-Path $toolsPath NServiceBus.Powershell.dll)
-
-Write-Host ""
-Write-Host "Type 'get-help about_NServiceBus' to see all available NServiceBus commands."
-Write-Host ""
 
 $nserviceBusKeyPath =  "HKCU:SOFTWARE\NServiceBus" 
-$machinePreparedKey = "MachinePrepared"
-$machinePrepared = $false
-$versionParts = $package.Version.ToString().Split('.')
-$nservicebusVersion = $versionParts[0]
 
-if($versionParts.Length -gt 1) {
-    $nservicebusVersion += "." + $versionParts[1]
+$platformKeyPath = "HKCU:SOFTWARE\ParticularSoftware"
+$isNewUser = $true
+
+$packageVersion = "Unknown"
+
+if($package){
+	$packageVersion = $package.Version
 }
 
-$nserviceBusVersionPath =  $nserviceBusKeyPath +  "\" + $nservicebusVersion
-
-#Figure out if this machine is properly setup
+#Figure out if this is a first time user
 try {
-	if (!(Test-Path $nserviceBusKeyPath)) {
-		New-Item -Path HKCU:SOFTWARE -Name NServiceBus | Out-Null
+
+	#Check for existing NServiceBus installations
+	if (Test-Path $nserviceBusKeyPath) {
+		"Existing NServiceBus v($packageVersion) user detected"
+	
+		$isNewUser = $false
+	}
+	
+	if (Test-Path $platformKeyPath){
+		"Existing Platform user detected"
+		$isNewUser = $false
 	}
 
-	if (!(Test-Path $nserviceBusVersionPath)){
-		$versionToAdd = $nservicebusVersion
-		New-Item -Path $nserviceBusKeyPath -Name $versionToAdd | Out-Null
-		New-ItemProperty -Path $nserviceBusVersionPath -Name $machinePreparedKey -PropertyType String -Value "false" | Out-Null
+	if (!($isNewuser)) {
+		exit
 	}
-	else
-	{
-		$a = Get-ItemProperty -path $nserviceBusVersionPath
-		$preparedInVersion  = $a.psobject.properties | ?{ $_.Name -eq $machinePreparedKey }
-		$dontCheckMachineSetup  = $a.psobject.properties | ?{ $_.Name -eq "DontCheckMachineSetup" }
 
-		if($preparedInVersion.value -eq $true){
-			$machinePrepared = $true
-		}
-	  
-		if($machinePrepared -or $dontCheckMachineSetup.value)
-		{
-			exit
-		}
+	if (!(Test-Path $platformKeyPath)){
+		New-Item -Path HKCU:SOFTWARE -Name ParticularSoftware | Out-Null
 	}
-} Catch [Exception] { 
-	Write-Warning "There was a problem checking if this machine is properly setup:"	
+
+	Set-ItemProperty -Path $platformKeyPath -Name "NuGetUser" -Value "true" | Out-Null
+
+} 
+Catch [Exception] { 
 	Write-Warning $error[0]
 }
 
-$perfCountersInstalled = $false
-$msmqInstalled = $false
-$dtcInstalled = $false
-$ravenDBInstalled = $false
-
-try {
-	$perfCountersInstalled = Test-NServiceBusPerformanceCountersInstallation
-} Catch [System.Security.SecurityException] { }
-try {
-	$msmqInstalled = Test-NServiceBusMSMQInstallation
-} Catch [System.Security.SecurityException] { }
-try {
-	$dtcInstalled = Test-NServiceBusDTCInstallation
-} Catch [System.Security.SecurityException] { }
-try {
-	$ravenDBInstalled = Test-NServiceBusRavenDBInstallation
-	if(!$ravenDBInstalled) {
-		$ravenDBInstalled = Test-NServiceBusRavenDBInstallation -Port 8081
-	}
-} Catch [System.Security.SecurityException] { }
-
-if(!$perfCountersInstalled){
-	Write-Warning "Performance counters are not installed."
-}
-if(!$msmqInstalled){
-	Write-Warning "Msmq is not installed or correctly setup."
-}
-if(!$dtcInstalled){
-	Write-Warning "DTC is not installed or started."
-}
-if(!$ravenDBInstalled){
-	Write-Warning "RavenDB is not installed. We checked port 8080 and 8081."
-}
-
-if($perfCountersInstalled -and $msmqInstalled -and $dtcInstalled -and $ravenDBInstalled){
-	Write-Host "Required infrastructure is all setup correctly."
-}
-
-try {
-	Set-ItemProperty -Path $nserviceBusVersionPath -Name $machinePreparedKey -Value "true" | Out-Null
-} Catch [Exception] { }
-
-
-$url = "http://particular.net/articles/preparing-your-machine-to-run-nservicebus?dtc=" + $dtcInstalled + "&msmq=" + $msmqInstalled + "&raven=" + $ravenDBInstalled + "&perfcounter=" + $perfCountersInstalled + "&installer=NServiceBus&method=nuget&version=" + $package.Version
+$url = "http://particular.net/download-the-particular-service-platform?&version=$packageVersion" 
 $url = $url.ToLowerInvariant(); 
 
-$dte.ExecuteCommand("View.URL", $url)
+if($dte){
+	$dte.ExecuteCommand("View.URL", $url)
+}
+else{
+	"No dte detected, url: $url"
+}
