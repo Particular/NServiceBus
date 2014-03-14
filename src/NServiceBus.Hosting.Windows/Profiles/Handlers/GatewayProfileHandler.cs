90d83a673356488a22b6308c337ecc07ea7583a4
Yves Goeleven <yves@goeleven.com>
2014-03-14 10:42:00 +0100

    obsoleting multisite profile

--- src/NServiceBus.Hosting.Windows/Profiles/Handlers/GatewayProfileHandler.cs
+++ src/NServiceBus.Hosting.Windows/Profiles/Handlers/GatewayProfileHandler.cs
@@ -1,12 +1,16 @@
 ﻿namespace NServiceBus.Hosting.Windows.Profiles.Handlers
 {
     using Hosting.Profiles;
+    using Logging;
 
+    [ObsoleteEx(RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0", Replacement = "Feature.Enable<Gateway>")]
     class GatewayProfileHandler : IHandleProfile<MultiSite>
     {
         void IHandleProfile.ProfileActivated()
         {
-            Configure.Instance.RunGateway();
+            Log.Warn("MultiSite Profile is obsolete as Gateway is a feature now, you can use Feature.Enable<Gateway> to turn it on.");
         }
+
+        private readonly static ILog Log = LogManager.GetLogger(typeof(GatewayProfileHandler));
     }
 }
--- src/NServiceBus.Hosting.Windows/Profiles/MultiSite.cs
+++ src/NServiceBus.Hosting.Windows/Profiles/MultiSite.cs
@@ -3,6 +3,7 @@
     /// <summary>
     /// Indicates that this node has a Gateway access to it.
     /// </summary>
+    [ObsoleteEx(RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0", Replacement = "Feature.Enable<Gateway>")]
     public interface MultiSite : IProfile
     {
     }
