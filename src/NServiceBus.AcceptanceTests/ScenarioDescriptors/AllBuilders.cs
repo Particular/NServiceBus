57b3f710ea1fe6e36b31db0f7afa8ce93aec82a5
John Simons <john.simons@nservicebus.com>
2014-03-20 15:39:16 +1000

    Separated  acceptance test builders to allow testing per builder

--- src/NServiceBus.AcceptanceTests/ScenarioDescriptors/AllBuilders.cs
+++ src/NServiceBus.AcceptanceTests/ScenarioDescriptors/AllBuilders.cs
@@ -1,28 +1,17 @@
 ﻿namespace NServiceBus.AcceptanceTests.ScenarioDescriptors
 {
-    using System.Collections.Generic;
-    using System.Linq;
     using AcceptanceTesting.Support;
-    using ObjectBuilder.Common;
 
     public class AllBuilders : ScenarioDescriptor
     {
         public AllBuilders()
         {
-            var builders = TypeScanner.GetAllTypesAssignableTo<IContainer>()
-                .Where(t => !t.Assembly.FullName.StartsWith("NServiceBus.Core") && //exclude the default builder
-                   t.Name.EndsWith("ObjectBuilder") ) //exclude the ninject child container 
-                .ToList();
-
-            foreach (var builder in builders)
-            {
-                var name = builder.Name.Replace("ObjectBuilder", "");
-                Add(new RunDescriptor
-                {
-                    Key = name,
-                    Settings = new Dictionary<string, string> { { "Builder", builder.AssemblyQualifiedName } }
-                });
-            }
+            Add(Builders.Autofac);
+            Add(Builders.Windsor);
+            Add(Builders.Ninject);
+            Add(Builders.Spring);
+            Add(Builders.StructureMap);
+            Add(Builders.Unity);
         }
     }
 }
