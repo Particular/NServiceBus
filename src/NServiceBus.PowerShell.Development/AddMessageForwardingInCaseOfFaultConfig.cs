dc9abd5470a3202c3f0e20bb836e8ab7bc4c8e02
Andreas Öhlund <andreas.ohlund@particular.net>
2014-03-24 11:01:33 +0100

    Adding a new project for development only powershells

---
+++ src/NServiceBus.PowerShell.Development/AddMessageForwardingInCaseOfFaultConfig.cs
@@ -0,0 +1,36 @@
+﻿namespace NServiceBus.PowerShell
+{
+    using System.Management.Automation;
+    using System.Xml.Linq;
+    using System.Xml.XPath;
+
+    [Cmdlet(VerbsCommon.Add, "NServiceBusMessageForwardingInCaseOfFaultConfig")]
+    public class AddMessageForwardingInCaseOfFaultConfig : AddConfigSection
+    {
+        public override void ModifyConfig(XDocument doc)
+        {
+            const string Instructions = @"<MessageForwardingInCaseOfFaultConfig 
+    ErrorQueue=""The queue to which errors will be forwarded."" />";
+
+            var sectionElement = doc.XPathSelectElement("/configuration/configSections/section[@name='MessageForwardingInCaseOfFaultConfig' and @type='NServiceBus.Config.MessageForwardingInCaseOfFaultConfig, NServiceBus.Core']");
+            if (sectionElement == null)
+            {
+
+                doc.XPathSelectElement("/configuration/configSections").Add(new XElement("section",
+                                                                                         new XAttribute("name",
+                                                                                                        "MessageForwardingInCaseOfFaultConfig"),
+                                                                                         new XAttribute("type",
+                                                                                                        "NServiceBus.Config.MessageForwardingInCaseOfFaultConfig, NServiceBus.Core")));
+
+            }
+
+            var forwardingElement = doc.XPathSelectElement("/configuration/MessageForwardingInCaseOfFaultConfig");
+            if (forwardingElement == null)
+            {
+                doc.Root.LastNode.AddAfterSelf(new XComment(Instructions), 
+                                                new XElement("MessageForwardingInCaseOfFaultConfig",
+                                                new XAttribute("ErrorQueue", "error")));
+            }
+        }
+    }
+}
