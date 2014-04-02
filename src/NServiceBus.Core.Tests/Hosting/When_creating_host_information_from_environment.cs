498fa1355ec5023671d691d64d736b4299f433fb
Yves Goeleven <yves@goeleven.com>
2014-04-02 10:02:11 +0200

    Incorporated all comments on https://github.com/Particular/NServiceBus/commit/8093ce907a3d558ecb59f20ef3be66593dbf11a4

--- src/NServiceBus.Core.Tests/Hosting/When_creating_host_information_from_environment.cs
+++ src/NServiceBus.Core.Tests/Hosting/When_creating_host_information_from_environment.cs
@@ -1,4 +1,4 @@
-namespace NServiceBus.MessageInterfaces.Tests
+namespace NServiceBus.Hosting.Tests
 {
     using System;
     using Hosting;
@@ -7,53 +7,53 @@ namespace NServiceBus.MessageInterfaces.Tests
     [TestFixture]
     public class When_creating_host_information_from_environment
     {
-        HostInformation information1;
-        HostInformation information2;
-        HostInformation information3;
-        HostInformation information4;
-
-        [SetUp]
-        public void SetUp()
-        {
-            information1 = HostInformation.CreateHostInformation("\"pathto\\mysuperduper.exe\" somevar", "MyMachine");
-            information2 = HostInformation.CreateHostInformation("pathto\\mysuperduper.exe somevar", "MyMachine");
-            information3 = HostInformation.CreateHostInformation("\"pathto\\mysuper duper.exe\" \"somevar with spaces\"", "MyMachine");
-            information4 = HostInformation.CreateHostInformation("\"pathto\\mysuper duper.exe\" somevar", "MyMachine");
-        }
-
         [Test]
         public void HostId_is_parsed_from_path_without_spaces_but_with_quotes()
         {
-            Assert.IsTrue(information1.HostId != default(Guid));
+            var information1 = HostInformation.CreateHostInformation("\"pathto\\mysuperduper.exe\" somevar", "MyMachine");
+
+            Assert.IsTrue(information1.HostId == Guid.Parse("{8dd7bbcc-dfc3-d84a-41ac-82c14f1ed7db}"));
         }
 
         [Test]
         public void HostId_is_parsed_from_path_without_spaces_but_without_quotes()
         {
-            Assert.IsTrue(information2.HostId != default(Guid));
+            var information2 = HostInformation.CreateHostInformation("pathto\\mysuperduper.exe somevar", "MyMachine");
+
+            Assert.IsTrue(information2.HostId == Guid.Parse("{8dd7bbcc-dfc3-d84a-41ac-82c14f1ed7db}"));
         }
 
         [Test]
         public void Both_hostIds_for_paths_without_spaces_are_equal()
         {
+            var information1 = HostInformation.CreateHostInformation("\"pathto\\mysuperduper.exe\" somevar", "MyMachine");
+            var information2 = HostInformation.CreateHostInformation("pathto\\mysuperduper.exe somevar", "MyMachine");
+
             Assert.IsTrue(information1.HostId == information2.HostId);
         }
 
         [Test]
         public void HostId_is_parsed_from_path_with_spaces_having_a_parameter_with_spaces()
         {
-            Assert.IsTrue(information3.HostId != default(Guid));
+            var information3 = HostInformation.CreateHostInformation("\"pathto\\mysuper duper.exe\" \"somevar with spaces\"", "MyMachine");
+
+            Assert.IsTrue(information3.HostId == Guid.Parse("{db3ff7ff-508f-cce7-0a8b-0092a7d750b9}"));
         }
 
         [Test]
         public void HostId_is_parsed_from_path_with_spaces_having_a_parameter_without_spaces()
         {
-            Assert.IsTrue(information4.HostId != default(Guid));
+            var information4 = HostInformation.CreateHostInformation("\"pathto\\mysuper duper.exe\" somevar", "MyMachine");
+
+            Assert.IsTrue(information4.HostId == Guid.Parse("{db3ff7ff-508f-cce7-0a8b-0092a7d750b9}"));
         }
 
         [Test]
         public void Both_hostIds_for_paths_with_spaces_are_equal()
         {
+            var information3 = HostInformation.CreateHostInformation("\"pathto\\mysuper duper.exe\" \"somevar with spaces\"", "MyMachine");
+            var information4 = HostInformation.CreateHostInformation("\"pathto\\mysuper duper.exe\" somevar", "MyMachine");
+
             Assert.IsTrue(information3.HostId == information4.HostId);
         }
 
