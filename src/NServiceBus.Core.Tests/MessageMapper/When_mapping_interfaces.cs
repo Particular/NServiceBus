42096b25dd4ffa767a2fdbe04a1e52cddeebeb93
Simon Cropp <simon.cropp@gmail.com>
2014-04-21 20:05:09 +1000

    refactor message mapper tests

--- src/NServiceBus.Core.Tests/MessageMapper/When_mapping_interfaces.cs
+++ src/NServiceBus.Core.Tests/MessageMapper/When_mapping_interfaces.cs
@@ -1,48 +1,63 @@
-namespace NServiceBus.MessageInterfaces.Tests
+namespace MessageMapperTests
 {
     using System;
+    using NServiceBus;
+    using NServiceBus.MessageInterfaces.MessageMapper.Reflection;
     using NUnit.Framework;
 
     [TestFixture]
     public class When_mapping_interfaces
     {
-        IMessageMapper mapper;
-
-        [SetUp]
-        public void SetUp()
-        {
-            mapper = new MessageMapper.Reflection.MessageMapper();
-        }
 
         [Test]
         public void Interfaces_with_only_properties_should_be_mapped()
         {
-            mapper.Initialize(new[] { typeof(InterfaceWithProperties) });
+            var mapper = new MessageMapper();
+            mapper.Initialize(new[] { typeof(InterfaceWithOnlyProperties) });
 
-            Assert.NotNull(mapper.GetMappedTypeFor(typeof(InterfaceWithProperties)));
+            Assert.NotNull(mapper.GetMappedTypeFor(typeof(InterfaceWithOnlyProperties)));
+        }
+
+        public interface InterfaceWithOnlyProperties : IMessage
+        {
+            string SomeProperty { get; set; }
         }
 
         [Test]
         public void Interface_should_be_created()
         {
-            mapper.Initialize(new[] { typeof(InterfaceWithProperties) });
+            var mapper = new MessageMapper();
+            mapper.Initialize(new[] { typeof(Interface) });
 
-            var result = mapper.CreateInstance<InterfaceWithProperties>(null);
+            var result = mapper.CreateInstance<Interface>(null);
 
             Assert.IsNotNull(result);
+        }
+
+        public interface Interface : IMessage
+        {
         }
 
         [Test]
         public void Interfaces_with_methods_should_be_ignored()
         {
-            mapper.Initialize(new[] {typeof(InterfaceWithMethods)});
+            var mapper = new MessageMapper();
+            mapper.Initialize(new[] { typeof(InterfaceWithMethods) });
 
             Assert.Null(mapper.GetMappedTypeFor(typeof(InterfaceWithMethods)));
         }
+
+        public interface InterfaceWithMethods
+        {
+            string SomeProperty { get; set; }
+            void MethodOnInterface();
+        }
+
         
         [Test]
         public void Attributes_on_properties_should_be_mapped()
         {
+            var mapper = new MessageMapper();
             mapper.Initialize(new[]{typeof(InterfaceWithPropertiesAndAttributes)});
             Assert.IsTrue(PropertyContainsAttribute("SomeProperty",typeof(SomeAttribute),mapper.CreateInstance(typeof(InterfaceWithPropertiesAndAttributes))));
             
@@ -50,9 +65,22 @@ namespace NServiceBus.MessageInterfaces.Tests
             Assert.IsFalse(PropertyContainsAttribute("SomeOtherProperty", typeof(SomeAttribute), mapper.CreateInstance(typeof(InterfaceWithPropertiesAndAttributes))));
         }
 
+        public interface InterfaceWithPropertiesAndAttributes
+        {
+            [Some]
+            string SomeProperty { get; set; }
+            string SomeOtherProperty { get; set; }
+        }
+
+        public class SomeAttribute : Attribute
+        {
+        }
+
+
         [Test]
         public void Accept_Attributes_with_no_default_ctor_as_long_as_the_parameter_in_constructor_has_the_same_name_as_the_property()
         {
+            var mapper = new MessageMapper();
             mapper.Initialize(new[] { typeof(InterfaceWithCustomAttributeThatHasNoDefaultConstructor) });
             var instance = mapper.CreateInstance(typeof (InterfaceWithCustomAttributeThatHasNoDefaultConstructor));
             var attributes = instance.GetType().GetProperty("SomeProperty").GetCustomAttributes(typeof(CustomAttributeWithNoDefaultConstructor),true);
@@ -60,35 +88,97 @@ namespace NServiceBus.MessageInterfaces.Tests
             Assert.AreEqual(attr.Name, "Blah");
         }
 
+        public interface InterfaceWithCustomAttributeThatHasNoDefaultConstructor
+        {
+            [CustomAttributeWithNoDefaultConstructor("Blah")]
+            string SomeProperty { get; set; }
+        }
+
+        public class CustomAttributeWithNoDefaultConstructor : Attribute
+        {
+            public string Name { get; set; }
+            public CustomAttributeWithNoDefaultConstructor(string name)
+            {
+                Name = name;
+            }
+        }
 
         [Test]
         public void Accept_Attributes_with_no_default_ctor_while_ctor_parameters_are_different_than_properties_of_custom_attribute()
         {
+            var mapper = new MessageMapper();
             mapper.Initialize(new[] { typeof(InterfaceWithCustomAttributeThatHasNoDefaultConstructorAndNoMatchingParameters) });
             var instance = mapper.CreateInstance(typeof(InterfaceWithCustomAttributeThatHasNoDefaultConstructorAndNoMatchingParameters));
             var attributes = instance.GetType().GetProperty("SomeProperty").GetCustomAttributes(typeof(CustomAttributeWithNoDefaultConstructorAndNoMatchingParameters), true);
             var attr = (CustomAttributeWithNoDefaultConstructorAndNoMatchingParameters)attributes[0];
             Assert.AreEqual(attr.Name, "Blah");
         }
+        public interface InterfaceWithCustomAttributeThatHasNoDefaultConstructorAndNoMatchingParameters
+        {
+            [CustomAttributeWithNoDefaultConstructorAndNoMatchingParameters("Blah", "Second Blah")]
+            string SomeProperty { get; set; }
+        }
 
+
+        /// <summary>
+        /// Break the heuristics of finding the properties from constructor parameter names
+        /// </summary>
+        public class CustomAttributeWithNoDefaultConstructorAndNoMatchingParameters : Attribute
+        {
+            public string Name { get; set; }
+            public CustomAttributeWithNoDefaultConstructorAndNoMatchingParameters(string someParam, string someOtherParam)
+            {
+                Name = someParam;
+            }
+        }
 
         [Test]
         public void Generated_type_should_preserve_namespace_to_make_it_easier_for_users_to_define_custom_conventions()
         {
-            mapper.Initialize(new[] { typeof(InterfaceWithProperties) });
+            var mapper = new MessageMapper();
+            mapper.Initialize(new[] { typeof(InterfaceToGenerate) });
 
-            Assert.AreEqual(typeof(InterfaceWithProperties).Namespace, mapper.CreateInstance(typeof(InterfaceWithProperties)).GetType().Namespace);
+            Assert.AreEqual(typeof(InterfaceToGenerate).Namespace, mapper.CreateInstance(typeof(InterfaceToGenerate)).GetType().Namespace);
+        }
+
+        public interface InterfaceToGenerate : IMessage
+        {
+            string SomeProperty { get; set; }
         }
 
         [Test]
         public void Accept_attributes_with_value_attribute()
         {
+            var mapper = new MessageMapper();
             mapper.Initialize(new[] { typeof(IMyEventWithAttributeWithBoolProperty) });
             var instance = mapper.CreateInstance(typeof(IMyEventWithAttributeWithBoolProperty));
             var attributes = instance.GetType().GetProperty("EventId").GetCustomAttributes(typeof(CustomAttributeWithValueProperties), true);
             var attr = attributes[0] as CustomAttributeWithValueProperties;
             Assert.AreEqual(attr != null && attr.FlagIsSet, true);
             if (attr != null) Assert.AreEqual(attr.MyAge, 21);
+        }
+
+        public interface IMyEventWithAttributeWithBoolProperty
+        {
+            [CustomAttributeWithValueProperties("bla bla", true, 21)]
+            Guid EventId { get; set; }
+        }
+        public class CustomAttributeWithValueProperties : Attribute
+        {
+            public CustomAttributeWithValueProperties() { }
+
+            public CustomAttributeWithValueProperties(string name, bool flag, int age)
+            {
+                Name = name;
+                FlagIsSet = flag;
+                MyAge = age;
+            }
+
+            public string Name { get; set; }
+
+            public bool FlagIsSet { get; set; }
+
+            public int MyAge { get; set; }
         }
         
         private bool PropertyContainsAttribute(string propertyName, Type attributeType, object obj)
@@ -97,82 +187,8 @@ namespace NServiceBus.MessageInterfaces.Tests
         }
     }
 
-    public interface InterfaceWithProperties : IMessage
-    {
-        string SomeProperty { get; set; }
-    }
 
-    public interface InterfaceWithMethods
-    {
-        string SomeProperty { get; set; }
-        void MethodOnInterface();
-    }
 
-    public interface InterfaceWithPropertiesAndAttributes
-    {
-        [Some]
-        string SomeProperty { get; set; }
-        
-        string SomeOtherProperty { get; set; }
-    }
 
-    public interface InterfaceWithCustomAttributeThatHasNoDefaultConstructor
-    {
-        [CustomAttributeWithNoDefaultConstructor("Blah")]
-        string SomeProperty { get; set; }
-    }
-
-    public class SomeAttribute : Attribute
-    {
-        
-    }
-
-    public class CustomAttributeWithNoDefaultConstructor : Attribute
-    {
-        public string Name { get; set; }
-        public CustomAttributeWithNoDefaultConstructor(string name)
-        {
-            Name = name;
-        }
-    }
-
-    /// <summary>
-    /// Break the heuristics of finding the properties from constructor parameter names
-    /// </summary>
-    public class CustomAttributeWithNoDefaultConstructorAndNoMatchingParameters : Attribute
-    {
-        public string Name { get; set; }
-        public CustomAttributeWithNoDefaultConstructorAndNoMatchingParameters(string someParam, string someOtherParam)
-        {
-            Name = someParam;
-        }
-    }
-    public interface InterfaceWithCustomAttributeThatHasNoDefaultConstructorAndNoMatchingParameters
-    {
-        [CustomAttributeWithNoDefaultConstructorAndNoMatchingParameters("Blah", "Second Blah")]
-        string SomeProperty { get; set; }
-    }
     
-    public class CustomAttributeWithValueProperties : Attribute
-    {
-        public CustomAttributeWithValueProperties(){}
-
-        public CustomAttributeWithValueProperties(string name, bool flag, int age)
-        {
-            Name = name;
-            FlagIsSet = flag;
-            MyAge = age;
-        }
-
-        public string Name { get; set; }
-
-        public bool FlagIsSet { get; set; }
-
-        public int MyAge { get; set; }
-    }
-    public interface IMyEventWithAttributeWithBoolProperty
-    {
-        [CustomAttributeWithValueProperties("bla bla", true, 21)]
-        Guid EventId { get; set; }
-    }
 }
--- src/NServiceBus.Core.Tests/MessageMapper/When_mapping_usinggenerics.cs
+++ src/NServiceBus.Core.Tests/MessageMapper/When_mapping_usinggenerics.cs
@@ -1,45 +1,62 @@
-namespace NServiceBus.MessageInterfaces.Tests
+namespace MessageMapperTests
 {
+    using NServiceBus;
+    using NServiceBus.MessageInterfaces.MessageMapper.Reflection;
     using NUnit.Framework;
 
     [TestFixture]
     public class When_mapping_usinggenerics
     {
-        IMessageMapper mapper;
-
-        [SetUp]
-        public void SetUp()
-        {
-            mapper = new MessageMapper.Reflection.MessageMapper();
-        }
 
         [Test]
         public void Class_abstract_generic_with_only_properties_generic_should_not_be_mapped()
         {
+            var mapper = new MessageMapper();
             var genericClassType = typeof(GenericAbstractCommand<>);
             mapper.Initialize(new[] { genericClassType });
             Assert.Null(mapper.GetMappedTypeFor(genericClassType));
+        }
+        public abstract class GenericAbstractCommand<T> : IMessage where T : Data
+        {
+            public T DataToTransfer { get; set; }
         }
 
         [Test]
         public void Class_generic_with_only_properties_generic_should_not_be_mapped()
         {
+            var mapper = new MessageMapper();
             var abstractClassType = typeof(GenericCommand<>);
             mapper.Initialize(new[] { abstractClassType });
             Assert.Null(mapper.GetMappedTypeFor(abstractClassType));
         }
 
+        public class GenericCommand<T> : IMessage where T : Data
+        {
+            public T DataToTransfer { get; set; }
+        }
+
+
         [Test]
         public void Class_derived_from_generic_with_only_properties_generic_should_be_mapped()
         {
+            var mapper = new MessageMapper();
             var abstractClassType = typeof(DerivedGenericCommand);
             mapper.Initialize(new[] { abstractClassType });
             Assert.NotNull(mapper.GetMappedTypeFor(abstractClassType));
+        }
+        public abstract class GenericBaseCommand<T> : IMessage where T : Data
+        {
+            public T DataToTransfer { get; set; }
+        }
+        public class DerivedGenericCommand : GenericBaseCommand<Data>
+        {
+
         }
 
         [Test]
         public void Class_concrete_generic_with_only_properties_generic_should_be_mapped()
         {
+            var mapper = new MessageMapper();
             var abstractClassType = typeof(GenericCommand<Data>);
             mapper.Initialize(new[] { abstractClassType });
             Assert.NotNull(mapper.GetMappedTypeFor(abstractClassType));
@@ -48,43 +65,74 @@ namespace NServiceBus.MessageInterfaces.Tests
         [Test]
         public void Class_abstract_with_only_properties_should_be_mapped()
         {
+            var mapper = new MessageMapper();
             var abstractClassType = typeof(SimpleAbstractClass);
             mapper.Initialize(new[] { abstractClassType });
             Assert.NotNull(mapper.GetMappedTypeFor(abstractClassType));
+        }
+        public abstract class SimpleAbstractClass : IMessage
+        {
+            string SomeProperty { get; set; }
         }
 
         [Test,Ignore]
         public void Class_abstract_with_methods_should_not_be_mapped()
         {
+            var mapper = new MessageMapper();
             var abstractClassType = typeof(SimpleAbstractClassWithMethods);
             mapper.Initialize(new[] { abstractClassType });
             Assert.Null(mapper.GetMappedTypeFor(abstractClassType));
+        }
+        public abstract class SimpleAbstractClassWithMethods : IMessage
+        {
+            string SomeProperty { get; set; }
+            protected abstract void DoTest();
         }
 
         [Test]
         public void Interfaces_generic_with_only_properties_should_not_be_mapped()
         {
+            var mapper = new MessageMapper();
             var genericInterfaceType = typeof(InterfaceGenericWithProperties<>);
             mapper.Initialize(new[] { genericInterfaceType });
             Assert.Null(mapper.GetMappedTypeFor(genericInterfaceType));
         }
 
+        public interface InterfaceGenericWithProperties<T> : IMessage where T : Data
+        {
+            string SomeProperty { get; set; }
+            T MessageProperty { get; set; }
+        }
+
         [Test]
         public void Interfaces_generic_with_methods_should_not_be_mapped()
         {
+            var mapper = new MessageMapper();
             var genericInterfaceType = typeof(InterfaceGenericWithMethods<>);
             mapper.Initialize(new[] { genericInterfaceType });
             Assert.Null(mapper.GetMappedTypeFor(genericInterfaceType));
         }
+        public interface InterfaceGenericWithMethods<in T> : IMessage where T : Data
+        {
+            string SomeProperty { get; set; }
+            void MethodOnInterface(T myMessage);
+        }
+
+
 
         [Test]
         public void Interfaces_with_only_properties_should_be_mapped()
         {
-            var simpleInterfaceType = typeof(InterfaceWithProperties);
+            var mapper = new MessageMapper();
+            var simpleInterfaceType = typeof(InterfaceWithOnlyProperties);
             mapper.Initialize(new[] { simpleInterfaceType });
             Assert.NotNull(mapper.GetMappedTypeFor(simpleInterfaceType));
         }
 
+        public interface InterfaceWithOnlyProperties : IMessage
+        {
+            string SomeProperty { get; set; }
+        }
     }
 
     public abstract class Data
@@ -92,46 +140,8 @@ namespace NServiceBus.MessageInterfaces.Tests
         
     }
 
-    public interface InterfaceGenericWithProperties<T> : IMessage where T : Data
-    {
-        string SomeProperty { get; set; }
-        T MessageProperty { get; set; }
-    }
 
-    public interface InterfaceGenericWithMethods<in T> : IMessage where T : Data
-    {
-        string SomeProperty { get; set; }
-        void MethodOnInterface(T myMessage);
-    }
 
-    public abstract class GenericAbstractCommand<T> : IMessage where T : Data
-    {
-        public T DataToTransfer { get; set; }
-    }
 
-    public class GenericCommand<T> : IMessage where T : Data
-    {
-        public T DataToTransfer { get; set; }
-    }
 
-    public class DerivedGenericCommand : GenericAbstractCommand<Data>
-    {
-        
-    }
-
-    public interface InterfaceSimplWithProperties : IMessage
-    {
-        string SomeProperty { get; set; }
-    }
-
-    public abstract class SimpleAbstractClass : IMessage
-    {
-        string SomeProperty { get; set; }
-    }
-
-    public abstract class SimpleAbstractClassWithMethods : IMessage
-    {
-        string SomeProperty { get; set; }
-        protected abstract void DoTest();
-    }
 }
