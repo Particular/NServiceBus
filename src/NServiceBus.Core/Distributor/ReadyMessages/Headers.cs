a7e0056f6a29eabbde7806c224c83be27da823b1
Simon Cropp <simon.cropp@gmail.com>
2014-05-02 20:03:27 +1000

    internalise ReadyMessages.headers

--- src/NServiceBus.Core/Distributor/ReadyMessages/Headers.cs
+++ src/NServiceBus.Core/Distributor/ReadyMessages/Headers.cs
@@ -1,7 +1,6 @@
 namespace NServiceBus.Distributor.ReadyMessages
 {
-    [ObsoleteEx(Message = "Not a public API.", TreatAsErrorFromVersion = "4.3", RemoveInVersion = "5.0")]    
-    public struct Headers
+    static class Headers
     {
         public static string WorkerCapacityAvailable = "NServiceBus.Distributor.WorkerCapacityAvailable";
         public static string WorkerStarting = "NServiceBus.Distributor.WorkerStarting";
