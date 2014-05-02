42a46d138c807736d504af5cec2ec23575d44861
Simon Cropp <simon.cropp@gmail.com>
2014-05-02 19:17:44 +1000

    remove RunTimeoutManagerWithInMemoryPersistence

--- src/NServiceBus.Core/Timeout/ConfigureTimeoutManager.cs
+++ src/NServiceBus.Core/Timeout/ConfigureTimeoutManager.cs
@@ -38,17 +38,6 @@ namespace NServiceBus
             Feature.Disable<TimeoutManager>();
             return config;
         }
-        
-        /// <summary>
-        /// Sets the default persistence to InMemory.
-        /// </summary>
-        [ObsoleteEx(Replacement = "UseInMemoryTimeoutPersister()", TreatAsErrorFromVersion = "4.0", RemoveInVersion = "5.0")]
-        public static Configure RunTimeoutManagerWithInMemoryPersistence(this Configure config)
-        {
-            Feature.Enable<TimeoutManager>();
-
-            return UseInMemoryTimeoutPersister(config);
-        }
 
         /// <summary>
         /// Sets the default persistence to InMemory.
