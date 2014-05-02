aa0795c21de0aa1358722dbe4d0ce23a22c6e6df
Simon Cropp <simon.cropp@gmail.com>
2014-05-02 20:02:42 +1000

    internalise WorkerQueueCreator

--- src/NServiceBus.Core/Distributor/MasterNode/AdjustSettingsForNonMasterNodes.cs
+++ src/NServiceBus.Core/Distributor/MasterNode/AdjustSettingsForNonMasterNodes.cs
@@ -2,7 +2,6 @@ namespace NServiceBus.MasterNode
 {
     using Settings;
 
-    [ObsoleteEx(Message = "Not a public API.", TreatAsErrorFromVersion = "4.3", RemoveInVersion = "5.0")]
     class AdjustSettingsForNonMasterNodes : IWantToRunBeforeConfigurationIsFinalized
     {
         public void Run()
--- src/NServiceBus.Core/Distributor/QueueCreators/WorkerQueueCreator.cs
+++ src/NServiceBus.Core/Distributor/QueueCreators/WorkerQueueCreator.cs
@@ -6,8 +6,7 @@ namespace NServiceBus.Distributor.QueueCreators
     /// <summary>
     /// Signal to create the queue for a worker
     /// </summary>
-    [ObsoleteEx(Message = "Not a public API.", TreatAsErrorFromVersion = "4.3", RemoveInVersion = "5.0")]    
-    public class WorkerQueueCreator : IWantQueueCreated
+    class WorkerQueueCreator : IWantQueueCreated
     {
         /// <summary>
         /// Address of worker queue
