875ce0ed9b1c9b31e54e6efbbfade18c59d4320a
Simon Cropp <simon.cropp@gmail.com>
2014-05-02 17:59:16 +1000

    remove DefineLocalAddressNameFunc

--- src/NServiceBus.Core/Config/Advanced/ConfigureSettingLocalAddressNameAction.cs
+++ src/NServiceBus.Core/Config/Advanced/ConfigureSettingLocalAddressNameAction.cs
@@ -1,26 +1,3 @@
-namespace NServiceBus.Config.Advanced
-{
-    using System;
-
-    /// <summary>
-    /// Allow overriding local address name.
-    /// </summary>
-    [ObsoleteEx(Message = "Moved to NServiceBus namespace.", RemoveInVersion = "5.0", TreatAsErrorFromVersion = "4.0")]
-    public static class ConfigureSettingLocalAddressNameAction
-    {
-        /// <summary>
-        /// Set a function that overrides the default naming of NServiceBus local addresses.
-        /// See: <a href="http://particular.net/articles/how-to-specify-your-input-queue-name">Here</a> for more details.
-        /// </summary>
-        [ObsoleteEx(Message = "Moved to NServiceBus namespace.", RemoveInVersion = "5.0", TreatAsErrorFromVersion = "4.0")]
-        public static Configure DefineLocalAddressNameFunc(this Configure config, Func<string> setLocalAddressNameFunc)
-        {
-            NServiceBus.ConfigureSettingLocalAddressNameAction.defineLocalAddressNameFunc = setLocalAddressNameFunc;
-            return config;
-        }
-    }
-}
-
 namespace NServiceBus
 {
     using System;
