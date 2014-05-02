f4be32ca13423eb895df8b668594e4e19644e6c3
Simon Cropp <simon.cropp@gmail.com>
2014-05-02 20:51:17 +1000

    remove OnCriticalError and DefineCriticalErrorAction

--- src/NServiceBus.Core/ConfigureCriticalErrorAction.cs
+++ src/NServiceBus.Core/ConfigureCriticalErrorAction.cs
@@ -8,7 +8,7 @@ namespace NServiceBus
     /// </summary>
     public static class ConfigureCriticalErrorAction
     {
-        private static Action<string, Exception> onCriticalErrorAction = (errorMessage, exception) =>
+        static Action<string, Exception> onCriticalErrorAction = (errorMessage, exception) =>
             {
                 if (!Configure.BuilderIsConfigured())
                     return;
@@ -26,8 +26,7 @@ namespace NServiceBus
         /// <param name="config">The configuration object.</param>
         /// <param name="onCriticalError">Assigns the action to perform on critical error.</param>
         /// <returns>The configuration object.</returns>
-        public static Configure DefineCriticalErrorAction(this Configure config,
-                                                          Action<string, Exception> onCriticalError)
+        public static Configure DefineCriticalErrorAction(this Configure config, Action<string, Exception> onCriticalError)
         {
             onCriticalErrorAction = onCriticalError;
             return config;
@@ -46,28 +45,5 @@ namespace NServiceBus
             onCriticalErrorAction(errorMessage, exception);
         }
 
-        /// <summary>
-        /// Sets the function to be used when critical error occurs
-        /// </summary>
-        /// <param name="config">The configuration object.</param>
-        /// <param name="onCriticalError">Assigns the action to perform on critical error.</param>
-        /// <returns>The configuration object.</returns>
-        [ObsoleteEx(Replacement = "DefineCriticalErrorAction(this Configure config, Action<string, Exception> onCriticalError)", TreatAsErrorFromVersion = "4.0", RemoveInVersion = "5.0")]
-        public static Configure DefineCriticalErrorAction(this Configure config, Action onCriticalError)
-        {
-            onCriticalErrorAction = (s, exception) => onCriticalError();
-            return config;
-        }
-        /// <summary>
-        /// Execute the configured Critical error action
-        /// </summary>
-        /// <param name="config">The configuration object.</param>
-        /// <returns>The configuration object.</returns>
-        [ObsoleteEx(Replacement = "RaiseCriticalError(this Configure config, string errorMessage, Exception exception)", TreatAsErrorFromVersion = "4.0", RemoveInVersion = "5.0")]
-        public static Configure OnCriticalError(this Configure config)
-        {
-            onCriticalErrorAction("A critical error occurred.", new Exception());
-            return config;
-        }
     }
 }
