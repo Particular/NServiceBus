9d472f748357c4be6eda6cb1f18eadef89dc6b0d
synhershko <itamar@code972.com>
2014-05-04 20:55:09 +0300

    Closes #2067
    Fixes deserialization issues with the KeyValuePair type when it is used outside of dictionaries

---
+++ src/NServiceBus.Core.Tests/Serializers/XML/Issue2067.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NServiceBus.Core.Tests.Serializers.XML
+{
+    using NServiceBus.Serializers.XML.Test;
+    using NUnit.Framework;
+
+    [Serializable]
+    class MyMessage
+    {
+        public KeyValuePair<string, string>[] FailureArgs { get; set; }
+    }
+
+    [TestFixture]
+    public class Issue2067
+    {
+
+        [Test]
+        public void CanSerializeNullElements()
+        {
+            var message = new MyMessage
+                          {
+                              FailureArgs = new[]
+                                            {
+                                                new KeyValuePair<string, string>("Arg0", "Value0"),
+                                                new KeyValuePair<string, string>("Arg0", "Value0")
+                                            }
+                          };
+
+            var result = ExecuteSerializer.ForMessage<MyMessage>(message);
+            Assert.IsNotNull(result.FailureArgs);
+            Assert.AreEqual(2, result.FailureArgs.Count());
+        }
+    }
+}
