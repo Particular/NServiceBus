19d7a5d4c7850b06c2d0e65f3831f10bb42c38aa
Simon Cropp <simon.cropp@gmail.com>
2014-05-16 18:17:25 +1000

    move generichost into into host exe
    fixes #1955

--- src/NServiceBus.Hosting.Windows/GenericHost.cs
+++ src/NServiceBus.Hosting.Windows/GenericHost.cs
@@ -14,18 +14,9 @@ namespace NServiceBus.Hosting
     using Utils;
     using Wcf;
 
-    /// <summary>
-    ///     A generic host that can be used to provide hosting services in different environments
-    /// </summary>
-    public class GenericHost : IHost
+    class GenericHost 
     {
-        /// <summary>
-        ///     Accepts the type which will specify the users custom configuration.
-        ///     This type should implement <see cref="IConfigureThisEndpoint" />.
-        /// </summary>
-        /// <param name="scannableAssembliesFullName">Assemblies full name that were scanned.</param>
-        public GenericHost(IConfigureThisEndpoint specifier, string[] args, List<Type> defaultProfiles,
-            string endpointName, IEnumerable<string> scannableAssembliesFullName = null)
+        public GenericHost(IConfigureThisEndpoint specifier, string[] args, List<Type> defaultProfiles, string endpointName, IEnumerable<string> scannableAssembliesFullName = null)
         {
             this.specifier = specifier;
 
