168ba35a82ddda2e10570f9ccda5d1f29dbd10de
Simon Cropp <simon.cropp@gmail.com>
2014-05-19 22:19:32 +1000

    missing file
    stupid VS

--- src/NServiceBus.Core/Unicast/Transport/ControlMessage.cs
+++ src/NServiceBus.Core/Unicast/Transport/ControlMessage.cs
@@ -1,7 +1,5 @@
 namespace NServiceBus.Unicast.Transport
 {
-    using Messages;
-
     /// <summary>
     /// Helper for creating control messages
     /// </summary>
@@ -21,28 +19,6 @@ namespace NServiceBus.Unicast.Transport
             transportMessage.Headers.Add(Headers.ControlMessageHeader, true.ToString());
 
             return transportMessage;
-        }
-    }
-
-    /// <summary>
-    /// Extensions to make the usage if control messages easier
-    /// </summary>
-    public static class TransportMessageExtensions
-    {
-        /// <summary>
-        /// True if the transport message is a control message
-        /// </summary>
-        public static bool IsControlMessage(this TransportMessage transportMessage)
-        {
-            return transportMessage.Headers != null &&
-                   transportMessage.Headers.ContainsKey(Headers.ControlMessageHeader);
-        }
-
-
-        public static bool IsControlMessage(this LogicalMessage transportMessage)
-        {
-            return transportMessage.Headers != null &&
-                   transportMessage.Headers.ContainsKey(Headers.ControlMessageHeader);
         }
     }
 }
