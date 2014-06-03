46766361bc44ca5d48a1c8f717c9cdf7061a1368
Simon Cropp <simon.cropp@gmail.com>
2014-06-03 15:00:36 +1000

    more doco

--- src/NServiceBus.Core/Transports/ConfigureTransport.cs
+++ src/NServiceBus.Core/Transports/ConfigureTransport.cs
@@ -10,6 +10,9 @@ namespace NServiceBus.Transports
     /// <typeparam name="T">The <see cref="TransportDefinition"/> to configure.</typeparam>
     public abstract class ConfigureTransport<T> : Feature, IConfigureTransport<T> where T : TransportDefinition, new()
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="ConfigureTransport{T}"/>.
+        /// </summary>
         public void Configure(Configure config)
         {
             var connectionString = config.Settings.Get<TransportConnectionString>().GetConnectionStringOrNull();
@@ -22,18 +25,26 @@ namespace NServiceBus.Transports
 
             config.Settings.Set("NServiceBus.Transport.ConnectionString", connectionString);
 
-
-
             var selectedTransportDefinition = new T();
             config.Settings.Set("NServiceBus.Transport.SelectedTransport", selectedTransportDefinition);
             config.Configurer.RegisterSingleton<TransportDefinition>(selectedTransportDefinition);
             InternalConfigure(config);
         }
 
+        /// <summary>
+        /// Gives implementations access to the <see cref="Configure"/> instance at construction time.
+        /// </summary>
         protected abstract void InternalConfigure(Configure config);
 
+        /// <summary>
+        /// Used by implementations to provide an example connection string that till be used for the possible exception thrown if the <see cref="RequiresConnectionString"/> requirement is not met.
+        /// </summary>
         protected abstract string ExampleConnectionStringForErrorMessage { get; }
 
+        /// <summary>
+        /// Used by implementations to control if a connection string is necessary.
+        /// </summary>
+        /// <remarks>If this is true and a connection string is not returned by <see cref="TransportConnectionString.GetConnectionStringOrNull"/> then an exception will be thrown.</remarks>
         protected virtual bool RequiresConnectionString
         {
             get { return true; }
