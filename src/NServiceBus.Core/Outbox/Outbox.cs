d0a95c0c093822709b85b64ad9794d1e7eecfae5
John Simons <john.simons@nservicebus.com>
2014-06-03 15:52:51 +1000

    Added enabling restrictions to outbox
    Added warning if dtc is on

--- src/NServiceBus.Core/Outbox/Outbox.cs
+++ src/NServiceBus.Core/Outbox/Outbox.cs
@@ -1,17 +1,76 @@
 ﻿namespace NServiceBus.Features
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.ServiceProcess;
+    using Logging;
     using NServiceBus.Outbox;
     using Pipeline;
     using Transports;
 
     /// <summary>
-    /// Used to configure the outbox.
+    /// Configure the Outbox.
     /// </summary>
     public class Outbox : Feature
     {
-        
         internal Outbox()
         {
+            var rabbit = Type.GetType("NServiceBus.Features.RabbitMqTransport, NServiceBus.Transports.RabbitMQ", false);
+            var azureServiceBus = Type.GetType("NServiceBus.Features.AzureServiceBusTransport, NServiceBus.Azure.Transports.WindowsAzureServiceBus", false);
+            var azureStorageQueue = Type.GetType("NServiceBus.Features.AzureStorageQueueTransport, NServiceBus.Azure.Transports.WindowsAzureStorageQueues", false);
+
+            var dependencies = new List<Type>();
+
+            if (rabbit != null)
+            {
+                dependencies.Add(rabbit);
+            }
+            if (azureServiceBus != null)
+            {
+                dependencies.Add(azureServiceBus);
+            }
+            if (azureStorageQueue != null)
+            {
+                dependencies.Add(azureStorageQueue);
+            }
+
+            if (dependencies.Count > 0)
+            {
+                DependsOnAtLeastOne(dependencies.ToArray());
+                EnableByDefault();
+            }
+            else
+            {
+                var sql = Type.GetType("NServiceBus.Features.SqlServerTransport, NServiceBus.Transports.SQLServer", false);
+
+                if (sql != null)
+                {
+                    dependencies.Add(sql);                        
+                }
+
+                dependencies.Add(typeof(MsmqTransport));
+
+                DependsOnAtLeastOne(dependencies.ToArray());
+
+                Prerequisite(context =>
+                {
+                    var configValue = ConfigurationManager.AppSettings.Get("NServiceBus/Outbox");
+
+                    if (configValue == null)
+                    {
+                        return false;
+                    }
+                    
+                    bool result;
+
+                    Boolean.TryParse(configValue, out result);
+
+                    return result;
+                });
+            }
+
+            RegisterStartupTask<DtcRunningWarning>();
         }
 
         /// <summary>
@@ -49,5 +108,33 @@
                 InsertAfter(WellKnownBehavior.UnitOfWork);
             }
         }
+    }
+
+    class DtcRunningWarning :FeatureStartupTask
+    {
+        protected override void OnStart()
+        {
+            try
+            {
+                var sc = new ServiceController
+                {
+                    ServiceName = "MSDTC",
+                    MachineName = "."
+                };
+
+                if (sc.Status == ServiceControllerStatus.Running)
+                {
+                    log.Warn(@"We have detected that MSDTC service is running on your machine.
+Because you have configured this endpoint to run with Outbox enabled we recommend turning MSDTC off to ensure that the Outbox behavior is working as expected and no other resources are enlisting in distributed transactions.");
+                }
+            }
+            catch (Exception)
+            {
+                // Ignore if we can't check it.
+            }
+            
+        }
+
+        static ILog log = LogManager.GetLogger<DtcRunningWarning>();
     }
 }
