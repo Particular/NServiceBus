f0f6a401ba9c44a0806c94cc8908c3337b577fba
Andreas Öhlund <andreas.ohlund@particular.net>
2014-06-04 20:41:02 +0200

    Adding back UseInMemoryTimeoutPersister with an obsolete on to give users better guidance when upgrading

---
+++ src/NServiceBus.Core/Persistence/InMemory/Obsoletes/ConfigureInMemoryTimeoutPersister.cs
@@ -0,0 +1,13 @@
+﻿namespace NServiceBus
+{
+    using Persistence;
+
+    public static class ConfigureInMemoryTimeoutPersister
+    {
+        [ObsoleteEx(RemoveInVersion = "6", TreatAsErrorFromVersion = "5.1", Replacement = "config.UsePersistence<Persistence.InMemory>()")]
+        public static Configure UseInMemoryTimeoutPersister(this Configure config)
+        {
+            return config.UsePersistence<Persistence.InMemory>();
+        }
+    }
+}
