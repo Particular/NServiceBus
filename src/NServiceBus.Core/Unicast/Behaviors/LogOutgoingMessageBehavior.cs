aea9a076e8a2f58d46e9571ded8037612366369f
Andreas Öhlund <andreas.ohlund@particular.net>
2014-06-16 20:31:02 +0200

    Making the logging behavior more robust

--- src/NServiceBus.Core/Unicast/Behaviors/LogOutgoingMessageBehavior.cs
+++ src/NServiceBus.Core/Unicast/Behaviors/LogOutgoingMessageBehavior.cs
@@ -11,22 +11,23 @@
         public void Invoke(OutgoingContext context, Action next)
         {
             var options = context.DeliveryOptions as SendOptions;
-            if (options != null)
+
+            if (options != null && log.IsDebugEnabled && context.OutgoingLogicalMessage != null)
             {
                 var destination = options.Destination.ToString();
 
                 log.DebugFormat("Sending message '{0}' with id '{1}' to destination '{2}'.\n" +
                                 "ToString() of the message yields: {3}\n" +
                                 "Message headers:\n{4}",
-                    context.OutgoingLogicalMessage.MessageType.AssemblyQualifiedName,
+                                context.OutgoingLogicalMessage.MessageType != null ? context.OutgoingLogicalMessage.MessageType.AssemblyQualifiedName : "unknown",
                     context.OutgoingMessage.Id,
                     destination,
                     context.OutgoingLogicalMessage.Instance,
-                    string.Join(", ", context.OutgoingLogicalMessage.Headers.Select(h => h.Key + ":" + h.Value).ToArray()
-                        ));
+                    string.Join(", ", context.OutgoingLogicalMessage.Headers.Select(h => h.Key + ":" + h.Value).ToArray()));
             }
 
             next();
+
         }
 
         static ILog log = LogManager.GetLogger("LogOutgoingMessage");
