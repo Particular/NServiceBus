0ea5c06aaad967eb4f471df86c738c512d84eddc
Cameron MacFarland <distantcam@gmail.com>
2014-06-18 17:45:43 +0800

    Patching v3 to support v4+ unwrapped messages.

--- src/impl/Serializers/NServiceBus.Serializers.Json/JsonMessageSerializerBase.cs
+++ src/impl/Serializers/NServiceBus.Serializers.Json/JsonMessageSerializerBase.cs
@@ -1,16 +1,20 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.IO;
 using System.Runtime.Serialization.Formatters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using NServiceBus.MessageInterfaces;
 using NServiceBus.Serialization;
 using NServiceBus.Serializers.Json.Internal;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 
 namespace NServiceBus.Serializers.Json
 {
     public abstract class JsonMessageSerializerBase : IMessageSerializer
     {
+        // From v4+ we just look for it, but don't expect it
+        const string EnclosedMessageTypes = "NServiceBus.EnclosedMessageTypes";
+
         private readonly IMessageMapper _messageMapper;
 
         protected JsonMessageSerializerBase(IMessageMapper messageMapper)
@@ -26,7 +30,7 @@ namespace NServiceBus.Serializers.Json
                                              {
                                                  TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple,
                                                  TypeNameHandling = TypeNameHandling.Auto,
-                                                 Converters = { new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.RoundtripKind} }
+                                                 Converters = { new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.RoundtripKind } }
                                              };
                 return serializerSettings;
             }
@@ -49,6 +53,23 @@ namespace NServiceBus.Serializers.Json
             jsonSerializer.ContractResolver = new MessageContractResolver(_messageMapper);
 
             JsonReader reader = CreateJsonReader(stream);
+            reader.Read();
+
+            JsonToken firstTokenType = reader.TokenType;
+
+            if (firstTokenType != JsonToken.StartArray)
+            {
+                string messageTypes;
+
+                var bus = Configure.Instance.Builder.Build<IBus>();
+                if (bus != null &&
+                    bus.CurrentMessageContext != null &&
+                    bus.CurrentMessageContext.Headers != null &&
+                    bus.CurrentMessageContext.Headers.TryGetValue(EnclosedMessageTypes, out messageTypes))
+                {
+                    return new[] { jsonSerializer.Deserialize(reader, Type.GetType(messageTypes.Split(';')[0])) };
+                }
+            }
 
             return jsonSerializer.Deserialize<object[]>(reader);
         }
