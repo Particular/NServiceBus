3c0889a3ecab5ab3a3bcc7742ea59a58edfc8c65
Andreas Öhlund <andreas.ohlund@particular.net>
2014-06-18 14:39:28 +0200

    Fix failing outbox test

--- src/NServiceBus.Core.Tests/Outbox/OutboxFeatureTests.cs
+++ src/NServiceBus.Core.Tests/Outbox/OutboxFeatureTests.cs
@@ -9,20 +9,16 @@
     [TestFixture]
     public class OutboxFeatureTests
     {
-         
         [Test]
         public void Should_throw_if_no_outbox_capable_storage_is_found()
         {
             var outbox = new Outbox();
             var config = Configure.With(c => c.TypesToScan(new List<Type>()));
 
-            config.Settings.Set<PersistenceDefinition>(new NoOutboxSupport());
+            config.Settings.Set<EnabledPersistences>(new EnabledPersistences());
             var context = new FeatureConfigurationContext(config);
 
             Assert.Throws<Exception>(() => outbox.SetupFeature(context), "Selected persister doesn't have support for outbox storage. Please select another storage or disable the outbox feature using config.Features(f=>f.Disable<Outbox>())");
-        }
-        public class NoOutboxSupport : PersistenceDefinition
-        {
         }
     }
 
