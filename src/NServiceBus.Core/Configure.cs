4b928f24e249ec5b8f2e3fda74b9678ba4d2482b
John Simons <john.simons@nservicebus.com>
2014-06-19 17:44:41 +1000

    Moved type to new file

--- src/NServiceBus.Core/Configure.cs
+++ src/NServiceBus.Core/Configure.cs
@@ -3,11 +3,9 @@ namespace NServiceBus
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
-    using System.IO;
     using System.Linq;
     using System.Reflection;
     using System.Text;
-    using System.Web;
     using Config;
     using Config.ConfigurationSource;
     using Config.Conventions;
@@ -15,7 +13,6 @@ namespace NServiceBus
     using Hosting.Helpers;
     using Logging;
     using ObjectBuilder;
-    using ObjectBuilder.Autofac;
     using ObjectBuilder.Common;
     using Pipeline;
     using Settings;
@@ -432,183 +429,5 @@ namespace NServiceBus
             Func<Type, bool> definesMessageType;
             Func<Type, TimeSpan> retrieveTimeToBeReceived;
         }
-    }
-
-    public class ConfigurationBuilder
-    {
-        internal ConfigurationBuilder()
-        {
-            configurationSourceToUse = new DefaultConfigurationSource();
-        }
-
-        /// <summary>
-        ///     Specifies the range of types that NServiceBus scans for handlers etc.
-        /// </summary>
-        public ConfigurationBuilder TypesToScan(IEnumerable<Type> typesToScan)
-        {
-            scannedTypes = typesToScan.ToList();
-            return this;
-        }
-
-        /// <summary>
-        ///     The assemblies to include when scanning for types.
-        /// </summary>
-        public ConfigurationBuilder AssembliesToScan(IEnumerable<Assembly> assemblies)
-        {
-            AssembliesToScan(assemblies.ToArray());
-            return this;
-        }
-
-        /// <summary>
-        ///     The assemblies to include when scanning for types.
-        /// </summary>
-        public ConfigurationBuilder AssembliesToScan(params Assembly[] assemblies)
-        {
-            scannedTypes = Configure.GetAllowedTypes(assemblies);
-            return this;
-        }
-
-
-        /// <summary>
-        ///     Specifies the directory where NServiceBus scans for types.
-        /// </summary>
-        public ConfigurationBuilder ScanAssembliesInDirectory(string probeDirectory)
-        {
-            directory = probeDirectory;
-            AssembliesToScan(Configure.GetAssembliesInDirectory(probeDirectory));
-            return this;
-        }
-
-
-        /// <summary>
-        ///     Overrides the default configuration source.
-        /// </summary>
-        public ConfigurationBuilder CustomConfigurationSource(IConfigurationSource configurationSource)
-        {
-            configurationSourceToUse = configurationSource;
-            return this;
-        }
-
-
-        /// <summary>
-        ///     Defines the name to use for this endpoint.
-        /// </summary>
-        public ConfigurationBuilder EndpointName(string name)
-        {
-            EndpointName(() => name);
-            return this;
-        }
-
-        /// <summary>
-        ///     Defines the name to use for this endpoint.
-        /// </summary>
-        public ConfigurationBuilder EndpointName(Func<string> nameFunc)
-        {
-            getEndpointNameAction = nameFunc;
-            return this;
-        }
-
-        /// <summary>
-        ///     Defines the version of this endpoint.
-        /// </summary>
-        public ConfigurationBuilder EndpointVersion(Func<string> versionFunc)
-        {
-            getEndpointVersionAction = versionFunc;
-            return this;
-        }
-
-        /// <summary>
-        ///     Defines the conventions to use for this endpoint.
-        /// </summary>
-        public ConfigurationBuilder Conventions(Action<Configure.ConventionsBuilder> conventions)
-        {
-            conventions(conventionsBuilder);
-
-            return this;
-        }
-
-        /// <summary>
-        /// Defines a custom builder to use
-        /// </summary>
-        /// <typeparam name="T">The builder type</typeparam>
-        /// <returns></returns>
-        public ConfigurationBuilder UseContainer<T>() where T : IContainer
-        {
-            return UseContainer(typeof(T));
-        }
-
-
-
-        /// <summary>
-        /// Defines a custom builder to use
-        /// </summary>
-        /// <param name="builderType">The type of the builder</param>
-        /// <returns></returns>
-        public ConfigurationBuilder UseContainer(Type builderType)
-        {
-            UseContainer(builderType.Construct<IContainer>());
-
-            return this;
-        }
-
-        /// <summary>
-        /// Uses an already active instance of a builder
-        /// </summary>
-        /// <param name="builder">The instance to use</param>
-        /// <returns></returns>
-        public ConfigurationBuilder UseContainer(IContainer builder)
-        {
-            customBuilder = builder;
-
-            return this;
-        }
-        /// <summary>
-        ///     Creates the configuration object
-        /// </summary>
-        internal Configure BuildConfiguration()
-        {
-            var version = getEndpointVersionAction();
-
-            endpointName = getEndpointNameAction();
-
-            if (scannedTypes == null)
-            {
-                var directoryToScan = AppDomain.CurrentDomain.BaseDirectory;
-                if (HttpRuntime.AppDomainAppId != null)
-                {
-                    directoryToScan = HttpRuntime.BinDirectory;
-                }
-
-                ScanAssembliesInDirectory(directoryToScan);
-            }
-
-            scannedTypes = scannedTypes.Union(Configure.GetAllowedTypes(Assembly.GetExecutingAssembly())).ToList();
-
-            if (HttpRuntime.AppDomainAppId == null)
-            {
-                var baseDirectory = directory ?? AppDomain.CurrentDomain.BaseDirectory;
-                var hostPath = Path.Combine(baseDirectory, "NServiceBus.Host.exe");
-                if (File.Exists(hostPath))
-                {
-                    scannedTypes = scannedTypes.Union(Configure.GetAllowedTypes(Assembly.LoadFrom(hostPath))).ToList();
-                }
-            }
-            var builder = customBuilder ?? new AutofacObjectBuilder();
-            var settings = new SettingsHolder();
-            settings.SetDefault("EndpointName", endpointName);
-            settings.SetDefault("TypesToScan", scannedTypes);
-            settings.SetDefault("EndpointVersion", version);
-
-            return new Configure(settings, configurationSourceToUse, builder, conventionsBuilder.BuildConventions());
-        }
-
-        IContainer customBuilder;
-        IConfigurationSource configurationSourceToUse;
-        Configure.ConventionsBuilder conventionsBuilder = new Configure.ConventionsBuilder();
-        string directory;
-        string endpointName;
-        Func<string> getEndpointNameAction = () => EndpointHelper.GetDefaultEndpointName();
-        Func<string> getEndpointVersionAction = () => EndpointHelper.GetEndpointVersion();
-        IList<Type> scannedTypes;
     }
 }
