6599e27017bd40adf65fec65c12907d54991d05c
Simon Cropp <simon.cropp@gmail.com>
2014-06-25 10:19:59 +0100

    add reminder to remove IScheduler

--- src/NServiceBus.Core/Scheduling/IScheduler.cs
+++ src/NServiceBus.Core/Scheduling/IScheduler.cs
@@ -2,6 +2,7 @@ namespace NServiceBus.Scheduling
 {
     using System;
 
+    [ObsoleteEx(RemoveInVersion = "5.1", TreatAsErrorFromVersion = "5.1", Message = "reminder that since this is no longer public we should remove it in the next release.")]
     interface IScheduler
     {
         void Schedule(ScheduledTask task);
