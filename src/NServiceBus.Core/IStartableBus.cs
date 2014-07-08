4805be39a5e06313fd5c226c01950ef848258c06
Simon Cropp <simon.cropp@gmail.com>
2014-07-08 10:01:49 +1000

    re add xml comment

--- src/NServiceBus.Core/IStartableBus.cs
+++ src/NServiceBus.Core/IStartableBus.cs
@@ -1,5 +1,8 @@
 namespace NServiceBus
-{
+{ 
+    /// <summary>
+    /// The interface used for starting and stopping an IBus.
+    /// </summary>
     [ObsoleteEx(RemoveInVersion = "6.0",TreatAsErrorFromVersion = "5.0", Replacement = "IBus")]
     public interface IStartableBus : IBus
     {
