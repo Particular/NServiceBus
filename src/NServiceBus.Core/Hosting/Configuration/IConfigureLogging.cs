2e0dcfd12d63e919599afdb835ea037201f7630f
John Simons <john.simons@nservicebus.com>
2014-07-09 11:17:19 +1000

    Fixed doco about obsolete API

--- src/NServiceBus.Core/Hosting/Configuration/IConfigureLogging.cs
+++ src/NServiceBus.Core/Hosting/Configuration/IConfigureLogging.cs
@@ -5,7 +5,7 @@
     /// </summary>
     /// <remarks>
     /// If you want logging configured regardless of profiles, do not use this interface,
-    /// instead implement <see cref="IWantCustomInitialization"/>  and configure logging before you call <see cref="NServiceBus.Configure.With()"/>.
+    /// instead configure logging before you call <see cref="NServiceBus.Configure.With()"/> if you self hosting or configure logging in <see cref="IConfigureThisEndpoint.Customize"/>.
     /// Implementors should work against the generic version of this interface <see cref="IConfigureLoggingForProfile{T}"/>.
     /// </remarks>
     public interface IConfigureLogging
--- src/NServiceBus.Core/Hosting/Configuration/IWantCustomLogging.cs
+++ src/NServiceBus.Core/Hosting/Configuration/IWantCustomLogging.cs
@@ -1,7 +1,7 @@
 #pragma warning disable 1591
 namespace NServiceBus
 {
-    [ObsoleteEx(TreatAsErrorFromVersion = "5.0", Replacement = "use IWantCustomInitialization and configure logging before you call Configure.With().", RemoveInVersion = "6.0")]
+    [ObsoleteEx(TreatAsErrorFromVersion = "5.0", Replacement = "IConfigureLogging", RemoveInVersion = "6.0")]
     public interface IWantCustomLogging
     {
     }
