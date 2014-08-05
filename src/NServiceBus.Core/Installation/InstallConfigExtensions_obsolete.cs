5fe776bac18d207775337c7604482baeb6d10082
John Simons <john.simons@nservicebus.com>
2014-08-05 17:47:25 +1000

    Fixed issue with static code analysis

--- src/NServiceBus.Core/Installation/InstallConfigExtensions_obsolete.cs
+++ src/NServiceBus.Core/Installation/InstallConfigExtensions_obsolete.cs
@@ -15,7 +15,9 @@ namespace NServiceBus
         /// <param name="config">The instance of <see cref="Configure"/> to apply these settings to.</param>
         /// <param name="username">The username to pass to <see cref="INeedToInstallSomething.Install"/></param>
         [ObsoleteEx(Replacement = "Configure.With(c=>.EnableInstallers())", RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0")]
+// ReSharper disable UnusedParameter.Global
         public static Configure EnableInstallers(this Configure config, string username = null)
+// ReSharper restore UnusedParameter.Global
         {
             throw new InvalidOperationException();
         }
