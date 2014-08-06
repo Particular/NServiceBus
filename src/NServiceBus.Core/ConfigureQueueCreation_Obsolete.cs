309cc71b794f03bd02722324b9a8aa98ee237675
Simon Cropp <simon.cropp@gmail.com>
2014-08-06 12:09:22 +1000

    Moved License configuration API to be a ConfigurationBuilder extension
    fixes #2278

--- src/NServiceBus.Core/ConfigureQueueCreation_Obsolete.cs
+++ src/NServiceBus.Core/ConfigureQueueCreation_Obsolete.cs
@@ -1,4 +1,4 @@
-
+#pragma warning disable 1591
 // ReSharper disable UnusedParameter.Global
 namespace NServiceBus
 {
@@ -7,18 +7,12 @@ namespace NServiceBus
     public static partial class ConfigureQueueCreation
     {
 
-        /// <summary>
-        /// If queues configured do not exist, will cause them not to be created on startup.
-        /// </summary>
         [ObsoleteEx(Replacement = "Configure.With(c=>.DoNotCreateQueues())", RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0")]
         public static Configure DoNotCreateQueues(this Configure config)
         {
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// Gets whether or not queues should be created
-        /// </summary>
         [ObsoleteEx(RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0")]
         public static bool DontCreateQueues
         {
--- src/NServiceBus.Core/Licensing/ConfigureLicenseExtensions.cs
+++ src/NServiceBus.Core/Licensing/ConfigureLicenseExtensions.cs
@@ -8,17 +8,16 @@ namespace NServiceBus
     /// <summary>
     /// Contains extension methods to configure license.
     /// </summary>
-    public static class ConfigureLicenseExtensions
+    public static partial class ConfigureLicenseExtensions
     {
         static ILog Logger = LogManager.GetLogger(typeof(LicenseManager));
 
         /// <summary>
         /// Allows user to specify the license string.
         /// </summary>
-        /// <param name="config">The current <see cref="Configure"/>.</param>
+        /// <param name="config">The current <see cref="ConfigurationBuilder"/>.</param>
         /// <param name="licenseText">The license text.</param>
-        /// <returns>The current <see cref="Configure"/>.</returns>
-        public static Configure License(this Configure config, string licenseText)
+        public static ConfigurationBuilder License(this ConfigurationBuilder config, string licenseText)
         {
             if (string.IsNullOrWhiteSpace(licenseText))
             {
@@ -34,12 +33,10 @@ namespace NServiceBus
         /// <summary>
         /// Allows user to specify the path for the license file.
         /// </summary>
-        /// <param name="config">The current <see cref="Configure"/>.</param>
+        /// <param name="config">The current <see cref="ConfigurationBuilder"/>.</param>
         /// <param name="licenseFile">A relative or absolute path to the license file.</param>
-        /// <returns>The current <see cref="Configure"/>.</returns>
-        public static Configure LicensePath(this Configure config, string licenseFile)
+        public static ConfigurationBuilder LicensePath(this ConfigurationBuilder config, string licenseFile)
         {
-
             if (!File.Exists(licenseFile))
             {
                 throw new FileNotFoundException("License file not found", licenseFile);
