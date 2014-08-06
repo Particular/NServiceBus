1b95f3771df50d83eaa59daa928d5593d2ee2e77
John Simons <john.simons@nservicebus.com>
2014-08-06 11:30:33 +1000

    Static analysis error

--- src/NServiceBus.Core/ConfigureFaultsForwarder.cs
+++ src/NServiceBus.Core/ConfigureFaultsForwarder.cs
@@ -11,7 +11,9 @@ namespace NServiceBus
 		/// Forward messages that have repeatedly failed to another endpoint.
 		/// </summary>
         [ObsoleteEx(Message="It is safe to remove this method call. This is the default behavior.", RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0")]
+// ReSharper disable UnusedParameter.Global
         public static Configure MessageForwardingInCaseOfFault(this Configure config)
+// ReSharper restore UnusedParameter.Global
 		{
 			throw new InvalidOperationException();
 		}
