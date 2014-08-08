b2461c38d0b94fe1c00a6d9c3c199eabc60962fd
Simon Cropp <simon.cropp@gmail.com>
2014-08-08 15:54:07 +1000

    split storage out

--- src/NServiceBus.Core/Persistence/PersistenceDefinition.cs
+++ src/NServiceBus.Core/Persistence/PersistenceDefinition.cs
@@ -25,36 +25,8 @@
         {
             return SupportedStorages.Contains(storage);
         }
-
         internal IEnumerable<Storage> SupportedStorages { get { return supportedStorages.Distinct(); } }
  
         List<Storage> supportedStorages = new List<Storage>();
-    }
-
-    /// <summary>
-    /// The storage needs of NServiceBus
-    /// </summary>
-    public enum Storage
-    {
-        /// <summary>
-        /// Storage for timeouts
-        /// </summary>
-        Timeouts = 1,
-        /// <summary>
-        /// Storage for subscriptions
-        /// </summary>
-        Subscriptions = 2,
-        /// <summary>
-        /// Storage for sagas
-        /// </summary>
-        Sagas = 3,
-        /// <summary>
-        /// Storage for gateway deduplication
-        /// </summary>
-        GatewayDeduplication = 4,
-        /// <summary>
-        /// Storage for the outbox
-        /// </summary>
-        Outbox = 5,
     }
 }
