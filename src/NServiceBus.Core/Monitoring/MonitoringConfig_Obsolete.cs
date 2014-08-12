db8ee6b27466aba1ff2fff7ffdece991df9c9d6f
Simon Cropp <simon.cropp@gmail.com>
2014-08-12 14:00:27 +1000

    fix XXXPerformanceCounter obsoletes

--- src/NServiceBus.Core/Monitoring/MonitoringConfig_Obsolete.cs
+++ src/NServiceBus.Core/Monitoring/MonitoringConfig_Obsolete.cs
@@ -5,17 +5,25 @@ namespace NServiceBus
 {
     using System;
 
-    [ObsoleteEx(Replacement = "Configure.With(c=>.EnableCriticalTimeCounter()) or Configure.With(c=>.EnableSLACounter(TimeSpan))", RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0")]
+    [ObsoleteEx(
+        Replacement = "Configure.With(c=>.EnableCriticalTimePerformanceCounter()) or Configure.With(c=>.EnableSLAPerformanceCounter(TimeSpan))", 
+        RemoveInVersion = "6.0", 
+        TreatAsErrorFromVersion = "5.0")]
     public static class MonitoringConfig
     {
-        [ObsoleteEx(Replacement = "Configure.With(c=>.EnableSLACounter(TimeSpan))", RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0")]
+        [ObsoleteEx(
+            Replacement = "Configure.With(c=>.EnableSLAPerformanceCounter(TimeSpan))", 
+            RemoveInVersion = "6.0", 
+            TreatAsErrorFromVersion = "5.0")]
         public static Configure SetEndpointSLA(this Configure config, TimeSpan sla)
         {
             throw new NotImplementedException();
         }
 
 
-        [ObsoleteEx(Replacement = "Configure.With(c=>.EnableCriticalTimeCounter())", RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0")]
+        [ObsoleteEx(Replacement = "Configure.With(c=>.EnableCriticalTimePerformanceCounter())", 
+            RemoveInVersion = "6.0",
+            TreatAsErrorFromVersion = "5.0")]
         public static Configure EnablePerformanceCounters(this Configure config)
         {
             throw new NotImplementedException();
