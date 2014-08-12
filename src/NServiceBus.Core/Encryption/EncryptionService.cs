2b70c47b80255c97bd0e9b2c0e83788a2d6b9e2f
Simon Cropp <simon.cropp@gmail.com>
2014-08-12 14:00:40 +1000

    better obsolete on EncryptionService

--- src/NServiceBus.Core/Encryption/EncryptionService.cs
+++ src/NServiceBus.Core/Encryption/EncryptionService.cs
@@ -1,7 +1,10 @@
 #pragma warning disable 1591
 namespace NServiceBus.Encryption.Rijndael
 {
-    [ObsoleteEx(TreatAsErrorFromVersion = "5.0", RemoveInVersion = "6.0")]
+    [ObsoleteEx(
+        TreatAsErrorFromVersion = "5.0",
+        RemoveInVersion = "6.0",
+        Message = "The Rijndael encryption functionality was an internal implementation detail of NServicebus as such it has been removed from the public API.")]
     public class EncryptionService 
     {
     }
