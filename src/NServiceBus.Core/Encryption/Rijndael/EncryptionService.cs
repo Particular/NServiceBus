a9b78aff3eca2664799bc568722110fb1a3bcbc3
Simon Cropp <simon.cropp@gmail.com>
2014-08-19 09:20:35 +1000

    VerifyKeysAreNotTooSimilar

--- src/NServiceBus.Core/Encryption/Rijndael/EncryptionService.cs
+++ src/NServiceBus.Core/Encryption/Rijndael/EncryptionService.cs
@@ -57,42 +57,43 @@ namespace NServiceBus.Encryption.Rijndael
                 return encryptedValue.EncryptedBase64Value;
             }
 
-            var decryptionKeys = new List<byte[]>{Key};
+            var decryptionKeys = new List<byte[]> { Key };
             if (ExpiredKeys != null)
             {
                 decryptionKeys.AddRange(ExpiredKeys);
             }
-            var encrypted = Convert.FromBase64String(encryptedValue.EncryptedBase64Value);
             var cryptographicExceptions = new List<CryptographicException>();
-            using (var rijndael = new RijndaelManaged())
-            {
-                rijndael.IV = Convert.FromBase64String(encryptedValue.Base64Iv);
-                rijndael.Mode = CipherMode.CBC;
 
-                foreach (var key in decryptionKeys)
+            foreach (var key in decryptionKeys)
+            {
+                try
                 {
-                    rijndael.Key = key;
-                    try
-                    {
-                        return Decrypt(rijndael, encrypted);
-                    }
-                    catch (CryptographicException exception)
-                    {
-                        cryptographicExceptions.Add(exception);
-                    }
+                    return Decrypt(encryptedValue, key);
+                }
+                catch (CryptographicException exception)
+                {
+                    cryptographicExceptions.Add(exception);
                 }
             }
             var message = string.Format("Could not decrypt message. Tried {0} keys.", decryptionKeys.Count);
             throw new AggregateException(message, cryptographicExceptions);
         }
-        static string Decrypt(RijndaelManaged rijndael, byte[] encrypted)
+
+        static string Decrypt(EncryptedValue encryptedValue, byte[] key)
         {
-            using (var decryptor = rijndael.CreateDecryptor())
-            using (var memoryStream = new MemoryStream(encrypted))
-            using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
-            using (var reader = new StreamReader(cryptoStream))
+            using (var rijndael = new RijndaelManaged())
             {
-                return reader.ReadToEnd();
+                var encrypted = Convert.FromBase64String(encryptedValue.EncryptedBase64Value);
+                rijndael.IV = Convert.FromBase64String(encryptedValue.Base64Iv);
+                rijndael.Mode = CipherMode.CBC;
+                rijndael.Key = key;
+                using (var decryptor = rijndael.CreateDecryptor())
+                using (var memoryStream = new MemoryStream(encrypted))
+                using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                using (var reader = new StreamReader(cryptoStream))
+                {
+                    return reader.ReadToEnd();
+                }
             }
         }
 
@@ -125,6 +126,29 @@ namespace NServiceBus.Encryption.Rijndael
             }
         }
 
-        private static readonly ILog Logger = LogManager.GetLogger(typeof (EncryptionService));
+        internal void VerifyKeysAreNotTooSimilar()
+        {
+            for (var index = 0; index < ExpiredKeys.Count; index++)
+            {
+                var decryption = ExpiredKeys[index];
+                CryptographicException exception = null;
+                var encryptedValue = ((IEncryptionService)this).Encrypt("a");
+                try
+                {
+                    Decrypt(encryptedValue, decryption);
+                }
+                catch (CryptographicException cryptographicException)
+                {
+                    exception = cryptographicException;
+                }
+                if (exception == null)
+                {
+                    var message = string.Format("The new Encryption Key is too similar to the Expired Key at index {0}. This can cause issues when decrypting data. To fix this issue please ensure the new encryption key is not too similar to the existing Expired Keys.", index);
+                    throw new Exception(message);
+                }
+            }
+        }
+
+        static readonly ILog Logger = LogManager.GetLogger(typeof(EncryptionService));
     }
 }
