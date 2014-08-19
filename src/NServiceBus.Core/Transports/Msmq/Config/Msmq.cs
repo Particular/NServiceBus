fcca39faee1d9bbad40598e7f8b12bee458df780
Yves Goeleven <yves@goeleven.com>
2014-08-19 15:39:22 +0200

    Made configure on transport definition public again so that transports can actually configure themselves

--- src/NServiceBus.Core/Transports/Msmq/Config/Msmq.cs
+++ src/NServiceBus.Core/Transports/Msmq/Config/Msmq.cs
@@ -19,7 +19,7 @@ namespace NServiceBus
         /// <summary>
         /// Gives implementations access to the <see cref="ConfigurationBuilder"/> instance at configuration time.
         /// </summary>
-        protected internal override void Configure(ConfigurationBuilder config)
+        public override void Configure(ConfigurationBuilder config)
         {
             config.EnableFeature<MsmqTransport>();
             config.EnableFeature<MessageDrivenSubscriptions>();
