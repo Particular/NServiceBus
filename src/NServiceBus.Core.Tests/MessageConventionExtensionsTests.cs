dccf3633033098b2c2e3be21b5e8cb0fc8a11d5d
Simon Cropp <simon.cropp@gmail.com>
2014-08-20 13:06:49 +1000

    dont include types in NSB in message conventions
    add acceptance test for greedy messages
    
    fix acceptance test
    
    us SN for detecting particular assembly
    
    fix inspection error
    
    remove the extension method
    
    sign tests with a diff SN key
    
    redundant using

--- src/NServiceBus.Core.Tests/MessageConventionExtensionsTests.cs
+++ src/NServiceBus.Core.Tests/MessageConventionExtensionsTests.cs
@@ -1,6 +1,9 @@
-﻿namespace NServiceBus.Core.Tests.Encryption
+﻿namespace NServiceBus.Core.Tests
 {
     using System;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using Unicast.Messages;
     using NUnit.Framework;
 
     [TestFixture]
@@ -12,6 +15,7 @@
             var timeToBeReceivedAction = MessageConventionExtensions.TimeToBeReceivedAction(typeof(InheritedClassWithAttribute));
             Assert.AreEqual(TimeSpan.FromSeconds(2), timeToBeReceivedAction);
         }
+
         [Test]
         public void Should_use_inherited_TimeToBeReceived()
         {
@@ -31,6 +35,38 @@
         class InheritedClassWithNoAttribute : BaseClass
         {
             
+        }
+
+
+        [Test]
+        public void Should_return_false_for_SN_and_non_particular_assembly()
+        {
+            Assert.IsFalse(MessageConventionExtensions.IsFromParticularAssembly(typeof(string)));
+        }
+
+        [Test]
+        public void Should_return_true_for_particular_assembly()
+        {
+            Assert.IsTrue(MessageConventionExtensions.IsFromParticularAssembly(typeof(ExecuteLogicalMessagesBehavior)));
+        }
+
+        [Test]
+        public void Should_return_false_for_non_SN_and_non_particular_assembly()
+        {
+            var type = GetNonSnFakeType();
+            Assert.IsFalse(MessageConventionExtensions.IsFromParticularAssembly(type));
+        }
+
+        static Type GetNonSnFakeType()
+        {
+            var assemblyName = new AssemblyName
+            {
+                Name = "myAssembly"
+            };
+            var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.ReflectionOnly);
+            var newModule = assemblyBuilder.DefineDynamicModule("myModule");
+            var myType = newModule.DefineType("myType", TypeAttributes.Public);
+            return myType.CreateType();
         }
     }
 
