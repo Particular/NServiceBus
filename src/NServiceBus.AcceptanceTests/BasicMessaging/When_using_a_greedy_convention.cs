333f40597703375e5d355242afe9be4d1c481613
Simon Cropp <simon.cropp@gmail.com>
2014-08-20 12:11:35 +1000

    add explicit failing test for bad conventions
    related to #2319

---
+++ src/NServiceBus.AcceptanceTests/BasicMessaging/When_using_a_greedy_convention.cs
@@ -0,0 +1,65 @@
+﻿namespace NServiceBus.AcceptanceTests.BasicMessaging
+{
+    using System;
+    using EndpointTemplates;
+    using AcceptanceTesting;
+    using NUnit.Framework;
+    using ScenarioDescriptors;
+
+    public class When_using_a_greedy_convention : NServiceBusAcceptanceTest
+    {
+        [Test]
+        [Explicit]
+        public void Should_receive_the_message()
+        {
+            Scenario.Define(() => new Context { Id = Guid.NewGuid() })
+                    .WithEndpoint<EndPoint>(b => b.Given((bus, context) => bus.SendLocal(new MyMessage
+                    {Id = context.Id})))
+                    .Done(c => c.WasCalled)
+                    .Repeat(r =>r
+                        .For(Transports.Msmq)
+                    )
+                    .Should(c => Assert.True(c.WasCalled, "The message handler should be called"))
+                    .Run();
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool WasCalled { get; set; }
+
+            public Guid Id { get; set; }
+        }
+
+        public class EndPoint : EndpointConfigurationBuilder
+        {
+            public EndPoint()
+            {
+                EndpointSetup<DefaultServer>(c => c.DefiningMessagesAs(MessageConvention));
+            }
+
+            static bool MessageConvention(Type t)
+            {
+
+                return t.Namespace != null && t.Namespace.EndsWith(".Messages");
+            }
+        }
+
+        [Serializable]
+        public class MyMessage : ICommand
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class MyMessageHandler : IHandleMessages<MyMessage>
+        {
+            public Context Context { get; set; }
+            public void Handle(MyMessage message)
+            {
+                if (Context.Id != message.Id)
+                    return;
+
+                Context.WasCalled = true;
+            }
+        }
+    }
+}
