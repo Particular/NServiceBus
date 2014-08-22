0e63964ab90b3eace715f189754f64fa83174936
Andreas Öhlund <andreas.ohlund@particular.net>
2014-08-22 19:46:02 +0200

    Introduced ISendOnlyBus
    This new interface contains a subset of the IBus functionallity that is
    available when using a send only bus

---
+++ src/NServiceBus.Core/ISendOnlyBus.cs
@@ -0,0 +1,112 @@
+namespace NServiceBus
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Provides the subset of bus operations that is applicable for a send only bus
+    /// </summary>
+    public interface ISendOnlyBus
+    {
+        /// <summary>
+        /// Publish the message to subscribers.
+        /// </summary>
+        void Publish<T>(T message);
+
+        /// <summary>
+        /// Publish the message to subscribers.
+        /// </summary>
+        void Publish<T>();
+
+        /// <summary>
+        /// Instantiates a message of type T and publishes it.
+        /// </summary>
+        /// <typeparam name="T">The type of message, usually an interface</typeparam>
+        /// <param name="messageConstructor">An action which initializes properties of the message</param>
+        void Publish<T>(Action<T> messageConstructor);
+
+        /// <summary>
+        /// Sends the provided message.
+        /// </summary>
+        /// <param name="message">The message to send.</param>
+        ICallback Send(object message);
+
+        /// <summary>
+        /// Instantiates a message of type T and sends it.
+        /// </summary>
+        /// <typeparam name="T">The type of message, usually an interface</typeparam>
+        /// <param name="messageConstructor">An action which initializes properties of the message</param>
+        /// <remarks>
+        /// The message will be sent to the destination configured for T
+        /// </remarks>
+        ICallback Send<T>(Action<T> messageConstructor);
+
+        /// <summary>
+        /// Sends the message.
+        /// </summary>
+        /// <param name="destination">
+        /// The address of the destination to which the message will be sent.
+        /// </param>
+        /// <param name="message">The message to send.</param>
+        ICallback Send(string destination, object message);
+
+        /// <summary>
+        /// Sends the provided message.
+        /// </summary>
+        /// <param name="address">
+        /// The address to which the message will be sent.
+        /// </param>
+        /// <param name="message">The message to send.</param>
+        ICallback Send(Address address, object message);
+
+        /// <summary>
+        /// Instantiates a message of type T and sends it to the given destination.
+        /// </summary>
+        /// <typeparam name="T">The type of message, usually an interface</typeparam>
+        /// <param name="destination">The destination to which the message will be sent.</param>
+        /// <param name="messageConstructor">An action which initializes properties of the message</param>
+        ICallback Send<T>(string destination, Action<T> messageConstructor);
+
+        /// <summary>
+        /// Instantiates a message of type T and sends it to the given address.
+        /// </summary>
+        /// <typeparam name="T">The type of message, usually an interface</typeparam>
+        /// <param name="address">The address to which the message will be sent.</param>
+        /// <param name="messageConstructor">An action which initializes properties of the message</param>
+        ICallback Send<T>(Address address, Action<T> messageConstructor);
+
+        /// <summary>
+        /// Sends the message to the destination as well as identifying this
+        /// as a response to a message containing the Id found in correlationId.
+        /// </summary>
+        ICallback Send(string destination, string correlationId, object message);
+
+        /// <summary>
+        /// Sends the message to the given address as well as identifying this
+        /// as a response to a message containing the Id found in correlationId.
+        /// </summary>
+        ICallback Send(Address address, string correlationId, object message);
+
+        /// <summary>
+        /// Instantiates a message of the type T using the given messageConstructor,
+        /// and sends it to the destination identifying it as a response to a message
+        /// containing the Id found in correlationId.
+        /// </summary>
+        ICallback Send<T>(string destination, string correlationId, Action<T> messageConstructor);
+
+        /// <summary>
+        /// Instantiates a message of the type T using the given messageConstructor,
+        /// and sends it to the given address identifying it as a response to a message
+        /// containing the Id found in correlationId.
+        /// </summary>
+        ICallback Send<T>(Address address, string correlationId, Action<T> messageConstructor);
+
+        /// <summary>
+        /// Gets the list of key/value pairs that will be in the header of
+        /// messages being sent by the same thread.
+        /// 
+        /// This value will be cleared when a thread receives a message.
+        /// </summary>
+        IDictionary<string, string> OutgoingHeaders { get; }
+    }
+}
