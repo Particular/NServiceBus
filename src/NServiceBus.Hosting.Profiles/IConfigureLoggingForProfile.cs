558da5b2e3abdc0734af2b236c544ea22cd4e810
John Simons <john.simons@nservicebus.com>
2014-08-25 13:13:48 +1000

    Fixed doco

--- src/NServiceBus.Hosting.Profiles/IConfigureLoggingForProfile.cs
+++ src/NServiceBus.Hosting.Profiles/IConfigureLoggingForProfile.cs
@@ -5,7 +5,7 @@
     /// </summary>
     /// <remarks>
     /// If you want logging configured regardless of profiles, do not use this interface,
-    /// instead configure logging before you call <see cref="NServiceBus.Configure.With()"/> if you self hosting or configure logging in <see cref="IConfigureThisEndpoint.Customize"/>.
+    /// instead configure logging before you call <see cref="Bus.Create"/> if you self hosting or configure logging in <see cref="IConfigureThisEndpoint.Customize"/>.
     /// Implementors should work against the generic version of this interface in the host.
     /// </remarks>
     public interface IConfigureLogging
