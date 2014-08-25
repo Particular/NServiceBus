087f3c467c90117c273ea47f8b5f42f4e9bedb65
Simon Cropp <simon.cropp@gmail.com>
2014-08-25 10:35:40 +1000

    fix namespace in MessageConventionExtensionsTests

--- src/NServiceBus.Core.Tests/MessageConventionExtensionsTests.cs
+++ src/NServiceBus.Core.Tests/MessageConventionExtensionsTests.cs
@@ -1,8 +1,7 @@
-﻿namespace NServiceBus.Core.Tests.Encryption
+﻿namespace NServiceBus.Core.Tests
 {
     using System;
     using NUnit.Framework;
-    using Conventions = NServiceBus.Conventions;
 
     [TestFixture]
     public class MessageConventionExtensionsTests
@@ -10,14 +9,15 @@
         [Test]
         public void Should_use_TimeToBeReceived_from_bottom_of_tree()
         {
-            var conventions = new Conventions();
+            var conventions = new NServiceBus.Conventions();
             var timeToBeReceivedAction = conventions.GetTimeToBeReceived(typeof(InheritedClassWithAttribute));
             Assert.AreEqual(TimeSpan.FromSeconds(2), timeToBeReceivedAction);
         }
+
         [Test]
         public void Should_use_inherited_TimeToBeReceived()
         {
-            var conventions = new Conventions();
+            var conventions = new NServiceBus.Conventions();
             var timeToBeReceivedAction = conventions.GetTimeToBeReceived(typeof(InheritedClassWithNoAttribute));
             Assert.AreEqual(TimeSpan.FromSeconds(1), timeToBeReceivedAction);
         }
@@ -26,15 +26,18 @@
         class BaseClass
         {
         }
+
         [TimeToBeReceivedAttribute("00:00:02")]
         class InheritedClassWithAttribute : BaseClass
         {
             
         }
+
         class InheritedClassWithNoAttribute : BaseClass
         {
             
         }
+
     }
 
 }
