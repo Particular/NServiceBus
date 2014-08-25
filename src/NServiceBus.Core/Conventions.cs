0639a8f18792ce84c8ae340b7349845a8a523e2b
Simon Cropp <simon.cropp@gmail.com>
2014-08-25 11:54:26 +1000

    redundant using

--- src/NServiceBus.Core/Conventions.cs
+++ src/NServiceBus.Core/Conventions.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Linq;
     using System.Reflection;
     using NServiceBus.Utils.Reflection;
