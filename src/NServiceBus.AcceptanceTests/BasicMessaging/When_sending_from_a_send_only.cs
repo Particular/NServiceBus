ea76771a21a5aebfa67b352e9e1342ad51ed4473
Simon Cropp <simon.cropp@gmail.com>
2014-08-26 14:46:36 +1000

    add sendonly AcceptanceTests

---
+++ src/NServiceBus.AcceptanceTests/BasicMessaging/When_sending_from_a_send_only.cs
@@ -0,0 +1,71 @@
+﻿namespace NServiceBus.AcceptanceTests.BasicMessaging
+{
+    using System;
+    using EndpointTemplates;
+    using AcceptanceTesting;
+    using NUnit.Framework;
+    using ScenarioDescriptors;
+
+    public class When_sending_from_a_send_only : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_receive_the_message()
+        {
+            Scenario.Define(() => new Context { Id = Guid.NewGuid() })
+                    .WithEndpoint<Sender>(b => b.Given((bus, context) => bus.Send(new MyMessage
+                    {
+                        Id = context.Id
+                    })))
+                    .WithEndpoint<Receiver>()
+                    .Done(c => c.WasCalled)
+                    .Repeat(r =>r.For(Serializers.Json))
+                    .Should(c => Assert.True(c.WasCalled, "The message handler should be called"))
+                    .Run();
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool WasCalled { get; set; }
+
+            public Guid Id { get; set; }
+        }
+
+        public class Sender : EndpointConfigurationBuilder
+        {
+            public Sender()
+            {
+                EndpointSetup<DefaultServer>(c => c.SendOnly())
+                    .AddMapping<MyMessage>(typeof(Receiver));
+            }
+        }
+
+        public class Receiver : EndpointConfigurationBuilder
+        {
+            public Receiver()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+        }
+
+        [Serializable]
+        public class MyMessage : ICommand
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class MyMessageHandler : IHandleMessages<MyMessage>
+        {
+            public Context Context { get; set; }
+
+            public IBus Bus { get; set; }
+
+            public void Handle(MyMessage message)
+            {
+                if (Context.Id != message.Id)
+                    return;
+
+                Context.WasCalled = true;
+            }
+        }
+    }
+}
