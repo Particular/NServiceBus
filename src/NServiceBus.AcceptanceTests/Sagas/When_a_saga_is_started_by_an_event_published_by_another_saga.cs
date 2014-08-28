1bb195f396f1fabdbc19c446f05f0b1bfa4c4700
Yves Goeleven <yves@goeleven.com>
2014-08-28 14:49:44 +0200

    split tests to prevent context spillover

--- src/NServiceBus.AcceptanceTests/Sagas/When_a_saga_is_started_by_an_event_published_by_another_saga.cs
+++ src/NServiceBus.AcceptanceTests/Sagas/When_a_saga_is_started_by_an_event_published_by_another_saga.cs
@@ -45,47 +45,6 @@
             public bool IsEventSubscriptionReceived { get; set; }
         }
 
-        [Test]
-        public void Should_start_the_saga_when_set_up_to_start_for_the_base_event()
-        {
-            Scenario.Define<SagaContext>()
-                .WithEndpoint<Publisher>(b =>
-                    b.When(c => c.IsEventSubscriptionReceived,
-                        bus =>
-                            bus.Publish<SomethingHappenedEvent>(m => { m.DataId = Guid.NewGuid(); }))
-                )
-                .WithEndpoint<SagaThatIsStartedByABaseEvent>(
-                    b => b.Given((bus, context) =>
-                    {
-                        bus.Subscribe<BaseEvent>();
-
-                        if (context.HasNativePubSubSupport)
-                            context.IsEventSubscriptionReceived = true;
-                    }))
-                .Done(c => c.DidSagaComplete)
-                .Repeat(r => r.For(Transports.Default))
-                .Should(c => Assert.True(c.DidSagaComplete))
-                .Run();
-        }
-
-        public class SagaContext : ScenarioContext
-        {
-            public bool IsEventSubscriptionReceived { get; set; }
-            public bool DidSagaComplete { get; set; }
-        }
-
-        public class Publisher : EndpointConfigurationBuilder
-        {
-            public Publisher()
-            {
-                EndpointSetup<DefaultPublisher>(b => b.OnEndpointSubscribed<SagaContext>((s, context) =>
-                {
-                    context.AddTrace("Subscription received for " + s.SubscriberReturnAddress.Queue);
-                    context.IsEventSubscriptionReceived = true;
-                }));
-            }
-        }
-
         public class SagaThatPublishesAnEvent : EndpointConfigurationBuilder
         {
             public SagaThatPublishesAnEvent()
@@ -171,37 +130,6 @@
                 }
 
                 protected override void ConfigureHowToFindSaga(SagaPropertyMapper<Saga2Data> mapper)
-                {
-                }
-            }
-        }
-
-        public class SagaThatIsStartedByABaseEvent : EndpointConfigurationBuilder
-        {
-            public SagaThatIsStartedByABaseEvent()
-            {
-                EndpointSetup<DefaultServer>(c => c.DisableFeature<AutoSubscribe>())
-                    .AddMapping<BaseEvent>(typeof(Publisher));
-            }
-
-            public class SagaStartedByBaseEvent : Saga<SagaStartedByBaseEvent.SagaData>, IAmStartedByMessages<BaseEvent>
-            {
-                public SagaContext Context { get; set; }
-
-                public void Handle(BaseEvent message)
-                {
-                    Data.DataId = message.DataId;
-                    MarkAsComplete();
-                    Context.DidSagaComplete = true;
-                }
-
-                public class SagaData : ContainSagaData
-                {
-                    [Unique]
-                    public virtual Guid DataId { get; set; }
-                }
-
-                protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaData> mapper)
                 {
                 }
             }
