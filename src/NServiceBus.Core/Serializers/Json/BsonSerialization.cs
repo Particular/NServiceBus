094e8e401925c38063dbda5e5ab62de3413c8bed
Andreas Öhlund <andreas.ohlund@particular.net>
2014-09-03 13:44:06 +0200

    Making the json serializer internal
    To avoid confusing users configuring NSB

--- src/NServiceBus.Core/Serializers/Json/BsonSerialization.cs
+++ src/NServiceBus.Core/Serializers/Json/BsonSerialization.cs
@@ -3,21 +3,15 @@
     using MessageInterfaces.MessageMapper.Reflection;
     using Serializers.Json;
 
-    /// <summary>
-    /// Uses Bson as the message serialization.
-    /// </summary>
-    public class BsonSerialization : Feature
+    class BsonSerialization : Feature
     {
         
-        internal BsonSerialization()
+        public BsonSerialization()
         {
             EnableByDefault();
             Prerequisite(this.ShouldSerializationFeatureBeEnabled, "BsonSerialization not enable since serialization definition not detected.");
         }
 
-        /// <summary>
-        /// See <see cref="Feature.Setup"/>
-        /// </summary>
         protected internal override void Setup(FeatureConfigurationContext context)
         {
             context.Container.ConfigureComponent<MessageMapper>(DependencyLifecycle.SingleInstance);
--- src/NServiceBus.Core/Serializers/Json/JsonMessageSerializer.cs
+++ src/NServiceBus.Core/Serializers/Json/JsonMessageSerializer.cs
@@ -6,72 +6,42 @@ namespace NServiceBus.Serializers.Json
     using MessageInterfaces;
     using Newtonsoft.Json;
 
-    /// <summary>
-    /// JSON message serializer.
-    /// </summary>
-    public class JsonMessageSerializer : JsonMessageSerializerBase
+    class JsonMessageSerializer : JsonMessageSerializerBase
     {
-        private Encoding encoding = Encoding.UTF8;
+        Encoding encoding = Encoding.UTF8;
 
-        /// <summary>
-        /// Constructor.
-        /// </summary>
         public JsonMessageSerializer(IMessageMapper messageMapper)
             : base(messageMapper)
         {
         }
 
-        /// <summary>
-        /// Creates the writer
-        /// </summary>
-        /// <param name="stream"></param>
         protected internal override JsonWriter CreateJsonWriter(Stream stream)
         {
             var streamWriter = new StreamWriter(stream, Encoding);
             return new JsonTextWriter(streamWriter) {Formatting = Formatting.None};
         }
 
-        /// <summary>
-        /// Creates the reader
-        /// </summary>
-        /// <param name="stream"></param>
         protected internal override JsonReader CreateJsonReader(Stream stream)
         {
             var streamReader = new StreamReader(stream, Encoding);
             return new JsonTextReader(streamReader);
         }
 
-        /// <summary>
-        /// Non strongly typed deserialization
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="type"></param>
         public object DeserializeObject(string value, Type type)
         {
             return JsonConvert.DeserializeObject(value, type);
         }
 
-        /// <summary>
-        /// Serializes the given object to a json string
-        /// </summary>
-        /// <param name="value">The actual object</param>
-        /// <returns>The json string</returns>
         public string SerializeObject(object value)
         {
             return JsonConvert.SerializeObject(value);
         }
 
-        /// <summary>
-        /// Returns the supported content type
-        /// </summary>
         protected internal override string GetContentType()
         {
             return ContentTypes.Json;
         }
 
-        /// <summary>
-        /// Gets or sets the stream encoding
-        /// </summary>
         public Encoding Encoding
         {
             get { return encoding; }
