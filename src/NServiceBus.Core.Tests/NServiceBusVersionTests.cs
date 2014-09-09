81b243032845572aea43c7b5ac1cf5ae09380ce4
Simon Cropp <simon.cropp@gmail.com>
2014-09-09 11:27:19 +1000

    NServiceBusVersion should still work
    since it is public and obsolete in v5 people can still use it

--- src/NServiceBus.Core.Tests/NServiceBusVersionTests.cs
+++ src/NServiceBus.Core.Tests/NServiceBusVersionTests.cs
@@ -9,6 +9,8 @@
         [Test]
         public void Verify_has_a_version_and_can_be_parsed()
         {
+            Assert.IsNotNullOrEmpty(NServiceBusVersion.Version);
+            Version.Parse(NServiceBusVersion.Version);
             Assert.IsNotNullOrEmpty(GitFlowVersion.MajorMinor);
             Version.Parse(GitFlowVersion.MajorMinor);
             Assert.IsNotNullOrEmpty(GitFlowVersion.MajorMinorPatch);
