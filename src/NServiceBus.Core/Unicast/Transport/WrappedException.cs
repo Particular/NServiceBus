e37d685b4c1c134f96290638de2d43ecc8bc96a5
Simon Cropp <simon.cropp@gmail.com>
2014-09-09 10:41:37 +1000

    make WrappedException serializable

--- src/NServiceBus.Core/Unicast/Transport/WrappedException.cs
+++ src/NServiceBus.Core/Unicast/Transport/WrappedException.cs
@@ -1,9 +1,14 @@
 namespace NServiceBus
 {
     using System;
+    using System.Runtime.Serialization;
 
+    [Serializable]
     class WrappedException : Exception
     {
+        protected WrappedException(SerializationInfo info, StreamingContext context)
+        {
+        }
         public WrappedException(Exception exceptionThatIsWrapped)
             : base(String.Empty, exceptionThatIsWrapped)
         {
