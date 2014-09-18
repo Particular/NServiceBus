bcdcb5d240e5ba407c467d70b08fe34ac8a540b3
Andreas Öhlund <andreas.ohlund@particular.net>
2014-09-18 13:24:16 +0200

    Removed obsoletes
    Also bumped to inmemory obsolete to 5.5

--- src/NServiceBus.Core/IInMemoryOperations.cs
+++ src/NServiceBus.Core/IInMemoryOperations.cs
@@ -1,7 +1,7 @@
 ﻿#pragma warning disable 1591
 namespace NServiceBus
 {
-    [ObsoleteEx(RemoveInVersion = "6", TreatAsErrorFromVersion = "5.1", Message = "Removed to reduce complexity and API confusion.")]
+    [ObsoleteEx(RemoveInVersion = "6", TreatAsErrorFromVersion = "5.5", Message = "Removed to reduce complexity and API confusion.")]
     public interface IInMemoryOperations
     {
     }
