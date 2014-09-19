a82851e1e69b172463d89b2b48b2035e178f1189
Simon Cropp <simon.cropp@gmail.com>
2014-09-19 14:40:12 +1000

    remove ripple

--- IntegrationTests/TimeoutManager/MyServer/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("MyServer")]
-[assembly: AssemblyProduct("MyServer")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
