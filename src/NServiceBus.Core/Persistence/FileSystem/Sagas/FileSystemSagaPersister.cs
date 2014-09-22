947d098b1ac03afdeb1b83890c3bd460c8153402
Itamar Syn-Hershko <itamar@code972.com>
2014-09-22 19:56:22 +0300

    Initial, very lame implementation of FileSystem persisters

---
+++ src/NServiceBus.Core/Persistence/FileSystem/Sagas/FileSystemSagaPersister.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace NServiceBus.Persistence.FileSystem.Sagas
+{
+    using System.IO;
+    using NServiceBus.Saga;
+
+    class FileSystemSagaPersister : ISagaPersister
+    {
+        // TODO Saga serialization / deserialization
+
+        string sagasStoragePath = @"z:\sagas\";
+
+        public void Save(IContainSagaData saga)
+        {
+            // TODO file locking
+
+            File.WriteAllText(Path.Combine(sagasStoragePath, saga.Id.ToString()), saga.ToString()); // TODO
+        }
+
+        public void Update(IContainSagaData saga)
+        {
+            Save(saga);
+        }
+
+        public TSagaData Get<TSagaData>(Guid sagaId) where TSagaData : IContainSagaData
+        {
+            throw new NotImplementedException();
+        }
+
+        public TSagaData Get<TSagaData>(string propertyName, object propertyValue) where TSagaData : IContainSagaData
+        {
+            // TODO do we really want to iterate through all files, deserialize and compare values?
+
+            throw new NotImplementedException();
+        }
+
+        public void Complete(IContainSagaData saga)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/NServiceBus.Core/Persistence/FileSystem/SerilalizationHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NServiceBus.Persistence.FileSystem
+{
+    using NServiceBus.Timeout.Core;
+
+    static class SerilalizationHelpers
+    {
+        public static string ToTabbedString(this TimeoutData timeout, string id)
+        {
+            // This is a simplified version of serialization, as some important fields are left behind
+            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}", id, timeout.SagaId, timeout.Time, timeout.Destination, timeout.OwningTimeoutManager);
+        }
+
+        public static TimeoutData ToTimeoutData(this string str)
+        {
+            var items = str.Split(new[] { '\t' });
+            return new TimeoutData
+            {
+                Id = items[0],
+                SagaId = Guid.Parse(items[1]),
+                Time = DateTime.Parse(items[2]),
+                Destination = Address.Parse(items[3]),
+                OwningTimeoutManager = items[4],
+            };
+        }
+    }
+}
