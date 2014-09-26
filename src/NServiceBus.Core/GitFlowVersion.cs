f2dbf13866597c74e18558d92227d949a4476094
Andreas Öhlund <andreas.ohlund@particular.net>
2014-09-26 09:44:47 +0200

    Updated how we pull version info from GV

--- src/NServiceBus.Core/GitFlowVersion.cs
+++ src/NServiceBus.Core/GitFlowVersion.cs
@@ -8,10 +8,12 @@ namespace NServiceBus
         {
             var assembly = typeof(GitFlowVersion).Assembly;
             var gitFlowVersionInformationType = assembly.GetType("GitVersionInformation", true);
-            var fieldInfo = gitFlowVersionInformationType.GetField("AssemblyFileVersion");
-            var assemblyFileVersion = Version.Parse((string)fieldInfo.GetValue(null));
-            MajorMinor = assemblyFileVersion.ToString(2);
-            MajorMinorPatch = assemblyFileVersion.ToString(3);
+            var fieldInfo = gitFlowVersionInformationType.GetField("MajorMinorPatch");
+
+            var version = Version.Parse((string) fieldInfo.GetValue(null));
+
+            MajorMinor = version.ToString(2);
+            MajorMinorPatch = version.ToString(3);
         }
 
         public static string MajorMinor;
