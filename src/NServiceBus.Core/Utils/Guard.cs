6751379dc21402b45f0871f5f6f6cc413c0564a4
Cameron MacFarland <distantcam@gmail.com>
2014-10-03 11:37:59 +0800

    Added guards for type construction. Fixes #2413

---
+++ src/NServiceBus.Core/Utils/Guard.cs
@@ -0,0 +1,15 @@
+﻿namespace NServiceBus.Utils
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    static class Guard
+    {
+        public static void TypeHasDefaultConstructor(Type type, string argumentName)
+        {
+            if (type.GetConstructors(BindingFlags.Instance).All(ctor => ctor.GetParameters().Length != 0))
+                throw new ArgumentException(String.Format("'{0}' must have a default constructor.", argumentName), argumentName);
+        }
+    }
+}
