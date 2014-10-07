78a0fc96f285c46736abb340e1c9db76812ba5cb
Andreas Öhlund <andreas.ohlund@particular.net>
2014-10-07 15:10:54 +0200

    Making sure that message with a pre-existing sagaId won't start new saga. Fixes #2446 Adding failing acpt test for #2446
    Making sure that a messages with a sagaid header won't start new sagas
    
    Since they are targeted to a specific saga a new one should not be created
    if it can't be found.
    
    Fixes #2446
    
    Adding test to verify that saga type should match
    
    So that other sagas could be started

---
+++ src/NServiceBus.AcceptanceTests/Sagas/When_message_has_a_saga_id.cs
@@ -0,0 +1,104 @@
+﻿namespace NServiceBus.AcceptanceTests.Sagas
+{
+    using System;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NServiceBus.Saga;
+    using NUnit.Framework;
+
+    public class When_message_has_a_saga_id : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_not_start_a_new_saga_if_not_found()
+        {
+            var context = Scenario.Define<Context>()
+                .WithEndpoint<SagaEndpoint>(b => b.Given(bus =>
+                {
+                    var message = new MessageWithSagaId();
+
+                    bus.SetMessageHeader(message, Headers.SagaId, Guid.NewGuid().ToString());
+                    bus.SetMessageHeader(message, Headers.SagaType, typeof(MySaga).AssemblyQualifiedName);
+
+                    bus.SendLocal(message);
+                }))
+                .Done(c => c.NotFoundHandlerCalled && c.OtherSagaStarted)
+                .Run(TimeSpan.FromSeconds(15));
+
+            Assert.True(context.NotFoundHandlerCalled);
+            Assert.True(context.OtherSagaStarted);
+            Assert.False(context.MessageHandlerCalled);
+            Assert.False(context.TimeoutHandlerCalled);
+        }
+
+        class MySaga : Saga<MySaga.SagaData>, IAmStartedByMessages<MessageWithSagaId>,
+            IHandleTimeouts<MessageWithSagaId>,
+            IHandleSagaNotFound
+        {
+            public Context Context { get; set; }
+
+            public class SagaData : ContainSagaData
+            {
+            }
+
+            protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaData> mapper)
+            {
+
+            }
+
+            public void Handle(MessageWithSagaId message)
+            {
+                Context.MessageHandlerCalled = true;
+            }
+
+            public void Handle(object message)
+            {
+                Context.NotFoundHandlerCalled = true;
+            }
+
+            public void Timeout(MessageWithSagaId state)
+            {
+                Context.TimeoutHandlerCalled = true;
+            }
+        }
+
+        class MyOtherSaga : Saga<MyOtherSaga.SagaData>, IAmStartedByMessages<MessageWithSagaId>
+        {
+            public Context Context { get; set; }
+
+            public void Handle(MessageWithSagaId message)
+            {
+                Context.OtherSagaStarted = true;
+            }
+            protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaData> mapper)
+            {
+
+            }
+
+            public class SagaData : ContainSagaData
+            {
+            }
+
+        }
+
+
+        class Context : ScenarioContext
+        {
+            public bool NotFoundHandlerCalled { get; set; }
+            public bool MessageHandlerCalled { get; set; }
+            public bool TimeoutHandlerCalled { get; set; }
+            public bool OtherSagaStarted { get; set; }
+        }
+
+        public class SagaEndpoint : EndpointConfigurationBuilder
+        {
+            public SagaEndpoint()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+        }
+
+        public class MessageWithSagaId : IMessage
+        {
+        }
+    }
+}
