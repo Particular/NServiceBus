08898764f6afc9a913bd2611dadaeb9780ffc497
Simon Cropp <simon.cropp@gmail.com>
2014-10-24 18:28:23 +1100

    add failing test

---
+++ src/NServiceBus.AcceptanceTests/Sagas/When_reply_from_a_finder.cs
@@ -0,0 +1,100 @@
+﻿namespace NServiceBus.AcceptanceTests.Sagas
+{
+    using System;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NServiceBus.Saga;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_reply_from_a_finder
+    {
+        [Test]
+        public void Should_be_received_by_handler()
+        {
+            var context = new Context
+            {
+                Id = Guid.NewGuid()
+            };
+
+            Scenario.Define(context)
+                .WithEndpoint<SagaEndpoint>(b => b.Given((bus, c) => bus.SendLocal(new StartSagaMessage
+                {
+                    Id = c.Id
+                })))
+                .Done(c => c.HandlerFired)
+                .Run();
+
+            Assert.True(context.HandlerFired);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool HandlerFired { get; set; }
+            public Guid Id { get; set; }
+        }
+
+        public class SagaEndpoint : EndpointConfigurationBuilder
+        {
+            public SagaEndpoint()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            class CustomFinder : IFindSagas<TestSaga.SagaData>.Using<StartSagaMessage>
+            {
+                public IBus Bus { get; set; }
+                public Context Context { get; set; }
+
+                public TestSaga.SagaData FindBy(StartSagaMessage message)
+                {
+                    Bus.Reply(new SagaNotFoundMessage
+                    {
+                        Id = Context.Id
+                    });
+                    return null;
+                }
+            }
+
+            public class TestSaga : Saga<TestSaga.SagaData>,
+                IAmStartedByMessages<StartSagaMessage>
+            {
+                public Context Context { get; set; }
+
+                public void Handle(StartSagaMessage message)
+                {
+                }
+
+                public class SagaData : ContainSagaData
+                {
+                }
+            }
+
+            public class Handler : IHandleMessages<SagaNotFoundMessage>
+            {
+                public Context Context { get; set; }
+
+                public void Handle(SagaNotFoundMessage message)
+                {
+                    if (Context.Id != message.Id)
+                    {
+                        return;
+                    }
+                    Context.HandlerFired = true;
+                }
+            }
+        }
+
+        [Serializable]
+        public class SagaNotFoundMessage : IMessage
+        {
+            public Guid Id { get; set; }
+        }
+
+        [Serializable]
+        public class StartSagaMessage : IMessage
+        {
+            public Guid Id { get; set; }
+        }
+    }
+}
