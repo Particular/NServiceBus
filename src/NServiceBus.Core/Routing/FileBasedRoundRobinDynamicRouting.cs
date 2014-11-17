08d64d6bc97a2fac867c0c4f988c450b19d6960d
John Simons <john.simons@nservicebus.com>
2014-11-17 11:12:46 +1000

    Standardising name as DynamicRouting

---
+++ src/NServiceBus.Core/Routing/FileBasedRoundRobinDynamicRouting.cs
@@ -0,0 +1,18 @@
+namespace NServiceBus.Features
+{
+    using System;
+    using NServiceBus.Routing;
+
+    class FileBasedRoundRobinDynamicRouting : Feature
+    {
+        public FileBasedRoundRobinDynamicRouting()
+        {
+            DependsOn<DynamicRouting>();
+        }
+
+        protected internal override void Setup(FeatureConfigurationContext context)
+        {
+            context.Container.RegisterSingleton(new FileBasedRoundRobinDynamicRoutingImpl(context.Settings.Get<string>("FileBasedRouting.BasePath"), TimeSpan.FromSeconds(5)));
+        }
+    }
+}
