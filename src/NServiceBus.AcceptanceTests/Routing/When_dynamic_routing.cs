50f48a6e707e1671d25c22a7d72e057dd3c4e7e9
John Simons <john.simons@nservicebus.com>
2014-11-20 10:42:50 +1000

    Added disconnect AT

--- src/NServiceBus.AcceptanceTests/Routing/When_dynamic_routing.cs
+++ src/NServiceBus.AcceptanceTests/Routing/When_dynamic_routing.cs
@@ -36,6 +36,9 @@
 
             Scenario.Define(context)
                     .WithEndpoint<Sender2>(b => b.Given((bus, c) => bus.Send(new Request())))
+                    {
+                        bus.Send(new Request());
+                    }))
                     .WithEndpoint<Receiver1>()
                     .WithEndpoint<Receiver2>()
                     .Done(c => c.SafeDisconnectReceived)
