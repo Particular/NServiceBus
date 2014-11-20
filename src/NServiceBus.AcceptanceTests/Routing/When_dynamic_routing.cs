625f68438d2f2cd9125189c83e64fa39d7443f51
John Simons <john.simons@nservicebus.com>
2014-11-20 10:42:50 +1000

    Added disconnect AT

--- src/NServiceBus.AcceptanceTests/Routing/When_dynamic_routing.cs
+++ src/NServiceBus.AcceptanceTests/Routing/When_dynamic_routing.cs
@@ -4,6 +4,9 @@
     using System.IO;
     using NServiceBus.AcceptanceTesting;
     using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NServiceBus.Transports;
+    using NServiceBus.Unicast;
+    using NServiceBus.Unicast.Transport;
     using NUnit.Framework;
 
     public class When_dynamic_routing : NServiceBusAcceptanceTest
@@ -14,23 +17,74 @@
             var context = new Context();
 
             Scenario.Define(context)
-                    .WithEndpoint<Sender>(b => b.Given((bus, c) =>
+                .WithEndpoint<Sender>(b => b.Given((bus, c) =>
+                {
+                    bus.Send(new Request());
+                }))
+                .WithEndpoint<Receiver1>()
+                .WithEndpoint<Receiver2>()
+                .Done(c => c.Receiver1TimesCalled > 4 && c.Receiver2TimesCalled > 4)
+                .Run();
+
+            Assert.IsTrue(context.Receiver1TimesCalled > 4);
+            Assert.IsTrue(context.Receiver2TimesCalled > 4);
+        }
+
+        [Test]
+        public void Should_notify_when_safe_to_disconnect()
+        {
+            var context = new Context();
+
+            Scenario.Define(context)
+                    .WithEndpoint<Sender2>(b => b.Given((bus, c) =>
                     {
                         bus.Send(new Request());
                     }))
                     .WithEndpoint<Receiver1>()
                     .WithEndpoint<Receiver2>()
-                    .Done(c => c.Receiver1TimesCalled > 4 && c.Receiver2TimesCalled > 4)
+                    .Done(c => c.SafeDisconnectReceived)
                     .Run();
 
-            Assert.IsTrue(context.Receiver1TimesCalled > 4);
-            Assert.IsTrue(context.Receiver2TimesCalled > 4);
+            Assert.IsTrue(context.SafeDisconnectReceived);
         }
 
         public class Context : ScenarioContext
         {
             public int Receiver1TimesCalled { get; set; }
             public int Receiver2TimesCalled { get; set; }
+            public bool SafeDisconnectReceived { get; set; }
+        }
+
+        public class Sender2 : EndpointConfigurationBuilder
+        {
+            public Sender2()
+            {
+                var basePath = AppDomain.CurrentDomain.BaseDirectory;
+
+                File.WriteAllLines(Path.Combine(basePath, "Receiver.txt"), new[]
+                {
+                    "DynamicRouting.Receiver1",
+                    "DynamicRouting.Receiver2"
+                });
+
+                EndpointSetup<DefaultServer>(c => c.UseDynamicRouting<FileBasedDynamicRouting>()
+                    .LookForFilesIn(basePath)
+                    .WithTranslator(address => "Receiver"))
+                    .AddMapping<Request>(typeof(Receiver1));
+            }
+
+            public class ResponseHandler : IHandleMessages<Response>
+            {
+                public ISendMessages MessageSender { get; set; }
+
+                public void Handle(Response message)
+                {
+                    var transportMessage = ControlMessage.Create();
+                    transportMessage.Headers["NServiceBus.DisconnectMessage"] = "foo";
+
+                    MessageSender.Send(transportMessage, new SendOptions("DynamicRouting.Receiver1"));
+                }
+            }
         }
 
         public class Sender : EndpointConfigurationBuilder
@@ -91,6 +145,27 @@
                     {
                         EndpointName = "Receiver1"
                     });
+                }
+            }
+
+            public class RegisterDisconnects : IWantToRunWhenBusStartsAndStops
+            {
+                readonly BusNotifications notifications;
+                readonly Context context;
+
+                public RegisterDisconnects(BusNotifications notifications, Context context)
+                {
+                    this.notifications = notifications;
+                    this.context = context;
+                }
+
+                public void Start()
+                {
+                    notifications.Endpoint.SafeToDisconnect.Subscribe(disconnect => context.SafeDisconnectReceived = true);
+                }
+
+                public void Stop()
+                {
                 }
             }
         }
