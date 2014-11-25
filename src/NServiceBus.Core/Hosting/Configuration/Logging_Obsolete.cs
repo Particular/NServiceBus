c27e222b1e72773902f6f465adb5b4ca6e7509d0
John Simons <john.simons@nservicebus.com>
2014-11-25 10:33:08 +1000

    Updated obsolete message for IWantCustomLogging
    The best replacement for this obsolete interface is the constructor of the class that implements IConfigureThisEndpoint.

--- src/NServiceBus.Core/Hosting/Configuration/Logging_Obsolete.cs
+++ src/NServiceBus.Core/Hosting/Configuration/Logging_Obsolete.cs
@@ -11,8 +11,8 @@ namespace NServiceBus
     }
 
     [ObsoleteEx(
-        TreatAsErrorFromVersion = "5.0", 
-        Message = "Use the `NServiceBus.Hosting.Profiles.IConfigureLoggingForProfile` interface which is contained with in the `NServiceBus.Host` nuget. ",
+        TreatAsErrorFromVersion = "5.0",
+        Message = "Use the `NServiceBus.Hosting.Profiles.IConfigureLoggingForProfile<T>` interface which is contained with in the `NServiceBus.Host` nuget.",
         RemoveInVersion = "6.0")]
     public interface IConfigureLoggingForProfile<T> 
     {
@@ -20,7 +20,7 @@ namespace NServiceBus
 
     [ObsoleteEx(
         TreatAsErrorFromVersion = "5.0",
-        Message = "Use the `NServiceBus.Hosting.Profiles.IConfigureLogging` interface which is contained with in the `NServiceBus.Host` nuget. ",
+        Message = "Configure logging in the constructor of the class that implements IConfigureThisEndpoint.",
         RemoveInVersion = "6.0")]
     public interface IWantCustomLogging
     {
