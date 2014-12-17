dedafedbdac179667171fb9ecdbdaa7fe66749b4
John Simons <john.simons@nservicebus.com>
2014-12-17 11:05:01 +1000

    Provides support for property handler injection
    Fixes #2600

---
+++ src/NServiceBus.AcceptanceTests/Basic/When_injecting_handler_props.cs
@@ -0,0 +1,67 @@
+﻿namespace NServiceBus.AcceptanceTests.Basic
+{
+    using System;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_injecting_handler_props : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Run()
+        {
+            var context = new Context();
+
+            Scenario.Define(context)
+                    .WithEndpoint<Receiver>(c=>c.When(b=>b.SendLocal(new MyMessage())))
+                    .Done(c => c.WasCalled)
+                    .Run();
+
+            Assert.AreEqual(10, context.Number);
+            Assert.AreEqual("Foo", context.Name);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool WasCalled { get; set; }
+            public string Name { get; set; }
+            public int Number { get; set; }
+        }
+
+        public class Receiver : EndpointConfigurationBuilder
+        {
+            public Receiver()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.InitializeHandlerProperty<MyMessageHandler>("Number", 10);
+                    c.InitializeHandlerProperty<MyMessageHandler>("Name", "Foo");
+                });
+
+            }
+
+            public class MyMessageHandler : IHandleMessages<MyMessage>
+            {
+                public Context Context { get; set; }
+
+                public IBus Bus { get; set; }
+
+                public string Name { get; set; }
+
+                public int Number { get; set; }
+
+                public void Handle(MyMessage message)
+                {
+                    Context.Number = Number;
+                    Context.Name = Name;
+                    Context.WasCalled = true;
+                }
+            }
+        }
+
+        [Serializable]
+        public class MyMessage : ICommand
+        {
+        }
+    }
+}
---
+++ src/NServiceBus.Core/ConfigureHandlerSettings.cs
@@ -0,0 +1,31 @@
+namespace NServiceBus
+{
+    using System;
+    using System.Collections.Generic;
+    using NServiceBus.ObjectBuilder;
+
+    /// <summary>
+    /// Extension methods for injecting props in <see cref="IHandleMessages{T}"/>.
+    /// </summary>
+    public static class ConfigureHandlerSettings
+    {
+        /// <summary>
+        /// Initializes <see cref="IHandleMessages{T}"/> with the specified properties.
+        /// </summary>
+        /// <typeparam name="THandler">The <see cref="IHandleMessages{T}"/> type.</typeparam>
+        /// <param name="config">The configuration instance.</param>
+        /// <param name="property">The property name to be injected.</param>
+        /// <param name="value">The value to assign to the <paramref name="property"/>.</param>
+        public static void InitializeHandlerProperty<THandler>(this BusConfiguration config, string property, object value)
+        {
+            List<Action<IConfigureComponents>> list;
+            if (!config.Settings.TryGet("NServiceBus.HandlerProperties", out list))
+            {
+                list = new List<Action<IConfigureComponents>>();
+                config.Settings.Set("NServiceBus.HandlerProperties", list);
+            }
+
+            list.Add(c=> c.ConfigureProperty<THandler>(property, value));
+        }
+    }
+}
