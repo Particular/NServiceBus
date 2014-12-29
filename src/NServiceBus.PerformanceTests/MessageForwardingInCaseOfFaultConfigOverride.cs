80e5735747f5950ba64901ae01b5666d855fac52
SzymonPobiega <szymon.pobiega@gmail.com>
2014-12-29 22:26:52 +0100

    Added missing config override to perf tests

---
+++ src/NServiceBus.PerformanceTests/MessageForwardingInCaseOfFaultConfigOverride.cs
@@ -0,0 +1,16 @@
+﻿namespace Runner
+{
+    using NServiceBus.Config;
+    using NServiceBus.Config.ConfigurationSource;
+
+    class MessageForwardingInCaseOfFaultConfigOverride : IProvideConfiguration<MessageForwardingInCaseOfFaultConfig>
+    {
+        public MessageForwardingInCaseOfFaultConfig GetConfiguration()
+        {
+            return new MessageForwardingInCaseOfFaultConfig()
+            {
+                ErrorQueue = "error"
+            };
+        }
+    }
+}
