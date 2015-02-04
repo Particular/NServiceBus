dbceb0092b16b99af95693d85e61dc7eb70fb358
Simon Cropp <simon.cropp@gmail.com>
2015-02-04 15:34:03 +1100

    Use ConcurrentDictionary for TypeToNameLookup
    fixes #1764

--- src/NServiceBus.Core/Utils/Reflection/ExtensionMethods.cs
+++ src/NServiceBus.Core/Utils/Reflection/ExtensionMethods.cs
@@ -16,7 +16,7 @@ namespace NServiceBus.Utils.Reflection
             var defaultConstructor = type.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { }, null);
             if (defaultConstructor != null)
             {
-                return (T)defaultConstructor.Invoke(null); 
+                return (T)defaultConstructor.Invoke(null);
             }
 
             return (T)Activator.CreateInstance(type);
@@ -43,41 +43,34 @@ namespace NServiceBus.Utils.Reflection
         /// </summary>
         public static string SerializationFriendlyName(this Type t)
         {
-            lock(TypeToNameLookup)
+            return TypeToNameLookup.GetOrAdd(t, type =>
             {
-                string typeName;
-                if (TypeToNameLookup.TryGetValue(t, out typeName))
+                var index = t.Name.IndexOf('`');
+                if (index >= 0)
                 {
-                    return typeName;
+                    var result = t.Name.Substring(0, index) + "Of";
+                    var args = t.GetGenericArguments();
+                    for (var i = 0; i < args.Length; i++)
+                    {
+                        result += args[i].SerializationFriendlyName();
+                        if (i != args.Length - 1)
+                        {
+                            result += "And";
+                        }
+                    }
+
+                    if (args.Length == 2)
+                    {
+                        if (typeof(KeyValuePair<,>).MakeGenericType(args[0], args[1]) == t)
+                        {
+                            result = "NServiceBus." + result;
+                        }
+                    }
+
+                    return result;
                 }
-            }
-
-            var index = t.Name.IndexOf('`');
-            if (index >= 0)
-            {
-                var result = t.Name.Substring(0, index) + "Of";
-                var args = t.GetGenericArguments();
-                for (var i = 0; i < args.Length; i++)
-                {
-                    result += args[i].SerializationFriendlyName();
-                    if (i != args.Length - 1)
-                        result += "And";
-                }
-
-                if (args.Length == 2)
-                    if (typeof(KeyValuePair<,>).MakeGenericType(args[0], args[1]) == t)
-                        result = "NServiceBus." + result;
-
-                lock(TypeToNameLookup)  
-                    TypeToNameLookup[t] = result;
-
-                return result;
-            }
-
-            lock(TypeToNameLookup)
-                TypeToNameLookup[t] = t.Name;
-
-            return t.Name;
+                return type.Name;
+            });
         }
 
         static byte[] MsPublicKeyToken = typeof(string).Assembly.GetName().GetPublicKeyToken();
@@ -110,11 +103,9 @@ namespace NServiceBus.Utils.Reflection
                    type == typeof(IEvent);
         }
 
-        static Dictionary<Type, string> TypeToNameLookup = new Dictionary<Type, string>();
+        static ConcurrentDictionary<Type, string> TypeToNameLookup = new ConcurrentDictionary<Type, string>();
 
-
-
-        static byte[] nsbPublicKeyToken= typeof(ExtensionMethods).Assembly.GetName().GetPublicKeyToken();
+        static byte[] nsbPublicKeyToken = typeof(ExtensionMethods).Assembly.GetName().GetPublicKeyToken();
 
         public static bool IsFromParticularAssembly(this Type type)
         {
