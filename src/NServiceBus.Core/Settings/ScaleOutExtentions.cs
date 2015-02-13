c677e2951595602c6c8dcc47682058695926d920
danielmarbach <daniel.marbach@particular.net>
2015-02-13 16:43:36 +0100

    Removed obsolete scaleout

--- src/NServiceBus.Core/Settings/ScaleOutExtentions.cs
+++ src/NServiceBus.Core/Settings/ScaleOutExtentions.cs
@@ -1,6 +1,5 @@
 namespace NServiceBus
 {
-    using System;
     using Settings;
 
     /// <summary>
@@ -14,17 +13,6 @@ namespace NServiceBus
         public static ScaleOutSettings ScaleOut(this BusConfiguration config)
         {
             return new ScaleOutSettings(config);
-        }
-
-#pragma warning disable 1591
-        // ReSharper disable UnusedParameter.Global
-        [ObsoleteEx(
-            Message = "Use `configuration.ScaleOut().UseSingleBrokerQueue()`, where `configuration` is an instance of `BusConfiguration`. If self-hosting the instance can be obtained from `new BusConfiguration()`. if using the NServiceBus Host the instance of `BusConfiguration` will be passed in via the `INeedInitialization` or `IConfigureThisEndpoint` interfaces.", 
-            RemoveInVersion = "6.0",
-            TreatAsErrorFromVersion = "5.0")]
-        public static Configure ScaleOut(this Configure config, Action<ScaleOutSettings> customScaleOutSettings)
-        {
-            throw new NotImplementedException();
         }
     }
 }
