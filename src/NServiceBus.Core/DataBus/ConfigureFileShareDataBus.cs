0ff54b1c567afe22800fe3293a55bb0d1e83d139
danielmarbach <daniel.marbach@particular.net>
2015-02-13 16:37:58 +0100

    Removed obsolete ConfigureFileShareDataBus

--- src/NServiceBus.Core/DataBus/ConfigureFileShareDataBus.cs
+++ src/NServiceBus.Core/DataBus/ConfigureFileShareDataBus.cs
@@ -5,23 +5,8 @@ namespace NServiceBus
     /// <summary>
     /// Contains extension methods to <see cref="BusConfiguration"/> for the file share data bus
     /// </summary>
-    public static partial class ConfigureFileShareDataBus
+    public static class ConfigureFileShareDataBus
     {
-        /// <summary>
-        /// Use the file-based databus implementation with the default binary serializer.
-        /// </summary>
-        /// <param name="config">The configuration.</param>
-        /// <param name="basePath">The location to which to write/read serialized properties for the databus.</param>
-        /// <returns>The configuration.</returns>
-       [ObsoleteEx(
-            Message = "Use `configuration.UseDataBus<FileShareDataBus>().BasePath(basePath)`, where `configuration` is an instance of `BusConfiguration`. If self-hosting the instance can be obtained from `new BusConfiguration()`. if using the NServiceBus Host the instance of `BusConfiguration` will be passed in via the `INeedInitialization` or `IConfigureThisEndpoint` interfaces.",
-            RemoveInVersion = "6.0",
-            TreatAsErrorFromVersion = "5.5")]
-        public static void FileShareDataBus(this BusConfiguration config, string basePath)
-        {
-            config.Settings.Set("FileShareDataBusPath", basePath);
-        }
-
         /// <summary>
         /// The location to which to write/read serialized properties for the databus.
         /// </summary>
