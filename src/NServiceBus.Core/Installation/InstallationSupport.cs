5fbbe789339a4e4de2e00b167606e5f5ea361229
danielmarbach <daniel.marbach@particular.net>
2015-03-04 16:55:32 +0100

    Installations are run as a feature startup task

--- src/NServiceBus.Core/Installation/InstallationSupport.cs
+++ src/NServiceBus.Core/Installation/InstallationSupport.cs
@@ -5,10 +5,9 @@ namespace NServiceBus.Features
     using System.Diagnostics;
     using System.Linq;
     using System.Security.Principal;
-    using Config;
-    using Installation;
-    using ObjectBuilder;
-    using Settings;
+    using NServiceBus.Installation;
+    using NServiceBus.ObjectBuilder;
+    using NServiceBus.Settings;
 
     /// <summary>
     /// Provides support for running installers
@@ -19,16 +18,11 @@ namespace NServiceBus.Features
 
         internal InstallationSupport()
         {
-            if (Debugger.IsAttached)
-            {
-                EnableByDefault();
-            }
+            EnableByDefault();
+            Prerequisite(c => Debugger.IsAttached, "Installation support only runs when the debugger is attached.");
+            RegisterStartupTask<InstallerRunner>();
         }
 
-        /// <summary>
-        /// Invoked if the feature is activated
-        /// </summary>
-        /// <param name="context">The feature context</param>
         protected internal override void Setup(FeatureConfigurationContext context)
         {
             foreach (var installerType in GetInstallerTypes(context))
@@ -43,20 +37,29 @@ namespace NServiceBus.Features
                 .Where(t => typeof(INeedToInstallSomething).IsAssignableFrom(t) && !(t.IsAbstract || t.IsInterface));
         }
 
-        class Starter : IWantToRunWhenConfigurationIsComplete
+        class InstallerRunner : FeatureStartupTask
         {
             IBuilder builder;
             ReadOnlySettings readOnlySettings;
             Configure configure;
 
-            public Starter(IBuilder builder, ReadOnlySettings readOnlySettings, Configure configure)
+            public InstallerRunner(IBuilder builder, ReadOnlySettings readOnlySettings, Configure configure)
             {
                 this.builder = builder;
                 this.readOnlySettings = readOnlySettings;
                 this.configure = configure;
             }
 
-            string GetInstallationUserName(ReadOnlySettings settings)
+            protected override void OnStart()
+            {
+                var username = GetInstallationUserName(readOnlySettings);
+                foreach (var installer in builder.BuildAll<INeedToInstallSomething>())
+                {
+                    installer.Install(username, configure);
+                }
+            }
+
+            static string GetInstallationUserName(ReadOnlySettings settings)
             {
                 string username;
                 if (settings.TryGet(UsernameKey, out username))
@@ -65,15 +68,6 @@ namespace NServiceBus.Features
                 }
 
                 return WindowsIdentity.GetCurrent().Name;
-            }
-
-            public void Run(Configure config)
-            {
-                var username = GetInstallationUserName(readOnlySettings);
-                foreach (var installer in builder.BuildAll<INeedToInstallSomething>())
-                {
-                    installer.Install(username, configure);
-                }
             }
         }
     }
