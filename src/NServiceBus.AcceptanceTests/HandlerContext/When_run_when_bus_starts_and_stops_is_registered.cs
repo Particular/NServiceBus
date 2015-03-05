74436ceae284c6a31ac722f34169f52a10e50f32
danielmarbach <daniel.marbach@particular.net>
2015-03-05 14:28:57 +0100

    Defined failing ATTs for IWantToRunWhenBusStartsAndStops replacements

---
+++ src/NServiceBus.AcceptanceTests/HandlerContext/When_run_when_bus_starts_and_stops_is_registered.cs
@@ -0,0 +1,51 @@
+﻿namespace NServiceBus.AcceptanceTests.HandlerContext
+{
+    using System;
+    using global::NServiceBus.AcceptanceTesting;
+    using global::NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_run_when_bus_starts_and_stops_is_registered : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_execute_start_and_stop()
+        {
+            var context = Scenario.Define<Context>()
+                    .WithEndpoint<StartedEndpoint>()
+                    .Done(c => c.StartCalled && c.StopCalled)
+                    .Run(TimeSpan.FromSeconds(10));
+
+            Assert.True(context.StartCalled);
+            Assert.True(context.StopCalled);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool StopCalled { get; set; }
+            public bool StartCalled { get; set; }
+        }
+
+        public class StartedEndpoint : EndpointConfigurationBuilder
+        {
+            public StartedEndpoint()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            class RunWhenBusStartsAndStops : IRunWhenBusStartsAndStops
+            {
+                public Context Context { get; set; }
+
+                public void Start(RunContext context)
+                {
+                    Context.StartCalled = true;
+                }
+
+                public void Stop(RunContext context)
+                {
+                    Context.StopCalled = true;
+                }
+            }
+        }
+    }
+}
--- src/NServiceBus.Core/IWantToRunWhenBusStartsAndStops.cs
+++ src/NServiceBus.Core/IWantToRunWhenBusStartsAndStops.cs
@@ -16,4 +16,27 @@
         /// </summary>
         void Stop();
     }
+
+    /// <summary>
+    /// Implementers will be invoked when the endpoint starts up.
+    /// Dependency injection is provided for these types.
+    /// </summary>
+    public interface IRunWhenBusStartsAndStops
+    {
+        /// <summary>
+        /// Method called at startup.
+        /// </summary>
+        void Start(RunContext context);
+
+        /// <summary>
+        /// Method called on shutdown.
+        /// </summary>
+        void Stop(RunContext context);
+    }
+
+#pragma warning disable 1591
+    public class RunContext
+    {
+    }
+#pragma warning restore 1591
 }
