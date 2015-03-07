b24ab3ddeec64dcb483e3f6f130c7b5e2f6d3571
John Simons <john.simons@nservicebus.com>
2015-03-07 12:13:24 +1000

    Updated unobtrusive sample for testing

---
+++ IntegrationTests/Unobtrusive/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using Client;
+using NServiceBus;
+
+class Program
+{
+    public static void Main()
+    {
+        var busConfiguration = new BusConfiguration();
+
+        busConfiguration.EnableInstallers();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+        busConfiguration.UseDataBus<FileShareDataBus>()
+            .BasePath(@"..\..\..\DataBusShare\");
+        busConfiguration.RijndaelEncryptionService();
+
+        var bus = Bus.Create(busConfiguration);
+        bus.Start();
+        new CommandSender {Bus = bus}.Start();
+    }
+}
+
