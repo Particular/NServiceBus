0704ccfa84427ede6260a43abf236f2dadcc9cb3
John Simons <john.simons@nservicebus.com>
2015-03-08 08:57:34 +1000

    We do not support registering a custom IDataBus via container
    The explicit API is now the only way

--- src/NServiceBus.Core/DataBus/DataBusFileBased.cs
+++ src/NServiceBus.Core/DataBus/DataBusFileBased.cs
@@ -15,21 +15,14 @@ namespace NServiceBus.Features
         /// </summary>
         protected internal override void Setup(FeatureConfigurationContext context)
         {
-            // We still doing this check here eventhough we now have an explicit API
-            // to register custom IDataBus implementations for backwards compatibility
-            var customDataBusComponentRegistered = context.Container.HasComponent<IDataBus>();
-
-            if (!customDataBusComponentRegistered)
+            string basePath;
+            if (!context.Settings.TryGet("FileShareDataBusPath", out basePath))
             {
-                string basePath;
-                if (!context.Settings.TryGet("FileShareDataBusPath", out basePath))
-                {
-                    throw new InvalidOperationException("Please specify the basepath for FileShareDataBus, eg config.UseDataBus<FileShareDataBus>().BasePath(\"c:\\databus\")");
-                }
-                var dataBus = new FileShareDataBusImplementation(basePath);
-
-                context.Container.RegisterSingleton<IDataBus>(dataBus);
+                throw new InvalidOperationException("Please specify the basepath for FileShareDataBus, eg config.UseDataBus<FileShareDataBus>().BasePath(\"c:\\databus\")");
             }
+            var dataBus = new FileShareDataBusImplementation(basePath);
+
+            context.Container.RegisterSingleton<IDataBus>(dataBus);
         }
     }
 }
