be6bd5fdf4b7ae194af4644aa925a136d17b5945
danielmarbach <daniel.marbach@particular.net>
2015-03-13 15:45:36 +0100

    Split out When_performing_slr ATTs

---
+++ src/NServiceBus.AcceptanceTests/Retries/When_performing_slr_with_serialization_exception.cs
@@ -0,0 +1,25 @@
+﻿namespace NServiceBus.AcceptanceTests.Retries
+{
+    using NServiceBus.AcceptanceTesting;
+    using NUnit.Framework;
+
+    public class When_performing_slr_with_serialization_exception : When_performing_slr
+    {
+        [Test]
+        public void Should_preserve_the_original_body_for_serialization_exceptions()
+        {
+            var context = new Context
+            {
+                SimulateSerializationException = true
+            };
+
+            Scenario.Define(context)
+                .WithEndpoint<RetryEndpoint>(b => b.Given(bus => bus.SendLocal(new MessageToBeRetried())))
+                .AllowExceptions()
+                .Done(c => c.SlrChecksum != default(byte))
+                .Run();
+
+            Assert.AreEqual(context.OriginalBodyChecksum, context.SlrChecksum, "The body of the message sent to slr should be the same as the original message coming off the queue");
+        }
+    }
+}
