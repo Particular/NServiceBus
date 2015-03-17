f587bd60226973369063304e5289dbe294fb58af
Andreas Öhlund <andreas.ohlund@particular.net>
2015-03-17 21:49:11 +0100

    Removed the v3 compat msmq mutator since the same logic was already added to the msmqutilities.convert

--- src/NServiceBus.Core/Transports/Msmq/Config/MsmqTransportConfigurator.cs
+++ src/NServiceBus.Core/Transports/Msmq/Config/MsmqTransportConfigurator.cs
@@ -60,7 +60,6 @@
             new CheckMachineNameForComplianceWithDtcLimitation()
             .Check();
 
-            context.Container.ConfigureComponent<CorrelationIdMutatorForBackwardsCompatibilityWithV3>(DependencyLifecycle.InstancePerCall);
             context.Container.ConfigureComponent<MsmqUnitOfWork>(DependencyLifecycle.SingleInstance);
 
             var endpointIsTransactional = context.Settings.Get<bool>("Transactions.Enabled");
