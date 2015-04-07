1272b7414d6818d6567f8ce993f963e3ecebcfd5
danielmarbach <daniel.marbach@particular.net>
2015-04-07 17:31:26 +0200

    Handler_throws_new

---
+++ src/NServiceBus.AcceptanceTests/Exceptions/Handler_throws_new.cs
@@ -0,0 +1,113 @@
+﻿namespace NServiceBus.AcceptanceTests.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NServiceBus.Config;
+    using NServiceBus.Features;
+    using NUnit.Framework;
+
+    public class Handler_throws_new : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_receive_exception_from_handler()
+        {
+            var context = new Context();
+
+            Scenario.Define(context)
+                    .WithEndpoint<Endpoint>(b => b.Given(bus => bus.SendLocal(new Message())))
+                    .AllowExceptions()
+                    .Done(c => c.ExceptionReceived)
+                    .Run();
+            Assert.AreEqual(typeof(HandlerException), context.ExceptionType);
+
+            StackTraceAssert.StartsWith(
+@"at NServiceBus.AcceptanceTests.Exceptions.Handler_throws_new.Endpoint.Handler.Handle(Message message, ICommandContext context)
+at NServiceBus.Unicast.Behaviors.MessageHandler.Invoke(Object message, Object context)
+at NServiceBus.InvokeHandlersBehavior.Invoke(Context context, Action next)
+at NServiceBus.HandlerTransactionScopeWrapperBehavior.Invoke(Context context, Action next)
+at NServiceBus.LoadHandlersConnector.Invoke(Context context, Action`1 next)
+at NServiceBus.ApplyIncomingMessageMutatorsBehavior.Invoke(Context context, Action next)
+at NServiceBus.ExecuteLogicalMessagesConnector.Invoke(Context context, Action`1 next)
+at NServiceBus.CallbackInvocationBehavior.Invoke(Context context, Action next)
+at NServiceBus.ApplyIncomingTransportMessageMutatorsBehavior.Invoke(Context context, Action next)
+at NServiceBus.SubscriptionReceiverBehavior.Invoke(Context context, Action next)
+at NServiceBus.UnitOfWorkBehavior.Invoke(Context context, Action next)
+at NServiceBus.ChildContainerBehavior.Invoke(Context context, Action next)
+at NServiceBus.ProcessingStatisticsBehavior.Invoke(Context context, Action next)
+at NServiceBus.EnforceMessageIdBehavior.Invoke(Context context, Action next)
+at NServiceBus.HostInformationBehavior.Invoke(Context context, Action next)
+at NServiceBus.MoveFaultsToErrorQueueBehavior.Invoke(Context context, Action next)", context.StackTrace);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool ExceptionReceived { get; set; }
+            public string StackTrace { get; set; }
+            public Type ExceptionType { get; set; }
+        }
+
+        public class Endpoint : EndpointConfigurationBuilder
+        {
+            public Endpoint()
+            {
+                EndpointSetup<DefaultServer>(b =>
+                {
+                    b.DisableFeature<TimeoutManager>();
+                    b.DisableFeature<SecondLevelRetries>();
+                })
+                    .WithConfig<TransportConfig>(c =>
+                    {
+                        c.MaxRetries = 0;
+                    });
+            }
+
+            class ErrorNotificationSpy : IRunWhenBusStartsAndStops
+            {
+                public Context Context { get; set; }
+
+                public BusNotifications BusNotifications { get; set; }
+
+                public void Start(IRunContext context)
+                {
+                    BusNotifications.Errors.MessageSentToErrorQueue.Subscribe(e =>
+                    {
+                        Context.ExceptionType = e.Exception.GetType();
+                        Context.StackTrace = e.Exception.StackTrace;
+                        Context.ExceptionReceived = true;
+                    });
+                }
+
+                public void Stop(IRunContext context) { }
+            }
+
+            class Handler : IProcessCommands<Message>
+            {
+                public void Handle(Message message, ICommandContext context)
+                {
+                    throw new HandlerException();
+                }
+            }
+        }
+
+        [Serializable]
+        public class Message : IMessage
+        {
+        }
+
+        [Serializable]
+        public class HandlerException : Exception
+        {
+            public HandlerException()
+                : base("HandlerException")
+            {
+            }
+
+            protected HandlerException(SerializationInfo info, StreamingContext context)
+            {
+            }
+        }
+    }
+    
+}
