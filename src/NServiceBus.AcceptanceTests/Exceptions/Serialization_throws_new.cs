dcc3e9e4a2f9c72487ccf3ab6df0433338108799
danielmarbach <daniel.marbach@particular.net>
2015-04-07 17:37:18 +0200

    Serialization_throws_new

---
+++ src/NServiceBus.AcceptanceTests/Exceptions/Serialization_throws_new.cs
@@ -0,0 +1,94 @@
+﻿namespace NServiceBus.AcceptanceTests.Exceptions
+{
+    using System;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NServiceBus.Config;
+    using NServiceBus.Features;
+    using NServiceBus.MessageMutator;
+    using NServiceBus.Unicast.Messages;
+    using NUnit.Framework;
+
+    public class Serialization_throws_new : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_receive_MessageDeserializationException()
+        {
+            var context = new Context();
+
+            Scenario.Define(context)
+                    .WithEndpoint<Endpoint>(b => b.Given(bus => bus.SendLocal(new Message())))
+                    .AllowExceptions()
+                    .Done(c => c.ExceptionReceived)
+                    .Run();
+
+            Assert.AreEqual(typeof(MessageDeserializationException), context.ExceptionType);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool ExceptionReceived { get; set; }
+            public Type ExceptionType { get; set; }
+        }
+
+        public class Endpoint : EndpointConfigurationBuilder
+        {
+            public Endpoint()
+            {
+                EndpointSetup<DefaultServer>(b =>
+                {
+                    b.RegisterComponents(c => c.ConfigureComponent<CorruptionMutator>(DependencyLifecycle.InstancePerCall));
+                    b.DisableFeature<TimeoutManager>();
+                    b.DisableFeature<SecondLevelRetries>();
+                })
+                    .WithConfig<TransportConfig>(c =>
+                    {
+                        c.MaxRetries = 0;
+                    });
+            }
+
+            class ErrorNotificationSpy : IRunWhenBusStartsAndStops
+            {
+                public Context Context { get; set; }
+
+                public BusNotifications BusNotifications { get; set; }
+
+                public void Start(IRunContext context)
+                {
+                    BusNotifications.Errors.MessageSentToErrorQueue.Subscribe(e =>
+                    {
+                        Context.ExceptionType = e.Exception.GetType();
+                        Context.ExceptionReceived = true;
+                    });
+                }
+
+                public void Stop(IRunContext context) { }
+            }
+
+
+            class CorruptionMutator : IMutateTransportMessages
+            {
+                public void MutateIncoming(TransportMessage transportMessage)
+                {
+                    transportMessage.Body[1]++;
+                }
+
+                public void MutateOutgoing(LogicalMessage logicalMessage, TransportMessage transportMessage)
+                {
+                }
+            }
+
+            class Handler : IProcessCommands<Message>
+            {
+                public void Handle(Message message, ICommandContext context)
+                {
+                }
+            }
+        }
+
+        public class Message : ICommand
+        {
+        }
+    }
+
+}
