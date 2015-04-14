a7eb4c5f8a474413975dd9ed9842e7a7defc90dd
John Simons <john.simons@nservicebus.com>
2015-04-14 13:12:25 +1000

    Made SendOptions a non derived class
    It would allow for inclosistent behavior in the API usage

--- src/NServiceBus.Core/SendOptions.cs
+++ src/NServiceBus.Core/SendOptions.cs
@@ -1,13 +1,20 @@
 namespace NServiceBus
 {
     using System;
+    using System.Collections.Generic;
 
     /// <summary>
     ///     Allows the users to control how the send is performed
     /// </summary>
-    public class SendOptions : SendLocalOptions
+    public class SendOptions
     {
         internal MessageIntentEnum Intent = MessageIntentEnum.Send;
+        readonly DateTime? at;
+        readonly string correlationId;
+        readonly TimeSpan? delay;
+        internal Dictionary<string, object> Context = new Dictionary<string, object>();
+        internal Dictionary<string, string> Headers = new Dictionary<string, string>();
+        internal string MessageId;
 
         /// <summary>
         ///     Creates an instance of <see cref="SendOptions" />.
@@ -16,10 +23,34 @@ namespace NServiceBus
         /// <param name="correlationId">Specifies a custom currelation id for the message.</param>
         /// <param name="deliverAt">Tells the bus to deliver the message at the given time.</param>
         /// <param name="delayDeliveryFor">Tells the bus to wait the specified amount of time before delivering the message.</param>
-        public SendOptions(string destination = null, string correlationId = null, DateTime? deliverAt = null, TimeSpan? delayDeliveryFor = null):
-            base(correlationId, deliverAt, delayDeliveryFor)
+        public SendOptions(string destination = null, string correlationId = null, DateTime? deliverAt = null, TimeSpan? delayDeliveryFor = null)
         {
+            if (deliverAt != null && delayDeliveryFor != null)
+            {
+                throw new ArgumentException("Ensure you either set `deliverAt` or `delayDeliveryFor`, but not both.");
+            }
+
+            delay = delayDeliveryFor;
+            at = deliverAt;
+            this.correlationId = correlationId;
             Destination = destination;
+        }
+
+        internal string Destination { get; private set; }
+
+        internal TimeSpan? Delay
+        {
+            get { return delay; }
+        }
+
+        internal DateTime? At
+        {
+            get { return at; }
+        }
+
+        internal string CorrelationId
+        {
+            get { return correlationId; }
         }
 
         /// <summary>
@@ -27,19 +58,17 @@ namespace NServiceBus
         /// </summary>
         /// <param name="key">The header key.</param>
         /// <param name="value">The header value.</param>
-        public new SendOptions AddHeader(string key, string value)
+        public SendOptions AddHeader(string key, string value)
         {
             Headers[key] = value;
             return this;
         }
 
-        internal string Destination { get; private set; }
-
         /// <summary>
         ///     Sets a custom message id for this message.
         /// </summary>
         /// <param name="messageId"></param>
-        public new SendOptions SetCustomMessageId(string messageId)
+        public SendOptions SetCustomMessageId(string messageId)
         {
             MessageId = messageId;
             return this;
