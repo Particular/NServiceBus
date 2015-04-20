571fe6c58d22ff10f6720fb1771749884f6162a6
Sean Feldman <feldman.sean@gmail.com>
2015-04-20 16:51:30 -0600

    Testing assembly scanning with BusConfiguration

--- src/NServiceBus.Core.Tests/AssemblyScanner/When_scanning_directory_with_nested_directories.cs
+++ src/NServiceBus.Core.Tests/AssemblyScanner/When_scanning_directory_with_nested_directories.cs
@@ -1,32 +1,35 @@
 namespace NServiceBus.Core.Tests.AssemblyScanner
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
-    using NServiceBus.Hosting.Helpers;
     using NUnit.Framework;
 
     [TestFixture]
     public class When_scanning_directory_with_nested_directories
-    {
+    {          
         [Test]
         public void Should_not_scan_nested_directories_by_default()
         {
-            var assemblyScanner = new AssemblyScanner(AssemblyScannerTests.GetTestAssemblyDirectory());
+            var busConfiguration = new BusConfiguration();
+            busConfiguration.BuildConfiguration();
 
-            var scannedNestedAssembly = assemblyScanner.GetScannableAssemblies().SkippedFiles.Any(x => x.FilePath.EndsWith("nested.dll"));
-            Assert.False(scannedNestedAssembly, "Was expected not to scan nested assemblies, but nested assembly was scanned.");
+            var scanedTypes = busConfiguration.Settings.Get<IList<Type>>("TypesToScan");
+            var foundTypeFromNestedAssembly = scanedTypes.Any(x => x.Name == "NestedClass");
+            Assert.False(foundTypeFromNestedAssembly, "Was expected not to scan nested assemblies, but nested assembly was scanned.");
         }
 
 
         [Test]
         public void Should_scan_nested_directories_if_requested()
         {
-            var assemblyScanner = new AssemblyScanner(AssemblyScannerTests.GetTestAssemblyDirectory())
-            {
-                ScanNestedDirectories = true
-            };
+            var busConfiguration = new BusConfiguration();
+            busConfiguration.ScanAssembliesInNestedDirectories();
+            busConfiguration.BuildConfiguration();
 
-            var scannedNestedAssembly = assemblyScanner.GetScannableAssemblies().SkippedFiles.Any(x => x.FilePath.EndsWith("nested.dll"));
-            Assert.True(scannedNestedAssembly, "Was expected to scan nested assemblies, but nested assembly were not scanned.");
+            var scanedTypes = busConfiguration.Settings.Get<IList<Type>>("TypesToScan");
+            var foundTypeFromNestedAssembly = scanedTypes.Any(x => x.Name == "NestedClass");
+            Assert.True(foundTypeFromNestedAssembly, "Was expected to scan nested assemblies, but nested assembly were not scanned.");
         }
     }
 }
