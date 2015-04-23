1669d7ace623ab6cad9736606388764a8cf65366
Cameron MacFarland <distantcam@gmail.com>
2015-04-23 10:29:59 +0800

    Added roslyn image tests.

---
+++ src/NServiceBus.Core.Tests/AssemblyScanner/When_checking_image_type.cs
@@ -0,0 +1,30 @@
+﻿namespace NServiceBus.Core.Tests.AssemblyScanner
+{
+    using System.IO;
+    using Hosting.Helpers;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_checking_image_type
+    {
+        [Test]
+        public void roslyn_x86_image_type_correctly_detected()
+        {
+            var file = Path.Combine(AssemblyScannerTests.GetTestAssemblyDirectory(), "RoslynX86.dll");
+
+            var compilationMode = Image.GetCompilationMode(file);
+
+            Assert.That(compilationMode, Is.EqualTo(Image.CompilationMode.CLRx86));
+        }
+
+        [Test]
+        public void roslyn_x64_image_type_correctly_detected()
+        {
+            var file = Path.Combine(AssemblyScannerTests.GetTestAssemblyDirectory(), "RoslynX64.dll");
+
+            var compilationMode = Image.GetCompilationMode(file);
+
+            Assert.That(compilationMode, Is.EqualTo(Image.CompilationMode.CLRx64));
+        }
+    }
+}
