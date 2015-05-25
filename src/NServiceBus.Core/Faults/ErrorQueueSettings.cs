f79974e7d956a8a25d0123797d138d43448807f7
Simon Cropp <simon.cropp@gmail.com>
2015-05-25 23:03:47 +1000

    add error queue code config

--- src/NServiceBus.Core/Faults/ErrorQueueSettings.cs
+++ src/NServiceBus.Core/Faults/ErrorQueueSettings.cs
@@ -1,6 +1,6 @@
 namespace NServiceBus.Faults
 {
-    using System.Configuration;
+    using System;
     using NServiceBus.Config;
     using NServiceBus.Logging;
     using NServiceBus.Settings;
@@ -10,41 +10,53 @@ namespace NServiceBus.Faults
     {
         public static string GetConfiguredErrorQueue(ReadOnlySettings settings)
         {
-            string errorQueue = null;
+            string errorQueue;
+
+            if (settings.TryGet("errorQueue", out errorQueue))
+            {
+                Logger.Debug("Error queue retrieved from code configuration via 'BusConfiguration.ErrorQueue().");
+                return errorQueue;
+            }
 
             var section = settings.GetConfigSection<MessageForwardingInCaseOfFaultConfig>();
             if (section != null)
             {
-                if (string.IsNullOrWhiteSpace(section.ErrorQueue))
+                if (!string.IsNullOrWhiteSpace(section.ErrorQueue))
                 {
-                    throw new ConfigurationErrorsException(
-                        "'MessageForwardingInCaseOfFaultConfig' configuration section is found but 'ErrorQueue' value is missing." +
-                        "\n The following is an example for adding such a value to your app config: " +
-                        "\n <MessageForwardingInCaseOfFaultConfig ErrorQueue=\"error\"/> \n");
+                    Logger.Debug("Error queue retrieved from <MessageForwardingInCaseOfFaultConfig> element in config file.");
+                    return section.ErrorQueue;
                 }
 
-                Logger.Debug("Error queue retrieved from <MessageForwardingInCaseOfFaultConfig> element in config file.");
-
-                errorQueue = section.ErrorQueue;
+                throw new Exception(
+                    @"'MessageForwardingInCaseOfFaultConfig' configuration section is found but 'ErrorQueue' value is empty. 
+Please take on of the following actions: 
+- set the error queue at configuration time using 'BusConfiguration.SendFailedMessagesTo()'
+- Add a valid value to to your app config. For example: 
+ <MessageForwardingInCaseOfFaultConfig ErrorQueue=""error""/>");
             }
-            else
+
+            var registryErrorQueue = RegistryReader.Read("ErrorQueue");
+            if (registryErrorQueue != null)
             {
-                var registryErrorQueue = RegistryReader.Read("ErrorQueue");
                 if (!string.IsNullOrWhiteSpace(registryErrorQueue))
                 {
                     Logger.Debug("Error queue retrieved from registry settings.");
-                    errorQueue = registryErrorQueue;
+                    return registryErrorQueue;
                 }
+                throw new Exception(
+                    @"'ErrorQueue' read from registry but the value is empty. 
+Please take on of the following actions: 
+- set the error queue at configuration time using 'BusConfiguration.SendFailedMessagesTo()'
+- add a 'MessageForwardingInCaseOfFaultConfig' section to your app.config
+- give 'HKEY_LOCAL_MACHINE\SOFTWARE\ParticularSoftware\ServiceBus\ErrorQueue' a valid value for your error queue");
             }
 
-            if (errorQueue == null)
-            {
-                throw new ConfigurationErrorsException("Faults forwarding requires an error queue to be specified. Please add a 'MessageForwardingInCaseOfFaultConfig' section to your app.config" +
-                                                       "\n or configure a global one using the powershell command: Set-NServiceBusLocalMachineSettings -ErrorQueue {address of error queue}");
-            }
-
-            return errorQueue;
-
+            throw new Exception(
+@"Faults forwarding requires an error queue to be specified. 
+Please take on of the following actions: 
+- set the error queue at configuration time using 'BusConfiguration.SendFailedMessagesTo()'
+- add a 'MessageForwardingInCaseOfFaultConfig' section to your app.config
+- configure a global error queue in the registry using the powershell command: Set-NServiceBusLocalMachineSettings -ErrorQueue {address of error queue}");
         }
 
         static ILog Logger = LogManager.GetLogger(typeof(ErrorQueueSettings));
