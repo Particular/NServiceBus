0bfa39409434099b161596d040971497f207b299
Simon Cropp <simon.cropp@gmail.com>
2015-06-05 15:52:20 +1000

    make MessageProcessingAbortedException internal

--- src/NServiceBus.Core/Unicast/Behaviors/MessageProcessingAbortedException.cs
+++ src/NServiceBus.Core/Unicast/Behaviors/MessageProcessingAbortedException.cs
@@ -1,28 +1,8 @@
 namespace NServiceBus
 {
     using System;
-    using System.Runtime.Serialization;
 
-    /// <summary>
-    /// Informs that message processing has been aborted.
-    /// </summary>
-    [Serializable]
-    public class MessageProcessingAbortedException : Exception
+    class MessageProcessingAbortedException : Exception
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        public MessageProcessingAbortedException()
-        {
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        protected MessageProcessingAbortedException(
-            SerializationInfo info,
-            StreamingContext context) : base(info, context)
-        {
-        }
     }
 }
