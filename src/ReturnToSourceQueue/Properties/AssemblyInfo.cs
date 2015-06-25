1711540a0ffcb5bfad49fadecdb62bc7e0281b37
Simon Cropp <simon.cropp@gmail.com>
2015-06-25 13:57:07 +1000

    remove ReturnToSourceQueue
    now exists in https://github.com/ParticularLabs/MsmqReturnToSourceQueue

--- src/ReturnToSourceQueue/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("ReturnToSourceQueue")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCopyright("Copyright 2010-2014 NServiceBus. All rights reserved")]
-[assembly: AssemblyProduct("NServiceBus")]
-[assembly: AssemblyCompany("NServiceBus Ltd.")]
-[assembly: ComVisible(false)]
