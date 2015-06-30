345e7b9ba1f2952c7f2c85a1e8398505e3f0cfa2
Simon Cropp <simon.cropp@gmail.com>
2015-06-30 12:27:16 +1000

    remove XsdGenerator
    now located here
    https://github.com/ParticularLabs/NServiceBus.XmlSerializer.XsdGenerator

--- src/NServiceBus.Serializers.XML.XsdGenerator/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NServiceBus XSD Generator")]
-[assembly: AssemblyDescription("XSD file generator for interface-based message schemas.")]
-[assembly: AssemblyCopyright("Copyright 2010-2014 NServiceBus. All rights reserved")]
-[assembly: AssemblyProduct("NServiceBus")]
-[assembly: AssemblyCompany("NServiceBus Ltd.")]
-[assembly: ComVisible(false)]
