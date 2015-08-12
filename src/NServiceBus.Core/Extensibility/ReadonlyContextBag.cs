6feaf3b962d9ceaf34e28805e48c8140c312ae1d
danielmarbach <daniel.marbach@particular.net>
2015-08-12 17:04:47 +0200

    Interface for readonly context bag

---
+++ src/NServiceBus.Core/Extensibility/ReadonlyContextBag.cs
@@ -0,0 +1,23 @@
+namespace NServiceBus.Extensibility
+{
+    /// <summary>
+    /// Context bag which is readonly
+    /// </summary>
+    public interface ReadonlyContextBag
+    {
+        /// <summary>
+        /// Retrieves the specified type from the context.
+        /// </summary>
+        /// <typeparam name="T">The type to retrieve.</typeparam>
+        /// <returns>The type instance.</returns>
+        T Get<T>();
+
+        /// <summary>
+        /// Tries to retrieves the specified type from the context.
+        /// </summary>
+        /// <typeparam name="T">The type to retrieve.</typeparam>
+        /// <param name="result">The type instance.</param>
+        /// <returns><code>true</code> if found, otherwise <code>false</code>.</returns>
+        bool TryGet<T>(out T result);
+    }
+}
