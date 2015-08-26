3ed27057ac3d172ddd7a82772ca5bd02723855d9
Igal Tabachnik <igal.tabachnik@particular.net>
2015-08-26 14:39:20 +0300

    Made SerializationFeatureHelper obsolete - handled by th ConfigureSerialization Feature class

--- src/NServiceBus.Core/Serialization/ConfigureSerialization.cs
+++ src/NServiceBus.Core/Serialization/ConfigureSerialization.cs
@@ -1,6 +1,8 @@
 ﻿namespace NServiceBus.Serialization
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using NServiceBus.Features;
     using NServiceBus.MessageInterfaces.MessageMapper.Reflection;
     using NServiceBus.Serializers;
@@ -56,6 +58,27 @@
 
             var c = context.Container.ConfigureComponent(serializerType, DependencyLifecycle.SingleInstance);
             context.Settings.ApplyTo(serializerType, c);
+        }
+
+        private bool IsDefaultSerializer(FeatureConfigurationContext context)
+        {
+            Guard.AgainstNull("context", context);
+
+            var serializationDefinition = context.Settings.GetSelectedSerializer();
+            return serializationDefinition.ProvidedByFeature() == GetType();
+        }
+
+        private bool IsAdditionalDeserializer(FeatureConfigurationContext context)
+        {
+            Guard.AgainstNull("context", context);
+
+            HashSet<SerializationDefinition> deserializers;
+            if (!context.Settings.TryGet("AdditionalDeserializers", out deserializers))
+            {
+                return false;
+            }
+
+            return deserializers.Any(definition => definition.ProvidedByFeature() == GetType());
         }
     }
 }
--- src/NServiceBus.Core/Serializers/SerializationFeatureHelper.cs
+++ src/NServiceBus.Core/Serializers/SerializationFeatureHelper.cs
@@ -1,41 +1,10 @@
 ﻿namespace NServiceBus.Features
 {
-    using System.Collections.Generic;
-    using System.Linq;
-    using NServiceBus.Serialization;
-
     /// <summary>
     /// Base class for all serialization <see cref="Feature"/>s.
     /// </summary>
+    [ObsoleteEx(Message = "Use the ConfigureSerialization Feature class instead", TreatAsErrorFromVersion = "6.0", RemoveInVersion = "7.0", ReplacementTypeOrMember = "ConfigureSerialization")]
     public static class SerializationFeatureHelper
     {
-        /// <summary>
-        /// Allows serialization features to verify their <see cref="ConfigureSerialization"/> Prerequisites.
-        /// </summary>
-        public static bool IsDefaultSerializer(this ConfigureSerialization serializationFeature, FeatureConfigurationContext context)
-        {
-            Guard.AgainstNull("serializationFeature", serializationFeature);
-            Guard.AgainstNull("context", context);
-
-            var serializationDefinition = context.Settings.GetSelectedSerializer();
-            return serializationDefinition.ProvidedByFeature() == serializationFeature.GetType();
-        }
-
-        /// <summary>
-        /// Allows serialization features to check whether a given <see cref="ConfigureSerialization">feature</see> is configured as an additional deserializer.
-        /// </summary>
-        public static bool IsAdditionalDeserializer(this ConfigureSerialization serializationFeature, FeatureConfigurationContext context)
-        {
-            Guard.AgainstNull("serializationFeature", serializationFeature);
-            Guard.AgainstNull("context", context);
-
-            HashSet<SerializationDefinition> deserializers;
-            if (!context.Settings.TryGet("AdditionalDeserializers", out deserializers))
-            {
-                return false;
-            }
-
-            return deserializers.Any(definition => definition.ProvidedByFeature() == serializationFeature.GetType());
-        }
     }
 }
