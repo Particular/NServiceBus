611a9b869a36b333776e800e4fd0c6625d418364
danielmarbach <daniel.marbach@particular.net>
2015-09-07 13:38:37 +0200

    Cosmetics

--- src/NServiceBus.Core/CriticalError/ConfigureCriticalErrorAction.cs
+++ src/NServiceBus.Core/CriticalError/ConfigureCriticalErrorAction.cs
@@ -20,6 +20,5 @@ namespace NServiceBus
             Guard.AgainstNull("onCriticalError", onCriticalError);
             busConfiguration.Settings.Set("onCriticalErrorAction", onCriticalError);
         }
-
     }
 }
