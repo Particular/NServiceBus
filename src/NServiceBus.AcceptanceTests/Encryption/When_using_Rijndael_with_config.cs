8668316455b3e4d8bdda9f8d49595c1b5c0f19a4
Ramon Smits <ramon.smits@gmail.com>
2015-09-09 15:08:02 +0200

    Added key identifiers to prevent possible data corruption then decryption data and Base64 support for encryption keys.

--- src/NServiceBus.AcceptanceTests/Encryption/When_using_Rijndael_with_config.cs
+++ src/NServiceBus.AcceptanceTests/Encryption/When_using_Rijndael_with_config.cs
@@ -110,6 +110,7 @@
             {
                 return new RijndaelEncryptionServiceConfig
                 {
+                    KeyIdentifier = "1st",
                     Key = "gdDbqRpqdRbTs3mhdZh9qCaDaxJXl+e6"
                 };
             }
