18e3eddf48b996216352897b2c308c6713576b87
Ramon Smits <ramon.smits@gmail.com>
2015-09-09 15:08:02 +0200

    Added key identifiers to prevent possible data corruption when decryption data and Base64 support for encryption keys.

---
+++ src/NServiceBus.Core/Encryption/KeyFormat.cs
@@ -0,0 +1,17 @@
+namespace NServiceBus.Config
+{
+    /// <summary>
+    /// The format in which an encryption value is specified.
+    /// </summary>
+    public enum KeyFormat
+    {
+        /// <summary>
+        /// Key is specified as in ASCII characters.
+        /// </summary>
+        Ascii = 0,
+        /// <summary>
+        /// Key is specified as a Base64 encoded string.
+        /// </summary>
+        Base64 = 1
+    }
+}
