6a390156411b1c6e36807389a35008a39c93d31e
Tim Bussmann <tim.bussmann@particular.net>
2015-09-15 10:57:22 +0200

    added unit tests for DispatchTimeoutBehavior

---
+++ src/NServiceBus.Core.Tests/DelayedDelivery/TimeoutManager/DispatchTimeoutBehaviorTest.cs
@@ -0,0 +1,81 @@
+﻿namespace NServiceBus.Core.Tests.Timeout.TimeoutManager
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+    using NServiceBus.InMemory.TimeoutPersister;
+    using NServiceBus.Pipeline.Contexts;
+    using NServiceBus.Timeout.Core;
+    using NServiceBus.Transports;
+    using NUnit.Framework;
+
+    public class DispatchTimeoutBehaviorTest
+    {
+        [Test]
+        public async Task Terminate_should_remove_timeout_from_timeout_storage()
+        {
+            var messageDispatcher = new FakeMessageDispatcher();
+            var timeoutPersister = new InMemoryTimeoutPersister();
+            var testee = new DispatchTimeoutBehavior(messageDispatcher, timeoutPersister);
+            var timeoutData = CreateTimeout();
+            await timeoutPersister.Add(timeoutData, null);
+
+            testee.Terminate(CreateContext(timeoutData.Id));
+
+            var result = await timeoutPersister.Peek(timeoutData.Id, null);
+            Assert.Null(result);
+        }
+
+        [Test]
+        public async Task Terminate_when_dispatching_message_fails_should_not_remove_timeout()
+        {
+            var messageDispatcher = new FakeMessageDispatcher { DispatchFails = true };
+            var timeoutPersister = new InMemoryTimeoutPersister();
+            var testee = new DispatchTimeoutBehavior(messageDispatcher, timeoutPersister);
+            var timeoutData = CreateTimeout();
+            await timeoutPersister.Add(timeoutData, null);
+
+            Assert.Throws<Exception>(() => testee.Terminate(CreateContext(timeoutData.Id)));
+            
+            var result = await timeoutPersister.Peek(timeoutData.Id, null);
+            Assert.NotNull(result);
+        }
+
+        static TimeoutData CreateTimeout()
+        {
+            return new TimeoutData
+            {
+                Destination = "endpointQueue",
+                Headers = new Dictionary<string, string>()
+            };
+        }
+
+        static PhysicalMessageProcessingStageBehavior.Context CreateContext(string timeoutId)
+        {
+            var messageId = Guid.NewGuid().ToString("D");
+            var headers = new Dictionary<string, string>
+            {
+                {"Timeout.Id", timeoutId}
+            };
+
+            return new PhysicalMessageProcessingStageBehavior.Context(
+                new TransportReceiveContext(
+                    new IncomingMessage(messageId, headers, new MemoryStream()),
+                    null));
+        }
+
+        class FakeMessageDispatcher : IDispatchMessages
+        {
+            public bool DispatchFails { get; set; }
+
+            public void Dispatch(OutgoingMessage message, DispatchOptions dispatchOptions)
+            {
+                if (DispatchFails)
+                {
+                    throw new Exception("simulated exception");
+                }
+            }
+        }
+    }
+}
