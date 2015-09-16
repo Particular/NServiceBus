585b0e14642df58b452fb5406190bf80b778c0a6
Weronika Łabaj <weronika.labaj@particular.net>
2015-09-16 11:17:05 +0200

    Log info if exception thrown.

--- src/NServiceBus.Core/Timeout/Hosting/Windows/TimeoutDispatcherProcessor.cs
+++ src/NServiceBus.Core/Timeout/Hosting/Windows/TimeoutDispatcherProcessor.cs
@@ -1,13 +1,17 @@
 namespace NServiceBus.Timeout.Hosting.Windows
 {
     using System;
+    using System.Text;
     using Core;
+    using NServiceBus.Logging;
     using Satellites;
     using Transports;
     using Unicast.Transport;
 
     class TimeoutDispatcherProcessor : IAdvancedSatellite
     {
+        static ILog logger = LogManager.GetLogger<TimeoutDispatcherProcessor>();
+
         public TimeoutDispatcherProcessor()
         {
             Disabled = true;
@@ -36,15 +40,40 @@ namespace NServiceBus.Timeout.Hosting.Windows
                 {
                     MessageSender.Send(timeoutData.ToTransportMessage(), timeoutData.ToSendOptions(Configure.LocalAddress));
                 }
-                catch
+                catch (Exception ex)
                 {
+                    logger.Warn("Failed to dispatch the timeout message " + TimeoutDataToString(timeoutData), ex);
                     timeoutData.Id = null;
                     timeoutData.Time = DateTime.UtcNow.AddSeconds(5);
-                    TimeoutsPersister.Add(timeoutData);
+                    try
+                    {
+                        TimeoutsPersister.Add(timeoutData);
+                    }
+                    catch (Exception e)
+                    {
+                        logger.Error("Failed to dispatch the timeout message. Failed add the timeout message to storage again. " + TimeoutDataToString(timeoutData) + ".", e);
+                    }
                 }
             }
 
             return true;
+        }
+
+        string TimeoutDataToString(TimeoutData timeout)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(timeout.ToString());
+            sb.AppendLine("Headers: ");
+            foreach (var header in timeout.Headers)
+                sb.AppendLine(header.Key + " : " + header.Value);
+            string contentType;
+            timeout.Headers.TryGetValue(Headers.ContentType, out contentType);
+            if (contentType != null && contentType.Contains("binary") == false)
+            {
+                sb.AppendLine("Body: ");
+                sb.AppendLine(Encoding.UTF8.GetString(timeout.State));
+            }
+            return sb.ToString();
         }
 
         public void Start()
