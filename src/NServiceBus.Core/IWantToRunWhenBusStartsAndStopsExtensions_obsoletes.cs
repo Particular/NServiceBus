d95e2154ff9d540a50db894e107dbdb01b21bb88
danielmarbach <daniel.marbach@particular.net>
2015-09-17 10:09:44 +0200

    Obsoletes for ForwardCurrentMessageTo, HandleCurrentMessageLater and Start/Stop on IWantToRunWhenBusStartsAndStops

---
+++ src/NServiceBus.Core/IWantToRunWhenBusStartsAndStopsExtensions_obsoletes.cs
@@ -0,0 +1,34 @@
+﻿namespace NServiceBus
+{
+    using System;
+
+    /// <summary>
+    /// Syntactic sugar for <see cref="IWantToRunWhenBusStartsAndStops"/>.
+    /// </summary>
+    public static class IWantToRunWhenBusStartsAndStopsExtensions_obsoletes
+    {
+        /// <summary>
+        /// Method called at startup.
+        /// </summary>
+        [ObsoleteEx(
+            TreatAsErrorFromVersion = "6",
+            RemoveInVersion = "7",
+            ReplacementTypeOrMember = "StartAsync()")]
+        public static void Start(this IWantToRunWhenBusStartsAndStops runnable)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Method called on shutdown.
+        /// </summary>
+        [ObsoleteEx(
+            TreatAsErrorFromVersion = "6",
+            RemoveInVersion = "7",
+            ReplacementTypeOrMember = "StopAsync()")]
+        public static void Stop(this IWantToRunWhenBusStartsAndStops runnable)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
