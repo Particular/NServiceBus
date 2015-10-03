6c52a86cc58d70cf5607576a5da07c86e03be313
SzymonPobiega <szymon.pobiega@gmail.com>
2015-10-03 06:31:35 +0200

    Allows disabling main message processing feature.

---
+++ src/NServiceBus.Core/Pipeline/Incoming/MessageProcessingBusConfigurationExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace NServiceBus.Pipeline
+{
+    using NServiceBus.Features;
+
+    /// <summary>
+    /// Allows configuring of the message processing feature.
+    /// </summary>
+    public static class MessageProcessingBusConfigurationExtensions
+    {
+        /// <summary>
+        /// Disables the main processing pipeline (by removing all the standard pipeline behaviors). 
+        /// </summary>
+        public static void DisableMainProcessingPipeline(this BusConfiguration busConfiguration)
+        {
+            busConfiguration.DisableFeature<MessageProcessingFeature>();
+            busConfiguration.DisableFeature<FirstLevelRetries>();
+        }
+    }
+}
