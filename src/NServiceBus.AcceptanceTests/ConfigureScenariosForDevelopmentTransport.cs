709886b3486f68b77e5951b6c46a73e7056fdb73
andreasohlund <andreas.ohlund@particular.net>
2015-10-03 22:00:46 +0200

    Introducing a transport for development
    Using the local file system as a transport
    
    Get basic read and writes going
    
    Got receives going
    
    Serializing headers
    
    Adding a committed dir
    
    First version of directory based tx
    
    Adapt to new routing seam
    
    Flagging the transport as non dtc
    
    Handle isolated dispatch
    
    creating error and audit queues properly
    
    Rollbacks now work
    
    Create folders for satellites properly
    
    Poor mans TTBR
    
    Storing bodies in a sub dir
    
    Handle current message later now works
    
    Using .bodies instead
    
    Updating to new transport config seam
    
    Fix ttbr bug
    
    Fix race condition
    
    Handing headers properly
    
    Use unique file names
    
    Using
    
    Adjuting after rebase
    
    Adding test config
    
    Renames
    
    Handling the instance discriminator properly
    
    Adapted to the latest transport seam
    
    Added unsupported test configs
    
    DTC not supported
    
    Adding support for native pubsub to the dev transport
    
    Handling polymorphic events properly
    
    Handle base class events properly
    
    Updated to latest acpt test seam
    
    Handle non existing event dirs

---
+++ src/NServiceBus.AcceptanceTests/ConfigureScenariosForDevelopmentTransport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.AcceptanceTests.ScenarioDescriptors;
+
+public class ConfigureScenariosForDevelopmentTransport : IConfigureSupportedScenariosForTestExecution
+{
+    public IEnumerable<Type> UnsupportedScenarioDescriptorTypes { get; } = new[]
+    {
+        typeof(AllDtcTransports),
+        typeof(AllTransportsWithMessageDrivenPubSub)
+    };
+}
---
+++ src/NServiceBus.Core/Transports/Development/DevelopmentTransportInfrastructure.cs
@@ -0,0 +1,57 @@
+﻿namespace NServiceBus
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+    using NServiceBus.Performance.TimeToBeReceived;
+    using NServiceBus.Routing;
+    using NServiceBus.Settings;
+    using NServiceBus.Transports;
+
+    class DevelopmentTransportInfrastructure : TransportInfrastructure
+    {
+        public DevelopmentTransportInfrastructure(SettingsHolder settings)
+        {
+            this.settings = settings;
+        }
+
+        public override IEnumerable<Type> DeliveryConstraints { get; } = new[]
+        {
+            typeof(DiscardIfNotReceivedBefore)
+        };
+
+        public override TransportTransactionMode TransactionMode => TransportTransactionMode.SendsAtomicWithReceive;
+
+        public override OutboundRoutingPolicy OutboundRoutingPolicy { get; } = new OutboundRoutingPolicy(OutboundRoutingType.Unicast, OutboundRoutingType.Multicast, OutboundRoutingType.Unicast);
+
+        public override TransportReceiveInfrastructure ConfigureReceiveInfrastructure()
+        {
+            return new TransportReceiveInfrastructure(() => new DevelopmentTransportMessagePump(), () => new DevelopmentTransportQueueCreator(), () => Task.FromResult(StartupCheckResult.Success));
+        }
+
+        public override TransportSendInfrastructure ConfigureSendInfrastructure()
+        {
+            return new TransportSendInfrastructure(() => new DevelopmentTransportDispatcher(), () => Task.FromResult(StartupCheckResult.Success));
+        }
+
+        public override TransportSubscriptionInfrastructure ConfigureSubscriptionInfrastructure()
+        {
+            return new TransportSubscriptionInfrastructure(() => new DevelopmentTransportSubscriptionManager(settings.EndpointName().ToString(), settings.LocalAddress()));
+        }
+
+        public override EndpointInstance BindToLocalEndpoint(EndpointInstance instance)
+        {
+            return instance;
+        }
+
+        public override string ToTransportAddress(LogicalAddress logicalAddress)
+        {
+            return Path.Combine(logicalAddress.EndpointInstance.Endpoint.ToString(),
+                logicalAddress.EndpointInstance.Discriminator ?? "",
+                logicalAddress.Qualifier ?? "");
+        }
+
+        SettingsHolder settings;
+    }
+}
