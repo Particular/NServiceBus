5731b7a4769c6b81e896f0ef329e5232a1265a05
Tim Bussmann <tim.bussmann@particular.net>
2015-11-27 14:34:42 +0100

    replace recursion with while loop

--- src/NServiceBus.Core/Transports/Msmq/MessagePump.cs
+++ src/NServiceBus.Core/Transports/Msmq/MessagePump.cs
@@ -102,23 +102,21 @@ namespace NServiceBus.Transports.Msmq
         [DebuggerNonUserCode]
         async Task ProcessMessages()
         {
-            try
+            while (!cancellationToken.IsCancellationRequested)
             {
-                await InnerProcessMessages().ConfigureAwait(false);
-            }
-            catch (OperationCanceledException)
-            {
-                // For graceful shutdown purposes
-            }
-            catch (Exception ex)
-            {
-                Logger.Error("MSMQ Message pump failed", ex);
-                await peekCircuitBreaker.Failure(ex).ConfigureAwait(false);
-            }
-
-            if (!cancellationToken.IsCancellationRequested)
-            {
-                await ProcessMessages().ConfigureAwait(false);
+                try
+                {
+                    await InnerProcessMessages().ConfigureAwait(false);
+                }
+                catch (OperationCanceledException)
+                {
+                    // For graceful shutdown purposes
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error("MSMQ Message pump failed", ex);
+                    await peekCircuitBreaker.Failure(ex).ConfigureAwait(false);
+                }
             }
         }
 
