964a4ae7d40c0b6b6b81f339174ffaaa41b1eb65
Simon Cropp <simon.cropp@gmail.com>
2015-12-03 18:49:36 +1100

    doco

--- src/NServiceBus.Core/Routing/MessageDrivenSubscriptions/SubscriptionAuthorizer.cs
+++ src/NServiceBus.Core/Routing/MessageDrivenSubscriptions/SubscriptionAuthorizer.cs
@@ -3,5 +3,7 @@ namespace NServiceBus
     /// <summary>
     /// The signature of a subscription authorizer used by <see cref="MessageDrivenSubscriptionsConfigExtensions.SubscriptionAuthorizer"/>.
     /// </summary>
+    /// <param name="context">The current <see cref="PhysicalMessageProcessingContext"/> of the subscription control message.</param>
+    /// <returns><code>true</code> to allow the subscription to be stores; <code>false</code> to decline the subscription.</returns>
     public delegate bool SubscriptionAuthorizer(PhysicalMessageProcessingContext context);
 }
