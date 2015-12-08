e13da9fb5a75c666c7c76066292b117d0959c1ca
Michał Wójcik <wojcik.mike@gmail.com>
2015-12-08 14:10:20 +0100

    Small refactoring of the code, to remove redundant code

--- src/NServiceBus.Core/Pipeline/Outgoing/LogOutgoingMessageBehavior.cs
+++ src/NServiceBus.Core/Pipeline/Outgoing/LogOutgoingMessageBehavior.cs
@@ -14,14 +14,13 @@
         public override async Task Invoke(DispatchContextImpl context, Func<Task> next)
         {
             var outgoingLogicalMessageContext = context.Get<OutgoingLogicalMessageContext>();
-            var outgoingLogicalMessage = context.Get<OutgoingLogicalMessageContext>().Message;
             
-            if (log.IsDebugEnabled && outgoingLogicalMessage != null)
+            if (log.IsDebugEnabled && outgoingLogicalMessageContext != null && outgoingLogicalMessageContext.Message != null)
             {
                 var sb = new StringBuilder();
 
                 sb.AppendFormat("Dispatching message '{0}' with id '{1}'. Routing Details:\n",
-                    outgoingLogicalMessage.MessageType != null ? outgoingLogicalMessage.MessageType.AssemblyQualifiedName : "unknown",
+                    outgoingLogicalMessageContext.Message.MessageType != null ? outgoingLogicalMessageContext.Message.MessageType.AssemblyQualifiedName : "unknown",
                     outgoingLogicalMessageContext.MessageId);
 
                 foreach (var transportOperation in context.Operations)
@@ -30,7 +29,7 @@
                     if (unicastAddressTag != null)
                         sb.AppendFormat("Destination: {0}\n", unicastAddressTag.Destination);
                     sb.AppendFormat("ToString() of the message yields: {0}\n" +
-                                    "Message headers:\n{1}", outgoingLogicalMessage.Instance, string.Join(", ", transportOperation.Message.Headers.Select(h => h.Key + ":" + h.Value).ToArray()));
+                                    "Message headers:\n{1}", outgoingLogicalMessageContext.Message.Instance, string.Join(", ", transportOperation.Message.Headers.Select(h => h.Key + ":" + h.Value).ToArray()));
                 }
 
                 log.Debug(sb.ToString());
