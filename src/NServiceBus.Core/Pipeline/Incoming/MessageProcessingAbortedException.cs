e241112e22b6bd0d219d8cc10c419468f3141559
Tomasz Masternak <tomasz.masternak@particular.net>
2015-12-14 09:42:08 +0100

    making MessageProcessingAbortedExcepton pubic

--- src/NServiceBus.Core/Pipeline/Incoming/MessageProcessingAbortedException.cs
+++ src/NServiceBus.Core/Pipeline/Incoming/MessageProcessingAbortedException.cs
@@ -1,8 +1,30 @@
 namespace NServiceBus
 {
     using System;
+    using System.Runtime.Serialization;
+    using JetBrains.Annotations;
+    using NServiceBus.Features;
+    using NServiceBus.Transports;
 
-    class MessageProcessingAbortedException : Exception
+    /// <summary>
+    /// This exception can be thrown by any of recoverability behaviors ie.e <see cref="FirstLevelRetries"/>, <see cref="SecondLevelRetries"/> or <see cref="StoreFaultsInErrorQueue"/>
+    /// It is meant to indicate to the <see cref="IPushMessages"/> that message should be returned to input queue and will be handeled by recoverability mechanisms on next receive.
+    /// </summary>
+    [Serializable]
+    public class MessageProcessingAbortedException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="MessageProcessingAbortedException"/>.
+        /// </summary>
+        public MessageProcessingAbortedException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="MessageProcessingAbortedException"/>.
+        /// </summary>
+        protected MessageProcessingAbortedException([NotNull] SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
