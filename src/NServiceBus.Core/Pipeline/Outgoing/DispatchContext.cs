0e5ec575f31c9679bdc4a7f2fa82c0f0aac6a517
Tim Bussmann <tim.bussmann@particular.net>
2016-01-08 12:53:12 +0100

    remove unused DispatchContext.Replace operation

--- src/NServiceBus.Core/Pipeline/Outgoing/DispatchContext.cs
+++ src/NServiceBus.Core/Pipeline/Outgoing/DispatchContext.cs
@@ -12,12 +12,6 @@ namespace NServiceBus
             Operations = operations;
         }
 
-        public IEnumerable<TransportOperation> Operations { get; private set; }
-
-        //note: will be removed in a different pull
-        internal void Replace(List<TransportOperation> operations)
-        {
-            Operations = operations;
-        }
+        public IEnumerable<TransportOperation> Operations { get; }
     }
 }
