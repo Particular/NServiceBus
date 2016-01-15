b806ab4b3337288e84419891ed5c57ca57365a5b
John Simons <john.simons@nservicebus.com>
2016-01-15 14:11:22 +1000

    Did I forget this

--- src/NServiceBus.AcceptanceTests/ConfigureMsmqPersistence.cs
+++ src/NServiceBus.AcceptanceTests/ConfigureMsmqPersistence.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.Persistence.Legacy;
 
 
 public class ConfigureMsmqPersistence : IConfigureTestExecution
@@ -9,6 +10,7 @@ public class ConfigureMsmqPersistence : IConfigureTestExecution
     public Task Configure(BusConfiguration configuration, IDictionary<string, string> settings)
     {
         configuration.UsePersistence<InMemoryPersistence>();
+        configuration.UsePersistence<MsmqPersistence>();
         return Task.FromResult(0);
     }
 
