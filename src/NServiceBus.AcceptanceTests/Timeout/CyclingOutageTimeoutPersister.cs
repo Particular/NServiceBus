c04a0ce6b375624cd4091e93fc8d6bd38bb289e4
Colin Higgins <colin.higgins@particular.net>
2016-01-27 12:23:36 -0500

    Port failing timeout circuit breaker tests to v6

---
+++ src/NServiceBus.AcceptanceTests/Timeout/CyclingOutageTimeoutPersister.cs
@@ -0,0 +1,118 @@
+﻿namespace NServiceBus.AcceptanceTests.Timeouts
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using NServiceBus.Extensibility;
+    using Timeout.Core;
+
+    /// <summary>
+    /// This class mocks outages for timeout storage. 
+    /// If SecondsToWait is set to 10, it will throw exceptions for 10 seconds, then be available for 10 seconds, and repeat.
+    /// </summary>
+    class CyclingOutageTimeoutPersister : IPersistTimeouts, IQueryTimeouts
+    {
+        int secondsToWait;
+
+        public int SecondsToWait
+        {
+            get { return secondsToWait; }
+            set
+            {
+                secondsToWait = value;
+                NextChangeTime = DateTime.Now.AddSeconds(SecondsToWait);
+            }
+        }
+
+        static bool isAvailable = false;
+        Task completedTask = Task.FromResult(0);
+        DateTime NextChangeTime;
+        ConcurrentDictionary<string, TimeoutData> storage = new ConcurrentDictionary<string, TimeoutData>(); 
+
+        void ThrowExceptionUntilWaitTimeReached()
+        {
+            if (NextChangeTime <= DateTime.Now)
+            {
+                NextChangeTime = DateTime.Now.AddSeconds(SecondsToWait);
+                isAvailable = !isAvailable;
+            }
+
+            if (!isAvailable)
+            {
+                throw new Exception("Persister is temporarily unavailable");
+            }
+        }
+
+        public IEnumerable<Tuple<string, DateTime>> GetNextChunk(DateTime startSlice, out DateTime nextTimeToRunQuery)
+        {
+            ThrowExceptionUntilWaitTimeReached();
+            nextTimeToRunQuery = DateTime.Now.AddSeconds(2);
+            return Enumerable.Empty<Tuple<string, DateTime>>().ToList();
+        }
+
+        public Task Add(TimeoutData timeout)
+        {
+            ThrowExceptionUntilWaitTimeReached();
+            return completedTask;
+        }
+
+        public Task<bool> TryRemove(string timeoutId, ContextBag context)
+        {
+            ThrowExceptionUntilWaitTimeReached();
+
+            TimeoutData timeoutData = null;
+
+            if (storage.ContainsKey(timeoutId))
+            {
+                storage.TryRemove(timeoutId, out timeoutData);
+            }
+
+            return Task.FromResult(timeoutData != null);
+        }
+
+        public Task RemoveTimeoutBy(Guid sagaId, ContextBag context)
+        {
+            ThrowExceptionUntilWaitTimeReached();
+            return completedTask;
+        }
+
+        public Task Add(TimeoutData timeout, ContextBag context)
+        {
+            ThrowExceptionUntilWaitTimeReached();
+            storage.TryAdd(timeout.Id, timeout);
+            return completedTask;
+        }
+
+        public Task<TimeoutData> Peek(string timeoutId, ContextBag context)
+        {
+            ThrowExceptionUntilWaitTimeReached();
+            if (storage.ContainsKey(timeoutId))
+            {
+                return Task.FromResult(storage[timeoutId]);
+            }
+            return Task.FromResult<TimeoutData>(null);
+        }
+
+        public Task<TimeoutsChunk> GetNextChunk(DateTime startSlice)
+        {
+            ThrowExceptionUntilWaitTimeReached();
+
+            var timeoutsDue = new List<TimeoutsChunk.Timeout>();
+            foreach (var key in storage.Keys)
+            {
+                var value = storage[key];
+                if (value.Time <= startSlice)
+                {
+                    var timeout = new TimeoutsChunk.Timeout(key, value.Time);
+                    timeoutsDue.Add(timeout);
+                }
+            }
+
+            var chunk = new TimeoutsChunk(timeoutsDue, DateTime.Now.AddSeconds(5));
+
+            return Task.FromResult(chunk);
+        }
+    }
+}
---
+++ src/NServiceBus.AcceptanceTests/Timeout/When_timeout_storage_is_unavailable_temporarily.cs
@@ -0,0 +1,90 @@
+﻿namespace NServiceBus.AcceptanceTests.Timeouts
+{
+    using System;
+    using System.Threading.Tasks;
+    using Features;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+    using NServiceBus.AcceptanceTests.ScenarioDescriptors;
+
+    class When_timeout_storage_is_unavailable_temporarily : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Endpoint_should_start()
+        {
+            await Scenario.Define<TimeoutTestContext>()
+                .WithEndpoint<EndpointWithFlakyTimeoutPersister>()
+                .Done(c => c.EndpointsStarted)
+                .Repeat(r => r.For(Transports.Msmq))
+                .Should(c => Assert.IsTrue(c.EndpointsStarted))
+                .Run();
+        }
+
+        [Test]
+        public async Task Endpoint_should_not_shutdown()
+        {
+            var stopTime = DateTime.Now.AddSeconds(45);
+
+            await Scenario.Define<TimeoutTestContext>(c =>
+            {
+                c.SecondsToWait = 10;
+            })
+                //.AllowExceptions(ex => ex.Message.Contains("Persister is temporarily unavailable"))
+                .WithEndpoint<EndpointWithFlakyTimeoutPersister>(b =>
+                {
+                    b.CustomConfig((busConfig, context) =>
+                    {
+                        busConfig.DefineCriticalErrorAction(criticalErrorContext =>
+                        {
+                            if (criticalErrorContext.Error.Contains("Persister is temporarily unavailable"))
+                                context.FatalErrorOccurred = true;
+                            return Task.FromResult(true);
+                        });
+                    });
+                })
+                .Done(c => c.FatalErrorOccurred || stopTime <= DateTime.Now)
+                .Repeat(r => r.For(Transports.Msmq))
+                .Should(c => Assert.IsFalse(c.FatalErrorOccurred, "Circuit breaker was trigged too soon."))
+                .Run();
+        }
+
+        public class TimeoutTestContext : ScenarioContext
+        {
+            public int SecondsToWait { get; set; }
+            public bool FatalErrorOccurred { get; set; }
+        }
+
+        [Serializable]
+        public class MyMessage : IMessage { }
+
+        public class EndpointWithFlakyTimeoutPersister : EndpointConfigurationBuilder
+        {
+            public TestContext TestContext { get; set; }
+            public EndpointWithFlakyTimeoutPersister()
+            {
+                EndpointSetup<DefaultServer>(config =>
+                {
+                    config.EnableFeature<TimeoutManager>();
+                    //config.SuppressOutdatedTimeoutPersistenceWarning();
+                });
+            }
+
+            class Initalizer : Feature
+            {
+                public Initalizer()
+                {
+                    EnableByDefault();
+                }
+
+                protected override void Setup(FeatureConfigurationContext context)
+                {
+                    var testContext = context.Settings.Get<TimeoutTestContext>();
+                    context.Container
+                        .ConfigureComponent<CyclingOutageTimeoutPersister>(DependencyLifecycle.SingleInstance)
+                        .ConfigureProperty(tp => tp.SecondsToWait, testContext.SecondsToWait);
+                }
+            }
+        }
+    }
+}
