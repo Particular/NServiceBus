252f0a19b27cbb6c177891cb4f44e4f5183d79b3
danielmarbach <daniel.marbach@particular.net>
2016-02-01 09:57:46 +0100

    MessageIntent.Publish for UnicastPublishRouterConnector, fixes #3344

---
+++ src/NServiceBus.Core.Tests/Routing/Routers/UnicastSendRouterConnectorTests.cs
@@ -0,0 +1,86 @@
+﻿namespace NServiceBus.Core.Tests.Routing.Routers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using NServiceBus.Extensibility;
+    using NServiceBus.ObjectBuilder;
+    using NServiceBus.Pipeline;
+    using NServiceBus.Routing;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UnicastSendRouterConnectorTests
+    {
+        [Test]
+        public async Task Should_set_messageintent_to_send()
+        {
+            var router = new UnicastSendRouterConnector("fake", new Router(), new DistributionPolicy());
+            var context = new FakeContext();
+
+            await router.Invoke(context, ctx => TaskEx.CompletedTask);
+
+            Assert.AreEqual(1, context.Headers.Count);
+            Assert.AreEqual(MessageIntentEnum.Send.ToString(), context.Headers[Headers.MessageIntent]);
+        }
+
+        class FakeContext : IOutgoingSendContext
+        {
+            public FakeContext()
+            {
+                Extensions = new ContextBag();
+                Headers = new Dictionary<string, string>();
+                Message = new OutgoingLogicalMessage(typeof(object), new object());
+            }
+
+            public ContextBag Extensions { get; }
+            public IBuilder Builder { get; }
+
+            public Task Send(object message, SendOptions options)
+            {
+                return null;
+            }
+
+            public Task Send<T>(Action<T> messageConstructor, SendOptions options)
+            {
+                return null;
+            }
+
+            public Task Publish(object message, PublishOptions options)
+            {
+                return null;
+            }
+
+            public Task Publish<T>(Action<T> messageConstructor, PublishOptions publishOptions)
+            {
+                return null;
+            }
+
+            public Task Subscribe(Type eventType, SubscribeOptions options)
+            {
+                return null;
+            }
+
+            public Task Unsubscribe(Type eventType, UnsubscribeOptions options)
+            {
+                return null;
+            }
+
+            public string MessageId { get; }
+            public Dictionary<string, string> Headers { get; }
+            public OutgoingLogicalMessage Message { get; }
+        }
+
+        class Router : IUnicastRouter
+        {
+            public Task<IEnumerable<UnicastRoutingStrategy>> Route(Type messageType, DistributionStrategy distributionStrategy, ContextBag contextBag)
+            {
+                IEnumerable<UnicastRoutingStrategy> unicastRoutingStrategies = new List<UnicastRoutingStrategy>
+                {
+                    new UnicastRoutingStrategy("Fake")
+                };
+                return Task.FromResult(unicastRoutingStrategies);
+            }
+        }
+    }
+}
