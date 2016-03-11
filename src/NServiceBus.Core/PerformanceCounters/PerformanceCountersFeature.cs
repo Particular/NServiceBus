3b1d770359d433a61b6d7f0d5072485f6661e917
andreasohlund <andreas.ohlund@particular.net>
2016-03-11 15:51:38 +0100

    Switching to a single feature for the perf counters

---
+++ src/NServiceBus.Core/PerformanceCounters/PerformanceCountersFeature.cs
@@ -0,0 +1,194 @@
+﻿namespace NServiceBus.Features
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+
+    class PerformanceCountersFeature : Feature
+    {
+        public PerformanceCountersFeature()
+        {
+            Prerequisite(c => !c.Settings.GetOrDefault<bool>("Endpoint.SendOnly"), "No performance counters applicable for send only endpoints");
+        }
+
+        protected internal override void Setup(FeatureConfigurationContext context)
+        {
+            var localAddress = context.Settings.LocalAddress();
+
+            var messagesPulledFromQueueCounter = InstantiatePerformanceCounter("# of msgs pulled from the input queue /sec", localAddress);
+            var successRateCounter = InstantiatePerformanceCounter("# of msgs successfully processed / sec", localAddress);
+            var failureRateCounter = InstantiatePerformanceCounter("# of msgs failures / sec", localAddress);
+
+            var criticalTimeCounter = InstantiatePerformanceCounter("Critical Time", context.Settings.EndpointName().ToString());
+            var criticalTimeCalculator = new CriticalTimeCalculator(criticalTimeCounter);
+
+            var disposables = new List<IDisposable>
+            {
+                messagesPulledFromQueueCounter,
+                successRateCounter,
+                failureRateCounter,
+                criticalTimeCalculator
+            };
+
+            TimeSpan endpointSla;
+            EstimatedTimeToSLABreachCalculator timeToSLABreachCalculator = null;
+
+            if (context.Settings.TryGet(EndpointSLAKey, out endpointSla))
+            {
+                var slaBreachCounter = InstantiatePerformanceCounter("SLA violation countdown", context.Settings.EndpointName().ToString());
+                timeToSLABreachCalculator = new EstimatedTimeToSLABreachCalculator(endpointSla, slaBreachCounter);
+                disposables.Add(timeToSLABreachCalculator);
+            }
+
+            context.RegisterStartupTask(new DisposeCountersOnShutdown(disposables));
+            context.Container.ConfigureComponent<PerformanceMonitorUsersInstaller.Installer>(DependencyLifecycle.SingleInstance);
+
+            context.Settings.Get<NotificationSubscriptions>()
+                .Subscribe<ReceivePipelineCompleted>(e =>
+                {
+                    var timeSent = DateTime.UtcNow;
+                    var processingStarted = DateTime.UtcNow;
+                    var processingCompleted = DateTime.UtcNow;
+
+                    successRateCounter.Increment();
+
+                    timeToSLABreachCalculator?.Update(timeSent, processingStarted, processingCompleted);
+                    criticalTimeCalculator.Update(timeSent, processingStarted, processingCompleted);
+
+                    return TaskEx.CompletedTask;
+                });
+        }
+
+        static PerformanceCounter InstantiatePerformanceCounter(string counterName, string instanceName)
+        {
+            if (instanceName.Length > 128)
+            {
+                throw new Exception($"The endpoint name ('{instanceName}') is too long (longer then {(int)sbyte.MaxValue}) to register as a performance counter instance name. Reduce the endpoint name.");
+            }
+
+            var message = $"NServiceBus performance counter for '{counterName}' is not set up correctly. To rectify this problem download the latest powershell commandlets from http://www.particular.net downloads page.";
+
+            try
+            {
+                return new PerformanceCounter("NServiceBus", counterName, instanceName, false);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(message, ex);
+            }
+        }
+
+        public const string EndpointSLAKey = "EndpointSLA";
+
+        class DisposeCountersOnShutdown : FeatureStartupTask
+        {
+            public DisposeCountersOnShutdown(List<IDisposable> counters)
+            {
+                this.counters = counters;
+            }
+
+            protected override Task OnStart(IMessageSession session)
+            {
+                return TaskEx.CompletedTask;
+            }
+
+            protected override Task OnStop(IMessageSession session)
+            {
+                counters.ForEach(c => c.Dispose());
+                return TaskEx.CompletedTask;
+            }
+
+            List<IDisposable> counters;
+        }
+    }
+}
+
+namespace NServiceBus.Features
+{
+    /// <summary>
+    /// Used to configure CriticalTimeMonitoring.
+    /// </summary>
+    public class CriticalTimeMonitoring
+    {
+    }
+}
+
+namespace NServiceBus.Features
+{
+    /// <summary>
+    /// Used to configure SLAMonitoring.
+    /// </summary>
+    public class SLAMonitoring
+    {
+    }
+}
+namespace NServiceBus
+{
+    using Features;
+
+    /// <summary>
+    /// </summary>
+    public static class PerformanceCountersConfig
+    {
+        /// <summary>
+        /// Enables the NServiceBus specific performance counters.
+        /// </summary>
+        /// <param name="config">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
+        public static void EnablePerformanceCounters(this EndpointConfiguration config)
+        {
+            config.EnableFeature<PerformanceCountersFeature>();
+        }
+    }
+}
+namespace NServiceBus
+{
+    /// <summary>
+    /// </summary>
+    public static class CriticalTimeMonitoringConfig
+    {
+        /// <summary>
+        /// Enables the NServiceBus specific performance counters.
+        /// </summary>
+        /// <param name="config">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
+        public static void EnableCriticalTimePerformanceCounter(this EndpointConfiguration config)
+        {
+        }
+    }
+}
+
+namespace NServiceBus
+{
+    using System;
+    using Features;
+
+    /// <summary>
+    /// Provide configuration options for monitoring related settings.
+    /// </summary>
+    public static class SLAMonitoringConfig
+    {
+        /// <summary>
+        /// Enables the time to breach endpoint SLA performance counter.
+        /// </summary>
+        /// <param name="config">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
+        /// <param name="sla">The critical time SLA for this endpoint. Must be greater than <see cref="TimeSpan.Zero" />.</param>
+        public static void EnableSLAPerformanceCounter(this EndpointConfiguration config, TimeSpan sla)
+        {
+            Guard.AgainstNull(nameof(config), config);
+            Guard.AgainstNegativeAndZero(nameof(sla), sla);
+
+            config.Settings.Set(PerformanceCountersFeature.EndpointSLAKey, sla);
+
+            config.EnablePerformanceCounters();
+        }
+
+        /// <summary>
+        /// Enables the NServiceBus specific performance counters with a specific EndpointSLA.
+        /// </summary>
+        /// <param name="config">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
+        public static void EnableSLAPerformanceCounter(this EndpointConfiguration config)
+        {
+            //huh??
+        }
+    }
+}
--- src/NServiceBus.Core/PerformanceCounters/PerformanceMonitorUsersInstaller.cs
+++ src/NServiceBus.Core/PerformanceCounters/PerformanceMonitorUsersInstaller.cs
@@ -7,100 +7,123 @@ namespace NServiceBus
     using System.Threading.Tasks;
     using Installation;
     using Logging;
+    using ObjectBuilder;
 
     // Add the identity to the 'Performance Monitor Users' local group 
     class PerformanceMonitorUsersInstaller : INeedToInstallSomething
     {
-        static PerformanceMonitorUsersInstaller()
+        public PerformanceMonitorUsersInstaller(IConfigureComponents configureComponents, IBuilder builder)
         {
-            builtinPerformanceMonitoringUsersName = new SecurityIdentifier(WellKnownSidType.BuiltinPerformanceMonitoringUsersSid, null).Translate(typeof(NTAccount)).ToString();
-            var parts = builtinPerformanceMonitoringUsersName.Split('\\');
-
-            if (parts.Length == 2)
-            {
-                builtinPerformanceMonitoringUsersName = parts[1];
-            }
+            this.configureComponents = configureComponents;
+            this.builder = builder;
         }
 
         public Task Install(string identity)
         {
-            //did not use DirectoryEntry to avoid a ref to the DirectoryServices.dll
-            try
+            if(!configureComponents.HasComponent<Installer>())
             {
-                if (!ElevateChecker.IsCurrentUserElevated())
-                {
-                    logger.InfoFormat(@"Did not attempt to add user '{0}' to group '{1}' since process is not running with elevate privileges. Processing will continue. To manually perform this action run the following command from an admin console:
-net localgroup ""{1}"" ""{0}"" /add", identity, builtinPerformanceMonitoringUsersName);
-                    return TaskEx.CompletedTask;
-                }
-                StartProcess(identity);
-            }
-            catch (Exception win32Exception)
-            {
-                var message = string.Format(
-                    @"Failed adding user '{0}' to group '{1}' due to an Exception. 
-To help diagnose the problem try running the following command from an admin console:
-net localgroup ""{1}"" ""{0}"" /add", identity, builtinPerformanceMonitoringUsersName);
-                logger.Warn(message, win32Exception);
+                return TaskEx.CompletedTask;
             }
 
-            return TaskEx.CompletedTask;
+            return builder.Build<Installer>().Install(identity);
         }
 
+        IBuilder builder;
+        IConfigureComponents configureComponents;
 
-        void StartProcess(string identity)
+        public class Installer
         {
-            //net localgroup "Performance Monitor Users" "{user account}" /add
-            var startInfo = new ProcessStartInfo
+            public Installer()
             {
-                CreateNoWindow = true,
-                UseShellExecute = false,
-                RedirectStandardError = true,
-                Arguments = string.Format("localgroup \"{1}\" \"{0}\" /add", identity, builtinPerformanceMonitoringUsersName),
-                FileName = "net",
-                WorkingDirectory = Path.GetTempPath()
-            };
-            using (var process = Process.Start(startInfo))
-            {
-                process.WaitForExit(5000);
+                builtinPerformanceMonitoringUsersName = new SecurityIdentifier(WellKnownSidType.BuiltinPerformanceMonitoringUsersSid, null).Translate(typeof(NTAccount)).ToString();
+                var parts = builtinPerformanceMonitoringUsersName.Split('\\');
 
-                if (process.ExitCode == 0)
+                if (parts.Length == 2)
                 {
-                    logger.Info($"Added user '{identity}' to group '{builtinPerformanceMonitoringUsersName}'.");
-                    return;
+                    builtinPerformanceMonitoringUsersName = parts[1];
                 }
-                var error = process.StandardError.ReadToEnd();
-                if (IsAlreadyAMemberError(error))
+            }
+
+            public Task Install(string identity)
+            {
+                //did not use DirectoryEntry to avoid a ref to the DirectoryServices.dll
+                try
                 {
-                    logger.Info($"Skipped adding user '{identity}' to group '{builtinPerformanceMonitoringUsersName}' because the user is already in group.");
-                    return;
+                    if (!ElevateChecker.IsCurrentUserElevated())
+                    {
+                        logger.InfoFormat(@"Did not attempt to add user '{0}' to group '{1}' since process is not running with elevate privileges. Processing will continue. To manually perform this action run the following command from an admin console:
+net localgroup ""{1}"" ""{0}"" /add", identity, builtinPerformanceMonitoringUsersName);
+                        return TaskEx.CompletedTask;
+                    }
+                    StartProcess(identity);
                 }
-                if (IsGroupDoesNotExistError(error))
+                catch (Exception win32Exception)
                 {
-                    logger.Info($"Skipped adding user '{identity}' to group '{builtinPerformanceMonitoringUsersName}' because the group does not exist.");
-                    return;
+                    var message = string.Format(
+                        @"Failed adding user '{0}' to group '{1}' due to an Exception. 
+To help diagnose the problem try running the following command from an admin console:
+net localgroup ""{1}"" ""{0}"" /add", identity, builtinPerformanceMonitoringUsersName);
+                    logger.Warn(message, win32Exception);
                 }
-                var message = string.Format(
-                    @"Failed to add user '{0}' to group '{2}'. 
+
+                return TaskEx.CompletedTask;
+            }
+
+
+            void StartProcess(string identity)
+            {
+                //net localgroup "Performance Monitor Users" "{user account}" /add
+                var startInfo = new ProcessStartInfo
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    Arguments = string.Format("localgroup \"{1}\" \"{0}\" /add", identity, builtinPerformanceMonitoringUsersName),
+                    FileName = "net",
+                    WorkingDirectory = Path.GetTempPath()
+                };
+                using (var process = Process.Start(startInfo))
+                {
+                    process.WaitForExit(5000);
+
+                    if (process.ExitCode == 0)
+                    {
+                        logger.Info($"Added user '{identity}' to group '{builtinPerformanceMonitoringUsersName}'.");
+                        return;
+                    }
+                    var error = process.StandardError.ReadToEnd();
+                    if (IsAlreadyAMemberError(error))
+                    {
+                        logger.Info($"Skipped adding user '{identity}' to group '{builtinPerformanceMonitoringUsersName}' because the user is already in group.");
+                        return;
+                    }
+                    if (IsGroupDoesNotExistError(error))
+                    {
+                        logger.Info($"Skipped adding user '{identity}' to group '{builtinPerformanceMonitoringUsersName}' because the group does not exist.");
+                        return;
+                    }
+                    var message = string.Format(
+                        @"Failed to add user '{0}' to group '{2}'. 
 Error: {1}
 To help diagnose the problem try running the following command from an admin console:
 net localgroup ""{2}"" ""{0}"" /add", identity, error, builtinPerformanceMonitoringUsersName);
-                logger.Info(message);
+                    logger.Info(message);
+                }
             }
-        }
 
-        static bool IsAlreadyAMemberError(string error)
-        {
-            return error.Contains("1378");
-        }
+            static bool IsAlreadyAMemberError(string error)
+            {
+                return error.Contains("1378");
+            }
 
-        static bool IsGroupDoesNotExistError(string error)
-        {
-            //required since 'Performance Monitor Users' does not exist on all windows OS.
-            return error.Contains("1376");
-        }
+            static bool IsGroupDoesNotExistError(string error)
+            {
+                //required since 'Performance Monitor Users' does not exist on all windows OS.
+                return error.Contains("1376");
+            }
 
-        static ILog logger = LogManager.GetLogger<PerformanceMonitorUsersInstaller>();
-        static string builtinPerformanceMonitoringUsersName;
+            static ILog logger = LogManager.GetLogger<PerformanceMonitorUsersInstaller>();
+            static string builtinPerformanceMonitoringUsersName;
+        }
     }
 }
