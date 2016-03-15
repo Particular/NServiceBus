941aaf9e516c45f9da9aa70f227ce6d0a5d593ec
WilliamBZA <william.brander@gmail.com>
2016-03-15 16:08:10 +0200

    Added test to reproduce ISynchronizationContext bug

---
+++ src/NServiceBus.AcceptanceTests/EndpointTemplates/PersistencelessServer.cs
@@ -0,0 +1,145 @@
+﻿namespace NServiceBus.AcceptanceTests.EndpointTemplates
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using NServiceBus.AcceptanceTesting.Customization;
+    using NServiceBus.AcceptanceTesting.Support;
+    using NServiceBus.Config.ConfigurationSource;
+    using NServiceBus.Configuration.AdvanceExtensibility;
+    using Extensibility;
+    using NServiceBus.Features;
+    using NServiceBus.Hosting.Helpers;
+    using NServiceBus.ObjectBuilder;
+    using NServiceBus.Serialization;
+    using Unicast.Subscriptions;
+    using Unicast.Subscriptions.MessageDrivenSubscriptions;
+
+    public class PersistencelessServer : IEndpointSetupTemplate
+    {
+        public PersistencelessServer()
+        {
+            typesToInclude = new List<Type>();
+        }
+
+        public PersistencelessServer(List<Type> typesToInclude)
+        {
+            this.typesToInclude = typesToInclude;
+        }
+
+        public async Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointConfiguration, IConfigurationSource configSource, Action<EndpointConfiguration> configurationBuilderCustomization)
+        {
+            var settings = runDescriptor.Settings;
+
+            var types = GetTypesScopedByTestClass(endpointConfiguration);
+
+            typesToInclude.AddRange(types);
+
+            var builder = new EndpointConfiguration();
+
+            builder.EndpointName(endpointConfiguration.EndpointName);
+            builder.TypesToIncludeInScan(typesToInclude);
+            builder.CustomConfigurationSource(configSource);
+            builder.EnableInstallers();
+
+            builder.DisableFeature<TimeoutManager>();
+            builder.DisableFeature<SecondLevelRetries>();
+            builder.DisableFeature<FirstLevelRetries>();
+
+            await builder.DefineTransport(settings, endpointConfiguration.EndpointName).ConfigureAwait(false);
+
+            builder.DefineBuilder(settings);
+            builder.RegisterComponents(r =>
+            {
+                RegisterInheritanceHierarchyOfContextOnContainer(runDescriptor, r);
+                r.ConfigureComponent<ISubscriptionStorage>(() => new NoOpISubscriptionStorage(), DependencyLifecycle.SingleInstance);
+            });
+
+            Type serializerType;
+            if (settings.TryGet("Serializer", out serializerType))
+            {
+                builder.UseSerialization((SerializationDefinition) Activator.CreateInstance(serializerType));
+            }
+            //await builder.DefinePersistence(settings, endpointConfiguration.EndpointName).ConfigureAwait(false);
+
+            builder.GetSettings().SetDefault("ScaleOut.UseSingleBrokerQueue", true);
+            configurationBuilderCustomization(builder);
+
+            return builder;
+        }
+
+        static void RegisterInheritanceHierarchyOfContextOnContainer(RunDescriptor runDescriptor, IConfigureComponents r)
+        {
+            var type = runDescriptor.ScenarioContext.GetType();
+            while (type != typeof(object))
+            {
+                r.RegisterSingleton(type, runDescriptor.ScenarioContext);
+                type = type.BaseType;
+            }
+        }
+
+        static IEnumerable<Type> GetTypesScopedByTestClass(EndpointCustomizationConfiguration endpointConfiguration)
+        {
+            var assemblies = new AssemblyScanner().GetScannableAssemblies();
+
+            var types = assemblies.Assemblies
+                //exclude all test types by default
+                .Where(a =>
+                {
+                    var references = a.GetReferencedAssemblies();
+
+                    return references.All(an => an.Name != "nunit.framework");
+                })
+                .SelectMany(a => a.GetTypes());
+
+
+            types = types.Union(GetNestedTypeRecursive(endpointConfiguration.BuilderType.DeclaringType, endpointConfiguration.BuilderType));
+
+            types = types.Union(endpointConfiguration.TypesToInclude);
+
+            return types.Where(t => !endpointConfiguration.TypesToExclude.Contains(t)).ToList();
+        }
+
+        static IEnumerable<Type> GetNestedTypeRecursive(Type rootType, Type builderType)
+        {
+            if (rootType == null)
+            {
+                throw new InvalidOperationException("Make sure you nest the endpoint infrastructure inside the TestFixture as nested classes");
+            }
+
+            yield return rootType;
+
+            if (typeof(IEndpointConfigurationFactory).IsAssignableFrom(rootType) && rootType != builderType)
+            {
+                yield break;
+            }
+
+            foreach (var nestedType in rootType.GetNestedTypes(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).SelectMany(t => GetNestedTypeRecursive(t, builderType)))
+            {
+                yield return nestedType;
+            }
+        }
+
+        List<Type> typesToInclude;
+    }
+
+    class NoOpISubscriptionStorage : ISubscriptionStorage
+    {
+        public Task<IEnumerable<Subscriber>> GetSubscriberAddressesForMessage(IEnumerable<MessageType> messageTypes, ContextBag context)
+        {
+            return Task.FromResult<IEnumerable<Subscriber>>(null);
+        }
+
+        public Task Subscribe(Subscriber subscriber, MessageType messageType, ContextBag context)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task Unsubscribe(Subscriber subscriber, MessageType messageType, ContextBag context)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ src/NServiceBus.AcceptanceTests/Persistence/When_a_persistence_does_not_provide_ISynchronizationContext.cs
@@ -0,0 +1,68 @@
+﻿using NServiceBus.AcceptanceTesting;
+using NServiceBus.AcceptanceTests.EndpointTemplates;
+using NServiceBus.Persistence;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NServiceBus.AcceptanceTests.Persistence
+{
+    public class When_a_persistence_does_not_provide_ISynchronizationContext
+    {
+        public class InMemoryNoSyncContextPersistence : PersistenceDefinition
+        {
+            internal InMemoryNoSyncContextPersistence()
+            {
+                Supports<StorageType.Timeouts>(s => { });
+                Supports<StorageType.Sagas>(s => { });
+                Supports<StorageType.Subscriptions>(s => { });
+            }
+        }
+
+        [Test]
+        public async Task ReceiveFeature_should_work_without_ISynchronizedStorage()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<NoSyncEndpoint>(e => e.When(b => b.SendLocal(new MyMessage())))
+                .Done(c => c.Done)
+                .Run();
+        }
+
+        public class NoSyncEndpoint : EndpointConfigurationBuilder
+        {
+            public NoSyncEndpoint()
+            {
+                EndpointSetup<PersistencelessServer>(c =>
+                {
+                    c.UsePersistence<InMemoryNoSyncContextPersistence>();
+                });
+            }
+        }
+
+        public class MyMessageHandler : IHandleMessages<MyMessage>
+        {
+            public Context Context { get; set; }
+
+            public Task Handle(MyMessage message, IMessageHandlerContext context)
+            {
+                Context.Done = true;
+
+                return Task.FromResult(0);
+            }
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool NotSet { get; set; }
+            public bool Done { get; set; }
+        }
+
+        [Serializable]
+        public class MyMessage : ICommand
+        {
+        }
+    }
+}
