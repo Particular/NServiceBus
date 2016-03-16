5377a8a7e1519621b4a52085e2427503f6eefa93
Michał Wójcik <wojcik.mike@gmail.com>
2016-03-16 16:59:31 +0100

    Making MasterNodeConfig obsolete

--- src/NServiceBus.Core/DetectObsoleteConfigurationSettings.cs
+++ src/NServiceBus.Core/DetectObsoleteConfigurationSettings.cs
@@ -29,6 +29,13 @@
             {
                 throw new NotSupportedException($"The {nameof(UnicastBusConfig.DistributorDataAddress)} attribute in the {nameof(UnicastBusConfig)} configuration section is no longer supported. Remove this from the configuration section. Switch to the code API by using `{nameof(EndpointConfiguration)}.EnlistWithLegacyMSMQDistributor` instead.");
             }
+
+            var masterNodeConfig = context.Settings.GetConfigSection<MasterNodeConfig>();
+
+            if (masterNodeConfig != null)
+            {
+                throw new NotSupportedException($"The {nameof(MasterNodeConfig)} configuration section is no longer supported. Remove this from this configuration section. Switch to the code API by using `{nameof(EndpointConfiguration)}.EnlistWithLegacyMSMQDistributor` instead.");
+            }
         }
     }
 }
