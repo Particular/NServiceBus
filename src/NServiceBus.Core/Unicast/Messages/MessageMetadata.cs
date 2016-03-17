55af37910a04ea8ec6153a78bef8f418e2030d34
Tim Bussmann <tim.bussmann@particular.net>
2016-03-17 17:47:08 +0100

    Make MessageMetadata constructor public

--- src/NServiceBus.Core/Unicast/Messages/MessageMetadata.cs
+++ src/NServiceBus.Core/Unicast/Messages/MessageMetadata.cs
@@ -8,7 +8,12 @@
     /// </summary>
     public partial class MessageMetadata
     {
-        internal MessageMetadata(Type messageType, IEnumerable<Type> messageHierarchy = null)
+        /// <summary>
+        /// Create a new instance of <see cref="MessageMetadata"/>.
+        /// </summary>
+        /// <param name="messageType">The type of the message this metadata belongs to.</param>
+        /// <param name="messageHierarchy">the hierarchy of all message types implemented by the message this metadata belongs to.</param>
+        public MessageMetadata(Type messageType, IEnumerable<Type> messageHierarchy = null)
         {
             MessageType = messageType;
             MessageHierarchy = (messageHierarchy == null ? new List<Type>() : new List<Type>(messageHierarchy)).AsReadOnly();
