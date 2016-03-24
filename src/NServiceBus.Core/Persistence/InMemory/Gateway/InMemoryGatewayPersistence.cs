4d3c033a232e563c1876827db3bfd0d53b3ba6f2
Brandon Ording <bording@gmail.com>
2016-03-24 15:48:55 -0400

    Remove incorrect gateway storage check and test

--- src/NServiceBus.Core/Persistence/InMemory/Gateway/InMemoryGatewayPersistence.cs
+++ src/NServiceBus.Core/Persistence/InMemory/Gateway/InMemoryGatewayPersistence.cs
@@ -1,8 +1,5 @@
 ﻿namespace NServiceBus.Features
 {
-    using System;
-    using Persistence;
-
     /// <summary>
     /// In-memory Gateway.
     /// </summary>
@@ -18,11 +15,6 @@
         /// </summary>
         protected internal override void Setup(FeatureConfigurationContext context)
         {
-            if (!PersistenceStartup.HasSupportFor<StorageType.GatewayDeduplication>(context.Settings))
-            {
-                throw new Exception("The selected persistence doesn't have support for gateway deduplication storage. Select another persistence or disable the gateway feature using endpointConfiguration.DisableFeature<Gateway>()");
-            }
-
             context.Container.ConfigureComponent<InMemoryGatewayDeduplication>(DependencyLifecycle.SingleInstance);
         }
     }
