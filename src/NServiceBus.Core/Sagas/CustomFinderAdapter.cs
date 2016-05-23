ea12ab5b09d7b9ab96ee2b9863dca3b94d043575
David Boike <david.boike@gmail.com>
2016-05-23 13:28:44 -0500

    A couple async spots that don't need a state machine

--- src/NServiceBus.Core/Sagas/CustomFinderAdapter.cs
+++ src/NServiceBus.Core/Sagas/CustomFinderAdapter.cs
@@ -9,16 +9,15 @@ namespace NServiceBus
 
     class CustomFinderAdapter<TSagaData, TMessage> : SagaFinder where TSagaData : IContainSagaData
     {
-        public override async Task<IContainSagaData> Find(IBuilder builder, SagaFinderDefinition finderDefinition, SynchronizedStorageSession storageSession, ContextBag context, object message)
+        public override Task<IContainSagaData> Find(IBuilder builder, SagaFinderDefinition finderDefinition, SynchronizedStorageSession storageSession, ContextBag context, object message)
         {
             var customFinderType = (Type) finderDefinition.Properties["custom-finder-clr-type"];
 
             var finder = (IFindSagas<TSagaData>.Using<TMessage>) builder.Build(customFinderType);
 
-            return await finder
+            return finder
                 .FindBy((TMessage) message, storageSession, context)
-                .ThrowIfNull()
-                .ConfigureAwait(false);
+                .ThrowIfNull() as Task<IContainSagaData>;
         }
     }
 }
