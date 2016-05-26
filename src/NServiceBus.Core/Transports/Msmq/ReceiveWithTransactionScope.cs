ea9da14ea7128f72a0fd70e098cc02075ff3bebb
andreasohlund <andreas.ohlund@particular.net>
2016-05-26 15:01:08 +0200

    Show how the msmq scope receive behavior would change

--- src/NServiceBus.Core/Transports/Msmq/ReceiveWithTransactionScope.cs
+++ src/NServiceBus.Core/Transports/Msmq/ReceiveWithTransactionScope.cs
@@ -19,45 +19,118 @@ namespace NServiceBus
 
         public override async Task ReceiveMessage(MessageQueue inputQueue, MessageQueue errorQueue, CancellationTokenSource cancellationTokenSource, Func<PushContext, Task> onMessage)
         {
-            using (var scope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
+            Func<ErrorContext, Task<RecoveryAction>> onError = context => Task.FromResult(new RecoveryAction());
+
+            Dictionary<string, string> headers = null;
+            Message message = null;
+
+            try
             {
-                var message = inputQueue.Receive(TimeSpan.FromMilliseconds(10), MessageQueueTransactionType.Automatic);
-
-                Dictionary<string, string> headers;
-
-                try
+                using (var scope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
                 {
-                    headers = MsmqUtilities.ExtractHeaders(message);
-                }
-                catch (Exception ex)
-                {
-                    var error = $"Message '{message.Id}' is corrupt and will be moved to '{errorQueue.QueueName}'";
-                    Logger.Error(error, ex);
+                    message = inputQueue.Receive(TimeSpan.FromMilliseconds(10), MessageQueueTransactionType.Automatic);
 
-                    errorQueue.Send(message, MessageQueueTransactionType.Automatic);
+                    if (PerformRecoverabilityAction(message))
+                    {
+                        scope.Complete();
+                        return;
+                    }
 
-                    scope.Complete();
-                    return;
-                }
+                    try
+                    {
+                        headers = MsmqUtilities.ExtractHeaders(message);
+                    }
+                    catch (Exception ex)
+                    {
+                        var error = $"Message '{message.Id}' is corrupt and will be moved to '{errorQueue.QueueName}'";
+                        Logger.Error(error, ex);
 
-                using (var bodyStream = message.BodyStream)
-                {
-                    var ambientTransaction = new TransportTransaction();
-                    ambientTransaction.Set(Transaction.Current);
-                    var pushContext = new PushContext(message.Id, headers, bodyStream, ambientTransaction, cancellationTokenSource, new ContextBag());
+                        errorQueue.Send(message, MessageQueueTransactionType.Automatic);
 
-                    await onMessage(pushContext).ConfigureAwait(false);
-                }
+                        scope.Complete();
+                        return;
+                    }
 
-                if (!cancellationTokenSource.Token.IsCancellationRequested)
-                {
-                    scope.Complete();
+                    using (var bodyStream = message.BodyStream)
+                    {
+                        var ambientTransaction = new TransportTransaction();
+                        ambientTransaction.Set(Transaction.Current);
+                        var pushContext = new PushContext(message.Id, headers, bodyStream, ambientTransaction, cancellationTokenSource, new ContextBag());
+
+                        await onMessage(pushContext).ConfigureAwait(false);
+                    }
+
+                    if (!cancellationTokenSource.Token.IsCancellationRequested)
+                    {
+                        scope.Complete();
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                if (message == null)
+                {
+                    throw;
+                }
+                var numberOfRetries = GetNumberOfRetries(message.Id);
+
+                var context = new ErrorContext(ex,
+                    message.Id,
+                    headers ?? new Dictionary<string, string>(),
+                    numberOfRetries
+                    );
+
+                var recoveryAction = await onError(context).ConfigureAwait(false);
+
+                AddToLRU(message.Id, recoveryAction);
+            }
         }
+
+        int GetNumberOfRetries(string messageId)
+        {
+            //get this from the LRU/headers etc
+            return 0;
+        }
+
+        void AddToLRU(string messageId, RecoveryAction recoveryAction)
+        {
+
+        }
+
+        bool PerformRecoverabilityAction(Message message)
+        {
+            //check LRU to see if we need to do anything, FLR == do nothing, SLR + Error == move it and return true
+            return false;
+        }
+
 
         TransactionOptions transactionOptions;
 
         static ILog Logger = LogManager.GetLogger<ReceiveWithTransactionScope>();
+    }
+
+    class ErrorContext
+    {
+        public Exception Exception { get; }
+        public string MessageId { get; }
+        public Dictionary<string, string> Headers { get; }
+
+        public  int NumberOfRetries{ get;  }
+
+        public ErrorContext(Exception exception, string messageId, Dictionary<string, string> headers, int numberOfRetries)
+        {
+            Exception = exception;
+            MessageId = messageId;
+            Headers = headers;
+            NumberOfRetries = numberOfRetries;
+        }
+    }
+
+    class RecoveryAction
+    {
+        public bool ImmediateRetry { get; set; }
+        public bool DelayedRetry { get; set; }
+
+        public Dictionary<string,string> AdditionalFaultDetailStuff{ get; set; }
     }
 }
