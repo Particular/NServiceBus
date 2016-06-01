1f0548e5b4f103a267db85ca2ccf1ac6f65a58b3
Brandon Ording <bording@gmail.com>
2016-06-01 15:21:35 -0400

    Add failing test

---
+++ src/NServiceBus.Core.Tests/Persistence/PersistenceStartupTests.cs
@@ -0,0 +1,20 @@
+﻿namespace NServiceBus.Core.Tests.Persistence
+{
+    using NServiceBus.Persistence;
+    using NUnit.Framework;
+    using Settings;
+
+    [TestFixture]
+    public class When_no_persistence_has_been_configured
+    {
+        [Test]
+        public void Should_return_false_when_checking_if_persistence_supports_storage_type()
+        {
+            var settings = new SettingsHolder();
+
+            var supported = PersistenceStartup.HasSupportFor<StorageType.Subscriptions>(settings);
+
+            Assert.IsFalse(supported);
+        }
+    }
+}
