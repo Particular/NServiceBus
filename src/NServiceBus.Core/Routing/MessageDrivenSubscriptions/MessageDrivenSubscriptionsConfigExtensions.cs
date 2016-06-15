434e64069acd642a929507ba97fda3094829bb0a
Brandon Ording <bording@gmail.com>
2016-06-15 19:12:41 -0400

    Cleanup

--- src/NServiceBus.Core/Routing/MessageDrivenSubscriptions/MessageDrivenSubscriptionsConfigExtensions.cs
+++ src/NServiceBus.Core/Routing/MessageDrivenSubscriptions/MessageDrivenSubscriptionsConfigExtensions.cs
@@ -14,14 +14,14 @@ namespace NServiceBus
     public static class MessageDrivenSubscriptionsConfigExtensions
     {
         /// <summary>
-        /// Set a Authorizer to be used when verifying a <see cref="MessageIntentEnum.Subscribe" /> or
+        /// Sets an authorizer to be used when verifying a <see cref="MessageIntentEnum.Subscribe" /> or
         /// <see cref="MessageIntentEnum.Unsubscribe" /> message.
         /// </summary>
         /// <remarks>
-        /// This is a "single instance" extension point. So calling this api multiple time will result in only the last
+        /// This is a "single instance" extension point, so calling this api multiple times will result in only the last
         /// one added being executed at message receive time.
         /// </remarks>
-        /// <param name="transportExtensions">The <see cref="TransportExtensions" /> to extend.</param>
+        /// <param name="transportExtensions">The <see cref="TransportExtensions&lt;T&gt;" /> to extend.</param>
         /// <param name="authorizer">The <see cref="Func{TI,TR}" /> to execute.</param>
         public static void SubscriptionAuthorizer<T>(this TransportExtensions<T> transportExtensions, Func<IIncomingPhysicalMessageContext, bool> authorizer) where T : TransportDefinition, IMessageDrivenSubscriptionTransport
         {
@@ -39,26 +39,26 @@ namespace NServiceBus
         }
 
         /// <summary>
-        /// Registers a publisherEndpoint endpoint for a given endpoint type.
+        /// Registers a publisher endpoint for a given event type.
         /// </summary>
-        /// <param name="extensions">Extensions object.</param>
-        /// <param name="publisherEndpoint">Publisher endpoint.</param>
-        /// <param name="eventType">Event type.</param>
-        public static void RegisterPublisherForType<T>(this TransportExtensions<T> extensions, string publisherEndpoint, Type eventType) where T : TransportDefinition, IMessageDrivenSubscriptionTransport
+        /// <param name="transportExtensions">The <see cref="TransportExtensions&lt;T&gt;" /> to extend.</param>
+        /// <param name="publisherEndpoint">The publisher endpoint.</param>
+        /// <param name="eventType">The event type.</param>
+        public static void RegisterPublisherForType<T>(this TransportExtensions<T> transportExtensions, string publisherEndpoint, Type eventType) where T : TransportDefinition, IMessageDrivenSubscriptionTransport
         {
-            extensions.Settings.GetOrCreate<Publishers>().Add(publisherEndpoint, eventType);
+            transportExtensions.Settings.GetOrCreate<Publishers>().Add(publisherEndpoint, eventType);
         }
 
         /// <summary>
-        /// Registers a publisherEndpoint for all events in a given assembly (and optionally namespace).
+        /// Registers a publisher endpoint for all event types in a given assembly and, optionally, namespace.
         /// </summary>
-        /// <param name="extensions">Extensions.</param>
-        /// <param name="publisherEndpoint">Publisher endpoint.</param>
-        /// <param name="eventAssembly">Assembly containing events.</param>
-        /// <param name="eventNamespace">Optional namespace containing events.</param>
-        public static void RegisterPublisherForAssembly<T>(this TransportExtensions<T> extensions, string publisherEndpoint, Assembly eventAssembly, string eventNamespace = null) where T : TransportDefinition, IMessageDrivenSubscriptionTransport
+        /// <param name="transportExtensions">The <see cref="TransportExtensions&lt;T&gt;" /> to extend.</param>
+        /// <param name="publisherEndpoint">The publisher endpoint.</param>
+        /// <param name="eventAssembly">The assembly containing the event types.</param>
+        /// <param name="eventNamespace">The namespace containing the event types.</param>
+        public static void RegisterPublisherForAssembly<T>(this TransportExtensions<T> transportExtensions, string publisherEndpoint, Assembly eventAssembly, string eventNamespace = null) where T : TransportDefinition, IMessageDrivenSubscriptionTransport
         {
-            extensions.Settings.GetOrCreate<Publishers>().Add(publisherEndpoint, eventAssembly, eventNamespace);
+            transportExtensions.Settings.GetOrCreate<Publishers>().Add(publisherEndpoint, eventAssembly, eventNamespace);
         }
     }
 }
