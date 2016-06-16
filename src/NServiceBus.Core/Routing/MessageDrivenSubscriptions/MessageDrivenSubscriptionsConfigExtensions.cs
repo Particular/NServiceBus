2dab730943f5de13a523a430398f5a5f5c4aa176
Brandon Ording <bording@gmail.com>
2016-06-16 11:57:24 -0400

    Subscription authorizer scoping (#3838)

--- src/NServiceBus.Core/Routing/MessageDrivenSubscriptions/MessageDrivenSubscriptionsConfigExtensions.cs
+++ src/NServiceBus.Core/Routing/MessageDrivenSubscriptions/MessageDrivenSubscriptionsConfigExtensions.cs
@@ -1,8 +1,12 @@
 namespace NServiceBus
 {
     using System;
+    using System.Reflection;
     using Pipeline;
+    using Routing;
+    using Routing.MessageDrivenSubscriptions;
     using Settings;
+    using Transports;
 
     /// <summary>
     /// Provides extensions for configuring message driven subscriptions.
@@ -10,16 +14,16 @@ namespace NServiceBus
     public static class MessageDrivenSubscriptionsConfigExtensions
     {
         /// <summary>
-        /// Set a Authorizer to be used when verifying a <see cref="MessageIntentEnum.Subscribe" /> or
+        /// Sets an authorizer to be used when verifying a <see cref="MessageIntentEnum.Subscribe" /> or
         /// <see cref="MessageIntentEnum.Unsubscribe" /> message.
         /// </summary>
         /// <remarks>
-        /// This is a "single instance" extension point. So calling this api multiple time will result in only the last
+        /// This is a "single instance" extension point, so calling this api multiple times will result in only the last
         /// one added being executed at message receive time.
         /// </remarks>
-        /// <param name="transportExtensions">The <see cref="TransportExtensions" /> to extend.</param>
+        /// <param name="transportExtensions">The <see cref="TransportExtensions&lt;T&gt;" /> to extend.</param>
         /// <param name="authorizer">The <see cref="Func{TI,TR}" /> to execute.</param>
-        public static void SubscriptionAuthorizer(this TransportExtensions transportExtensions, Func<IIncomingPhysicalMessageContext, bool> authorizer)
+        public static void SubscriptionAuthorizer<T>(this TransportExtensions<T> transportExtensions, Func<IIncomingPhysicalMessageContext, bool> authorizer) where T : TransportDefinition, IMessageDrivenSubscriptionTransport
         {
             Guard.AgainstNull(nameof(authorizer), authorizer);
             var settings = transportExtensions.Settings;
@@ -32,6 +36,29 @@ namespace NServiceBus
             Func<IIncomingPhysicalMessageContext, bool> authorizer;
             settings.TryGet("SubscriptionAuthorizer", out authorizer);
             return authorizer;
+        }
+
+        /// <summary>
+        /// Registers a publisher endpoint for a given event type.
+        /// </summary>
+        /// <param name="transportExtensions">The <see cref="TransportExtensions&lt;T&gt;" /> to extend.</param>
+        /// <param name="publisherEndpoint">The publisher endpoint.</param>
+        /// <param name="eventType">The event type.</param>
+        public static void RegisterPublisherForType<T>(this TransportExtensions<T> transportExtensions, string publisherEndpoint, Type eventType) where T : TransportDefinition, IMessageDrivenSubscriptionTransport
+        {
+            transportExtensions.Settings.GetOrCreate<Publishers>().Add(publisherEndpoint, eventType);
+        }
+
+        /// <summary>
+        /// Registers a publisher endpoint for all event types in a given assembly and, optionally, namespace.
+        /// </summary>
+        /// <param name="transportExtensions">The <see cref="TransportExtensions&lt;T&gt;" /> to extend.</param>
+        /// <param name="publisherEndpoint">The publisher endpoint.</param>
+        /// <param name="eventAssembly">The assembly containing the event types.</param>
+        /// <param name="eventNamespace">The namespace containing the event types.</param>
+        public static void RegisterPublisherForAssembly<T>(this TransportExtensions<T> transportExtensions, string publisherEndpoint, Assembly eventAssembly, string eventNamespace = null) where T : TransportDefinition, IMessageDrivenSubscriptionTransport
+        {
+            transportExtensions.Settings.GetOrCreate<Publishers>().Add(publisherEndpoint, eventAssembly, eventNamespace);
         }
     }
 }
