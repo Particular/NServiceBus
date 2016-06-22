5b0183dea6e2daa44acf28a5fa59196eade41c58
andreasohlund <andreas.ohlund@particular.net>
2016-06-22 11:08:52 +0200

    Remove readonly

--- src/NServiceBus.Core/Pipeline/PipelineConfiguration.cs
+++ src/NServiceBus.Core/Pipeline/PipelineConfiguration.cs
@@ -18,6 +18,6 @@
             }
         }
 
-        public readonly PipelineModifications Modifications = new PipelineModifications();
+        public PipelineModifications Modifications = new PipelineModifications();
     }
 }
