77d5c296df83dd59cecbd67ee9318408a352cbd2
Tim Bussmann <tim.bussmann@particular.net>
2016-06-23 16:28:33 +0200

    add DelayedRetryAction

---
+++ src/NServiceBus.Core.Tests/Recoverability/SecondLevelRetries/DelayedRetryActionTests.cs
@@ -0,0 +1,132 @@
+﻿namespace NServiceBus.Core.Tests.Recoverability.SecondLevelRetries
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DelayedDelivery;
+    using Extensibility;
+    using NServiceBus.Transports;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DelayedRetryActionTests
+    {
+        const string TimeoutManagerAddress = "timeout handling endpoint";
+        const string EndpointInputQueue = "endpoint input queue";
+
+        [Test]
+        public async Task Should_dispatch_original_message_body()
+        {
+            var dispatcher = new FakeDispatcher();
+            var delayedRetryAction = new DelayedRetryAction(EndpointInputQueue, dispatcher);
+            var originalBody = Encoding.UTF8.GetBytes("original message body");
+            var incomingMessage = new IncomingMessage("messageId", new Dictionary<string, string>(), new MemoryStream(originalBody));
+            incomingMessage.Body = Encoding.UTF8.GetBytes("updated message body");
+
+            await delayedRetryAction.Retry(incomingMessage, TimeSpan.Zero, new ContextBag());
+
+            var outgoingMessage = dispatcher.TransportOperations.UnicastTransportOperations.Single();
+            Assert.That(outgoingMessage.Message.Body, Is.EqualTo(originalBody));
+        }
+
+        [Test]
+        public async Task Should_float_context_to_dispatcher()
+        {
+            var context = new ContextBag();
+            var dispatcher = new FakeDispatcher();
+            var delayedRetryAction = new DelayedRetryAction(EndpointInputQueue, dispatcher);
+            var incomingMessage = new IncomingMessage("messageId", new Dictionary<string, string>(), Stream.Null);
+
+            await delayedRetryAction.Retry(incomingMessage, TimeSpan.Zero, context);
+
+            Assert.That(dispatcher.ContextBag, Is.SameAs(context));
+        }
+
+        [Test]
+        public async Task When_native_delayed_delivery_should_add_delivery_constraint()
+        {
+            var dispatcher = new FakeDispatcher();
+            var delayedRetryAction = new DelayedRetryAction(EndpointInputQueue, dispatcher);
+            var delay = TimeSpan.FromSeconds(42);
+            var incomingMessage = new IncomingMessage("messageId", new Dictionary<string, string>(), Stream.Null);
+
+            await delayedRetryAction.Retry(incomingMessage, delay, new ContextBag());
+
+            var outgoingMessage = dispatcher.TransportOperations.UnicastTransportOperations.Single();
+            Assert.That(outgoingMessage.Destination, Is.EqualTo(EndpointInputQueue));
+            var deliveryConstraint = outgoingMessage.DeliveryConstraints.OfType<DelayDeliveryWith>().SingleOrDefault();
+            Assert.That(deliveryConstraint, Is.Not.Null);
+            Assert.That(deliveryConstraint.Delay, Is.EqualTo(delay));
+        }
+
+        [Test]
+        public async Task When_no_native_delayed_delivery_should_route_message_to_timeout_manager()
+        {
+            var dispatcher = new FakeDispatcher();
+            var delayedRetryAction = new DelayedRetryAction(EndpointInputQueue, dispatcher, TimeoutManagerAddress);
+            var delay = TimeSpan.FromSeconds(42);
+            var incomingMessage = new IncomingMessage("messageId", new Dictionary<string, string>(), Stream.Null);
+
+            await delayedRetryAction.Retry(incomingMessage, delay, new ContextBag());
+
+            var outgoingMessage = dispatcher.TransportOperations.UnicastTransportOperations.Single();
+            var deliveryConstraint = outgoingMessage.DeliveryConstraints.OfType<DelayDeliveryWith>().SingleOrDefault();
+            Assert.That(deliveryConstraint, Is.Null);
+
+            Assert.That(outgoingMessage.Message.Headers[TimeoutManagerHeaders.RouteExpiredTimeoutTo], Is.EqualTo(EndpointInputQueue));
+            Assert.That(DateTimeExtensions.ToUtcDateTime(outgoingMessage.Message.Headers[TimeoutManagerHeaders.Expire]), Is.GreaterThan(DateTime.UtcNow).And.LessThanOrEqualTo(DateTime.UtcNow + delay));
+            Assert.That(outgoingMessage.Destination, Is.EqualTo(TimeoutManagerAddress));
+        }
+
+        [Test]
+        public async Task Should_update_retry_headers_when_present()
+        {
+            var dispatcher = new FakeDispatcher();
+            var delayedRetryAction = new DelayedRetryAction(EndpointInputQueue, dispatcher);
+            var dictionary = new Dictionary<string, string>()
+            {
+                {Headers.Retries, "2"}
+            };
+            var incomingMessage = new IncomingMessage("messageId", dictionary, Stream.Null);
+
+            await delayedRetryAction.Retry(incomingMessage, TimeSpan.Zero, new ContextBag());
+
+            var outgoingMessageHeaders = dispatcher.TransportOperations.UnicastTransportOperations.Single().Message.Headers;
+            Assert.That(outgoingMessageHeaders[Headers.Retries], Is.EqualTo("3"));
+            Assert.That(incomingMessage.Headers[Headers.Retries], Is.EqualTo("2"));
+            Assert.That(outgoingMessageHeaders.ContainsKey(Headers.RetriesTimestamp), Is.True);
+            Assert.That(incomingMessage.Headers.ContainsKey(Headers.RetriesTimestamp), Is.False);
+        }
+
+        [Test]
+        public async Task Should_add_retry_headers_when_not_present()
+        {
+            var dispatcher = new FakeDispatcher();
+            var delayedRetryAction = new DelayedRetryAction(EndpointInputQueue, dispatcher);
+            var incomingMessage = new IncomingMessage("messageId", new Dictionary<string, string>(), Stream.Null);
+
+            await delayedRetryAction.Retry(incomingMessage, TimeSpan.Zero, new ContextBag());
+
+            var outgoingMessageHeaders = dispatcher.TransportOperations.UnicastTransportOperations.Single().Message.Headers;
+            Assert.That(outgoingMessageHeaders[Headers.Retries], Is.EqualTo("1"));
+            Assert.That(incomingMessage.Headers.ContainsKey(Headers.Retries), Is.False);
+        }
+
+        class FakeDispatcher : IDispatchMessages
+        {
+            public TransportOperations TransportOperations { get; set; }
+
+            public ContextBag ContextBag { get; set; }
+
+            public Task Dispatch(TransportOperations outgoingMessages, ContextBag context)
+            {
+                TransportOperations = outgoingMessages;
+                ContextBag = context;
+                return Task.FromResult(0);
+            }
+        }
+    }
+}
---
+++ src/NServiceBus.Core/Recoverability/SecondLevelRetries/DelayedRetryAction.cs
@@ -0,0 +1,69 @@
+﻿namespace NServiceBus
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using DelayedDelivery;
+    using DeliveryConstraints;
+    using Extensibility;
+    using Routing;
+    using Transports;
+
+    class DelayedRetryAction
+    {
+        string timeoutManagerAddress;
+        readonly IDispatchMessages dispatcher;
+        readonly string endpointInputQueue;
+
+        public DelayedRetryAction(string endpointInputQueue, IDispatchMessages dispatcher, string timeoutManagerAddress = null)
+        {
+            this.timeoutManagerAddress = timeoutManagerAddress;
+            this.dispatcher = dispatcher;
+            this.endpointInputQueue = endpointInputQueue;
+        }
+
+        public Task Retry(IncomingMessage message, TimeSpan delay, ContextBag context)
+        {
+            message.RevertToOriginalBodyIfNeeded();
+
+            var outgoingMessage = new OutgoingMessage(message.MessageId, new Dictionary<string, string>(message.Headers), message.Body);
+
+            var previousRetries = GetNumberOfRetries(message.Headers);
+            outgoingMessage.Headers[Headers.Retries] = (previousRetries + 1).ToString();
+            outgoingMessage.Headers[Headers.RetriesTimestamp] = DateTimeExtensions.ToWireFormattedString(DateTime.UtcNow);
+
+            UnicastAddressTag messageDestination;
+            DeliveryConstraint[] deliveryConstraints = null;
+            if (timeoutManagerAddress == null)
+            {
+                // transport supports native deferred messages, directly send to input queue with delay constraint:
+                deliveryConstraints = new DeliveryConstraint[] { new DelayDeliveryWith(delay)};
+                messageDestination = new UnicastAddressTag(endpointInputQueue);
+            }
+            else
+            {
+                // transport doesn't support native deferred messages, reroute to timeout manager:
+                outgoingMessage.Headers[TimeoutManagerHeaders.RouteExpiredTimeoutTo] = endpointInputQueue;
+                outgoingMessage.Headers[TimeoutManagerHeaders.Expire] = DateTimeExtensions.ToWireFormattedString(DateTime.UtcNow + delay);
+                messageDestination = new UnicastAddressTag(timeoutManagerAddress);
+            }
+
+            var transportOperations = new TransportOperations(new TransportOperation(outgoingMessage, messageDestination, deliveryConstraints: deliveryConstraints));
+            return dispatcher.Dispatch(transportOperations, context);
+        }
+
+        static int GetNumberOfRetries(Dictionary<string, string> headers)
+        {
+            string value;
+            if (headers.TryGetValue(Headers.Retries, out value))
+            {
+                int i;
+                if (int.TryParse(value, out i))
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+    }
+}
