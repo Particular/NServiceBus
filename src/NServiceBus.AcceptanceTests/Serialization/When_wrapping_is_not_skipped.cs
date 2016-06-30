65d77f95b205b1b9a7a2acc6c1ab589aadf24be0
Tim Bussmann <tim.bussmann@particular.net>
2016-06-30 15:43:44 +0200

    added acceptancte test for wrapping raw xml elements

---
+++ src/NServiceBus.AcceptanceTests/Serialization/When_wrapping_is_not_skipped.cs
@@ -0,0 +1,94 @@
+﻿namespace NServiceBus.AcceptanceTests.Serialization
+{
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml.Linq;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using MessageMutator;
+    using NUnit.Framework;
+
+    public class When_wrapping_is_not_skipped : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_wrap_xml_content()
+        {
+            XNamespace ns = "demoNamepsace";
+            var xmlContent = new XDocument(new XElement(ns + "Document", new XElement(ns + "Value", "content")));
+
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<WrappingEndpoint>(e => e
+                    .When(session => session.SendLocal(new MessageWithRawXml { Document = xmlContent })))
+                .Done(c => c.MessageReceived)
+                .Run();
+
+            Assert.That(context.XmlPropertyValue.ToString(), Is.EqualTo(xmlContent.ToString()));
+            Assert.That(context.XmlMessage.Root.Name.LocalName, Is.EqualTo(nameof(MessageWithRawXml)));
+            Assert.That(context.XmlMessage.Root.Elements().Single().Name.LocalName, Is.EqualTo("Document"));
+            Assert.That(context.XmlMessage.Root.Elements().Single().Elements().Single().Name.LocalName, Is.EqualTo("Document"));
+            Assert.That(context.XmlMessage.Root.Elements().Single().Elements().Single().ToString(), Is.EqualTo(xmlContent.ToString()));
+        }
+
+        class Context : ScenarioContext
+        {
+            public bool MessageReceived { get; set; }
+
+            public XDocument XmlMessage { get; set; }
+
+            public XDocument XmlPropertyValue { get; set; }
+        }
+
+        class WrappingEndpoint : EndpointConfigurationBuilder
+        {
+            public WrappingEndpoint()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.UseSerialization<XmlSerializer>(); // wrapping is enabled by default
+                    c.RegisterComponents(r => r.ConfigureComponent<IncomingMutator>(DependencyLifecycle.SingleInstance));
+                });
+            }
+
+            class RawXmlMessageHandler : IHandleMessages<MessageWithRawXml>
+            {
+                public RawXmlMessageHandler(Context scenarioContext)
+                {
+                    this.scenarioContext = scenarioContext;
+                }
+
+                public Task Handle(MessageWithRawXml messageWithRawXml, IMessageHandlerContext context)
+                {
+                    scenarioContext.MessageReceived = true;
+                    scenarioContext.XmlPropertyValue = messageWithRawXml.Document;
+
+                    return Task.FromResult(0);
+                }
+
+                Context scenarioContext;
+            }
+
+            public class IncomingMutator : IMutateIncomingTransportMessages
+            {
+                public IncomingMutator(Context scenarioContext)
+                {
+                    this.scenarioContext = scenarioContext;
+                }
+
+                public Task MutateIncoming(MutateIncomingTransportMessageContext context)
+                {
+                    scenarioContext.XmlMessage = XDocument.Parse(Encoding.UTF8.GetString(context.Body));
+
+                    return Task.FromResult(0);
+                }
+
+                Context scenarioContext;
+            }
+        }
+
+        class MessageWithRawXml : ICommand
+        {
+            public XDocument Document { get; set; }
+        }
+    }
+}
