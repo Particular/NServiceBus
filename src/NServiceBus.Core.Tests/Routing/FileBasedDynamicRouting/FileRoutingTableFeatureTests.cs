6a876330b687a6edc7ef182942585101efa2731d
Tim Bussmann <tim.bussmann@particular.net>
2016-07-06 09:23:25 +0200

    testing instance mapping default values

---
+++ src/NServiceBus.Core.Tests/Routing/FileBasedDynamicRouting/FileRoutingTableFeatureTests.cs
@@ -0,0 +1,32 @@
+﻿namespace NServiceBus.Core.Tests.Routing.FileBasedDynamicRouting
+{
+    using System;
+    using System.Linq;
+    using NServiceBus.Features;
+    using NUnit.Framework;
+    using Settings;
+
+    [TestFixture]
+    public class FileRoutingTableFeatureTests
+    {
+        [Test]
+        public void Should_configure_default_values()
+        {
+            var feature = new FileRoutingTableFeature();
+            var settings = new SettingsHolder();
+
+            feature.ConfigureDefaults(settings);
+
+            Assert.That(settings.Get<string>(FileRoutingTableFeature.FilePathSettingsKey), Is.EqualTo("instance-mapping.xml"));
+            Assert.That(settings.Get<TimeSpan>(FileRoutingTableFeature.CheckIntervalSettingsKey), Is.EqualTo(TimeSpan.FromSeconds(30)));
+        }
+
+        [Test]
+        public void Requires_routing_enabled()
+        {
+            var feature = new FileRoutingTableFeature();
+
+            Assert.That(feature.Dependencies.Any(dependencies => dependencies.Any(dependency => dependency == typeof(RoutingFeature).FullName)));
+        }
+    }
+}
