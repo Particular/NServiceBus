e878ad9b6e49d922de183301acda077634c07c56
Tim Bussmann <tim.bussmann@particular.net>
2016-07-19 12:26:06 +0200

    add acceptance tests for subscriptions

---
+++ src/NServiceBus.AcceptanceTests/Routing/AutomaticSubscriptions/When_subscribing_to_base_of_polymorphic_event.cs
@@ -0,0 +1,86 @@
+﻿namespace NServiceBus.AcceptanceTests.Routing.AutomaticSubscriptions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using AcceptanceTesting.Customization;
+    using Configuration.AdvanceExtensibility;
+    using EndpointTemplates;
+    using Features;
+    using NServiceBus.Routing;
+    using NUnit.Framework;
+
+    public class When_subscribing_to_base_of_polymorphic_event
+    {
+        [Test]
+        public async Task Should_use_child_route()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<PolymorphicRoutingConfigSubscriber>(e => e
+                    .When(c => c.Subscribe<BaseEvent>()))
+                .WithEndpoint<PolymorphicEventPublisher>()
+                .Done(c => c.SubscribedEventTypes.Any())
+                .Run();
+
+            Assert.That(context.SubscribedEventTypes, Contains.Item(typeof(BaseEvent).FullName));
+        }
+
+        [Test]
+        public async Task Should_use_child_endpoint_mapping()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<PolymorphicEndpointMappingSubscriber>(e => e
+                    .When(c => c.Subscribe<BaseEvent>()))
+                .WithEndpoint<PolymorphicEventPublisher>()
+                .Done(c => c.SubscribedEventTypes.Any())
+                .Run();
+
+            Assert.That(context.SubscribedEventTypes, Contains.Item(typeof(BaseEvent).AssemblyQualifiedName));
+        }
+
+        class Context : ScenarioContext
+        {
+            public List<string> SubscribedEventTypes { get; } = new List<string>();
+        }
+
+        class PolymorphicRoutingConfigSubscriber : EndpointConfigurationBuilder
+        {
+            public PolymorphicRoutingConfigSubscriber()
+            {
+                // configure routing for the child message, no routing for the base message
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.DisableFeature<AutoSubscribe>();
+                    c.GetSettings().GetOrCreate<UnicastRoutingTable>()
+                        .RouteToEndpoint(typeof(ChildEvent), Conventions.EndpointNamingConvention(typeof(PolymorphicEventPublisher)));
+                });
+            }
+        }
+
+        class PolymorphicEndpointMappingSubscriber : EndpointConfigurationBuilder
+        {
+            public PolymorphicEndpointMappingSubscriber()
+            {
+                // configure routing for the child message, no routing for the base message
+                EndpointSetup<DefaultServer>().AddMapping<ChildEvent>(typeof(PolymorphicEventPublisher));
+            }
+        }
+
+        class PolymorphicEventPublisher : EndpointConfigurationBuilder
+        {
+            public PolymorphicEventPublisher()
+            {
+                EndpointSetup<DefaultServer>(c => c.OnEndpointSubscribed<Context>((subscripton, context) => context.SubscribedEventTypes.Add(subscripton.MessageType)));
+            }
+        }
+
+        public class BaseEvent : IEvent
+        {
+        }
+
+        public class ChildEvent : BaseEvent
+        {
+        }
+    }
+}
---
+++ src/NServiceBus.AcceptanceTests/Routing/AutomaticSubscriptions/When_subscribing_to_child_of_polymorphic_event.cs
@@ -0,0 +1,79 @@
+﻿namespace NServiceBus.AcceptanceTests.Routing.AutomaticSubscriptions
+{
+    using System;
+    using AcceptanceTesting;
+    using AcceptanceTesting.Customization;
+    using Configuration.AdvanceExtensibility;
+    using EndpointTemplates;
+    using Features;
+    using NServiceBus.Routing;
+    using NUnit.Framework;
+
+    public class When_subscribing_to_child_of_polymorphic_event : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_not_find_destination_from_routing()
+        {
+            var exception = Assert.ThrowsAsync<AggregateException>(async () => await Scenario.Define<ScenarioContext>()
+                .WithEndpoint<PolymorphicRoutingConfigSubscriber>(e => e
+                    .When(c => c.Subscribe<ChildEvent>()))
+                .WithEndpoint<PolymorphicEventPublisher>()
+                .Done(c => true)
+                .Run());
+
+            Assert.That(exception.InnerException.InnerException, Is.TypeOf<Exception>().With.Message.Contain("No publisher address could be found for message type NServiceBus.AcceptanceTests.Routing.AutomaticSubscriptions.When_subscribing_to_child_of_polymorphic_event+ChildEvent"));
+        }
+
+        [Test]
+        public void Should_not_find_destination_from_endpoint_mappings()
+        {
+            var exception = Assert.ThrowsAsync<AggregateException>(async () => await Scenario.Define<ScenarioContext>()
+                .WithEndpoint<PolymorphicEndpointMappingSubscriber>(e => e
+                    .When(c => c.Subscribe<ChildEvent>()))
+                .WithEndpoint<PolymorphicEventPublisher>()
+                .Done(c => true)
+                .Run());
+
+            Assert.That(exception.InnerException.InnerException, Is.TypeOf<Exception>().With.Message.Contain("No publisher address could be found for message type NServiceBus.AcceptanceTests.Routing.AutomaticSubscriptions.When_subscribing_to_child_of_polymorphic_event+ChildEvent"));
+        }
+
+        class PolymorphicRoutingConfigSubscriber : EndpointConfigurationBuilder
+        {
+            public PolymorphicRoutingConfigSubscriber()
+            {
+                // configure routing for the child message, no routing for the base message
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.DisableFeature<AutoSubscribe>();
+                    c.GetSettings().GetOrCreate<UnicastRoutingTable>()
+                        .RouteToEndpoint(typeof(BaseEvent), Conventions.EndpointNamingConvention(typeof(PolymorphicEventPublisher)));
+                });
+            }
+        }
+
+        class PolymorphicEndpointMappingSubscriber : EndpointConfigurationBuilder
+        {
+            public PolymorphicEndpointMappingSubscriber()
+            {
+                // configure routing for the child message, no routing for the base message
+                EndpointSetup<DefaultServer>().AddMapping<BaseEvent>(typeof(PolymorphicEventPublisher));
+            }
+        }
+
+        class PolymorphicEventPublisher : EndpointConfigurationBuilder
+        {
+            public PolymorphicEventPublisher()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+        }
+
+        public class BaseEvent : IEvent
+        {
+        }
+
+        public class ChildEvent : BaseEvent
+        {
+        }
+    }
+}
