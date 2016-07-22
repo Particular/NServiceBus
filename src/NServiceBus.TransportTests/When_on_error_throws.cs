e8bdafcb24ad0b7728e9fc6c427bd58f06a8cf9f
Tomasz Masternak <tomasz.masternak@particular.net>
2016-07-22 10:54:21 +0200

    test case for failing onError method

---
+++ src/NServiceBus.TransportTests/When_on_error_throws.cs
@@ -0,0 +1,48 @@
+﻿namespace NServiceBus.TransportTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Transport;
+
+    public class When_on_error_throws : NServiceBusTransportTest
+    {
+        // [TestCase(TransportTransactionMode.None)] -- not relevant
+        [TestCase(TransportTransactionMode.ReceiveOnly)]
+        [TestCase(TransportTransactionMode.SendsAtomicWithReceive)]
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_reinvoke_on_error_with_orignal_exception(TransportTransactionMode transactionMode)
+        {
+            var onErrorCalled = new TaskCompletionSource<ErrorContext>();
+
+            OnTestTimeout(() => onErrorCalled.SetCanceled());
+
+            var firstInvocation = true;
+
+            await StartPump(
+                context =>
+                {
+                    throw new Exception("Simulated exception");
+                },
+                context =>
+                {
+                    if (firstInvocation)
+                    {
+                        firstInvocation = false;
+
+                        throw new Exception("Exception from onError");
+                    }
+
+                    onErrorCalled.SetResult(context);
+
+                    return Task.FromResult(ErrorHandleResult.Handled);
+                }, transactionMode);
+
+            await SendMessage(InputQueueName);
+
+            var errorContext = await onErrorCalled.Task;
+
+            Assert.AreEqual(errorContext.Exception.Message, "Simulated exception");
+        }
+    }
+}
