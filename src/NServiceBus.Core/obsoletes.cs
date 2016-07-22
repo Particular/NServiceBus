c12cc4b2166eb250b91c39c7f61cce42c8fd0a2e
Daniel Marbach <daniel.marbach@particular.net>
2016-07-22 12:41:45 +0200

    Fixed misleading obsolete for ISatellite

--- src/NServiceBus.Core/obsoletes.cs
+++ src/NServiceBus.Core/obsoletes.cs
@@ -1541,7 +1541,7 @@ namespace NServiceBus.Pipeline
 namespace NServiceBus.Satellites
 {
     [ObsoleteEx(
-        Message = "ISatellite is no longer an extension point. In order to create a satellite one must create a feature that uses AddSatellitePipeline() method and a class that inherits from SatelliteBehavior that is used for processing the messages.",
+        Message = "ISatellite is no longer an extension point. In order to create a satellite one must create a feature that uses AddSatelliteReceiver() method and a class that inherits from PipelineTerminator<ISatelliteProcessingContext> that is used for processing the messages.",
         RemoveInVersion = "7",
         TreatAsErrorFromVersion = "6")]
     public interface IAdvancedSatellite
@@ -1549,7 +1549,7 @@ namespace NServiceBus.Satellites
     }
 
     [ObsoleteEx(
-        Message = "ISatellite is no longer an extension point. In order to create a satellite one must create a feature that uses AddSatellitePipeline() method and a class that inherits from SatelliteBehavior that is used for processing the messages.",
+        Message = "ISatellite is no longer an extension point. In order to create a satellite one must create a feature that uses AddSatelliteReceiver() method and a class that inherits from PipelineTerminator<ISatelliteProcessingContext> that is used for processing the messages.",
         RemoveInVersion = "7",
         TreatAsErrorFromVersion = "6")]
     public interface ISatellite
