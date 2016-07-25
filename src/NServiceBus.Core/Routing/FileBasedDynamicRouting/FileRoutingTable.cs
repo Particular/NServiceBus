bb941acd7c1a927846d0f96fe8ea4a6ca90abe1f
Tim Bussmann <tim.bussmann@particular.net>
2016-07-25 09:56:02 +0200

    log changes to instance mapping file

--- src/NServiceBus.Core/Routing/FileBasedDynamicRouting/FileRoutingTable.cs
+++ src/NServiceBus.Core/Routing/FileBasedDynamicRouting/FileRoutingTable.cs
@@ -3,6 +3,7 @@ namespace NServiceBus
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
     using System.Threading.Tasks;
     using Features;
     using Logging;
@@ -36,8 +37,10 @@ namespace NServiceBus
                 var instances = parser.Parse(doc);
 
                 var newInstanceMap = instances
-                .GroupBy(i => i.Endpoint)
-                .ToDictionary(g => g.Key, g => new HashSet<EndpointInstance>(g));
+                    .GroupBy(i => i.Endpoint)
+                    .ToDictionary(g => g.Key, g => new HashSet<EndpointInstance>(g));
+
+                LogChanges(instanceMap, newInstanceMap);
 
                 instanceMap = newInstanceMap;
             }
@@ -45,6 +48,50 @@ namespace NServiceBus
             {
                 throw new Exception($"An error occurred while reading the endpoint instance mapping file at {filePath}. See the inner exception for more details.", ex);
             }
+        }
+
+        void LogChanges(Dictionary<string, HashSet<EndpointInstance>> oldInstanceMap, Dictionary<string, HashSet<EndpointInstance>> newInstanceMap)
+        {
+            var output = new StringBuilder();
+            var hasChanges = false;
+            output.AppendLine($"Updating instance mapping table from '{filePath}':");
+
+            foreach (var endpoint in newInstanceMap)
+            {
+                HashSet<EndpointInstance> existingInstances;
+                if (oldInstanceMap.TryGetValue(endpoint.Key, out existingInstances))
+                {
+                    var newInstances = endpoint.Value.Except(existingInstances).Count();
+                    var removedInstances = existingInstances.Except(endpoint.Value).Count();
+
+                    if (newInstances > 0 || removedInstances > 0)
+                    {
+                        output.AppendLine($"Updated endpoint '{endpoint.Key}': +{Instances(newInstances)}, -{Instances(removedInstances)}");
+                        hasChanges = true;
+                    }
+                }
+                else
+                {
+                    output.AppendLine($"Added endpoint '{endpoint.Key}' with {Instances(endpoint.Value.Count)}");
+                    hasChanges = true;
+                }
+            }
+
+            foreach (var removedEndpoint in oldInstanceMap.Keys.Except(newInstanceMap.Keys))
+            {
+                output.AppendLine($"Removed all instances of endpoint '{removedEndpoint}'");
+                hasChanges = true;
+            }
+
+            if (hasChanges)
+            {
+                log.Info(output.ToString());
+            }
+        }
+
+        static string Instances(int count)
+        {
+            return count > 1 ? $"{count} instances" : $"{count} instance";
         }
 
         public Task<IEnumerable<EndpointInstance>> FindInstances(string endpoint)
@@ -63,7 +110,7 @@ namespace NServiceBus
         IRoutingFileAccess fileAccess;
         string filePath;
 
-        Dictionary<string, HashSet<EndpointInstance>> instanceMap;
+        Dictionary<string, HashSet<EndpointInstance>> instanceMap = new Dictionary<string, HashSet<EndpointInstance>>(0);
         FileRoutingTableParser parser = new FileRoutingTableParser();
         IAsyncTimer timer;
         Task<IEnumerable<EndpointInstance>> noInstances = Task.FromResult(Enumerable.Empty<EndpointInstance>());
