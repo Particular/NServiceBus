8e88129b629c92a78bfaf6d8feac0d8d0b164fd7
Daniel Marbach <daniel.marbach@openplace.net>
2016-07-27 13:45:44 +0000

    ExtendableOptions sets the message id header. (#3961)

--- src/NServiceBus.Core/Pipeline/Outgoing/OutgoingPhysicalToRoutingConnector.cs
+++ src/NServiceBus.Core/Pipeline/Outgoing/OutgoingPhysicalToRoutingConnector.cs
@@ -9,8 +9,6 @@
     {
         public override Task Invoke(IOutgoingPhysicalMessageContext context, Func<IRoutingContext, Task> stage)
         {
-            context.Headers[Headers.MessageId] = context.MessageId;
-
             var message = new OutgoingMessage(context.MessageId, context.Headers, context.Body);
 
             return stage(this.CreateRoutingContext(message, context.RoutingStrategies, context));
