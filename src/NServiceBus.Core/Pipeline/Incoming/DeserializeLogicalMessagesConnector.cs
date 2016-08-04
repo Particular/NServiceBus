7d3267abb2c045c9600195875319b9bd51966be8
Daniel Marbach <daniel.marbach@particular.net>
2016-08-04 11:26:03 +0200

    Reduced DeserializeLogicalMessagesConnector to one last linq statement

--- src/NServiceBus.Core/Pipeline/Incoming/DeserializeLogicalMessagesConnector.cs
+++ src/NServiceBus.Core/Pipeline/Incoming/DeserializeLogicalMessagesConnector.cs
@@ -1,4 +1,5 @@
-﻿namespace NServiceBus
+﻿// ReSharper disable ReturnTypeCanBeEnumerable.Local
+namespace NServiceBus
 {
     using System;
     using System.Collections.Generic;
@@ -36,7 +37,7 @@
             return incomingMessage.Headers.ContainsKey(Headers.ControlMessageHeader) && incomingMessage.Headers[Headers.ControlMessageHeader] == Boolean.TrueString;
         }
 
-        IEnumerable<LogicalMessage> ExtractWithExceptionHandling(IncomingMessage message)
+        LogicalMessage[] ExtractWithExceptionHandling(IncomingMessage message)
         {
             try
             {
@@ -48,19 +49,19 @@
             }
         }
 
-        IEnumerable<LogicalMessage> Extract(IncomingMessage physicalMessage)
+        LogicalMessage[] Extract(IncomingMessage physicalMessage)
         {
             // We need this check to be compatible with v3.3 endpoints, v3.3 control messages also include a body
             if (IsControlMessage(physicalMessage))
             {
                 log.Debug("Received a control message. Skipping deserialization as control message data is contained in the header.");
-                return Enumerable.Empty<LogicalMessage>();
+                return NoMessagesFound;
             }
 
             if (physicalMessage.Body == null || physicalMessage.Body.Length == 0)
             {
                 log.Debug("Received a message without body. Skipping deserialization.");
-                return Enumerable.Empty<LogicalMessage>();
+                return NoMessagesFound;
             }
 
             string messageTypeIdentifier;
@@ -68,7 +69,7 @@
 
             if (physicalMessage.Headers.TryGetValue(Headers.EnclosedMessageTypes, out messageTypeIdentifier))
             {
-                foreach (var messageTypeString in messageTypeIdentifier.Split(';'))
+                foreach (var messageTypeString in messageTypeIdentifier.Split(EnclosedMessageTypeSeparator))
                 {
                     var typeString = messageTypeString;
 
@@ -105,23 +106,29 @@
             var messageTypes = messageMetadata.Select(metadata => metadata.MessageType).ToList();
             var messageSerializer = deserializerResolver.Resolve(physicalMessage.Headers);
 
-            // For nested behaviors who have an expectation ContentType existing 
-            // add the default content type 
+            // For nested behaviors who have an expectation ContentType existing
+            // add the default content type
             physicalMessage.Headers[Headers.ContentType] = messageSerializer.ContentType;
 
             using (var stream = new MemoryStream(physicalMessage.Body))
             {
-                return messageSerializer.Deserialize(stream, messageTypes)
-                    .Select(x => logicalMessageFactory.Create(x.GetType(), x));
+                var deserializedMessages = messageSerializer.Deserialize(stream, messageTypes);
+                var logicalMessages = new LogicalMessage[deserializedMessages.Length];
+                for (var i = 0; i < deserializedMessages.Length; i++)
+                {
+                    var x = deserializedMessages[i];
+                    logicalMessages[i] = logicalMessageFactory.Create(x.GetType(), x);
+                }
+                return logicalMessages;
             }
         }
 
-        bool DoesTypeHaveImplAddedByVersion3(string existingTypeString)
+        static bool DoesTypeHaveImplAddedByVersion3(string existingTypeString)
         {
             return existingTypeString.Contains("__impl");
         }
 
-        bool IsV4OrBelowScheduledTask(string existingTypeString)
+        static bool IsV4OrBelowScheduledTask(string existingTypeString)
         {
             return existingTypeString.StartsWith("NServiceBus.Scheduling.Messages.ScheduledTask, NServiceBus.Core");
         }
@@ -129,6 +136,13 @@
         MessageDeserializerResolver deserializerResolver;
         LogicalMessageFactory logicalMessageFactory;
         MessageMetadataRegistry messageMetadataRegistry;
+
+        static LogicalMessage[] NoMessagesFound = new LogicalMessage[0];
+
+        static char[] EnclosedMessageTypeSeparator =
+        {
+            ';'
+        };
 
         static ILog log = LogManager.GetLogger<DeserializeLogicalMessagesConnector>();
     }
