8780189a57dee550f4c8124569b2f13e27eb57c5
Tim Bussmann <timbussmann@users.noreply.github.com>
2016-08-09 17:21:12 +0200

    Improve exception on ambiguous deserializers (#4033)
    * throw meaningful exception when registering multiple deserializers with same content-type
    
    * using FakeSerializer in MessageDeserializerResolverTests
    
    * adjust exception message

--- src/NServiceBus.Core.Tests/Serializers/MessageDeserializerResolverTests.cs
+++ src/NServiceBus.Core.Tests/Serializers/MessageDeserializerResolverTests.cs
@@ -2,57 +2,95 @@
 {
     using System;
     using System.Collections.Generic;
-    using MessageInterfaces.MessageMapper.Reflection;
+    using System.IO;
     using Serialization;
     using NUnit.Framework;
 
     [TestFixture]
     public class MessageDeserializerResolverTests
     {
-        MessageDeserializerResolver resolver;
-
-        [SetUp]
-        public void Setup()
+        [TestCase(ContentTypes.Xml)]
+        [TestCase(ContentTypes.Json)]
+        public void RetrievesSerializerByContentType(string contentType)
         {
-            var mapper = new MessageMapper();
-            var xml = new XmlMessageSerializer(mapper, new Conventions());
-            var json = new JsonMessageSerializer(mapper);
-            resolver = new MessageDeserializerResolver(xml, new IMessageSerializer[]
+            var expectedResolver = new FakeSerializer(contentType);
+            var resolver = new MessageDeserializerResolver(new FakeSerializer("default"), new IMessageSerializer[]
             {
-                xml,
-                json
+                new FakeSerializer("some/content/type"),
+                expectedResolver,
+                new FakeSerializer("another/content/type")
             });
-        }
 
-        [TestCase(ContentTypes.Xml, typeof(XmlMessageSerializer))]
-        [TestCase(ContentTypes.Json, typeof(JsonMessageSerializer))]
-        public void RetrievesSerializerByContentType(string contentType, Type expected)
-        {
             var headers = new Dictionary<string, string>
             {
                 {Headers.ContentType, contentType}
             };
             var serializer = resolver.Resolve(headers);
-            Assert.IsInstanceOf(expected, serializer);
+            Assert.AreSame(expectedResolver, serializer);
         }
 
         [Test]
         public void UnknownContentTypeFallsBackToDefaultSerialization()
         {
+            var defaultSerializer = new FakeSerializer(ContentTypes.Xml);
+            var resolver = new MessageDeserializerResolver(defaultSerializer, new IMessageSerializer[]
+            {
+                new FakeSerializer(ContentTypes.Json)
+            });
+
             var headers = new Dictionary<string, string>
             {
                 {Headers.ContentType, "unknown/unsupported"}
             };
             var serializer = resolver.Resolve(headers);
-            Assert.IsInstanceOf<XmlMessageSerializer>(serializer);
+
+            Assert.AreSame(defaultSerializer, serializer);
         }
 
         [Test]
         public void NoContentTypeFallsBackToDefaultSerialization()
         {
-            var headers = new Dictionary<string, string>();
-            var serializer = resolver.Resolve(headers);
-            Assert.IsInstanceOf<XmlMessageSerializer>(serializer);
+            var defaultSerializer = new FakeSerializer(ContentTypes.Xml);
+            var resolver = new MessageDeserializerResolver(defaultSerializer, new IMessageSerializer[]
+            {
+                new FakeSerializer(ContentTypes.Json)
+            });
+
+            var serializer = resolver.Resolve(new Dictionary<string, string>());
+
+            Assert.AreEqual(defaultSerializer, serializer);
+        }
+
+        [Test]
+        public void MultipleDeserializersWithSameContentTypeShouldThrowException()
+        {
+            var deserializer1 = new FakeSerializer("my/content/type");
+            var deserializer2 = new FakeSerializer("my/content/type");
+
+            Assert.That(() => new MessageDeserializerResolver(new FakeSerializer("xml"), new IMessageSerializer[]
+            {
+                deserializer1,
+                deserializer2
+            }), Throws.Exception.TypeOf<Exception>().And.Message.Contains($"Multiple deserializers are registered for content-type '{deserializer1.ContentType}'. Remove ambiguous deserializers."));
+        }
+
+        class FakeSerializer : IMessageSerializer
+        {
+            public FakeSerializer(string contentType)
+            {
+                ContentType = contentType;
+            }
+
+            public void Serialize(object message, Stream stream)
+            {
+            }
+
+            public object[] Deserialize(Stream stream, IList<Type> messageTypes = null)
+            {
+                throw new NotImplementedException();
+            }
+
+            public string ContentType { get; }
         }
     }
 }
