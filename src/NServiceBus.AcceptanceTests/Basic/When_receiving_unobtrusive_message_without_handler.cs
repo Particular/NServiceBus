ab696a408ef800eea56c50940d96a65c7b8da5f5
Daniel Marbach <daniel.marbach@openplace.net>
2016-08-11 09:05:23 +0000

    MessageMetadataRegistry GetMessageMetadata with strings registers type if it matches the message conventions (#4029)
    * Reproducing message metadata registry lazy registration issue with unobtrusive mode
    
    * Lazy registration of message metadata if the message type can be loaded

---
+++ src/NServiceBus.AcceptanceTests/Basic/When_receiving_unobtrusive_message_without_handler.cs
@@ -0,0 +1,65 @@
+namespace NServiceBus.AcceptanceTests.Basic
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using Logging;
+    using NUnit.Framework;
+
+    public class When_receiving_unobtrusive_message_without_handler : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Message_should_be_moved_to_error_cause_handler_not_found()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<Sender>(c => c.When(s => s.Send(new MyCommand())))
+                .WithEndpoint<Receiver>(c => c.DoNotFailOnErrorMessages())
+                .Done(c => c.FailedMessages.Any())
+                .Run();
+
+            Assert.True(context.Logs.Any(l => l.Level == LogLevel.Error && l.Message.Contains($"No handlers could be found for message type: { typeof(MyCommand).FullName}")), "No handlers could be found was not logged.");
+            Assert.False(context.Logs.Any(l => l.Level == LogLevel.Warn && l.Message.Contains($"Message header '{ typeof(MyCommand).FullName }' was mapped to type '{ typeof(MyCommand).FullName }' but that type was not found in the message registry, ensure the same message registration conventions are used in all endpoints, especially if using unobtrusive mode.")), "Message type could not be mapped.");
+            Assert.False(context.Logs.Any(l => l.Level == LogLevel.Warn && l.Message.Contains($"Could not determine message type from message header '{ typeof(MyCommand).FullName}'")), "Message type could not be mapped.");
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool WasCalled { get; set; }
+        }
+
+        public class Sender : EndpointConfigurationBuilder
+        {
+            public Sender()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.Conventions()
+                        .DefiningCommandsAs(t => t.Namespace != null && t.FullName == typeof(MyCommand).FullName);
+
+                    c.UseSerialization<JsonSerializer>();
+                }).AddMapping<MyCommand>(typeof(Receiver))
+                    .ExcludeType<MyCommand>(); // remove that type from assembly scanning to simulate what would happen with true unobtrusive mode
+            }
+        }
+
+
+        public class Receiver : EndpointConfigurationBuilder
+        {
+            public Receiver()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.Conventions().DefiningCommandsAs(t => t.Namespace != null && t.FullName == typeof(MyCommand).FullName);
+
+                    c.UseSerialization<JsonSerializer>();
+                })
+                    .ExcludeType<MyCommand>(); // remove that type from assembly scanning to simulate what would happen with true unobtrusive mode
+            }
+        }
+
+        public class MyCommand
+        {
+        }
+    }
+}
