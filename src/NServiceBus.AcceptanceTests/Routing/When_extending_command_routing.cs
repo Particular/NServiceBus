4157364af0fc975d0b667d89c5eca140720502dc
SzymonPobiega <szymon.pobiega@gmail.com>
2016-08-11 11:05:04 +0200

    Added acceptance tests for extensibility APIs.

---
+++ src/NServiceBus.AcceptanceTests/Routing/When_extending_command_routing.cs
@@ -0,0 +1,109 @@
+﻿namespace NServiceBus.AcceptanceTests.Routing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using AcceptanceTesting.Customization;
+    using EndpointTemplates;
+    using Features;
+    using NServiceBus.Routing;
+    using NUnit.Framework;
+    using ScenarioDescriptors;
+
+    public class When_extending_command_routing : NServiceBusAcceptanceTest
+    {
+        static string ReceiverEndpoint => Conventions.EndpointNamingConvention(typeof(Receiver));
+
+        [Test]
+        public async Task Should_route_commands_correctly()
+        {
+            await Scenario.Define<Context>()
+                .WithEndpoint<Sender>(b =>
+                    b.When(c => c.EndpointsStarted, async session =>
+                    {
+                        await session.Send(new MyCommand());
+                        await session.Send(new MyCommand());
+                        await session.Send(new MyCommand());
+                        await session.Send(new MyCommand());
+                    })
+                )
+                .WithEndpoint<Receiver>()
+                .Done(c => c.MessageDelivered >= 4)
+                .Repeat(r => r.For(Transports.Default))
+                .Should(c =>
+                {
+                    Assert.IsTrue(c.MessageDelivered >= 4);
+                })
+                .Run();
+        }
+
+        public class Context : ScenarioContext
+        {
+            public int MessageDelivered;
+        }
+
+        public class Sender : EndpointConfigurationBuilder
+        {
+            public Sender()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class CustomRoutingFeature : Feature
+            {
+                public CustomRoutingFeature()
+                {
+                    EnableByDefault();
+                }
+
+                protected override void Setup(FeatureConfigurationContext context)
+                {
+                    context.RoutingTable().AddOrReplaceRoutes(Guid.NewGuid(), new List<RouteTableEntry>
+                    {
+                        new RouteTableEntry(typeof(MyCommand), UnicastRoute.CreateFromEndpointName(ReceiverEndpoint))
+                    });
+                    context.EndpointInstances().AddOrReplaceInstances(Guid.NewGuid(), new List<EndpointInstance>()
+                    {
+                        new EndpointInstance(ReceiverEndpoint, "XYZ"),
+                        new EndpointInstance(ReceiverEndpoint, "ABC")
+                    });
+                    context.DistributionPolicy().SetDistributionStrategy(ReceiverEndpoint, new XyzDistributionStrategy());
+                }
+
+                class XyzDistributionStrategy : DistributionStrategy
+                {
+                    public override UnicastRoutingTarget SelectDestination(UnicastRoutingTarget[] allInstances)
+                    {
+                        return allInstances.First(x => x.Instance.Discriminator.Contains("XYZ"));
+                    }
+                }
+            }
+        }
+
+        public class Receiver : EndpointConfigurationBuilder
+        {
+            public Receiver()
+            {
+                EndpointSetup<DefaultServer>(c => c.MakeInstanceUniquelyAddressable("XYZ"));
+            }
+
+            public class MyCommandHandler : IHandleMessages<MyCommand>
+            {
+                public Context Context { get; set; }
+
+                public Task Handle(MyCommand evnt, IMessageHandlerContext context)
+                {
+                    Interlocked.Increment(ref Context.MessageDelivered);
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+        public class MyCommand : ICommand
+        {
+        }
+    }
+}
