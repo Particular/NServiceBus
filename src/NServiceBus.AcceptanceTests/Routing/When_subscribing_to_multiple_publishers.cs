ffbb4941fea846076866cd9ea46c10f866ccc20b
Tim Bussmann <tim.bussmann@particular.net>
2016-08-15 14:17:58 +0200

    add acceptance test for subscribing to multiple publishers

---
+++ src/NServiceBus.AcceptanceTests/Routing/When_subscribing_to_multiple_publishers.cs
@@ -0,0 +1,69 @@
+﻿namespace NServiceBus.AcceptanceTests.Routing
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NUnit.Framework;
+    using ScenarioDescriptors;
+
+    public class When_subscribing_to_multiple_publishers : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_subscribe_to_all_registered_publishers_of_same_type()
+        {
+            await Scenario.Define<Context>()
+                .WithEndpoint<Subscriber>(e => e
+                    .When(s => s.Subscribe<SomeEvent>()))
+                .WithEndpoint<Publisher>(e => e
+                    .CustomConfig(cfg =>
+                    {
+                        cfg.OverrideLocalAddress("Publisher1");
+                        cfg.OnEndpointSubscribed<Context>((args, ctx) => ctx.SubscribedToPublisher1 = true);
+                    }))
+                .WithEndpoint<Publisher>(e => e
+                    .CustomConfig(cfg =>
+                    {
+                        cfg.OverrideLocalAddress("Publisher2");
+                        cfg.OnEndpointSubscribed<Context>((args, ctx) => ctx.SubscribedToPublisher2 = true);
+                    }))
+                .Repeat(r => r.For<AllTransportsWithMessageDrivenPubSub>())
+                .Should(c =>
+                {
+                    Assert.That(c.SubscribedToPublisher1, Is.True);
+                    Assert.That(c.SubscribedToPublisher2, Is.True);
+                })
+                .Run();
+        }
+
+        class Context : ScenarioContext
+        {
+            public bool SubscribedToPublisher1 { get; set; }
+            public bool SubscribedToPublisher2 { get; set; }
+        }
+
+        class Subscriber : EndpointConfigurationBuilder
+        {
+            public Subscriber()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    var routing = c.MessageDrivenPubSubRouting();
+                    routing.RegisterPublisher(typeof(SomeEvent), "Publisher1");
+                    routing.RegisterPublisher(typeof(SomeEvent), "Publisher2");
+                });
+            }
+        }
+
+        class Publisher : EndpointConfigurationBuilder
+        {
+            public Publisher()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+        }
+
+        class SomeEvent : IEvent
+        {
+        }
+    }
+}
