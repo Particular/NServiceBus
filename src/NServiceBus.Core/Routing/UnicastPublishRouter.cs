981ecc843ba6d520796aed7bfd890538c62f118e
Tim Bussmann <tim.bussmann@particular.net>
2016-08-16 18:59:59 +0200

    cleanup publishrouter

--- src/NServiceBus.Core/Routing/UnicastPublishRouter.cs
+++ src/NServiceBus.Core/Routing/UnicastPublishRouter.cs
@@ -22,17 +22,17 @@ namespace NServiceBus
         {
             var typesToRoute = messageMetadataRegistry.GetMessageMetadata(messageType).MessageHierarchy;
 
-            var subscribers = await GetDestinations(contextBag, typesToRoute).ConfigureAwait(false);
+            var subscribers = await GetSubscribers(contextBag, typesToRoute).ConfigureAwait(false);
 
             var selectedDestinations = SelectDestinationsForEachEndpoint(distributionPolicy, subscribers);
 
-            return selectedDestinations
-                .Distinct() //Make sure we are sending only one to each transport destination. Might happen when there are multiple routing information sources.
-                .Select(destination => new UnicastRoutingStrategy(destination));
+            return selectedDestinations.Select(destination => new UnicastRoutingStrategy(destination));
         }
 
-        IEnumerable<string> SelectDestinationsForEachEndpoint(IDistributionPolicy distributionPolicy, IEnumerable<Subscriber> subscribers)
+        HashSet<string> SelectDestinationsForEachEndpoint(IDistributionPolicy distributionPolicy, IEnumerable<Subscriber> subscribers)
         {
+            //Make sure we are sending only one to each transport destination. Might happen when there are multiple routing information sources.
+            var addresses = new HashSet<string>();
             var destinationsByEndpoint = subscribers
                 .GroupBy(d => d.Endpoint, d => d);
 
@@ -43,25 +43,20 @@ namespace NServiceBus
                     //Send a message to each target as we have no idea which endpoint they represent
                     foreach (var subscriber in group)
                     {
-                        yield return subscriber.TransportAddress;
+                        addresses.Add(subscriber.TransportAddress);
                     }
                 }
                 else
                 {
-                    //TODO use a real distribution strategy
                     var subscriber = distributionPolicy.GetDistributionStrategy(group.First().Endpoint).SelectSubscriber(group.Select(s => s.TransportAddress).ToArray());
-                    yield return subscriber;
-//                    //Use the distribution strategy to select subset of instances of a given endpoint
-//                    var destinationForEndpoint = distributionPolicy.GetDistributionStrategy(@group.Key).SelectDestination(@group.Select(t => t.Instance).ToArray());
-//                    if (destinationForEndpoint != null)
-//                    {
-//                        yield return UnicastRoutingTarget.ToEndpointInstance(destinationForEndpoint);
-//                    }
+                    addresses.Add(subscriber);
                 }
             }
+
+            return addresses;
         }
 
-        async Task<IEnumerable<Subscriber>> GetDestinations(ContextBag contextBag, Type[] typesToRoute)
+        async Task<IEnumerable<Subscriber>> GetSubscribers(ContextBag contextBag, Type[] typesToRoute)
         {
             var messageTypes = typesToRoute.Select(t => new MessageType(t));
             var subscribers = await subscriptionStorage.GetSubscriberAddressesForMessage(messageTypes, contextBag).ConfigureAwait(false);
