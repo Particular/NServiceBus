6701ce07ff2004ac4ecc2c8d678b2ef0bc251d5b
Andreas Öhlund <andreas.ohlund@particular.net>
2016-08-16 11:15:12 +0200

    Disable delayed retries in test to make the count correct (#4059)

--- src/NServiceBus.AcceptanceTests/Recoverability/Retries/When_immediate_retries_with_native_transactions.cs
+++ src/NServiceBus.AcceptanceTests/Recoverability/Retries/When_immediate_retries_with_native_transactions.cs
@@ -6,40 +6,32 @@
     using AcceptanceTesting;
     using EndpointTemplates;
     using NUnit.Framework;
-    using ScenarioDescriptors;
 
     public class When_immediate_retries_with_native_transactions : NServiceBusAcceptanceTest
     {
         [Test]
-        public Task Should_do_the_configured_number_of_retries_with_native_transactions()
+        public async Task Should_do_the_configured_number_of_retries_with_native_transactions()
         {
-            return Scenario.Define<Context>(c => { c.Id = Guid.NewGuid(); })
+            var context = await Scenario.Define<Context>()
                 .WithEndpoint<RetryEndpoint>(b => b
-                    .When((session, context) => session.SendLocal(new MessageToBeRetried
-                    {
-                        Id = context.Id
-                    }))
+                    .When((session, c) => session.SendLocal(new MessageToBeRetried()))
                     .DoNotFailOnErrorMessages())
                 .Done(c => c.ForwardedToErrorQueue)
-                .Repeat(r => r.For(Transports.Default))
-                .Should(c =>
-                {
-                    Assert.AreEqual(5 + 1, c.NumberOfTimesInvoked, "The Immediate Retries retry 5 times");
-                    Assert.AreEqual(5, c.Logs.Count(l => l.Message
-                        .StartsWith($"Immediate Retry is going to retry message '{c.PhysicalMessageId}' because of an exception:")));
-                })
                 .Run();
+
+            Assert.True(context.ForwardedToErrorQueue);
+            Assert.AreEqual(5 + 1, context.NumberOfTimesInvoked, "Message should be retried 5 times immediately");
+            Assert.AreEqual(5, context.Logs.Count(l => l.Message
+                .StartsWith($"Immediate Retry is going to retry message '{context.MessageId}' because of an exception:")));
         }
 
         class Context : ScenarioContext
         {
-            public Guid Id { get; set; }
-
             public int NumberOfTimesInvoked { get; set; }
 
             public bool ForwardedToErrorQueue { get; set; }
 
-            public string PhysicalMessageId { get; set; }
+            public string MessageId { get; set; }
         }
 
         public class RetryEndpoint : EndpointConfigurationBuilder
@@ -53,7 +45,7 @@
 
                     var recoverability = config.Recoverability();
                     recoverability.Immediate(immediate => immediate.NumberOfRetries(5));
-                    recoverability.Delayed(settings => settings.TimeIncrease(TimeSpan.FromMilliseconds(1)).NumberOfRetries(3));
+                    recoverability.Delayed(delayed => delayed.NumberOfRetries(0)); //disable the delayed retries
 
                     config.UseTransport(context.GetTransportType())
                         .Transactions(TransportTransactionMode.ReceiveOnly);
@@ -66,12 +58,7 @@
 
                 public Task Handle(MessageToBeRetried message, IMessageHandlerContext context)
                 {
-                    if (message.Id != TestContext.Id)
-                    {
-                        return Task.FromResult(0); // messages from previous test runs must be ignored
-                    }
-
-                    TestContext.PhysicalMessageId = context.MessageId;
+                    TestContext.MessageId = context.MessageId;
                     TestContext.NumberOfTimesInvoked++;
 
                     throw new SimulatedException();
