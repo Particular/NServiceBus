ebde7201da3d3e949a2cbf3bc7566f4bf2118420
Brandon Ording <bording@gmail.com>
2016-08-17 19:01:09 -0400

    Improve ToUtcDateTime performance

--- src/NServiceBus.Core/DateTimeExtensions.cs
+++ src/NServiceBus.Core/DateTimeExtensions.cs
@@ -13,7 +13,7 @@ namespace NServiceBus
         /// </summary>
         public static string ToWireFormattedString(DateTime dateTime)
         {
-            return dateTime.ToUniversalTime().ToString(Format, CultureInfo.InvariantCulture);
+            return dateTime.ToUniversalTime().ToString(format, CultureInfo.InvariantCulture);
         }
 
         /// <summary>
@@ -23,9 +23,54 @@ namespace NServiceBus
         public static DateTime ToUtcDateTime(string wireFormattedString)
         {
             Guard.AgainstNullAndEmpty(nameof(wireFormattedString), wireFormattedString);
-            return DateTime.ParseExact(wireFormattedString, Format, CultureInfo.InvariantCulture).ToUniversalTime();
+
+            var year = 0;
+            var month = 0;
+            var day = 0;
+            var hour = 0;
+            var minute = 0;
+            var second = 0;
+            var microSecond = 0;
+
+            for (var i = 0; i < format.Length; i++)
+            {
+                var digit = wireFormattedString[i];
+
+                switch (format[i])
+                {
+                    case 'y':
+                        year = year * 10 + (digit - '0');
+                        break;
+
+                    case 'M':
+                        month = month * 10 + (digit - '0');
+                        break;
+
+                    case 'd':
+                        day = day * 10 + (digit - '0');
+                        break;
+
+                    case 'H':
+                        hour = hour * 10 + (digit - '0');
+                        break;
+
+                    case 'm':
+                        minute = minute * 10 + (digit - '0');
+                        break;
+
+                    case 's':
+                        second = second * 10 + (digit - '0');
+                        break;
+
+                    case 'f':
+                        microSecond = microSecond * 10 + (digit - '0');
+                        break;
+                }
+            }
+
+            return new DateTime(year, month, day, hour, minute, second, microSecond / 1000, DateTimeKind.Utc);
         }
 
-        const string Format = "yyyy-MM-dd HH:mm:ss:ffffff Z";
+        const string format = "yyyy-MM-dd HH:mm:ss:ffffff Z";
     }
 }
