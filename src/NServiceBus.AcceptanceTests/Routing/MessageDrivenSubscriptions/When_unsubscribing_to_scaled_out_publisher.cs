f0a7f88d2f3968a165274e95ad2ab5acc4cc22c6
Tim Bussmann <tim.bussmann@particular.net>
2016-09-19 09:14:29 +0200

    fix namings

--- src/NServiceBus.AcceptanceTests/Routing/MessageDrivenSubscriptions/When_unsubscribing_to_scaled_out_publisher.cs
+++ src/NServiceBus.AcceptanceTests/Routing/MessageDrivenSubscriptions/When_unsubscribing_to_scaled_out_publisher.cs
@@ -13,38 +13,38 @@
     public class When_unsubscribing_to_scaled_out_publisher : NServiceBusAcceptanceTest
     {
         [Test]
-        public Task Should_send_unsubscription_message_to_each_instance()
+        public Task Should_send_unsubscribe_message_to_each_instance()
         {
             return Scenario.Define<Context>()
                 .WithEndpoint<ScaledOutPublisher>(b => b.CustomConfig(c => c.MakeInstanceUniquelyAddressable("1")))
                 .WithEndpoint<ScaledOutPublisher>(b => b.CustomConfig(c => c.MakeInstanceUniquelyAddressable("2")))
                 .WithEndpoint<Unsubscriber>(b => b.When(s => s.Unsubscribe<MyEvent>()))
-                .Done(c => c.PublisherReceivedUnsubscription.Count >= 2)
+                .Done(c => c.PublisherReceivedUnsubscribeMessage.Count >= 2)
                 .Repeat(r => r.For<AllTransportsWithMessageDrivenPubSub>())
                 .Should(c =>
                 {
-                    // each instance should receive an unsubscription message
-                    Assert.That(c.PublisherReceivedUnsubscription, Does.Contain("1"));
-                    Assert.That(c.PublisherReceivedUnsubscription, Does.Contain("2"));
-                    Assert.That(c.PublisherReceivedUnsubscription.Count, Is.EqualTo(2));
+                    // each instance should receive an unsubscribe message
+                    Assert.That(c.PublisherReceivedUnsubscribeMessage, Does.Contain("1"));
+                    Assert.That(c.PublisherReceivedUnsubscribeMessage, Does.Contain("2"));
+                    Assert.That(c.PublisherReceivedUnsubscribeMessage.Count, Is.EqualTo(2));
                 })
                 .Run();
         }
 
         class Context : ScenarioContext
         {
-            public List<string> PublisherReceivedUnsubscription { get; } = new List<string>();
+            public List<string> PublisherReceivedUnsubscribeMessage { get; } = new List<string>();
         }
 
         class ScaledOutPublisher : EndpointConfigurationBuilder
         {
             public ScaledOutPublisher()
             {
-                // store the instance discriminator of each instance receiving a unsubscription message:
+                // store the instance discriminator of each instance receiving a unsubscribe message:
                 EndpointSetup<DefaultServer>(c =>
                 {
                     c.OnEndpointUnsubscribed<Context>((subscription, context) =>
-                            context.PublisherReceivedUnsubscription.Add(c.GetSettings().Get<string>("EndpointInstanceDiscriminator")));
+                            context.PublisherReceivedUnsubscribeMessage.Add(c.GetSettings().Get<string>("EndpointInstanceDiscriminator")));
                 });
             }
         }
