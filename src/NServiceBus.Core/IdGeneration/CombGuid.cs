d60677f871c3e2ab6d6cc0a303c77c54fcaa1924
Ramon Smits <ramon.smits@gmail.com>
2016-10-06 19:52:09 +0200

    CTRL+E,C => Particular profile

--- src/NServiceBus.Core/IdGeneration/CombGuid.cs
+++ src/NServiceBus.Core/IdGeneration/CombGuid.cs
@@ -4,7 +4,8 @@ namespace NServiceBus
 
     /// <summary>
     /// Generates a Guid using http://www.informit.com/articles/article.asp?p=25862
-    /// The Comb algorithm is designed to make the use of Guids as Primary Keys, Foreign Keys, and Indexes nearly as efficient as ints.
+    /// The Comb algorithm is designed to make the use of Guids as Primary Keys, Foreign Keys, and Indexes nearly as efficient
+    /// as ints.
     /// </summary>
     /// <remarks>Source: https://github.com/nhibernate/nhibernate-core/blob/4.0.4.GA/src/NHibernate/Id/GuidCombGenerator.cs</remarks>
     static class CombGuid
