aeb22023272d4ce5c050adeeb26547dfbd06a02d
Brandon Ording <bording@gmail.com>
2016-10-31 18:45:30 -0400

    Fix inspections

--- src/NServiceBus.Core.Tests/AssemblyScanner/AssemblyScannerTests.cs
+++ src/NServiceBus.Core.Tests/AssemblyScanner/AssemblyScannerTests.cs
@@ -4,7 +4,6 @@
     using System.CodeDom.Compiler;
     using System.Diagnostics;
     using System.IO;
-    using System.Linq;
     using System.Reflection;
     using System.Text;
     using System.Threading;
