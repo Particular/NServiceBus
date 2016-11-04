b1f4e41bdda3efb76a1150b08faeb1021b7a5cd7
SzymonPobiega <szymon.pobiega@gmail.com>
2016-11-04 12:37:09 +0100

    Changes allowing wiring dispatcher directly to pump
     * CiriticalError does not retquire IEndpointInstance
     * TransportConnectionString is public to allow reading connection string

--- src/NServiceBus.Core/Unicast/Transport/TransportConnectionString.cs
+++ src/NServiceBus.Core/Unicast/Transport/TransportConnectionString.cs
@@ -2,35 +2,40 @@
 {
     using System;
     using System.Configuration;
-    using Transport;
 
-    sealed class TransportConnectionString
+    /// <summary>
+    /// Allows to get the configured transport connection string.
+    /// </summary>
+    public sealed class TransportConnectionString
     {
         TransportConnectionString()
         {
         }
 
+        /// <summary>
+        /// Creates new connection string configuration that returns value provided by a callback.
+        /// </summary>
+        /// <param name="func">Callback providing connection string value.</param>
         public TransportConnectionString(Func<string> func)
         {
             GetValue = func;
         }
 
-
-        public TransportConnectionString(string name)
+        internal TransportConnectionString(string name)
         {
             GetValue = () => ReadConnectionString(name);
         }
 
-        public static TransportConnectionString Default => new TransportConnectionString();
+        internal static TransportConnectionString Default => new TransportConnectionString();
 
-        public string GetConnectionStringOrRaiseError(TransportDefinition transportDefinition)
+        /// <summary>
+        /// Returns the configured transport connection string.
+        /// </summary>
+        /// <returns></returns>
+        public bool TryGetValue(out string connectionString)
         {
-            var connectionString = GetValue();
-            if (connectionString == null && transportDefinition.RequiresConnectionString)
-            {
-                throw new InvalidOperationException(string.Format(Message, transportDefinition.GetType().Name, transportDefinition.ExampleConnectionStringForErrorMessage));
-            }
-            return connectionString;
+            connectionString = GetValue();
+            return connectionString != null;
         }
 
         static string ReadConnectionString(string connectionStringName)
@@ -42,20 +47,7 @@
 
         Func<string> GetValue = () => ReadConnectionString(DefaultConnectionStringName);
 
-        const string Message =
-            @"Transport connection string has not been explicitly configured via ConnectionString method and no default connection has been was found in the app.config or web.config file for the {0} Transport.
-
-To run NServiceBus with {0} Transport you need to specify the database connectionstring.
-Here are examples of what is required:
-  
-  <connectionStrings>
-    <add name=""NServiceBus/Transport"" connectionString=""{1}"" />
-  </connectionStrings>
-
-or
-
-  busConfig.UseTransport<{0}>().ConnectionString(""{1}"");
-";
+        
 
         const string DefaultConnectionStringName = "NServiceBus/Transport";
     }
