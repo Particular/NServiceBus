bdbb16056983d4a4f082bfaa80f257e1aa2d7b7d
indualagarsamy <indu.alagarsamy@particular.net>
2016-11-07 16:37:53 -0800

    Relates to https://github.com/Particular/OptimizeTrialExtensions/issues/114

--- packaging/nuget/tools/init.ps1
+++ packaging/nuget/tools/init.ps1
@@ -12,6 +12,8 @@ if($package){
 }
 
 #Figure out if this is a first time user
+
+#Figure out if this is a first time user
 try {
 
 	#Check for existing NServiceBus installations
@@ -33,14 +35,17 @@ try {
 
 	Set-ItemProperty -Path $platformKeyPath -Name "NuGetUser" -Value "true" | Out-Null
 
-
-    $url = "http://particular.net/download-the-particular-service-platform?version=$packageVersion" 
-    $url = $url.ToLowerInvariant(); 
-
-    if($dte){
-	    $dte.ExecuteCommand("View.URL", $url)
-    }
+	Write-Host `Reporting first time install and version information to www.particular.net. This call does not collect any personal information. For more details, see the License Agreement and the Privacy Policy available here: http://particular.net/licenseagreement. Subsequent NuGet installs or updates will not invoke this call.`
+	$url = 'https://particular.net/api/ReportFirstTimeUsage'
+	$postData  = New-Object System.Collections.Specialized.NameValueCollection
+	$postData.Add("version", $packageversion)
+	$wc = New-Object System.Net.WebClient
+	$wc.UseDefaultCredentials = $true
+	$wc.UploadValuesAsync($url,"post", $postdata)
 } 
 Catch [Exception] { 
 	Write-Warning $error[0]
+}
+finally {
+	$wc.Dispose()
 }
