750905fff2c1ae63c46cb19c07d1c7bd66cd4ed4
Greg Bielleman <greg.bielleman@particular.net>
2016-11-17 14:18:11 +1100

    Rework to show text only once

--- packaging/nuget/tools/init.ps1
+++ packaging/nuget/tools/init.ps1
@@ -1,31 +1,35 @@
 param($installPath, $toolsPath, $package, $project)
 
 $packageVersion = 'Unknown'
+$noticeEvent = 'noticeEvent'
+$jobName = 'analytics'
 
-$notice = @"
-For first time users this script collects install and version information. 
-    
-This call does not collect any personal information. For more details, see the 
-License Agreement and the Privacy Policy available here: http://particular.net/licenseagreement.
-"@
-
+# cleanup previous runs
+Get-Job | ? { $_.Name  -eq $jobName } | Remove-Job -Force -ErrorAction SilentlyContinue    
+ 
 if ($package) {
     $packageVersion = $package.Version
 }
 
 # Define script to run in background job
 $jobScriptBlock = { 
-    param($packageVersion)
+    param($packageVersion, $noticeEvent)
 
-    # Set Tracing on with the Job
+    # Set Tracing on within the Job
     Set-PSDebug -Trace 2
 
+    # Setup event forwarding to foreground
+    Register-EngineEvent -SourceIdentifier $noticeEvent -Forward 
+    
     $nserviceBusKeyPath = 'HKCU:SOFTWARE\NServiceBus' 
     $platformKeyPath = 'HKCU:SOFTWARE\ParticularSoftware'
     
-    # Test for Existing reg keys and skip if either are found
-    if (-not ((Test-Path $nserviceBusKeyPath) -or (Test-Path $platformKeyPath))) {
-    
+    if ((Test-Path $nserviceBusKeyPath) -or (Test-Path $platformKeyPath)) {
+        New-Event -SourceIdentifier $noticeEvent -Sender "analytics" -MessageData "existing"
+    }
+    else {
+        New-Event -SourceIdentifier $noticeEvent -Sender "analytics" -MessageData "newuser"
+
         # Set Flag to bypass first time user feedback in Platform Installer
         New-Item -Path $platformKeyPath -Force | Out-Null
         Set-ItemProperty -Path $platformKeyPath -Name 'NuGetUser' -Value 'true' -Force
@@ -39,17 +43,25 @@ $jobScriptBlock = {
             $wc.UseDefaultCredentials = $true
             $wc.UploadValues($url, "post", $postdata)
         } 
-        finally {
+        finally {$noticeEvent
             # Dispose
             Remove-Variable -Name wc 
-        }
+        } 
     }
 }
 
-# If an existing instance of this job exists then we can skip running it, otherwise create and run 
-$jobName = 'particular.analytics'
-$job = Get-Job -Name  $jobName -ErrorAction SilentlyContinue
-if (-not $job) {
+$notice = @" 
+"@
+
+# Run JobScript
+$job = Start-Job -ScriptBlock $jobScriptBlock -Name $jobName -ArgumentList $packageVersion, $noticeEvent 
+
+# Wait for show notice event
+$event = Wait-Event -SourceIdentifier $noticeEvent -Timeout 5 
+Remove-Event -SourceIdentifier $noticeEvent -ErrorAction SilentlyContinue  
+if ($event.MessageData -eq "newuser") {
     Write-Host $notice
-    $job = Start-Job -ScriptBlock $jobScriptBlock -Name $jobName -ArgumentList $packageVersion 
 }
+
+
+
