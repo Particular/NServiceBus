35e445a9c8a984a9dd3bab5369c5a85913e514ba
Szymon Kulec <scooletz@gmail.com>
2016-11-25 12:00:11 +0100

    The recalculating algorithm made simpler, possibly running the same computations a few times. Having a multiple writers and handling them would be easier with a custom data structure.

--- src/NServiceBus.Core/Routing/UnicastSubscriberTable.cs
+++ src/NServiceBus.Core/Routing/UnicastSubscriberTable.cs
@@ -1,7 +1,6 @@
 namespace NServiceBus.Routing
 {
     using System;
-    using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading;
@@ -11,7 +10,9 @@ namespace NServiceBus.Routing
     /// </summary>
     public class UnicastSubscriberTable
     {
-        static readonly UnicastRoute[] emptyResult = { };
+        static readonly UnicastRoute[] emptyResult =
+        {
+        };
 
         internal UnicastRoute[] GetRoutesFor(Type messageType)
         {
@@ -30,42 +31,39 @@ namespace NServiceBus.Routing
         /// <param name="entries">Group entries.</param>
         public void AddOrReplaceRoutes(string sourceKey, IList<RouteTableEntry> entries)
         {
-            /*
-            * The algorithm ensures that the last thread that updates (with side effects) the routeGroups value also updates the routeTable.
-            * It does so by claiming a token when entering the code block. Only the thread with a current token updates routeTable. If the token is not valid any more
-            * it means that there is another thread inside the method which came after it. The thread also spin-waits until all the threads that entered the method before it
-            * exit to ensure that they don't overwrite the value it is going to put into routeTable
-            *
-            */
-            IList<RouteTableEntry> existing = null;
-            var newEntries = routeGroups.AddOrUpdate(sourceKey, entries, (key, e) =>
-            {
-                existing = e;
-                return entries;
-            });
-            if (existing != null && existing.SequenceEqual(newEntries)) //No change for that key. We save some allocations.
+            // The algorithm uses ReaderWriterLockSlim. First entries are read. If then exists they are compared with passed entries and skipped if equal.
+            // Otherwise, the write path is used. It's possible than one thread will execute all the work
+            var existing = GetExistingRoutes(sourceKey);
+            if (existing != null && existing.SequenceEqual(entries))
             {
                 return;
             }
-            var enterToken = Interlocked.Increment(ref enterCounter);
+
+            readerWriterLock.EnterWriteLock();
             try
             {
-                var newRouteTable = CalculateNewRouteTable();
-                var current = Interlocked.CompareExchange(ref enterCounter, enterToken, enterToken);
-                if (current == enterToken) //We still have valid token -- nobody else entered the method after us.
-                {
-                    //We need to wait until the previous thread exits
-                    while (exitCounter != current)
-                    {
-                        //spin wait
-                    }
-                    routeTable = newRouteTable;
-                }
+                routeGroups[sourceKey] = entries;
+                routeTable = CalculateNewRouteTable();
             }
             finally
             {
-                exitCounter = enterToken + 1; //Allow the next one to successfully write value
+                readerWriterLock.ExitWriteLock();
             }
+        }
+
+        IList<RouteTableEntry> GetExistingRoutes(string sourceKey)
+        {
+            IList<RouteTableEntry> existing;
+            readerWriterLock.EnterReadLock();
+            try
+            {
+                routeGroups.TryGetValue(sourceKey, out existing);
+            }
+            finally
+            {
+                readerWriterLock.ExitReadLock();
+            }
+            return existing;
         }
 
         Dictionary<Type, UnicastRoute[]> CalculateNewRouteTable()
@@ -84,9 +82,8 @@ namespace NServiceBus.Routing
             return newRouteTable.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToArray());
         }
 
-        Dictionary<Type, UnicastRoute[]> routeTable = new Dictionary<Type, UnicastRoute[]>();
-        ConcurrentDictionary<object, IList<RouteTableEntry>> routeGroups = new ConcurrentDictionary<object, IList<RouteTableEntry>>();
-        long enterCounter;
-        long exitCounter = 1;
+        volatile Dictionary<Type, UnicastRoute[]> routeTable = new Dictionary<Type, UnicastRoute[]>();
+        Dictionary<string, IList<RouteTableEntry>> routeGroups = new Dictionary<string, IList<RouteTableEntry>>();
+        ReaderWriterLockSlim readerWriterLock = new ReaderWriterLockSlim();
     }
 }
