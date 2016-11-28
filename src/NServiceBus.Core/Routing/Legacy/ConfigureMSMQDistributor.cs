91088168a62391a339da1fbe10e5ff7c32e311fc
Tim Bussmann <tim.bussmann@particular.net>
2016-11-28 13:52:11 +0100

    add comment regarding disabling the TM

--- src/NServiceBus.Core/Routing/Legacy/ConfigureMSMQDistributor.cs
+++ src/NServiceBus.Core/Routing/Legacy/ConfigureMSMQDistributor.cs
@@ -21,6 +21,9 @@
             config.GetSettings().Set("LegacyDistributor.Address", masterNodeAddress);
             config.GetSettings().Set("LegacyDistributor.ControlAddress", masterNodeControlAddress);
             config.GetSettings().Set("LegacyDistributor.Capacity", capacity);
+
+            // note that the TimeoutManager will be disabled by it's prerequisite check anyway as
+            // WorkerFeature sets up an external timeout manager. This line is kept for readability.
             config.DisableFeature<TimeoutManager>();
             config.EnableFeature<WorkerFeature>();
         }
