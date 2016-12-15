96d6aeb1cd1d50696d8c8b1885dca3c2473414a6
Donald Belcham <donald.belcham@particular.net>
2016-12-15 11:59:01 -0400

    Add test that ensures messages are emitted on failure when isolated dispatch is used

---
+++ src/NServiceBus.TransportTests/When_failure_happens_after_isolated_send.cs
@@ -0,0 +1,60 @@
+﻿namespace NServiceBus.TransportTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Transport;
+
+    public class When_failure_happens_after_isolated_send : NServiceBusTransportTest
+    {
+        //[TestCase(TransportTransactionMode.None)] - not relevant
+        //[TestCase(TransportTransactionMode.ReceiveOnly)] - not relevant
+        [TestCase(TransportTransactionMode.SendsAtomicWithReceive)]
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_emit_messages(TransportTransactionMode transactionMode)
+        {
+            var onMessageCalled = new TaskCompletionSource<bool>();
+
+            OnTestTimeout(() => onMessageCalled.SetCanceled());
+
+            await StartPump(async context =>
+                {
+                    if (context.Headers.ContainsKey("CompleteTest"))
+                    {
+                        onMessageCalled.SetResult(true);
+                        return;
+                    }
+
+                    if (context.Headers.ContainsKey("EnlistedSend"))
+                    {
+                        onMessageCalled.SetResult(false);
+                        return;
+                    }
+
+                    await SendMessage(InputQueueName, new Dictionary<string, string>
+                    {
+                        {"EnlistedSend", "true"}
+                    }, context.TransportTransaction, null, DispatchConsistency.Isolated);
+
+                    throw new Exception("Simulated exception");
+                },
+                async context =>
+                {
+                    await SendMessage(InputQueueName, new Dictionary<string, string>
+                    {
+                        {"CompleteTest", "true"}
+                    }, context.TransportTransaction, null, DispatchConsistency.Isolated);
+
+                    return ErrorHandleResult.Handled;
+                }, transactionMode);
+
+            await SendMessage(InputQueueName, new Dictionary<string, string>
+            {
+                {"MyHeader", "MyValue"}
+            });
+
+            Assert.False(await onMessageCalled.Task, "Should emit enlisted sends");
+        }
+    }
+}
