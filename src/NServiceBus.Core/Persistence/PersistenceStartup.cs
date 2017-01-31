f60bfc92b5d6956abb6263cce85fb820b117511f
Andreas Öhlund <andreas.ohlund@particular.net>
2017-01-31 13:01:14 +0100

    No longer require persistence to be configured (#4449)
    * No longer require persistence to be configured
    
    * Remove message consRemove message const

--- src/NServiceBus.Core/Persistence/PersistenceStartup.cs
+++ src/NServiceBus.Core/Persistence/PersistenceStartup.cs
@@ -14,12 +14,7 @@
 
             if (!settings.TryGet("PersistenceDefinitions", out definitions))
             {
-                if (settings.Get<bool>("Endpoint.SendOnly"))
-                {
-                    return;
-                }
-
-                throw new Exception(errorMessage);
+                return;
             }
 
             var enabledPersistences = PersistenceStorageMerger.Merge(definitions, settings);
@@ -50,8 +45,6 @@
 
             return supportedStorages?.Contains(typeof(T)) ?? false;
         }
-
-        const string errorMessage = "No persistence has been selected, select a persistence by calling endpointConfiguration.UsePersistence<T>() in the class that implements either IConfigureThisEndpoint or INeedInitialization, where T can be any of the supported persistence option. If previously using RavenDB, note that it has been moved to its own stand alone nuget 'NServiceBus.RavenDB'. This package will need to be installed and then enabled by calling endpointConfiguration.UsePersistence<RavenDBPersistence>().";
 
         static ILog Logger = LogManager.GetLogger(typeof(PersistenceStartup));
     }
