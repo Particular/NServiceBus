e55deb7059a39cf6dbfd094adcd4bc9261205cb6
andreasohlund <andreas.ohlund@particular.net>
2017-02-07 10:25:53 +0100

    Default DLQ to off when TTBR is set

--- src/NServiceBus.Core.Tests/Msmq/MsmqMessageDispatcherTests.cs
+++ src/NServiceBus.Core.Tests/Msmq/MsmqMessageDispatcherTests.cs
@@ -3,7 +3,9 @@
     using System;
     using System.Collections.Generic;
     using System.Messaging;
+    using DeliveryConstraints;
     using Extensibility;
+    using NServiceBus.Performance.TimeToBeReceived;
     using NServiceBus.Routing;
     using Transport;
     using NUnit.Framework;
@@ -40,6 +42,7 @@
                 MsmqHelpers.DeleteQueue(path);
             }
         }
+
         [Test]
         public void Should_use_string_empty_label_when_no_convention_configured()
         {
@@ -69,12 +72,86 @@
             }
         }
 
+        [Test]
+        public void Should_default_dlq_to_off_for_messages_with_ttbr()
+        {
+            var queueName = "dlqWithTTBRDefaultTest";
+            var path = $@".\private$\{queueName}";
+            try
+            {
+                MsmqHelpers.DeleteQueue(path);
+                MsmqHelpers.CreateQueue(path);
+                var messageSender = new MsmqMessageDispatcher(new MsmqSettings(), pairs => string.Empty);
+
+                var bytes = new byte[]
+                {
+                    1
+                };
+                var headers = new Dictionary<string, string>();
+                var outgoingMessage = new OutgoingMessage("1", headers, bytes);
+                var transportOperation = new TransportOperation(outgoingMessage, new UnicastAddressTag(queueName), DispatchConsistency.Default,new List<DeliveryConstraint>
+                {
+                    new DiscardIfNotReceivedBefore(TimeSpan.FromMinutes(10))
+                });
+
+                messageSender.Dispatch(new TransportOperations(transportOperation), new TransportTransaction(), new ContextBag());
+                var message = ReadMessage(path);
+
+                Assert.False(message.UseDeadLetterQueue);
+
+            }
+            finally
+            {
+                MsmqHelpers.DeleteQueue(path);
+            }
+        }
+
+        [Test]
+        public void Should_default_honor_dlq_setting()
+        {
+            var queueName = "dlqDefaultTest";
+            var path = $@".\private$\{queueName}";
+            try
+            {
+                MsmqHelpers.DeleteQueue(path);
+                MsmqHelpers.CreateQueue(path);
+                var messageSender = new MsmqMessageDispatcher(new MsmqSettings
+                {
+                    UseDeadLetterQueue = true
+                }, pairs => string.Empty);
+
+                var bytes = new byte[]
+                {
+                    1
+                };
+                var headers = new Dictionary<string, string>();
+                var outgoingMessage = new OutgoingMessage("1", headers, bytes);
+                var transportOperation = new TransportOperation(outgoingMessage, new UnicastAddressTag(queueName), DispatchConsistency.Default);
+
+                messageSender.Dispatch(new TransportOperations(transportOperation), new TransportTransaction(), new ContextBag());
+                var message = ReadMessage(path);
+
+                Assert.True(message.UseDeadLetterQueue);
+
+            }
+            finally
+            {
+                MsmqHelpers.DeleteQueue(path);
+            }
+        }
+
+
         static string ReadMessageLabel(string path)
+        {
+            return ReadMessage(path)?.Label;
+        }
+
+        static Message ReadMessage(string path)
         {
             using (var queue = new MessageQueue(path))
             using (var message = queue.Receive(TimeSpan.FromSeconds(5)))
             {
-                return message?.Label;
+                return message;
             }
         }
     }
