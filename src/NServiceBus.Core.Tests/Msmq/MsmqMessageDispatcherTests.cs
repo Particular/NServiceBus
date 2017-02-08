cfc51194c245575aabfa3852a20674350a06b1dc
andreasohlund <andreas.ohlund@particular.net>
2017-02-08 09:48:25 +0100

    Cleanup msmq dispatcher tests

--- src/NServiceBus.Core.Tests/Msmq/MsmqMessageDispatcherTests.cs
+++ src/NServiceBus.Core.Tests/Msmq/MsmqMessageDispatcherTests.cs
@@ -9,7 +9,6 @@
     using NServiceBus.Routing;
     using Transport;
     using NUnit.Framework;
-    using Support;
 
     [TestFixture]
     public class MsmqMessageDispatcherTests
@@ -17,139 +16,56 @@
         [Test]
         public void Should_set_label_when_convention_configured()
         {
-            var queueName = "labelTest";
-            var path = $@"{RuntimeEnvironment.MachineName}\private$\{queueName}";
-            try
-            {
-                MsmqHelpers.DeleteQueue(path);
-                MsmqHelpers.CreateQueue(path);
-                var messageSender = new MsmqMessageDispatcher(new MsmqSettings(), _ => "mylabel");
+            var dispatchedMessage = DispatchMessage(new MsmqSettings(), messageLabelGenerator: _ => "mylabel");
 
-                var bytes = new byte[]
-                {
-                    1
-                };
-                var headers = new Dictionary<string, string>();
-                var outgoingMessage = new OutgoingMessage("1", headers, bytes);
-                var transportOperation = new TransportOperation(outgoingMessage, new UnicastAddressTag(queueName), DispatchConsistency.Default);
-                messageSender.Dispatch(new TransportOperations(transportOperation), new TransportTransaction(), new ContextBag());
-                var messageLabel = ReadMessageLabel(path);
-                Assert.AreEqual("mylabel", messageLabel);
-
-            }
-            finally
-            {
-                MsmqHelpers.DeleteQueue(path);
-            }
-        }
-
-        [Test]
-        public void Should_use_string_empty_label_when_no_convention_configured()
-        {
-            var queueName = "emptyLabelTest";
-            var path = $@".\private$\{queueName}";
-            try
-            {
-                MsmqHelpers.DeleteQueue(path);
-                MsmqHelpers.CreateQueue(path);
-                var messageSender = new MsmqMessageDispatcher(new MsmqSettings(), pairs => string.Empty);
-
-                var bytes = new byte[]
-                {
-                    1
-                };
-                var headers = new Dictionary<string, string>();
-                var outgoingMessage = new OutgoingMessage("1", headers, bytes);
-                var transportOperation = new TransportOperation(outgoingMessage, new UnicastAddressTag(queueName), DispatchConsistency.Default);
-                messageSender.Dispatch(new TransportOperations(transportOperation), new TransportTransaction(), new ContextBag());
-                var messageLabel = ReadMessageLabel(path);
-                Assert.IsEmpty(messageLabel);
-
-            }
-            finally
-            {
-                MsmqHelpers.DeleteQueue(path);
-            }
+            Assert.AreEqual("mylabel", dispatchedMessage.Label);
         }
 
         [Test]
         public void Should_default_dlq_to_off_for_messages_with_ttbr()
         {
-            var queueName = "dlqWithTTBRDefaultTest";
-            var path = $@".\private$\{queueName}";
-            try
-            {
-                MsmqHelpers.DeleteQueue(path);
-                MsmqHelpers.CreateQueue(path);
-                var messageSender = new MsmqMessageDispatcher(new MsmqSettings(), pairs => string.Empty);
+            var dispatchedMessage = DispatchMessage(new MsmqSettings(), new DiscardIfNotReceivedBefore(TimeSpan.FromMinutes(10)));
 
-                var bytes = new byte[] { 1 };
-                var headers = new Dictionary<string, string>();
-                var outgoingMessage = new OutgoingMessage("1", headers, bytes);
-                var transportOperation = new TransportOperation(outgoingMessage, new UnicastAddressTag(queueName), DispatchConsistency.Default, new List<DeliveryConstraint>
-                {
-                    new DiscardIfNotReceivedBefore(TimeSpan.FromMinutes(10))
-                });
-
-                messageSender.Dispatch(new TransportOperations(transportOperation), new TransportTransaction(), new ContextBag());
-                var message = ReadMessage(path);
-
-                Assert.False(message.UseDeadLetterQueue);
-
-            }
-            finally
-            {
-                MsmqHelpers.DeleteQueue(path);
-            }
+            Assert.False(dispatchedMessage.UseDeadLetterQueue);
         }
 
         [Test]
         public void Should_allow_optin_for_dlq_on_ttbr_messages()
         {
-            var queueName = "dlqOptInForTTBRTest";
-            var path = $@".\private$\{queueName}";
-            try
+            var settings = new MsmqSettings
             {
-                MsmqHelpers.DeleteQueue(path);
-                MsmqHelpers.CreateQueue(path);
-                var messageSender = new MsmqMessageDispatcher(new MsmqSettings
-                {
-                    UseDeadLetterQueueForMessagesWithTimeToReachQueue = true
-                }, pairs => string.Empty);
+                UseDeadLetterQueueForMessagesWithTimeToReachQueue = true
+            };
 
-                var bytes = new byte[] { 1 };
-                var headers = new Dictionary<string, string>();
-                var outgoingMessage = new OutgoingMessage("1", headers, bytes);
-                var transportOperation = new TransportOperation(outgoingMessage, new UnicastAddressTag(queueName), DispatchConsistency.Default, new List<DeliveryConstraint>
-                {
-                    new DiscardIfNotReceivedBefore(TimeSpan.FromMinutes(10))
-                });
+            var dispatchedMessage = DispatchMessage(settings, new DiscardIfNotReceivedBefore(TimeSpan.FromMinutes(10)));
 
-                messageSender.Dispatch(new TransportOperations(transportOperation), new TransportTransaction(), new ContextBag());
-                var message = ReadMessage(path);
-
-                Assert.True(message.UseDeadLetterQueue);
-
-            }
-            finally
-            {
-                MsmqHelpers.DeleteQueue(path);
-            }
+            Assert.True(dispatchedMessage.UseDeadLetterQueue);
         }
 
         [Test]
-        public void Should_default_honor_dlq_setting()
+        public void Should_set_dlq_by_default_for_non_ttbr_messages()
         {
-            var queueName = "dlqDefaultTest";
+            var dispatchedMessage = DispatchMessage(new MsmqSettings());
+
+            Assert.True(dispatchedMessage.UseDeadLetterQueue);
+        }
+
+        static Message DispatchMessage(MsmqSettings settings, DeliveryConstraint deliveryConstraint = null, Func<IReadOnlyDictionary<string, string>, string> messageLabelGenerator = null)
+        {
+            var queueName = "messageDispatcherTests";
             var path = $@".\private$\{queueName}";
+
             try
             {
                 MsmqHelpers.DeleteQueue(path);
                 MsmqHelpers.CreateQueue(path);
-                var messageSender = new MsmqMessageDispatcher(new MsmqSettings
+
+                if (messageLabelGenerator == null)
                 {
-                    UseDeadLetterQueue = true
-                }, pairs => string.Empty);
+                    messageLabelGenerator = _ => string.Empty;
+                }
+
+                var messageSender = new MsmqMessageDispatcher(settings, messageLabelGenerator);
 
                 var bytes = new byte[]
                 {
@@ -157,32 +73,26 @@
                 };
                 var headers = new Dictionary<string, string>();
                 var outgoingMessage = new OutgoingMessage("1", headers, bytes);
-                var transportOperation = new TransportOperation(outgoingMessage, new UnicastAddressTag(queueName), DispatchConsistency.Default);
+                var deliveryConstraints = new List<DeliveryConstraint>();
+
+                if (deliveryConstraint != null)
+                {
+                    deliveryConstraints.Add(deliveryConstraint);
+                }
+
+                var transportOperation = new TransportOperation(outgoingMessage, new UnicastAddressTag(queueName), DispatchConsistency.Default, deliveryConstraints);
 
                 messageSender.Dispatch(new TransportOperations(transportOperation), new TransportTransaction(), new ContextBag());
-                var message = ReadMessage(path);
 
-                Assert.True(message.UseDeadLetterQueue);
-
+                using (var queue = new MessageQueue(path))
+                using (var message = queue.Receive(TimeSpan.FromSeconds(5)))
+                {
+                    return message;
+                }
             }
             finally
             {
                 MsmqHelpers.DeleteQueue(path);
-            }
-        }
-
-
-        static string ReadMessageLabel(string path)
-        {
-            return ReadMessage(path)?.Label;
-        }
-
-        static Message ReadMessage(string path)
-        {
-            using (var queue = new MessageQueue(path))
-            using (var message = queue.Receive(TimeSpan.FromSeconds(5)))
-            {
-                return message;
             }
         }
     }
