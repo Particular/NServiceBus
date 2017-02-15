e60ea1ae81e561d54757e7f05a7248b7a26421e9
danielmarbach <daniel.marbach@openplace.net>
2017-02-15 09:54:09 +0100

    Added unit test for recoverability extensions, moved unrecoverable exception test to core subfolder, switched to ErrorSpy free concept

---
+++ src/NServiceBus.AcceptanceTests/Core/Recoverability/When_configuring_unrecoverable_exception.cs
@@ -0,0 +1,73 @@
+namespace NServiceBus.AcceptanceTests.Core.Recoverability
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using AcceptanceTesting.Support;
+    using EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_configuring_unrecoverable_exception : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_move_to_error_queue_without_retries()
+        {
+            Context context = null;
+
+            var exception = Assert.ThrowsAsync<MessagesFailedException>(async () =>
+            {
+                await Scenario.Define<Context>(ctx => context = ctx)
+                    .WithEndpoint<EndpointWithFailingHandler>(b => b
+                        .When((session, ctx) => session.SendLocal(new InitiatingMessage
+                        {
+                            Id = ctx.TestRunId
+                        }))
+                    )
+                    .Done(c => c.FailedMessages.Any())
+                    .Run();
+            });
+
+            var failedMessage = exception.FailedMessages.Single();
+            Assert.That(failedMessage.Exception, Is.TypeOf<CustomException>());
+            Assert.AreEqual(1, context.HandlerInvoked);
+        }
+
+        class Context : ScenarioContext
+        {
+            public int HandlerInvoked { get; set; }
+        }
+
+        class EndpointWithFailingHandler : EndpointConfigurationBuilder
+        {
+            public EndpointWithFailingHandler()
+            {
+                EndpointSetup<DefaultServer>((config, context) =>
+                {
+                    config.Recoverability().AddUnrecoverableException(typeof(SimulatedException));
+                });
+            }
+
+            class InitiatingHandler : IHandleMessages<InitiatingMessage>
+            {
+                public Context TestContext { get; set; }
+
+                public Task Handle(InitiatingMessage initiatingMessage, IMessageHandlerContext context)
+                {
+                    TestContext.HandlerInvoked++;
+                    throw new CustomException();
+                }
+            }
+
+        }
+
+        class CustomException : SimulatedException
+        {
+        }
+
+        public class InitiatingMessage : IMessage
+        {
+            public Guid Id { get; set; }
+        }
+    }
+}
