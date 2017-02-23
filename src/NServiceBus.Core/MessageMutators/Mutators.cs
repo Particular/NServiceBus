2a15fd1d02c2cf38bb0a7755a6e018c5e500c518
Tim Bussmann <tim.bussmann@particular.net>
2017-02-23 09:54:08 +0100

    create dedicated mutator feature

---
+++ src/NServiceBus.Core/MessageMutators/Mutators.cs
@@ -0,0 +1,19 @@
+﻿namespace NServiceBus.Features
+{
+    class Mutators : Feature
+    {
+        public Mutators()
+        {
+            EnableByDefault();
+        }
+
+        protected internal override void Setup(FeatureConfigurationContext context)
+        {
+            context.Pipeline.Register("MutateIncomingTransportMessage", new MutateIncomingTransportMessageBehavior(), "Executes IMutateIncomingTransportMessages");
+            context.Pipeline.Register("MutateIncomingMessages", new MutateIncomingMessageBehavior(), "Executes IMutateIncomingMessages");
+
+            context.Pipeline.Register("MutateOutgoingMessages", new MutateOutgoingMessageBehavior(), "Executes IMutateOutgoingMessages");
+            context.Pipeline.Register("MutateOutgoingTransportMessage", new MutateOutgoingTransportMessageBehavior(), "Executes IMutateOutgoingTransportMessages");
+        }
+    }
+}
