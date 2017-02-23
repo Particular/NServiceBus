fc3865779186f06feaa89379bb94674e562f33b3
Tim Bussmann <tim.bussmann@particular.net>
2017-02-23 10:34:03 +0100

    new registration API

---
+++ src/NServiceBus.Core.Tests/MessageMutators/MutatorRegistrationExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿namespace NServiceBus.Core.Tests.MessageMutators
+{
+    using System;
+    using System.Threading.Tasks;
+    using MessageMutator;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MutatorRegistrationExtensionsTests
+    {
+        [Test]
+        public void Should_throw_ArgumentException_when_registering_non_mutator_type()
+        {
+            var endpointConfiguration = new EndpointConfiguration("test");
+
+            var exception = Assert.Throws<ArgumentException>(() => endpointConfiguration.RegisterMessageMutator<object>());
+            StringAssert.Contains(
+                "The specified type is no valid message mutator. Implement one of the following mutator interfaces: NServiceBus.MessageMutator.IMutateIncomingMessages, NServiceBus.MessageMutator.IMutateIncomingTransportMessages, NServiceBus.MessageMutator.IMutateOutgoingMessages, NServiceBus.MessageMutator.IMutateOutgoingTransportMessages",
+                exception.Message);
+        }
+
+        [Test]
+        public void Should_not_throw_when_registering_IMutateIncomingMessage()
+        {
+            var endpointConfiguration = new EndpointConfiguration("test");
+
+            endpointConfiguration.RegisterMessageMutator<MyIncomingMessageMutator>();
+        }
+
+        class MyIncomingMessageMutator : IMutateIncomingMessages
+        {
+            public Task MutateIncoming(MutateIncomingMessageContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ src/NServiceBus.Core/MessageMutators/MutatorRegistrationExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace NServiceBus.MessageMutator
+{
+    using System;
+
+    /// <summary>
+    /// Provides extension methods to register message mutators.
+    /// </summary>
+    public static class MutatorRegistrationExtensions
+    {
+        /// <summary>
+        /// Register an incoming message mutator with the pipeline.
+        /// </summary>
+        public static void RegisterMessageMutator<TMessageMutator>(this EndpointConfiguration endpointConfiguration)
+        {
+            Guard.AgainstNull(nameof(endpointConfiguration), endpointConfiguration);
+
+            if (!IsMessageMutator(typeof(TMessageMutator)))
+            {
+                throw new ArgumentException($"The specified type is no valid message mutator. Implement one of the following mutator interfaces: {typeof(IMutateIncomingMessages).FullName}, {typeof(IMutateIncomingTransportMessages).FullName}, {typeof(IMutateOutgoingMessages).FullName}, {typeof(IMutateOutgoingTransportMessages).FullName}");
+            }
+
+            endpointConfiguration.RegisterComponents(c => c.ConfigureComponent<TMessageMutator>(DependencyLifecycle.InstancePerCall));
+        }
+
+        static bool IsMessageMutator(Type messageMutatorType)
+        {
+            return typeof(IMutateIncomingMessages).IsAssignableFrom(messageMutatorType)
+                   || typeof(IMutateIncomingTransportMessages).IsAssignableFrom(messageMutatorType)
+                   || typeof(IMutateOutgoingMessages).IsAssignableFrom(messageMutatorType)
+                   || typeof(IMutateOutgoingTransportMessages).IsAssignableFrom(messageMutatorType);
+        }
+    }
+}
