44f9d24788f521b9c1b2898b66c7492bd316fd0f
andreasohlund <andreas.ohlund@particular.net>
2017-02-27 11:59:30 +0100

    solating receive perf counter into separate feature
    To allow the external package to disable it

---
+++ src/NServiceBus.Core/Performance/ReceiveStatisticsPerformanceCounterFeature.cs
@@ -0,0 +1,45 @@
+﻿namespace NServiceBus
+{
+    using System.Threading.Tasks;
+    using Features;
+
+    class ReceiveStatisticsPerformanceCounterFeature : Feature
+    {
+        public ReceiveStatisticsPerformanceCounterFeature()
+        {
+            EnableByDefault();
+        }
+
+        protected internal override void Setup(FeatureConfigurationContext context)
+        {
+            var logicalAddress = context.Settings.LogicalAddress();
+            var performanceDiagnosticsBehavior = new ReceivePerformanceDiagnosticsBehavior(logicalAddress.EndpointInstance.Endpoint);
+
+            context.Pipeline.Register(performanceDiagnosticsBehavior, "Provides various performance counters for receive statistics");
+
+            context.RegisterStartupTask(new WarmupCooldownTask(performanceDiagnosticsBehavior));
+        }
+
+        class WarmupCooldownTask : FeatureStartupTask
+        {
+            public WarmupCooldownTask(ReceivePerformanceDiagnosticsBehavior behavior)
+            {
+                this.behavior = behavior;
+            }
+
+            protected override Task OnStart(IMessageSession session)
+            {
+                behavior.Warmup();
+                return TaskEx.CompletedTask;
+            }
+
+            protected override Task OnStop(IMessageSession session)
+            {
+                behavior.Cooldown();
+                return TaskEx.CompletedTask;
+            }
+
+            readonly ReceivePerformanceDiagnosticsBehavior behavior;
+        }
+    }
+}
