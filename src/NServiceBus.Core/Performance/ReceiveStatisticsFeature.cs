3033fca5071e976d2784a5b5252ad77ebd9b0eea
Andreas Öhlund <andreas.ohlund@particular.net>
2017-02-28 14:19:27 +0100

    Isolate receive statistic perf counters in separate feature (#4507)
    * solating receive perf counter into separate feature
    
    To allow the external package to disable it
    
    * Make feature public so that it can be disabled
    
    * Name feature properly

--- src/NServiceBus.Core/Performance/ReceiveStatisticsFeature.cs
+++ src/NServiceBus.Core/Performance/ReceiveStatisticsFeature.cs
@@ -1,6 +1,5 @@
 ﻿namespace NServiceBus
 {
-    using System.Threading.Tasks;
     using Features;
 
     class ReceiveStatisticsFeature : Feature
@@ -12,36 +11,8 @@
 
         protected internal override void Setup(FeatureConfigurationContext context)
         {
-            var logicalAddress = context.Settings.LogicalAddress();
-            var performanceDiagnosticsBehavior = new ReceivePerformanceDiagnosticsBehavior(logicalAddress.EndpointInstance.Endpoint);
-
-            context.Pipeline.Register(performanceDiagnosticsBehavior, "Provides various performance counters for receive statistics");
             context.Pipeline.Register("ProcessingStatistics", new ProcessingStatisticsBehavior(), "Collects timing for ProcessingStarted and adds the state to determine ProcessingEnded");
             context.Pipeline.Register("AuditProcessingStatistics", new AuditProcessingStatisticsBehavior(), "Add ProcessingStarted and ProcessingEnded headers");
-
-            context.RegisterStartupTask(new WarmupCooldownTask(performanceDiagnosticsBehavior));
-        }
-
-        class WarmupCooldownTask : FeatureStartupTask
-        {
-            public WarmupCooldownTask(ReceivePerformanceDiagnosticsBehavior behavior)
-            {
-                this.behavior = behavior;
-            }
-
-            protected override Task OnStart(IMessageSession session)
-            {
-                behavior.Warmup();
-                return TaskEx.CompletedTask;
-            }
-
-            protected override Task OnStop(IMessageSession session)
-            {
-                behavior.Cooldown();
-                return TaskEx.CompletedTask;
-            }
-
-            readonly ReceivePerformanceDiagnosticsBehavior behavior;
         }
     }
 }
