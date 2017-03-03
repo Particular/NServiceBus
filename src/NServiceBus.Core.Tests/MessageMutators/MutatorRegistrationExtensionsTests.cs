d71370d69235cc61d4f50f74ddc7908c3e887f82
Tim Bussmann <tim.bussmann@particular.net>
2017-03-03 11:25:31 +0100

    add more tests

--- src/NServiceBus.Core.Tests/MessageMutators/MutatorRegistrationExtensionsTests.cs
+++ src/NServiceBus.Core.Tests/MessageMutators/MutatorRegistrationExtensionsTests.cs
@@ -15,21 +15,81 @@
 
             var exception = Assert.Throws<ArgumentException>(() => endpointConfiguration.RegisterMessageMutator(new object()));
             StringAssert.Contains(
-                "The specified type is no valid message mutator. Implement one of the following mutator interfaces: NServiceBus.MessageMutator.IMutateIncomingMessages, NServiceBus.MessageMutator.IMutateIncomingTransportMessages, NServiceBus.MessageMutator.IMutateOutgoingMessages, NServiceBus.MessageMutator.IMutateOutgoingTransportMessages",
+                "The given instance is no valid message mutator. Implement one of the following mutator interfaces: NServiceBus.MessageMutator.IMutateIncomingMessages, NServiceBus.MessageMutator.IMutateIncomingTransportMessages, NServiceBus.MessageMutator.IMutateOutgoingMessages, NServiceBus.MessageMutator.IMutateOutgoingTransportMessages",
                 exception.Message);
         }
 
+        [Theory]
+        [TestCase(typeof(IncomingMessageMutator))]
+        [TestCase(typeof(IncomingTransportMessageMutator))]
+        [TestCase(typeof(OutgoingMessageMutator))]
+        [TestCase(typeof(OutgoingTransportMessageMutator))]
+        public void Should_not_throw_when_registering_mutator(Type mutatorType)
+        {
+            var endpointConfiguration = new EndpointConfiguration("test");
+            var messageMutator = Activator.CreateInstance(mutatorType);
+
+            Assert.DoesNotThrow(() => endpointConfiguration.RegisterMessageMutator(messageMutator));
+        }
+
         [Test]
-        public void Should_not_throw_when_registering_IMutateIncomingMessage()
+        public void Should_not_throw_when_registering_mutator_implementing_multiple_mutator_interfaces()
         {
             var endpointConfiguration = new EndpointConfiguration("test");
 
-            endpointConfiguration.RegisterMessageMutator(new MyIncomingMessageMutator());
+            Assert.DoesNotThrow(() => endpointConfiguration.RegisterMessageMutator(new MultiMutator()));
         }
 
-        class MyIncomingMessageMutator : IMutateIncomingMessages
+        class IncomingMessageMutator : IMutateIncomingMessages
         {
             public Task MutateIncoming(MutateIncomingMessageContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class IncomingTransportMessageMutator : IMutateIncomingTransportMessages
+        {
+            public Task MutateIncoming(MutateIncomingTransportMessageContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class OutgoingMessageMutator : IMutateOutgoingMessages
+        {
+            public Task MutateOutgoing(MutateOutgoingMessageContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class OutgoingTransportMessageMutator : IMutateOutgoingTransportMessages
+        {
+            public Task MutateOutgoing(MutateOutgoingTransportMessageContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class MultiMutator : IMutateIncomingMessages, IMutateIncomingTransportMessages, IMutateOutgoingTransportMessages, IMutateOutgoingMessages
+        {
+            public Task MutateIncoming(MutateIncomingMessageContext context)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task MutateIncoming(MutateIncomingTransportMessageContext context)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task MutateOutgoing(MutateOutgoingTransportMessageContext context)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task MutateOutgoing(MutateOutgoingMessageContext context)
             {
                 throw new NotImplementedException();
             }
