24a99ba5a2c7bd008465ba8c6315cfcd4539054c
Tim Bussmann <tim.bussmann@particular.net>
2017-03-06 16:06:08 +0100

    add TreatAsErrorFromVersion attribute

--- src/NServiceBus.Core/Recoverability/Faults/MessageForwardingInCaseOfFaultConfig.cs
+++ src/NServiceBus.Core/Recoverability/Faults/MessageForwardingInCaseOfFaultConfig.cs
@@ -5,14 +5,22 @@ namespace NServiceBus.Config
     /// <summary>
     /// Message Forwarding In Case Of Fault Config.
     /// </summary>
-    [ObsoleteEx(Message = "Error queue configuration via configuration section is discouraged.", ReplacementTypeOrMember = "EndpointConfiguration.SendFailedMessagesTo", RemoveInVersion = "7")]
+    [ObsoleteEx(
+        Message = "Error queue configuration via configuration section is discouraged.",
+        ReplacementTypeOrMember = "EndpointConfiguration.SendFailedMessagesTo",
+        TreatAsErrorFromVersion = "7",
+        RemoveInVersion = "8")]
     public class MessageForwardingInCaseOfFaultConfig : ConfigurationSection
     {
         /// <summary>
         /// The queue to which errors will be forwarded.
         /// </summary>
         [ConfigurationProperty("ErrorQueue", IsRequired = true)]
-        [ObsoleteEx(Message = "Error queue configuration via configuration section is discouraged.", ReplacementTypeOrMember = "EndpointConfiguration.SendFailedMessagesTo", RemoveInVersion = "7")]
+        [ObsoleteEx(
+            Message = "Error queue configuration via configuration section is discouraged.",
+            ReplacementTypeOrMember = "EndpointConfiguration.SendFailedMessagesTo",
+            TreatAsErrorFromVersion = "7",
+            RemoveInVersion = "8")]
         public string ErrorQueue
         {
             get { return this["ErrorQueue"] as string; }
