df8ab2cfdc1a24cb1c963c17da62a4f9817ffc42
Kyle Baley <kyle@baley.org>
2017-03-06 18:27:41 -0600

    Obsolete performance counter features

--- src/NServiceBus.Core/Performance/Statistics/CriticalTime/CriticalTimeMonitoringConfig.cs
+++ src/NServiceBus.Core/Performance/Statistics/CriticalTime/CriticalTimeMonitoringConfig.cs
@@ -5,6 +5,10 @@ namespace NServiceBus
     /// <summary>
     /// Provide configuration options for monitoring related settings.
     /// </summary>
+    [ObsoleteEx(
+        Message = "Performance counters have been released as a separate package: NServiceBus.WindowsPerformanceCounters",
+        RemoveInVersion = "8",
+        TreatAsErrorFromVersion = "7")]
     public static class CriticalTimeMonitoringConfig
     {
         /// <summary>
--- src/NServiceBus.Core/Performance/Statistics/SLA/SLAMonitoringConfig.cs
+++ src/NServiceBus.Core/Performance/Statistics/SLA/SLAMonitoringConfig.cs
@@ -6,6 +6,10 @@ namespace NServiceBus
     /// <summary>
     /// Provide configuration options for monitoring related settings.
     /// </summary>
+    [ObsoleteEx(
+        Message = "Performance counters have been released as a separate package: NServiceBus.WindowsPerformanceCounters",
+        RemoveInVersion = "8",
+        TreatAsErrorFromVersion = "7")]
     public static class SLAMonitoringConfig
     {
         /// <summary>
@@ -13,6 +17,10 @@ namespace NServiceBus
         /// </summary>
         /// <param name="config">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
         /// <param name="sla">The <see cref="TimeSpan" /> to use oa the SLA. Must be greater than <see cref="TimeSpan.Zero" />.</param>
+        [ObsoleteEx(
+            Message = "Performance counters have been released as a separate package: NServiceBus.WindowsPerformanceCounters",
+            RemoveInVersion = "8",
+            TreatAsErrorFromVersion = "7")]
         public static void EnableSLAPerformanceCounter(this EndpointConfiguration config, TimeSpan sla)
         {
             Guard.AgainstNull(nameof(config), config);
@@ -25,6 +33,10 @@ namespace NServiceBus
         /// Enables the NServiceBus specific performance counters with a specific EndpointSLA.
         /// </summary>
         /// <param name="config">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
+        [ObsoleteEx(
+            Message = "Performance counters have been released as a separate package: NServiceBus.WindowsPerformanceCounters",
+            RemoveInVersion = "8",
+            TreatAsErrorFromVersion = "7")]
         public static void EnableSLAPerformanceCounter(this EndpointConfiguration config)
         {
             Guard.AgainstNull(nameof(config), config);
