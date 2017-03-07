404ff9517aab25907ff1e879c7b545af130a0bd0
Kyle Baley <kyle@baley.org>
2017-03-07 10:56:04 -0600

    Obsolete extension method rather than encompassing class

--- src/NServiceBus.Core/Performance/Statistics/CriticalTime/CriticalTimeMonitoringConfig.cs
+++ src/NServiceBus.Core/Performance/Statistics/CriticalTime/CriticalTimeMonitoringConfig.cs
@@ -5,16 +5,16 @@ namespace NServiceBus
     /// <summary>
     /// Provide configuration options for monitoring related settings.
     /// </summary>
-    [ObsoleteEx(
-        Message = "Performance counters have been released as a separate package: NServiceBus.WindowsPerformanceCounters",
-        RemoveInVersion = "8",
-        TreatAsErrorFromVersion = "7")]
     public static class CriticalTimeMonitoringConfig
     {
         /// <summary>
         /// Enables the NServiceBus specific performance counters.
         /// </summary>
         /// <param name="config">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
+        [ObsoleteEx(
+            Message = "Performance counters have been released as a separate package: NServiceBus.WindowsPerformanceCounters",
+            RemoveInVersion = "8",
+            TreatAsErrorFromVersion = "7")]
         public static void EnableCriticalTimePerformanceCounter(this EndpointConfiguration config)
         {
             Guard.AgainstNull(nameof(config), config);
