4ee6b04db76fb958d706b61906bfbb9a624c9733
andreasohlund <andreas.ohlund@particular.net>
2017-03-14 10:51:20 +0100

    Add option to configure storage location

--- src/NServiceBus.Core/Transports/Development/DevelopmentTransport.cs
+++ src/NServiceBus.Core/Transports/Development/DevelopmentTransport.cs
@@ -14,7 +14,15 @@
         public override bool RequiresConnectionString => false;
 
         /// <summary>
-        /// Initializes all the factories and supported features for the transport. This method is called right before all features are activated and the settings will be locked down. This means you can use the SettingsHolder both for providing default capabilities as well as for initializing the transport's configuration based on those settings (the user cannot provide information anymore at this stage).
+        /// Gets an example connection string to use when reporting lack of configured connection string to the user.
+        /// </summary>
+        public override string ExampleConnectionStringForErrorMessage { get; } = "";
+
+        /// <summary>
+        /// Initializes all the factories and supported features for the transport. This method is called right before all features
+        /// are activated and the settings will be locked down. This means you can use the SettingsHolder both for providing
+        /// default capabilities as well as for initializing the transport's configuration based on those settings (the user cannot
+        /// provide information anymore at this stage).
         /// </summary>
         /// <param name="settings">An instance of the current settings.</param>
         /// <param name="connectionString">The connection string.</param>
@@ -23,10 +31,5 @@
         {
             return new DevelopmentTransportInfrastructure(settings);
         }
-
-        /// <summary>
-        /// Gets an example connection string to use when reporting lack of configured connection string to the user.
-        /// </summary>
-        public override string ExampleConnectionStringForErrorMessage { get; } = "";
     }
 }
