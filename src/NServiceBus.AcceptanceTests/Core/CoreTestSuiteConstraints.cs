9b9267820b78fabcd5bd5a8e03555edc73eb5ee6
andreasohlund <andreas.ohlund@particular.net>
2017-03-14 16:13:20 +0100

    fix pubsub bug

--- src/NServiceBus.AcceptanceTests/Core/CoreTestSuiteConstraints.cs
+++ src/NServiceBus.AcceptanceTests/Core/CoreTestSuiteConstraints.cs
@@ -7,7 +7,7 @@
         public bool SupportsDtc => false;
         public bool SupportsCrossQueueTransactions => true;
         public bool SupportsNativePubSub => true;
-        public bool SupportsNativeDeferral => false;
+        public bool SupportsNativeDeferral => true;
         public bool SupportsOutbox => true;
         public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointDevelopmentTransport();
         public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointInMemoryPersistence();
