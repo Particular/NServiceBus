699d0efca59bac41c1b4382f9fe994cc843f4540
andreasohlund <andreas.ohlund@particular.net>
2017-03-15 13:19:58 +0100

    Handle txmode none properly

--- src/NServiceBus.Core/Transports/Development/DirectoryBasedTransaction.cs
+++ src/NServiceBus.Core/Transports/Development/DirectoryBasedTransaction.cs
@@ -5,7 +5,19 @@ namespace NServiceBus
     using System.IO;
     using System.Linq;
 
-    class DirectoryBasedTransaction
+    interface IDevelopmentTransportTransaction
+    {
+        string FileToProcess { get; }
+
+        void BeginTransaction(string incomingFilePath);
+        void Commit();
+        void Rollback();
+        void ClearPendingOutgoingOperations();
+        void Enlist(string messagePath, List<string> messageContents);
+        void Complete();
+    }
+
+    class DirectoryBasedTransaction : IDevelopmentTransportTransaction
     {
         public DirectoryBasedTransaction(string basePath)
         {
