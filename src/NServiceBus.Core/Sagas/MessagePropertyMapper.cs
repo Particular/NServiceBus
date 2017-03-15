f6be13bd5ceeb57c0699b806dc0e32cdf3d36247
Simon Cropp <simon.cropp@gmail.com>
2017-03-16 09:36:18 +1100

    SagaV2 (#4546)
    * add simplesaga
    
    * typo
    
    * fix build errors
    
    * switch to string for CorrelationProperty
    
    * no need to expose generic class
    
    * better mapping name
    
    * make name unique
    
    * better doco
    
    * no need to verify on every construction
    
    * fix doco
    
    * doco
    
    * better exception
    
    * simplify GetExpression
    
    * remove LimitMessageProcessingConcurrencyTo
    
    * rename to saga v2
    
    * Misc cleanup

---
+++ src/NServiceBus.Core/Sagas/MessagePropertyMapper.cs
@@ -0,0 +1,31 @@
+namespace NServiceBus
+{
+    using System;
+    using System.Linq.Expressions;
+    using Sagas;
+
+    class MessagePropertyMapper<TSagaData> : IMessagePropertyMapper
+        where TSagaData : IContainSagaData
+    {
+        internal MessagePropertyMapper(IConfigureHowToFindSagaWithMessage sagaMessageFindingConfiguration, Expression<Func<TSagaData, object>> sagaEntityProperty, Type sagaType)
+        {
+            this.sagaMessageFindingConfiguration = sagaMessageFindingConfiguration;
+            this.sagaEntityProperty = sagaEntityProperty;
+            this.sagaType = sagaType;
+        }
+
+        public void ConfigureMapping<TMessage>(Expression<Func<TMessage, object>> messageProperty)
+        {
+            if (sagaEntityProperty == null)
+            {
+                throw new Exception($"The saga '{sagaType.FullName}' has not defined a CorrelationPropertyName, so it is expected that a {nameof(IFindSagas<TSagaData>)} will be defined for all messages the saga handles.");
+            }
+            Guard.AgainstNull(nameof(messageProperty), messageProperty);
+            sagaMessageFindingConfiguration.ConfigureMapping(sagaEntityProperty, messageProperty);
+        }
+
+        IConfigureHowToFindSagaWithMessage sagaMessageFindingConfiguration;
+        Expression<Func<TSagaData, object>> sagaEntityProperty;
+        Type sagaType;
+    }
+}
