a467d96a902c0c94215a4a20b55c0b21998a373b
Tim Bussmann <tim.bussmann@particular.net>
2017-03-22 19:07:01 +0100

    align AssemblyScanner config API with API design guidelines

--- src/NServiceBus.Core/Hosting/Helpers/AssemblyScannerConfigurationExtensions.cs
+++ src/NServiceBus.Core/Hosting/Helpers/AssemblyScannerConfigurationExtensions.cs
@@ -1,7 +1,5 @@
 ﻿namespace NServiceBus
 {
-    using System;
-
     /// <summary>
     /// Contains extension methods to configure the <see cref="AssemblyScanner"/> behavior.
     /// </summary>
@@ -10,13 +8,12 @@
         /// <summary>
         /// Configure the <see cref="AssemblyScanner"/>.
         /// </summary>
-        public static void AssemblyScanner(this EndpointConfiguration configuration, Action<AssemblyScannerConfiguration> configure)
+        public static AssemblyScannerConfiguration AssemblyScanner(this EndpointConfiguration configuration)
         {
             Guard.AgainstNull(nameof(configuration), configuration);
-            Guard.AgainstNull(nameof(configure), configure);
 
             var config = configuration.Settings.GetOrCreate<AssemblyScannerConfiguration>();
-            configure(config);
+            return config;
         }
     }
 }
