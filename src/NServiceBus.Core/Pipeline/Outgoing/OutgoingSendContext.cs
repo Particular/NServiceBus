ba8f9f1148e39185fd05bf340562f550222d7a3d
Brandon Ording <bording@gmail.com>
2017-04-12 14:25:38 -0400

    Fix typo in OutgoingSendContext

--- src/NServiceBus.Core/Pipeline/Outgoing/OutgoingSendContext.cs
+++ src/NServiceBus.Core/Pipeline/Outgoing/OutgoingSendContext.cs
@@ -6,16 +6,16 @@
 
     class OutgoingSendContext : OutgoingContext, IOutgoingSendContext
     {
-        public OutgoingSendContext(OutgoingLogicalMessage message, string messageId, Dictionary<string, string> headers, ContextBag extensinos, IBehaviorContext parentContext)
+        public OutgoingSendContext(OutgoingLogicalMessage message, string messageId, Dictionary<string, string> headers, ContextBag extensions, IBehaviorContext parentContext)
             : base(messageId, headers, parentContext)
         {
             Guard.AgainstNull(nameof(parentContext), parentContext);
             Guard.AgainstNull(nameof(message), message);
-            Guard.AgainstNull(nameof(extensinos), extensinos);
+            Guard.AgainstNull(nameof(extensions), extensions);
 
             Message = message;
 
-            Merge(extensinos);
+            Merge(extensions);
         }
 
         public OutgoingLogicalMessage Message { get; }
