73da3a10dc1978085490ce091c6ed306bf52ecbe
danielmarbach <daniel.marbach@openplace.net>
2017-04-24 21:19:42 +0200

    Adressed review comment

--- src/NServiceBus.Core/Persistence/Learning/SagaPersister/LearningSagaPersisterConfigExtensions.cs
+++ src/NServiceBus.Core/Persistence/Learning/SagaPersister/LearningSagaPersisterConfigExtensions.cs
@@ -1,7 +1,6 @@
 ﻿namespace NServiceBus
 {
     using Features;
-    using Persistence;
 
     /// <summary>
     /// Configuration options for the development saga persister.
@@ -13,7 +12,7 @@
         /// </summary>
         /// <param name="config">Config object to extend.</param>
         /// <param name="path">The storage path.</param>
-        public static void SagaStorageDirectory(this PersistenceExtensions<LearningPersistence, StorageType.Sagas> config, string path)
+        public static void SagaStorageDirectory(this PersistenceExtensions<LearningPersistence> config, string path)
         {
             Guard.AgainstNullAndEmpty(nameof(path), path);
 
