43a6765761877c2c3c937268d3c720dbf06e8233
Tim Bussmann <tim.bussmann@particular.net>
2017-04-26 11:33:56 +0200

    convert acceptance to unit test

---
+++ src/NServiceBus.Core.Tests/Performance/MessageDurability/DetermineMessageDurabilityBehaviorTests.cs
@@ -0,0 +1,58 @@
+﻿namespace NServiceBus.Core.Tests.Performance.MessageDurability
+{
+    using System.Threading.Tasks;
+    using DeliveryConstraints;
+    using NUnit.Framework;
+    using Testing;
+
+    public class DetermineMessageDurabilityBehaviorTests
+    {
+        [Test]
+        public async Task When_message_is_non_durable_should_add_non_durable_constraint()
+        {
+            var behavior = new DetermineMessageDurabilityBehavior(t => true);
+            var context = new TestableOutgoingLogicalMessageContext();
+
+            await behavior.Invoke(context, _ => TaskEx.CompletedTask);
+
+            NonDurableDelivery nonDurableDeliveryConstraint;
+            Assert.IsTrue(context.Extensions.TryGetDeliveryConstraint(out nonDurableDeliveryConstraint));
+            Assert.IsNotNull(nonDurableDeliveryConstraint);
+        }
+
+        [Test]
+        public async Task When_message_is_non_durable_should_set_non_durable_header()
+        {
+            var behavior = new DetermineMessageDurabilityBehavior(t => true);
+            var context = new TestableOutgoingLogicalMessageContext();
+
+            await behavior.Invoke(context, _ => TaskEx.CompletedTask);
+
+            Assert.IsTrue(bool.Parse(context.Headers[Headers.NonDurableMessage]));
+        }
+
+        [Test]
+        public async Task When_message_is_durable_should_not_add_non_durable_constraint()
+        {
+            var behavior = new DetermineMessageDurabilityBehavior(t => false);
+            var context = new TestableOutgoingLogicalMessageContext();
+
+            await behavior.Invoke(context, _ => TaskEx.CompletedTask);
+
+            NonDurableDelivery nonDurableDeliveryConstraint;
+            Assert.IsFalse(context.Extensions.TryGetDeliveryConstraint(out nonDurableDeliveryConstraint));
+            Assert.IsNull(nonDurableDeliveryConstraint);
+        }
+
+        [Test]
+        public async Task When_message_is_durable_should_not_set_non_durable_header()
+        {
+            var behavior = new DetermineMessageDurabilityBehavior(t => false);
+            var context = new TestableOutgoingLogicalMessageContext();
+
+            await behavior.Invoke(context, _ => TaskEx.CompletedTask);
+
+            Assert.IsFalse(context.Headers.ContainsKey(Headers.NonDurableMessage));
+        }
+    }
+}
