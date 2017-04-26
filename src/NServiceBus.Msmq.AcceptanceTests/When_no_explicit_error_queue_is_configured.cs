3a44953d75f5bf067d86c9674e10491f01b9cb5e
andreasohlund <andreas.ohlund@particular.net>
2017-04-26 17:32:42 +0200

    Cleanup test

--- src/NServiceBus.Msmq.AcceptanceTests/When_no_explicit_error_queue_is_configured.cs
+++ src/NServiceBus.Msmq.AcceptanceTests/When_no_explicit_error_queue_is_configured.cs
@@ -1,18 +1,9 @@
 ﻿namespace NServiceBus.AcceptanceTests.Recoverability
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Messaging;
-    using System.Threading.Tasks;
     using AcceptanceTesting;
-    using AcceptanceTesting.Customization;
-    using AcceptanceTesting.Support;
-    using Config.ConfigurationSource;
-    using Configuration.AdvanceExtensibility;
     using EndpointTemplates;
     using NUnit.Framework;
-    using Transport;
 
     public class When_no_explicit_error_queue_is_configured : NServiceBusAcceptanceTest
     {
@@ -41,88 +32,13 @@
         {
         }
 
-        public class PlainVanillaEndpoint : IEndpointSetupTemplate
+        public class PlainVanillaEndpoint : BasicServer
         {
-            public PlainVanillaEndpoint()
+         
+            protected override void ApplyConfig(EndpointConfiguration configuration)
             {
-                typesToInclude = new List<Type>();
+                //no-op
             }
-
-            public PlainVanillaEndpoint(List<Type> typesToInclude)
-            {
-                this.typesToInclude = typesToInclude;
-            }
-
-#pragma warning disable CS0618
-            public Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointConfiguration, IConfigurationSource configSource, Action<EndpointConfiguration> configurationBuilderCustomization)
-#pragma warning restore CS0618
-            {
-                var types = endpointConfiguration.GetTypesScopedByTestClass();
-
-                typesToInclude.AddRange(types);
-
-                var configuration = new EndpointConfiguration(endpointConfiguration.EndpointName);
-
-                configuration.TypesToIncludeInScan(typesToInclude);
-                configuration.EnableInstallers();
-
-                configuration.UseTransport<MsmqTransport>();
-
-                var queueBindings = configuration.GetSettings().Get<QueueBindings>();
-                runDescriptor.OnTestCompleted(_ => DeleteQueues(queueBindings));
-
-                configuration.RegisterComponentsAndInheritanceHierarchy(runDescriptor);
-
-                configuration.UsePersistence<InMemoryPersistence>();
-
-                configurationBuilderCustomization(configuration);
-
-                return Task.FromResult(configuration);
-            }
-
-            Task DeleteQueues(QueueBindings queueBindings)
-            {
-                var allQueues = MessageQueue.GetPrivateQueuesByMachine("localhost");
-                var queuesToBeDeleted = new List<string>();
-
-                foreach (var messageQueue in allQueues)
-                {
-                    using (messageQueue)
-                    {
-                        if (queueBindings.ReceivingAddresses.Any(ra =>
-                        {
-                            var indexOfAt = ra.IndexOf("@", StringComparison.Ordinal);
-                            if (indexOfAt >= 0)
-                            {
-                                ra = ra.Substring(0, indexOfAt);
-                            }
-                            return messageQueue.QueueName.StartsWith(@"private$\" + ra, StringComparison.OrdinalIgnoreCase);
-                        }))
-                        {
-                            queuesToBeDeleted.Add(messageQueue.Path);
-                        }
-                    }
-                }
-
-                foreach (var queuePath in queuesToBeDeleted)
-                {
-                    try
-                    {
-                        MessageQueue.Delete(queuePath);
-                        Console.WriteLine("Deleted '{0}' queue", queuePath);
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Could not delete queue '{0}'", queuePath);
-                    }
-                }
-
-                MessageQueue.ClearConnectionCache();
-
-                return Task.FromResult(0);
-            }
-
-            List<Type> typesToInclude;
         }
     }
 }
