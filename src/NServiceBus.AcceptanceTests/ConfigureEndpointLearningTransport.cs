633a9b54e8686879e999f1670ad6792e4bc54491
andreasohlund <andreas.ohlund@particular.net>
2017-05-03 10:22:14 +0200

    Remove using

--- src/NServiceBus.AcceptanceTests/ConfigureEndpointLearningTransport.cs
+++ src/NServiceBus.AcceptanceTests/ConfigureEndpointLearningTransport.cs
@@ -2,7 +2,6 @@ using System.IO;
 using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.AcceptanceTesting.Support;
-using NUnit.Framework;
 
 public class ConfigureEndpointLearningTransport : IConfigureEndpointTestExecution
 {
