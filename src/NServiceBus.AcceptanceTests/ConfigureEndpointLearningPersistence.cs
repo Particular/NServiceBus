5ff5fdf31588885ced4b7f54715b2f6c2b80c6eb
Simon Cropp <simon.cropp@gmail.com>
2017-05-05 07:51:38 +1000

    use N for guid

--- src/NServiceBus.AcceptanceTests/ConfigureEndpointLearningPersistence.cs
+++ src/NServiceBus.AcceptanceTests/ConfigureEndpointLearningPersistence.cs
@@ -9,7 +9,8 @@ public class ConfigureEndpointLearningPersistence : IConfigureEndpointTestExecut
 {
     public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
     {
-        storageDir = Path.Combine(@"c:\temp", Guid.NewGuid().ToString().Replace("-", "")); //can't use bindir since that will be to long on the build agents
+        //can't use bindir since that will be to long on the build agents
+        storageDir = Path.Combine(@"c:\temp", Guid.NewGuid().ToString("N"));
 
         configuration.UsePersistence<InMemoryPersistence, StorageType.Subscriptions>();
         configuration.UsePersistence<InMemoryPersistence, StorageType.Timeouts>();
