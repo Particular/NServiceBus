e9a4d5cbc5ddc1db749d2339ab6021b00642317a
Brandon Ording <bording@gmail.com>
2017-05-08 16:18:57 -0400

    Rename class and method parameter

--- src/NServiceBus.Core/Persistence/Learning/SagaPersister/LearningSagaPersisterConfigurationExtensions.cs
+++ src/NServiceBus.Core/Persistence/Learning/SagaPersister/LearningSagaPersisterConfigurationExtensions.cs
@@ -5,18 +5,18 @@
     /// <summary>
     /// Configuration options for the learning saga persister.
     /// </summary>
-    public static class LearningSagaPersisterConfigExtensions
+    public static class LearningSagaPersisterConfigurationExtensions
     {
         /// <summary>
         /// Configures the location where sagas are stored.
         /// </summary>
-        /// <param name="config">Config object to extend.</param>
+        /// <param name="persistenceExtensions">The persistence extensions to extend.</param>
         /// <param name="path">The storage path.</param>
-        public static void SagaStorageDirectory(this PersistenceExtensions<LearningPersistence> config, string path)
+        public static void SagaStorageDirectory(this PersistenceExtensions<LearningPersistence> persistenceExtensions, string path)
         {
             Guard.AgainstNullAndEmpty(nameof(path), path);
 
-            config.Settings.Set(LearningSagaPersistence.StorageLocationKey, path);
+            persistenceExtensions.Settings.Set(LearningSagaPersistence.StorageLocationKey, path);
         }
     }
 }
