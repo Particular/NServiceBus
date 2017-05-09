34ff86f53431036eca3de5e4368bf9eacc43e00e
andreasohlund <andreas.ohlund@particular.net>
2017-05-09 21:08:25 +0200

    Add opt out config option

---
+++ src/NServiceBus.AcceptanceTests/Core/LearningTransport/When_disabling_payload_restrictions.cs
@@ -0,0 +1,57 @@
+﻿namespace NServiceBus.AcceptanceTests.Core.Pipeline
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_disabling_payload_restrictions : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_allow_messages_above_64kb()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<LargePayloadEndpoint>(b => b.When(session => session.SendLocal(new SomeMessage
+                {
+                    LargeProperty = new byte[1024 * 64]
+                })))
+                .Done(c => c.MessageReceived)
+                .Run();
+
+            Assert.True(context.MessageReceived, "Message was not received");
+        }
+
+        class Context : ScenarioContext
+        {
+            public bool MessageReceived { get; set; }
+        }
+
+        class LargePayloadEndpoint : EndpointConfigurationBuilder
+        {
+            public LargePayloadEndpoint()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.UseTransport<LearningTransport>()
+                        .NoPayloadSizeRestriction();
+                });
+            }
+
+            class SomeMessageHandler : IHandleMessages<SomeMessage>
+            {
+                public Context TestContext { get; set; }
+
+                public Task Handle(SomeMessage message, IMessageHandlerContext context)
+                {
+                    TestContext.MessageReceived = true;
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+        public class SomeMessage : IMessage
+        {
+            public byte[] LargeProperty { get; set; }
+        }
+    }
+}
--- src/NServiceBus.Core/Transports/Learning/LearningTransportInfrastructure.cs
+++ src/NServiceBus.Core/Transports/Learning/LearningTransportInfrastructure.cs
@@ -68,8 +68,9 @@
 
         public override TransportSendInfrastructure ConfigureSendInfrastructure()
         {
-            //todo: make size configurable
-            return new TransportSendInfrastructure(() => new LearningTransportDispatcher(storagePath, 64), () => Task.FromResult(StartupCheckResult.Success));
+            var maxPayloadSize = settings.GetOrDefault<bool>(NoPayloadSizeRestrictionKey) ? int.MaxValue / 1024 : 64; //64 kB is the max size of the ASQ transport
+
+            return new TransportSendInfrastructure(() => new LearningTransportDispatcher(storagePath, maxPayloadSize), () => Task.FromResult(StartupCheckResult.Success));
         }
 
         public override TransportSubscriptionInfrastructure ConfigureSubscriptionInfrastructure()
@@ -104,6 +105,8 @@
 
         string storagePath;
         SettingsHolder settings;
+
         public const string StorageLocationKey = "LearningTransport.StoragePath";
+        public const string NoPayloadSizeRestrictionKey = "LearningTransport.NoPayloadSizeRestrictionKey";
     }
 }
