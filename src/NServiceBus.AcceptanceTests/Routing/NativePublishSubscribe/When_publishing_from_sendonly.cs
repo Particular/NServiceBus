31c04cbe562de50cc31f6667b32e6693b3db344c
Donald Belcham <donald.belcham@particular.net>
2017-05-12 09:48:30 -0300

    Add copy of Message Driven PubSub test to Native

---
+++ src/NServiceBus.AcceptanceTests/Routing/NativePublishSubscribe/When_publishing_from_sendonly.cs
@@ -0,0 +1,65 @@
+﻿namespace NServiceBus.AcceptanceTests.Routing.NativePublishSubscribe
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NServiceBus;
+    using NUnit.Framework;
+
+    public class When_publishing_from_sendonly : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_be_delivered_to_all_subscribers()
+        {
+            Requires.NativePubSubSupport();
+
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<SendOnlyPublisher>(b => b.When((session, c) => session.Publish(new MyEvent())))
+                .WithEndpoint<Subscriber>()
+                .Done(c => c.SubscriberGotTheEvent)
+                .Run();
+
+            Assert.True(context.SubscriberGotTheEvent);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool SubscriberGotTheEvent { get; set; }
+        }
+
+        public class SendOnlyPublisher : EndpointConfigurationBuilder
+        {
+            public SendOnlyPublisher()
+            {
+                EndpointSetup<DefaultPublisher>(b =>
+                {
+                    b.SendOnly();
+                });
+            }
+        }
+
+        public class Subscriber : EndpointConfigurationBuilder
+        {
+            public Subscriber()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class MyEventHandler : IHandleMessages<MyEvent>
+            {
+                public Context Context { get; set; }
+
+                public Task Handle(MyEvent messageThatIsEnlisted, IMessageHandlerContext context)
+                {
+                    Context.SubscriberGotTheEvent = true;
+
+                    return Task.FromResult(0);
+                }
+            }
+        }
+        
+        public class MyEvent : IEvent
+        {
+        }
+    }
+}
