4dfe5f3c2004644a01eeaadb2728f97a1aba91af
Tim Bussmann <timbussmann@users.noreply.github.com>
2017-05-15 13:40:30 +0200

    remove MEM from routing exception messages (#4707)

--- src/NServiceBus.Core/Routing/Routers/UnicastPublishRouterConnector.cs
+++ src/NServiceBus.Core/Routing/Routers/UnicastPublishRouterConnector.cs
@@ -34,7 +34,7 @@ namespace NServiceBus
             }
             catch (QueueNotFoundException ex)
             {
-                throw new Exception($"The destination queue '{ex.Queue}' could not be found. The destination may be misconfigured for this kind of message ({eventType}) in the MessageEndpointMappings of the UnicastBusConfig section in the configuration file. It may also be the case that the given queue hasn\'t been created yet, or has been deleted.", ex);
+                throw new Exception($"The destination queue '{ex.Queue}' could not be found. The destination may be misconfigured for this kind of message ({context.Message.MessageType}) in the routing section of the transport configuration. It may also be the case that the given queue hasn\'t been created yet, or has been deleted.", ex);
             }
         }
 
