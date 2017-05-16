c67c5d6aa0e1c2c1b092865f444464f3edc28aa0
Brandon Ording <bording@gmail.com>
2017-05-16 15:00:56 -0400

    Wording tweaks

--- src/NServiceBus.Core/DelayedDelivery/TimeoutManager/ConfigurationTimeoutExtensions.cs
+++ src/NServiceBus.Core/DelayedDelivery/TimeoutManager/ConfigurationTimeoutExtensions.cs
@@ -8,11 +8,10 @@
     public static class ConfigurationTimeoutExtensions
     {
         /// <summary>
-        /// A critical error is raised when timeout retrieval fails over a certain period of time.
-        /// This method allows to change the default and extend the wait time before raising a critical error.
+        /// Configures the amount of time to wait after a timeout retrieval fails before triggering a critical error.
         /// </summary>
         /// <param name="config">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
-        /// <param name="timeToWait">Time to wait before raising a critical error.</param>
+        /// <param name="timeToWait">The amount of time to wait before triggering a critical error.</param>
         public static void TimeToWaitBeforeTriggeringCriticalErrorOnTimeoutOutages(this EndpointConfiguration config, TimeSpan timeToWait)
         {
             Guard.AgainstNull(nameof(config), config);
