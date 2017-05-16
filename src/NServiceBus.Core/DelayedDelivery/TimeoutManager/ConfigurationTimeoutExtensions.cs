10a3d56af23901c77cfb59a5c7b98e11729805d0
Tim Bussmann <timbussmann@users.noreply.github.com>
2017-05-16 21:01:32 +0200

    Remove incorrect default value in method summary (#4714)
    * remove incorrect default value in method summary
    
    * Wording tweaks

--- src/NServiceBus.Core/DelayedDelivery/TimeoutManager/ConfigurationTimeoutExtensions.cs
+++ src/NServiceBus.Core/DelayedDelivery/TimeoutManager/ConfigurationTimeoutExtensions.cs
@@ -8,12 +8,10 @@
     public static class ConfigurationTimeoutExtensions
     {
         /// <summary>
-        /// A critical error is raised when timeout retrieval fails.
-        /// By default we wait for 2 seconds for the storage to come back.
-        /// This method allows to change the default and extend the wait time.
+        /// Configures the amount of time to wait after a timeout retrieval fails before triggering a critical error.
         /// </summary>
         /// <param name="config">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
-        /// <param name="timeToWait">Time to wait before raising a critical error.</param>
+        /// <param name="timeToWait">The amount of time to wait before triggering a critical error.</param>
         public static void TimeToWaitBeforeTriggeringCriticalErrorOnTimeoutOutages(this EndpointConfiguration config, TimeSpan timeToWait)
         {
             Guard.AgainstNull(nameof(config), config);
