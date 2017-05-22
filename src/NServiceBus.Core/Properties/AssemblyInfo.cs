1cb4a938058f19f7cced89f380186eb565c9f7dd
Brandon Ording <bording@gmail.com>
2017-05-22 16:51:57 -0400

    Update NServiceBus.Core

--- src/NServiceBus.Core/Properties/AssemblyInfo.cs
+++ src/NServiceBus.Core/Properties/AssemblyInfo.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NServiceBus.Core")]
-[assembly: AssemblyCopyright("Copyright NServiceBus Ltd. All rights reserved")]
-[assembly: AssemblyProduct("NServiceBus.Core")]
-[assembly: AssemblyCompany("NServiceBus Ltd.")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
