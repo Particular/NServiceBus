8c8752f0e5ed5fe8c0e746116b05e3db515caff0
Brandon Ording <bording@gmail.com>
2017-05-24 03:46:04 -0400

    New project system (#4729)

--- src/NServiceBus.AcceptanceTesting/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("NServiceBus.AcceptanceTesting")]
-[assembly: AssemblyProduct("NServiceBus.AcceptanceTesting")]
-[assembly: AssemblyCopyright("Copyright 2010-2014 NServiceBus. All rights reserved")]
