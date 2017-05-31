ff2be18089b6484db280d44ead064c08aff75947
Andreas Öhlund <andreas.ohlund@particular.net>
2017-05-31 20:22:22 +0200

    Expose audit expiration setting (#4764)
    * Expose audit expiration
    
    Since the external msmq transport needs it
    
    * Tweak comments
    
    * More tweaks
    
    * Fix typo

--- src/NServiceBus.Core.Tests/Audit/AuditConfigReaderTests.cs
+++ src/NServiceBus.Core.Tests/Audit/AuditConfigReaderTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace NServiceBus.Core.Tests.Audit
 {
+    using System;
     using NUnit.Framework;
     using Settings;
 
@@ -8,16 +9,37 @@
         [Test]
         public void ShouldUseExplicitValueInSettingsIfPresent()
         {
-            string address;
             var settingsHolder = new SettingsHolder();
+            var configuredAddress = "myAuditQueue";
 
             settingsHolder.Set<AuditConfigReader.Result>(new AuditConfigReader.Result
             {
-                Address = "myAuditQueue"
+                Address = configuredAddress
             });
 
-            Assert.True(AuditConfigReader.TryGetAuditQueueAddress(settingsHolder, out address));
-            Assert.AreEqual("myAuditQueue", address);
+            Assert.True(AuditConfigReader.TryGetAuditQueueAddress(settingsHolder, out var address));
+            Assert.AreEqual(configuredAddress, address);
+        }
+
+        [Test]
+        public void ShouldReturnConfiguredExpiration()
+        {
+            var settingsHolder = new SettingsHolder();
+            var configuredExpiration = TimeSpan.FromSeconds(10);
+
+            settingsHolder.Set<AuditConfigReader.Result>(new AuditConfigReader.Result
+            {
+                TimeToBeReceived = configuredExpiration
+            });
+
+            Assert.True(AuditConfigReader.TryGetAuditMessageExpiration(settingsHolder, out var expiration));
+            Assert.AreEqual(configuredExpiration, expiration);
+        }
+
+        [Test]
+        public void ShouldReturnFalseIfNoExpirationIsConfigured()
+        {
+            Assert.False(AuditConfigReader.TryGetAuditMessageExpiration(new SettingsHolder(), out _));
         }
     }
 }
--- src/NServiceBus.Core/Audit/ConfigureAudit.cs
+++ src/NServiceBus.Core/Audit/ConfigureAudit.cs
@@ -8,11 +8,11 @@ namespace NServiceBus
     public static class ConfigureAudit
     {
         /// <summary>
-        /// Configure Audit settings.
+        /// Configures audit settings.
         /// </summary>
         /// <param name="config">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
         /// <param name="auditQueue">The name of the audit queue to use.</param>
-        /// <param name="timeToBeReceived">The custom TTR to use for messages sent to the audit queue.</param>
+        /// <param name="timeToBeReceived">The message expiration time span to use for messages sent to the audit queue.</param>
         public static void AuditProcessedMessagesTo(this EndpointConfiguration config, string auditQueue, TimeSpan? timeToBeReceived = null)
         {
             Guard.AgainstNull(nameof(config), config);
