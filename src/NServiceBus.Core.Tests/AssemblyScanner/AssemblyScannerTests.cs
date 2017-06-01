49462b1f33bcf768fa5f10e4c9643f16a8f5a36e
Simon Cropp <simon.cropp@gmail.com>
2017-06-01 12:56:19 +1000

    Make scanner handle circular references
    clean up test
    
    suppress test side effect
    
    fix circular assembly scanning
    
    Add instructions on how to recreate asms

--- src/NServiceBus.Core.Tests/AssemblyScanner/AssemblyScannerTests.cs
+++ src/NServiceBus.Core.Tests/AssemblyScanner/AssemblyScannerTests.cs
@@ -63,6 +63,22 @@
         }
 
         [Test]
+        public void ReferencesNServiceBus_circular()
+        {
+            var circularDirectory = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestDlls\circular");
+            var classLibB = Path.Combine(circularDirectory, "ClassLibraryB.dll");
+          
+            // Put ClassLibraryB.dll in CurrentDomain.BaseDirectory so it resolves correctly
+            var tempClassLibB = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ClassLibraryB.dll");
+
+            File.Copy(classLibB, tempClassLibB, true);
+
+            var classLibA = Path.Combine(circularDirectory, "ClassLibraryA.dll");
+         
+            Assert.IsFalse(AssemblyScanner.ReferencesNServiceBus(classLibA, new Dictionary<string, bool>()));
+        }
+
+        [Test]
         public void ReferencesNServiceBus_returns_false_for_no_reference()
         {
             var combine = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestDlls", "dotNet.dll");
