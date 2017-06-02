bcc9417a2863e966d64d63328ea2dacb9079b4c9
David Boike <david.boike@gmail.com>
2017-06-02 12:30:49 -0500

    Moar logging

--- src/NServiceBus.Core/Transports/Msmq/ReceiveOnlyNativeTransactionStrategy.cs
+++ src/NServiceBus.Core/Transports/Msmq/ReceiveOnlyNativeTransactionStrategy.cs
@@ -1,5 +1,6 @@
 ﻿namespace NServiceBus
 {
+    using NServiceBus.Logging;
     using System;
     using System.Collections.Generic;
     using System.Messaging;
@@ -19,10 +20,13 @@
 
             try
             {
+                log.Info("ReceiveMessage - using MSMQ transaction");
                 using (var msmqTransaction = new MessageQueueTransaction())
                 {
+                    log.Info("ReceiveMessage - msmqTransaction.Begin()");
                     msmqTransaction.Begin();
 
+                    log.Info("ReceiveMessage - TryReceive");
                     if (!TryReceive(msmqTransaction, out message))
                     {
                         return;
@@ -30,6 +34,7 @@
 
                     Dictionary<string, string> headers;
 
+                    log.Info("ReceiveMessage - TryExtractHeaders");
                     if (!TryExtractHeaders(message, out headers))
                     {
                         MovePoisonMessageToErrorQueue(message, IsQueuesTransactional ? MessageQueueTransactionType.Single : MessageQueueTransactionType.None);
@@ -38,29 +43,38 @@
                         return;
                     }
 
+                    log.Info("ReceiveMessage - Call ProcessMessage");
                     var shouldCommit = await ProcessMessage(message, headers).ConfigureAwait(false);
 
                     if (shouldCommit)
                     {
+                        log.Info("ReceiveMessage - committing MSMQ transaction");
                         msmqTransaction.Commit();
                         failureInfoStorage.ClearFailureInfoForMessage(message.Id);
                     }
                     else
                     {
+                        log.Info("ReceiveMessage - aborting MSMQ transaction");
                         msmqTransaction.Abort();
                     }
+                    log.Info("ReceiveMessage - disposing MsmqTransaction");
                 }
+                log.Info("ReceiveMessage - MsmqTransaction disposed");
             }
             // We'll only get here if Commit/Abort/Dispose throws which should be rare.
             // Note: If that happens the attempts counter will be inconsistent since the message might be picked up again before we can register the failure in the LRU cache.
             catch (Exception exception)
             {
+                log.Info($"ReceiveMessage - Caught {exception.GetType().FullName}: {exception.Message}");
                 if (message == null)
                 {
+                    log.Info("ReceiveMessage - No message, throwing");
                     throw;
                 }
 
+                log.Info($"ReceiveMessage - Exception Handler - Recording failure for MSMQ MsgId {message.Id}");
                 failureInfoStorage.RecordFailureInfoForMessage(message.Id, exception);
+                log.Info($"ReceiveMessage - Exception Handler - Failure recording complete for MSMQ MsgId {message.Id}");
             }
         }
 
@@ -70,7 +84,9 @@
 
             if (failureInfoStorage.TryGetFailureInfoForMessage(message.Id, out failureInfo))
             {
+                log.Info($"ProcessMessage - Have failure info for MsmqMsgId {message.Id}");
                 var errorHandleResult = await HandleError(message, headers, failureInfo.Exception, transportTransaction, failureInfo.NumberOfProcessingAttempts).ConfigureAwait(false);
+                log.Info($"ProcessMessage - HandleError complete, Result={errorHandleResult}");
 
                 if (errorHandleResult == ErrorHandleResult.Handled)
                 {
@@ -82,7 +98,9 @@
             {
                 using (var bodyStream = message.BodyStream)
                 {
+                    log.Info("ProcessMessage - calling ReceiveStrategy.TryProcessMessage()");
                     var shouldAbortMessageProcessing = await TryProcessMessage(message.Id, headers, bodyStream, transportTransaction).ConfigureAwait(false);
+                    log.Info("ProcessMessage - ReceiveStrategy.TryProcessMessage() complete");
 
                     if (shouldAbortMessageProcessing)
                     {
@@ -93,7 +111,9 @@
             }
             catch (Exception exception)
             {
+                log.Info($"ProcessMessage - Caught {exception.GetType().FullName}: {exception.Message} for MsmqMsgId {message.Id}");
                 failureInfoStorage.RecordFailureInfoForMessage(message.Id, exception);
+                log.Info($"ProcessMessage - Catch - recorded failure");
 
                 return false;
             }
@@ -101,5 +121,6 @@
 
         MsmqFailureInfoStorage failureInfoStorage;
         static TransportTransaction transportTransaction = new TransportTransaction();
+        static readonly ILog log = LogManager.GetLogger<ReceiveOnlyNativeTransactionStrategy>();
     }
 }
