0fc09f4e2d0e7d056bcb5b8ae6f90b40d7879216
Andreas Öhlund <andreas.ohlund@particular.net>
2017-06-15 21:30:10 +0200

    Enable parallel acceptance tests (#4796)
    * Enable parallel tests for core acceptance tests
    
    * Make core atts isolated per test

--- src/NServiceBus.AcceptanceTests/ConfigureEndpointLearningPersistence.cs
+++ src/NServiceBus.AcceptanceTests/ConfigureEndpointLearningPersistence.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.AcceptanceTesting.Support;
 using NServiceBus.Persistence;
+using NUnit.Framework;
 
 public class ConfigureEndpointLearningPersistence : IConfigureEndpointTestExecution
 {
     public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
     {
+        var testRunId = TestContext.CurrentContext.Test.ID;
+
         //can't use bin dir since that will be too long on the build agents
-        storageDir = Path.Combine(@"c:\temp", Guid.NewGuid().ToString("N"));
+        storageDir = Path.Combine(@"c:\temp", testRunId);
 
         configuration.UsePersistence<InMemoryPersistence, StorageType.Subscriptions>();
         configuration.UsePersistence<InMemoryPersistence, StorageType.Timeouts>();
