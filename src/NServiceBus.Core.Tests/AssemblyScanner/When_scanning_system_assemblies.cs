e0f6b630678400936c17c5d445393c75549da1d3
Tim Bussmann <tim.bussmann@particular.net>
2017-07-11 08:32:42 +0200

    merge .net core and .net framework specific tests

--- src/NServiceBus.Core.Tests/AssemblyScanner/When_scanning_system_assemblies.cs
+++ src/NServiceBus.Core.Tests/AssemblyScanner/When_scanning_system_assemblies.cs
@@ -8,7 +8,6 @@
     [TestFixture]
     public class When_scanning_system_assemblies
     {
-#if NET452
         [Test]
         public void Should_exclude_system_assemblies_on_net_framework()
         {
@@ -19,20 +18,6 @@
                 Assert.IsTrue(AssemblyScanner.IsRuntimeAssembly(assembly.GetName()), $"should exclude {assembly.FullName}");
             }
         }
-#endif
-
-#if NETCOREAPP2_0
-        [Test]
-        public void Should_exclude_system_assemblies_on_net_core()
-        {
-            var systemAssemblies = AppDomain.CurrentDomain.GetAssemblies().Where(a => a.FullName.Contains("System"));
-
-            foreach (var assembly in systemAssemblies)
-            {
-                Assert.IsTrue(AssemblyScanner.IsRuntimeAssembly(assembly.GetName()), $"should exclude {assembly.FullName}");
-            }
-        }
-#endif
 
         [Test]
         public void NServiceBus_core_should_be_included()
