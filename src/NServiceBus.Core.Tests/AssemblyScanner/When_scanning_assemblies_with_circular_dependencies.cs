1507cc2b6fc6a52448f429988abfc1b771151fb4
Brandon Ording <bording@gmail.com>
2017-07-12 18:52:54 -0400

    Simplify circular reference test
    Both assemblies are actually loaded during scanning now

--- src/NServiceBus.Core.Tests/AssemblyScanner/When_scanning_assemblies_with_circular_dependencies.cs
+++ src/NServiceBus.Core.Tests/AssemblyScanner/When_scanning_assemblies_with_circular_dependencies.cs
@@ -11,19 +11,6 @@
         [Test]
         public void ReferencesNServiceBus_circular()
         {
-            // Assemblies already exist in TestDlls/circular:
-            var circularDirectory = Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestDlls\circular");
-
-            var classLibA = Path.Combine(circularDirectory, "ClassLibraryA.dll");
-            var classLibB = Path.Combine(circularDirectory, "ClassLibraryB.dll");
-
-            // Put assemblies in TestContext.CurrentContext.TestDirectory so they resolve correctly
-            var tempClassLibA = Path.Combine(TestContext.CurrentContext.TestDirectory, "ClassLibraryA.dll");
-            var tempClassLibB = Path.Combine(TestContext.CurrentContext.TestDirectory, "ClassLibraryB.dll");
-
-            File.Copy(classLibA, tempClassLibA, true);
-            File.Copy(classLibB, tempClassLibB, true);
-
             var scanner = new AssemblyScanner(Path.Combine(TestContext.CurrentContext.TestDirectory, @"TestDlls\circular"));
             scanner.ScanAppDomainAssemblies = false;
 
