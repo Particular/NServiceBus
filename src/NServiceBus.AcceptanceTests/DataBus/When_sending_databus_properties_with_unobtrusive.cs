6aeb03ac7bfa11058b0f3b916769751886a71a5d
danielmarbach <daniel.marbach@openplace.net>
2017-07-12 14:10:44 +0200

    RegisterMutator

--- src/NServiceBus.AcceptanceTests/DataBus/When_sending_databus_properties_with_unobtrusive.cs
+++ src/NServiceBus.AcceptanceTests/DataBus/When_sending_databus_properties_with_unobtrusive.cs
@@ -65,7 +65,7 @@
 
                     var basePath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"databus\sender");
                     builder.UseDataBus<FileShareDataBus>().BasePath(basePath);
-                    builder.RegisterComponents(c => c.ConfigureComponent<Mutator>(DependencyLifecycle.InstancePerCall));
+                    builder.RegisterMessageMutator(new Mutator());
                 });
             }
 
