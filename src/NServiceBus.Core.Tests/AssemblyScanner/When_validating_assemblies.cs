8871cb0d42b8b4e496a56f61a7178da81d4867ff
Brandon Ording <bording@gmail.com>
2017-07-14 20:06:57 -0400

    Validate assembly file before attempting to load it

---
+++ src/NServiceBus.Core.Tests/AssemblyScanner/When_validating_assemblies.cs
@@ -0,0 +1,47 @@
+﻿namespace NServiceBus.Core.Tests.AssemblyScanner
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_validating_assemblies
+    {
+        [Test]
+        public void Should_not_validate_system_assemblies()
+        {
+            var validator = new AssemblyValidator();
+            var systemAssemblies = AppDomain.CurrentDomain.GetAssemblies().Where(a => a.FullName.Contains("System"));
+
+            foreach (var assembly in systemAssemblies)
+            {
+                var (shouldLoad, reason) = validator.ValidateAssemblyFile(assembly.Location);
+
+                Assert.IsFalse(shouldLoad, $"Should not validate {assembly.FullName}");
+                Assert.That(reason == "File is a .NET runtime assembly.");
+            }
+        }
+
+        [Test]
+        public void Should_validate_NServiceBus_Core_assembly()
+        {
+            var validator = new AssemblyValidator();
+
+            var (shouldLoad, reason) = validator.ValidateAssemblyFile(typeof(EndpointConfiguration).Assembly.Location);
+
+            Assert.IsTrue(shouldLoad);
+            Assert.That(reason == "File is a .NET assembly.");
+        }
+
+        [Test]
+        public void Should_validate_non_system_assemblies()
+        {
+            var validator = new AssemblyValidator();
+
+            var (shouldLoad, reason) = validator.ValidateAssemblyFile(GetType().Assembly.Location);
+
+            Assert.IsTrue(shouldLoad);
+            Assert.That(reason == "File is a .NET assembly.");
+        }
+    }
+}
---
+++ src/NServiceBus.Core/Hosting/Helpers/AssemblyValidator.cs
@@ -0,0 +1,96 @@
+﻿namespace NServiceBus
+{
+    using System;
+    using System.IO;
+    using System.Reflection.Metadata;
+    using System.Reflection.PortableExecutable;
+    using System.Security.Cryptography;
+
+    class AssemblyValidator
+    {
+        public (bool shouldLoad, string reason) ValidateAssemblyFile(string assemblyPath)
+        {
+            using (var stream = File.OpenRead(assemblyPath))
+            using (var file = new PEReader(stream))
+            {
+                var hasMetadata = false;
+
+                try
+                {
+                    hasMetadata = file.HasMetadata;
+                }
+                catch (BadImageFormatException) { }
+
+                if (!hasMetadata)
+                {
+                    return (false, "File is not a .NET assembly.");
+                }
+
+                var reader = file.GetMetadataReader();
+                var assemblyDefinition = reader.GetAssemblyDefinition();
+
+                if (!assemblyDefinition.PublicKey.IsNil)
+                {
+                    var publicKey = reader.GetBlobBytes(assemblyDefinition.PublicKey);
+                    var publicKeyToken = GetPublicKeyToken(publicKey);
+
+                    if (IsRuntimeAssembly(publicKeyToken))
+                    {
+                        return (false, "File is a .NET runtime assembly.");
+                    }
+                }
+
+                return (true, "File is a .NET assembly.");
+            }
+        }
+
+        public static bool IsRuntimeAssembly(byte[] publicKeyToken)
+        {
+            var tokenString = BitConverter.ToString(publicKeyToken).Replace("-", string.Empty).ToLowerInvariant();
+
+            //Compare token to known Microsoft tokens
+
+            if (tokenString == "b77a5c561934e089")
+            {
+                return true;
+            }
+
+            if (tokenString == "7cec85d7bea7798e")
+            {
+                return true;
+            }
+
+            if (tokenString == "b03f5f7f11d50a3a")
+            {
+                return true;
+            }
+
+            if (tokenString == "31bf3856ad364e35")
+            {
+                return true;
+            }
+
+            if (tokenString == "cc7b13ffcd2ddd51")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        byte[] GetPublicKeyToken(byte[] publicKey)
+        {
+            var hash = provider.ComputeHash(publicKey);
+            var publicKeyToken = new byte[8];
+
+            for (var i = 0; i < 8; i++)
+            {
+                publicKeyToken[i] = hash[hash.Length - (i + 1)];
+            }
+
+            return publicKeyToken;
+        }
+
+        SHA1CryptoServiceProvider provider = new SHA1CryptoServiceProvider();
+    }
+}
