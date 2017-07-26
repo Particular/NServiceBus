375d346dde951e9da54937fcba54e194313eea85
Brandon Ording <bording@gmail.com>
2017-07-26 07:21:44 -0400

    Simplify AssemblyValidator by using AssemblyName (#4870)

--- src/NServiceBus.Core/Hosting/Helpers/AssemblyValidator.cs
+++ src/NServiceBus.Core/Hosting/Helpers/AssemblyValidator.cs
@@ -1,47 +1,27 @@
 ﻿namespace NServiceBus
 {
     using System;
-    using System.IO;
-    using System.Reflection.Metadata;
-    using System.Reflection.PortableExecutable;
-    using System.Security.Cryptography;
+    using System.Reflection;
 
     class AssemblyValidator
     {
         public (bool shouldLoad, string reason) ValidateAssemblyFile(string assemblyPath)
         {
-            using (var stream = File.OpenRead(assemblyPath))
-            using (var file = new PEReader(stream))
+            try
             {
-                var hasMetadata = false;
+                var token = AssemblyName.GetAssemblyName(assemblyPath).GetPublicKeyToken();
 
-                try
+                if (IsRuntimeAssembly(token))
                 {
-                    hasMetadata = file.HasMetadata;
+                    return (false, "File is a .NET runtime assembly.");
                 }
-                catch (BadImageFormatException) { }
-
-                if (!hasMetadata)
-                {
-                    return (false, "File is not a .NET assembly.");
-                }
-
-                var reader = file.GetMetadataReader();
-                var assemblyDefinition = reader.GetAssemblyDefinition();
-
-                if (!assemblyDefinition.PublicKey.IsNil)
-                {
-                    var publicKey = reader.GetBlobBytes(assemblyDefinition.PublicKey);
-                    var publicKeyToken = GetPublicKeyToken(publicKey);
-
-                    if (IsRuntimeAssembly(publicKeyToken))
-                    {
-                        return (false, "File is a .NET runtime assembly.");
-                    }
-                }
-
-                return (true, "File is a .NET assembly.");
             }
+            catch (BadImageFormatException)
+            {
+                return (false, "File is not a .NET assembly.");
+            }
+
+            return (true, "File is a .NET assembly.");
         }
 
         public static bool IsRuntimeAssembly(byte[] publicKeyToken)
@@ -77,21 +57,5 @@
 
             return false;
         }
-
-        byte[] GetPublicKeyToken(byte[] publicKey)
-        {
-            var hash = provider.ComputeHash(publicKey);
-            var publicKeyToken = new byte[8];
-
-            for (var i = 0; i < 8; i++)
-            {
-                publicKeyToken[i] = hash[hash.Length - (i + 1)];
-            }
-
-            return publicKeyToken;
-        }
-#pragma warning disable PC001
-        SHA1CryptoServiceProvider provider = new SHA1CryptoServiceProvider();
-#pragma warning restore PC001
     }
 }
