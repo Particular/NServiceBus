a6cd78b16ba8b5872eb7a4ed696d42cdae119da3
Brandon Ording <bording@gmail.com>
2017-08-10 08:36:23 -0400

    Adjust license source ordering (#4895)
    * Update Particular.Licensing.Sources to 1.0.0-alpha0004
    
    * Adjust license source order to prefer API over file locations

--- src/NServiceBus.Core/Licensing/LicenseSources.cs
+++ src/NServiceBus.Core/Licensing/LicenseSources.cs
@@ -1,12 +1,13 @@
 namespace NServiceBus
 {
+    using System.Collections.Generic;
     using Particular.Licensing;
 
     static class LicenseSources
     {
         public static LicenseSource[] GetLicenseSources(string licenseText, string licenseFilePath)
         {
-            var sources = LicenseSource.GetStandardLicenseSources();
+            var sources = new List<LicenseSource>();
 
             if (licenseText != null)
             {
@@ -17,6 +18,10 @@ namespace NServiceBus
             {
                 sources.Add(new LicenseSourceFilePath(licenseFilePath));
             }
+
+            var standardSources = LicenseSource.GetStandardLicenseSources();
+
+            sources.AddRange(standardSources);
 
 #if APPCONFIGLICENSESOURCE
             sources.Add(new LicenseSourceAppConfigLicenseSetting());
