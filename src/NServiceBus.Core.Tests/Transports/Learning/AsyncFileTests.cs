ed7264531ca4a7ef1d14d2030c673f876b4497e6
Tim <bussmann.t@gmail.com>
2017-08-18 16:22:05 +0200

    undo AsyncFileTests

--- src/NServiceBus.Core.Tests/Transports/Learning/AsyncFileTests.cs
+++ src/NServiceBus.Core.Tests/Transports/Learning/AsyncFileTests.cs
@@ -11,7 +11,7 @@
         public async Task When_file_content_larger_than_buffer_size()
         {
             var originalContent = string.Join("", Enumerable.Repeat("a#~×ψؾࢯ‽%1", 2000));
-            var filePath = Path.Combine(TestContext.CurrentContext.TestDirectory, "test.txt");
+            const string filePath = "test.txt";
 
             try
             {
