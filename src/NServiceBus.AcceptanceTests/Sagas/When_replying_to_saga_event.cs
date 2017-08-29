52f190e61485900ecc4c500381cdc4a77c1b0e29
Donald Belcham <donald.belcham@particular.net>
2017-08-29 09:33:41 -0300

    Rename test and cleanup content

--- src/NServiceBus.AcceptanceTests/Sagas/When_replying_to_saga_event.cs
+++ src/NServiceBus.AcceptanceTests/Sagas/When_replying_to_saga_event.cs
@@ -7,35 +7,36 @@
     using Features;
     using NUnit.Framework;
 
-    public class When_replies_to_message_published_by_a_saga : NServiceBusAcceptanceTest
+    public class When_replying_to_saga_event : NServiceBusAcceptanceTest
     {
         [Test]
-        public async Task Should_reply_to_a_message_published_by_a_saga()
+        public async Task Should_correlate_reply_to_publishing_saga_instance()
         {
             var context = await Scenario.Define<Context>()
-                .WithEndpoint<SagaEndpoint>
-                (b => b.When(c => c.Subscribed, session => session.SendLocal(new StartSaga
-                {
-                    DataId = Guid.NewGuid()
-                }))
-                )
-                .WithEndpoint<ReplyEndpoint>(b => b.When(async (session, c) =>
-                {
-                    await session.Subscribe<DidSomething>();
-                    if (c.HasNativePubSubSupport)
+                .WithEndpoint<SagaEndpoint>(b => b
+                    .When(c => c.Subscribed, session => session.SendLocal(new StartSaga
                     {
-                        c.Subscribed = true;
-                    }
-                }))
-                .Done(c => c.DidSagaReplyMessageGetCorrelated)
+                        DataId = Guid.NewGuid()
+                    }))
+                )
+                .WithEndpoint<ReplyEndpoint>(b => b
+                    .When(async (session, c) =>
+                    {
+                        await session.Subscribe<DidSomething>();
+                        if (c.HasNativePubSubSupport)
+                        {
+                            c.Subscribed = true;
+                        }
+                    }))
+                .Done(c => c.CorrelatedResponseReceived)
                 .Run();
 
-            Assert.True(context.DidSagaReplyMessageGetCorrelated);
+            Assert.True(context.CorrelatedResponseReceived);
         }
 
         public class Context : ScenarioContext
         {
-            public bool DidSagaReplyMessageGetCorrelated { get; set; }
+            public bool CorrelatedResponseReceived { get; set; }
             public bool Subscribed { get; set; }
         }
 
@@ -75,7 +76,6 @@
 
                 public Task Handle(StartSaga message, IMessageHandlerContext context)
                 {
-                    Data.DataId = message.DataId;
                     return context.Publish(new DidSomething
                     {
                         DataId = message.DataId
@@ -84,8 +84,7 @@
 
                 public Task Handle(DidSomethingResponse message, IMessageHandlerContext context)
                 {
-                    Context.DidSagaReplyMessageGetCorrelated = message.ReceivedDataId == Data.DataId;
-                    MarkAsComplete();
+                    Context.CorrelatedResponseReceived = message.ReceivedDataId == Data.DataId;
                     return Task.FromResult(0);
                 }
 
---
+++ src/NServiceBus.Core/App_Packages/Particular.Licensing/ReleaseDateAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Particular.Licensing
+{
+    using System;
+
+    sealed class ReleaseDateAttribute : Attribute
+    {
+        public ReleaseDateAttribute(string originalDate, string date)
+        {
+            OriginalDate = originalDate;
+            Date = date;
+        }
+
+        public string OriginalDate { get; }
+        public string Date { get; }
+    }
+}
