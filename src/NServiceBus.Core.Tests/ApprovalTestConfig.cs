6a0c43c2bb2b922f2a0f545f768a6d452489d24e
Simon Cropp <simon.cropp@gmail.com>
2017-09-15 08:39:35 +1000

    remove release filter on reporter config (#4953)

--- src/NServiceBus.Core.Tests/ApprovalTestConfig.cs
+++ src/NServiceBus.Core.Tests/ApprovalTestConfig.cs
@@ -1,6 +1,2 @@
 ﻿using ApprovalTests.Reporters;
-#if(DEBUG)
 [assembly: UseReporter(typeof(DiffReporter), typeof(AllFailingTestsClipboardReporter))]
-#else
-[assembly: UseReporter(typeof(DiffReporter))]
-#endif
