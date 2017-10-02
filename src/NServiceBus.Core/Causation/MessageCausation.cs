cc76fd7676005e35f71304834de29e5afd6ec851
Simon Cropp <simon.cropp@gmail.com>
2017-10-03 08:55:52 +1100

    throw better error when id generation fails

--- src/NServiceBus.Core/Causation/MessageCausation.cs
+++ src/NServiceBus.Core/Causation/MessageCausation.cs
@@ -11,12 +11,29 @@
 
         protected internal override void Setup(FeatureConfigurationContext context)
         {
-            if (!context.Settings.TryGet<Func<ConversationIdGeneratorContext, string>>(MessageCausationConfigurationExtensions.CustomConversationIdGeneratorKey, out var idGenerator))
-            {
-                idGenerator = _ => CombGuid.Generate().ToString();
-            }
+            var newIdGenerator = GetIdGenerator(context);
 
-            context.Pipeline.Register("AttachCausationHeaders", new AttachCausationHeadersBehavior(idGenerator), "Adds related to and conversation id headers to outgoing messages");
+            context.Pipeline.Register("AttachCausationHeaders", new AttachCausationHeadersBehavior(newIdGenerator), "Adds related to and conversation id headers to outgoing messages");
+        }
+
+        static Func<ConversationIdGeneratorContext, string> GetIdGenerator(FeatureConfigurationContext context)
+        {
+            var settings = context.Settings;
+            if (settings.TryGet<Func<ConversationIdGeneratorContext, string>>(MessageCausationConfigurationExtensions.CustomConversationIdGeneratorKey, out var idGenerator))
+            {
+                return generatorContext =>
+                {
+                    try
+                    {
+                        return idGenerator(generatorContext);
+                    }
+                    catch (Exception exception)
+                    {
+                        throw new Exception($"Failed to execute CustomConversationIdGenerator. This configuration option was defined using {nameof(EndpointConfiguration)}.{nameof(MessageCausationConfigurationExtensions.CustomConversationIdGenerator)}.", exception);
+                    }
+                };
+            }
+            return _ => CombGuid.Generate().ToString();
         }
     }
 }
