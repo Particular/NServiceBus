b39e5310a4656c2f6fcaa43ba62e6167a58b7ca4
Tim Bussmann <timbussmann@users.noreply.github.com>
2017-10-12 13:59:36 +0200

    add audit diagnostics section (#5022)

--- src/NServiceBus.Core/Audit/Audit.cs
+++ src/NServiceBus.Core/Audit/Audit.cs
@@ -29,6 +29,12 @@
             context.Pipeline.Register("AuditProcessedMessage", new InvokeAuditPipelineBehavior(auditConfig.Address), "Execute the audit pipeline");
 
             context.Settings.Get<QueueBindings>().BindSending(auditConfig.Address);
+
+            context.Settings.AddStartupDiagnosticsSection("Audit", new
+            {
+                AuditQueue = auditConfig.Address,
+                AuditTTBR = auditConfig.TimeToBeReceived?.ToString("g") ?? "-"
+            });
         }
     }
 }
