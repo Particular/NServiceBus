1e37860bf132d9c0e86b015458fc0f4c350d2601
Andreas Öhlund <andreas.ohlund@particular.net>
2017-10-19 08:01:32 +0200

    Add more hosting/environment related diagnostics (#5035)
    * Add more hosting/environment related diagnostics
    
    * Remove using
    
    * Better name

--- src/NServiceBus.Core/Hosting/HostInformationFeature.cs
+++ src/NServiceBus.Core/Hosting/HostInformationFeature.cs
@@ -3,7 +3,10 @@
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Net;
+    using System.Runtime;
     using Hosting;
+    using Settings;
     using Support;
 
     class HostInformationFeature : Feature
@@ -38,13 +41,33 @@
                 context.Settings.Get<string>("NServiceBus.HostInformation.DisplayName"),
                 context.Settings.Get<Dictionary<string, string>>("NServiceBus.HostInformation.Properties"));
 
-            context.Settings.AddStartupDiagnosticsSection("HostInformation", hostInformation);
-
             context.Container.ConfigureComponent(() => hostInformation, DependencyLifecycle.SingleInstance);
 
             var endpointName = context.Settings.EndpointName();
             context.Pipeline.Register("AuditHostInformation", new AuditHostInformationBehavior(hostInformation, endpointName), "Adds audit host information");
             context.Pipeline.Register("AddHostInfoHeaders", new AddHostInfoHeadersBehavior(hostInformation, endpointName), "Adds host info headers to outgoing headers");
+
+            AddDiagnostics(context.Settings, hostInformation);
+        }
+
+        static void AddDiagnostics(ReadOnlySettings settings, HostInformation hostInformation)
+        {
+            settings.AddStartupDiagnosticsSection("Hosting", new
+            {
+                hostInformation.HostId,
+                HostDisplayName = hostInformation.DisplayName,
+                RuntimeEnvironment.MachineName,
+                GCSettings.IsServerGC,
+                GCLatencyMode = GCSettings.LatencyMode,
+                Environment.ProcessorCount,
+                Environment.Is64BitProcess,
+                CLRVersion = Environment.Version,
+                Environment.WorkingSet,
+                Environment.SystemPageSize,
+                HostName = Dns.GetHostName(),
+                Environment.UserName,
+                PathToExe = PathUtilities.SanitizedPath(Environment.CommandLine)
+            });
         }
 
         internal const string HostIdSettingsKey = "NServiceBus.HostInformation.HostId";
