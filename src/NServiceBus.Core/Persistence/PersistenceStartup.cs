ceac08e8c7441dab4add154afd5ec4cd576c7836
Andreas Öhlund <andreas.ohlund@particular.net>
2017-10-19 08:01:05 +0200

    Emit persistence diagnostics (#5033)
    * Emit persistence diagnostics
    
    * Order entries by name ASC
    
    * Remove using
    
    * Formatting

--- src/NServiceBus.Core/Persistence/PersistenceStartup.cs
+++ src/NServiceBus.Core/Persistence/PersistenceStartup.cs
@@ -18,6 +18,7 @@
             var enabledPersistences = PersistenceStorageMerger.Merge(definitions, settings);
 
             var resultingSupportedStorages = new List<Type>();
+            var diagnostics = new Dictionary<string, object>();
 
             foreach (var definition in enabledPersistences)
             {
@@ -30,10 +31,18 @@
                     Logger.DebugFormat("Activating persistence '{0}' to provide storage for '{1}' storage.", definition.DefinitionType.Name, storageType);
                     persistenceDefinition.ApplyActionForStorage(storageType, settings);
                     resultingSupportedStorages.Add(storageType);
+
+                    diagnostics.Add(storageType.Name, new
+                    {
+                        Type = definition.DefinitionType.FullName,
+                        Version = FileVersionRetriever.GetFileVersion(definition.DefinitionType)
+                    });
                 }
             }
 
             settings.Set("ResultingSupportedStorages", resultingSupportedStorages);
+
+            settings.AddStartupDiagnosticsSection("Persistence", diagnostics);
         }
 
         internal static bool HasSupportFor<T>(ReadOnlySettings settings) where T : StorageType
