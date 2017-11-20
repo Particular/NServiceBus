c9f3507a89340adeb6c34160e92af8a8b4f7ef03
Brandon Ording <bording@gmail.com>
2017-11-20 17:53:09 -0500

    Create separate approval files for net452 and netstandard2.0

--- src/NServiceBus.Core.Tests/API/APIApprovals.cs
+++ src/NServiceBus.Core.Tests/API/APIApprovals.cs
@@ -9,13 +9,25 @@
     [TestFixture]
     public class APIApprovals
     {
+#if NET452
         [Test]
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public void ApproveNServiceBus()
+        public void ApproveNServiceBus__NET452()
         {
             var publicApi = Filter(ApiGenerator.GeneratePublicApi(typeof(Endpoint).Assembly));
             TestApprover.Verify(publicApi);
         }
+#endif
+
+#if NETCOREAPP2_0
+        [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void ApproveNServiceBus__NETSTANDARD2_0()
+        {
+            var publicApi = Filter(ApiGenerator.GeneratePublicApi(typeof(Endpoint).Assembly));
+            TestApprover.Verify(publicApi);
+        }
+#endif
 
         string Filter(string text)
         {
@@ -24,7 +36,6 @@
                 Environment.NewLine
             }, StringSplitOptions.RemoveEmptyEntries)
                 .Where(l => !l.StartsWith("[assembly: ReleaseDateAttribute("))
-                .Where(l => !l.StartsWith(@"[assembly: System.Runtime.Versioning.TargetFrameworkAttribute("))
                 .Where(l => !string.IsNullOrWhiteSpace(l))
                 );
         }
