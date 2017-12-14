b14505d8a3ccd14b101afe9d416960f637ef0b6c
Tim <bussmann.t@gmail.com>
2017-12-14 15:45:06 +0100

    add inmemory persistence tests

---
+++ src/NServiceBus.InMemoryPersistence.AcceptanceTests/ConfigureAcceptanceTestTransport.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.Transport;
+using NUnit.Framework;
+
+public class ConfigureAcceptanceTestTransport : IConfigureEndpointTestExecution
+{
+    public Task Cleanup()
+    {
+        if (Directory.Exists(storageDir))
+        {
+            Directory.Delete(storageDir, true);
+        }
+
+        return Task.FromResult(0);
+    }
+
+    public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
+    {
+        var testRunId = TestContext.CurrentContext.Test.ID;
+
+        string tempDir;
+
+        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+        {
+            //can't use bin dir since that will be too long on the build agents
+            tempDir = @"c:\temp";
+        }
+        else
+        {
+            tempDir = Path.GetTempPath();
+        }
+
+        storageDir = Path.Combine(tempDir, testRunId);
+
+        //we want the tests to be exposed to concurrency
+        configuration.LimitMessageProcessingConcurrencyTo(PushRuntimeSettings.Default.MaxConcurrency);
+
+        var transportConfig = configuration.UseTransport<AcceptanceTestingTransport>()
+            .StorageDirectory(storageDir)
+            .UseNativePubSub(false)
+            .UseNativeDelayedDelivery(false);
+
+        var routingConfig = transportConfig.Routing();
+
+        foreach (var publisherMetadataPublisher in publisherMetadata.Publishers)
+        {
+            foreach (var @event in publisherMetadataPublisher.Events)
+            {
+                routingConfig.RegisterPublisher(@event, publisherMetadataPublisher.PublisherName);
+            }
+        }
+
+        return Task.FromResult(0);
+    }
+
+    string storageDir;
+}
---
+++ src/NServiceBus.InMemoryPersistence.AcceptanceTests/InMemoryAcceptanceTestSuiteConstraints.cs
@@ -0,0 +1,15 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using AcceptanceTesting.Support;
+
+    public partial class TestSuiteConstraints
+    {
+        public bool SupportsDtc => false;
+        public bool SupportsCrossQueueTransactions => true;
+        public bool SupportsNativePubSub => false;
+        public bool SupportsNativeDeferral => false;
+        public bool SupportsOutbox => false;
+        public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointLearningTransport();
+        public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointLearningPersistence();
+    }
+}
