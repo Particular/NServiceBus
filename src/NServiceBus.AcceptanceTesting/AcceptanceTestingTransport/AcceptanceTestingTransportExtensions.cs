506f7437d7e239611fed28e05e72ae4c22402b90
Tim <bussmann.t@gmail.com>
2017-12-14 09:17:01 +0100

    Add a configurable acceptance testing transport

---
+++ src/NServiceBus.AcceptanceTesting/AcceptanceTestingTransport/AcceptanceTestingTransportExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace NServiceBus
+{
+    public static class AcceptanceTestingTransportExtensions
+    {
+        public static TransportExtensions<AcceptanceTestingTransport> UseNativeDelayedDelivery(this TransportExtensions<AcceptanceTestingTransport> config, bool useNativeDelayedDelivery = true)
+        {
+            config.Settings.Set("AcceptanceTestingTransport.UseNativeDelayedDelivery", useNativeDelayedDelivery);
+            return config;
+        }
+
+        public static TransportExtensions<AcceptanceTestingTransport> UseNativePubSub(this TransportExtensions<AcceptanceTestingTransport> config, bool useNativePubSub = true)
+        {
+            config.Settings.Set("AcceptanceTestingTransport.UseNativePubSub", useNativePubSub);
+            return config;
+        }
+
+        public static TransportExtensions<AcceptanceTestingTransport> StorageDirectory(this TransportExtensions<AcceptanceTestingTransport> transportExtensions, string path)
+        {
+            Guard.AgainstNullAndEmpty(nameof(path), path);
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
+            PathChecker.ThrowForBadPath(path, "StorageDirectory");
+
+            transportExtensions.Settings.Set(LearningTransportInfrastructure.StorageLocationKey, path);
+            return transportExtensions;
+        }
+    }
+}
