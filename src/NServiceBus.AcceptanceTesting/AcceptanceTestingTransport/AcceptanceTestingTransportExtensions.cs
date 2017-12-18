be077dd4cea7b291874b54096d7798d519058d02
Brandon Ording <bording@gmail.com>
2017-12-18 17:30:32 -0500

    Clean up configuration extensions

--- src/NServiceBus.AcceptanceTesting/AcceptanceTestingTransport/AcceptanceTestingTransportExtensions.cs
+++ src/NServiceBus.AcceptanceTesting/AcceptanceTestingTransport/AcceptanceTestingTransportExtensions.cs
@@ -4,22 +4,26 @@
 
     public static class AcceptanceTestingTransportExtensions
     {
-        public static TransportExtensions<AcceptanceTestingTransport> UseNativeDelayedDelivery(this TransportExtensions<AcceptanceTestingTransport> config, bool useNativeDelayedDelivery)
+        public static TransportExtensions<AcceptanceTestingTransport> UseNativeDelayedDelivery(this TransportExtensions<AcceptanceTestingTransport> transportExtensions, bool useNativeDelayedDelivery)
         {
-            config.Settings.Set(AcceptanceTestingTransportInfrastructure.UseNativeDelayedDeliveryKey, useNativeDelayedDelivery);
-            return config;
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
+
+            transportExtensions.Settings.Set(AcceptanceTestingTransportInfrastructure.UseNativeDelayedDeliveryKey, useNativeDelayedDelivery);
+            return transportExtensions;
         }
 
-        public static TransportExtensions<AcceptanceTestingTransport> UseNativePubSub(this TransportExtensions<AcceptanceTestingTransport> config, bool useNativePubSub)
+        public static TransportExtensions<AcceptanceTestingTransport> UseNativePubSub(this TransportExtensions<AcceptanceTestingTransport> transportExtensions, bool useNativePubSub)
         {
-            config.Settings.Set(AcceptanceTestingTransportInfrastructure.UseNativePubSubKey, useNativePubSub);
-            return config;
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
+
+            transportExtensions.Settings.Set(AcceptanceTestingTransportInfrastructure.UseNativePubSubKey, useNativePubSub);
+            return transportExtensions;
         }
 
         public static TransportExtensions<AcceptanceTestingTransport> StorageDirectory(this TransportExtensions<AcceptanceTestingTransport> transportExtensions, string path)
         {
-            Guard.AgainstNullAndEmpty(nameof(path), path);
             Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
+            Guard.AgainstNullAndEmpty(nameof(path), path);
             PathChecker.ThrowForBadPath(path, "StorageDirectory");
 
             transportExtensions.Settings.Set(AcceptanceTestingTransportInfrastructure.StorageLocationKey, path);
