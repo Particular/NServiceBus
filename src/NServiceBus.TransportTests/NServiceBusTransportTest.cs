701694886d39c411c34a8509513498b46d919ac6
Tim <bussmann.t@gmail.com>
2018-01-24 14:31:23 +0100

    prevent NRE from reusing tests

--- src/NServiceBus.TransportTests/NServiceBusTransportTest.cs
+++ src/NServiceBus.TransportTests/NServiceBusTransportTest.cs
@@ -25,6 +25,7 @@
 
             //when using [TestCase] NUnit will reuse the same test instance so we need to make sure that the message pump is a fresh one
             MessagePump = null;
+            testCancellationTokenSource = null;
         }
 
         static IConfigureTransportInfrastructure CreateConfigurer()
