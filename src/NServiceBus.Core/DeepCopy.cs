ff335a501e6a166080a57d43169448547b94935d
andreasohlund <andreas.ohlund@particular.net>
2018-03-28 09:30:28 +0200

    Exclude deep copy from standards check

--- src/NServiceBus.Core/DeepCopy.cs
+++ src/NServiceBus.Core/DeepCopy.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections.Generic;
+﻿/* This file is copied from https://github.com/Burtsev-Alexey/net-object-deep-copy
+Source code is released under the MIT license.
+
+The MIT License(MIT)
+Copyright(c) 2014 Burtsev Alexey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+using System.Collections.Generic;
 using System.Reflection;
 using System.ArrayExtensions;
 // ReSharper disable SuggestVarOrType_SimpleTypes
@@ -6,6 +16,7 @@ using System.ArrayExtensions;
 
 namespace System
 {
+
     static class ObjectExtensions
     {
         private static readonly MethodInfo CloneMethod = typeof(Object).GetMethod("MemberwiseClone", BindingFlags.NonPublic | BindingFlags.Instance);
@@ -129,5 +140,4 @@ namespace System
             }
         }
     }
-
 }
