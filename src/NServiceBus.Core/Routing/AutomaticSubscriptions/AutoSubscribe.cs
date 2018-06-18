32a2f067ef62f9b25da95a39a90100a0e2faba37
Mauro Servienti <mauro.servienti@gmail.com>
2018-06-18 13:47:22 +0200

    better exception message

--- src/NServiceBus.Core/Routing/AutomaticSubscriptions/AutoSubscribe.cs
+++ src/NServiceBus.Core/Routing/AutomaticSubscriptions/AutoSubscribe.cs
@@ -93,7 +93,7 @@
                 }
                 catch (Exception e) when (!(e is QueueNotFoundException))
                 {
-                    Logger.Warn($"AutoSubscribe was unable to subscribe to event '{eventType.FullName}'.", e);
+                    Logger.Warn($"AutoSubscribe was unable to subscribe to event '{eventType.FullName}': {e.Message}");
                     // swallow exception
                 }
             }
