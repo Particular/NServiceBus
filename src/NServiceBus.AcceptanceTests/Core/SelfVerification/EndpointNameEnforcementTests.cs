0ce2905a0caa226dcd8db81cf30b830751c45434
Sean Feldman <SeanFeldman@users.noreply.github.com>
2018-08-16 00:59:41 -0600

    Queue length check (#5232)
    * Remove endpoint name length validation from infrastructure code
    
    * Enter commit message

---
+++ src/NServiceBus.AcceptanceTests/Core/SelfVerification/EndpointNameEnforcementTests.cs
@@ -0,0 +1,31 @@
+﻿namespace NServiceBus.AcceptanceTests.Core.SelfVerification
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using AcceptanceTesting;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class EndpointNameEnforcementTests : NServiceBusAcceptanceTest
+    {
+        const int endpointNameMaxLength = 77;
+
+        [Test]
+        public void EndpointName_should_not_exceed_maximum_length()
+        {
+            var testTypes = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(IsEndpointClass);
+
+            var violators = testTypes
+                .Where(t => AcceptanceTesting.Customization.Conventions.EndpointNamingConvention(t).Length > endpointNameMaxLength)
+                .ToList();
+
+            CollectionAssert.IsEmpty(violators, string.Join(",", violators));
+        }
+
+        static bool IsEndpointClass(Type t) => endpointConfigurationBuilderType.IsAssignableFrom(t);
+
+        static Type endpointConfigurationBuilderType = typeof(EndpointConfigurationBuilder);
+    }
+}
