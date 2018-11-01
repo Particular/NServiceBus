d19ff70195245343896742ef6a6cafb7d2b9eff0
Simon Cropp <simon.cropp@gmail.com>
2018-11-02 01:43:11 +1100

    Update UnicastRoutingStrategy.cs (#5288)

--- src/NServiceBus.Core/Routing/UnicastRoutingStrategy.cs
+++ src/NServiceBus.Core/Routing/UnicastRoutingStrategy.cs
@@ -21,7 +21,6 @@
         /// <param name="headers">Message headers.</param>
         public override AddressTag Apply(Dictionary<string, string> headers)
         {
-            Guard.AgainstNull(nameof(headers), headers);
             return new UnicastAddressTag(destination);
         }
 
