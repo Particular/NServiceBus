20260b714b527de11e29922e47c742cad6590c30
Michał Wójcik <wojcik.mike@gmail.com>
2019-03-12 19:10:42 +0100

    Remove XML cmments

--- src/NServiceBus.Testing.Fakes/obsoletes.cs
+++ src/NServiceBus.Testing.Fakes/obsoletes.cs
@@ -4,11 +4,7 @@ namespace NServiceBus.Testing
     using System.Threading.Tasks;
     
     public partial class TestableInvokeHandlerContext
-    {
-        /// <summary>
-        /// Moves the message being handled to the back of the list of available
-        /// messages so it can be handled later.
-        /// </summary>
+    {        
         [ObsoleteEx(
             Message = "HandleCurrentMessageLater has been deprecated.",
             RemoveInVersion = "8",
@@ -17,10 +13,7 @@ namespace NServiceBus.Testing
         {
             throw new NotSupportedException("HandleCurrentMessageLater has been deprecated and will be removed in NServiceBus.Core Version 8.");
         }
-
-        /// <summary>
-        /// Indicates if <see cref="IMessageHandlerContext.HandleCurrentMessageLater" /> has been called.
-        /// </summary>
+        
         [ObsoleteEx(
             Message = "HandleCurrentMessageLaterWasCalled has been deprecated.",
             RemoveInVersion = "8",
