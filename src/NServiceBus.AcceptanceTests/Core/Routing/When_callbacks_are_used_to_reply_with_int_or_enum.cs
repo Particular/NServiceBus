bda1434bb082784f30098aef36efec9c003820bb
Andreas Öhlund <andreas.ohlund@particular.net>
2019-05-20 16:06:49 +0200

    Allow downstreams to support Int and Enum replies (#5383)
    * Throw good exception when replying with simple types
    
    * Typo
    
    * Split tests
    
    * Remove test

---
+++ src/NServiceBus.AcceptanceTests/Core/Routing/When_callbacks_are_used_to_reply_with_int_or_enum.cs
@@ -0,0 +1,76 @@
+﻿namespace NServiceBus.AcceptanceTests.Core.Routing
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NServiceBus.Pipeline;
+    using NUnit.Framework;
+
+    public class When_callbacks_are_used_to_reply_with_int_or_enum : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_throw_late_enough_to_allow_intercept()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<Endpoint>(c => c.When(b => b.SendLocal(new MyRequest())))
+                .Done(c => c.GotTheRequest)
+                .Run();
+
+            Assert.True(context.GotExceptionFromReply);
+           
+            //this verifies a callbacks assumption that core won't throw until after the `IOutgoingLogicalMessageContext` stage
+            // See https://github.com/Particular/NServiceBus.Callbacks/blob/develop/src/NServiceBus.Callbacks/Reply/SetCallbackResponseReturnCodeBehavior.cs#L7
+            Assert.True(context.WasAbleToInterceptBeforeCoreThrows, "Callbacks needs to be able to intercept the pipeline before core throws");
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool GotTheRequest { get; set; }
+            public bool GotExceptionFromReply { get; set; }
+            public bool WasAbleToInterceptBeforeCoreThrows { get; set; }
+        }
+
+        public class Endpoint : EndpointConfigurationBuilder
+        {
+            public Endpoint()
+            {
+                EndpointSetup<DefaultServer>(c => c.Pipeline.Register(typeof(CallbacksAssumptionVerifier), "Makes sure that callbacks can allow int replies"));
+            }
+
+            public class StartMessageHandler : IHandleMessages<MyRequest>
+            {
+                public Context TestContext { get; set; }
+
+                public async Task Handle(MyRequest message, IMessageHandlerContext context)
+                {
+                    try
+                    {
+                        await context.Reply(10);
+                    }
+                    catch (Exception)
+                    {
+                        TestContext.GotExceptionFromReply = true;
+                    }
+
+                    TestContext.GotTheRequest = true;
+                }
+            }
+
+            class CallbacksAssumptionVerifier : Behavior<IOutgoingLogicalMessageContext>
+            {
+                public Context TestContext { get; set; }
+
+                public override Task Invoke(IOutgoingLogicalMessageContext context, Func<Task> next)
+                {
+                    TestContext.WasAbleToInterceptBeforeCoreThrows = true;
+                    return next();
+                }
+            }
+        }
+
+        public class MyRequest : IMessage
+        {
+        }
+    }
+}
