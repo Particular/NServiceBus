3dde3bf0d302b6db9f92a1624ddf75ce85528148
Hadi Eskandari <h.eskandari@gmail.com>
2019-06-28 12:42:37 +1000

    Failing test for learning persistence

---
+++ src/NServiceBus.Core.Tests/Persistence/Learning/LearningPersistenceTests.cs
@@ -0,0 +1,86 @@
+﻿namespace NServiceBus.Core.Tests.Persistence.Learning
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.IO;
+    using System.Threading.Tasks;
+    using NServiceBus.Sagas;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LearningPersistenceTests
+    {
+        [Test]
+        public async Task Should_deserialize_sub_properties_when_loading_sagas()
+        {
+            var temp = Path.GetTempPath();
+            var storageFolder = Path.Combine(temp, "SagaStorage");
+            var metaData = new SagaMetadataCollection();
+            metaData.Initialize(new[] {typeof(SagaUnderTest)});
+
+            var sagaManifests = new SagaManifestCollection(metaData, storageFolder);
+            var storage = new LearningSynchronizedStorageSession(sagaManifests);
+
+            var pageId = 10;
+            var sagaId = Guid.NewGuid();
+            var sagaData = new SagaDataContainer
+            {
+                Id = sagaId,
+                SagaID = sagaId,
+                Detail = new Details()
+            };
+            sagaData.Detail.AddPage(pageId, true);
+            await storage.Save(sagaData);
+            await storage.CompleteAsync();
+
+            storage.Dispose(); //to close the streams
+
+            var readSagaData = await storage.Read<SagaDataContainer>(sagaId);
+            
+            Assert.AreEqual(sagaId, readSagaData.Id);
+            Assert.NotNull(readSagaData.Detail);
+            Assert.NotNull(readSagaData.Detail.Pages);
+            Assert.IsTrue(readSagaData.Detail.Pages[pageId]);
+        }
+    }
+
+    public class SagaUnderTest : Saga<SagaDataContainer>, IAmStartedByMessages<SagaStartMessage>
+    {
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaDataContainer> mapper)
+        {
+            mapper.ConfigureMapping<SagaStartMessage>(x => x.ID).ToSaga(s => s.SagaID);
+        }
+
+        public Task Handle(SagaStartMessage message, IMessageHandlerContext context)
+        {
+            return Task.FromResult(0);
+        }
+    }
+
+    public class SagaStartMessage : ICommand
+    {
+        public Guid ID { get; }
+    }
+
+    public class SagaDataContainer : ContainSagaData
+    {
+        public Guid SagaID { get; set; }
+        public Details Detail { get; set; }
+    }
+
+    [Serializable]
+    public class Details
+    {
+        public Details()
+        {
+            Pages = new ConcurrentDictionary<int, bool>();
+        }
+
+        public void AddPage(int pageNo, bool flag)
+        {
+            Pages[pageNo] = flag;
+        }
+
+        public ConcurrentDictionary<int, bool> Pages { get; set; }
+    }
+}
