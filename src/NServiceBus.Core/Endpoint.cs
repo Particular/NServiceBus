a710772c57dcaa158334c3bc41537cc65d7a76e0
Andreas Öhlund <andreas.ohlund@particular.net>
2019-09-05 21:19:49 +0200

    Split public entry point

--- src/NServiceBus.Core/Endpoint.cs
+++ src/NServiceBus.Core/Endpoint.cs
@@ -1,28 +1,12 @@
 namespace NServiceBus
 {
     using System.Threading.Tasks;
-    using ObjectBuilder;
 
     /// <summary>
     /// Factory methods for configuring, creating and starting endpoint instances.
     /// </summary>
     public static class Endpoint
     {
-        /// <summary>
-        /// Configures an endpoint to run with an externally managed container.
-        /// </summary>
-        /// <param name="configuration">The endpoint configuration.</param>
-        /// <param name="configureComponents">The registration API adapter for the external container.</param>
-        /// <returns>The configured endpoint.</returns>
-        public static IConfiguredEndpointWithExternallyManagedContainer ConfigureWithExternallyManagedContainer(EndpointConfiguration configuration, IConfigureComponents configureComponents)
-        {
-            Guard.AgainstNull(nameof(configuration), configuration);
-            Guard.AgainstNull(nameof(configureComponents), configureComponents);
-
-            return ConfiguredEndpoint
-                .ConfigureWithExternallyManagedContainer(configuration, configureComponents);
-        }
-
         /// <summary>
         /// Creates a new startable endpoint based on the provided configuration.
         /// </summary>
---
+++ src/NServiceBus.Core/EndpointWithExternallyManagedContainer.cs
@@ -0,0 +1,25 @@
+﻿namespace NServiceBus
+{
+    using ObjectBuilder;
+
+    /// <summary>
+    /// Factory methods for configuring endpoints with an externally managed container.
+    /// </summary>
+    public static class EndpointWithExternallyManagedContainer
+    {
+        /// <summary>
+        /// Configures an endpoint to use an externally managed container.
+        /// </summary>
+        /// <param name="configuration">The endpoint configuration.</param>
+        /// <param name="configureComponents">The registration API adapter for the external container.</param>
+        /// <returns>The configured endpoint.</returns>
+        public static IConfiguredEndpointWithExternallyManagedContainer Configure(EndpointConfiguration configuration, IConfigureComponents configureComponents)
+        {
+            Guard.AgainstNull(nameof(configuration), configuration);
+            Guard.AgainstNull(nameof(configureComponents), configureComponents);
+
+            return ConfiguredEndpoint
+                .ConfigureWithExternallyManagedContainer(configuration, configureComponents);
+        }
+    }
+}
