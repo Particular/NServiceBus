cd55b105f9c19f3fdd77deac56383a2e4ce144d8
Andreas Öhlund <andreas.ohlund@particular.net>
2019-09-19 09:23:32 +0200

    Update EndpointCreator.cs

--- src/NServiceBus.Core/EndpointCreator.cs
+++ src/NServiceBus.Core/EndpointCreator.cs
@@ -11,8 +11,6 @@ namespace NServiceBus
     using MessageInterfaces;
     using MessageInterfaces.MessageMapper.Reflection;
     using ObjectBuilder;
-    using Routing;
-    using Routing.MessageDrivenSubscriptions;
     using Settings;
     using Transport;
     using Unicast.Messages;
