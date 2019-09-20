3a46e77c571d536d123b581b3ad045d762dc00a4
Andreas Öhlund <andreas.ohlund@particular.net>
2019-09-20 10:34:13 +0200

    Obsolete feature

--- src/NServiceBus.Core/obsoletes.cs
+++ src/NServiceBus.Core/obsoletes.cs
@@ -1055,4 +1055,29 @@ namespace NServiceBus.Pipeline
     }
 }
 
+namespace NServiceBus
+{
+    using Features;
+
+    [ObsoleteEx(
+           Message = "Can safely removed.",
+           RemoveInVersion = "8",
+           TreatAsErrorFromVersion = "7")]
+    class Recoverability : Feature
+    {
+        public Recoverability()
+        {
+            EnableByDefault();
+            DependsOnOptionally<DelayedDeliveryFeature>();
+
+            Prerequisite(context => !context.Settings.GetOrDefault<bool>("Endpoint.SendOnly"),
+                "Message recoverability is only relevant for endpoints receiving messages.");
+        }
+
+        protected internal override void Setup(FeatureConfigurationContext context)
+        {
+        }
+    }
+}
+
 #pragma warning restore 1591
