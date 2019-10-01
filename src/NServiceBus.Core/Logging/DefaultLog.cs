905f179358ac0288415d05f984231bfa23fbb3f9
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-10-01 12:25:25 +0200

    log exception.data with the default logger

--- src/NServiceBus.Core/Logging/DefaultLog.cs
+++ src/NServiceBus.Core/Logging/DefaultLog.cs
@@ -1,6 +1,8 @@
 namespace NServiceBus
 {
     using System;
+    using System.Collections;
+    using System.Text;
     using Logging;
 
     class NamedLogger : ILog
@@ -24,7 +26,7 @@ namespace NServiceBus
 
         public void Debug(string message, Exception exception)
         {
-            defaultLoggerFactory.Write(name, LogLevel.Debug, message + Environment.NewLine + exception);
+            defaultLoggerFactory.Write(name, LogLevel.Debug, message + Environment.NewLine + ExceptionOutput(exception));
         }
 
         public void DebugFormat(string format, params object[] args)
@@ -39,7 +41,7 @@ namespace NServiceBus
 
         public void Info(string message, Exception exception)
         {
-            defaultLoggerFactory.Write(name, LogLevel.Info, message + Environment.NewLine + exception);
+            defaultLoggerFactory.Write(name, LogLevel.Info, message + Environment.NewLine + ExceptionOutput(exception));
         }
 
         public void InfoFormat(string format, params object[] args)
@@ -54,7 +56,7 @@ namespace NServiceBus
 
         public void Warn(string message, Exception exception)
         {
-            defaultLoggerFactory.Write(name, LogLevel.Warn, message + Environment.NewLine + exception);
+            defaultLoggerFactory.Write(name, LogLevel.Warn, message + Environment.NewLine + ExceptionOutput(exception));
         }
 
         public void WarnFormat(string format, params object[] args)
@@ -69,7 +71,7 @@ namespace NServiceBus
 
         public void Error(string message, Exception exception)
         {
-            defaultLoggerFactory.Write(name, LogLevel.Error, message + Environment.NewLine + exception);
+            defaultLoggerFactory.Write(name, LogLevel.Error, message + Environment.NewLine + ExceptionOutput(exception));
         }
 
         public void ErrorFormat(string format, params object[] args)
@@ -84,12 +86,31 @@ namespace NServiceBus
 
         public void Fatal(string message, Exception exception)
         {
-            defaultLoggerFactory.Write(name, LogLevel.Fatal, message + Environment.NewLine + exception);
+            defaultLoggerFactory.Write(name, LogLevel.Fatal, message + Environment.NewLine + ExceptionOutput(exception));
         }
 
         public void FatalFormat(string format, params object[] args)
         {
             defaultLoggerFactory.Write(name, LogLevel.Fatal, string.Format(format, args));
+        }
+
+        static string ExceptionOutput(Exception exception)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(exception.ToString());
+            if (exception.Data.Count > 0)
+            {
+                stringBuilder.AppendLine("Exception details:");
+
+#pragma warning disable DE0006 // API is deprecated
+                foreach (DictionaryEntry exceptionData in exception.Data)
+#pragma warning restore DE0006 // API is deprecated
+                {
+                    stringBuilder.Append('\t').Append(exceptionData.Key).Append(": ").AppendLine(exceptionData.Value.ToString());
+                }
+            }
+
+            return stringBuilder.ToString();
         }
 
         DefaultLoggerFactory defaultLoggerFactory;
