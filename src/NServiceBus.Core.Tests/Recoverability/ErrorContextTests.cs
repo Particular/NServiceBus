678518f666b25ca207697ffac5f40b931b08cb97
Szymon Pobiega <szymon.pobiega@gmail.com>
2019-10-03 17:11:55 +0200

    Add ContextBag to ErrorContext (#5433)
    * Add ContextBag to ErrorContext
    
    * Approved tests
    
    * Switched to ReadOnlyContextBag to not allocate that much if not needed
    
    * Update src/NServiceBus.Core/Transports/ErrorContext.cs
    
    Co-Authored-By: Sean Feldman <SeanFeldman@users.noreply.github.com>
    
    * Update src/NServiceBus.Core/Transports/ErrorContext.cs
    
    Co-Authored-By: Tim Bussmann <timbussmann@users.noreply.github.com>
    
    * Update src/NServiceBus.Core/Transports/ErrorContext.cs
    
    Co-Authored-By: Tim Bussmann <timbussmann@users.noreply.github.com>

---
+++ src/NServiceBus.Core.Tests/Recoverability/ErrorContextTests.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus.Core.Tests.Recoverability
+{
+    using System;
+    using System.Collections.Generic;
+    using Extensibility;
+    using NUnit.Framework;
+    using Transport;
+
+    [TestFixture]
+    public class ErrorContextTests
+    {
+        [Test]
+        public void Can_pass_additional_information_via_context_bag()
+        {
+            var contextBag = new ContextBag();
+            contextBag.Set("MyKey", "MyValue");
+            var context = new ErrorContext(new Exception(), new Dictionary<string, string>(), "ID", new byte[0], new TransportTransaction(), 0, contextBag);
+
+            Assert.AreEqual("MyValue", context.Extensions.Get<string>("MyKey"));
+        }
+    }
+}
--- src/NServiceBus.Core/Transports/ErrorContext.cs
+++ src/NServiceBus.Core/Transports/ErrorContext.cs
@@ -2,16 +2,40 @@
 {
     using System;
     using System.Collections.Generic;
+    using Extensibility;
 
     /// <summary>
     /// The context for messages that has failed processing.
     /// </summary>
     public class ErrorContext
     {
+        static ReadOnlyContextBag emptyBag = new ContextBag();
+
         /// <summary>
         /// Initializes the error context.
         /// </summary>
+        /// <param name="exception">The exception that caused the message processing failure.</param>
+        /// <param name="headers">The message headers.</param>
+        /// <param name="transportMessageId">Native message id.</param>
+        /// <param name="body">The message body.</param>
+        /// <param name="transportTransaction">Transaction (along with connection if applicable) used to receive the message.</param>
+        /// <param name="immediateProcessingFailures">Number of failed immediate processing attempts.</param>
         public ErrorContext(Exception exception, Dictionary<string, string> headers, string transportMessageId, byte[] body, TransportTransaction transportTransaction, int immediateProcessingFailures)
+            : this(exception, headers, transportMessageId, body, transportTransaction, immediateProcessingFailures, emptyBag)
+        {
+        }
+
+        /// <summary>
+        /// Initializes the error context.
+        /// </summary>
+        /// <param name="exception">The exception that caused the message processing failure.</param>
+        /// <param name="headers">The message headers.</param>
+        /// <param name="transportMessageId">Native message id.</param>
+        /// <param name="body">The message body.</param>
+        /// <param name="transportTransaction">Transaction (along with connection if applicable) used to receive the message.</param>
+        /// <param name="immediateProcessingFailures">Number of failed immediate processing attempts.</param>
+        /// <param name="context">A <see cref="ContextBag" /> which can be used to extend the current object.</param>
+        public ErrorContext(Exception exception, Dictionary<string, string> headers, string transportMessageId, byte[] body, TransportTransaction transportTransaction, int immediateProcessingFailures, ReadOnlyContextBag context)
         {
             Exception = exception;
             TransportTransaction = transportTransaction;
@@ -20,6 +44,7 @@
             Message = new IncomingMessage(transportMessageId, headers, body);
 
             DelayedDeliveriesPerformed = Message.GetDelayedDeliveriesPerformed();
+            Extensions = context;
         }
 
         /// <summary>
@@ -46,5 +71,10 @@
         /// Failed incoming message.
         /// </summary>
         public IncomingMessage Message { get; }
+
+        /// <summary>
+        /// A collection of additional information provided by the transport.
+        /// </summary>
+        public ReadOnlyContextBag Extensions { get; }
     }
 }
