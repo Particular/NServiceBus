18d7d088ff8d7e6dc43473692cb8967425ef662e
Sean Feldman <SeanFeldman@users.noreply.github.com>
2019-10-04 08:25:26 -0600

    Check for saga correlation property for null to avoid NullReferenceException (#5442)
    * Check for saga correlation property null to avoid NullReferenceException
    
    * Saga data class should be unique
    
    * Incorporated feedback
    
    * Move correlation property check from behaviour to PropertySagaFinder
    
    * Formatting
    
    * formatting
    
    * better exception message

--- src/NServiceBus.Core/Sagas/PropertySagaFinder.cs
+++ src/NServiceBus.Core/Sagas/PropertySagaFinder.cs
@@ -25,6 +25,15 @@ namespace NServiceBus
             var lookupValues = context.GetOrCreate<SagaLookupValues>();
             lookupValues.Add<TSagaData>(sagaPropertyName, propertyValue);
 
+            if (propertyValue == null)
+            {
+                var saga = context.Get<ActiveSagaInstance>();
+                var sagaEntityName = saga.Metadata.Name;
+                var messageName = finderDefinition.MessageTypeName;
+
+                throw new Exception($"Message {messageName} mapped to saga {sagaEntityName} has attempted to assign null to the correlation property {sagaPropertyName}. Correlation properties cannot be assigned null.");
+            }
+
             if (sagaPropertyName.ToLower() == "id")
             {
                 return await sagaPersister.Get<TSagaData>((Guid) propertyValue, storageSession, context).ConfigureAwait(false);
