95c95fdcd9b18630ab0f2cd5726386bbab0320ed
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-10-04 17:19:54 +0200

    add obsoletes

--- src/NServiceBus.Core/Recoverability/Faults/ErrorsNotifications.cs
+++ src/NServiceBus.Core/Recoverability/Faults/ErrorsNotifications.cs
@@ -9,47 +9,59 @@ namespace NServiceBus.Faults
     /// </summary>
     public class ErrorsNotifications
     {
-        ///// <summary>
-        ///// Notification when a message is moved to the error queue.
-        ///// </summary>
-        //public event EventHandler<FailedMessage> MessageSentToErrorQueue;
+        /// <summary>
+        /// Notification when a message is moved to the error queue.
+        /// </summary>
+        [ObsoleteEx(
+            Message = "The .NET event based error notifications will be deprecated in favor of Task-based callbacks. Use endpointConfiguration.Recoverability().Failed(settings => settings.OnMessageSentToErrorQueue(callback)) instead.",
+            RemoveInVersion = "9.0", 
+            TreatAsErrorFromVersion = "8.0")]
+        public event EventHandler<FailedMessage> MessageSentToErrorQueue;
 
-        ///// <summary>
-        ///// Notification when a message fails a immediate retry.
-        ///// </summary>
-        //public event EventHandler<ImmediateRetryMessage> MessageHasFailedAnImmediateRetryAttempt;
+        /// <summary>
+        /// Notification when a message fails a immediate retry.
+        /// </summary>
+        [ObsoleteEx(
+            Message = "The .NET event based error notifications will be deprecated in favor of Task-based callbacks. Use endpointConfiguration.Recoverability().Immediate(settings => settings.OnMessageBeingRetried(callback)) instead.",
+            RemoveInVersion = "9.0",
+            TreatAsErrorFromVersion = "8.0")]
+        public event EventHandler<ImmediateRetryMessage> MessageHasFailedAnImmediateRetryAttempt;
 
-        ///// <summary>
-        ///// Notification when a message is sent to Delayed Retries queue.
-        ///// </summary>
-        //public event EventHandler<DelayedRetryMessage> MessageHasBeenSentToDelayedRetries;
+        /// <summary>
+        /// Notification when a message is sent to Delayed Retries queue.
+        /// </summary>
+        [ObsoleteEx(
+            Message = "The .NET event based error notifications will be deprecated in favor of Task-based callbacks. Use endpointConfiguration.Recoverability().Delayed(settings => settings.OnMessageBeingRetried(callback)) instead.",
+            RemoveInVersion = "9.0",
+            TreatAsErrorFromVersion = "8.0")]
+        public event EventHandler<DelayedRetryMessage> MessageHasBeenSentToDelayedRetries;
 
         internal void InvokeMessageHasBeenSentToErrorQueue(IncomingMessage message, Exception exception, string errorQueue)
         {
-            //MessageSentToErrorQueue?.Invoke(this, new FailedMessage(
-            //    message.MessageId,
-            //    new Dictionary<string, string>(message.Headers),
-            //    CopyOfBody(message.Body), exception, errorQueue));
+            MessageSentToErrorQueue?.Invoke(this, new FailedMessage(
+                message.MessageId,
+                new Dictionary<string, string>(message.Headers),
+                CopyOfBody(message.Body), exception, errorQueue));
         }
 
         internal void InvokeMessageHasFailedAnImmediateRetryAttempt(int immediateRetryAttempt, IncomingMessage message, Exception exception)
         {
-            //MessageHasFailedAnImmediateRetryAttempt?.Invoke(this, new ImmediateRetryMessage(
-            //    message.MessageId,
-            //    new Dictionary<string, string>(message.Headers),
-            //    CopyOfBody(message.Body),
-            //    exception,
-            //    immediateRetryAttempt));
+            MessageHasFailedAnImmediateRetryAttempt?.Invoke(this, new ImmediateRetryMessage(
+                message.MessageId,
+                new Dictionary<string, string>(message.Headers),
+                CopyOfBody(message.Body),
+                exception,
+                immediateRetryAttempt));
         }
 
         internal void InvokeMessageHasBeenSentToDelayedRetries(int delayedRetryAttempt, IncomingMessage message, Exception exception)
         {
-            //MessageHasBeenSentToDelayedRetries?.Invoke(this, new DelayedRetryMessage(
-            //    message.MessageId,
-            //    new Dictionary<string, string>(message.Headers),
-            //    CopyOfBody(message.Body),
-            //    exception,
-            //    delayedRetryAttempt));
+            MessageHasBeenSentToDelayedRetries?.Invoke(this, new DelayedRetryMessage(
+                message.MessageId,
+                new Dictionary<string, string>(message.Headers),
+                CopyOfBody(message.Body),
+                exception,
+                delayedRetryAttempt));
         }
 
         static byte[] CopyOfBody(byte[] body)
