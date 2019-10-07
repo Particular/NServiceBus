f0bb590e3c1fff5c3acca199f8c9e21031ff223e
SzymonPobiega <szymon.pobiega@gmail.com>
2019-10-07 13:49:11 +0200

    Fix AssemblyScanner logging -- remove redundant NewLine

---
+++ src/NServiceBus.Core/Logging/StringBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace NServiceBus
+{
+    using System;
+    using System.Text;
+
+    static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Appends a new line marker to the StringBuilder followed by the text in the <paramref name="newLine"/>.
+        /// </summary>
+        public static StringBuilder NewLine(this StringBuilder stringBuilder, string newLine)
+        {
+            return stringBuilder.Append(Environment.NewLine).Append(newLine);
+        }
+
+        /// <summary>
+        /// Appends a new line marker to the StringBuilder followed by the text in the <paramref name="newLine"/>.
+        /// </summary>
+        public static StringBuilder NewLine(this StringBuilder stringBuilder, StringBuilder newLine)
+        {
+            return stringBuilder.Append(Environment.NewLine).Append(newLine);
+        }
+    }
+}
