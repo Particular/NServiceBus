6cab68b839a9c0fb1969604cc05a67cbbbe2cbc7
SzymonPobiega <szymon.pobiega@gmail.com>
2019-10-07 13:48:45 +0200

    More tests for the AssemblyScanner logging

--- src/NServiceBus.Core.Tests/AssemblyScanner/ErrorLoggingTest.cs
+++ src/NServiceBus.Core.Tests/AssemblyScanner/ErrorLoggingTest.cs
@@ -25,6 +25,18 @@
         }
 
         [Test]
+        public void ApproveErrorLog_FileLoadException_NServiceBus_Only()
+        {
+            var exception = new ReflectionTypeLoadException(new Type[0], new Exception[]
+            {
+                new FileLoadException("File load exception", typeof(AssemblyScanner).Assembly.FullName),
+            });
+
+            var formattedException = AssemblyScanner.FormatReflectionTypeLoadException("MyFile", exception);
+            Approver.Verify(formattedException);
+        }
+
+        [Test]
         public void ApproveErrorLog_FileLoadException()
         {
             var exception = new ReflectionTypeLoadException(new Type[0], new[]
@@ -32,6 +44,31 @@
                 new Exception("Generic exception 1"),
                 new FileLoadException("File load exception", typeof(ErrorLoggingTest).Assembly.FullName),
                 new Exception("Generic exception 2"),
+            });
+
+            var formattedException = AssemblyScanner.FormatReflectionTypeLoadException("MyFile", exception);
+            Approver.Verify(formattedException);
+        }
+
+        [Test]
+        public void ApproveErrorLog_FileLoadException_Only()
+        {
+            var exception = new ReflectionTypeLoadException(new Type[0], new Exception[]
+            {
+                new FileLoadException("File load exception", typeof(ErrorLoggingTest).Assembly.FullName),
+            });
+
+            var formattedException = AssemblyScanner.FormatReflectionTypeLoadException("MyFile", exception);
+            Approver.Verify(formattedException);
+        }
+
+        [Test]
+        public void ApproveErrorLog_FileLoadException_NServiceBus_Other()
+        {
+            var exception = new ReflectionTypeLoadException(new Type[0], new Exception[]
+            {
+                new FileLoadException("File load exception", typeof(ErrorLoggingTest).Assembly.FullName),
+                new FileLoadException("File load exception", typeof(AssemblyScanner).Assembly.FullName),
             });
 
             var formattedException = AssemblyScanner.FormatReflectionTypeLoadException("MyFile", exception);
