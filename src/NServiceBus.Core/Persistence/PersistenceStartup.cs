b4b8517b113d66e738e37442e900ff452d2867fc
Sean Feldman <feldman.sean@gmail.com>
2019-10-15 12:58:09 -0600

    Improve persistence check to ensure exception is thrown when both persistences are enabled and different

--- src/NServiceBus.Core/Persistence/PersistenceStartup.cs
+++ src/NServiceBus.Core/Persistence/PersistenceStartup.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using Features;
     using Logging;
     using Persistence;
     using Settings;
@@ -17,12 +18,13 @@
             }
 
             var enabledPersistences = PersistenceStorageMerger.Merge(definitions, settings);
+            var bothFeaturesEnabled = settings.IsFeatureEnabled(typeof(Features.Sagas)) && settings.IsFeatureEnabled(typeof(Features.Outbox));
+
+            ValidateSagaAndOutboxUseSamePersistence(enabledPersistences, bothFeaturesEnabled);
 
             var resultingSupportedStorages = new List<Type>();
             var diagnostics = new Dictionary<string, object>();
-
-            ValidateSagaAndOutboxUseSamePersistence(enabledPersistences);
-
+            
             foreach (var definition in enabledPersistences)
             {
                 var persistenceDefinition = definition.DefinitionType.Construct<PersistenceDefinition>();
@@ -48,14 +50,15 @@
             settings.AddStartupDiagnosticsSection("Persistence", diagnostics);
         }
 
-        static void ValidateSagaAndOutboxUseSamePersistence(List<EnabledPersistence> enabledPersistences)
+        static void ValidateSagaAndOutboxUseSamePersistence(List<EnabledPersistence> enabledPersistences, bool bothFeaturesEnabled)
         {
             var sagaPersisterType = enabledPersistences.FirstOrDefault(p => p.SelectedStorages.Contains(typeof(StorageType.Sagas)));
             var outboxPersisterType = enabledPersistences.FirstOrDefault(p => p.SelectedStorages.Contains(typeof(StorageType.Outbox)));
 
             if (sagaPersisterType != null 
                 && outboxPersisterType != null
-                && sagaPersisterType.DefinitionType != outboxPersisterType.DefinitionType)
+                && sagaPersisterType.DefinitionType != outboxPersisterType.DefinitionType
+                && bothFeaturesEnabled)
             {
                 throw new Exception($"Sagas and the Outbox need to use the same type of persistence. Saga persistence is configured to use {sagaPersisterType.DefinitionType.Name}. Outbox persistence is configured to use {outboxPersisterType.DefinitionType.Name}.");
             }
