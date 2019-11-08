77d925d2f965cf8d1d0c37e3d95247af8ed2aed2
Andreas Öhlund <andreas.ohlund@particular.net>
2019-11-08 09:44:29 +0100

    Make sure we're backwards compatible

--- src/NServiceBus.AcceptanceTests/Core/Hosting/When_feature_overrides_hostid_from_feature_default.cs
+++ src/NServiceBus.AcceptanceTests/Core/Hosting/When_feature_overrides_hostid_from_feature_default.cs
@@ -9,23 +9,25 @@ namespace NServiceBus.AcceptanceTests.Core.Hosting
     using Features;
     using NUnit.Framework;
 
-    public class When_feature_overrides_hostid : NServiceBusAcceptanceTest
+    public class When_feature_overrides_hostid_from_feature_default : NServiceBusAcceptanceTest
     {
         [Test]
         public async Task MD5_should_not_be_used()
         {
-            var context = await Scenario.Define<Context>()
-                .WithEndpoint<MyEndpoint>(e => e.When(b => b.SendLocal(new MyMessage())))
-                .Done(c => c.Done)
+            var context = await Scenario.Define<Context>(c => { c.CustomIdToApplyInFeature = Guid.NewGuid(); })
+                .WithEndpoint<MyEndpoint>()
+                .Done(c => c.EndpointsStarted)
                 .Run();
 
-            Assert.IsTrue(context.NotSet);
+            Assert.False(context.HostIdDefaultPresentWhenFeatureDefaultsAreApplied);
+            Assert.AreEqual(context.CustomIdToApplyInFeature, context.HostIdExposedToFeatureSetup);
         }
 
         public class MyEndpoint : EndpointConfigurationBuilder
         {
             public MyEndpoint()
             {
+                //Configuring a value up front prevents the default to be applied, this can be anything, we will override it later
                 EndpointSetup<DefaultServer>(c => c.UniquelyIdentifyRunningInstance().UsingCustomIdentifier(Guid.NewGuid()));
             }
         }
@@ -39,43 +41,31 @@ namespace NServiceBus.AcceptanceTests.Core.Hosting
                 {
                     // remove the override, we need to hack it via reflection!
                     var fieldInfo = s.GetType().GetField("Overrides", BindingFlags.Instance | BindingFlags.NonPublic);
-                    var dictionary = (ConcurrentDictionary<string, object>) fieldInfo.GetValue(s);
+                    var dictionary = (ConcurrentDictionary<string, object>)fieldInfo.GetValue(s);
                     dictionary.TryRemove("NServiceBus.HostInformation.HostId", out _);
 
-                    // Try to get value, setting should not exist
+                    // Try to get value, setting should not exist since that means that the default was applied
                     var context = s.Get<Context>();
-                    context.NotSet = !s.HasSetting("NServiceBus.HostInformation.HostId");
+                    context.HostIdDefaultPresentWhenFeatureDefaultsAreApplied = s.HasSetting("NServiceBus.HostInformation.HostId");
 
                     // Set override again so we have something
-                    s.Set("NServiceBus.HostInformation.HostId", Guid.NewGuid());
+                    s.Set("NServiceBus.HostInformation.HostId", context.CustomIdToApplyInFeature);
                 });
             }
 
             protected override void Setup(FeatureConfigurationContext context)
             {
-            }
-        }
-
-        public class MyMessageHandler : IHandleMessages<MyMessage>
-        {
-            public Context Context { get; set; }
-
-            public Task Handle(MyMessage message, IMessageHandlerContext context)
-            {
-                Context.Done = true;
-
-                return Task.FromResult(0);
+                context.Settings.Get<Context>().HostIdExposedToFeatureSetup = context.Settings.Get<Guid>("NServiceBus.HostInformation.HostId");
             }
         }
 
         public class Context : ScenarioContext
         {
-            public bool NotSet { get; set; }
-            public bool Done { get; set; }
-        }
+            public bool HostIdDefaultPresentWhenFeatureDefaultsAreApplied { get; set; }
 
-        public class MyMessage : ICommand
-        {
+            public Guid HostIdExposedToFeatureSetup { get; set; }
+
+            public Guid CustomIdToApplyInFeature { get; set; }
         }
     }
 }
--- src/NServiceBus.Core/Hosting/HostingComponent.cs
+++ src/NServiceBus.Core/Hosting/HostingComponent.cs
@@ -76,15 +76,7 @@
             {
                 get
                 {
-                    //we can't use a default since we have a test that makes sure that users can create a feature that
-                    // prevents MD5 hash to be used if they override the host id. For more details see the test:
-                    // When_feature_overrides_hostid
-                    if (settings.TryGet<Guid>(HostIdSettingsKey, out var hostId))
-                    {
-                        return hostId;
-                    }
-
-                    return DeterministicGuid.Create(fullPathToStartingExe, RuntimeEnvironment.MachineName);
+                    return settings.Get<Guid>(HostIdSettingsKey);
                 }
                 set
                 {
@@ -114,6 +106,21 @@
                 {
                     settings.Set(PropertiesSettingsKey, value);
                 }
+            }
+
+            // Since the host id default is using MD5 which breaks MIPS compliant users we need to delay setting the default until users have a chance to override
+            // via a custom feature to be backwards compatible.
+            // For more details see the test: When_feature_overrides_hostid_from_feature_default
+            // When this is removed in v8 downstreams can no longer rely on the setting to always be there
+            [ObsoleteEx(RemoveInVersion = "8", TreatAsErrorFromVersion = "7")]
+            internal void ApplyHostIdDefaultIfNeeded()
+            {
+                if (settings.HasExplicitValue(HostIdSettingsKey))
+                {
+                    return;
+                }
+
+                settings.SetDefault(HostIdSettingsKey, DeterministicGuid.Create(fullPathToStartingExe, RuntimeEnvironment.MachineName));
             }
 
             SettingsHolder settings;
