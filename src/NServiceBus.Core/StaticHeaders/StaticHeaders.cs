f1d0e80e634da40173a7b5369616d0f49bc446d6
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-11-13 08:52:26 +0100

    rollback StaticHeaders feature

---
+++ src/NServiceBus.Core/StaticHeaders/StaticHeaders.cs
@@ -0,0 +1,19 @@
+﻿namespace NServiceBus
+{
+    using Features;
+
+    class StaticHeaders : Feature
+    {
+        public StaticHeaders()
+        {
+            EnableByDefault();
+            Prerequisite(c => c.Settings.HasSetting<CurrentStaticHeaders>(), "No static outgoing headers registered");
+        }
+
+        protected internal override void Setup(FeatureConfigurationContext context)
+        {
+            var headers = context.Settings.Get<CurrentStaticHeaders>();
+            context.Pipeline.Register("ApplyStaticHeaders", new ApplyStaticHeadersBehavior(headers), "Applies static headers to outgoing messages");
+        }
+    }
+}
