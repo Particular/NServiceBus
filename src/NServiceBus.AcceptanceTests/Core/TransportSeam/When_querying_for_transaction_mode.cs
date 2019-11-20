b1ea661e29fb710a92f130c4bea6631721308ee9
Sean Feldman <feldman.sean@gmail.com>
2019-11-19 22:25:30 -0700

    Add acceptance test to verify TransactionModeSettingsExtensions.GetRequiredTransactionModeForReceives() extension method works

---
+++ src/NServiceBus.AcceptanceTests/Core/TransportSeam/When_querying_for_transaction_mode.cs
@@ -0,0 +1,47 @@
+﻿namespace NServiceBus.AcceptanceTests.Core.TransportSeam
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using FakeTransport;
+    using Features;
+    using NUnit.Framework;
+
+    public class When_querying_for_transaction_mode : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_retrieve_value_set_for_transport_transaction_mode()
+        {
+            await Scenario.Define<Context>()
+                .WithEndpoint<Endpoint>(c => c.CustomConfig(ec =>
+                {
+                    ec.EnableFeature<Endpoint.FeatureEnabledByUser>();
+                    var transport = ec.UseTransport<LearningTransport>();
+                    transport.Transactions(TransportTransactionMode.ReceiveOnly);
+                }))
+                .Done(c => c.EndpointsStarted)
+                .Run();
+        }
+
+        class Context : ScenarioContext
+        {
+        }
+
+        class Endpoint : EndpointConfigurationBuilder
+        {
+            public Endpoint()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class FeatureEnabledByUser : Feature
+            {
+                protected override void Setup(FeatureConfigurationContext context)
+                {
+                    var endpointTransactionMode = ConsistencyGuarantees.TransactionModeSettingsExtensions.GetRequiredTransactionModeForReceives(context.Settings);
+                    Assert.AreEqual(TransportTransactionMode.ReceiveOnly, endpointTransactionMode, "Transport transaction mode for the endpoint did not match the expected value.");
+                }
+            }
+        }
+    }
+}
