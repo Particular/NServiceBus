0753f198c124841797d1fc205f9fc6619595700f
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-11-21 14:59:14 +0100

    read ExecuteTheseHandlersFirst from correct settings key

--- src/NServiceBus.Core/Receiving/ReceiveComponent.cs
+++ src/NServiceBus.Core/Receiving/ReceiveComponent.cs
@@ -364,9 +364,14 @@ namespace NServiceBus
             public Settings(SettingsHolder settings)
             {
                 this.settings = settings;
+                ExecuteTheseHandlersFirst = new List<Type>(0);
             }
 
-            public List<Type> ExecuteTheseHandlersFirst => settings.GetOrCreate<List<Type>>();
+            public List<Type> ExecuteTheseHandlersFirst
+            {
+                get => settings.Get<List<Type>>("NServiceBus.ExecuteTheseHandlersFirst");
+                set => settings.Set("NServiceBus.ExecuteTheseHandlersFirst", value);
+            }
 
             public MessageHandlerRegistry MessageHandlerRegistry => settings.GetOrCreate<MessageHandlerRegistry>();
 
