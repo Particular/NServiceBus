a8bbf6457a0611ccfad1b55bd69a734d25fdeadc
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-11-27 19:24:48 +0100

    fix return value description for DeduplicateMessage

--- src/NServiceBus.Core/Gateway/IDeduplicateMessages.cs
+++ src/NServiceBus.Core/Gateway/IDeduplicateMessages.cs
@@ -10,7 +10,7 @@
     public interface IDeduplicateMessages
     {
         /// <summary>
-        /// Returns true if the message is a duplicate.
+        /// Returns false if the message is a duplicate.
         /// </summary>
         /// <param name="clientId">The client id that defines the range of ids to check for duplicates.</param>
         /// <param name="timeReceived">The time received of the message to allow the storage to do cleanup.</param>
