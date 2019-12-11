6dafc4a4a18a694ee20f876f96dc6ff60295f94d
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-12-11 19:05:37 +0100

    add test for TransportInfrastructure in settings

---
+++ src/NServiceBus.AcceptanceTests/Core/TransportSeam/When_configuring_transport.cs
@@ -0,0 +1,47 @@
+﻿namespace NServiceBus.AcceptanceTests.Core.TransportSeam
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using Features;
+    using NUnit.Framework;
+    using Transport;
+
+    public class When_configuring_transport : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_provide_transport_infrastructure_to_features()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<EndpointWithFeature>()
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            Assert.IsNotNull(context.TransportInfrastructure);
+        }
+
+        class Context : ScenarioContext
+        {
+            public TransportInfrastructure TransportInfrastructure { get; set; }
+        }
+
+        class EndpointWithFeature : EndpointConfigurationBuilder
+        {
+            public EndpointWithFeature()
+            {
+                EndpointSetup<DefaultServer>(c => c.EnableFeature<FeatureAccessingInfrastructure>());
+            }
+
+            class FeatureAccessingInfrastructure : Feature
+            {
+                protected override void Setup(FeatureConfigurationContext context)
+                {
+                    var testContext = (Context)context.Settings.Get<ScenarioContext>();
+                    testContext.TransportInfrastructure = context.Settings.Get<TransportInfrastructure>();
+                }
+            }
+        }
+    }
+
+    
+}
