f219028620687e5093989d77b7a621d4f4da94a3
Andreas Öhlund <andreas.ohlund@particular.net>
2020-01-24 13:35:04 +0100

    Add comments

--- src/NServiceBus.Core/Persistence/InMemory/Gateway/InMemoryGatewayDeduplication.cs
+++ src/NServiceBus.Core/Persistence/InMemory/Gateway/InMemoryGatewayDeduplication.cs
@@ -15,12 +15,16 @@
 
         public Task<bool> DeduplicateMessage(string clientId, DateTime timeReceived, ContextBag context)
         {
+            // since this storage is best effort given that scaling out will lead to duplicates anyway we decided to not
+            // add any locking here. 2 threads could potentially treat the same ID as a non duplicate but that is unlikely to happen
+            // since transports will most times only allow a single comsumer of a given message
             if (clientIdStorage.IsDuplicate(clientId))
             {
                 return TaskEx.FalseTask;
             }
 
-            //the current design of the gateway seam will only allow us to safly add in transaction commit
+            // The current design of the gateway seam will only allow us to safly add the id when the scope commits.
+            // This is fine since the gateway will always wrap the call in a transaction scope so this should always be true
             if (Transaction.Current != null)
             {
                 Transaction.Current.EnlistVolatile(new EnlistmentNotification(clientIdStorage, clientId), EnlistmentOptions.None);
