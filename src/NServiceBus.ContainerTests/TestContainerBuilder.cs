d9971794a4f4de45bff44af86a3193cb8e6f7656
Tomek Masternak <tomasz.masternak@particular.net>
2020-03-30 13:01:33 +0200

    container tests setup

--- src/NServiceBus.ContainerTests/TestContainerBuilder.cs
+++ src/NServiceBus.ContainerTests/TestContainerBuilder.cs
@@ -5,7 +5,7 @@ namespace NServiceBus.ContainerTests
 
     public static class TestContainerBuilder
     {
-        public static Func<IContainer> ConstructBuilder = () => (IContainer)Activator.CreateInstance(Type.GetType("NServiceBus.LightInjectObjectBuilder, NServiceBus.Core"));
+        public static Func<IContainer> ConstructBuilder = () => (IContainer)Activator.CreateInstance(Type.GetType("NServiceBus.ObjectBuilder.MsDI.MsDIObjectBuilder, NServiceBus.Core"));
 
     }
 }
