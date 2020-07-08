8247ab89b21738b2e64c033c9fb7a0f6e9fe520b
lailabougria <laila.bougria@particular.net>
2020-07-08 10:41:30 +0200

    Reference test packages

---
+++ src/NServiceBus.PersistenceTests/bla.cs
@@ -0,0 +1,27 @@
+﻿namespace NServiceBus.PersistenceTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    
+    [TestFixture]
+    public class When_completing_a_saga_loaded_by_id : SagaPersisterTests<TestSaga, TestSagaData>
+    {
+        [Test]
+        public async Task Should_delete_the_saga()
+        {
+            var correlationPropertyData = Guid.NewGuid().ToString();
+
+            var saga = new TestSagaData {SomeId = correlationPropertyData, DateTimeProperty = DateTime.UtcNow};
+
+            await SaveSaga(saga);
+
+            var sagaData = await GetByIdAndComplete(saga.Id);
+
+            var completedSaga = await GetById(saga.Id);
+
+            Assert.NotNull(sagaData);
+            Assert.Null(completedSaga);
+        }
+    }
+}
