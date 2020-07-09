65251c0fd2b93c22e8510a69b26734d571c757f5
lailabougria <laila.bougria@particular.net>
2020-07-09 14:35:45 +0200

    Scope all dependent classes within the test class for When_completing_a_saga_with_correlation_property

--- src/NServiceBus.PersistenceTests/Sagas/When_completing_a_saga_with_correlation_property.cs
+++ src/NServiceBus.PersistenceTests/Sagas/When_completing_a_saga_with_correlation_property.cs
@@ -5,7 +5,7 @@
     using NUnit.Framework;
 
     [TestFixture]
-    public class When_completing_a_saga_with_correlation_property : SagaPersisterTests<SagaWithCorrelationProperty, SagaWithCorrelationPropertyData>
+    public class When_completing_a_saga_with_correlation_property : SagaPersisterTests<When_completing_a_saga_with_correlation_property.SagaWithCorrelationProperty, When_completing_a_saga_with_correlation_property.SagaWithCorrelationPropertyData>
     {
         [Test]
         public async Task Should_delete_the_saga()
@@ -23,6 +23,26 @@
 
             Assert.NotNull(sagaData);
             Assert.Null(completedSaga);
+        }
+        
+        public class SagaWithCorrelationPropertyData : ContainSagaData
+        {
+            public string CorrelatedProperty { get; set; }
+
+            public DateTime DateTimeProperty { get; set; }
+        }
+        
+        public class SagaWithCorrelationProperty : Saga<SagaWithCorrelationPropertyData>, IAmStartedByMessages<SagaCorrelationPropertyStartingMessage>
+        {
+            public Task Handle(SagaCorrelationPropertyStartingMessage message, IMessageHandlerContext context)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaWithCorrelationPropertyData> mapper)
+            {
+                mapper.ConfigureMapping<SagaCorrelationPropertyStartingMessage>(m => m.CorrelatedProperty).ToSaga(s => s.CorrelatedProperty);
+            }
         }
     }
 }
