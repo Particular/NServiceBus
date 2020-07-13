9a2b0e104df22f6db66ff5ef9a3d2ef4fb9b4672
lailabougria <laila.bougria@particular.net>
2020-07-13 14:13:52 +0200

    Added test When_retrieving_same_saga_on_different_threads.cs

---
+++ src/NServiceBus.PersistenceTests/Sagas/When_retrieving_same_saga_on_different_threads.cs
@@ -0,0 +1,102 @@
+﻿namespace NServiceBus.PersistenceTests.Sagas
+{
+    using System;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_retrieving_same_saga_on_different_threads : SagaPersisterTests
+    {
+        [Test]
+        public async Task Save_should_fail_when_data_changes_between_read_and_update_on_same_thread()
+        {
+            configuration.RequiresOptimisticConcurrencySupport();
+            var correlationPropertyData = Guid.NewGuid().ToString();
+
+            var persister = configuration.SagaStorage;
+            var insertContextBag = configuration.GetContextBagForSagaStorage();
+
+            Guid generatedSagaId;
+            using (var insertSession = await configuration.SynchronizedStorage.OpenSession(insertContextBag))
+            {
+                var sagaData = new TestSagaData {SomeId = correlationPropertyData, DateTimeProperty = DateTime.UtcNow};
+                var correlationProperty = SetActiveSagaInstanceForSave(insertContextBag, new TestSaga(), sagaData);
+                generatedSagaId = sagaData.Id;
+
+                await persister.Save(sagaData, correlationProperty, insertSession, insertContextBag);
+                await insertSession.CompleteAsync();
+            }
+
+            var startSecondTaskSync = new TaskCompletionSource<bool>();
+            var firstTaskCanCompleteSync = new TaskCompletionSource<bool>();
+
+            var firstTask = Task.Run(async () =>
+            {
+                var winningContext = configuration.GetContextBagForSagaStorage();
+                using (var winningSaveSession = await configuration.SynchronizedStorage.OpenSession(winningContext))
+                {
+                    SetActiveSagaInstanceForGet<TestSaga, TestSagaData>(winningContext, new TestSagaData {Id = generatedSagaId, SomeId = correlationPropertyData});
+                    var record = await persister.Get<TestSagaData>(generatedSagaId, winningSaveSession, winningContext);
+                    SetActiveSagaInstanceForGet<TestSaga, TestSagaData>(winningContext, record);
+
+                    startSecondTaskSync.SetResult(true);
+                    await firstTaskCanCompleteSync.Task;
+
+                    record.DateTimeProperty = DateTime.UtcNow;
+                    await persister.Update(record, winningSaveSession, winningContext);
+                    await winningSaveSession.CompleteAsync();
+                }
+            });
+
+            var secondTask = Task.Run(async () =>
+            {
+                await startSecondTaskSync.Task;
+
+                var losingSaveContext = configuration.GetContextBagForSagaStorage();
+                using (var losingSaveSession = await configuration.SynchronizedStorage.OpenSession(losingSaveContext))
+                {
+                    SetActiveSagaInstanceForGet<TestSaga, TestSagaData>(losingSaveContext, new TestSagaData {Id = generatedSagaId, SomeId = correlationPropertyData});
+                    var staleRecord = await persister.Get<TestSagaData>("SomeId", correlationPropertyData, losingSaveSession, losingSaveContext);
+                    SetActiveSagaInstanceForGet<TestSaga, TestSagaData>(losingSaveContext, staleRecord);
+
+                    firstTaskCanCompleteSync.SetResult(true);
+                    await firstTask;
+
+                    staleRecord.DateTimeProperty = DateTime.UtcNow.AddHours(1);
+                    Assert.That(async () =>
+                    {
+                        await persister.Update(staleRecord, losingSaveSession, losingSaveContext);
+                        await losingSaveSession.CompleteAsync();
+                    }, Throws.InstanceOf<Exception>());
+                }
+            });
+
+            await secondTask;
+        }
+
+        public class TestSaga : Saga<TestSagaData>, IAmStartedByMessages<StartMessage>
+        {
+            public Task Handle(StartMessage message, IMessageHandlerContext context)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override void ConfigureHowToFindSaga(SagaPropertyMapper<TestSagaData> mapper)
+            {
+                mapper.ConfigureMapping<StartMessage>(msg => msg.SomeId).ToSaga(saga => saga.SomeId);
+            }
+        }
+
+        public class StartMessage
+        {
+            public string SomeId { get; set; }
+        }
+
+        public class TestSagaData : ContainSagaData
+        {
+            public string SomeId { get; set; } = "Test";
+
+            public DateTime DateTimeProperty { get; set; }
+        }
+    }
+}
