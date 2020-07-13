203c3d1d15e1ddeb1e963e4563a37fdb52e5b40a
Szymon Pobiega <szymon.pobiega@gmail.com>
2020-07-13 11:25:18 +0200

    Add test When_persisting_a_saga_with_the_same_unique_property_as_another_saga

---
+++ src/NServiceBus.PersistenceTests/Sagas/When_persisting_a_saga_with_the_same_unique_property_as_another_saga.cs
@@ -0,0 +1,76 @@
+﻿namespace NServiceBus.PersistenceTests.Sagas
+{
+    using System;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_persisting_a_saga_with_the_same_unique_property_as_another_saga : SagaPersisterTests
+    {
+        [Test]
+        public async Task It_should_enforce_uniqueness()
+        {
+            var correlationPropertyData = Guid.NewGuid().ToString();
+            var saga1 = new SagaWithCorrelationPropertyData
+            {
+                CorrelatedProperty = correlationPropertyData,
+                DateTimeProperty = DateTime.UtcNow
+            };
+            var saga2 = new SagaWithCorrelationPropertyData
+            {
+                CorrelatedProperty = correlationPropertyData,
+                DateTimeProperty = DateTime.UtcNow
+            };
+
+            var persister = configuration.SagaStorage;
+
+            var winningContextBag = configuration.GetContextBagForSagaStorage();
+            using (var winningSession = await configuration.SynchronizedStorage.OpenSession(winningContextBag))
+            {
+                var correlationPropertySaga1 = SetActiveSagaInstanceForSave(winningContextBag, new SagaWithCorrelationProperty(), saga1);
+                await persister.Save(saga1, correlationPropertySaga1, winningSession, winningContextBag);
+                await winningSession.CompleteAsync();
+            }
+
+            var losingContextBag = configuration.GetContextBagForSagaStorage();
+            using (var losingSession = await configuration.SynchronizedStorage.OpenSession(losingContextBag))
+            {
+                var correlationPropertySaga2 = SetActiveSagaInstanceForSave(losingContextBag, new SagaWithCorrelationProperty(), saga2);
+
+                Assert.That(async () =>
+                {
+                    await persister.Save(saga2, correlationPropertySaga2, losingSession, losingContextBag);
+                    await losingSession.CompleteAsync();
+                }, Throws.InstanceOf<Exception>());
+            }
+        }
+
+        public class SagaWithCorrelationProperty : Saga<SagaWithCorrelationPropertyData>, IAmStartedByMessages<SagaCorrelationPropertyStartingMessage>
+        {
+            public Task Handle(SagaCorrelationPropertyStartingMessage message, IMessageHandlerContext context)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaWithCorrelationPropertyData> mapper)
+            {
+                mapper.ConfigureMapping<SagaCorrelationPropertyStartingMessage>(m => m.CorrelatedProperty).ToSaga(s => s.CorrelatedProperty);
+            }
+        }
+
+        public class SagaWithCorrelationPropertyData : ContainSagaData
+        {
+            public string CorrelatedProperty { get; set; }
+
+            public DateTime DateTimeProperty { get; set; }
+        }
+
+        public class SagaCorrelationPropertyStartingMessage
+        {
+            public string CorrelatedProperty { get; set; }
+        }
+
+    }
+
+
+}
