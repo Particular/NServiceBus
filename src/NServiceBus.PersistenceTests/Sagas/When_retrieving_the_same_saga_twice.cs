463ec3846afae60eb9f5f2455913b13e3fa4032a
lailabougria <laila.bougria@particular.net>
2020-07-13 14:21:44 +0200

    Added test When_retrieving_the_same_saga_twice.cs

---
+++ src/NServiceBus.PersistenceTests/Sagas/When_retrieving_the_same_saga_twice.cs
@@ -0,0 +1,51 @@
+﻿namespace NServiceBus.PersistenceTests.Sagas
+{
+    using System;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_retrieving_the_same_saga_twice : SagaPersisterTests<When_retrieving_the_same_saga_twice.TestSaga, When_retrieving_the_same_saga_twice.TestSagaData>
+    {
+        [Test]
+        public async Task Get_returns_different_instance_of_saga_data()
+        {
+            var correlationPropertyData = Guid.NewGuid().ToString();
+            var saga = new TestSagaData {SomeId = correlationPropertyData, DateTimeProperty = DateTime.UtcNow};
+
+            await SaveSaga(saga);
+
+            var returnedSaga1 = await GetById(saga.Id);
+            var returnedSaga2 = await GetById(saga.Id);
+
+            Assert.AreNotSame(returnedSaga2, returnedSaga1);
+            Assert.AreNotSame(returnedSaga1, saga);
+            Assert.AreNotSame(returnedSaga2, saga);
+        }
+
+        public class TestSaga : Saga<TestSagaData>, IAmStartedByMessages<StartMessage>
+        {
+            public Task Handle(StartMessage message, IMessageHandlerContext context)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override void ConfigureHowToFindSaga(SagaPropertyMapper<TestSagaData> mapper)
+            {
+                mapper.ConfigureMapping<StartMessage>(msg => msg.SomeId).ToSaga(saga => saga.SomeId);
+            }
+        }
+
+        public class StartMessage
+        {
+            public string SomeId { get; set; }
+        }
+
+        public class TestSagaData : ContainSagaData
+        {
+            public string SomeId { get; set; } = "Test";
+
+            public DateTime DateTimeProperty { get; set; }
+        }
+    }
+}
