8da6a81732c22fedc9e1f695a793f57c532be123
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-07-20 16:38:35 +0200

    slightly rework When_concurrent_update_exceed_transaction_timeout_pessimistic test

--- src/NServiceBus.PersistenceTesting/Sagas/When_concurrent_update_exceed_lock_request_timeout_pessimistic.cs
+++ src/NServiceBus.PersistenceTesting/Sagas/When_concurrent_update_exceed_lock_request_timeout_pessimistic.cs
@@ -5,7 +5,7 @@
     using NUnit.Framework;
 
     [TestFixture]
-    public class When_concurrent_update_exceed_transaction_timeout_pessimistic : SagaPersisterTests<When_concurrent_update_exceed_transaction_timeout_pessimistic.TestSaga, When_concurrent_update_exceed_transaction_timeout_pessimistic.TestSagaData>
+    public class When_concurrent_update_exceed_lock_request_timeout_pessimistic : SagaPersisterTests<When_concurrent_update_exceed_lock_request_timeout_pessimistic.TestSaga, When_concurrent_update_exceed_lock_request_timeout_pessimistic.TestSagaData>
     {
         public override async Task OneTimeSetUp()
         {
@@ -19,11 +19,8 @@
             configuration.RequiresPessimisticConcurrencySupport();
 
             var correlationPropertyData = Guid.NewGuid().ToString();
-            var saga = new TestSagaData {SomeId = correlationPropertyData, DateTimeProperty = DateTime.UtcNow};
+            var saga = new TestSagaData {SomeId = correlationPropertyData, SagaProperty = "initial value"};
             await SaveSaga(saga);
-
-            var firstSessionDateTimeValue = DateTime.UtcNow.AddDays(-2);
-            var secondSessionDateTimeValue = DateTime.UtcNow.AddDays(-1);
 
             var firstSessionGetDone = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
             var secondSessionGetDone = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
@@ -31,23 +28,18 @@
 
             async Task FirstSession()
             {
-                var firstContent = configuration.GetContextBagForSagaStorage();
-                var firstSaveSession = await configuration.SynchronizedStorage.OpenSession(firstContent);
-                try
+                var firstSessionContext = configuration.GetContextBagForSagaStorage();
+                using (var firstSaveSession = await configuration.SynchronizedStorage.OpenSession(firstSessionContext))
                 {
-                    var record = await persister.Get<TestSagaData>(saga.Id, firstSaveSession, firstContent);
+                    var record = await persister.Get<TestSagaData>(saga.Id, firstSaveSession, firstSessionContext);
                     firstSessionGetDone.SetResult(true);
 
                     await Task.Delay(200).ConfigureAwait(false);
 
-                    record.DateTimeProperty = firstSessionDateTimeValue;
-                    await persister.Update(record, firstSaveSession, firstContent);
+                    record.SagaProperty = "session 1 value";
+                    await persister.Update(record, firstSaveSession, firstSessionContext);
                     await secondSessionGetDone.Task.ConfigureAwait(false);
                     await firstSaveSession.CompleteAsync();
-                }
-                finally
-                {
-                    firstSaveSession.Dispose();
                 }
             }
 
@@ -62,7 +54,7 @@
                     var recordTask = persister.Get<TestSagaData>(saga.Id, secondSession, secondContext);
                     secondSessionGetDone.SetResult(true);
                     var record = await recordTask.ConfigureAwait(false);
-                    record.DateTimeProperty = secondSessionDateTimeValue;
+                    record.SagaProperty = "session 2 value";
                     await persister.Update(record, secondSession, secondContext);
                     await secondSession.CompleteAsync();
                 }
@@ -75,11 +67,11 @@
             var firstSessionTask = FirstSession();
             var secondSessionTask = SecondSession();
 
-            await firstSessionTask;
-            var updatedSaga = await GetById(saga.Id);
-
+            Assert.DoesNotThrowAsync(async () => await firstSessionTask);
             Assert.ThrowsAsync<TimeoutException>(async () => await secondSessionTask);
-            Assert.That(updatedSaga.DateTimeProperty, Is.EqualTo(firstSessionDateTimeValue));
+
+            var updatedSaga = await GetById(saga.Id);
+            Assert.That(updatedSaga.SagaProperty, Is.EqualTo("session 1 value"));
         }
 
         public class TestSaga : Saga<TestSagaData>, IAmStartedByMessages<StartMessage>
@@ -97,9 +89,9 @@
 
         public class TestSagaData : ContainSagaData
         {
-            public string SomeId { get; set; } = "Test";
+            public string SomeId { get; set; }
 
-            public DateTime DateTimeProperty { get; set; }
+            public string SagaProperty { get; set; }
         }
 
         public class StartMessage
