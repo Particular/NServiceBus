9feddeab8c1cced78351ad784e595a5ecb61363d
David Boike <david.boike@gmail.com>
2020-07-27 13:59:45 -0500

    Unused usings

--- src/NServiceBus.Core/Pipeline/Incoming/InvokeHandlerContext.cs
+++ src/NServiceBus.Core/Pipeline/Incoming/InvokeHandlerContext.cs
@@ -1,8 +1,6 @@
 namespace NServiceBus
 {
-    using System;
     using System.Collections.Generic;
-    using System.Threading.Tasks;
     using Persistence;
     using Pipeline;
     using Unicast.Messages;
