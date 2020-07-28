3274dc1eed52df775a744113e11cf51d53b0b5d6
Mike Minutillo <mike.minutillo@particular.net>
2020-07-28 17:46:36 +0800

    Test default value for Guids

---
+++ src/NServiceBus.AcceptanceTests/Sagas/When_correlation_property_is_guid.cs
@@ -0,0 +1,94 @@
+﻿namespace NServiceBus.AcceptanceTests.Sagas
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NServiceBus;
+    using NUnit.Framework;
+
+    class When_correlation_property_is_guid : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Show_allow_default()
+        {
+            var context = await Scenario.Define<GuidCorrelationIdContext>()
+                .WithEndpoint<EndpointWithGuidSaga>(e => e
+                    .When(s => s
+                        .SendLocal(new MessageWithGuidCorrelationProperty
+                        {
+                            CorrelationProperty = default
+                        })))
+                .Done(c => c.MessageCorrelated)
+                .Run();
+
+            Assert.That(context.MessageCorrelated, Is.True);
+            Assert.That(context.CorrelatedId, Is.EqualTo(default(Guid)));
+        }
+
+        public class MessageWithGuidCorrelationProperty : IMessage
+        {
+            public Guid CorrelationProperty { get; set; }
+        }
+
+        public class RequestWithGuidCorrelationProperty : IMessage
+        {
+            public Guid RequestedId { get; set; }
+        }
+
+        class EndpointWithGuidSaga : EndpointConfigurationBuilder
+        {
+            public EndpointWithGuidSaga()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class SagaDataWithGuidCorrelatedProperty : ContainSagaData
+            {
+                public virtual Guid CorrelatedProperty { get; set; }
+            }
+
+            class SagaWithGuidCorrelatedProperty : Saga<SagaDataWithGuidCorrelatedProperty>,
+                IAmStartedByMessages<MessageWithGuidCorrelationProperty>,
+                IHandleMessages<RequestWithGuidCorrelationProperty>
+            {
+                GuidCorrelationIdContext scenarioContext;
+
+                public SagaWithGuidCorrelatedProperty(GuidCorrelationIdContext scenarioContext)
+                {
+                    this.scenarioContext = scenarioContext;
+                }
+
+                protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaDataWithGuidCorrelatedProperty> mapper)
+                {
+                    mapper.ConfigureMapping<MessageWithGuidCorrelationProperty>(msg => msg.CorrelationProperty)
+                        .ToSaga(saga => saga.CorrelatedProperty);
+
+                    mapper.ConfigureMapping<RequestWithGuidCorrelationProperty>(msg => msg.RequestedId)
+                        .ToSaga(saga => saga.CorrelatedProperty);
+                }
+
+                public Task Handle(MessageWithGuidCorrelationProperty message, IMessageHandlerContext context)
+                {
+                    return context.SendLocal(new RequestWithGuidCorrelationProperty
+                    {
+                        RequestedId = Data.CorrelatedProperty
+                    });
+                }
+
+                public Task Handle(RequestWithGuidCorrelationProperty message, IMessageHandlerContext context)
+                {
+                    scenarioContext.MessageCorrelated = true;
+                    scenarioContext.CorrelatedId = Data.CorrelatedProperty;
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+        class GuidCorrelationIdContext : ScenarioContext
+        {
+            public bool MessageCorrelated { get; set; }
+            public Guid CorrelatedId { get; set; }
+        }
+    }
+}
---
+++ src/NServiceBus.AcceptanceTests/Sagas/When_correlation_property_is_int.cs
@@ -0,0 +1,93 @@
+﻿namespace NServiceBus.AcceptanceTests.Sagas
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NServiceBus;
+    using NUnit.Framework;
+
+    class When_correlation_property_is_int : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Show_allow_default()
+        {
+            var context = await Scenario.Define<DefaultIntCorrelationIdContext>()
+                .WithEndpoint<EndpointWithIntSaga>(e => e
+                    .When(s => s
+                        .SendLocal(new MessageWithIntCorrelationProperty
+                        {
+                            CorrelationProperty = default
+                        })))
+                .Done(c => c.MessageCorrelated)
+                .Run();
+
+            Assert.That(context.MessageCorrelated, Is.True);
+            Assert.That(context.CorrelatedId, Is.EqualTo(default(int)));
+        }
+
+        public class MessageWithIntCorrelationProperty : IMessage
+        {
+            public int CorrelationProperty { get; set; }
+        }
+
+        public class RequestWithIntCorrelationProperty : IMessage
+        {
+            public int RequestedId { get; set; }
+        }
+
+        class EndpointWithIntSaga : EndpointConfigurationBuilder
+        {
+            public EndpointWithIntSaga()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class SagaDataWithIntCorrelatedProperty : ContainSagaData
+            {
+                public virtual int CorrelatedProperty { get; set; }
+            }
+
+            class SagaWithIntCorrelatedProperty : Saga<SagaDataWithIntCorrelatedProperty>, 
+                IAmStartedByMessages<MessageWithIntCorrelationProperty>, 
+                IHandleMessages<RequestWithIntCorrelationProperty>
+            {
+                DefaultIntCorrelationIdContext scenarioContext;
+
+                public SagaWithIntCorrelatedProperty(DefaultIntCorrelationIdContext scenarioContext)
+                {
+                    this.scenarioContext = scenarioContext;
+                }
+
+                protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaDataWithIntCorrelatedProperty> mapper)
+                {
+                    mapper.ConfigureMapping<MessageWithIntCorrelationProperty>(msg => msg.CorrelationProperty)
+                        .ToSaga(saga => saga.CorrelatedProperty);
+
+                    mapper.ConfigureMapping<RequestWithIntCorrelationProperty>(msg => msg.RequestedId)
+                        .ToSaga(saga => saga.CorrelatedProperty);
+                }
+
+                public Task Handle(MessageWithIntCorrelationProperty message, IMessageHandlerContext context)
+                {
+                    return context.SendLocal(new RequestWithIntCorrelationProperty
+                    {
+                        RequestedId = Data.CorrelatedProperty
+                    });
+                }
+
+                public Task Handle(RequestWithIntCorrelationProperty message, IMessageHandlerContext context)
+                {
+                    scenarioContext.MessageCorrelated = true;
+                    scenarioContext.CorrelatedId = Data.CorrelatedProperty;
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+        class DefaultIntCorrelationIdContext : ScenarioContext
+        {
+            public bool MessageCorrelated { get; set; }
+            public int CorrelatedId { get; set; }
+        }
+    }
+}
