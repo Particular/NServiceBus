1345e4152c1d43f51d3df5da40c32f447487ba60
Hadi Eskandari <h.eskandari@gmail.com>
2020-08-03 12:48:51 +1000

    Add new feature for the message type enricher

---
+++ src/NServiceBus.Core/Routing/MessageTypeEnricherFeature.cs
@@ -0,0 +1,17 @@
+﻿namespace NServiceBus.Routing
+{
+    using Features;
+
+    class MessageTypeEnricherFeature : Feature
+    {
+        public MessageTypeEnricherFeature()
+        {
+            EnableByDefault();
+        }
+
+        protected internal override void Setup(FeatureConfigurationContext context)
+        {
+            context.Pipeline.Register(typeof(MessageTypeEnricherBehavior), "Adds EnclosedMessageType to the header of the message if it doesn't exist.");
+        }
+    }
+}
