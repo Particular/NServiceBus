8b19814de0e52ae726cb20cf380381fd735486cf
danielmarbach <daniel.marbach@openplace.net>
2020-08-12 13:18:23 +0200

    Tests for IWantToRunBeforeConfigurationIsFinalized

---
+++ src/NServiceBus.AcceptanceTests/Core/ConfigurationHooks/When_configuration_is_finalized.cs
@@ -0,0 +1,44 @@
+﻿namespace NServiceBus.AcceptanceTests.Core.ConfigurationHooks
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NUnit.Framework;
+    using Settings;
+
+    public class When_configuration_is_finalized : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_run_configuration_hook()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<EndpointWithConfigurationHook>()
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            Assert.IsTrue(context.HookWasCalled);
+        }
+
+        class Context : ScenarioContext
+        {
+            public bool HookWasCalled { get; set; }
+        }
+
+        class EndpointWithConfigurationHook : EndpointConfigurationBuilder
+        {
+            public EndpointWithConfigurationHook()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class CustomInstaller : IWantToRunBeforeConfigurationIsFinalized
+            {
+                public void Run(SettingsHolder settings)
+                {
+                    var testContext = settings.Get<Context>();
+                    testContext.HookWasCalled = true;
+                }
+            }
+        }
+    }
+}
