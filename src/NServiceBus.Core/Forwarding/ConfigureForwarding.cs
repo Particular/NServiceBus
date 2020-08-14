80dd5db868d5d6f7cccc93956dccb338eb53fbc8
Hadi Eskandari <h.eskandari@gmail.com>
2020-08-14 13:49:41 +1000

    Obsolete message update to reflect the changes in v8

--- src/NServiceBus.Core/Forwarding/ConfigureForwarding.cs
+++ src/NServiceBus.Core/Forwarding/ConfigureForwarding.cs
@@ -11,7 +11,10 @@
         /// </summary>
         /// <param name="config">The <see cref="EndpointConfiguration" /> instance to apply the settings to.</param>
         /// <param name="address">The address to forward successfully processed messages to.</param>
-        [ObsoleteEx(TreatAsErrorFromVersion = "8", RemoveInVersion = "9" )]
+        [ObsoleteEx(
+            Message = "Message forwarding is no longer supported natively by NServiceBus. For auditing messages, use endpointConfiguration.AuditProcessedMessagesTo(address). If true message forwarding capabilities are needed, use a custom pipeline behavior, an example of which can be found in the documentation.",
+            TreatAsErrorFromVersion = "8", 
+            RemoveInVersion = "9" )]
         public static void ForwardReceivedMessagesTo(this EndpointConfiguration config, string address)
         {
             Guard.AgainstNull(nameof(config), config);
