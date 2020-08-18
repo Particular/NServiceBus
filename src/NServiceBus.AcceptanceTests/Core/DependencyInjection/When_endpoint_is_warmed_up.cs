e03bf9bceb04d22c0a5243a035fabf568d772416
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-08-18 09:15:51 +0200

    update When_endpoint_is_warmed_up

--- src/NServiceBus.AcceptanceTests/Core/DependencyInjection/When_endpoint_is_warmed_up.cs
+++ src/NServiceBus.AcceptanceTests/Core/DependencyInjection/When_endpoint_is_warmed_up.cs
@@ -1,81 +1,136 @@
-﻿//TODO this test should use ServiceCollection if possible
-//namespace NServiceBus.AcceptanceTests.Core.DependencyInjection
-//{
-//    using System.Linq;
-//    using System.Text;
-//    using System.Threading.Tasks;
-//    using AcceptanceTesting;
-//    using EndpointTemplates;
-//    using NUnit.Framework;
-//    using Particular.Approvals;
+﻿namespace NServiceBus.AcceptanceTests.Core.DependencyInjection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using Microsoft.Extensions.DependencyInjection;
+    using NUnit.Framework;
+    using Particular.Approvals;
 
-//    public class When_endpoint_is_warmed_up : NServiceBusAcceptanceTest
-//    {
-//        [Test]
-//        public async Task Make_sure_things_are_in_DI()
-//        {
-//            await Scenario.Define<Context>()
-//                .WithEndpoint<StartedEndpoint>(b => b.When(e => e.SendLocal(new SomeMessage())))
-//                .Done(c => c.GotTheMessage)
-//                .Run();
+    public class When_endpoint_is_warmed_up : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Make_sure_things_are_in_DI()
+        {
+            var serviceCollection = new ServiceCollection();
+            SpyConainer spyContainer = null;
+            await Scenario.Define<Context>()
+                .WithEndpoint<StartedEndpoint>(b =>
+                {
+                    b.ToCreateInstance(
+                        endpointConfiguration => Task.FromResult(EndpointWithExternallyManagedContainer.Create(endpointConfiguration, serviceCollection)),
+                        startableEndpoint =>
+                        {
+                            spyContainer = new SpyConainer(serviceCollection);
+                            return startableEndpoint.Start(spyContainer);
+                        });
+                    b.When(e => e.SendLocal(new SomeMessage()));
+                })
+                .Done(c => c.GotTheMessage)
+                .Run();
 
-//            var builder = new StringBuilder();
-//            var coreComponents = spyContainer.RegisteredComponents.Values
-//                .Where(c => c.Type.Assembly == typeof(IMessage).Assembly)
-//                    .OrderBy(c => c.Type.FullName)
-//                .ToList();
+            var builder = new StringBuilder();
+            var coreComponents = spyContainer.RegisteredComponents.Values
+                .OrderBy(c => c.Type.FullName)
+                .ToList();
 
-//            builder.AppendLine("----------- Used registrations (Find ways to stop accessing them)-----------");
+            builder.AppendLine("----------- Used registrations (Find ways to stop accessing them)-----------");
 
-//            foreach (var component in coreComponents.Where(c => c.WasResolved))
-//            {
-//                builder.AppendLine(component.ToString());
-//            }
+            foreach (var component in coreComponents.Where(c => c.WasResolved))
+            {
+                builder.AppendLine(component.ToString());
+            }
 
-//            builder.AppendLine("----------- Registrations not used by the core, can be removed in next major if downstreams have been confirmed to not use it -----------");
+            builder.AppendLine("----------- Registrations not used by the core, can be removed in next major if downstreams have been confirmed to not use it -----------");
 
-//            foreach (var component in coreComponents.Where(c => !c.WasResolved))
-//            {
-//                builder.AppendLine(component.ToString());
-//            }
+            foreach (var component in coreComponents.Where(c => !c.WasResolved))
+            {
+                builder.AppendLine(component.ToString());
+            }
 
-//            Approver.Verify(builder.ToString());
-//        }
+            Approver.Verify(builder.ToString());
+        }
 
-//        class Context : ScenarioContext
-//        {
-//            public bool GotTheMessage { get; set; }
-//        }
+        class Context : ScenarioContext
+        {
+            public bool GotTheMessage { get; set; }
+        }
 
-//        public class StartedEndpoint : EndpointConfigurationBuilder
-//        {
-//            public StartedEndpoint()
-//            {
-//                EndpointSetup<DefaultServer>(c => c.UseContainer(spyContainer));
-//            }
+        public class StartedEndpoint : EndpointConfigurationBuilder
+        {
+            public StartedEndpoint()
+            {
+                EndpointSetup<ExternallyManagedContainerServer>();
+            }
 
-//            class SomeMessageHandler : IHandleMessages<SomeMessage>
-//            {
-//                public SomeMessageHandler(Context context)
-//                {
-//                    testContext = context;
-//                }
+            class SomeMessageHandler : IHandleMessages<SomeMessage>
+            {
+                public SomeMessageHandler(Context context)
+                {
+                    testContext = context;
+                }
 
-//                public Task Handle(SomeMessage message, IMessageHandlerContext context)
-//                {
-//                    testContext.GotTheMessage = true;
+                public Task Handle(SomeMessage message, IMessageHandlerContext context)
+                {
+                    testContext.GotTheMessage = true;
 
-//                    return Task.FromResult(0);
-//                }
+                    return Task.FromResult(0);
+                }
 
-//                Context testContext;
-//            }
-//        }
+                Context testContext;
+            }
+        }
 
-//        public class SomeMessage : IMessage
-//        {
-//        }
+        public class SomeMessage : IMessage
+        {
+        }
 
-//        static AcceptanceTestingContainer spyContainer = new AcceptanceTestingContainer();
-//    }
-//}
+        class SpyConainer : IServiceProvider
+        {
+            public Dictionary<Type, RegisteredService> RegisteredComponents { get; } = new Dictionary<Type, RegisteredService>();
+
+            IServiceProvider ServiceProvider { get; }
+
+            public SpyConainer(IServiceCollection serviceCollection)
+            {
+                foreach (var serviceDescriptor in serviceCollection
+                    .Where(sd => sd.ServiceType.Assembly == typeof(IMessage).Assembly))
+                {
+                    RegisteredComponents[serviceDescriptor.ServiceType] = new RegisteredService
+                    {
+                        Type = serviceDescriptor.ServiceType,
+                        Lifecycle = serviceDescriptor.Lifetime
+                    };
+                }
+
+                ServiceProvider = serviceCollection.BuildServiceProvider();
+            }
+
+            public object GetService(Type serviceType)
+            {
+                if (RegisteredComponents.TryGetValue(serviceType, out var registeredService))
+                {
+                    registeredService.WasResolved = true;
+                }
+
+                return ServiceProvider.GetService(serviceType);
+            }
+
+            public class RegisteredService
+            {
+                public Type Type { get; set; }
+                public ServiceLifetime Lifecycle { get; set; }
+                public bool WasResolved { get; set; }
+
+                public override string ToString()
+                {
+                    return $"{Type.FullName} - {Lifecycle}";
+                }
+            }
+        }
+    }
+}
