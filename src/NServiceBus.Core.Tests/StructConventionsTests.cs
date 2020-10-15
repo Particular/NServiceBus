888370653fbbe01ce5995e0746ad213d00363e60
Laila Bougria <64011637+lailabougria@users.noreply.github.com>
2020-10-15 09:13:43 +0200

    ignore all types from nservicebus.timeout.core

--- src/NServiceBus.Core.Tests/StructConventionsTests.cs
+++ src/NServiceBus.Core.Tests/StructConventionsTests.cs
@@ -39,7 +39,7 @@ In all other cases, you should define your types as classes.
                 }
                 
                 // For some reason this class's size is different across platforms causing the test to fail on Linux. Disabling here since it won't be used as of v8
-                if (type.FullName.Equals(typeof(NServiceBus.Timeout.Core.TimeoutData).FullName)) 
+                if (type.Namespace.StartsWith("NServiceBus.Timeout.Core"))
                 {
                     continue;
                 }
