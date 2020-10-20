d7d8b259f2e604f56d7f7ad145380ccf3fc63137
Brandon Ording <bording@gmail.com>
2020-10-20 16:28:10 -0400

    Disable native deferral to find failing tests

--- src/NServiceBus.AcceptanceTests/Core/CoreTestSuiteConstraints.cs
+++ src/NServiceBus.AcceptanceTests/Core/CoreTestSuiteConstraints.cs
@@ -7,9 +7,9 @@
         public bool SupportsDtc => false;
         public bool SupportsCrossQueueTransactions => true;
         public bool SupportsNativePubSub => true;
-        public bool SupportsNativeDeferral => true;
+        public bool SupportsNativeDeferral => false;
         public bool SupportsOutbox => false;
-        public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointLearningTransport();
+        public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointAcceptanceTestingTransport(SupportsNativePubSub, SupportsNativeDeferral);
         public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointLearningPersistence();
     }
 }
