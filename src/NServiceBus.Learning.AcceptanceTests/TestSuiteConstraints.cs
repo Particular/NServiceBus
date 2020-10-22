a6c8881319207fe9d7c68ae5ae2d3f464624462b
Brandon Ording <bording@gmail.com>
2020-10-22 14:34:12 -0400

    Add Learning AcceptanceTests project

---
+++ src/NServiceBus.Learning.AcceptanceTests/TestSuiteConstraints.cs
@@ -0,0 +1,21 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using AcceptanceTesting.Support;
+
+    public partial class TestSuiteConstraints
+    {
+        public bool SupportsDtc => false;
+
+        public bool SupportsCrossQueueTransactions => true;
+
+        public bool SupportsNativePubSub => true;
+
+        public bool SupportsDelayedDelivery => true;
+
+        public bool SupportsOutbox => false;
+
+        public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointLearningTransport();
+
+        public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointLearningPersistence();
+    }
+}
