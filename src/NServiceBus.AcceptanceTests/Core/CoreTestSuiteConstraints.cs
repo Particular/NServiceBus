e51b711a0aa7f2d999bd9d379422f455f069688c
Brandon Ording <bording@gmail.com>
2020-10-22 14:20:48 -0400

    Use AcceptanceTestingPersistence

--- src/NServiceBus.AcceptanceTests/Core/CoreTestSuiteConstraints.cs
+++ src/NServiceBus.AcceptanceTests/Core/CoreTestSuiteConstraints.cs
@@ -5,11 +5,17 @@
     public partial class TestSuiteConstraints
     {
         public bool SupportsDtc => false;
+
         public bool SupportsCrossQueueTransactions => true;
+
         public bool SupportsNativePubSub => true;
+
         public bool SupportsDelayedDelivery => false;
-        public bool SupportsOutbox => false;
+
+        public bool SupportsOutbox => true;
+
         public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointAcceptanceTestingTransport(SupportsNativePubSub, SupportsDelayedDelivery);
-        public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointLearningPersistence();
+
+        public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointAcceptanceTestingPersistence();
     }
 }
