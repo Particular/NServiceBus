3fe26e40341f4f8ce5c77cca5ef128b6507784d4
Dennis van der Stelt <dvdstelt@gmail.com>
2020-10-26 14:44:32 +0100

    NServiceBus 7.4 Pipeline UOW ordering fix (#5819)
    Co-authored-by: danielmarbach <daniel.marbach@openplace.net>

---
+++ src/NServiceBus.AcceptanceTests/Core/UnitOfWork/When_using_custom_unit_of_work_with_failed_message.cs
@@ -0,0 +1,83 @@
+namespace NServiceBus.AcceptanceTests.UnitOfWork
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NServiceBus.UnitOfWork;
+    using NUnit.Framework;
+
+    public class When_using_custom_unit_of_work_with_failed_message : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_execute_uow_and_provide_exception_details()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<EndpointWithCustomUnitOfWork>(g =>
+                {
+                    g.DoNotFailOnErrorMessages();
+                    g.When(b => b.SendLocal(new MyMessage()));
+                })
+                .Done(c => c.BeginCalled && c.EndCalled)
+                .Run();
+
+            Assert.True(context.BeginCalled, "Unit of work should have been executed");
+            Assert.True(context.EndCalled, "Unit of work should have been executed");
+            Assert.That(context.EndException, Is.InstanceOf<SimulatedException>().And.Message.Contain("Something went wrong"), "Exception was not provided but should have been");
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool Done { get; set; }
+            public bool BeginCalled { get; set; }
+            public bool EndCalled { get; set; }
+            public Exception EndException { get; set; }
+        }
+
+        public class EndpointWithCustomUnitOfWork : EndpointConfigurationBuilder
+        {
+            public EndpointWithCustomUnitOfWork()
+            {
+                EndpointSetup<DefaultServer>((c, r) =>
+                {
+                    c.RegisterComponents(container => container.ConfigureComponent<CustomUnitOfWork>(DependencyLifecycle.InstancePerCall));
+                });
+            }
+
+            class CustomUnitOfWork : IManageUnitsOfWork
+            {
+                public CustomUnitOfWork(Context testContext)
+                {
+                    this.testContext = testContext;
+                }
+
+                public Task Begin()
+                {
+                    testContext.BeginCalled = true;
+                    return Task.FromResult(0);
+                }
+
+                public Task End(Exception ex = null)
+                {
+                    testContext.EndCalled = true;
+                    testContext.EndException = ex;
+                    return Task.FromResult(0);
+                }
+
+                Context testContext;
+            }
+
+            class MyMessageHandler : IHandleMessages<MyMessage>
+            {
+                public Task Handle(MyMessage message, IMessageHandlerContext context)
+                {
+                    throw new SimulatedException("Something went wrong");
+                }
+            }
+        }
+
+        public class MyMessage : IMessage
+        {
+        }
+    }
+}
---
+++ src/NServiceBus.AcceptanceTests/Core/UnitOfWork/When_using_custom_unit_of_work_with_wrap_handlers_in_scope.cs
@@ -0,0 +1,85 @@
+namespace NServiceBus.AcceptanceTests.UnitOfWork
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Transactions;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NServiceBus.UnitOfWork;
+    using NUnit.Framework;
+
+    public class When_using_custom_unit_of_work_with_wrap_handlers_in_scope : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_fail()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<EndpointWithCustomUnitOfWork>(g =>
+                {
+                    g.DoNotFailOnErrorMessages();
+                    g.When(b => b.SendLocal(new MyMessage()));
+                })
+                .Done(c => c.FailedMessages.Any())
+                .Run();
+
+            Assert.False(context.ShouldNeverBeCalled, "Unit of work should have been executed");
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool Done { get; set; }
+            public bool ShouldNeverBeCalled { get; set; }
+        }
+
+        public class EndpointWithCustomUnitOfWork : EndpointConfigurationBuilder
+        {
+            public EndpointWithCustomUnitOfWork()
+            {
+                EndpointSetup<DefaultServer>((c, r) =>
+                {
+                    c.UnitOfWork().WrapHandlersInATransactionScope();
+
+                    c.RegisterComponents(container => container.ConfigureComponent<CustomUnitOfWork>(DependencyLifecycle.InstancePerCall));
+                });
+            }
+
+            class CustomUnitOfWork : IManageUnitsOfWork
+            {
+                TransactionScope transactionScope;
+                public Task Begin()
+                {
+                    // this only works because we are not using the async state machine
+                    transactionScope = new TransactionScope(TransactionScopeOption.Required, TransactionScopeAsyncFlowOption.Enabled);
+                    return Task.FromResult(0);
+                }
+
+                public Task End(Exception ex = null)
+                {
+                    transactionScope.Complete();
+                    return Task.FromResult(0);
+                }
+            }
+
+            class MyMessageHandler : IHandleMessages<MyMessage>
+            {
+                Context testContext;
+
+                public MyMessageHandler(Context testContext)
+                {
+                    this.testContext = testContext;
+                }
+
+                public Task Handle(MyMessage message, IMessageHandlerContext context)
+                {
+                    testContext.ShouldNeverBeCalled = true;
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+        public class MyMessage : IMessage
+        {
+        }
+    }
+}
