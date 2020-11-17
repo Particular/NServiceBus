4466b333ab95dfb5d0eced5a36e20acbb783635d
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-11-17 09:41:13 +0100

    use message-driven pub-sub in core acceptance tests

--- src/NServiceBus.AcceptanceTests/Core/TestSuiteConstraints.cs
+++ src/NServiceBus.AcceptanceTests/Core/TestSuiteConstraints.cs
@@ -8,7 +8,7 @@
 
         public bool SupportsCrossQueueTransactions => true;
 
-        public bool SupportsNativePubSub => true;
+        public bool SupportsNativePubSub => false;
 
         public bool SupportsDelayedDelivery => false;
 
