325a8b2260b81653794d93a49bae92fb12cc3d72
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-01-19 09:54:56 +0100

    fix xml docs on HostSettings

--- src/NServiceBus.Core/Transports/HostSettings.cs
+++ src/NServiceBus.Core/Transports/HostSettings.cs
@@ -20,32 +20,26 @@ namespace NServiceBus.Transport
         }
 
         /// <summary>
-        /// 
         /// A name that describes the host (e.g. the endpoint name).
         /// </summary>
         public string Name { get; }
 
         /// <summary>
-        /// TODO: rethink the whole name properties. Which ones do we really need?
         /// The name for the host as it should be shown on UIs.
         /// </summary>
-        // TODO: rethink the whole name properties. Which ones do we really need?
         public string HostDisplayName { get; }
 
         /// <summary>
-        /// 
         /// A <see cref="StartupDiagnosticEntries"/> instance that can store diagnostic information about this transport.
         /// </summary>
         public StartupDiagnosticEntries StartupDiagnostic { get; }
 
         /// <summary>
-        /// 
         /// A callback to invoke when exception occur that can't be handled by the transport.
         /// </summary>
         public Action<string, Exception> CriticalErrorAction { get; }
 
         /// <summary>
-        /// 
         /// A flag that indicates whether the transport should automatically setup necessary infrastructure.
         /// </summary>
         public bool SetupInfrastructure { get; }
