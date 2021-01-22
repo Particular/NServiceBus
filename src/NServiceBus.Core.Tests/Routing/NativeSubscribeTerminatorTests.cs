b7f5270e3aeb505a578b87a0a33acc4edd17e29b
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-01-22 17:21:04 +0100

    add exception handling tests for NativeSubscribeTerminator

---
+++ src/NServiceBus.Core.Tests/Routing/NativeSubscribeTerminatorTests.cs
@@ -0,0 +1,86 @@
+﻿namespace NServiceBus.Core.Tests.Routing
+{
+    using System;
+    using System.Threading.Tasks;
+    using Extensibility;
+    using NUnit.Framework;
+    using Testing;
+    using Transport;
+    using Unicast.Messages;
+
+    [TestFixture]
+    public class NativeSubscribeTerminatorTests
+    {
+        [Test]
+        public void When_subscriptionmanager_throws_aggregateexception_on_subscribe()
+        {
+            var innerException = new Exception("expected exception");
+            var fakeSubscriptionManager = new FakeSubscriptionManager(new AggregateException(innerException));
+            var terminator =
+                new NativeSubscribeTerminator(fakeSubscriptionManager, new MessageMetadataRegistry(_ => true));
+
+            var exception = Assert.ThrowsAsync<Exception>(() => terminator.Invoke(new TestableSubscribeContext(), _ => Task.CompletedTask));
+
+            Assert.AreSame(innerException, exception);
+        }
+
+        [Test]
+        public void When_subscriptionmanager_throws_exception_on_subscribe()
+        {
+            var expectedException = new Exception("expected exception");
+            var fakeSubscriptionManager = new FakeSubscriptionManager(expectedException);
+            var terminator =
+                new NativeSubscribeTerminator(fakeSubscriptionManager, new MessageMetadataRegistry(_ => true));
+
+            var exception = Assert.ThrowsAsync<Exception>(() => terminator.Invoke(new TestableSubscribeContext(), _ => Task.CompletedTask));
+
+            Assert.AreSame(expectedException, exception);
+        }
+
+        [Test]
+        public void When_subscriptionmanager_throws_aggregateexception_on_subscribeAll()
+        {
+            var aggregateException = new AggregateException(new Exception("expected exception"));
+            var fakeSubscriptionManager = new FakeSubscriptionManager(aggregateException);
+            var terminator =
+                new NativeSubscribeTerminator(fakeSubscriptionManager, new MessageMetadataRegistry(_ => true));
+            var testableSubscribeContext = new TestableSubscribeContext();
+            testableSubscribeContext.Extensions.Set(MessageSession.SubscribeAllFlagKey, true);
+
+            var exception = Assert.ThrowsAsync<AggregateException>(() => terminator.Invoke(testableSubscribeContext, _ => Task.CompletedTask));
+
+            Assert.AreSame(aggregateException, exception);
+        }
+
+        [Test]
+        public void When_subscriptionmanager_throws_exception_on_subscribeAll()
+        {
+            var expectedException = new Exception("expected exception");
+            var fakeSubscriptionManager = new FakeSubscriptionManager(expectedException);
+            var terminator =
+                new NativeSubscribeTerminator(fakeSubscriptionManager, new MessageMetadataRegistry(_ => true));
+            var testableSubscribeContext = new TestableSubscribeContext();
+            testableSubscribeContext.Extensions.Set(MessageSession.SubscribeAllFlagKey, true);
+
+            var exception = Assert.ThrowsAsync<Exception>(() => terminator.Invoke(testableSubscribeContext, _ => Task.CompletedTask));
+
+            Assert.AreSame(expectedException, exception);
+        }
+
+        class FakeSubscriptionManager : ISubscriptionManager
+        {
+            Exception exceptionToThrow;
+
+            public FakeSubscriptionManager(Exception exceptionToThrow)
+            {
+                this.exceptionToThrow = exceptionToThrow;
+            }
+
+            public Task Subscribe(MessageMetadata eventType, ContextBag context) => throw new NotImplementedException();
+
+            public Task SubscribeAll(MessageMetadata[] eventTypes, ContextBag context) => throw exceptionToThrow;
+
+            public Task Unsubscribe(MessageMetadata eventType, ContextBag context) => throw new NotImplementedException();
+        }
+    }
+}
