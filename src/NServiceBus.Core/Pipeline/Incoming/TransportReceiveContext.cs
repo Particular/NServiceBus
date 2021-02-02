3e76f15f9b281c2ecd7532ee9f83b6089fea50c4
Andreas Öhlund <andreas.ohlund@particular.net>
2021-02-02 14:26:56 +0100

    Make prop public and remove uneeded xml doco

--- src/NServiceBus.Core/Pipeline/Incoming/TransportReceiveContext.cs
+++ src/NServiceBus.Core/Pipeline/Incoming/TransportReceiveContext.cs
@@ -3,14 +3,8 @@
     using Pipeline;
     using Transport;
 
-    /// <summary>
-    /// Context containing a physical message.
-    /// </summary>
     class TransportReceiveContext : BehaviorContext, ITransportReceiveContext
     {
-        /// <summary>
-        /// Creates a new transport receive context.
-        /// </summary>
         public TransportReceiveContext(IncomingMessage receivedMessage, TransportTransaction transportTransaction, RootContext rootContext)
             : base(rootContext)
         {
@@ -19,19 +13,13 @@
             Set(transportTransaction);
         }
 
-        /// <summary>
-        /// The physical message being processed.
-        /// </summary>
         public IncomingMessage Message { get; }
 
-        /// <summary>
-        /// Allows the pipeline to flag that it has been aborted and the receive operation should be rolled back.
-        /// </summary>
         public void AbortReceiveOperation()
         {
             ReceiveOperationWasAborted = true;
         }
 
-        internal bool ReceiveOperationWasAborted { get; private set; }
+        public bool ReceiveOperationWasAborted { get; private set; }
     }
 }
