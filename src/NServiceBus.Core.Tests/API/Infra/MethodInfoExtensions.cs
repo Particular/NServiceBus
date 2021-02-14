cf5bb63b0f57dd23365f7a81e8e20603184135b5
Adam Ralph <adam@adamralph.com>
2021-02-14 22:56:17 +0100

    add constructors to token tests

--- src/NServiceBus.Core.Tests/API/Infra/MethodInfoExtensions.cs
+++ src/NServiceBus.Core.Tests/API/Infra/MethodInfoExtensions.cs
@@ -1,22 +1,12 @@
 ﻿namespace NServiceBus.Core.Tests.API.Infra
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
     using System.Runtime.CompilerServices;
 
     static partial class MethodInfoExtensions
     {
-        public static bool IsCompilerGenerated(this MethodInfo method) =>
-            method.GetCustomAttributes(typeof(CompilerGeneratedAttribute)).Any() ||
-            method.DeclaringType.GetCustomAttributes(typeof(CompilerGeneratedAttribute)).Any();
-
-        public static bool IsObsolete(this MethodInfo method) =>
-            method.GetCustomAttributes(typeof(ObsoleteAttribute), true).Any() ||
-            method.ReturnType.IsObsolete() ||
-            method.GetParameters().Any(param => param.ParameterType.IsObsolete());
-
         public static bool IsOn(this MethodInfo method, params Type[] types) =>
             types.Any(type =>
                 type.IsAssignableFrom(method.DeclaringType) ||
@@ -24,15 +14,5 @@
 
         public static bool IsVisible(this MethodInfo method) =>
             method.DeclaringType.IsVisible && (method.IsPublic || method.IsFamily || method.IsFamilyOrAssembly);
-
-        public static bool HasCancellableContext(this MethodInfo method) =>
-            method.GetParameters().Any(parameter => typeof(ICancellableContext).IsAssignableFrom(parameter.ParameterType));
-
-        public static IEnumerable<string> Prettify(this IEnumerable<MethodInfo> methods) =>
-            methods
-                .OrderBy(_ => _, MethodInfoComparer.Instance)
-                .Select(method => method.Prettify());
-
-        public static string Prettify(this MethodInfo method) => $"{method.DeclaringType.FullName} {{ {method} }}";
     }
 }
---
+++ src/NServiceBus.Core.Tests/API/Infra/ParameterInfoExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace NServiceBus.Core.Tests.API.Infra
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading;
+
+    static partial class ParameterInfoExtensions
+    {
+        public static bool ContainsCancellableContext(this IEnumerable<ParameterInfo> parameters) =>
+            parameters.Any(parameter => typeof(ICancellableContext).IsAssignableFrom(parameter.ParameterType));
+
+        public static bool ContainsCancellationToken(this IEnumerable<ParameterInfo> parameters) =>
+            parameters.Any(parameter => parameter.ParameterType == typeof(CancellationToken));
+    }
+}
