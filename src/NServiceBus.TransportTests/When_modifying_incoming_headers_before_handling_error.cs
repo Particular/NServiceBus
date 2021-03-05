cd8545a33fa49dc8010418f5698c560a742bf14c
David Boike <david.boike@gmail.com>
2021-03-05 09:57:13 -0600

    Split When_modifying_incoming_headers into 1 test per class

---
+++ src/NServiceBus.TransportTests/When_modifying_incoming_headers_before_handling_error.cs
@@ -0,0 +1,40 @@
+﻿namespace NServiceBus.TransportTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Transport;
+
+    public class When_modifying_incoming_headers_before_handling_error : NServiceBusTransportTest
+    {
+        [TestCase(TransportTransactionMode.None)]
+        [TestCase(TransportTransactionMode.ReceiveOnly)]
+        [TestCase(TransportTransactionMode.SendsAtomicWithReceive)]
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_roll_back_modifications(TransportTransactionMode transactionMode)
+        {
+            var errorHandled = new TaskCompletionSource<ErrorContext>();
+            OnTestTimeout(() => errorHandled.SetCanceled());
+
+            await StartPump(
+                (context, _) =>
+                {
+                    context.Headers["test-header"] = "modified";
+                    throw new Exception();
+                },
+                (context, __) =>
+                {
+                    errorHandled.SetResult(context);
+                    return Task.FromResult(ErrorHandleResult.Handled);
+                },
+                transactionMode);
+
+            await SendMessage(InputQueueName, new Dictionary<string, string> { { "test-header", "original" } });
+
+            var errorContext = await errorHandled.Task;
+
+            Assert.AreEqual("original", errorContext.Message.Headers["test-header"]);
+        }
+    }
+}
---
+++ src/NServiceBus.TransportTests/When_modifying_incoming_headers_while_handling_error.cs
@@ -0,0 +1,38 @@
+﻿namespace NServiceBus.TransportTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Transport;
+
+    public class When_modifying_incoming_headers_while_handling_error : NServiceBusTransportTest
+    {
+        [TestCase(TransportTransactionMode.ReceiveOnly)]
+        [TestCase(TransportTransactionMode.SendsAtomicWithReceive)]
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_roll_back_modifications(TransportTransactionMode transactionMode)
+        {
+            var retried = new TaskCompletionSource<MessageContext>();
+            OnTestTimeout(() => retried.SetCanceled());
+
+            var retrying = false;
+
+            await StartPump(
+                (context, _) => retrying ? retried.SetCompleted(context) : throw new Exception(),
+                (context, _) =>
+                {
+                    retrying = true;
+                    context.Message.Headers["test-header"] = "modified";
+                    return Task.FromResult(ErrorHandleResult.RetryRequired);
+                },
+                transactionMode);
+
+            await SendMessage(InputQueueName, new Dictionary<string, string> { { "test-header", "original" } });
+
+            var retryMessageContext = await retried.Task;
+
+            Assert.AreEqual("original", retryMessageContext.Headers["test-header"]);
+        }
+    }
+}
