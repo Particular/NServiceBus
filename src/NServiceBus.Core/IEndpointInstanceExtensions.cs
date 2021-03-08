a0d767f079cc20004e07a518e4e90a650abff753
David Boike <david.boike@gmail.com>
2021-03-08 16:38:17 -0600

    Provide endpoint.Stop(TimeSpan)

---
+++ src/NServiceBus.Core/IEndpointInstanceExtensions.cs
@@ -0,0 +1,29 @@
+namespace NServiceBus
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents an endpoint in the running phase.
+    /// </summary>
+    public static class IEndpointInstanceExtensions
+    {
+        /// <summary>
+        /// Stops the endpoint from processing new messages, giving the endpoint a period of time
+        /// to gracefuly complete message processing on any in-flight messages before forcefully
+        /// cancelling processing on message handlers that exceed the timeout.
+        /// </summary>
+        /// <param name="endpoint">The endpoint to stop.</param>
+        /// <param name="gracefulShutdownTimeout">
+        /// A length of time to allow messages that have already begun processing to complete naturally before throwing
+        /// <see cref="OperationCanceledException"/> to forcefully abort them.
+        /// </param>
+        /// <returns></returns>
+        public static Task Stop(this IEndpointInstance endpoint, TimeSpan gracefulShutdownTimeout)
+        {
+            var timedCancellationTokenSource = new CancellationTokenSource(gracefulShutdownTimeout);
+            return endpoint.Stop(timedCancellationTokenSource.Token);
+        }
+    }
+}
