6590d1233d986ce60c8bc214c5db86452f8db46b
David Boike <david.boike@gmail.com>
2021-03-09 09:34:54 -0600

    Apply suggestions from code review
    Co-authored-by: Adam Ralph <adam@adamralph.com>

--- src/NServiceBus.Core/IEndpointInstanceExtensions.cs
+++ src/NServiceBus.Core/IEndpointInstanceExtensions.cs
@@ -10,16 +10,14 @@ namespace NServiceBus
     public static class IEndpointInstanceExtensions
     {
         /// <summary>
-        /// Stops the endpoint from processing new messages, giving the endpoint a period of time
-        /// to gracefuly complete message processing on any in-flight messages before forcefully
-        /// cancelling processing on message handlers that exceed the timeout.
+        /// Stops the endpoint from processing new messages,
+        /// granting a period of time to gracefully complete processing before forceful cancellation.
         /// </summary>
         /// <param name="endpoint">The endpoint to stop.</param>
         /// <param name="gracefulShutdownTimeout">
-        /// A length of time to allow messages that have already begun processing to complete naturally before throwing
-        /// <see cref="OperationCanceledException"/> to forcefully abort them.
+        /// The length of time granted to gracefully complete processing.
         /// </param>
-        public static Task Stop(this IEndpointInstance endpoint, TimeSpan gracefulShutdownTimeout) =>
+        public static Task Stop(this IEndpointInstance endpoint, TimeSpan gracefulStopTimeout) =>
             endpoint.Stop(new CancellationTokenSource(gracefulShutdownTimeout).Token);
     }
 }
