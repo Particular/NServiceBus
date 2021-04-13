f2f3e1736b85b178652c6e437a5fcd27f50a5301
Brandon Ording <bording@gmail.com>
2021-04-13 17:56:09 -0400

    Endpoints need to communicate

--- src/NServiceBus.AcceptanceTests/ConfigureEndpointAcceptanceTestingTransport.cs
+++ src/NServiceBus.AcceptanceTests/ConfigureEndpointAcceptanceTestingTransport.cs
@@ -40,7 +40,7 @@ public class ConfigureEndpointAcceptanceTestingTransport : IConfigureEndpointTes
             tempDir = Path.GetTempPath();
         }
 
-        storageDir = Path.Combine(tempDir, Path.GetRandomFileName(), testRunId);
+        storageDir = Path.Combine(tempDir, "acc", testRunId);
 
         var acceptanceTestingTransport = new AcceptanceTestingTransport(
             enableNativeDelayedDelivery: useNativeDelayedDelivery,
