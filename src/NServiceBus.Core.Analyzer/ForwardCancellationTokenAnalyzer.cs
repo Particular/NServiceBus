862419e1b41c869316d67c7c553e6e30f21530c1
Adam Ralph <adam@adamralph.com>
2021-05-05 15:28:03 +0200

    remove using :facepalm:

--- src/NServiceBus.Core.Analyzer/ForwardCancellationTokenAnalyzer.cs
+++ src/NServiceBus.Core.Analyzer/ForwardCancellationTokenAnalyzer.cs
@@ -2,7 +2,6 @@
 {
     using System.Collections.Generic;
     using System.Collections.Immutable;
-    using System.Composition;
     using System.Linq;
     using System.Runtime.CompilerServices;
     using Microsoft.CodeAnalysis;
