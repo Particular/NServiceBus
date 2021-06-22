dcae49cc7d0f3872aa30e44cd9ad05a8aef17d8c
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-06-22 14:14:33 +0200

    add behavior to send retry confirmation control message

---
+++ src/NServiceBus.Core/ServicePlatform/Retries/ManualRetryNotificationBehavior.cs
@@ -0,0 +1,60 @@
+﻿namespace NServiceBus
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Pipeline;
+    using Routing;
+    using Transport;
+
+    class ManualRetryNotificationBehavior : IForkConnector<IIncomingLogicalMessageContext, IIncomingLogicalMessageContext, IRoutingContext>
+    {
+        const string RetryUniqueMessageIdHeader = "ServiceControl.Retry.UniqueMessageId";
+
+        readonly string errorQueue;
+
+        public ManualRetryNotificationBehavior(string errorQueue)
+        {
+            this.errorQueue = errorQueue;
+        }
+
+        public async Task Invoke(IIncomingLogicalMessageContext context, Func<IIncomingLogicalMessageContext, Task> next)
+        {
+            await next(context).ConfigureAwait(false);
+
+            if (IsRetriedMessage(out var id))
+            {
+                await ConfirmSuccessfulRetry().ConfigureAwait(false);
+            }
+
+            async Task ConfirmSuccessfulRetry()
+            {
+                var messageToDispatch = new OutgoingMessage(
+                    CombGuid.Generate().ToString(),
+                    new Dictionary<string, string>
+                    {
+                        { "ServiceControl.Retry.Successful", DateTimeOffset.UtcNow.ToString("O") },
+                        { RetryUniqueMessageIdHeader, id }
+                    },
+                    new byte[0]);
+                var routingContext = new RoutingContext(messageToDispatch, new UnicastRoutingStrategy(errorQueue), context);
+                await this.Fork(routingContext).ConfigureAwait(false);
+            }
+
+            bool IsRetriedMessage(out string retryUniqueMessageId)
+            {
+                // check if the message is coming from a manual retry attempt
+                if (context.Headers.TryGetValue(RetryUniqueMessageIdHeader, out var uniqueMessageId) &&
+                    // The SC version that supports the confirmation message also started to add the SC version header
+                    context.Headers.ContainsKey("ServiceControl.Version"))
+                {
+                    retryUniqueMessageId = uniqueMessageId;
+                    return true;
+                }
+
+                retryUniqueMessageId = null;
+                return false;
+            }
+        }
+    }
+}
---
+++ src/NServiceBus.Core/ServicePlatform/Retries/PlatformRetryNotifications.cs
@@ -0,0 +1,20 @@
+﻿namespace NServiceBus
+{
+    using Features;
+
+    /// <summary>
+    /// Provides notifications to ServiceControl about successfully retried messages
+    /// </summary>
+    public class PlatformRetryNotifications : Feature
+    {
+        PlatformRetryNotifications() => EnableByDefault();
+
+        /// <inheritdoc />
+        protected internal override void Setup(FeatureConfigurationContext context)
+        {
+            var errorQueueAddress = context.Settings.ErrorQueueAddress();
+            var forkBehavior = new ManualRetryNotificationBehavior(errorQueueAddress);
+            context.Pipeline.Register(forkBehavior, "Provides retry notifications to ServiceControl");
+        }
+    }
+}
