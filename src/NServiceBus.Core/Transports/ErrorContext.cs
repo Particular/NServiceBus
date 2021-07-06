7d4d9a35f1a11c02f30e0355f1940e230fc1f2bc
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-07-06 17:24:03 +0200

    change error context to use wrapping dictionary

--- src/NServiceBus.Core/Transports/ErrorContext.cs
+++ src/NServiceBus.Core/Transports/ErrorContext.cs
@@ -19,7 +19,7 @@
         /// <param name="transportTransaction">Transaction (along with connection if applicable) used to receive the message.</param>
         /// <param name="immediateProcessingFailures">Number of failed immediate processing attempts.</param>
         /// <param name="context">A <see cref="ReadOnlyContextBag" /> which can be used to extend the current object.</param>
-        public ErrorContext(Exception exception, Dictionary<string, string> headers, string nativeMessageId, byte[] body, TransportTransaction transportTransaction, int immediateProcessingFailures, ReadOnlyContextBag context)
+        public ErrorContext(Exception exception, IReadOnlyDictionary<string, string> headers, string nativeMessageId, byte[] body, TransportTransaction transportTransaction, int immediateProcessingFailures, ReadOnlyContextBag context)
         {
             Guard.AgainstNull(nameof(exception), exception);
             Guard.AgainstNull(nameof(transportTransaction), transportTransaction);
@@ -30,7 +30,8 @@
             TransportTransaction = transportTransaction;
             ImmediateProcessingFailures = immediateProcessingFailures;
 
-            Message = new IncomingMessage(nativeMessageId, headers, body);
+            var changeTrackingHeaders = new ChangeTrackingDictionary(headers);
+            Message = new IncomingMessage(nativeMessageId, changeTrackingHeaders, body);
 
             DelayedDeliveriesPerformed = Message.GetDelayedDeliveriesPerformed();
             Extensions = context;
