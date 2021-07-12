26ab9a4764f1ca6b37091e6f9ed2a7156115d173
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-07-12 16:22:57 +0200

    remove import

--- src/NServiceBus.TransportTests/When_modifying_body_before_retry.cs
+++ src/NServiceBus.TransportTests/When_modifying_body_before_retry.cs
@@ -1,7 +1,6 @@
 ﻿namespace NServiceBus.TransportTests
 {
     using System;
-    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
     using System.Text;
