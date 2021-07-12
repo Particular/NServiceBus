b7d635c9458e00f289336bddafad04260b211e9e
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-07-12 13:10:36 +0200

    body modification transport tests

---
+++ src/NServiceBus.TransportTests/When_modifying_body_before_on_error.cs
@@ -0,0 +1,42 @@
+﻿namespace NServiceBus.TransportTests
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Transport;
+
+    // Failing
+    public class When_modifying_body_before_on_error : NServiceBusTransportTest
+    {
+        [TestCase(TransportTransactionMode.None)]
+        [TestCase(TransportTransactionMode.ReceiveOnly)]
+        [TestCase(TransportTransactionMode.SendsAtomicWithReceive)]
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_roll_back(TransportTransactionMode transactionMode)
+        {
+            var originalBody = new ReadOnlyCollection<byte>(Encoding.UTF8.GetBytes("Hello World!"));
+            var errorHandled = CreateTaskCompletionSource<ErrorContext>();
+            await StartPump(
+                (context, _) =>
+                {
+                    context.Body[0] = 0;
+                    throw new Exception();
+                },
+                (context, __) =>
+                {
+                    errorHandled.SetResult(context);
+                    return Task.FromResult(ErrorHandleResult.Handled);
+                },
+                transactionMode);
+
+            await SendMessage(InputQueueName, body: originalBody.ToArray());
+
+            var errorContext = await errorHandled.Task;
+
+            Assert.AreEqual(originalBody, errorContext.Message.Body);
+        }
+    }
+}
---
+++ src/NServiceBus.TransportTests/When_modifying_body_before_retry.cs
@@ -0,0 +1,49 @@
+﻿namespace NServiceBus.TransportTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Transport;
+
+    public class When_modifying_body_before_retry : NServiceBusTransportTest
+    {
+        [TestCase(TransportTransactionMode.ReceiveOnly)]
+        [TestCase(TransportTransactionMode.SendsAtomicWithReceive)]
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_roll_back(TransportTransactionMode transactionMode)
+        {
+            var originalBody = new ReadOnlyCollection<byte>(Encoding.UTF8.GetBytes("Hello World!"));
+            var retried = CreateTaskCompletionSource<MessageContext>();
+
+            var retrying = false;
+
+            await StartPump(
+                (context, _) =>
+                {
+                    if (retrying)
+                    {
+                        return retried.SetCompleted(context);
+                    }
+
+                    context.Body[0] = 0;
+                    throw new Exception();
+                },
+                (context, _) =>
+                {
+                    retrying = true;
+                    return Task.FromResult(ErrorHandleResult.RetryRequired);
+                },
+                transactionMode);
+
+            await SendMessage(InputQueueName, body: originalBody.ToArray());
+
+            var retryMessageContext = await retried.Task;
+
+            Assert.AreEqual(originalBody, retryMessageContext.Body);
+        }
+    }
+}
