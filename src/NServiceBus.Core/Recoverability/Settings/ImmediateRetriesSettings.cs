438c4fabd553aee4aeec55386a93f216980f942c
Tomek Masternak <tomasz.masternak@particular.net>
2021-07-26 12:03:02 +0200

    align notificatons

--- src/NServiceBus.Core/Recoverability/Settings/ImmediateRetriesSettings.cs
+++ src/NServiceBus.Core/Recoverability/Settings/ImmediateRetriesSettings.cs
@@ -45,8 +45,7 @@ namespace NServiceBus
                 }
 
                 var headerCopy = new Dictionary<string, string>(retry.Message.Headers);
-                var bodyCopy = retry.Message.Body;
-                return notificationCallback(new ImmediateRetryMessage(retry.Message.MessageId, headerCopy, bodyCopy, retry.Exception, retry.Attempt), cancellationToken);
+                return notificationCallback(new ImmediateRetryMessage(retry.Message.MessageId, headerCopy, retry.Message.Body, retry.Exception, retry.Attempt), cancellationToken);
             });
 
             return this;
