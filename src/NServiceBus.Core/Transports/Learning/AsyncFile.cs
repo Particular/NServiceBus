d2f90f6a04adf63ebe8d66c03808c26c5d1dd0d7
Tomek Masternak <tomasz.masternak@particular.net>
2021-07-27 13:14:00 +0200

    learning transport low memory tweaks

--- src/NServiceBus.Core/Transports/Learning/AsyncFile.cs
+++ src/NServiceBus.Core/Transports/Learning/AsyncFile.cs
@@ -16,7 +16,7 @@ namespace NServiceBus
             using (var stream = CreateWriteStream(filePath, FileMode.Create))
             {
 #if NETSTANDARD2_1
-                await stream.WriteAsync(bytes, 0, bytes.Length, cancellationToken).ConfigureAwait(false);
+                await stream.WriteAsync(bytes, cancellationToken).ConfigureAwait(false);
 #else
                 await stream.WriteAsync(bytes.ToArray(), 0, bytes.Length, cancellationToken).ConfigureAwait(false);
 #endif
