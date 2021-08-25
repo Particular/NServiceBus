b9995e1a9e0bc6065055f54dc80a09cb6c78024b
Tomek Masternak <tomasz.masternak@particular.net>
2021-08-25 12:14:27 +0200

    fixing merge conflicts

--- src/NServiceBus.Core/Transports/ErrorContext.cs
+++ src/NServiceBus.Core/Transports/ErrorContext.cs
@@ -19,7 +19,7 @@
         /// <param name="transportTransaction">Transaction (along with connection if applicable) used to receive the message.</param>
         /// <param name="immediateProcessingFailures">Number of failed immediate processing attempts.</param>
         /// <param name="context">A <see cref="ReadOnlyContextBag" /> which can be used to extend the current object.</param>
-        public ErrorContext(Exception exception, Dictionary<string, string> headers, string nativeMessageId, ReadOnlyMemory<byte> body, TransportTransaction transportTransaction, int immediateProcessingFailures, ReadOnlyContextBag context)
+        public ErrorContext(Exception exception, Dictionary<string, string> headers, string nativeMessageId, ReadOnlyMemory<byte> body, TransportTransaction transportTransaction, int immediateProcessingFailures, IReadOnlyContextBag context)
         {
             Guard.AgainstNull(nameof(exception), exception);
             Guard.AgainstNull(nameof(transportTransaction), transportTransaction);
