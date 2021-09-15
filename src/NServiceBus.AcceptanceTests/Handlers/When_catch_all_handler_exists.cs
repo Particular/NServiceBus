c636a65c0514e3e706fa4b9ad99b8bf7d6442abf
Mike Minutillo <mike.minutillo@particular.net>
2021-09-15 13:46:47 +0800

    Catch All Handler test

---
+++ src/NServiceBus.AcceptanceTests/Handlers/When_catch_all_handler_exists.cs
@@ -0,0 +1,57 @@
+﻿namespace NServiceBus.AcceptanceTests.Handlers
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_catch_all_handler_exists : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task It_should_be_invoked()
+        {
+            var ctx = await Scenario.Define<CatchAllHandlerScenarioContext>()
+                .WithEndpoint<EndpointWithCatchAllHandler>(b
+                    => b.When(session
+                        => session.SendLocal(new SomeMessage())
+                    )
+                )
+                .Done(c => c.CatchAllHandlerWaCalled)
+                .Run();
+
+            Assert.IsTrue(ctx.CatchAllHandlerWaCalled);
+        }
+
+
+        class EndpointWithCatchAllHandler : EndpointConfigurationBuilder
+        {
+            public EndpointWithCatchAllHandler()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class CatchAllMessageHandler : IHandleMessages<object>
+            {
+                CatchAllHandlerScenarioContext scenarioContext;
+
+                public CatchAllMessageHandler(CatchAllHandlerScenarioContext scenarioContext)
+                {
+                    this.scenarioContext = scenarioContext;
+                }
+
+                public Task Handle(object message, IMessageHandlerContext context)
+                {
+                    scenarioContext.CatchAllHandlerWaCalled = true;
+                    return Task.CompletedTask;
+                }
+            }
+        }
+
+        class SomeMessage : IMessage { }
+
+        class CatchAllHandlerScenarioContext : ScenarioContext
+        {
+            public bool CatchAllHandlerWaCalled { get; set; }
+        }
+    }
+}
