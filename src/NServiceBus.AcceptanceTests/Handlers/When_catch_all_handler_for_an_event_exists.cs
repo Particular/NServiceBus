c3db19969ef19e6a3e84f3187615e396f0b7081d
Mike Minutillo <mike.minutillo@particular.net>
2021-09-15 14:07:20 +0800

    Add test for event handler

---
+++ src/NServiceBus.AcceptanceTests/Handlers/When_catch_all_handler_for_an_event_exists.cs
@@ -0,0 +1,60 @@
+﻿namespace NServiceBus.AcceptanceTests.Handlers
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_catch_all_handler_for_an_event_exists : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task It_should_be_invoked()
+        {
+            var ctx = await Scenario.Define<CatchAllEventHandlerScenarioContext>()
+                .WithEndpoint<EndpointWithCatchAllHandler>(b
+                    => b.When(async session
+                            =>
+                        {
+                            await session.Subscribe<SomeEvent>();
+                            await session.Publish(new SomeEvent());
+                        }
+                    ))
+                .Done(x => x.CatchAllHandlerWasCalled)
+                .Run();
+
+
+            Assert.IsTrue(ctx.CatchAllHandlerWasCalled);
+        }
+
+        class EndpointWithCatchAllHandler : EndpointConfigurationBuilder
+        {
+            public EndpointWithCatchAllHandler()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class CatchAllMessageHandler : IHandleMessages<object>
+            {
+                CatchAllEventHandlerScenarioContext scenarioContext;
+
+                public CatchAllMessageHandler(CatchAllEventHandlerScenarioContext scenarioContext)
+                {
+                    this.scenarioContext = scenarioContext;
+                }
+
+                public Task Handle(object message, IMessageHandlerContext context)
+                {
+                    scenarioContext.CatchAllHandlerWasCalled = true;
+                    return Task.CompletedTask;
+                }
+            }
+        }
+
+        public class SomeEvent : IEvent { }
+
+        class CatchAllEventHandlerScenarioContext : ScenarioContext
+        {
+            public bool CatchAllHandlerWasCalled { get; set; }
+        }
+    }
+}
