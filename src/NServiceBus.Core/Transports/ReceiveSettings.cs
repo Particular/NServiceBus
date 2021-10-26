511bcb6cb2afb346e06afc5cc1116d06a9bcd913
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-10-26 16:55:13 +0200

    rename QueueAddress property

--- src/NServiceBus.Core/Transports/ReceiveSettings.cs
+++ src/NServiceBus.Core/Transports/ReceiveSettings.cs
@@ -8,13 +8,13 @@
         /// <summary>
         /// Creates a new instance of <see cref="ReceiveSettings"/>.
         /// </summary>
-        public ReceiveSettings(string id, QueueAddress receiveName, bool usePublishSubscribe, bool purgeOnStartup, string errorQueue)
+        public ReceiveSettings(string id, QueueAddress receiveAddress, bool usePublishSubscribe, bool purgeOnStartup, string errorQueue)
         {
             Id = id;
             UsePublishSubscribe = usePublishSubscribe;
             PurgeOnStartup = purgeOnStartup;
             ErrorQueue = errorQueue;
-            ReceiverName = receiveName;
+            ReceiveAddress = receiveAddress;
         }
 
         /// <summary>
@@ -25,7 +25,7 @@
         /// <summary>
         /// The logical name of the receiver. The name will be translated by <see cref="TransportInfrastructure.ToTransportAddress"/>.
         /// </summary>
-        public QueueAddress ReceiverName { get; set; }
+        public QueueAddress ReceiveAddress { get; set; }
 
         /// <summary>
         /// A flag indicating whether events will be subscribed to this receiver's queue.
