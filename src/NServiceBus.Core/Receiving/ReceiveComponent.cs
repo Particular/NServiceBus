d4d8e3a406c95d348d38cfae6bdf8debaa655feb
Andreas Öhlund <andreas.ohlund@particular.net>
2021-11-18 11:04:56 +0100

    Fix namning

--- src/NServiceBus.Core/Receiving/ReceiveComponent.cs
+++ src/NServiceBus.Core/Receiving/ReceiveComponent.cs
@@ -103,7 +103,7 @@ namespace NServiceBus
             {
                 receiveComponent.mainReceiverSubscriptionManager = infrastructure.Receivers[MainReceiverId].Subscriptions;
 
-                var receiveAddresses = infrastructure.Receivers[MainReceiverId].ReceiveAddress;
+                var mainReceiveAddress = infrastructure.Receivers[MainReceiverId].ReceiveAddress;
 
                 string instanceReceiveAddress = null;
 
@@ -117,7 +117,7 @@ namespace NServiceBus
                     .Select(r => r.ReceiveAddress)
                     .ToArray();
 
-                receiveComponent.receiveAddresses = new ReceiveAddresses(receiveAddresses, instanceReceiveAddress, satelliteReceiveAddresses);
+                receiveComponent.receiveAddresses = new ReceiveAddresses(mainReceiveAddress, instanceReceiveAddress, satelliteReceiveAddresses);
             };
 
             configuration.transportSeam.Configure(receiveSettings.ToArray());
