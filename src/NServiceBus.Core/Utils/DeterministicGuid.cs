721130bfc61415b015f6d6923592911fe20cb46d
David Boike <david.boike@gmail.com>
2021-11-29 15:05:59 -0600

    Add support for .NET 6 - release-7.5

--- src/NServiceBus.Core/Utils/DeterministicGuid.cs
+++ src/NServiceBus.Core/Utils/DeterministicGuid.cs
@@ -9,10 +9,10 @@
         public static Guid Create(params object[] data)
         {
             // use MD5 hash to get a 16-byte hash of the string
-            using (var provider = new MD5CryptoServiceProvider())
+            using (var md5 = MD5.Create())
             {
                 var inputBytes = Encoding.Default.GetBytes(string.Concat(data));
-                var hashBytes = provider.ComputeHash(inputBytes);
+                var hashBytes = md5.ComputeHash(inputBytes);
                 // generate a guid from the hash:
                 return new Guid(hashBytes);
             }
