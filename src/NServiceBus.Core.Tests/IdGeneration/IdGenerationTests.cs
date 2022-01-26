4f4b3b0c32a0abd046ae5705795335ffb361271a
Daniel Marbach <daniel.marbach@nservicebus.com>
2022-01-26 10:20:30 +0100

    Optimize the CombGuid generation strategy (#6236)
    * Optimize the CombGuid generation strategy
    
    * Simplify conditions, add method overload to simplify testing
    
    * Use one scratch buffer
    
    * Switch to ticks constant
    
    * Revert "Switch to ticks constant"
    
    This reverts commit 9d92d7716da2845129df42990e53c04913224390.
    
    * Revert "Use one scratch buffer"
    
    This reverts commit d200e58f93f5cc5dde1e2f0ce1b398c097b2ee56.
    
    * Switch to ticks constant

---
+++ src/NServiceBus.Core.Tests/IdGeneration/IdGenerationTests.cs
@@ -0,0 +1,68 @@
+﻿namespace NServiceBus.Core.Tests.IdGeneration
+{
+    using System;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class IdGenerationTests
+    {
+        [TestCaseSource(nameof(RandomizedInput))]
+        public void Should_return_same_result_for_same_input(Guid guid, DateTime now)
+        {
+            var oldCombGuid = LocalCopyOfCombGuid.GenerateOld(guid, now);
+            var improvedCombGuid = CombGuid.Generate(guid, now);
+
+            Assert.AreEqual(oldCombGuid, improvedCombGuid);
+        }
+
+        static readonly Random Random = new Random();
+
+        static IEnumerable<object[]> RandomizedInput
+        {
+            get
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    yield return new object[]
+                    {
+                        Guid.NewGuid(),
+                        DateTime.UtcNow.AddDays(Random.Next(0, 30)).AddHours(Random.Next(0, 24)).AddMinutes(Random.Next(0, 60)).AddSeconds(Random.Next(0, 60))
+                    };
+                }
+            }
+        }
+
+        class LocalCopyOfCombGuid
+        {
+            // Only slightly modified copy of the original algorithm
+            public static Guid GenerateOld(Guid input, DateTime nowInput)
+            {
+                var guidArray = input.ToByteArray();
+
+                var now = nowInput; // Internal use, no need for DateTimeOffset
+
+                // Get the days and milliseconds which will be used to build the byte string
+                var days = new TimeSpan(now.Ticks - BaseDateTicks);
+                var timeOfDay = now.TimeOfDay;
+
+                // Convert to a byte array
+                // Note that SQL Server is accurate to 1/300th of a millisecond so we divide by 3.333333
+                var daysArray = BitConverter.GetBytes(days.Days);
+                var millisecondArray = BitConverter.GetBytes((long)(timeOfDay.TotalMilliseconds / 3.333333));
+
+                // Reverse the bytes to match SQL Servers ordering
+                Array.Reverse(daysArray);
+                Array.Reverse(millisecondArray);
+
+                // Copy the bytes into the guid
+                Array.Copy(daysArray, daysArray.Length - 2, guidArray, guidArray.Length - 6, 2);
+                Array.Copy(millisecondArray, millisecondArray.Length - 4, guidArray, guidArray.Length - 4, 4);
+
+                return new Guid(guidArray);
+            }
+
+            static readonly long BaseDateTicks = new DateTime(1900, 1, 1).Ticks;
+        }
+    }
+}
--- src/NServiceBus.Core/IdGeneration/CombGuid.cs
+++ src/NServiceBus.Core/IdGeneration/CombGuid.cs
@@ -1,44 +1,83 @@
 namespace NServiceBus
 {
     using System;
+    using System.Buffers.Binary;
+#if NETFRAMEWORK
+    using System.Runtime.InteropServices;
+#endif
 
     /// <summary>
     /// Generates a Guid using http://www.informit.com/articles/article.asp?p=25862
     /// The Comb algorithm is designed to make the use of <see cref="Guid"/>s as Primary Keys, Foreign Keys, and Indexes nearly as efficient
     /// as <see cref="int"/>.
     /// </summary>
-    /// <remarks>Source: https://github.com/nhibernate/nhibernate-core/blob/4.0.4.GA/src/NHibernate/Id/GuidCombGenerator.cs</remarks>
+    /// <remarks>Original Source: https://github.com/nhibernate/nhibernate-core/blob/4.0.4.GA/src/NHibernate/Id/GuidCombGenerator.cs</remarks>
     static class CombGuid
     {
-        /// <summary>
-        /// Generate a new <see cref="Guid" /> using the comb algorithm.
-        /// </summary>
-        public static Guid Generate()
-        {
-            var guidArray = Guid.NewGuid().ToByteArray();
+        public static Guid Generate() =>
+            // Internal use, no need for DateTimeOffset
+            Generate(Guid.NewGuid(), DateTime.UtcNow);
 
-            var now = DateTime.UtcNow; // Internal use, no need for DateTimeOffset
+        internal static Guid Generate(Guid inputGuid, DateTime inputNow)
+        {
+            var newGuid = inputGuid;
+            Span<byte> guidArray = stackalloc byte[16];
+#if NETCOREAPP
+            if (!newGuid.TryWriteBytes(guidArray))
+#else
+            if (!MemoryMarshal.TryWrite(guidArray, ref newGuid))
+#endif
+            {
+
+                guidArray = newGuid.ToByteArray();
+            }
+
+            var now = inputNow;
 
             // Get the days and milliseconds which will be used to build the byte string
             var days = new TimeSpan(now.Ticks - BaseDateTicks);
             var timeOfDay = now.TimeOfDay;
 
             // Convert to a byte array
-            // Note that SQL Server is accurate to 1/300th of a millisecond so we divide by 3.333333
-            var daysArray = BitConverter.GetBytes(days.Days);
-            var millisecondArray = BitConverter.GetBytes((long)(timeOfDay.TotalMilliseconds / 3.333333));
-
+            Span<byte> daysArray = stackalloc byte[sizeof(int)];
             // Reverse the bytes to match SQL Servers ordering
-            Array.Reverse(daysArray);
-            Array.Reverse(millisecondArray);
+            if (BitConverter.IsLittleEndian)
+            {
+                BinaryPrimitives.WriteInt32BigEndian(daysArray, days.Days);
+            }
+            else
+            {
+                BinaryPrimitives.WriteInt32LittleEndian(daysArray, days.Days);
+            }
+            Span<byte> milliSecondsArray = stackalloc byte[sizeof(long)];
+            // Reverse the bytes to match SQL Servers ordering
+            // Note that SQL Server is accurate to 1/300th of a millisecond so we divide by 3.333333
+            if (BitConverter.IsLittleEndian)
+            {
+                BinaryPrimitives.WriteInt64BigEndian(milliSecondsArray, (long)(timeOfDay.TotalMilliseconds / 3.333333));
+            }
+            else
+            {
+                BinaryPrimitives.WriteInt64LittleEndian(milliSecondsArray, (long)(timeOfDay.TotalMilliseconds / 3.333333));
+            }
 
-            // Copy the bytes into the guid
-            Array.Copy(daysArray, daysArray.Length - 2, guidArray, guidArray.Length - 6, 2);
-            Array.Copy(millisecondArray, millisecondArray.Length - 4, guidArray, guidArray.Length - 4, 4);
+            // // Copy the bytes into the guid
+            daysArray.Slice(daysArray.Length - 2).CopyTo(guidArray.Slice(10, 2));
+            milliSecondsArray.Slice(milliSecondsArray.Length - 4).CopyTo(guidArray.Slice(12, 4));
 
+#if NETCOREAPP
             return new Guid(guidArray);
+#else
+            if (!MemoryMarshal.TryRead(guidArray, out Guid readGuid))
+            {
+                readGuid = new Guid(guidArray.ToArray());
+            }
+            return readGuid;
+#endif
         }
 
-        static readonly long BaseDateTicks = new DateTime(1900, 1, 1).Ticks;
+        // Represents new DateTime(1900, 1, 1).Ticks, while this would be more readable having a const here instead of
+        // a static field the less readable version slightly improves the throughput
+        const long BaseDateTicks = 599266080000000000; // new DateTime(1900, 1, 1).Ticks
     }
 }
