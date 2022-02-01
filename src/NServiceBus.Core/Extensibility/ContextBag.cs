80d601a6afd9ae79abe78f9eeb073b63fa1113ba
Daniel Marbach <daniel.marbach@nservicebus.com>
2022-02-01 23:14:18 +0100

    Update ContextBag.cs

--- src/NServiceBus.Core/Extensibility/ContextBag.cs
+++ src/NServiceBus.Core/Extensibility/ContextBag.cs
@@ -148,7 +148,7 @@ namespace NServiceBus.Extensibility
         /// This internal property is here for performance optimizations. It allows the pipeline to set all
         /// behaviors of a given stage which then can be extracted as part of the next delegate invocation from the context
         /// to avoid closure capturing. The behaviors set on the current bag take precedence. In case they are not set
-        /// it walks up the parent hierarchy to find the appropriate parent behaviors (for stages that have no pipeline)
+        /// it walks up the parent hierarchy to find the appropriate parent behaviors (for stages that have no pipeline).
         /// </summary>
         internal IBehavior[] Behaviors
         {
