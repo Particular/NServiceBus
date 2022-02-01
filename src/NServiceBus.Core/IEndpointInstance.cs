e5e50f091163d0196ad44ba934584f597dba7484
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-02-01 19:10:41 +0100

    use default interface implementation

--- src/NServiceBus.Core/IEndpointInstance.cs
+++ src/NServiceBus.Core/IEndpointInstance.cs
@@ -1,5 +1,9 @@
 namespace NServiceBus
 {
+#if NETCOREAPP
+    using System;
+#endif
+
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -7,10 +11,17 @@ namespace NServiceBus
     /// Represents an endpoint in the running phase.
     /// </summary>
     public interface IEndpointInstance : IMessageSession
+#if NETCOREAPP
+    , IAsyncDisposable
+#endif
     {
         /// <summary>
         /// Stops the endpoint.
         /// </summary>
         Task Stop(CancellationToken cancellationToken = default);
+
+#if NETCOREAPP
+        ValueTask IAsyncDisposable.DisposeAsync() => new ValueTask(Stop(CancellationToken.None));
+#endif
     }
 }
