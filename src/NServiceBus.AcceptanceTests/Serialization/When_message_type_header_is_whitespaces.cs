c3a85a5286e047296fcc00a7d6e0ee201a99e1e8
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-02-09 19:18:22 +0100

    extract dedicated test

--- src/NServiceBus.AcceptanceTests/Serialization/When_message_type_header_is_whitespaces.cs
+++ src/NServiceBus.AcceptanceTests/Serialization/When_message_type_header_is_whitespaces.cs
@@ -1,27 +1,22 @@
 ﻿namespace NServiceBus.AcceptanceTests.Serialization
 {
     using System;
-    using System.Collections.Generic;
-    using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
     using AcceptanceTesting;
     using EndpointTemplates;
-    using MessageInterfaces;
     using NServiceBus.Pipeline;
-    using NServiceBus.Serialization;
     using NUnit.Framework;
-    using Settings;
 
-    class When_disabling_serializer_type_detection : NServiceBusAcceptanceTest
+    public class When_message_type_header_is_whitespaces : NServiceBusAcceptanceTest
     {
         [Test]
-        public async Task Should_not_pass_messages_without_types_header()
+        public async Task Should_move_message_to_error_queue()
         {
             var context = await Scenario.Define<Context>()
                 .WithEndpoint<ReceivingEndpoint>(e => e
                     .DoNotFailOnErrorMessages()
-                    .When(s => s.SendLocal(new MessageWithoutTypeHeader())))
+                    .When(s => s.SendLocal(new MessageWithEmptyTypeHeader())))
                 .Done(c => c.IncomingMessageReceived)
                 .Run(TimeSpan.FromSeconds(20));
 
@@ -29,7 +24,6 @@
             Assert.AreEqual(1, context.FailedMessages.Single().Value.Count);
             Exception exception = context.FailedMessages.Single().Value.Single().Exception;
             Assert.IsInstanceOf<MessageDeserializationException>(exception);
-            StringAssert.Contains($"Could not determine message type from {Headers.EnclosedMessageTypes} header.", exception.InnerException.Message);
         }
 
         class Context : ScenarioContext
@@ -44,17 +38,15 @@
                 EndpointSetup<DefaultServer>(c =>
                 {
                     c.Pipeline.Register(typeof(TypeHeaderRemovingBehavior), "Removes the EnclosedMessageTypes header from incoming messages");
-                    c.UseSerialization<CustomSerializer>();
-                    c.DisableMessageTypeInference();
                 });
 
-            public class MessageHandler : IHandleMessages<MessageWithoutTypeHeader>
+            public class MessageHandler : IHandleMessages<MessageWithEmptyTypeHeader>
             {
                 Context testContext;
 
                 public MessageHandler(Context testContext) => this.testContext = testContext;
 
-                public Task Handle(MessageWithoutTypeHeader message, IMessageHandlerContext context)
+                public Task Handle(MessageWithEmptyTypeHeader message, IMessageHandlerContext context)
                 {
                     testContext.HandlerInvoked = true;
                     return Task.CompletedTask;
@@ -71,34 +63,15 @@
                 {
                     testContext.IncomingMessageReceived = true;
 
-                    context.Message.Headers.Remove(Headers.EnclosedMessageTypes);
+                    context.Message.Headers[Headers.EnclosedMessageTypes] = "   ";
 
                     return next();
                 }
             }
         }
 
-        public class MessageWithoutTypeHeader : IMessage
+        public class MessageWithEmptyTypeHeader : IMessage
         {
-        }
-
-        class CustomSerializer : SerializationDefinition, IMessageSerializer
-        {
-            public string ContentType { get; } = "CustomSerializer";
-
-            public void Serialize(object message, Stream stream)
-            {
-                stream.WriteByte(42); // need to write some byte for message serialization to work
-            }
-
-            public object[] Deserialize(ReadOnlyMemory<byte> body, IList<Type> messageTypes = null) =>
-                // simulating type detection behavior implemented by the serializer
-                new object[]
-                {
-                    new MessageWithoutTypeHeader()
-                };
-
-            public override Func<IMessageMapper, IMessageSerializer> Configure(IReadOnlySettings settings) => _ => this;
         }
     }
 }
