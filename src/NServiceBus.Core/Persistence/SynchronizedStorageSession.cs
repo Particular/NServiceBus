af14ac1c011521a2c5dd52f82f74c0552a06d5a8
Tomek Masternak <tomek.masternak@gmail.com>
2022-04-13 12:22:28 +0200

    remove comments

--- src/NServiceBus.Core/Persistence/SynchronizedStorageSession.cs
+++ src/NServiceBus.Core/Persistence/SynchronizedStorageSession.cs
@@ -8,22 +8,4 @@
 #pragma warning restore IDE1006 // Naming Styles
     {
     }
-
-    ///// <summary>
-    ///// Represents a storage session.
-    ///// </summary>
-    //public interface ISynchronizedStorageSession
-    //{
-    //}
-
-    //TODO
-    //Version constraints:
-    //- old persistence packages need to work with NServiceBus 7.8.
-    //- Persisters that target 7.8 don't work with core 7.7 and earlier.
-    //
-    //Plan:
-    //New interface ICompletableSSS
-    //7.8-compatible persisters register their SSS impl as scoped in the container. They DON'T implement the SynchronizedStorage and Adapter interfaces
-    //
-    //Core tries to resolve either SynchronizedStorage/Adapter or 
 }
