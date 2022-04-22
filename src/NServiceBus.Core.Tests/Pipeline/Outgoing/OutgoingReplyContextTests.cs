0cca7607061429fe0e3e2086657332498fec7f61
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-04-22 10:54:06 +0200

    Port #6356 to master

--- src/NServiceBus.Core.Tests/Pipeline/Outgoing/OutgoingReplyContextTests.cs
+++ src/NServiceBus.Core.Tests/Pipeline/Outgoing/OutgoingReplyContextTests.cs
@@ -1,5 +1,7 @@
 ﻿namespace NServiceBus.Core.Tests.Pipeline.Outgoing
 {
+    using System.Collections.Generic;
+    using Extensibility;
     using NServiceBus.Pipeline;
     using NUnit.Framework;
 
@@ -40,6 +42,44 @@
             var valueFound = parentContext.TryGet("someKey", out string _);
 
             Assert.IsFalse(valueFound);
+        }
+
+        [Test]
+        public void ShouldExposeSendOptionsExtensionsAsOperationProperties()
+        {
+            var message = new OutgoingLogicalMessage(typeof(object), new object());
+            var parentContext = new FakeRootContext(); // exact parent context doesn't matter
+            var options = new ContextBag();
+            options.Set("some key", "some value");
+
+            var context = new OutgoingReplyContext(message, "message-id", new Dictionary<string, string>(), options, parentContext);
+
+            var operationProperties = context.GetOperationProperties();
+            Assert.AreEqual("some value", operationProperties.Get<string>("some key"));
+        }
+
+        [Test]
+        public void ShouldNotLeakParentsOperationProperties()
+        {
+            var outerOptions = new ContextBag();
+            outerOptions.Set("outer key", "outer value");
+            outerOptions.Set("shared key", "outer shared value");
+            var parentContext = new OutgoingReplyContext(new OutgoingLogicalMessage(typeof(object), new object()), "message-id", new Dictionary<string, string>(), outerOptions, new FakeRootContext());
+
+            var innerOptions = new ContextBag();
+            innerOptions.Set("inner key", "inner value");
+            innerOptions.Set("shared key", "inner shared value");
+            var innerContext = new OutgoingReplyContext(new OutgoingLogicalMessage(typeof(object), new object()), "message-id", new Dictionary<string, string>(), innerOptions, parentContext);
+
+            var innerOperationProperties = innerContext.GetOperationProperties();
+            Assert.AreEqual("inner value", innerOperationProperties.Get<string>("inner key"));
+            Assert.AreEqual("inner shared value", innerOperationProperties.Get<string>("shared key"));
+            Assert.IsFalse(innerOperationProperties.TryGet("outer key", out string _));
+
+            var outerOperationProperties = parentContext.GetOperationProperties();
+            Assert.AreEqual("outer value", outerOperationProperties.Get<string>("outer key"));
+            Assert.AreEqual("outer shared value", outerOperationProperties.Get<string>("shared key"));
+            Assert.IsFalse(outerOperationProperties.TryGet("inner key", out string _));
         }
     }
 }
--- src/NServiceBus.Core.Tests/Routing/SubscribeContextTests.cs
+++ src/NServiceBus.Core.Tests/Routing/SubscribeContextTests.cs
@@ -13,7 +13,7 @@
             var context = new ContextBag();
             context.Set("someKey", "someValue");
 
-            var testee = new SubscribeContext(new FakeRootContext(), new Type[0], context);
+            var testee = new SubscribeContext(new FakeRootContext(), Type.EmptyTypes, context);
             testee.Extensions.Set("someKey", "updatedValue");
             testee.Extensions.Set("anotherKey", "anotherValue");
             context.TryGet("someKey", out string value);
@@ -33,11 +33,48 @@
 
             var parentContext = new FakeRootContext();
 
-            new SubscribeContext(parentContext, new Type[0], context);
+            new SubscribeContext(parentContext, Type.EmptyTypes, context);
 
             var valueFound = parentContext.TryGet("someKey", out string _);
 
             Assert.IsFalse(valueFound);
+        }
+
+        [Test]
+        public void ShouldExposeSendOptionsExtensionsAsOperationProperties()
+        {
+            var parentContext = new FakeRootContext(); // exact parent context doesn't matter
+            var options = new ContextBag();
+            options.Set("some key", "some value");
+
+            var context = new SubscribeContext(parentContext, Type.EmptyTypes, options);
+
+            var operationProperties = context.GetOperationProperties();
+            Assert.AreEqual("some value", operationProperties.Get<string>("some key"));
+        }
+
+        [Test]
+        public void ShouldNotLeakParentsOperationProperties()
+        {
+            var outerOptions = new ContextBag();
+            outerOptions.Set("outer key", "outer value");
+            outerOptions.Set("shared key", "outer shared value");
+            var parentContext = new SubscribeContext(new FakeRootContext(), Type.EmptyTypes, outerOptions);
+
+            var innerOptions = new ContextBag();
+            innerOptions.Set("inner key", "inner value");
+            innerOptions.Set("shared key", "inner shared value");
+            var innerContext = new SubscribeContext(parentContext, Type.EmptyTypes, innerOptions);
+
+            var innerOperationProperties = innerContext.GetOperationProperties();
+            Assert.AreEqual("inner value", innerOperationProperties.Get<string>("inner key"));
+            Assert.AreEqual("inner shared value", innerOperationProperties.Get<string>("shared key"));
+            Assert.IsFalse(innerOperationProperties.TryGet("outer key", out string _));
+
+            var outerOperationProperties = parentContext.GetOperationProperties();
+            Assert.AreEqual("outer value", outerOperationProperties.Get<string>("outer key"));
+            Assert.AreEqual("outer shared value", outerOperationProperties.Get<string>("shared key"));
+            Assert.IsFalse(outerOperationProperties.TryGet("inner key", out string _));
         }
     }
 }
