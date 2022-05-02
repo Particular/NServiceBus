a595afc67761eaefc0625cff4575f0609b89c85c
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-05-02 18:11:58 +0200

    move remaining functionality to audit feature

--- src/NServiceBus.Core/Audit/Audit.cs
+++ src/NServiceBus.Core/Audit/Audit.cs
@@ -28,6 +28,7 @@
 
             context.Pipeline.Register("AuditToDispatchConnector", new AuditToRoutingConnector(), "Dispatches the audit message to the transport");
             context.Pipeline.Register("AuditProcessedMessage", new InvokeAuditPipelineBehavior(auditConfig.Address, auditConfig.TimeToBeReceived), "Execute the audit pipeline");
+            context.Pipeline.Register("AuditProcessingStatistics", new AuditProcessingStatisticsBehavior(), "Add ProcessingStarted and ProcessingEnded headers");
 
             context.Settings.Get<QueueBindings>().BindSending(auditConfig.Address);
 
--- src/NServiceBus.Core/Audit/InvokeAuditPipelineBehavior.cs
+++ src/NServiceBus.Core/Audit/InvokeAuditPipelineBehavior.cs
@@ -16,6 +16,8 @@
 
         public async Task Invoke(IIncomingPhysicalMessageContext context, Func<IIncomingPhysicalMessageContext, Task> next)
         {
+            context.Extensions.Set(AuditProcessingStatisticsBehavior.ProcessingStartedKey, DateTimeOffset.UtcNow);
+
             await next(context).ConfigureAwait(false);
 
             context.Message.RevertToOriginalBodyIfNeeded();
