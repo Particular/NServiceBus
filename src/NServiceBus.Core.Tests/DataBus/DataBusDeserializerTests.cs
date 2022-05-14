c0bded344417e31a65004079a97fabaf7f7c8daf
Andreas Öhlund <andreas.ohlund@particular.net>
2022-05-14 13:25:08 +0200

    Add fallback

--- src/NServiceBus.Core.Tests/DataBus/DataBusDeserializerTests.cs
+++ src/NServiceBus.Core.Tests/DataBus/DataBusDeserializerTests.cs
@@ -13,7 +13,7 @@ namespace NServiceBus.Core.Tests.DataBus
         public void Should_use_the_specified_serializer()
         {
             var jsonSerializer = new SystemJsonDataBusSerializer();
-            var deserializer = new DataBusDeserializer(new List<IDataBusSerializer> { new SystemJsonDataBusSerializer() });
+            var deserializer = new DataBusDeserializer(new List<IDataBusSerializer> { jsonSerializer });
             var somePropertyValue = "test";
 
             using (var stream = new MemoryStream())
@@ -31,7 +31,7 @@ namespace NServiceBus.Core.Tests.DataBus
         public void Should_throw_if_matching_serializer_not_found()
         {
             var jsonSerializer = new SystemJsonDataBusSerializer();
-            var deserializer = new DataBusDeserializer(new List<IDataBusSerializer> { new SystemJsonDataBusSerializer() });
+            var deserializer = new DataBusDeserializer(new List<IDataBusSerializer> { jsonSerializer });
             var somePropertyValue = "test";
 
             using (var stream = new MemoryStream())
@@ -42,6 +42,50 @@ namespace NServiceBus.Core.Tests.DataBus
                 var ex = Assert.Throws<Exception>(() => deserializer.Deserialize("other-serializer-not-configured", typeof(string), stream));
 
                 StringAssert.Contains("other-serializer-not-configured", ex.Message);
+            }
+        }
+
+        [Test]
+        public void Should_try_all_available_when_serializer_not_known()
+        {
+            var jsonSerializer = new SystemJsonDataBusSerializer();
+#pragma warning disable CS0618
+            var binarySerializer = new BinaryFormatterDataBusSerializer();
+#pragma warning restore CS0618
+
+            var deserializer = new DataBusDeserializer(new List<IDataBusSerializer> { jsonSerializer, binarySerializer });
+            var somePropertyValue = "test";
+
+            using (var stream = new MemoryStream())
+            {
+                binarySerializer.Serialize(somePropertyValue, stream);
+                stream.Position = 0;
+
+                var deserializedProperty = deserializer.Deserialize(null, typeof(string), stream);
+
+                Assert.AreEqual(somePropertyValue, deserializedProperty);
+            }
+        }
+
+        [Test]
+        public void Should_throw_when_no_serializer_is_able_to_deserialize()
+        {
+            var jsonSerializer = new SystemJsonDataBusSerializer();
+#pragma warning disable CS0618
+            var binarySerializer = new BinaryFormatterDataBusSerializer();
+#pragma warning restore CS0618
+
+            var deserializer = new DataBusDeserializer(new List<IDataBusSerializer> { jsonSerializer });
+            var somePropertyValue = "test";
+
+            using (var stream = new MemoryStream())
+            {
+                binarySerializer.Serialize(somePropertyValue, stream);
+                stream.Position = 0;
+
+                var ex = Assert.Throws<Exception>(() => deserializer.Deserialize(null, typeof(string), stream));
+
+                StringAssert.Contains(jsonSerializer.Name, ex.Message);
             }
         }
     }
--- src/NServiceBus.Core/DataBus/DataBusDeserializer.cs
+++ src/NServiceBus.Core/DataBus/DataBusDeserializer.cs
@@ -3,7 +3,9 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using NServiceBus.DataBus;
+    using NServiceBus.Logging;
 
     class DataBusDeserializer
     {
@@ -17,6 +19,26 @@
 
         public object Deserialize(string serializerUsed, Type type, Stream stream)
         {
+            if (string.IsNullOrEmpty(serializerUsed))
+            {
+                foreach (var serializerToTry in availableSerializers.Values)
+                {
+                    try
+                    {
+                        return serializerToTry.Deserialize(type, stream);
+                    }
+                    catch (Exception)
+                    {
+                        stream.Position = 0;
+                        logger.Info($"Failed to deserialize data bus property using the {serializerToTry.Name} serializer.");
+                    }
+                }
+
+                var triedSerializers = string.Join(",", availableSerializers.Values.Select(s => s.Name));
+
+                throw new Exception($"None of the {triedSerializers} serializers was able to deserialize the data bus property.");
+            }
+
             if (!availableSerializers.TryGetValue(serializerUsed, out var serializer))
             {
                 throw new Exception($"Serializer {serializerUsed} not configured.");
@@ -26,5 +48,7 @@
         }
 
         readonly IDictionary<string, IDataBusSerializer> availableSerializers = new Dictionary<string, IDataBusSerializer>();
+
+        static readonly ILog logger = LogManager.GetLogger<DataBusDeserializer>();
     }
 }
