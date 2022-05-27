a123bf7990a4bafc754d426d27cb91e5e024ad7c
Mike Minutillo <mike.minutillo@particular.net>
2022-05-27 15:17:08 +0800

    Add todos

--- src/NServiceBus.Core/Diagnostics/ActivitySources.cs
+++ src/NServiceBus.Core/Diagnostics/ActivitySources.cs
@@ -4,6 +4,8 @@
 
     static class ActivitySources
     {
+        // TODO: Set a real version
+        // TODO: Should we re-use the name NServiceBus.Extensions.Diagnostics for minimal migration effort?
         public static readonly ActivitySource Main = new("NServiceBus.Diagnostics", "1.42.0");
     }
 }
