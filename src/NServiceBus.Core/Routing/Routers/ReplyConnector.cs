a21ecf6c3eede899954ebe70abacbaed13913a0a
Hadi Eskandari <h.eskandari@gmail.com>
2022-06-02 14:52:49 +1000

    reuse connector error handling logic in logical message stager
    Co-authored-by: Mike Minutillo <michael.minutillo@gmail.com>

--- src/NServiceBus.Core/Routing/Routers/ReplyConnector.cs
+++ src/NServiceBus.Core/Routing/Routers/ReplyConnector.cs
@@ -6,11 +6,10 @@ namespace NServiceBus
     using Extensibility;
     using Pipeline;
     using Routing;
-    using Unicast.Queuing;
 
     class ReplyConnector : StageConnector<IOutgoingReplyContext, IOutgoingLogicalMessageContext>
     {
-        public override async Task Invoke(IOutgoingReplyContext context, Func<IOutgoingLogicalMessageContext, Task> stage)
+        public override Task Invoke(IOutgoingReplyContext context, Func<IOutgoingLogicalMessageContext, Task> stage)
         {
             string replyToAddress = null;
             if (context.GetOperationProperties().TryGet(out State state))
@@ -33,27 +32,7 @@ namespace NServiceBus
             var addressLabels = new[] { new UnicastRoutingStrategy(replyToAddress) };
             var logicalMessageContext = this.CreateOutgoingLogicalMessageContext(context.Message, addressLabels, context);
 
-            try
-            {
-                await stage(logicalMessageContext).ConfigureAwait(false);
-            }
-            catch (QueueNotFoundException qnfe)
-            {
-                var err = new Exception($"The destination queue '{qnfe.Queue}' could not be found. " +
-                                        "It may be the case that the given queue hasn't been created yet, or has been deleted.", qnfe);
-                activity?.SetStatus(ActivityStatusCode.Error, err.Message);
-                throw err;
-            }
-#pragma warning disable PS0019 // Do not catch Exception without considering OperationCanceledException - enriching and rethrowing
-            catch (Exception ex)
-            {
-                activity?.SetStatus(ActivityStatusCode.Error, ex.Message);
-                throw;
-            }
-#pragma warning restore PS0019 // Do not catch Exception without considering OperationCanceledException
-
-            //TODO should we stop the acitivty only once the message has been handed to the dispatcher?
-            activity?.SetStatus(ActivityStatusCode.Ok); //Set acitivity state.
+            return LogicalMessageStager.Stage(stage, logicalMessageContext, activity);
         }
 
         static string GetReplyToAddressFromIncomingMessage(IOutgoingReplyContext context)
--- src/NServiceBus.Core/Routing/Routers/SendConnector.cs
+++ src/NServiceBus.Core/Routing/Routers/SendConnector.cs
@@ -4,7 +4,6 @@ namespace NServiceBus
     using System.Diagnostics;
     using System.Threading.Tasks;
     using Pipeline;
-    using Unicast.Queuing;
 
     class SendConnector : StageConnector<IOutgoingSendContext, IOutgoingLogicalMessageContext>
     {
@@ -13,7 +12,7 @@ namespace NServiceBus
             this.unicastSendRouter = unicastSendRouter;
         }
 
-        public override async Task Invoke(IOutgoingSendContext context, Func<IOutgoingLogicalMessageContext, Task> stage)
+        public override Task Invoke(IOutgoingSendContext context, Func<IOutgoingLogicalMessageContext, Task> stage)
         {
             using var activity = ActivitySources.Main.StartActivity(ActivityNames.OutgoingMessageActivityName, ActivityKind.Producer);
 
@@ -24,27 +23,8 @@ namespace NServiceBus
             context.Headers[Headers.MessageIntent] = MessageIntent.Send.ToString();
             var logicalMessageContext = this.CreateOutgoingLogicalMessageContext(context.Message, new[] { routingStrategy }, context);
 
-            try
-            {
-                await stage(logicalMessageContext).ConfigureAwait(false);
-            }
-            catch (QueueNotFoundException qnfe)
-            {
-                var err = new Exception($"The destination queue '{qnfe.Queue}' could not be found. " +
-                                        $"The destination may be misconfigured for this kind of message ({context.Message.MessageType}) in the routing section of the transport configuration. It may also be the case that the given queue hasn't been created yet, or has been deleted.", qnfe);
-                activity?.SetStatus(ActivityStatusCode.Error, err.Message);
-                throw err;
-            }
-#pragma warning disable PS0019 // Do not catch Exception without considering OperationCanceledException - enriching and rethrowing
-            catch (Exception ex)
-            {
-                activity?.SetStatus(ActivityStatusCode.Error, ex.Message);
-                throw;
-            }
-#pragma warning restore PS0019 // Do not catch Exception without considering OperationCanceledException
-
-            //TODO should we stop the acitivty only once the message has been handed to the dispatcher?
-            activity?.SetStatus(ActivityStatusCode.Ok); //Set acitivity state.
+            return LogicalMessageStager.Stage(stage, logicalMessageContext, activity,
+                $"The destination may be misconfigured for this kind of message ({context.Message.MessageType}) in the routing section of the transport configuration. ");
         }
 
         readonly UnicastSendRouter unicastSendRouter;
