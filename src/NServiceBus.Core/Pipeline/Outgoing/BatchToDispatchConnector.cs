66cd77d86b52aa4a1d6400efba67b09ad31fec9a
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-06-09 19:38:51 +0200

    test removing dispatch span

--- src/NServiceBus.Core/Pipeline/Outgoing/BatchToDispatchConnector.cs
+++ src/NServiceBus.Core/Pipeline/Outgoing/BatchToDispatchConnector.cs
@@ -10,17 +10,17 @@
     {
         public override async Task Invoke(IBatchDispatchContext context, Func<IDispatchContext, Task> stage)
         {
-            var activityLinks = context.Operations.Select(o =>
-            {
-                var operationActivityContext = ActivityContext.Parse(o.Properties.TraceParent, null);
-                return new ActivityLink(operationActivityContext);
-            });
-            //TODO do we need this activity at all?
-            using var activity = ActivitySources.Main.StartActivity(name: ActivityNames.MessageDispatchActivityName, links: activityLinks, kind: ActivityKind.Producer);
-            if (activity != null)
-            {
-                activity.DisplayName = "batch dispatch";
-            }
+            //var activityLinks = context.Operations.Select(o =>
+            //{
+            //    var operationActivityContext = ActivityContext.Parse(o.Properties.TraceParent, null);
+            //    return new ActivityLink(operationActivityContext);
+            //});
+            ////TODO do we need this activity at all?
+            //using var activity = ActivitySources.Main.StartActivity(name: ActivityNames.MessageDispatchActivityName, links: activityLinks, kind: ActivityKind.Producer);
+            //if (activity != null)
+            //{
+            //    activity.DisplayName = "batch dispatch";
+            //}
 
             await stage(this.CreateDispatchContext(context.Operations, context)).ConfigureAwait(false);
         }
