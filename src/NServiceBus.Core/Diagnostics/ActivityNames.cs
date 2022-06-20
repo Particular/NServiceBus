a1b3053b3cec835605efff5bdd1b4af636926af7
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-06-20 16:48:52 +0200

    remove dispatch span

--- src/NServiceBus.Core/Diagnostics/ActivityNames.cs
+++ src/NServiceBus.Core/Diagnostics/ActivityNames.cs
@@ -6,7 +6,6 @@ namespace NServiceBus
         public const string IncomingMessageActivityName = "NServiceBus.Diagnostics.IncomingMessage";
         public const string OutgoingMessageActivityName = "NServiceBus.Diagnostics.OutgoingMessage";
         public const string OutgoingEventActivityName = "NServiceBus.Diagnostics.PublishMessage";
-        public const string MessageDispatchActivityName = "NServiceBus.Diagnostics.Dispatch";
     }
 
     class DiagnosticsKeys
--- src/NServiceBus.Core/Pipeline/Outgoing/BatchToDispatchConnector.cs
+++ src/NServiceBus.Core/Pipeline/Outgoing/BatchToDispatchConnector.cs
@@ -1,46 +1,14 @@
 ﻿namespace NServiceBus
 {
     using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Threading.Tasks;
     using Pipeline;
-    using Transport;
 
     class BatchToDispatchConnector : StageConnector<IBatchDispatchContext, IDispatchContext>
     {
-        readonly bool createDispatchSpan;
-
-        public BatchToDispatchConnector(bool createDispatchSpan) => this.createDispatchSpan = createDispatchSpan;
-
-        public override async Task Invoke(IBatchDispatchContext context, Func<IDispatchContext, Task> stage)
+        public override Task Invoke(IBatchDispatchContext context, Func<IDispatchContext, Task> stage)
         {
-            using var activity = CreateDispatchActivity(context);
-
-            await stage(this.CreateDispatchContext(context.Operations, context)).ConfigureAwait(false);
-        }
-
-        Activity CreateDispatchActivity(IBatchDispatchContext context)
-        {
-            // The dispatch activity is optional for scenarios where a transport package or transport SDK creates spans that can't be directly connected to the send operation
-            if (!createDispatchSpan)
-            {
-                return null;
-            }
-
-            // link to send operations
-            var activityLinks = new List<ActivityLink>(context.Operations.Count);
-            foreach (TransportOperation transportOperation in context.Operations)
-            {
-                if (transportOperation.Message.Headers.TryGetValue(Headers.DiagnosticsTraceParent, out var traceparent))
-                {
-                    var operationActivityContext = ActivityContext.Parse(traceparent, null);
-                    activityLinks.Add(new ActivityLink(operationActivityContext));
-                }
-            }
-
-            return ActivitySources.Main.StartActivity(name: ActivityNames.MessageDispatchActivityName, links: activityLinks, kind: ActivityKind.Producer);
-
+            return stage(this.CreateDispatchContext(context.Operations, context));
         }
     }
 }
