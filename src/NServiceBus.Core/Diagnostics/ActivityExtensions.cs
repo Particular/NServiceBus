ad3a3a62027ae98dd725d3898923914e9cdab6df
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-07-05 17:17:00 +0200

    Update src/NServiceBus.Core/Diagnostics/ActivityExtensions.cs
    Co-authored-by: Mike Minutillo <mike.minutillo@particular.net>

--- src/NServiceBus.Core/Diagnostics/ActivityExtensions.cs
+++ src/NServiceBus.Core/Diagnostics/ActivityExtensions.cs
@@ -16,7 +16,7 @@ static class ActivityExtensions
 
     static bool TryGetRecordingPipelineActivity(this ContextBag pipelineContext, string activityKey, out Activity activity)
     {
-        if (Activity.Current != null // Cheaper to check than searching the pipeline context to start with. If there is no ambient activity, there can't be a context in the context.
+        if (Activity.Current != null // Cheaper to check than searching the pipeline context to start with. If there is no ambient activity, there can't be an activity in the context.
             && pipelineContext.TryGet(activityKey, out activity)  // Search activity in context bag
             && activity.IsAllDataRequested) // do not apply "expensive" work on non-recording activities
         {
