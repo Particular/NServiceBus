baaa88a1ffbfb9343279c71ace09c841249a1507
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-07-08 12:31:51 +0200

    introduce meter tags class

---
+++ src/NServiceBus.Core/Diagnostics/Metrics/MeterTags.cs
@@ -0,0 +1,9 @@
+﻿namespace NServiceBus;
+
+static class MeterTags
+{
+    public const string EndpointDiscriminator = "nservicebus.discriminator";
+    public const string QueueName = "nservicebus.queue";
+    public const string MessageType = "nservicebus.message_type";
+    public const string FailureType = "nservicebus.failure_type";
+}
