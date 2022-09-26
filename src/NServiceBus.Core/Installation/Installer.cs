412c24a220de0cc9f6f8fbf6ecda328a827cc1af
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-09-26 19:36:35 +0200

    add Installer API

---
+++ src/NServiceBus.Core/Installation/Installer.cs
@@ -0,0 +1,51 @@
+﻿namespace NServiceBus.Installation;
+
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using System.Threading;
+
+/// <summary>
+/// Provides methods to setup an NServiceBus endpoint.
+/// </summary>
+public static class Installer
+{
+
+    /// <summary>
+    /// Executes all the installers and transport configuration without starting the endpoint.
+    /// <see cref="Setup"/> always runs installers, even if <see cref="InstallConfigExtensions.EnableInstallers"/> has not been configured.
+    /// </summary>
+    public static async Task Setup(EndpointConfiguration configuration, CancellationToken cancellationToken = default)
+    {
+        Guard.AgainstNull(nameof(configuration), configuration);
+
+        // does not overwrite installer usernames configured by the user.
+        configuration.EnableInstallers();
+
+        var serviceCollection = new ServiceCollection();
+        var endpointCreator = EndpointCreator.Create(configuration, serviceCollection);
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        await using (serviceProvider.ConfigureAwait(false))
+        {
+            var endpoint = endpointCreator.CreateStartableEndpoint(serviceProvider, true);
+            await endpoint.RunInstallers(cancellationToken).ConfigureAwait(false);
+            await endpoint.Setup(cancellationToken).ConfigureAwait(false);
+        }
+    }
+
+    /// <summary>
+    /// Creates an instance of <see cref="InstallerWithExternallyManagedContainer"/> that can be used to setup an NServiceBus when access to externally registered container dependencies are required.
+    /// </summary>
+    public static InstallerWithExternallyManagedContainer CreateInstallerWithExternallyManagedContainer(EndpointConfiguration configuration, IServiceCollection serviceCollection)
+    {
+        Guard.AgainstNull(nameof(configuration), configuration);
+        Guard.AgainstNull(nameof(serviceCollection), serviceCollection);
+
+        // does not overwrite installer usernames configured by the user.
+        configuration.EnableInstallers();
+
+        var endpointCreator = EndpointCreator.Create(configuration, serviceCollection);
+
+        return new InstallerWithExternallyManagedContainer(endpointCreator);
+    }
+}
---
+++ src/NServiceBus.Core/Installation/InstallerWithExternallyManagedContainer.cs
@@ -0,0 +1,29 @@
+﻿namespace NServiceBus.Installation;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Provides methods to setup an NServiceBus endpoint with an externally managed dependency injection container.
+/// </summary>
+public class InstallerWithExternallyManagedContainer
+{
+    readonly EndpointCreator endpointCreator;
+
+    internal InstallerWithExternallyManagedContainer(EndpointCreator endpointCreator)
+    {
+        this.endpointCreator = endpointCreator;
+    }
+
+    /// <summary>
+    /// Executes all the installers and transport configuration without starting the endpoint.
+    /// Installers are always run, even if <see cref="InstallConfigExtensions.EnableInstallers"/> has not been configured.
+    /// </summary>
+    public async Task Setup(IServiceProvider builder, CancellationToken cancellationToken = default)
+    {
+        var endpoint = endpointCreator.CreateStartableEndpoint(builder, false);
+        await endpoint.RunInstallers(cancellationToken).ConfigureAwait(false);
+        await endpoint.Setup(cancellationToken).ConfigureAwait(false);
+    }
+}
