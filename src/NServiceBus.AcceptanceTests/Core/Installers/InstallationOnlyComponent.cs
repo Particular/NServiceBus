0e3594851d4a4d938605c9986ed988da31ecac88
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-10-06 22:24:50 +0200

    Add dedicated Installer APIs (#6563)
    Co-authored-by: Kyle Baley <kyle@baley.org>

---
+++ src/NServiceBus.AcceptanceTests/Core/Installers/InstallationOnlyComponent.cs
@@ -0,0 +1,53 @@
+﻿namespace NServiceBus.AcceptanceTests.Core.Installers;
+
+using System.Threading;
+using System.Threading.Tasks;
+using AcceptanceTesting;
+using AcceptanceTesting.Customization;
+using AcceptanceTesting.Support;
+using Configuration.AdvancedExtensibility;
+using Installation;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Custom test component that uses the <see cref="Installer.Setup"/> API instead of fully starting the endpoint.
+/// </summary>
+public class InstallationOnlyComponent<TConfigurationFactory> : IComponentBehavior
+    where TConfigurationFactory : IEndpointConfigurationFactory, new()
+{
+    public async Task<ComponentRunner> CreateRunner(RunDescriptor run)
+    {
+        var configurationFactory = new TConfigurationFactory();
+        var customizationConfiguration = configurationFactory.Get();
+        customizationConfiguration.EndpointName = Conventions.EndpointNamingConvention(configurationFactory.GetType());
+        var endpointConfiguration = await customizationConfiguration.GetConfiguration(run);
+        RegisterScenarioContext(endpointConfiguration, run.ScenarioContext);
+        return new InstallationRunner(endpointConfiguration);
+    }
+
+    void RegisterScenarioContext(EndpointConfiguration endpointConfiguration, ScenarioContext scenarioContext)
+    {
+        var type = scenarioContext.GetType();
+        while (type != typeof(object))
+        {
+            var currentType = type;
+            endpointConfiguration.GetSettings().Set(currentType.FullName, scenarioContext);
+            endpointConfiguration.RegisterComponents(serviceCollection => serviceCollection.AddSingleton(currentType, scenarioContext));
+            type = type.BaseType;
+        }
+    }
+
+    public class InstallationRunner : ComponentRunner
+    {
+        EndpointConfiguration endpointConfiguration;
+
+        public InstallationRunner(EndpointConfiguration endpointConfiguration)
+        {
+            this.endpointConfiguration = endpointConfiguration;
+        }
+
+        public override string Name => "Installation only runner";
+
+        public override Task Start(CancellationToken cancellationToken = default) => Installer.Setup(endpointConfiguration, cancellationToken);
+    }
+}
---
+++ src/NServiceBus.AcceptanceTests/Core/Installers/When_installing_endpoint.cs
@@ -0,0 +1,104 @@
+﻿namespace NServiceBus.AcceptanceTests.Core.Installers;
+
+using System.Threading;
+using System.Threading.Tasks;
+using AcceptanceTesting;
+using EndpointTemplates;
+using FakeTransport;
+using Features;
+using Installation;
+using Configuration.AdvancedExtensibility;
+using NUnit.Framework;
+using Transport;
+
+public class When_installing_endpoint : NServiceBusAcceptanceTest
+{
+    [Test]
+    public async Task Should_only_execute_setup_and_complete()
+    {
+        var context = await Scenario.Define<Context>()
+            .WithComponent(new InstallationOnlyComponent<EndpointWithInstaller>())
+            .Run();
+
+        Assert.IsTrue(context.InstallerCalled, "Should run installers");
+        Assert.IsTrue(context.FeatureSetupCalled, "Should initialize Features");
+        Assert.IsFalse(context.FeatureStartupTaskCalled, "Should not start FeatureStartupTasks");
+        CollectionAssert.AreEqual(context.TransportStartupSequence, new string[]
+        {
+            $"{nameof(TransportDefinition)}.{nameof(TransportDefinition.Initialize)}",
+            $"{nameof(IMessageReceiver)}.{nameof(IMessageReceiver.Initialize)} for receiver Main",
+        }, "Should not start the receivers");
+    }
+
+    class Context : ScenarioContext
+    {
+        public bool InstallerCalled { get; set; }
+        public bool FeatureSetupCalled { get; set; }
+        public bool FeatureStartupTaskCalled { get; set; }
+        public FakeTransport.StartUpSequence TransportStartupSequence { get; set; }
+    }
+
+    class EndpointWithInstaller : EndpointConfigurationBuilder
+    {
+        public EndpointWithInstaller()
+        {
+            EndpointSetup<DefaultServer>((c, r) =>
+            {
+                // Disable installers (enabled by default in DefaultServer)
+                c.GetSettings().Set("Installers.Enable", true);
+
+                c.EnableFeature<CustomFeature>();
+
+                // Register FakeTransport to track transport seam usage during installation
+                var fakeTransport = new FakeTransport();
+                c.UseTransport(fakeTransport);
+                ((Context)r.ScenarioContext).TransportStartupSequence = fakeTransport.StartupSequence;
+            });
+        }
+
+        class CustomInstaller : INeedToInstallSomething
+        {
+            Context testContext;
+
+            public CustomInstaller(Context testContext)
+            {
+                this.testContext = testContext;
+            }
+
+            public Task Install(string identity, CancellationToken cancellationToken = default)
+            {
+                testContext.InstallerCalled = true;
+                return Task.CompletedTask;
+            }
+        }
+
+        class CustomFeature : Feature
+        {
+            protected override void Setup(FeatureConfigurationContext context)
+            {
+                var testContext = context.Settings.Get<Context>();
+                testContext.FeatureSetupCalled = true;
+
+                context.RegisterStartupTask(new CustomFeatureStartupTask(testContext));
+            }
+
+            class CustomFeatureStartupTask : FeatureStartupTask
+            {
+                readonly Context testContext;
+
+                public CustomFeatureStartupTask(Context testContext)
+                {
+                    this.testContext = testContext;
+                }
+
+                protected override Task OnStart(IMessageSession session, CancellationToken cancellationToken = default)
+                {
+                    testContext.FeatureStartupTaskCalled = true;
+                    return Task.CompletedTask;
+                }
+
+                protected override Task OnStop(IMessageSession session, CancellationToken cancellationToken = default) => Task.CompletedTask;
+            }
+        }
+    }
+}
