5f8690b3760b48f5d2329bb731c596c0201857c1
Mauro Servienti <mauro.servienti@gmail.com>
2022-11-24 17:10:18 +0100

    Persistence Tests - Make sure stale records are modified before save (#6603) (#6604)
    * Make sure stale records are modified before save
    
    Co-Authored-By: Daniel Marbach <daniel.marbach@openplace.net>
    
    * Update the stale record outside the Assert
    
    Co-authored-by: Daniel Marbach <daniel.marbach@openplace.net>
    
    Co-authored-by: Daniel Marbach <daniel.marbach@openplace.net>

--- src/NServiceBus.PersistenceTests/Sagas/When_updating_saga_concurrently_on_same_thread.cs
+++ src/NServiceBus.PersistenceTests/Sagas/When_updating_saga_concurrently_on_same_thread.cs
@@ -42,6 +42,7 @@
 
             try
             {
+                staleRecord.DateTimeProperty = DateTime.UtcNow.AddHours(1);
                 Assert.CatchAsync<Exception>(async () =>
                 {
                     await persister.Update(staleRecord, losingSaveSession, losingContext);
