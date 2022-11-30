628109e29fe7f8ff1c6283b2c5e79a7c075618e9
lailabougria <laila.bougria@particular.net>
2022-11-30 10:17:20 +0100

    nits and cleanup

--- src/NServiceBus.AcceptanceTests/Core/Recoverability/When_unrecoverable_exception_thrown.cs
+++ src/NServiceBus.AcceptanceTests/Core/Recoverability/When_unrecoverable_exception_thrown.cs
@@ -1,8 +1,8 @@
 ﻿namespace NServiceBus.AcceptanceTests.Recoverability
 {
+    using System.Linq;
     using System.Threading.Tasks;
     using AcceptanceTesting;
-    using AcceptanceTesting.Customization;
     using EndpointTemplates;
     using NUnit.Framework;
 
@@ -12,13 +12,16 @@
         public async Task Should_move_to_error_queue_without_retries()
         {
             var context = await Scenario.Define<Context>()
-                .WithEndpoint<EndpointThatThrowsUnrecoverableExceptions>(b => b.DoNotFailOnErrorMessages()
+                .WithEndpoint<EndpointThatThrowsUnrecoverableExceptions>(b => b
+                    .DoNotFailOnErrorMessages()
                     .When((session, c) => session.SendLocal(new MessageOne()))
                 )
-                .WithEndpoint<ErrorSpy>()
-                .Done(c => c.MessageMovedToErrorQueue)
+                .Done(c => c.FailedMessages.Any())
                 .Run();
 
+            var failedMessages = context.FailedMessages.Single().Value;
+            Assert.AreEqual(1, failedMessages.Count);
+            Assert.IsInstanceOf<UnrecoverableException>(failedMessages.First().Exception);
             Assert.AreEqual(1, context.NrOfTimesHandlerWasInvoked);
         }
 
@@ -26,13 +29,16 @@
         public async Task Should_move_to_error_queue_without_retries_when_inheriting_from_unrecoverable_ex()
         {
             var context = await Scenario.Define<Context>()
-                .WithEndpoint<EndpointThatThrowsUnrecoverableExceptions>(b => b.DoNotFailOnErrorMessages()
+                .WithEndpoint<EndpointThatThrowsUnrecoverableExceptions>(b => b
+                    .DoNotFailOnErrorMessages()
                     .When((session, c) => session.SendLocal(new MessageTwo()))
                 )
-                .WithEndpoint<ErrorSpy>()
-                .Done(c => c.MessageMovedToErrorQueue)
+                .Done(c => c.FailedMessages.Any())
                 .Run();
 
+            var failedMessages = context.FailedMessages.Single().Value;
+            Assert.AreEqual(1, failedMessages.Count);
+            Assert.IsInstanceOf<SpecificException>(failedMessages.First().Exception);
             Assert.AreEqual(1, context.NrOfTimesHandlerWasInvoked);
         }
 
@@ -46,10 +52,7 @@
         {
             public EndpointThatThrowsUnrecoverableExceptions()
             {
-                EndpointSetup<DefaultServer>((config, context) =>
-                {
-                    config.SendFailedMessagesTo(Conventions.EndpointNamingConvention(typeof(ErrorSpy)));
-                });
+                EndpointSetup<DefaultServer>();
             }
 
             class MessageOneHandler : IHandleMessages<MessageOne>
@@ -83,52 +86,12 @@
 
                 Context testContext;
             }
-
-            class SpecificException : UnrecoverableException
-            {
-                public SpecificException(string message) : base(message)
-                {
-                }
-            }
         }
 
-        class ErrorSpy : EndpointConfigurationBuilder
+        class SpecificException : UnrecoverableException
         {
-            public ErrorSpy()
+            public SpecificException(string message) : base(message)
             {
-                EndpointSetup<DefaultServer>();
-            }
-
-            class MessageOneHandler : IHandleMessages<MessageOne>
-            {
-                public MessageOneHandler(Context context)
-                {
-                    testContext = context;
-                }
-
-                public Task Handle(MessageOne message, IMessageHandlerContext context)
-                {
-                    testContext.MessageMovedToErrorQueue = true;
-                    return Task.CompletedTask;
-                }
-
-                Context testContext;
-            }
-
-            class MessageTwoHandler : IHandleMessages<MessageTwo>
-            {
-                public MessageTwoHandler(Context context)
-                {
-                    testContext = context;
-                }
-
-                public Task Handle(MessageTwo message, IMessageHandlerContext context)
-                {
-                    testContext.MessageMovedToErrorQueue = true;
-                    return Task.CompletedTask;
-                }
-
-                Context testContext;
             }
         }
 
