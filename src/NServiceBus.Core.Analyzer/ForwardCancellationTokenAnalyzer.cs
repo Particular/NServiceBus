5ee4de1756e0110a3b37a9c0bca45f3442661345
David Boike <david.boike@gmail.com>
2022-12-02 12:34:04 -0600

    namespace

--- src/NServiceBus.Core.Analyzer/ForwardCancellationTokenAnalyzer.cs
+++ src/NServiceBus.Core.Analyzer/ForwardCancellationTokenAnalyzer.cs
@@ -3,6 +3,7 @@
     using System.Collections.Generic;
     using System.Collections.Immutable;
     using System.Linq;
+    using System.Threading;
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
