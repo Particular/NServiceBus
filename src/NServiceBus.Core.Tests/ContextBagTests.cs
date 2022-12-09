2785cd2593407a3535b55ac5e002adbeeb4b2f29
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-12-09 20:22:31 +0100

    add ContextBag.SetOnRoot unit test

--- src/NServiceBus.Core.Tests/ContextBagTests.cs
+++ src/NServiceBus.Core.Tests/ContextBagTests.cs
@@ -16,5 +16,22 @@
             ((IReadOnlyContextBag)contextBag).TryGet("MonkeyPatch", out string theValue);
             Assert.AreEqual("some string", theValue);
         }
+
+        [Test]
+        public void SetOnRoot_should_set_value_on_root_context()
+        {
+            const string key = "testkey";
+
+            var root = new ContextBag();
+            var intermediate = new ContextBag(root);
+            var context = new ContextBag(intermediate);
+            var fork = new ContextBag(intermediate);
+
+            context.SetOnRoot(key, 42);
+
+            Assert.AreEqual(42, root.Get<int>(key), "should store value on root context");
+            Assert.AreEqual(42, context.Get<int>(key), "stored value should be readable in the writing context");
+            Assert.AreEqual(42, fork.Get<int>(key), "stored value should be visible to a forked context");
+        }
     }
 }
