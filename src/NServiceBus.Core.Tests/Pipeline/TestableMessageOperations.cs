f59de10f86699477191cd9f593e708d2d7cb0163
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-12-09 19:50:16 +0100

    Add RecoverabilityExecutorTests

---
+++ src/NServiceBus.Core.Tests/Pipeline/TestableMessageOperations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NServiceBus.Pipeline;
+using System.Threading.Tasks;
+using NServiceBus.MessageInterfaces.MessageMapper.Reflection;
+
+namespace NServiceBus.Core.Tests.Pipeline;
+
+class TestableMessageOperations : MessageOperations
+{
+    public Pipeline<IOutgoingPublishContext> PublishPipeline => (Pipeline<IOutgoingPublishContext>)publishPipeline;
+    public Pipeline<IOutgoingSendContext> SendPipeline => (Pipeline<IOutgoingSendContext>)sendPipeline;
+    public Pipeline<IOutgoingReplyContext> ReplyPipeline => (Pipeline<IOutgoingReplyContext>)replyPipeline;
+    public Pipeline<ISubscribeContext> SubscribePipeline => (Pipeline<ISubscribeContext>)subscribePipeline;
+    public Pipeline<IUnsubscribeContext> UnsubscribePipeline => (Pipeline<IUnsubscribeContext>)unsubscribePipeline;
+
+    public TestableMessageOperations() : base(new MessageMapper(), new Pipeline<IOutgoingPublishContext>(), new Pipeline<IOutgoingSendContext>(), new Pipeline<IOutgoingReplyContext>(), new Pipeline<ISubscribeContext>(), new Pipeline<IUnsubscribeContext>(), new ActivityFactory())
+    {
+    }
+
+    public class Pipeline<TContext> : IPipeline<TContext> where TContext : IBehaviorContext
+    {
+        public Action<TContext> OnInvoke { get; set; }
+
+        public TContext Context { get; private set; }
+
+        public Task Invoke(TContext context)
+        {
+            Context = context;
+            OnInvoke?.Invoke(context);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/NServiceBus.Core.Tests/Recoverability/RecoverabilityExecutorTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus.Core.Tests.Pipeline;
+using NServiceBus.Extensibility;
+using NServiceBus.Pipeline;
+using NServiceBus.Transport;
+using NUnit.Framework;
+
+namespace NServiceBus.Core.Tests.Recoverability
+{
+    [TestFixture]
+    public class RecoverabilityExecutorTests
+    {
+        [Test]
+        public async Task Should_use_error_context_extensions_as_extensions_root()
+        {
+            var newValue = Guid.NewGuid();
+            var existingValue = Guid.NewGuid();
+
+            ContextBag pipelineExtensions = null;
+            var recoverabilityPipeline = new TestableMessageOperations.Pipeline<IRecoverabilityContext>
+            {
+                OnInvoke = context =>
+                {
+                    context.Extensions.SetOnRoot("new value", newValue);
+                    pipelineExtensions = context.Extensions;
+                }
+            };
+
+            var executor = CreateRecoverabilityExecutor(recoverabilityPipeline);
+
+            ErrorContext errorContext = CreateErrorContext();
+            errorContext.Extensions.Set("existing value", existingValue);
+
+            await executor.Invoke(errorContext);
+
+            Assert.AreEqual(newValue, errorContext.Extensions.Get<Guid>("new value"));
+            Assert.AreEqual(existingValue, pipelineExtensions.Get<Guid>("existing value"));
+        }
+
+        private static RecoverabilityPipelineExecutor<object> CreateRecoverabilityExecutor(TestableMessageOperations.Pipeline<IRecoverabilityContext> recoverabilityPipeline)
+        {
+            var executor = new RecoverabilityPipelineExecutor<object>(
+                new ServiceCollection().BuildServiceProvider(),
+                null,
+                new TestableMessageOperations(),
+                null, (_, _) => RecoverabilityAction.Discard("test"),
+                recoverabilityPipeline,
+                new FaultMetadataExtractor(new Dictionary<string, string>(0), _ => { }),
+                null);
+            return executor;
+        }
+
+        private static ErrorContext CreateErrorContext() => new(new Exception("test"), new Dictionary<string, string>(), Guid.NewGuid().ToString(), Array.Empty<byte>(), new TransportTransaction(), 10, "receive address", new ContextBag());
+    }
+}
