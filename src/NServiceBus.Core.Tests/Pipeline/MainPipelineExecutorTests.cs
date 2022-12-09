18ec89cdc42a3b2b33ae580bba8df9a30c853a85
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-12-09 19:00:34 +0100

    add test for shared context in MainPipelineExecutorTests

--- src/NServiceBus.Core.Tests/Pipeline/MainPipelineExecutorTests.cs
+++ src/NServiceBus.Core.Tests/Pipeline/MainPipelineExecutorTests.cs
@@ -14,64 +14,85 @@
     [TestFixture]
     public class MainPipelineExecutorTests
     {
-        TestingActivityListener nsbActivityListener;
-
-        [OneTimeSetUp]
-        public void Setup()
-        {
-            nsbActivityListener = TestingActivityListener.SetupNServiceBusDiagnosticListener();
-        }
-
-        [OneTimeTearDown]
-        public void TearDown()
-        {
-            nsbActivityListener.Dispose();
-        }
-
         [Test]
-        public async Task When_invoking_pipeline()
+        public async Task Should_use_message_context_extensions_as_context_root()
         {
+            var existingValue = Guid.NewGuid();
+            var newValue = Guid.NewGuid();
+
             var executor = CreateMainPipelineExecutor(out var receivePipeline);
             var messageContext = CreateMessageContext();
-
+            messageContext.Extensions.Set("existing value", existingValue);
             await executor.Invoke(messageContext);
 
-            Assert.NotNull(receivePipeline.PipelineAcitivty);
-            Assert.AreEqual(ActivityNames.IncomingMessageActivityName, receivePipeline.PipelineAcitivty.OperationName);
-            Assert.AreEqual("process message", receivePipeline.PipelineAcitivty.DisplayName);
-            Assert.AreEqual(receivePipeline.PipelineAcitivty, receivePipeline.TransportReceiveContext.Extensions.Get<Activity>(ActivityExtensions.IncomingActivityKey));
+            Assert.AreEqual(existingValue, receivePipeline.TransportReceiveContext.Extensions.Get<Guid>("existing value"));
+
+            receivePipeline.TransportReceiveContext.Extensions.SetOnRoot("new value", newValue);
+            Assert.AreEqual(newValue, messageContext.Extensions.Get<Guid>("new value"));
         }
 
-        [Test]
-        public async Task When_pipeline_successful()
+        class When_activity_listener_registered
         {
-            var executor = CreateMainPipelineExecutor(out var receivePipeline);
+            TestingActivityListener nsbActivityListener;
 
-            await executor.Invoke(CreateMessageContext());
+            [OneTimeSetUp]
+            public void Setup()
+            {
+                nsbActivityListener = TestingActivityListener.SetupNServiceBusDiagnosticListener();
+            }
 
-            Assert.AreEqual(ActivityStatusCode.Ok, receivePipeline.PipelineAcitivty.Status);
+            [OneTimeTearDown]
+            public void TearDown()
+            {
+                nsbActivityListener.Dispose();
+            }
+
+            [Test]
+            public async Task Should_start_Activity_when_invoking_pipeline()
+            {
+                var executor = CreateMainPipelineExecutor(out var receivePipeline);
+                var messageContext = CreateMessageContext();
+
+                await executor.Invoke(messageContext);
+
+                Assert.NotNull(receivePipeline.PipelineAcitivty);
+                Assert.AreEqual(ActivityNames.IncomingMessageActivityName, receivePipeline.PipelineAcitivty.OperationName);
+                Assert.AreEqual("process message", receivePipeline.PipelineAcitivty.DisplayName);
+                Assert.AreEqual(receivePipeline.PipelineAcitivty, receivePipeline.TransportReceiveContext.Extensions.Get<Activity>(ActivityExtensions.IncomingActivityKey));
+            }
+
+            [Test]
+            public async Task Should_set_ok_status_on_activity_when_pipeline_successful()
+            {
+                var executor = CreateMainPipelineExecutor(out var receivePipeline);
+
+                await executor.Invoke(CreateMessageContext());
+
+                Assert.AreEqual(ActivityStatusCode.Ok, receivePipeline.PipelineAcitivty.Status);
+            }
+
+            [Test]
+            public void Should_set_error_status_on_activity_when_pipeline_throws_exception()
+            {
+                var executor = CreateMainPipelineExecutor(out var receivePipeline);
+                receivePipeline.ThrowsException = true;
+
+                Assert.ThrowsAsync<Exception>(async () => await executor.Invoke(CreateMessageContext()));
+
+                Assert.AreEqual(ActivityStatusCode.Error, receivePipeline.PipelineAcitivty.Status);
+            }
         }
+        
 
-        [Test]
-        public void When_pipeline_throws_exception()
-        {
-            var executor = CreateMainPipelineExecutor(out var receivePipeline);
-            receivePipeline.ThrowsException = true;
-
-            Assert.ThrowsAsync<Exception>(async () => await executor.Invoke(CreateMessageContext()));
-
-            Assert.AreEqual(ActivityStatusCode.Error, receivePipeline.PipelineAcitivty.Status);
-        }
-
-        static MessageContext CreateMessageContext(Dictionary<string, string> messageHeaders = null, ContextBag contextBag = null)
+        static MessageContext CreateMessageContext()
         {
             return new MessageContext(
                 Guid.NewGuid().ToString(),
-                messageHeaders ?? new Dictionary<string, string>(),
+                new Dictionary<string, string>(),
                 Array.Empty<byte>(),
                 new TransportTransaction(),
                 "receiver",
-                contextBag ?? new ContextBag());
+                new ContextBag());
         }
 
         static MainPipelineExecutor CreateMainPipelineExecutor(out ReceivePipeline receivePipeline)
