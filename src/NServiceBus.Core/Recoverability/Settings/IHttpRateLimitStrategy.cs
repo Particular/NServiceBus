e4c984da5612290f5b860fd01ec822670e8547fd
lailabougria <laila.bougria@particular.net>
2022-12-13 11:08:12 +0100

    httpratelimitstrategy

---
+++ src/NServiceBus.Core/Recoverability/Settings/IHttpRateLimitStrategy.cs
@@ -0,0 +1,25 @@
+﻿namespace NServiceBus.Recoverability.Settings;
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+/// <summary>
+/// A contract that allows for customization of the delayed retry strategy based on the <see cref="HttpRequestException"/> and <see cref="HttpResponseHeaders"/>.
+/// </summary>
+public interface IHttpRateLimitStrategy
+{
+    /// <summary>
+    /// Status code returned by the HTTP API
+    /// </summary>
+    HttpStatusCode StatusCode { get; }
+
+    /// <summary>
+    /// Gets the delay to apply to the next retry.
+    /// </summary>
+    /// <param name="exception">The exception that occurred.</param>
+    /// <param name="headers">The response headers.</param>
+    /// <returns></returns>
+    TimeSpan? GetDelay(HttpRequestException exception, HttpResponseHeaders headers);
+}
