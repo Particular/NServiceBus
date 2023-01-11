377b85f7130073537584f4a0f9f78b238e06efb1
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-01-11 16:24:37 +0100

    add repro test

---
+++ src/NServiceBus.AcceptanceTests/Recoverability/When_custom_policy_discards_message.cs
@@ -0,0 +1,56 @@
+﻿namespace NServiceBus.AcceptanceTests.Recoverability
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_custom_policy_discards_failed_message : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_consume_message_without_retries()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<EndpointWithDiscardPolicy>(e => e
+                    .When(s => s.SendLocal(new FailingMessage())))
+                .Done(c => c.HandlerInvoked >= 1)
+                .Run();
+
+            Assert.IsEmpty(context.FailedMessages, "the message should not be moved to the error queue");
+            Assert.AreEqual(1, context.HandlerInvoked, "the discarded message should not be retried");
+        }
+
+        class Context : ScenarioContext
+        {
+            public int HandlerInvoked { get; set; }
+        }
+
+        class EndpointWithDiscardPolicy : EndpointConfigurationBuilder
+        {
+            public EndpointWithDiscardPolicy() =>
+                EndpointSetup<DefaultServer>(c => c
+                    .Recoverability()
+                    .CustomPolicy((config, context) => RecoverabilityAction.Discard("discard on purpose")));
+        }
+
+        class FailingMessageHandler : IHandleMessages<FailingMessage>
+        {
+            Context testContext;
+
+            public FailingMessageHandler(Context testContext)
+            {
+                this.testContext = testContext;
+            }
+
+            public Task Handle(FailingMessage message, IMessageHandlerContext context)
+            {
+                testContext.HandlerInvoked++;
+                throw new SimulatedException();
+            }
+        }
+
+        public class FailingMessage : IMessage
+        {
+        }
+    }
+}
