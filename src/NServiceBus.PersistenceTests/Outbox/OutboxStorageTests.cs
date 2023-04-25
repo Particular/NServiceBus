85d1f18ee13a7a9e743b56ad0c8aae4df645bd10
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-04-25 10:48:56 +0200

    Add preflight get to outbox test (#6725)
    * add preflight get
    
    * Public and discard
    
    * Array.Empty and spelling
    
    * Spelling
    
    * Assert.That
    
    * Add transport operation message id verification
    
    Co-authored-by: Tim Bussmann <timbussmann@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: danielmarbach <daniel.marbach@openplace.net>

--- src/NServiceBus.PersistenceTests/Outbox/OutboxStorageTests.cs
+++ src/NServiceBus.PersistenceTests/Outbox/OutboxStorageTests.cs
@@ -6,7 +6,7 @@
     using NUnit.Framework;
 
     [TestFixtureSource(typeof(PersistenceTestsConfiguration), nameof(PersistenceTestsConfiguration.OutboxVariants))]
-    class OutboxStorageTests
+    public class OutboxStorageTests
     {
         public OutboxStorageTests(TestVariant param)
         {
@@ -35,7 +35,10 @@
             var ctx = configuration.GetContextBagForOutbox();
 
             string messageId = Guid.NewGuid().ToString();
-            var messageToStore = new OutboxMessage(messageId, new[] { new TransportOperation("x", null, null, null) });
+            _ = await storage.Get(messageId, ctx);
+
+            string transportOperationMessageId = Guid.NewGuid().ToString();
+            var messageToStore = new OutboxMessage(messageId, new[] { new TransportOperation(transportOperationMessageId, null, null, null) });
             using (var transaction = await storage.BeginTransaction(ctx))
             {
                 await storage.Store(messageToStore, transaction, ctx);
@@ -47,7 +50,8 @@
 
             Assert.IsNotNull(message);
             Assert.AreEqual(messageId, message.MessageId);
-            Assert.AreEqual(1, message.TransportOperations.Length);
+            Assert.That(message.TransportOperations, Has.Length.EqualTo(1));
+            Assert.That(message.TransportOperations[0].MessageId, Is.EqualTo(transportOperationMessageId));
         }
 
         [Test]
@@ -59,7 +63,7 @@
             var ctx = configuration.GetContextBagForOutbox();
 
             var messageId = Guid.NewGuid().ToString();
-            await storage.Get(messageId, ctx);
+            _ = await storage.Get(messageId, ctx);
 
             var messageToStore = new OutboxMessage(messageId, new[] { new TransportOperation("x", null, null, null) });
             using (var transaction = await storage.BeginTransaction(ctx))
@@ -82,37 +86,31 @@
         {
             configuration.RequiresOutboxSupport();
 
-            var failed = false;
-
             var storage = configuration.OutboxStorage;
             var winningContextBag = configuration.GetContextBagForOutbox();
             var losingContextBag = configuration.GetContextBagForOutbox();
-            await storage.Get("MySpecialId", winningContextBag);
-            await storage.Get("MySpecialId", losingContextBag);
+            _ = await storage.Get("MySpecialId", winningContextBag);
+            _ = await storage.Get("MySpecialId", losingContextBag);
 
             using (var transactionA = await storage.BeginTransaction(winningContextBag))
             {
-                await storage.Store(new OutboxMessage("MySpecialId", new TransportOperation[0]), transactionA, winningContextBag);
+                await storage.Store(new OutboxMessage("MySpecialId", Array.Empty<TransportOperation>()), transactionA, winningContextBag);
                 await transactionA.Commit();
             }
 
-            try
+            Assert.That(async () =>
             {
                 using (var transactionB = await storage.BeginTransaction(losingContextBag))
                 {
-                    await storage.Store(new OutboxMessage("MySpecialId", new TransportOperation[0]), transactionB, losingContextBag);
+                    await storage.Store(new OutboxMessage("MySpecialId", Array.Empty<TransportOperation>()),
+                        transactionB, losingContextBag);
                     await transactionB.Commit();
                 }
-            }
-            catch (Exception)
-            {
-                failed = true;
-            }
-            Assert.IsTrue(failed);
+            }, Throws.Exception);
         }
 
         [Test]
-        public async Task Should_not_store_when_transaction_not_commited()
+        public async Task Should_not_store_when_transaction_not_committed()
         {
             configuration.RequiresOutboxSupport();
 
@@ -120,7 +118,7 @@
             var ctx = configuration.GetContextBagForOutbox();
 
             var messageId = Guid.NewGuid().ToString();
-            await storage.Get(messageId, ctx);
+            _ = await storage.Get(messageId, ctx);
 
             using (var transaction = await storage.BeginTransaction(ctx))
             {
@@ -135,7 +133,7 @@
         }
 
         [Test]
-        public async Task Should_store_when_transaction_commited()
+        public async Task Should_store_when_transaction_committed()
         {
             configuration.RequiresOutboxSupport();
 
@@ -143,7 +141,7 @@
             var ctx = configuration.GetContextBagForOutbox();
 
             var messageId = Guid.NewGuid().ToString();
-            await storage.Get(messageId, ctx);
+            _ = await storage.Get(messageId, ctx);
 
             using (var transaction = await storage.BeginTransaction(ctx))
             {
