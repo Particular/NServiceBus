b48bd9ebfc5cade8e27a17ccbf8e2c5683be50ad
Andreas Öhlund <andreas.ohlund@particular.net>
2023-06-11 09:06:33 +0200

    Add test ti document that interfaces are not supported

---
+++ src/NServiceBus.AcceptanceTests/Core/JsonSerializer/When_deserializing_interface_message.cs
@@ -0,0 +1,56 @@
+﻿namespace NServiceBus.AcceptanceTests.Core.JsonSerializer
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_deserializing_interface_message : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_blow_up()
+        {
+            var context = await Scenario.Define<Context>()
+               .WithEndpoint<Endpoint>(c => c
+                   .When(b => b.SendLocal<IMyMessage>(_ => { }))
+                   .DoNotFailOnErrorMessages())
+               .Done(c => c.FailedMessages.Any())
+               .Run();
+
+            var failedMessage = context.FailedMessages.SingleOrDefault();
+
+            Assert.NotNull(failedMessage);
+
+            var exception = failedMessage.Value.First().Exception;
+
+            Assert.IsInstanceOf<MessageDeserializationException>(exception);
+            StringAssert.Contains("interface", exception.ToString());
+        }
+
+        public class Context : ScenarioContext
+        {
+        }
+
+        public class Endpoint : EndpointConfigurationBuilder
+        {
+            public Endpoint()
+            {
+                EndpointSetup<DefaultServer>((c, r) =>
+                {
+                    c.UseSerialization<SystemJsonSerializer>();
+                });
+            }
+
+            class MyHandler : IHandleMessages<IMyMessage>
+            {
+                public Task Handle(IMyMessage message, IMessageHandlerContext context) => throw new NotImplementedException();
+            }
+        }
+
+        public interface IMyMessage
+        {
+        }
+    }
+}
