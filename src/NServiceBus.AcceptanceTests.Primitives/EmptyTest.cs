0e775a893c4e9aa93dbb0a9cb072558a6d875acc
Mauro Servienti <mauro.servienti@gmail.com>
2023-09-04 17:21:57 +0200

    Add an empty test to avoid CI failing because in a *Tests project there are no tests

---
+++ src/NServiceBus.AcceptanceTests.Primitives/EmptyTest.cs
@@ -0,0 +1,10 @@
+﻿namespace NServiceBus.AcceptanceTests.Versioning
+{
+    using NUnit.Framework;
+
+    class EmptyTest
+    {
+        [Test]
+        public void To_prevent_CI_from_failing_because_a_tests_project_contains_no_tests() => Assert.Ignore();
+    }
+}
