163c29244677f62454a2af14c92341a93a2d5bf9
Mauro Servienti <mauro.servienti@gmail.com>
2023-09-04 16:48:35 +0200

    Move the TestSuiteConstraints Current to a static interface property
    Co-Authored-By: Szymon Pobiega <szymon@pobiega.com>
    Co-Authored-By: Kyle Baley <130546+kbaley@users.noreply.github.com>

---
+++ src/NServiceBus.AcceptanceTests.Primitives/EndpointTemplates/ITestSuiteConstraints.cs
@@ -0,0 +1,40 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using System;
+    using AcceptanceTesting.Support;
+
+    public interface ITestSuiteConstraints
+    {
+        bool SupportsDtc { get; }
+
+        bool SupportsCrossQueueTransactions { get; }
+
+        bool SupportsNativePubSub { get; }
+
+        bool SupportsDelayedDelivery { get; }
+
+        bool SupportsOutbox { get; }
+
+        bool SupportsPurgeOnStartup { get; }
+
+        IConfigureEndpointTestExecution CreateTransportConfiguration();
+
+        IConfigureEndpointTestExecution CreatePersistenceConfiguration();
+
+        static ITestSuiteConstraints current;
+        static ITestSuiteConstraints Current
+        {
+            get
+            {
+                if (current == null)
+                {
+                    throw new NotImplementedException("Tests setup is not completed. Create a module initializer by adding a class with a public static method decorated with the ModuleInitializer .NET attribute." +
+                                                      " In the static method, create an instance of the ITestSuiteConstraints implementation in your test project and sets it to the ITestSuiteConstraints.Current property.");
+                }
+
+                return current;
+            }
+            set => current = value;
+        }
+    }
+}
