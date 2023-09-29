cd5797437f267063181b4cad056b587980edb149
Andreas Öhlund <andreas.ohlund@particular.net>
2023-09-29 12:03:15 +0200

    Validate that ambient transaction haven't been changed when running in transaction scope mode

--- src/NServiceBus.Core.Tests/Unicast/LoadHandlersConnectorTests.cs
+++ src/NServiceBus.Core.Tests/Unicast/LoadHandlersConnectorTests.cs
@@ -6,6 +6,9 @@
     using NUnit.Framework;
     using Testing;
     using Core.Tests.Fakes;
+    using System.Transactions;
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
 
     [TestFixture]
     public class LoadHandlersConnectorTests
@@ -21,6 +24,65 @@
             context.Extensions.Set(new TransportTransaction());
 
             Assert.That(async () => await behavior.Invoke(context, c => Task.CompletedTask), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Should_throw_if_scope_in_transport_transaction_differs_from_the_ambient()
+        {
+            var behavior = new LoadHandlersConnector(new MessageHandlerRegistry());
+
+            var context = new TestableIncomingLogicalMessageContext();
+
+            context.Extensions.Set<IOutboxTransaction>(new NoOpOutboxTransaction());
+
+            using (new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var transportTransaction = new TransportTransaction();
+
+                transportTransaction.Set(Transaction.Current);
+
+                context.Extensions.Set(transportTransaction);
+
+                using (new TransactionScope(TransactionScopeOption.RequiresNew, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    var ex = Assert.ThrowsAsync<InvalidOperationException>(async () => await behavior.Invoke(context, c => Task.CompletedTask));
+
+                    StringAssert.Contains("A TransactionScope has been opened in the current context overriding the one created by the transport", ex.Message);
+                }
+            }
+        }
+
+        [Test]
+        public void Should_not_throw_if_scope_in_transport_transaction_are_the_same_as_the_ambient()
+        {
+            var messageHandlerRegistry = new MessageHandlerRegistry();
+            messageHandlerRegistry.RegisterHandler(typeof(FakeHandler));
+
+            var context = new TestableIncomingLogicalMessageContext();
+
+            context.Services.AddSingleton<FakeHandler>();
+            context.Extensions.Set<IOutboxTransaction>(new NoOpOutboxTransaction());
+
+            var behavior = new LoadHandlersConnector(messageHandlerRegistry);
+
+            using (new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var transportTransaction = new TransportTransaction();
+
+                transportTransaction.Set(Transaction.Current);
+
+                context.Extensions.Set(transportTransaction);
+
+                using (new TransactionScope(TransactionScopeOption.Required, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    Assert.DoesNotThrowAsync(async () => await behavior.Invoke(context, c => Task.CompletedTask));
+                }
+            }
+        }
+
+        class FakeHandler : IHandleMessages<object>
+        {
+            public Task Handle(object message, IMessageHandlerContext context) => Task.CompletedTask;
         }
     }
 }
