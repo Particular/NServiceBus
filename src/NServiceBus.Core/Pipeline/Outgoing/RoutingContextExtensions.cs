60ba4fd53996b36232e07d6d6f333e31d06643e5
danielmarbach <daniel.marbach@openplace.net>
2023-10-31 17:06:46 +0100

    Fix the state sharing problem in ToTransportOperation

--- src/NServiceBus.Core/Pipeline/Outgoing/RoutingContextExtensions.cs
+++ src/NServiceBus.Core/Pipeline/Outgoing/RoutingContextExtensions.cs
@@ -1,20 +1,20 @@
 ﻿namespace NServiceBus
 {
+    using System.Collections.Generic;
     using Pipeline;
     using Routing;
     using Transport;
 
     static class RoutingContextExtensions
     {
-        public static TransportOperation ToTransportOperation(this IRoutingContext context, RoutingStrategy strategy, DispatchConsistency dispatchConsistency)
+        public static TransportOperation ToTransportOperation(this IRoutingContext context, RoutingStrategy strategy, DispatchConsistency dispatchConsistency, bool copySharedMutableMessageState)
         {
-            var addressLabel = strategy.Apply(context.Message.Headers);
-            var message = new OutgoingMessage(context.Message.MessageId, context.Message.Headers, context.Message.Body);
-
-            if (!context.Extensions.TryGet(out DispatchProperties dispatchProperties))
-            {
-                dispatchProperties = [];
-            }
+            var headers = copySharedMutableMessageState ? new Dictionary<string, string>(context.Message.Headers) : context.Message.Headers;
+            var dispatchProperties = context.Extensions.TryGet(out DispatchProperties properties)
+                ? copySharedMutableMessageState ? new DispatchProperties(properties) : properties
+                : [];
+            var addressLabel = strategy.Apply(headers);
+            var message = new OutgoingMessage(context.Message.MessageId, headers, context.Message.Body);
 
             var transportOperation = new TransportOperation(message, addressLabel, dispatchProperties, dispatchConsistency);
             return transportOperation;
