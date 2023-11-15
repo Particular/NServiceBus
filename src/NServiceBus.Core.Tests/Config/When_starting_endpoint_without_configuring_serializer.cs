94d3ae9d51e082df0d5d33fda367d2727ea38d1d
Brandon Ording <bording@gmail.com>
2023-11-15 17:09:03 -0500

    Add new test

---
+++ src/NServiceBus.Core.Tests/Config/When_starting_endpoint_without_configuring_serializer.cs
@@ -0,0 +1,21 @@
+﻿namespace NServiceBus.Core.Tests.Config;
+
+using System;
+using NUnit.Framework;
+
+[TestFixture]
+class When_starting_endpoint_without_configuring_serializer
+{
+    [Test]
+    public void Should_throw()
+    {
+        var configuration = new EndpointConfiguration("Endpoint1");
+        configuration.UseTransport(new LearningTransport());
+
+        var scanner = configuration.AssemblyScanner();
+        scanner.ExcludeAssemblies("NServiceBus.Core.Tests.dll");
+
+        var exception = Assert.ThrowsAsync<Exception>(async () => await Endpoint.Create(configuration).ConfigureAwait(false));
+        Assert.True(exception.Message.StartsWith("A serializer has not been configured"));
+    }
+}
