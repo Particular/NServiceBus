e4311d95c627b7edc677c9c570e70d68d56c3faa
danielmarbach <daniel.marbach@openplace.net>
2023-12-01 16:30:44 +0100

    TransportTest to verify message receivers can be started, stopped and started again
    Co-authored-by: Brandon Ording <bording@gmail.com>

---
+++ src/NServiceBus.TransportTests/When_starting_again.cs
@@ -0,0 +1,39 @@
+﻿namespace NServiceBus.TransportTests
+{
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Transport;
+
+    public class When_starting_again : NServiceBusTransportTest
+    {
+        [TestCase(TransportTransactionMode.None)]
+        [TestCase(TransportTransactionMode.ReceiveOnly)]
+        [TestCase(TransportTransactionMode.SendsAtomicWithReceive)]
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_complete(TransportTransactionMode transactionMode)
+        {
+            bool messageReceived = false;
+            var messageReceivedCompletionSource = CreateTaskCompletionSource();
+
+            await StartPump(
+                (_, __) =>
+                {
+                    messageReceived = true;
+                    messageReceivedCompletionSource.SetResult();
+                    return Task.CompletedTask;
+                },
+                (_, __) => Task.FromResult(ErrorHandleResult.Handled),
+                transactionMode);
+
+            await receiver.StopReceive();
+
+            await SendMessage(InputQueueName);
+
+            await receiver.StartReceive();
+
+            await messageReceivedCompletionSource.Task;
+
+            Assert.That(messageReceived, Is.True);
+        }
+    }
+}
