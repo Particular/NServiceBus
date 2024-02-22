c0396311e1f3f8e288c5b8532bf8c563accdd030
David Boike <david.boike@gmail.com>
2024-02-22 16:32:47 -0600

    Analyzer fixes

--- src/NServiceBus.Core/MessageMutators/MutateTransportMessage/MutateIncomingTransportMessageBehavior.cs
+++ src/NServiceBus.Core/MessageMutators/MutateTransportMessage/MutateIncomingTransportMessageBehavior.cs
@@ -7,6 +7,7 @@
     using Microsoft.Extensions.DependencyInjection;
     using Pipeline;
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Code", "PS0025:Dictionary keys should implement GetHashCode", Justification = "Mutators are registered based on reference equality")]
     class MutateIncomingTransportMessageBehavior : IBehavior<IIncomingPhysicalMessageContext, IIncomingPhysicalMessageContext>
     {
         public MutateIncomingTransportMessageBehavior(HashSet<IMutateIncomingTransportMessages> mutators)
