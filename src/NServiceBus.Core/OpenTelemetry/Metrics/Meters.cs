e1633921e97881a8288239bdeb50bcd7920cbb2f
Sara Pellegrini <sara.pellegrini@gmail.com>
2024-05-31 08:59:43 +0200

    The API has been updated, and accordingly the approved txt. This implied an increase in the version, that still remains 0.x since the whole metrics module is in experimental state.

--- src/NServiceBus.Core/OpenTelemetry/Metrics/Meters.cs
+++ src/NServiceBus.Core/OpenTelemetry/Metrics/Meters.cs
@@ -6,7 +6,7 @@ class Meters
 {
     internal static readonly Meter NServiceBusMeter = new Meter(
         "NServiceBus.Core",
-        "0.1.0");
+        "0.2.0");
 
     internal static readonly Counter<long> TotalProcessedSuccessfully =
         NServiceBusMeter.CreateCounter<long>("nservicebus.messaging.successes", description: "Total number of messages processed successfully by the endpoint.");
