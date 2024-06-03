63d9b6c047ce16a96db412244421e598e2e4ec1b
Sara Pellegrini <sara.pellegrini@gmail.com>
2024-06-03 14:42:32 +0200

    Introduce new unit test for metrics' name validation. Fix failing tests.

---
+++ src/NServiceBus.Core/OpenTelemetry/Metrics/Metrics.cs
@@ -0,0 +1,9 @@
+﻿namespace NServiceBus;
+
+static class Metrics
+{
+    public const string TotalProcessedSuccessfully = "nservicebus.messaging.successes";
+    public const string TotalFetched = "nservicebus.messaging.fetches";
+    public const string TotalFailures = "nservicebus.messaging.failures";
+    public const string HandlingTime = "nservicebus.messaging.handling_time";
+}
