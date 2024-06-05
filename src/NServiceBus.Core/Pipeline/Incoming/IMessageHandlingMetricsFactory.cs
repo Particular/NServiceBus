bb41e19c4f1fd65fcb0e369184fa5e9e7cfa9a8f
Sara Pellegrini <sara.pellegrini@gmail.com>
2024-06-06 00:23:49 +0200

    Rename HandlingMetrics... types to MessageHandlingMetrics... to be more explicit.

--- src/NServiceBus.Core/Pipeline/Incoming/IMessageHandlingMetricsFactory.cs
+++ src/NServiceBus.Core/Pipeline/Incoming/IMessageHandlingMetricsFactory.cs
@@ -3,15 +3,15 @@
 using Pipeline;
 
 /// <summary>
-/// Factory for IHandlingMetrics needed to record metrics for the handler invocation.
+/// Factory for IMessageHandlingMetrics needed to record metrics for the handler invocation.
 /// </summary>
 interface IMessageHandlingMetricsFactory
 {
     /// <summary>
-    /// Creates a new <c>IHandlingMetrics</c> instance for recording the metrics for a specific handler execution.
+    /// Creates a new <c>IMessageHandlingMetrics</c> instance for recording the metrics for a specific handler execution.
     /// </summary>
     /// <param name="context"> Needed to properly initialize the IHandlingMetrics instance. </param>
-    /// <returns> The newly created <c>IHandlingMetrics</c> instance. </returns>
+    /// <returns> The newly created <c>IMessageHandlingMetrics</c> instance. </returns>
     IMessageHandlingMetrics StartHandling(IInvokeHandlerContext context);
 }
 
