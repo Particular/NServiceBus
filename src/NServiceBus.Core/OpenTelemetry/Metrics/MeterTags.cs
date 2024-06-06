166a320c9493ff61036c44df2fcc9f268c11fd45
Sara Pellegrini <sara.pellegrini@gmail.com>
2024-06-06 14:13:16 +0200

    Rename the static method that provides the common metric tags for clarity.

--- src/NServiceBus.Core/OpenTelemetry/Metrics/MeterTags.cs
+++ src/NServiceBus.Core/OpenTelemetry/Metrics/MeterTags.cs
@@ -12,7 +12,7 @@ static class MeterTags
     public const string FailureType = "nservicebus.failure_type";
     public const string MessageHandlerType = "nservicebus.message_handler_type";
 
-    public static TagList BaseTagList(string queueName, string discriminator, string messageType)
+    public static TagList CommonMessagingMetricTags(string queueName, string discriminator, string messageType)
     {
         return new TagList(new KeyValuePair<string, object>[]
         {
