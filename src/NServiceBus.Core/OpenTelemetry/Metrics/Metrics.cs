192b770c36ad96bd3ec4829b2b000e92b1a4f58c
Sara Pellegrini <sara.pellegrini@gmail.com>
2024-06-06 10:54:35 +0200

    Rename HandlingTime to MessageHandlerTime for clarity.

--- src/NServiceBus.Core/OpenTelemetry/Metrics/Metrics.cs
+++ src/NServiceBus.Core/OpenTelemetry/Metrics/Metrics.cs
@@ -5,5 +5,5 @@ static class Metrics
     public const string TotalProcessedSuccessfully = "nservicebus.messaging.successes";
     public const string TotalFetched = "nservicebus.messaging.fetches";
     public const string TotalFailures = "nservicebus.messaging.failures";
-    public const string HandlingTime = "nservicebus.messaging.handling_time";
+    public const string MessageHandlerTime = "nservicebus.messaging.handler_time";
 }
