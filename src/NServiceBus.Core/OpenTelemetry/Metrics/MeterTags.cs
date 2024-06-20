d93a2b87a72784e0ded73eb96b2577e5670e34d4
Mauro Servienti <mauro.servienti@gmail.com>
2024-06-20 17:01:32 +0200

    Prefer being explicit. Do not use a common way for creating a TagList to prevent adding unwanted tags everywhere in the future.

--- src/NServiceBus.Core/OpenTelemetry/Metrics/MeterTags.cs
+++ src/NServiceBus.Core/OpenTelemetry/Metrics/MeterTags.cs
@@ -1,9 +1,5 @@
 ﻿namespace NServiceBus;
 
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-
 static class MeterTags
 {
     public const string EndpointDiscriminator = "nservicebus.discriminator";
@@ -14,12 +10,4 @@ static class MeterTags
     public const string MessageHandlerType = "nservicebus.message_handler_type";
     public const string ExecutionResult = "execution.result";
     public const string ErrorType = "error.type";
-
-    public static TagList CommonMessagingMetricTags(string queueName, string discriminator, string messageType)
-    {
-        return new TagList(new KeyValuePair<string, object>[]
-        {
-            new(QueueName, queueName ?? ""), new(EndpointDiscriminator, discriminator ?? ""), new(MessageType, messageType ?? "")
-        }.AsSpan());
-    }
 }
