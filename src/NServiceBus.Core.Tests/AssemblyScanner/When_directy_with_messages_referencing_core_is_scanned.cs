4af8ef185d1b393e1d383ed715d096e863457ba2
danielmarbach <daniel.marbach@openplace.net>
2024-06-21 11:39:55 +0200

    Add a test to verify the messages referencing core are scanned

---
+++ src/NServiceBus.Core.Tests/AssemblyScanner/When_directy_with_messages_referencing_core_is_scanned.cs
@@ -0,0 +1,21 @@
+﻿namespace NServiceBus.Core.Tests.AssemblyScanner;
+
+using System.IO;
+using System.Reflection;
+using Hosting.Helpers;
+using NUnit.Framework;
+
+[TestFixture]
+public class When_directory_with_messages_referencing_core_is_scanned
+{
+    [Test]
+    public void Assemblies_referencing_core_should_be_scanned()
+    {
+        var assemblyToScan = Assembly.LoadFrom(Path.Combine(TestContext.CurrentContext.TestDirectory, "TestDlls", "Messages.Referencing.Core.dll"));
+        var scanner = new AssemblyScanner(assemblyToScan);
+
+        var result = scanner.GetScannableAssemblies();
+
+        Assert.That(result.Assemblies.Contains(assemblyToScan), Is.True);
+    }
+}
