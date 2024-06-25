119e75fc8c40e322a408239923f5c9b8c1a67fb6
Laila Bougria <laila.bougria@particular.net>
2024-06-25 10:10:42 +0200

    add xml docs

--- src/NServiceBus.Core/Pipeline/Incoming/IncomingPipelineMetricTags.cs
+++ src/NServiceBus.Core/Pipeline/Incoming/IncomingPipelineMetricTags.cs
@@ -6,15 +6,29 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
+/// <summary>
+/// Captures possible metric tags that can be applied to a metric throughout the incoming processing pipeline.
+/// </summary>
 public sealed class IncomingPipelineMetricTags
 {
     Dictionary<string, KeyValuePair<string, object?>>? tags;
+
+    /// <summary>
+    /// Adds the specified tag and value to the collection.
+    /// </summary>
+    /// <param name="tagKey"></param>
+    /// <param name="value"></param>
     public void Add(string tagKey, object value)
     {
         tags ??= [];
         tags.Add(tagKey, new(tagKey, value));
     }
 
+    /// <summary>
+    /// Applies the specified tag to the <paramref name="tagList"/>.
+    /// </summary>
+    /// <param name="tagList">The tagList to apply the specified tag to.</param>
+    /// <param name="tagKey">The tag to add to the <paramref name="tagList"/>.</param>
     public void ApplyTag(ref TagList tagList, string tagKey)
     {
         if (tags == null)
@@ -28,6 +42,11 @@ public sealed class IncomingPipelineMetricTags
         }
     }
 
+    /// <summary>
+    /// Applies the specified tags to the <paramref name="tagList"/>.
+    /// </summary>
+    /// <param name="tagList">The tagList to add the tags to.</param>
+    /// <param name="tagKeys">The collection of tag keys to apply to the <paramref name="tagList"/></param>
     public void ApplyTags(ref TagList tagList, ReadOnlySpan<string> tagKeys)
     {
         if (tags == null || tagKeys.IsEmpty)
