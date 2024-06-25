d08433880c025c9b69e7695873d945f1389de6f4
SzymonPobiega <szymon.pobiega@gmail.com>
2024-06-25 11:03:16 +0200

    Fix rebase

--- src/NServiceBus.Core/Pipeline/Incoming/IncomingPipelineMetricTags.cs
+++ src/NServiceBus.Core/Pipeline/Incoming/IncomingPipelineMetricTags.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
-<<<<<<< HEAD
 /// <summary>
 /// Captures possible metric tags that can be applied to a metric throughout the incoming processing pipeline.
 /// </summary>
@@ -19,25 +18,17 @@ public sealed class IncomingPipelineMetricTags
     /// </summary>
     /// <param name="tagKey">The tag to add.</param>
     /// <param name="value">The value assigned to the tag.</param>
-=======
-public sealed class IncomingPipelineMetricTags
-{
-    Dictionary<string, KeyValuePair<string, object?>>? tags;
->>>>>>> bb41e19ba (capture the metric tags when we have the most accurate values)
     public void Add(string tagKey, object value)
     {
         tags ??= [];
         tags.Add(tagKey, new(tagKey, value));
     }
 
-<<<<<<< HEAD
     /// <summary>
     /// Applies the specified tag to the <paramref name="tagList"/>.
     /// </summary>
     /// <param name="tagList">The tagList to apply the specified tag to.</param>
     /// <param name="tagKey">The tag to add to the <paramref name="tagList"/>.</param>
-=======
->>>>>>> bb41e19ba (capture the metric tags when we have the most accurate values)
     public void ApplyTag(ref TagList tagList, string tagKey)
     {
         if (tags == null)
@@ -51,14 +42,11 @@ public sealed class IncomingPipelineMetricTags
         }
     }
 
-<<<<<<< HEAD
     /// <summary>
     /// Applies the specified tags to the <paramref name="tagList"/>.
     /// </summary>
     /// <param name="tagList">The tagList to add the tags to.</param>
     /// <param name="tagKeys">The collection of tag keys to apply to the <paramref name="tagList"/>.</param>
-=======
->>>>>>> bb41e19ba (capture the metric tags when we have the most accurate values)
     public void ApplyTags(ref TagList tagList, ReadOnlySpan<string> tagKeys)
     {
         if (tags == null || tagKeys.IsEmpty)
