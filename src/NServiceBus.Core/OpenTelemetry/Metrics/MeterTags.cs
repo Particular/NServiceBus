1f6f6f4c00c9caffbfb0c67cc4ddb9945eae3492
Mauro Servienti <mauro.servienti@gmail.com>
2024-06-25 18:10:21 +0200

    Comment on the differences between MessageHandlerTypes and MessageHandlerType

--- src/NServiceBus.Core/OpenTelemetry/Metrics/MeterTags.cs
+++ src/NServiceBus.Core/OpenTelemetry/Metrics/MeterTags.cs
@@ -6,7 +6,10 @@ static class MeterTags
     public const string QueueName = "nservicebus.queue";
     public const string MessageType = "nservicebus.message_type";
     public const string FailureType = "nservicebus.failure_type";
+    // used when publishing TotalProcessedSuccessfully and TotalFailures
+    // to record all the message handlers invoked during the message processing
     public const string MessageHandlerTypes = "nservicebus.message_handler_types";
+    // used when publishing MessageHandlerTime to record the invoked handler
     public const string MessageHandlerType = "nservicebus.message_handler_type";
     public const string ExecutionResult = "execution.result";
     public const string ErrorType = "error.type";
