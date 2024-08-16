13b60e2ed246c122c2287dbfd3f6aeceed65f6a0
Daniel Marbach <danielmarbach@users.noreply.github.com>
2024-08-17 00:13:12 +0200

    Autoformat of test projects for NUnit2012

--- src/NServiceBus.AcceptanceTests/Core/FakeTransport/CriticalError/When_registering_custom_critical_error_handler.cs
+++ src/NServiceBus.AcceptanceTests/Core/FakeTransport/CriticalError/When_registering_custom_critical_error_handler.cs
@@ -19,7 +19,7 @@ public class When_registering_custom_critical_error_handler : NServiceBusAccepta
             .Done(c => c.ExceptionReceived)
             .Run();
 
-        Assert.That(context.Message.StartsWith("Startup task failed to complete."), Is.True);
+        Assert.That(context.Message, Does.StartWith("Startup task failed to complete."));
         Assert.That(context.Exception.Message, Is.EqualTo("ExceptionInBusStarts"));
     }
 
