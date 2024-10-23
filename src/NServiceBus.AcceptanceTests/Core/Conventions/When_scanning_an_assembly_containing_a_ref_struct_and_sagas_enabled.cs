4093c3a5c5f6ffba1c962383d6a3f438307f8f3d
Andreas Öhlund <andreas.ohlund@particular.net>
2024-10-23 08:46:30 +0200

    Tweaks

--- src/NServiceBus.AcceptanceTests/Core/Conventions/When_scanning_an_assembly_containing_a_ref_struct_and_sagas_enabled.cs
+++ src/NServiceBus.AcceptanceTests/Core/Conventions/When_scanning_an_assembly_containing_a_ref_struct_and_sagas_enabled.cs
@@ -17,11 +17,6 @@ public class When_scanning_an_assembly_containing_a_ref_struct_and_sagas_enabled
                           .Run()
     );
 
-    public class SomeMessage : IMessage
-    {
-        public Guid BusinessId { get; set; }
-    }
-
     // HINT: This will get picked up by the AssemblyRouteSource created by the routing call below
     // Even though it is not a message type, it is still checked by passing it to conventions.
     // The conventions added by Sagas were throwing an exception when passed a ref struct.
@@ -38,7 +33,6 @@ public class When_scanning_an_assembly_containing_a_ref_struct_and_sagas_enabled
             )
         );
 
-
         class RealSagaToSetUpConventions : Saga<RealSagaToSetUpConventions.RealSagaToSetUpConventionsSagaData>, IAmStartedByMessages<SomeMessage>
         {
             public Task Handle(SomeMessage message, IMessageHandlerContext context) => Task.CompletedTask;
@@ -50,5 +44,10 @@ public class When_scanning_an_assembly_containing_a_ref_struct_and_sagas_enabled
                 public virtual Guid BusinessId { get; set; }
             }
         }
+    }
+
+    public class SomeMessage : IMessage
+    {
+        public Guid BusinessId { get; set; }
     }
 }
