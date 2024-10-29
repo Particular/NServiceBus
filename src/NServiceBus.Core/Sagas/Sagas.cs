216e3b18fc9c7feec1115fbfb467f455785edbbd
Andreas Öhlund <andreas.ohlund@particular.net>
2024-10-29 08:11:13 +0100

    Update src/NServiceBus.Core/Sagas/Sagas.cs
    Co-authored-by: Daniel Marbach <daniel.marbach@openplace.net>

--- src/NServiceBus.Core/Sagas/Sagas.cs
+++ src/NServiceBus.Core/Sagas/Sagas.cs
@@ -100,7 +100,7 @@
         {
             // MakeGenericType() throws an exception if passed a ref struct type
             // Messages cannot be ref struct types
-            if (type.CustomAttributes.Any(a => a.AttributeType.Name == "IsByRefLikeAttribute"))
+            if (type.CustomAttributes.Any(a => a.AttributeType.FullName == "System.Runtime.CompilerServices.IsByRefLikeAttribute"))
             {
                 return false;
             }
