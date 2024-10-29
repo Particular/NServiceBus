bd4903209d36799e628a33290dd77329e930fc83
Andreas Öhlund <andreas.ohlund@particular.net>
2024-10-29 15:26:50 +0100

    Avoid null check

--- src/NServiceBus.Core/Sagas/Sagas.cs
+++ src/NServiceBus.Core/Sagas/Sagas.cs
@@ -100,7 +100,7 @@
         {
             // MakeGenericType() throws an exception if passed a ref struct type
             // Messages cannot be ref struct types
-            if (type.CustomAttributes.Any(a => a.AttributeType.FullName != null && a.AttributeType.FullName.Equals("System.Runtime.CompilerServices.IsByRefLikeAttribute")))
+            if (type.CustomAttributes.Any(a => string.Equals(a.AttributeType.FullName, "System.Runtime.CompilerServices.IsByRefLikeAttribute")))
             {
                 return false;
             }
