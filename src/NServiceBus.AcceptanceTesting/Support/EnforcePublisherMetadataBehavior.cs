c09d30972e0849c8989e3efc1e0088b4b57da182
danielmarbach <danielmarbach@users.noreply.github.com>
2025-01-08 12:14:06 +0100

    Better exception messages

--- src/NServiceBus.AcceptanceTesting/Support/EnforcePublisherMetadataBehavior.cs
+++ src/NServiceBus.AcceptanceTesting/Support/EnforcePublisherMetadataBehavior.cs
@@ -11,7 +11,7 @@ public class EnforcePublisherMetadataBehavior(string endpointName, PublisherMeta
         var publisherDetails = publisherMetadata[endpointName];
         if (!publisherDetails.Events.Contains(context.Message.MessageType))
         {
-            throw new Exception("The event being published does not have a corresponding mapping in the PublisherMetadata.");
+            throw new Exception($"The event '{context.Message.MessageType}' being published by '{endpointName}' does not have a corresponding mapping in the PublisherMetadata. Add the following code to the endpoint configuration builder: metadata.RegisterSelfAsPublisherFor<{context.Message.MessageType}>(this);");
         }
         return next(context);
     }
--- src/NServiceBus.AcceptanceTesting/Support/EnforceSubscriptionPublisherMetadataBehavior.cs
+++ src/NServiceBus.AcceptanceTesting/Support/EnforceSubscriptionPublisherMetadataBehavior.cs
@@ -3,20 +3,28 @@ namespace NServiceBus.AcceptanceTesting.Support;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Pipeline;
 
 public class EnforceSubscriptionPublisherMetadataBehavior(string endpointName, PublisherMetadata publisherMetadata) : IBehavior<ISubscribeContext, ISubscribeContext>
 {
-    readonly Dictionary<Type, bool> eventTypeMap = publisherMetadata.Publishers.SelectMany(publisher => publisher.Events).ToDictionary(k => k, v => true);
+    readonly Dictionary<Type, bool> eventTypeMap = publisherMetadata.Publishers.SelectMany(publisher => publisher.Events).ToDictionary(eventType => eventType, _ => true);
 
     public Task Invoke(ISubscribeContext context, Func<ISubscribeContext, Task> next)
     {
         var unmappedEventTypes = new List<Type>(context.EventTypes.Where(eventType => !eventTypeMap.ContainsKey(eventType)));
-        if (unmappedEventTypes.Count != 0)
+        if (unmappedEventTypes.Count == 0)
         {
-            throw new Exception($"The event(s) '{string.Join(", ", unmappedEventTypes)}' being subscribed to by '{endpointName}' do(es) not have a corresponding mapping in the PublisherMetadata.");
+            return next(context);
         }
-        return next(context);
+
+        var builder = new StringBuilder();
+        _ = builder.AppendLine($"The following event(s) are being subscribed to by '{endpointName}' but do not have a corresponding mapping in the PublisherMetadata:");
+        foreach (var eventType in unmappedEventTypes)
+        {
+            _ = builder.AppendLine($"- metadata.RegisterPublisherFor<{eventType}>(\"typeof(PublisherEndpointToBeDetermined)\");");
+        }
+        throw new Exception(builder.ToString());
     }
 }
