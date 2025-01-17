7839a45e49d25e1777d9ff162f02eb3cea1bfe1d
Jayanthi <jayanthi.sourirajan@particular.net>
2025-01-17 07:47:50 -0800

    suppress the warning for  IDE0051 Remove unused private members

--- src/NServiceBus.Core.Tests/Sagas/SagaModelPartialProperty1Tests.cs
+++ src/NServiceBus.Core.Tests/Sagas/SagaModelPartialProperty1Tests.cs
@@ -3,7 +3,9 @@ namespace NServiceBus.Core.Tests.Sagas.TypeBasedSagas;
 public partial class MyPartialEntity : ContainSagaData
 {
 
+#pragma warning disable IDE0051 // Remove unused private members
     int _uniqueProperty;
+#pragma warning restore IDE0051 // Remove unused private members
 
     public partial int UniqueProperty { get; set; }
 
