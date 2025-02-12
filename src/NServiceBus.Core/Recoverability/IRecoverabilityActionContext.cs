cfeb50acd3fbf3eecd59a14f960a7e37c0c8c4b3
JasonTaylorDev <hello@jasontaylor.dev>
2025-02-12 15:15:54 +1000

    Code analysis cleanup

--- src/NServiceBus.Core/Recoverability/IRecoverabilityActionContext.cs
+++ src/NServiceBus.Core/Recoverability/IRecoverabilityActionContext.cs
@@ -12,27 +12,27 @@
         /// <summary>
         /// The message that failed processing.
         /// </summary>
-        public IncomingMessage FailedMessage { get; }
+        IncomingMessage FailedMessage { get; }
 
         /// <summary>
         /// The exception that caused processing to fail.
         /// </summary>
-        public Exception Exception { get; }
+        Exception Exception { get; }
 
         /// <summary>
         /// The receive address where this message failed.
         /// </summary>
-        public string ReceiveAddress { get; }
+        string ReceiveAddress { get; }
 
         /// <summary>
         /// The number of times the message have been retried immediately but failed.
         /// </summary>
-        public int ImmediateProcessingFailures { get; }
+        int ImmediateProcessingFailures { get; }
 
         /// <summary>
         /// Number of delayed deliveries performed so far.
         /// </summary>
-        public int DelayedDeliveriesPerformed { get; }
+        int DelayedDeliveriesPerformed { get; }
 
         /// <summary>
         /// Metadata for this message.
