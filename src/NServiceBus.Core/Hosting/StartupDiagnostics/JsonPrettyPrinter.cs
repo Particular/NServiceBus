ea3f8092ae8de0e9398b3e38419bcf72ff21533a
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-08-06 15:13:51 +0200

    Simplify JsonPrettyPrinter by leveraging the built-in mechanism

--- src/NServiceBus.Core/Hosting/StartupDiagnostics/JsonPrettyPrinter.cs
+++ src/NServiceBus.Core/Hosting/StartupDiagnostics/JsonPrettyPrinter.cs
@@ -1,91 +1,21 @@
 namespace NServiceBus;
 
-using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
 
 static class JsonPrettyPrinter
 {
-    const string LINE_INDENT = "  ";
-
     internal static string Print(string input)
     {
-        var builder = new StringBuilder(input.Length);
-        var quoted = false;
-        var indent = 0;
+        using var doc = JsonDocument.Parse(input);
+        var root = doc.RootElement;
 
-        for (var i = 0; i < input.Length; i++)
-        {
-            var ch = input[i];
-            switch (ch)
-            {
-                case '{':
-                case '[':
-                    builder.Append(ch);
-                    if (!quoted)
-                    {
-                        PrintIndent(builder, ++indent);
-                    }
-
-                    break;
-                case '}':
-                case ']':
-                    if (!quoted)
-                    {
-                        PrintIndent(builder, --indent);
-                    }
-
-                    builder.Append(ch);
-                    break;
-                case '"':
-                    builder.Append(ch);
-                    var escaped = IsEscaped(input, i);
-                    if (!escaped)
-                    {
-                        quoted = !quoted;
-                    }
-
-                    break;
-                case ',':
-                    builder.Append(ch);
-                    if (!quoted)
-                    {
-                        PrintIndent(builder, indent);
-                    }
-
-                    break;
-                case ':':
-                    builder.Append(ch);
-                    if (!quoted)
-                    {
-                        builder.Append(' ');
-                    }
-
-                    break;
-                default:
-                    builder.Append(ch);
-                    break;
-            }
-        }
-
-        return builder.ToString();
+        return JsonSerializer.Serialize(root, jsonSerializerOptions);
     }
 
-    static bool IsEscaped(string input, int i)
+    static readonly JsonSerializerOptions jsonSerializerOptions = new()
     {
-        var escaped = false;
-        var index = i;
-        while (index > 0 && input[--index] == '\\')
-        {
-            escaped = !escaped;
-        }
-        return escaped;
-    }
-
-    static void PrintIndent(StringBuilder sb, int indent)
-    {
-        sb.AppendLine();
-        for (var i = 0; i < indent; i++)
-        {
-            sb.Append(LINE_INDENT);
-        }
-    }
+        WriteIndented = true,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
 }
