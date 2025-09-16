9c6ab45c05de778ca7ae3a4e10458ae6564ab4fe
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-09-16 16:36:17 +0200

    Spiking around

---
+++ src/NServiceBus.Core/Features/FeatureFactory.cs
@@ -0,0 +1,17 @@
+#nullable enable
+namespace NServiceBus.Features;
+
+using System;
+
+class FeatureFactory
+{
+    public virtual Feature CreateFeature(Type featureType)
+    {
+        if (!typeof(Feature).IsAssignableFrom(featureType))
+        {
+            throw new ArgumentException($"The provided type '{featureType.FullName}' is not a valid feature. All features must inherit from '{typeof(Feature).FullName}'.");
+        }
+
+        return (Feature)Activator.CreateInstance(featureType)!;
+    }
+}
