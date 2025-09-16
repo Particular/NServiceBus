bed0e2af54d906736a06d3b614920b4414a51fe3
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-09-16 19:19:38 +0200

    Use construct

--- src/NServiceBus.Core/Features/FeatureFactory.cs
+++ src/NServiceBus.Core/Features/FeatureFactory.cs
@@ -5,13 +5,8 @@ using System;
 
 class FeatureFactory
 {
-    public virtual Feature CreateFeature(Type featureType)
-    {
-        if (!typeof(Feature).IsAssignableFrom(featureType))
-        {
-            throw new ArgumentException($"The provided type '{featureType.FullName}' is not a valid feature. All features must inherit from '{typeof(Feature).FullName}'.");
-        }
-
-        return (Feature)Activator.CreateInstance(featureType)!;
-    }
+    public virtual Feature CreateFeature(Type featureType) => !typeof(Feature).IsAssignableFrom(featureType)
+        ? throw new ArgumentException(
+            $"The provided type '{featureType.FullName}' is not a valid feature. All features must inherit from '{typeof(Feature).FullName}'.")
+        : featureType.Construct<Feature>();
 }
