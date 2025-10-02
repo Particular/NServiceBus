a5b8d11fb8e4079dd9fc9884e358b04d930d0691
Adam Furmanek <adam.furmanek@particular.net>
2025-10-02 11:47:12 +0200

    Sketching tests for json structured implementation

---
+++ src/NServiceBus.Core/Marshaling/CloudEventJsonStructuredUnmarshaler.cs
@@ -0,0 +1,104 @@
+﻿namespace NServiceBus.Marshaling
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.Json;
+    using System.Text.Json.Nodes;
+    using Transport;
+
+    class CloudEventJsonStructuredUnmarshaler : IUnmarshalMessages
+    {
+        public IncomingMessage CreateIncomingMessage(MessageContext messageContext)
+        {
+            JsonObject receivedCloudEvent = DeserializeOrThrow(messageContext);
+            var headers = ExtractHeaders(receivedCloudEvent);
+            var body = ExtractBody(receivedCloudEvent);
+            string id = ExtractId(receivedCloudEvent);
+
+            return new IncomingMessage(id, headers, body);
+        }
+
+        ReadOnlyMemory<byte> ExtractBody(JsonObject receivedCloudEvent)
+        {
+            if (receivedCloudEvent.ContainsKey("data_base64"))
+            {
+                return ExtractBodyFromBase64(receivedCloudEvent);
+            }
+            else
+            {
+                return ExtractBodyFromProperty(receivedCloudEvent);
+            }
+        }
+
+        ReadOnlyMemory<byte> ExtractBodyFromProperty(JsonObject receivedCloudEvent)
+        {
+            if(receivedCloudEvent["datacontenttype"].GetValue<string>()!.EndsWith("json"))
+            {
+                return new ReadOnlyMemory<byte>(
+                    Encoding.UTF8.GetBytes(JsonSerializer.Serialize(receivedCloudEvent["data"])));
+            }
+            else
+            {
+                return new ReadOnlyMemory<byte>(
+                    Encoding.UTF8.GetBytes(receivedCloudEvent["data"].GetValue<string>()));
+            }
+        }
+
+        ReadOnlyMemory<byte> ExtractBodyFromBase64(JsonObject receivedCloudEvent)
+        {
+            return new ReadOnlyMemory<byte>(Convert.FromBase64String(receivedCloudEvent["data_base64"].GetValue<string>()));
+        }
+
+        static Dictionary<string, string> ExtractHeaders(JsonObject receivedCloudEvent)
+        {
+            var propertiesToIgnore = new[] { "data", "data_base64" };
+
+            return receivedCloudEvent
+                .Where(p => !propertiesToIgnore.Contains(p.Key))
+                .Where(p => p.Value is not null)
+                .ToDictionary(p => p.Key, p => p.Value!.GetValue<string>());
+        }
+
+        static string ExtractId(JsonObject receivedCloudEvent) => receivedCloudEvent["id"].GetValue<string>();
+
+        static void ThrowIfInvalidCloudEvent(JsonObject receivedCloudEvent)
+        {
+            if (receivedCloudEvent == null)
+            {
+                // TODO throw
+            }
+
+            // TODO check headers throw
+        }
+
+        JsonObject DeserializeOrThrow(MessageContext messageContext)
+        {
+            ThrowIfInvalidMessage(messageContext);
+
+            JsonSerializerOptions options = new()
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+
+            var receivedCloudEvent = JsonSerializer.Deserialize<JsonObject>(messageContext.Body.Span, options);
+
+            ThrowIfInvalidCloudEvent(receivedCloudEvent);
+
+            return receivedCloudEvent;
+        }
+
+        void ThrowIfInvalidMessage(MessageContext messageContext)
+        {
+            if (!IsValidMessage(messageContext))
+            {
+                // TODO throw
+            }
+        }
+
+        public bool IsValidMessage(MessageContext messageContext) =>
+            messageContext.Headers.TryGetValue(Headers.ContentType, out string value) && value == "application/cloudevents+json";
+    }
+}
