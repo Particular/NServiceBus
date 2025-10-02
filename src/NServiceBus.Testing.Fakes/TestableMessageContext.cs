0ed601cd637d42238432f5596ec7ee4679abb8e0
Adam Furmanek <adam.furmanek@particular.net>
2025-10-02 11:47:12 +0200

    Sketching tests for json structured implementation

---
+++ src/NServiceBus.Testing.Fakes/TestableMessageContext.cs
@@ -0,0 +1,19 @@
+﻿namespace NServiceBus.Testing;
+
+using System;
+using System.Collections.Generic;
+using Extensibility;
+using NServiceBus.Transport;
+
+public class TestableMessageContext : MessageContext
+{
+    public TestableMessageContext(string nativeMessageId, Dictionary<string, string> headers, ReadOnlyMemory<byte> body, TransportTransaction transportTransaction, string receiveAddress, ContextBag context)
+        : base(nativeMessageId, headers, body, transportTransaction, receiveAddress, context)
+    {
+    }
+
+    public TestableMessageContext(string nativeMessageId, Dictionary<string, string> headers, ReadOnlyMemory<byte> body)
+        : base(nativeMessageId, headers, body, new TransportTransaction(), "receiveAddress", new ContextBag())
+    {
+    }
+}
