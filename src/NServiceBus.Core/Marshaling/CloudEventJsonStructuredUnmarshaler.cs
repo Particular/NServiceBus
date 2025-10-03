1059246f3cbff3166635a3571b1f7e579fcdddc0
Adam Furmanek <adam.furmanek@particular.net>
2025-10-03 13:43:39 +0200

    Adding tests and covering edge cases

--- src/NServiceBus.Core/Marshaling/CloudEventJsonStructuredUnmarshaler.cs
+++ src/NServiceBus.Core/Marshaling/CloudEventJsonStructuredUnmarshaler.cs
@@ -9,23 +9,31 @@
 
     class CloudEventJsonStructuredUnmarshaler : IUnmarshalMessages
     {
+        const string TYPE_PROPERTY = "type";
+        const string DATA_CONTENT_TYPE_PROPERTY = "datacontenttype";
+        const string DATA_PROPERTY = "data";
+        const string DATA_BASE64_PROPERTY = "data_base64";
+        const string ID_PROPERTY = "id";
+        const string JSON_SUFFIX = "json";
+        const string SUPPORTED_CONTENT_TYPE = "application/cloudevents+json";
+
         static readonly JsonSerializerOptions options = new()
         {
             PropertyNameCaseInsensitive = true
         };
+
         public IncomingMessage CreateIncomingMessage(MessageContext messageContext)
         {
             JsonDocument receivedCloudEvent = DeserializeOrThrow(messageContext);
-            var headers = ExtractHeaders(receivedCloudEvent);
+            var headers = ExtractHeaders(messageContext.Headers, receivedCloudEvent);
             var body = ExtractBody(receivedCloudEvent);
-            string id = ExtractId(receivedCloudEvent);
 
-            return new IncomingMessage(id, headers, body);
+            return new IncomingMessage(messageContext.NativeMessageId, headers, body);
         }
 
         static ReadOnlyMemory<byte> ExtractBody(JsonDocument receivedCloudEvent)
         {
-            if (receivedCloudEvent.RootElement.TryGetProperty("data_base64", out _))
+            if (receivedCloudEvent.RootElement.TryGetProperty(DATA_BASE64_PROPERTY, out _))
             {
                 return ExtractBodyFromBase64(receivedCloudEvent);
             }
@@ -37,49 +45,71 @@
 
         static ReadOnlyMemory<byte> ExtractBodyFromProperty(JsonDocument receivedCloudEvent)
         {
-            if (receivedCloudEvent.RootElement.TryGetProperty("datacontenttype", out var property)
-                && property.GetString()!.EndsWith("json"))
+            if (receivedCloudEvent.RootElement.TryGetProperty(DATA_CONTENT_TYPE_PROPERTY, out var property)
+                && property.GetString()!.EndsWith(JSON_SUFFIX))
             {
                 return new ReadOnlyMemory<byte>(
-                    Encoding.UTF8.GetBytes(receivedCloudEvent.RootElement.GetProperty("data").GetRawText()));
+                    Encoding.UTF8.GetBytes(receivedCloudEvent.RootElement.GetProperty(DATA_PROPERTY).GetRawText()));
             }
-            else 
-            {
-                return new ReadOnlyMemory<byte>(
-                    Encoding.UTF8.GetBytes(receivedCloudEvent.RootElement.GetProperty("data").GetString()!));
-            }
+
+            return new ReadOnlyMemory<byte>(
+                    Encoding.UTF8.GetBytes(receivedCloudEvent.RootElement.GetProperty(DATA_PROPERTY).GetString()!));
         }
 
         static ReadOnlyMemory<byte> ExtractBodyFromBase64(JsonDocument receivedCloudEvent)
         {
-            return new ReadOnlyMemory<byte>(Convert.FromBase64String(receivedCloudEvent.RootElement.GetProperty("data_base64").GetString()!));
+            return new ReadOnlyMemory<byte>(Convert.FromBase64String(receivedCloudEvent.RootElement.GetProperty(DATA_BASE64_PROPERTY).GetString()!));
         }
 
-        static Dictionary<string, string> ExtractHeaders(JsonDocument receivedCloudEvent)
+        static Dictionary<string, string> ExtractHeaders(Dictionary<string, string> existingHeaders, JsonDocument receivedCloudEvent)
         {
-            var propertiesToIgnore = new[] { "data", "data_base64" };
+            var propertiesToIgnore = new[] { DATA_PROPERTY, DATA_BASE64_PROPERTY };
 
-            return receivedCloudEvent
-                .RootElement
-                .EnumerateObject()
-                .Where(p => !propertiesToIgnore.Contains(p.Name))
-                .Where(p => p.Value.ValueKind != JsonValueKind.Null)
-                .ToDictionary(p => p.Name, p => p.Value.GetString());
+            var id = ExtractId(receivedCloudEvent);
+
+            var headersCopy = existingHeaders.ToDictionary(k => k.Key, k => k.Value);
+
+            foreach (var kvp in receivedCloudEvent
+                         .RootElement
+                         .EnumerateObject()
+                         .Where(p => !propertiesToIgnore.Contains(p.Name))
+                         .Where(p => p.Value.ValueKind != JsonValueKind.Null))
+            {
+                headersCopy[kvp.Name] = kvp.Value.ValueKind == JsonValueKind.String
+                    ? kvp.Value.GetString()!
+                    : kvp.Value.GetRawText();
+            }
+
+            headersCopy[Headers.MessageId] = id;
+
+            return headersCopy;
         }
 
-        static string ExtractId(JsonDocument receivedCloudEvent) => receivedCloudEvent.RootElement.GetProperty("id").GetString();
+        static string ExtractId(JsonDocument receivedCloudEvent) => receivedCloudEvent.RootElement.GetProperty(ID_PROPERTY).GetString();
 
         static void ThrowIfInvalidCloudEvent(JsonDocument receivedCloudEvent)
         {
             if (receivedCloudEvent == null)
             {
-                // TODO throw
+                throw new NotSupportedException("Couldn't deserialize the message into a cloud event");
             }
 
-            // TODO check headers throw
+            foreach (var property in new string[] { DATA_CONTENT_TYPE_PROPERTY, ID_PROPERTY, TYPE_PROPERTY })
+            {
+                if (!receivedCloudEvent.RootElement.TryGetProperty(property, out _))
+                {
+                    throw new NotSupportedException($"Message lacks {property} property");
+                }
+            }
+
+            if (!receivedCloudEvent.RootElement.TryGetProperty(DATA_BASE64_PROPERTY, out _) &&
+                !receivedCloudEvent.RootElement.TryGetProperty(DATA_PROPERTY, out _))
+            {
+                throw new NotSupportedException($"Message lacks both {DATA_PROPERTY} and {DATA_BASE64_PROPERTY} property");
+            }
         }
 
-        JsonDocument DeserializeOrThrow(MessageContext messageContext)
+        static JsonDocument DeserializeOrThrow(MessageContext messageContext)
         {
             ThrowIfInvalidMessage(messageContext);
             var receivedCloudEvent = JsonSerializer.Deserialize<JsonDocument>(messageContext.Body.Span, options);
@@ -89,15 +119,23 @@
             return receivedCloudEvent;
         }
 
-        void ThrowIfInvalidMessage(MessageContext messageContext)
+        static void ThrowIfInvalidMessage(MessageContext messageContext)
         {
-            if (!IsValidMessage(messageContext))
+
+            if (messageContext.Headers.TryGetValue(Headers.ContentType, out string value))
             {
-                // TODO throw
+                if (value != SUPPORTED_CONTENT_TYPE)
+                {
+                    throw new NotSupportedException($"Unsupported content type {value}");
+                }
+            }
+            else
+            {
+                throw new NotSupportedException("Missing content type");
             }
         }
 
         public bool IsValidMessage(MessageContext messageContext) =>
-            messageContext.Headers.TryGetValue(Headers.ContentType, out string value) && value == "application/cloudevents+json";
+            messageContext.Headers.TryGetValue(Headers.ContentType, out string value) && value == SUPPORTED_CONTENT_TYPE;
     }
 }
