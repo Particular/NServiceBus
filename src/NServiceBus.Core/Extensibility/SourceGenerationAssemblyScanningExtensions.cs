f72c740d2619f89097edff2b67953c40e337f466
David Boike <david.boike@gmail.com>
2025-10-07 13:48:16 -0500

    .UseSourceGeneratedTypeDiscovery().RegisterHandlersAndSagas()

--- src/NServiceBus.Core/Extensibility/SourceGenerationAssemblyScanningExtensions.cs
+++ src/NServiceBus.Core/Extensibility/SourceGenerationAssemblyScanningExtensions.cs
@@ -1,9 +1,10 @@
 namespace NServiceBus;
 
 using System;
+using System.ComponentModel;
 
 /// <summary>
-/// Provides an extension method <see cref="TurnOffAssemblyScanningAndUseSourceGenerationInstead" /> that
+/// Provides an extension method <see cref="SourceGenerationTypeDiscoveryOptions" /> that
 /// disables runtime assembly scanning and registers required types at compile time instead.
 /// </summary>
 public static class SourceGenerationAssemblyScanningExtensions
@@ -13,6 +14,26 @@ public static class SourceGenerationAssemblyScanningExtensions
     /// Roslyn analyzers and source generators must be enabled to replace the implementation of this
     /// method at compile time, otherwise a <see cref="NotImplementedException" /> will be thrown.
     /// </summary>
-    public static void TurnOffAssemblyScanningAndUseSourceGenerationInstead(this EndpointConfiguration endpointConfiguration, bool autoRegisterHandlers)
+    public static SourceGenerationTypeDiscoveryOptions UseSourceGeneratedTypeDiscovery(this EndpointConfiguration endpointConfiguration)
         => throw new NotImplementedException("You can't turn off analyzers / source generation and use this configuration method.");
+
+    /// <summary>
+    /// Auto-registers message handlers and sagas using source generation at compile time.
+    /// Roslyn analyzers and source generators must be enabled to replace the implementation of this
+    /// method at compile time, otherwise a <see cref="NotImplementedException" /> will be thrown.
+    /// </summary>
+    public static SourceGenerationTypeDiscoveryOptions RegisterHandlersAndSagas(this SourceGenerationTypeDiscoveryOptions options)
+        => throw new NotImplementedException("You can't turn off analyzers / source generation and use this configuration method.");
+}
+
+/// <summary>
+/// Provides options to auto-register message handlers and sagas using source generation.
+/// </summary>
+public sealed class SourceGenerationTypeDiscoveryOptions(EndpointConfiguration endpointConfiguration)
+{
+    /// <summary>
+    /// Makes the <see cref="EndpointConfiguration" /> instance available for source generator.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public EndpointConfiguration Configuration => endpointConfiguration;
 }
