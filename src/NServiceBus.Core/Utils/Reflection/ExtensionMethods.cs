6fe8cd49edf8b6718936c43389846155d40e6bc9
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-10-13 17:50:15 +0200

    Small cleanup

--- src/NServiceBus.Core/Utils/Reflection/ExtensionMethods.cs
+++ src/NServiceBus.Core/Utils/Reflection/ExtensionMethods.cs
@@ -1,7 +1,6 @@
 namespace NServiceBus;
 
 using System;
-using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
@@ -18,17 +17,15 @@ static class TypeExtensionMethods
     /// <summary>
     /// Returns true if the type can be serialized as is.
     /// </summary>
-    public static bool IsSimpleType(this Type type)
-    {
-        return type == typeof(string) ||
-               type.IsPrimitive ||
-               type == typeof(decimal) ||
-               type == typeof(Guid) ||
-               type == typeof(DateTime) ||
-               type == typeof(TimeSpan) ||
-               type == typeof(DateTimeOffset) ||
-               type.IsEnum;
-    }
+    public static bool IsSimpleType(this Type type) =>
+        type == typeof(string) ||
+        type.IsPrimitive ||
+        type == typeof(decimal) ||
+        type == typeof(Guid) ||
+        type == typeof(DateTime) ||
+        type == typeof(TimeSpan) ||
+        type == typeof(DateTimeOffset) ||
+        type.IsEnum;
 
     public static bool IsNullableType(this Type type)
     {
@@ -44,9 +41,8 @@ static class TypeExtensionMethods
     /// Takes the name of the given type and makes it friendly for serialization
     /// by removing problematic characters.
     /// </summary>
-    public static string SerializationFriendlyName(this Type t)
-    {
-        return TypeToNameLookup.GetOrAdd(t.TypeHandle, typeHandle =>
+    public static string SerializationFriendlyName([NotNull] this Type t) =>
+        TypeToNameLookup.GetOrAdd(t.TypeHandle, static (typeHandle, t) =>
         {
             var index = t.Name.IndexOf('`');
             if (index >= 0)
@@ -73,8 +69,7 @@ static class TypeExtensionMethods
                 return result;
             }
             return Type.GetTypeFromHandle(typeHandle).Name;
-        });
-    }
+        }, t);
 
     static bool IsClrType(byte[] a1)
     {
@@ -84,27 +79,26 @@ static class TypeExtensionMethods
 
     public static bool IsSystemType(this Type type)
     {
-        if (!IsSystemTypeCache.TryGetValue(type.TypeHandle, out var result))
+        if (IsSystemTypeCache.TryGetValue(type.TypeHandle, out var result))
         {
-            var nameOfContainingAssembly = type.Assembly.GetName().GetPublicKeyToken();
-            IsSystemTypeCache[type.TypeHandle] = result = IsClrType(nameOfContainingAssembly);
+            return result;
         }
 
+        var publicKeyTokenOfContainingAssembly = type.Assembly.GetName().GetPublicKeyToken();
+        IsSystemTypeCache[type.TypeHandle] = result = IsClrType(publicKeyTokenOfContainingAssembly);
         return result;
     }
 
-    public static bool IsFromParticularAssembly(this Type type)
-    {
-        return type.Assembly.GetName()
+    public static bool IsFromParticularAssembly(this Type type) =>
+        type.Assembly.GetName()
             .GetPublicKeyToken()
             .SequenceEqual(nsbPublicKeyToken);
-    }
 
     static readonly byte[] MsPublicKeyToken = typeof(string).Assembly.GetName().GetPublicKeyToken();
 
-    static readonly ConcurrentDictionary<RuntimeTypeHandle, bool> IsSystemTypeCache = new ConcurrentDictionary<RuntimeTypeHandle, bool>();
+    static readonly ConcurrentDictionary<RuntimeTypeHandle, bool> IsSystemTypeCache = new();
 
-    static readonly ConcurrentDictionary<RuntimeTypeHandle, string> TypeToNameLookup = new ConcurrentDictionary<RuntimeTypeHandle, string>();
+    static readonly ConcurrentDictionary<RuntimeTypeHandle, string> TypeToNameLookup = new();
 
     static readonly byte[] nsbPublicKeyToken = typeof(TypeExtensionMethods).Assembly.GetName().GetPublicKeyToken();
 }
