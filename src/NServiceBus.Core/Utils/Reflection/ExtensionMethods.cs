f0a246114ef86f80b297d9153e2e4e491691704e
Daniel Marbach <daniel.marbach@openplace.net>
2025-10-14 15:19:38 +0200

    Remove unneeded reflection in persistence seam (#7429)
    * Remove unneeded reflection
    
    * Use Activator.CreateInstance directly to avoid constructor selection
    
    * Small cleanup
    
    * Get rid of structural equatable since that should not be needed
    
    * Guard against null
    
    * Align attributes
    
    * Fix another null case
    
    * Remove null checks since they are not necessary since null can be casted to ReadOnlySpan<byte> which creates an empty span
    
    ---------
    
    Co-authored-by: Daniel Marbach <danielmarbach@users.noreply.github.com>

--- src/NServiceBus.Core/Utils/Reflection/ExtensionMethods.cs
+++ src/NServiceBus.Core/Utils/Reflection/ExtensionMethods.cs
@@ -1,39 +1,28 @@
 namespace NServiceBus;
 
 using System;
-using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+using System.Diagnostics.CodeAnalysis;
 
 static class TypeExtensionMethods
 {
-    public static T Construct<T>(this Type type)
-    {
-        var defaultConstructor = type.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, Array.Empty<Type>(), null);
-        if (defaultConstructor != null)
-        {
-            return (T)defaultConstructor.Invoke(null);
-        }
-
-        return (T)Activator.CreateInstance(type);
-    }
+    public static T Construct<T>(
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.NonPublicConstructors)]
+        this Type type) => (T)Activator.CreateInstance(type, nonPublic: true);
 
     /// <summary>
     /// Returns true if the type can be serialized as is.
     /// </summary>
-    public static bool IsSimpleType(this Type type)
-    {
-        return type == typeof(string) ||
-               type.IsPrimitive ||
-               type == typeof(decimal) ||
-               type == typeof(Guid) ||
-               type == typeof(DateTime) ||
-               type == typeof(TimeSpan) ||
-               type == typeof(DateTimeOffset) ||
-               type.IsEnum;
-    }
+    public static bool IsSimpleType(this Type type) =>
+        type == typeof(string) ||
+        type.IsPrimitive ||
+        type == typeof(decimal) ||
+        type == typeof(Guid) ||
+        type == typeof(DateTime) ||
+        type == typeof(TimeSpan) ||
+        type == typeof(DateTimeOffset) ||
+        type.IsEnum;
 
     public static bool IsNullableType(this Type type)
     {
@@ -49,9 +38,8 @@ static class TypeExtensionMethods
     /// Takes the name of the given type and makes it friendly for serialization
     /// by removing problematic characters.
     /// </summary>
-    public static string SerializationFriendlyName(this Type t)
-    {
-        return TypeToNameLookup.GetOrAdd(t.TypeHandle, typeHandle =>
+    public static string SerializationFriendlyName([NotNull] this Type t) =>
+        TypeToNameLookup.GetOrAdd(t.TypeHandle, static (typeHandle, t) =>
         {
             var index = t.Name.IndexOf('`');
             if (index >= 0)
@@ -78,38 +66,32 @@ static class TypeExtensionMethods
                 return result;
             }
             return Type.GetTypeFromHandle(typeHandle).Name;
-        });
-    }
+        }, t);
 
-    static bool IsClrType(byte[] a1)
-    {
-        IStructuralEquatable structuralEquatable = a1;
-        return structuralEquatable.Equals(MsPublicKeyToken, StructuralComparisons.StructuralEqualityComparer);
-    }
+    static bool IsClrType(ReadOnlySpan<byte> publicKeyToken) => publicKeyToken.SequenceEqual(MsPublicKeyToken);
 
     public static bool IsSystemType(this Type type)
     {
-        if (!IsSystemTypeCache.TryGetValue(type.TypeHandle, out var result))
+        if (IsSystemTypeCache.TryGetValue(type.TypeHandle, out var result))
         {
-            var nameOfContainingAssembly = type.Assembly.GetName().GetPublicKeyToken();
-            IsSystemTypeCache[type.TypeHandle] = result = IsClrType(nameOfContainingAssembly);
+            return result;
         }
 
+        var publicKeyTokenOfContainingAssembly = type.Assembly.GetName().GetPublicKeyToken();
+        IsSystemTypeCache[type.TypeHandle] = result = IsClrType(publicKeyTokenOfContainingAssembly);
         return result;
     }
 
-    public static bool IsFromParticularAssembly(this Type type)
-    {
-        return type.Assembly.GetName()
+    public static bool IsFromParticularAssembly(this Type type) =>
+        type.Assembly.GetName()
             .GetPublicKeyToken()
             .SequenceEqual(nsbPublicKeyToken);
-    }
 
     static readonly byte[] MsPublicKeyToken = typeof(string).Assembly.GetName().GetPublicKeyToken();
 
-    static readonly ConcurrentDictionary<RuntimeTypeHandle, bool> IsSystemTypeCache = new ConcurrentDictionary<RuntimeTypeHandle, bool>();
+    static readonly ConcurrentDictionary<RuntimeTypeHandle, bool> IsSystemTypeCache = new();
 
-    static readonly ConcurrentDictionary<RuntimeTypeHandle, string> TypeToNameLookup = new ConcurrentDictionary<RuntimeTypeHandle, string>();
+    static readonly ConcurrentDictionary<RuntimeTypeHandle, string> TypeToNameLookup = new();
 
     static readonly byte[] nsbPublicKeyToken = typeof(TypeExtensionMethods).Assembly.GetName().GetPublicKeyToken();
 }
