c8b42efe3adf4d298effbbb8bfe8847af3807a08
David Boike <david.boike@gmail.com>
2025-10-28 13:47:19 -0500

    Memoizable at all 3 levels

---
+++ src/NServiceBus.Core.Analyzer/Utility/EquatableArray.cs
@@ -0,0 +1,33 @@
+namespace NServiceBus.Core.Analyzer;
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+/// <summary>
+/// Useful in incremental source generators because ImmutableArray&lt;T&gt;> is not memoizable.
+/// </summary>
+readonly record struct EquatableArray<T>(ImmutableArray<T> Items)
+{
+    public bool Equals(EquatableArray<T> other) =>
+        Items.SequenceEqual(other.Items);
+
+    public override int GetHashCode()
+    {
+        // System.HashCode unavailable in netstandard2.0 :-(
+
+        unchecked // Overflow is fine, just wrap
+        {
+            int hash = 17;
+            foreach (var item in Items)
+            {
+                hash = (hash * 23) + item.GetHashCode();
+            }
+            return hash;
+        }
+    }
+
+    public static implicit operator ImmutableArray<T>(EquatableArray<T> e) => e.Items;
+    public static implicit operator EquatableArray<T>(ImmutableArray<T> a) => new(a);
+
+}
