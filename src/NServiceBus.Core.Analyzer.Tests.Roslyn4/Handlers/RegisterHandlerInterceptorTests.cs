8adea2c45af2b98486fadea788192b7446bb9bbb
David Boike <david.boike@gmail.com>
2025-10-30 16:23:30 -0500

    Hash only handler type and dedupe calls

--- src/NServiceBus.Core.Analyzer.Tests.Roslyn4/Handlers/RegisterHandlerInterceptorTests.cs
+++ src/NServiceBus.Core.Analyzer.Tests.Roslyn4/Handlers/RegisterHandlerInterceptorTests.cs
@@ -19,6 +19,8 @@ public class RegisterHandlerInterceptorTests
                         {
                             cfg.RegisterHandler<Handles1>();
                             cfg.RegisterHandler<Handles3>();
+                            // Duplicate call, methods should be deduped with 2 InterceptsLocation attributes
+                            cfg.RegisterHandler<Handles3>();
                         }
                      }
                      
