b2e83cb62b382700c71eb145838ca0b96c52d0ff
Andreas Öhlund <andreas.ohlund@particular.net>
2025-11-02 14:05:58 +0100

    More cleanup

--- src/NServiceBus.Core/Sagas/SagaFinderDefinition.cs
+++ src/NServiceBus.Core/Sagas/SagaFinderDefinition.cs
@@ -11,18 +11,12 @@ public class SagaFinderDefinition
     {
         SagaFinder = sagaFinder;
         MessageType = messageType;
-        MessageTypeName = messageType.FullName;
     }
 
     /// <summary>
     /// The type of message this finder is associated with.
     /// </summary>
     public Type MessageType { get; }
-
-    /// <summary>
-    /// The full name of the message type.
-    /// </summary>
-    public string MessageTypeName { get; }
 
     internal ICoreSagaFinder SagaFinder { get; }
 }
