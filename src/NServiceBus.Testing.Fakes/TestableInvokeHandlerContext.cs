ce82ea7c55676bfd57f6c6f42f41c8288784fc53
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-11-03 17:48:32 +0100

    Simplify

--- src/NServiceBus.Testing.Fakes/TestableInvokeHandlerContext.cs
+++ src/NServiceBus.Testing.Fakes/TestableInvokeHandlerContext.cs
@@ -1,5 +1,6 @@
 ﻿namespace NServiceBus.Testing;
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Persistence;
@@ -41,7 +42,10 @@ public partial class TestableInvokeHandlerContext : TestableIncomingContext, IIn
     /// <summary>
     /// The current <see cref="T:NServiceBus.IHandleMessages`1" /> being executed.
     /// </summary>
-    public MessageHandler MessageHandler { get; set; } = new MessageHandler((instance, message, context) => Task.CompletedTask, typeof(object));
+    public MessageHandler MessageHandler { get; set; } = new MessageHandler
+    {
+        HandlerType = typeof(object)
+    };
 
     /// <summary>
     /// Message headers.
