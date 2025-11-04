840f3c70647939541fc7aa5de3f904ac28b81190
Daniel Marbach <daniel.marbach@openplace.net>
2025-11-04 19:15:09 +0100

    Move UseTransport to extension type similar to how persistence, serializer works (#7451)
    * Move UseTransport to extension type similar to how persistence, serializer etc. works
    
    * Skip extension method
    It will still fail when an extension method doesn't end with a period or is empty
    
    * Simplify
    
    * Add more documentation
    
    ---------
    
    Co-authored-by: Daniel Marbach <danielmarbach@users.noreply.github.com>

---
+++ src/NServiceBus.Core/Transports/TransportConfig.cs
@@ -0,0 +1,27 @@
+#nullable enable
+
+namespace NServiceBus;
+
+using Configuration.AdvancedExtensibility;
+using Transport;
+
+/// <summary>
+/// Enables users to select the transport by calling .UseTransport().
+/// </summary>
+public static class TransportConfig
+{
+    extension(EndpointConfiguration endpointConfiguration)
+    {
+        /// <summary>
+        /// Configures NServiceBus to use the given transport.
+        /// </summary>
+        /// <typeparam name="TTransport">The transport definition eg <see cref="LearningTransport" />, AzureServiceBusTransport etc.</typeparam>
+        public RoutingSettings<TTransport> UseTransport<TTransport>(TTransport transportDefinition)
+            where TTransport : TransportDefinition
+        {
+            var settings = endpointConfiguration.GetSettings();
+            settings.Get<TransportSeam.Settings>().TransportDefinition = transportDefinition;
+            return new RoutingSettings<TTransport>(settings);
+        }
+    }
+}
