e7feb7491a03d82c0a82c37211b7ebe49780e460
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-11-06 12:45:03 +0100

    Remove unnecessary tests

--- src/NServiceBus.Core.Tests/Persistence/PersistenceStartupTests.cs
+++ src/NServiceBus.Core.Tests/Persistence/PersistenceStartupTests.cs
@@ -1,8 +1,5 @@
 ﻿namespace NServiceBus.Core.Tests.Persistence;
 
-using System;
-using NServiceBus.Features;
-using NServiceBus.Persistence;
 using NUnit.Framework;
 using Settings;
 
@@ -17,68 +14,5 @@ public class When_no_persistence_has_been_configured
         var supported = settings.HasSupportFor<StorageType.Subscriptions>();
 
         Assert.That(supported, Is.False);
-    }
-}
-
-[TestFixture]
-public class When_persistence_has_been_configured
-{
-    [Test]
-    public void Should_prevent_using_different_persistence_for_sagas_and_outbox_for_both_features_enabled()
-    {
-        var config = new EndpointConfiguration("MyEndpoint");
-        config.UsePersistence<FakeSagaPersistence, StorageType.Sagas>();
-        config.UsePersistence<FakeOutboxPersistence, StorageType.Outbox>();
-
-        config.EnableFeature<Outbox>();
-        config.EnableFeature<Sagas>();
-
-        Assert.Throws<Exception>(() =>
-        {
-            config.Settings.ConfigurePersistence();
-        }, "Sagas and Outbox need to use the same type of persistence. Saga is configured to use FakeSagaPersistence. Outbox is configured to use FakeOutboxPersistence");
-    }
-
-    [TestCase(true, false)]
-    [TestCase(false, true)]
-    public void Should_not_prevent_using_different_persistence_for_sagas_and_outbox_if_only_one_of_the_features_is_enabled(bool sagasEnabled, bool outboxEnabled)
-    {
-        var config = new EndpointConfiguration("MyEndpoint");
-        config.UsePersistence<FakeSagaPersistence, StorageType.Sagas>();
-        config.UsePersistence<FakeOutboxPersistence, StorageType.Outbox>();
-
-        if (sagasEnabled)
-        {
-            config.EnableFeature<Sagas>();
-        }
-
-        if (outboxEnabled)
-        {
-            config.EnableFeature<Outbox>();
-        }
-
-        Assert.DoesNotThrow(() => config.Settings.ConfigurePersistence(), "Should not throw for a single single feature enabled out of the two.");
-    }
-
-    class FakeSagaPersistence : PersistenceDefinition, IPersistenceDefinitionFactory<FakeSagaPersistence>
-    {
-        FakeSagaPersistence() => Supports<StorageType.Sagas, FakeStorage>();
-        public static FakeSagaPersistence Create() => new();
-
-        class FakeStorage : Feature
-        {
-            protected override void Setup(FeatureConfigurationContext context) => throw new System.NotImplementedException();
-        }
-    }
-
-    class FakeOutboxPersistence : PersistenceDefinition, IPersistenceDefinitionFactory<FakeOutboxPersistence>
-    {
-        FakeOutboxPersistence() => Supports<StorageType.Outbox, FakeStorage>();
-        public static FakeOutboxPersistence Create() => new();
-
-        class FakeStorage : Feature
-        {
-            protected override void Setup(FeatureConfigurationContext context) => throw new System.NotImplementedException();
-        }
     }
 }
