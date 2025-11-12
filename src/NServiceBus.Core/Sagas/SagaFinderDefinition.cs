28a43239947fcc99cbc112eb360b7fe4b41050e4
Andreas Öhlund <andreas.ohlund@particular.net>
2025-11-12 11:30:11 +0100

    Add finder type back in

--- src/NServiceBus.Core/Sagas/SagaFinderDefinition.cs
+++ src/NServiceBus.Core/Sagas/SagaFinderDefinition.cs
@@ -11,12 +11,18 @@ public class SagaFinderDefinition
     {
         SagaFinder = sagaFinder;
         MessageType = messageType;
+        Type = sagaFinder.GetType();
     }
 
     /// <summary>
     /// The type of message this finder is associated with.
     /// </summary>
     public Type MessageType { get; }
+
+    /// <summary>
+    /// The finder type.
+    /// </summary>
+    public Type Type { get; }
 
     internal ICoreSagaFinder SagaFinder { get; }
 }
