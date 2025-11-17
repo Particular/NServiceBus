a3dac85db30bf94681adb2030a9d338ec4622c39
Andreas Öhlund <andreas.ohlund@particular.net>
2025-11-17 12:53:59 +0100

    Simplify acceptance test scanning

--- src/NServiceBus.AcceptanceTesting/Customization/EndpointConfigurationExtensions.cs
+++ src/NServiceBus.AcceptanceTesting/Customization/EndpointConfigurationExtensions.cs
@@ -4,49 +4,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Features;
 using Hosting.Helpers;
-using Installation;
 using Sagas;
 using Support;
 
 public static class EndpointConfigurationExtensions
 {
     /// <summary>
-    /// Backdoor into the core types to scan. This allows people to test a subset of functionality when running Acceptance tests
-    /// </summary>
-    /// <param name="config">The <see cref="EndpointConfiguration"/> instance to apply the settings to.</param>
-    /// <param name="typesToScan">Override the types to scan.</param>
-    public static void TypesToIncludeInScan(this EndpointConfiguration config, IEnumerable<Type> typesToScan)
-    {
-        config.TypesToScanInternal(typesToScan);
-    }
-
-    /// <summary>
-    /// Uses <see cref="TypesToIncludeInScan"/> to scan all types via the <see cref="AssemblyScanner"/> that are currently loaded, filtering by customizations defined in <see cref="EndpointCustomizationConfiguration"/>.
+    /// Uses <see cref="EndpointConfiguration.T"/> to scan all types via the <see cref="AssemblyScanner"/> that are currently loaded, filtering by customizations defined in <see cref="EndpointCustomizationConfiguration"/>.
     /// Additionally, this method excludes all types on the same assembly that not relevant to the specific test case. All types that should be scanned by default must be nested classes of the test class.
     /// </summary>
     public static void ScanTypesForTest(this EndpointConfiguration config,
         EndpointCustomizationConfiguration customizationConfiguration)
     {
-        // disable file system scanning for better performance
-        // note that this might cause issues when required assemblies are only being loaded at endpoint startup time
-        var assemblyScanner = new AssemblyScanner
-        {
-            ScanFileSystemAssemblies = false
-        };
+        var typesToIncludeInScanning = GetNestedTypeRecursive(customizationConfiguration.BuilderType.DeclaringType, customizationConfiguration.BuilderType)
+            .Where(t => t.IsAssignableTo(typeof(IHandleMessages))
+                        || t.IsAssignableTo(typeof(IFinder))
+                        || t.IsAssignableTo(typeof(IHandleSagaNotFound))
+                        || t.IsAssignableTo(typeof(Saga)))
+            .Union(customizationConfiguration.TypesToInclude);
 
-        config.TypesToIncludeInScan(
-        [
-            .. assemblyScanner.GetScannableAssemblies().Types
-                .Except(customizationConfiguration.BuilderType.Assembly.GetTypes()) // exclude all types from test assembly by default
-                .Union(GetNestedTypeRecursive(customizationConfiguration.BuilderType.DeclaringType, customizationConfiguration.BuilderType))
-                .Where(t => t.IsAssignableTo(typeof(IHandleMessages))
-                            || t.IsAssignableTo(typeof(IFinder))
-                            || t.IsAssignableTo(typeof(IHandleSagaNotFound))
-                            || t.IsAssignableTo(typeof(Saga)))
-                .Union(customizationConfiguration.TypesToInclude)
-        ]);
+        config.TypesToScanInternal(typesToIncludeInScanning);
 
         IEnumerable<Type> GetNestedTypeRecursive(Type rootType, Type builderType)
         {
