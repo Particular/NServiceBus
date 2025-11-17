d2fabbfc885ecc243f8ba0d4efb61bd2126a8ee7
Andreas Öhlund <andreas.ohlund@particular.net>
2025-11-17 10:18:13 +0100

    Add unit test for both file and appdomain scanning being turned off

--- src/NServiceBus.Core.Tests/AssemblyScanner/AssemblyScanningComponentTests.cs
+++ src/NServiceBus.Core.Tests/AssemblyScanner/AssemblyScanningComponentTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace NServiceBus.Core.Tests.AssemblyScanner;
 
+using System;
 using System.IO;
 using System.Linq;
 using Settings;
@@ -16,7 +17,10 @@ class AssemblyScanningComponentTests
 
         var configuration = new AssemblyScanningComponent.Configuration(settingsHolder)
         {
-            AssemblyScannerConfiguration = { AdditionalAssemblyScanningPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestDlls", "Nested", "Subfolder") }
+            AssemblyScannerConfiguration =
+            {
+                AdditionalAssemblyScanningPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestDlls", "Nested", "Subfolder")
+            }
         };
 
         var component = AssemblyScanningComponent.Initialize(configuration, settingsHolder);
@@ -24,5 +28,25 @@ class AssemblyScanningComponentTests
         var foundTypeFromScannedPath = component.AvailableTypes.Any(x => x.Name == "NestedClass");
 
         Assert.That(foundTypeFromScannedPath, Is.True, "Was expected to scan a custom path, but 'nested.dll' was not scanned.");
+    }
+
+    [Test]
+    public void Should_throw_when_both_file_and_appdomain_scanning_turned_off()
+    {
+        var settingsHolder = new SettingsHolder();
+        settingsHolder.Set(new HostingComponent.Settings(settingsHolder));
+
+        var configuration = new AssemblyScanningComponent.Configuration(settingsHolder)
+        {
+            AssemblyScannerConfiguration =
+            {
+                ScanFileSystemAssemblies = false,
+                ScanAppDomainAssemblies = false
+            }
+        };
+
+        var exception = Assert.Throws<Exception>(() => AssemblyScanningComponent.Initialize(configuration, settingsHolder));
+
+        Assert.That(exception.Message, Does.Contain($"Assembly scanning has been disabled. This prevents messages, message handlers, features and other functionality from loading correctly. Enable {nameof(AssemblyScannerConfiguration.ScanAppDomainAssemblies)} or {nameof(AssemblyScannerConfiguration.ScanFileSystemAssemblies)}"));
     }
 }
