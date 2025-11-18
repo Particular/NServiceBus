696166727e85b9a470c80767364dcd79f1574a9d
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-11-18 15:42:58 +0100

    Remove using

--- src/NServiceBus.Core.Analyzer/Handlers/AddHandlerInterceptor.cs
+++ src/NServiceBus.Core.Analyzer/Handlers/AddHandlerInterceptor.cs
@@ -11,7 +11,6 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Operations;
-using Microsoft.CodeAnalysis.Text;
 
 [Generator]
 public class AddHandlerInterceptor : IIncrementalGenerator
