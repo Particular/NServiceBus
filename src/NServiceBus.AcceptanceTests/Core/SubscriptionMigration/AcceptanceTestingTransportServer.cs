1910fa32d88dddeeaac5a1a380780b2bc02ba927
Andreas Öhlund <andreas.ohlund@particular.net>
2025-11-19 14:23:58 +0100

    Cleanup att server

--- src/NServiceBus.AcceptanceTests/Core/SubscriptionMigration/AcceptanceTestingTransportServer.cs
+++ src/NServiceBus.AcceptanceTests/Core/SubscriptionMigration/AcceptanceTestingTransportServer.cs
@@ -5,40 +5,32 @@ using System.Threading.Tasks;
 using AcceptanceTesting.Customization;
 using AcceptanceTesting.Support;
 
-class AcceptanceTestingTransportServer : IEndpointSetupTemplate
+class AcceptanceTestingTransportServer(bool useNativePubSub) : IEndpointSetupTemplate
 {
-    readonly bool useNativePubSub;
-
-    public AcceptanceTestingTransportServer(bool useNativePubSub)
-    {
-        this.useNativePubSub = useNativePubSub;
-    }
-
     public async Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointCustomizationConfiguration, Func<EndpointConfiguration, Task> configurationBuilderCustomization)
     {
-        var configuration = new EndpointConfiguration(endpointCustomizationConfiguration.EndpointName);
-        configuration.EnableInstallers();
+        var endpointConfiguration = new EndpointConfiguration(endpointCustomizationConfiguration.EndpointName);
+        endpointConfiguration.EnableInstallers();
 
-        var recoverability = configuration.Recoverability();
+        var recoverability = endpointConfiguration.Recoverability();
         recoverability.Delayed(delayed => delayed.NumberOfRetries(0));
         recoverability.Immediate(immediate => immediate.NumberOfRetries(0));
-        configuration.SendFailedMessagesTo("error");
+        endpointConfiguration.SendFailedMessagesTo("error");
 
         var transportConfiguration = new ConfigureEndpointAcceptanceTestingTransport(useNativePubSub, true);
-        await transportConfiguration.Configure(endpointCustomizationConfiguration.EndpointName, configuration, runDescriptor.Settings, endpointCustomizationConfiguration.PublisherMetadata);
+        await transportConfiguration.Configure(endpointCustomizationConfiguration.EndpointName, endpointConfiguration, runDescriptor.Settings, endpointCustomizationConfiguration.PublisherMetadata);
         runDescriptor.OnTestCompleted(_ => transportConfiguration.Cleanup());
 
         var persistenceConfiguration = new ConfigureEndpointAcceptanceTestingPersistence();
-        await persistenceConfiguration.Configure(endpointCustomizationConfiguration.EndpointName, configuration, runDescriptor.Settings, endpointCustomizationConfiguration.PublisherMetadata);
+        await persistenceConfiguration.Configure(endpointCustomizationConfiguration.EndpointName, endpointConfiguration, runDescriptor.Settings, endpointCustomizationConfiguration.PublisherMetadata);
         runDescriptor.OnTestCompleted(_ => persistenceConfiguration.Cleanup());
 
-        configuration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 
-        await configurationBuilderCustomization(configuration);
+        await configurationBuilderCustomization(endpointConfiguration);
 
-        // scan types at the end so that all types used by the configuration have been loaded into the AppDomain
-        configuration.ScanTypesForTest(endpointCustomizationConfiguration);
+        endpointConfiguration.ScanTypesForTest(endpointCustomizationConfiguration);
 
-        return configuration;
+        return endpointConfiguration;
     }
 }
