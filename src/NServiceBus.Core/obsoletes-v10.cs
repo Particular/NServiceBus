a7a956ff584b45a04363d54cf238d1c7f1b748d1
Daniel Marbach <daniel.marbach@openplace.net>
2025-11-20 14:50:44 +0100

    Update obsolete messages for MessageHandler to reflect correct replacement methods (#7494)
    Co-authored-by: Daniel Marbach <danielmarbach@users.noreply.github.com>

--- src/NServiceBus.Core/obsoletes-v10.cs
+++ src/NServiceBus.Core/obsoletes-v10.cs
@@ -502,8 +502,8 @@ namespace NServiceBus.Pipeline
             // Won't be needed once the obsolete member is removed.
         }
 
-        [ObsoleteMetadata(ReplacementTypeOrMember = "MessageHandler<THandler, TMessage>", RemoveInVersion = "11", TreatAsErrorFromVersion = "10")]
-        [Obsolete("Use 'MessageHandler<THandler, TMessage>' instead. Will be removed in version 11.0.0.", true)]
+        [ObsoleteMetadata(ReplacementTypeOrMember = "MessageHandler()", RemoveInVersion = "11", TreatAsErrorFromVersion = "10")]
+        [Obsolete("Use 'MessageHandler()' instead. Will be removed in version 11.0.0.", true)]
         public MessageHandler(Func<object, object, IMessageHandlerContext, Task> invocation, Type handlerType)
             => throw new NotImplementedException();
     }
