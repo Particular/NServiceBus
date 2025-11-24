67e2eb4372147e5792aba69d7071a318942847dc
Hazel Bohon <hazel.bohon@particular.net>
2025-11-24 09:10:18 -0600

    Update names, add docstring.

--- src/NServiceBus.Core/Envelopes/IEnvelopeHandler.cs
+++ src/NServiceBus.Core/Envelopes/IEnvelopeHandler.cs
@@ -7,7 +7,15 @@ using Transport;
 
 public interface IEnvelopeHandler
 {
-    (Dictionary<string, string> headers, ReadOnlyMemory<byte> body) CreateIncomingMessage(string nativeMessageId, IDictionary<string, string> headers, ContextBag extensions, ReadOnlyMemory<byte> body);
+    /// <summary>
+    /// Create the incoming message context from the transport prior to passing the incoming message to the pipeline.
+    /// </summary>
+    /// <param name="nativeMessageId">The native message id provided by the transport. This is included for reference purposes, and should be considered readonly.</param>
+    /// <param name="incomingHeaders">Headers provided by the transport.</param>
+    /// <param name="extensions">ContextBag of extension values provided by the transport.</param>
+    /// <param name="incomingBody">The raw body provided by the transport.</param>
+    /// <returns></returns>
+    (Dictionary<string, string> headers, ReadOnlyMemory<byte> body) CreateIncomingMessage(string nativeMessageId, IDictionary<string, string> incomingHeaders, ContextBag extensions, ReadOnlyMemory<byte> incomingBody);
 
     /// <summary>
     /// Validates the given <see cref="MessageContext"/>.
