b857f17f1ad2c0e3e6fce414ad025f1252b2b873
Andreas Öhlund <andreas.ohlund@particular.net>
2025-11-25 16:33:38 +0100

    Fix test

--- src/NServiceBus.Core.Analyzer.Tests.Common/Sagas/ConfigureHowToFindSagaTests.cs
+++ src/NServiceBus.Core.Analyzer.Tests.Common/Sagas/ConfigureHowToFindSagaTests.cs
@@ -36,32 +36,6 @@ public class ConfigureHowToFindSagaTests : AnalyzerTestFixture<SagaAnalyzer>
     }
 
     [Test]
-    public Task OldMapping()
-    {
-        var code = @"
-    protected override void [|ConfigureHowToFindSaga|](SagaPropertyMapper<MyData> mapper)
-    {
-        mapper.ConfigureMapping<Msg1>(msg => msg.CorrId).ToSaga(saga => saga.CorrId);
-        mapper.ConfigureMapping<Msg2>(msg => msg.CorrId).ToSaga(saga => saga.CorrId);
-    }";
-
-        return RunTest(code, DiagnosticIds.SagaMappingExpressionCanBeSimplified);
-    }
-
-    [Test]
-    public Task OldMappingWithMultipleCorrelationIds()
-    {
-        var code = @"
-    protected override void ConfigureHowToFindSaga(SagaPropertyMapper<MyData> mapper)
-    {
-        mapper.ConfigureMapping<Msg1>(msg => msg.CorrId).ToSaga(saga => saga.CorrId);
-        mapper.ConfigureMapping<Msg2>(msg => msg.CorrId).ToSaga([|saga => saga.OtherId|]);
-    }";
-
-        return RunTest(code, DiagnosticIds.MultipleCorrelationIdValues);
-    }
-
-    [Test]
     public Task NonMappingExpressionInMethod()
     {
         var code = @"
@@ -83,7 +57,7 @@ public class ConfigureHowToFindSagaTests : AnalyzerTestFixture<SagaAnalyzer>
     protected virtual Task RunTest(string configureHowToFindSagaMethod, string diagnosticId)
     {
         var nullableTypesSource =
-@"
+            @"
 #nullable enable
 using System;
 using System.Threading.Tasks;
