866d1a6b5e8bf8c112877a2da14689e96bb624e6
Mauro Servienti <mauro.servienti@gmail.com>
2025-11-26 11:11:52 +0100

    Redesign to use a single method

--- src/NServiceBus.Core/Envelopes/EnvelopesRouter.cs
+++ src/NServiceBus.Core/Envelopes/EnvelopesRouter.cs
@@ -10,14 +10,22 @@ class EnvelopesRouter(IEnumerable<IEnvelopeHandler> envelopeHandlers)
 
     internal IncomingMessage Translate(MessageContext messageContext)
     {
-        // TODO: Is there any point in optimizing this to never hit the foreach if the translators list is empty.
-        // https://stackoverflow.com/questions/45651325/performance-before-using-a-foreach-loop-check-if-the-list-is-empty
         foreach (var envelopeHandler in envelopeHandlers)
         {
-            if (envelopeHandler.CanUnwrapEnvelope(messageContext.NativeMessageId, messageContext.Headers, messageContext.Extensions, messageContext.Body))
+            var result = envelopeHandler.UnwrapEnvelope(messageContext.NativeMessageId, messageContext.Headers, messageContext.Extensions, messageContext.Body);
+            switch (result)
             {
-                (Dictionary<string, string> headers, ReadOnlyMemory<byte> body) = envelopeHandler.UnwrapEnvelope(messageContext.NativeMessageId, messageContext.Headers, messageContext.Extensions, messageContext.Body);
-                return new IncomingMessage(messageContext.NativeMessageId, headers, body);
+                case EnvelopeUnwrapResult.Malformed { Exception: not null } malformedEnvelope:
+                    throw new Exception("Failure while handling the message envelope", malformedEnvelope.Exception);
+                case EnvelopeUnwrapResult.Malformed:
+                    throw new Exception("Failure while handling the message envelope");
+                case EnvelopeUnwrapResult.Success success:
+                    return new IncomingMessage(messageContext.NativeMessageId, success.Headers, success.Body);
+                case EnvelopeUnwrapResult.UnsupportedEnvelope:
+                    // maybe log and move on
+                    continue;
+                default:
+                    continue;
             }
         }
 
--- src/NServiceBus.Core/Envelopes/IEnvelopeHandler.cs
+++ src/NServiceBus.Core/Envelopes/IEnvelopeHandler.cs
@@ -18,16 +18,6 @@ public interface IEnvelopeHandler
     /// <param name="incomingHeaders">Headers provided by the transport.</param>
     /// <param name="extensions">ContextBag of extension values provided by the transport.</param>
     /// <param name="incomingBody">The raw body provided by the transport.</param>
-    /// <returns>Dictionary of headers and byte array of message body.</returns>
-    (Dictionary<string, string> headers, ReadOnlyMemory<byte> body) UnwrapEnvelope(string nativeMessageId, IDictionary<string, string> incomingHeaders, ContextBag extensions, ReadOnlyMemory<byte> incomingBody);
-
-    /// <summary>
-    /// Determines if this envelope handler can unwrap the given message.
-    /// </summary>
-    /// <param name="nativeMessageId">The native message id provided by the transport. This is included for reference purposes, and should be considered readonly.</param>
-    /// <param name="incomingHeaders">Headers provided by the transport.</param>
-    /// <param name="extensions">ContextBag of extension values provided by the transport.</param>
-    /// <param name="incomingBody">The raw body provided by the transport.</param>
-    /// <returns>True if the message envelope can be unwrapped by this handler; otherwise, false.</returns>
-    bool CanUnwrapEnvelope(string nativeMessageId, IDictionary<string, string> incomingHeaders, ContextBag extensions, ReadOnlyMemory<byte> incomingBody);
+    /// <returns>The result of the unwrap operation.</returns>
+    EnvelopeUnwrapResult UnwrapEnvelope(string nativeMessageId, IDictionary<string, string> incomingHeaders, ContextBag extensions, ReadOnlyMemory<byte> incomingBody);
 }
