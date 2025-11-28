0b57bac6ce4103fb493221269ce64ec55c6bea50
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-11-28 09:04:42 +0100

    Cleanup

--- src/NServiceBus.Core.Analyzer/Sagas/AddSagaInterceptor.Parser.cs
+++ src/NServiceBus.Core.Analyzer/Sagas/AddSagaInterceptor.Parser.cs
@@ -5,7 +5,6 @@ namespace NServiceBus.Core.Analyzer.Sagas;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using Handlers;
 using Microsoft.CodeAnalysis;
