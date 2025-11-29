a80a6ad14e037e9d3f3aceb1404597d05c658b8f
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-11-29 13:21:42 +0100

    Remove behaviors from the service collection

--- src/NServiceBus.Core/Pipeline/PipelineComponent.cs
+++ src/NServiceBus.Core/Pipeline/PipelineComponent.cs
@@ -5,29 +5,14 @@ using System.Diagnostics.Metrics;
 using Microsoft.Extensions.DependencyInjection;
 using Pipeline;
 
-class PipelineComponent
+sealed class PipelineComponent
 {
-    PipelineComponent(PipelineModifications modifications)
-    {
-        this.modifications = modifications;
-    }
+    PipelineComponent(PipelineModifications modifications) => this.modifications = modifications;
 
     public static PipelineComponent Initialize(PipelineSettings settings,
         HostingComponent.Configuration hostingConfiguration, ReceiveComponent.Configuration receiveConfiguration)
     {
-        var modifications = settings.modifications;
-
-        foreach (var registeredBehavior in modifications.Replacements)
-        {
-            hostingConfiguration.Services.AddTransient(registeredBehavior.BehaviorType);
-        }
-
-        foreach (var step in modifications.Additions)
-        {
-            step.ApplyContainerRegistration(hostingConfiguration.Services);
-        }
-
-        // make the PipelineMetrics available to the Pipeline 
+        // make the PipelineMetrics available to the Pipeline
         hostingConfiguration.Services.AddSingleton(sp =>
         {
             var meterFactory = sp.GetService<IMeterFactory>();
@@ -35,18 +20,12 @@ class PipelineComponent
             return new IncomingPipelineMetrics(meterFactory, receiveConfiguration.LocalQueueAddress.BaseAddress, discriminator);
         });
 
-        return new PipelineComponent(modifications);
+        return new PipelineComponent(settings.modifications);
     }
 
-    public Pipeline<T> CreatePipeline<T>(IServiceProvider builder) where T : IBehaviorContext
-    {
-        return new Pipeline<T>(builder, modifications);
-    }
+    public Pipeline<T> CreatePipeline<T>(IServiceProvider builder) where T : IBehaviorContext => new(builder, modifications);
 
-    public PipelineCache BuildPipelineCache(IServiceProvider rootBuilder)
-    {
-        return new PipelineCache(rootBuilder, modifications);
-    }
+    public PipelineCache BuildPipelineCache(IServiceProvider rootBuilder) => new(rootBuilder, modifications);
 
     readonly PipelineModifications modifications;
 }
