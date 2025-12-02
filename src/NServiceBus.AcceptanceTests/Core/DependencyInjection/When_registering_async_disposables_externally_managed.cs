dd81af5e824f9b9eb1c3d528d5c40c379b446240
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-12-02 20:32:38 +0100

    Augment the externally managed test

--- src/NServiceBus.AcceptanceTests/Core/DependencyInjection/When_registering_async_disposables_externally_managed.cs
+++ src/NServiceBus.AcceptanceTests/Core/DependencyInjection/When_registering_async_disposables_externally_managed.cs
@@ -3,6 +3,7 @@ namespace NServiceBus.AcceptanceTests.Core.DependencyInjection;
 using System;
 using System.Threading.Tasks;
 using AcceptanceTesting;
+using AcceptanceTesting.Support;
 using EndpointTemplates;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
@@ -14,6 +15,7 @@ public class When_registering_async_disposables_externally_managed : NServiceBus
     public async Task Should_dispose()
     {
         var context = await Scenario.Define<Context>()
+            .WithComponent(new CustomComponent())
             .WithEndpoint<EndpointWithAsyncDisposable>(b =>
             {
                 b.Services(static s =>
@@ -32,13 +34,30 @@ public class When_registering_async_disposables_externally_managed : NServiceBus
         {
             Assert.That(context.ScopedAsyncDisposableDisposed, Is.True, "Scoped AsyncDisposable wasn't disposed as it should have been.");
             Assert.That(context.SingletonAsyncDisposableDisposed, Is.True, "Singleton AsyncDisposable wasn't disposed as it should have been.");
+            Assert.That(context.SingletonAsyncDisposableSharedDisposed, Is.True, "Singleton AsyncDisposable Shared wasn't disposed as it should have been.");
+            Assert.That(context.ScopedAsyncDisposableSharedDisposed, Is.True, "Scoped AsyncDisposable Shared wasn't disposed as it should have been.");
         }
+    }
+
+    // Custom component that mimicks registering global shared keyed services
+    class CustomComponent : ComponentRunner, IComponentBehavior
+    {
+        public Task<ComponentRunner> CreateRunner(RunDescriptor run)
+        {
+            run.Services.AddSingleton<SingletonAsyncDisposableShared>();
+            run.Services.AddScoped<ScopedAsyncDisposableShared>();
+            return Task.FromResult<ComponentRunner>(this);
+        }
+
+        public override string Name => nameof(CustomComponent);
     }
 
     class Context : ScenarioContext
     {
         public bool ScopedAsyncDisposableDisposed { get; set; }
         public bool SingletonAsyncDisposableDisposed { get; set; }
+        public bool SingletonAsyncDisposableSharedDisposed { get; set; }
+        public bool ScopedAsyncDisposableSharedDisposed { get; set; }
     }
 
     public class EndpointWithAsyncDisposable : EndpointConfigurationBuilder
@@ -49,13 +68,33 @@ public class When_registering_async_disposables_externally_managed : NServiceBus
         class HandlerWithAsyncDisposable(
             Context testContext,
             ScopedAsyncDisposable scopedAsyncDisposable,
-            SingletonAsyncDisposable singletonAsyncDisposable)
+            SingletonAsyncDisposable singletonAsyncDisposable,
+            ScopedAsyncDisposableShared scopedAsyncDisposableShared,
+            SingletonAsyncDisposableShared singletonAsyncDisposableShared,
+            IServiceProvider serviceProvider)
             : IHandleMessages<SomeMessage>
         {
             public Task Handle(SomeMessage message, IMessageHandlerContext context)
             {
                 scopedAsyncDisposable.Initialize(testContext);
                 singletonAsyncDisposable.Initialize(testContext);
+                singletonAsyncDisposableShared.Initialize(testContext);
+                scopedAsyncDisposableShared.Initialize(testContext);
+
+                using (Assert.EnterMultipleScope())
+                {
+                    Assert.That(scopedAsyncDisposable, Is.SameAs(serviceProvider.GetRequiredService<ScopedAsyncDisposable>()));
+                    Assert.That(scopedAsyncDisposable, Is.SameAs(serviceProvider.GetService<ScopedAsyncDisposable>()));
+
+                    Assert.That(singletonAsyncDisposable, Is.SameAs(serviceProvider.GetRequiredService<SingletonAsyncDisposable>()));
+                    Assert.That(singletonAsyncDisposable, Is.SameAs(serviceProvider.GetService<SingletonAsyncDisposable>()));
+
+                    Assert.That(scopedAsyncDisposableShared, Is.SameAs(serviceProvider.GetRequiredService<ScopedAsyncDisposableShared>()));
+                    Assert.That(scopedAsyncDisposableShared, Is.SameAs(serviceProvider.GetService<ScopedAsyncDisposableShared>()));
+
+                    Assert.That(singletonAsyncDisposableShared, Is.SameAs(serviceProvider.GetRequiredService<SingletonAsyncDisposableShared>()));
+                    Assert.That(singletonAsyncDisposableShared, Is.SameAs(serviceProvider.GetService<SingletonAsyncDisposableShared>()));
+                }
                 return Task.CompletedTask;
             }
         }
@@ -63,31 +102,49 @@ public class When_registering_async_disposables_externally_managed : NServiceBus
 
     public class SomeMessage : IMessage;
 
-    class SingletonAsyncDisposable : IAsyncDisposable
+    abstract class InitializableBase : IAsyncDisposable
     {
         // This method is here to make the code being used in the handler to not trigger compiler warnings
         public void Initialize(Context scenarioContext) => context = scenarioContext;
 
-        public ValueTask DisposeAsync()
+        public abstract ValueTask DisposeAsync();
+
+        protected Context context;
+    }
+
+    class SingletonAsyncDisposableShared : InitializableBase
+    {
+        public override ValueTask DisposeAsync()
+        {
+            context.SingletonAsyncDisposableSharedDisposed = true;
+            return new ValueTask();
+        }
+    }
+
+    class ScopedAsyncDisposableShared : InitializableBase
+    {
+        public override ValueTask DisposeAsync()
+        {
+            context.ScopedAsyncDisposableSharedDisposed = true;
+            return new ValueTask();
+        }
+    }
+
+    class SingletonAsyncDisposable : InitializableBase
+    {
+        public override ValueTask DisposeAsync()
         {
             context.SingletonAsyncDisposableDisposed = true;
             return new ValueTask();
         }
-
-        Context context;
     }
 
-    class ScopedAsyncDisposable : IAsyncDisposable
+    sealed class ScopedAsyncDisposable : InitializableBase
     {
-        // This method is here to make the code being used in the handler to not trigger compiler warnings
-        public void Initialize(Context scenarioContext) => context = scenarioContext;
-
-        public ValueTask DisposeAsync()
+        public override ValueTask DisposeAsync()
         {
             context.ScopedAsyncDisposableDisposed = true;
             return new ValueTask();
         }
-
-        Context context;
     }
 }
