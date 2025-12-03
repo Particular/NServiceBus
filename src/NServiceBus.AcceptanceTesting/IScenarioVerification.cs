a757cc764862056b83402df39717eb71aa84a912
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-12-03 16:05:29 +0100

    Delete no longer used verification classes

--- src/NServiceBus.AcceptanceTesting/IScenarioVerification.cs
+++
@@ -1,9 +0,0 @@
-namespace NServiceBus.AcceptanceTesting;
-
-using System;
-
-public interface IScenarioVerification
-{
-    Type ContextType { get; set; }
-    void Verify(ScenarioContext context);
-}
