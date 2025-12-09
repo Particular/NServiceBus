00d2658a80917ac3bd2e50bd54cdb270d881eca6
Daniel Marbach <daniel.marbach@openplace.net>
2025-12-09 19:38:16 +0100

    Obsolete IWantToRunBeforeConfigurationIsFinalized with a warning (#7544)
    Co-authored-by: Daniel Marbach <danielmarbach@users.noreply.github.com>

--- src/NServiceBus.Core/IWantToRunBeforeConfigurationIsFinalized.cs
+++ src/NServiceBus.Core/IWantToRunBeforeConfigurationIsFinalized.cs
@@ -2,12 +2,16 @@
 
 namespace NServiceBus;
 
+using System;
+using Particular.Obsoletes;
 using Settings;
 
 /// <summary>
 /// Indicates that this class contains logic that needs to run just before
 /// configuration is finalized.
 /// </summary>
+[ObsoleteMetadata(Message = "Final adjustments to settings before configuration is finalized should be applied via an explicit last configuration step on the endpoint configuration, instead of via implementations of this interface discovered by scanning", TreatAsErrorFromVersion = "11.0", RemoveInVersion = "12.0")]
+[Obsolete("Final adjustments to settings before configuration is finalized should be applied via an explicit last configuration step on the endpoint configuration, instead of via implementations of this interface discovered by scanning. Will be treated as an error from version 11.0.0. Will be removed in version 12.0.0.", false)]
 public interface IWantToRunBeforeConfigurationIsFinalized
 {
     /// <summary>
