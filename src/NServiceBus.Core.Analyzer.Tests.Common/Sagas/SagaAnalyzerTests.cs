3043c0a5ef5af1bec6068c96cd51e865311f23a8
Andreas Öhlund <andreas.ohlund@particular.net>
2025-12-09 18:27:02 +0100

    Add test

--- src/NServiceBus.Core.Analyzer.Tests.Common/Sagas/SagaAnalyzerTests.cs
+++ src/NServiceBus.Core.Analyzer.Tests.Common/Sagas/SagaAnalyzerTests.cs
@@ -744,7 +744,59 @@ namespace NServiceBus.Persistence.Sql
 
         return Assert(source);
     }
+ [Test]
+    public Task InterfaceMessagesShouldNotTriggerNSB0016()
+    {
+        var source =
+            @"using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using NServiceBus;
+public class MySaga : Saga<RenewalSagaData>, IAmStartedByMessages<IRenewalSagaCommand>
+{
+    protected override void ConfigureHowToFindSaga(SagaPropertyMapper<RenewalSagaData> mapper)
+    {
+        mapper.MapSaga(saga => saga.PolicyHolderIdentifier)
+            .ToMessage<IRenewalSagaCommand>(m => m.CustomerNumber);
+    }
+    public Task Handle(IRenewalSagaCommand message, IMessageHandlerContext context) => throw new NotImplementedException();
+}
 
+public class RenewalSagaData : CustomerBasedBaseSagaData;
+
+public abstract class CustomerBasedBaseSagaData : BaseSagaData
+{
+    public string PolicyHolderIdentifier { get; set; }
+}
+
+public abstract class BaseSagaData : ContainSagaData
+{
+    protected BaseSagaData()
+    {
+        SagaStartDate = DateTime.UtcNow;
+        SagaLastModifiedDate = DateTime.UtcNow;
+        PoliciesToHandle = new HashSet<string>();
+    }
+    public DateTime SagaStartDate { get; set; }
+    public DateTime SagaLastModifiedDate { get; set; }
+    public HashSet<string> PoliciesToHandle { get; set; }
+    public bool TimeoutRequested { get; set; }
+    public DateTime TimeoutTriggerTime { get; set; }
+}
+public interface IRenewalSagaCommand : ICustomerBasedBaseSagaCommand;
+
+public interface ICustomerBasedBaseSagaCommand : IBaseSagaCommand
+{
+    string CustomerNumber { get; set; }
+}
+
+public interface IBaseSagaCommand : ICommand
+{
+    string PolicyId { get; set; }
+}";
+
+        return Assert(source);
+    }
     [Test]
     public Task ClassesInSeparateFilesAnalyzeSaga()
     {
