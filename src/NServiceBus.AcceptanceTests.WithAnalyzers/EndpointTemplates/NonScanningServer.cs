7a8f8e443f24ccbdb27bdb671d2262273e50e20a
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-12-12 13:03:41 +0100

    Fix non scanning server

--- src/NServiceBus.AcceptanceTests.WithAnalyzers/EndpointTemplates/NonScanningServer.cs
+++ src/NServiceBus.AcceptanceTests.WithAnalyzers/EndpointTemplates/NonScanningServer.cs
@@ -8,15 +8,15 @@ public class NonScanningServer : IEndpointSetupTemplate
 {
     public async Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointCustomizationConfiguration, Func<EndpointConfiguration, Task> configurationBuilderCustomization)
     {
+        endpointCustomizationConfiguration.AutoRegisterHandlers = false;
+        endpointCustomizationConfiguration.AutoRegisterSagas = false;
+
         var defaultServer = new DefaultServer();
         var endpointConfiguration = await defaultServer.GetConfiguration(runDescriptor, endpointCustomizationConfiguration,
             configurationBuilderCustomization);
 
         var scanner = endpointConfiguration.AssemblyScanner();
         scanner.Disable = true;
-
-        endpointCustomizationConfiguration.AutoRegisterHandlers = false;
-        endpointCustomizationConfiguration.AutoRegisterSagas = false;
 
         return endpointConfiguration;
     }
