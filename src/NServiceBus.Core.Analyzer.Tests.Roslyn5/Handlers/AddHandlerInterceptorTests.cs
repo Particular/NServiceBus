e9c742e572072e18dd7b17f0e337c1ea2d168e99
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-12-12 11:03:29 +0100

    More test coverage for partial

--- src/NServiceBus.Core.Analyzer.Tests.Roslyn5/Handlers/AddHandlerInterceptorTests.cs
+++ src/NServiceBus.Core.Analyzer.Tests.Roslyn5/Handlers/AddHandlerInterceptorTests.cs
@@ -57,9 +57,9 @@ public class AddHandlerInterceptorTests
                      using System.Threading.Tasks;
                      using NServiceBus;
 
+                     [NServiceBusRegistrations]
                      public class Test
                      {
-                         [NServiceBusRegistrations]
                          public void Configure(EndpointConfiguration cfg)
                          {
                              cfg.AddHandler<Handles1>();
@@ -277,6 +277,159 @@ public class AddHandlerInterceptorTests
                       public partial class Test
                       {
                           [NServiceBus.NServiceBusRegistrations]
+                          public partial void Configure(NServiceBus.EndpointConfiguration cfg);
+                      }
+                      """;
+
+        SourceGeneratorTest.ForIncrementalGenerator<AddHandlerInterceptor>()
+            .WithSource(source, "test.cs")
+            .WithSource(partial, "partial.cs")
+            .WithGeneratorStages("HandlerSpec", "HandlerSpecs")
+            .Approve()
+            .AssertRunsAreEqual();
+    }
+
+    [Test]
+    public void DecoratedPartialMethodOtherSide()
+    {
+        var source = """
+                     using System.Threading.Tasks;
+                     using NServiceBus;
+
+                     public partial class Test
+                     {
+                         [NServiceBus.NServiceBusRegistrations]
+                         public partial void Configure(EndpointConfiguration cfg)
+                         {
+                             cfg.AddHandler<Handles1>();
+                             cfg.AddHandler<Handles3>();
+                             // Duplicate call, methods should be deduped with 2 InterceptsLocation attributes
+                             cfg.AddHandler<Handles3>();
+                         }
+                     }
+
+                     public class Handles1 : IHandleMessages<Cmd1>
+                     {
+                         public Task Handle(Cmd1 cmd, IMessageHandlerContext context) => Task.CompletedTask;
+                     }
+
+                     public class Handles3 : IHandleMessages<Cmd1>, IHandleMessages<Cmd2>, IHandleMessages<Evt1>
+                     {
+                         public Task Handle(Cmd1 cmd, IMessageHandlerContext context) => Task.CompletedTask;
+                         public Task Handle(Cmd2 cmd, IMessageHandlerContext context) => Task.CompletedTask;
+                         public Task Handle(Evt1 cmd, IMessageHandlerContext context) => Task.CompletedTask;
+                     }
+                     public class Cmd1 : CmdBase { }
+                     public class Cmd2 : ICommand { }
+                     public class Evt1 : IEvent { }
+                     public class CmdBase : ICommand { }
+                     """;
+
+        var partial = """
+                      public partial class Test
+                      {
+                          public partial void Configure(NServiceBus.EndpointConfiguration cfg);
+                      }
+                      """;
+
+        SourceGeneratorTest.ForIncrementalGenerator<AddHandlerInterceptor>()
+            .WithSource(source, "test.cs")
+            .WithSource(partial, "partial.cs")
+            .WithGeneratorStages("HandlerSpec", "HandlerSpecs")
+            .Approve()
+            .AssertRunsAreEqual();
+    }
+
+    [Test]
+    public void DecoratedPartialClass()
+    {
+        var source = """
+                     using System.Threading.Tasks;
+                     using NServiceBus;
+
+                     public partial class Test
+                     {
+                         public partial void Configure(EndpointConfiguration cfg)
+                         {
+                             cfg.AddHandler<Handles1>();
+                             cfg.AddHandler<Handles3>();
+                             // Duplicate call, methods should be deduped with 2 InterceptsLocation attributes
+                             cfg.AddHandler<Handles3>();
+                         }
+                     }
+
+                     public class Handles1 : IHandleMessages<Cmd1>
+                     {
+                         public Task Handle(Cmd1 cmd, IMessageHandlerContext context) => Task.CompletedTask;
+                     }
+
+                     public class Handles3 : IHandleMessages<Cmd1>, IHandleMessages<Cmd2>, IHandleMessages<Evt1>
+                     {
+                         public Task Handle(Cmd1 cmd, IMessageHandlerContext context) => Task.CompletedTask;
+                         public Task Handle(Cmd2 cmd, IMessageHandlerContext context) => Task.CompletedTask;
+                         public Task Handle(Evt1 cmd, IMessageHandlerContext context) => Task.CompletedTask;
+                     }
+                     public class Cmd1 : CmdBase { }
+                     public class Cmd2 : ICommand { }
+                     public class Evt1 : IEvent { }
+                     public class CmdBase : ICommand { }
+                     """;
+
+        var partial = """
+                      [NServiceBus.NServiceBusRegistrations]
+                      public partial class Test
+                      {
+                          public partial void Configure(NServiceBus.EndpointConfiguration cfg);
+                      }
+                      """;
+
+        SourceGeneratorTest.ForIncrementalGenerator<AddHandlerInterceptor>()
+            .WithSource(source, "test.cs")
+            .WithSource(partial, "partial.cs")
+            .WithGeneratorStages("HandlerSpec", "HandlerSpecs")
+            .Approve()
+            .AssertRunsAreEqual();
+    }
+
+    [Test]
+    public void DecoratedPartialClassOtherSide()
+    {
+        var source = """
+                     using System.Threading.Tasks;
+                     using NServiceBus;
+
+                     [NServiceBus.NServiceBusRegistrations]
+                     public partial class Test
+                     {
+                         public partial void Configure(EndpointConfiguration cfg)
+                         {
+                             cfg.AddHandler<Handles1>();
+                             cfg.AddHandler<Handles3>();
+                             // Duplicate call, methods should be deduped with 2 InterceptsLocation attributes
+                             cfg.AddHandler<Handles3>();
+                         }
+                     }
+
+                     public class Handles1 : IHandleMessages<Cmd1>
+                     {
+                         public Task Handle(Cmd1 cmd, IMessageHandlerContext context) => Task.CompletedTask;
+                     }
+
+                     public class Handles3 : IHandleMessages<Cmd1>, IHandleMessages<Cmd2>, IHandleMessages<Evt1>
+                     {
+                         public Task Handle(Cmd1 cmd, IMessageHandlerContext context) => Task.CompletedTask;
+                         public Task Handle(Cmd2 cmd, IMessageHandlerContext context) => Task.CompletedTask;
+                         public Task Handle(Evt1 cmd, IMessageHandlerContext context) => Task.CompletedTask;
+                     }
+                     public class Cmd1 : CmdBase { }
+                     public class Cmd2 : ICommand { }
+                     public class Evt1 : IEvent { }
+                     public class CmdBase : ICommand { }
+                     """;
+
+        var partial = """
+                      public partial class Test
+                      {
                           public partial void Configure(NServiceBus.EndpointConfiguration cfg);
                       }
                       """;
