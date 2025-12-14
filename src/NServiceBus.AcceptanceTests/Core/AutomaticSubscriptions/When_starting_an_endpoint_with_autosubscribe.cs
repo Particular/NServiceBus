dd586fe87e933c20f222db912e0b20f41024108e
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-12-14 19:45:52 +0100

    Fix more tests in AutomaticSubscriptions

--- src/NServiceBus.AcceptanceTests/Core/AutomaticSubscriptions/When_starting_an_endpoint_with_autosubscribe.cs
+++ src/NServiceBus.AcceptanceTests/Core/AutomaticSubscriptions/When_starting_an_endpoint_with_autosubscribe.cs
@@ -17,7 +17,6 @@ public class When_starting_an_endpoint_with_autosubscribe : NServiceBusAcceptanc
 
         var context = await Scenario.Define<Context>()
             .WithEndpoint<Subscriber>()
-            .Done(c => c.EndpointsStarted.Task.IsCompletedSuccessfully && c.EventsSubscribedTo.Count >= 1)
             .Run();
 
         Assert.That(context.EventsSubscribedTo, Has.Count.EqualTo(1));
@@ -33,18 +32,14 @@ public class When_starting_an_endpoint_with_autosubscribe : NServiceBusAcceptanc
 
     class Context : ScenarioContext
     {
-        public Context()
-        {
-            EventsSubscribedTo = [];
-        }
+        public List<string> EventsSubscribedTo { get; } = [];
 
-        public List<string> EventsSubscribedTo { get; }
+        public void MaybeComplete() => MarkAsCompleted(EventsSubscribedTo.Count >= 1);
     }
 
     class Subscriber : EndpointConfigurationBuilder
     {
-        public Subscriber()
-        {
+        public Subscriber() =>
             EndpointSetup<DefaultServer>((c, r) =>
                 {
                     c.ConfigureRouting().RouteToEndpoint(typeof(MyMessage), typeof(Subscriber)); //just route to our self for this test
@@ -52,65 +47,45 @@ public class When_starting_an_endpoint_with_autosubscribe : NServiceBusAcceptanc
 
                     // we can't check the context.Events on the SubscribeContext as events with no route are also contained. Instead we need to check which subscription messages were sent to the configured publisher.
                     c.OnEndpointSubscribed<Context>((subscription, ctx) =>
-                        ctx.EventsSubscribedTo.Add(subscription.MessageType));
+                    {
+                        ctx.EventsSubscribedTo.Add(subscription.MessageType);
+                        ctx.MaybeComplete();
+                    });
                 },
                 metadata =>
                 {
                     metadata.RegisterPublisherFor<MyEvent, Subscriber>();
                     metadata.RegisterPublisherFor<MyEventWithNoHandler, Subscriber>();
                 });
-        }
 
         class MyMessageHandler : IHandleMessages<MyMessage>
         {
-            public Task Handle(MyMessage message, IMessageHandlerContext context)
-            {
-                return Task.CompletedTask;
-            }
+            public Task Handle(MyMessage message, IMessageHandlerContext context) => Task.CompletedTask;
         }
 
         public class EventMessageHandler : IHandleMessages<MyEvent>
         {
-            public Task Handle(MyEvent message, IMessageHandlerContext context)
-            {
-                return Task.CompletedTask;
-            }
+            public Task Handle(MyEvent message, IMessageHandlerContext context) => Task.CompletedTask;
         }
 
         public class MyEventWithNoRoutingHandler : IHandleMessages<MyEventWithNoRouting>
         {
-            public Task Handle(MyEventWithNoRouting message, IMessageHandlerContext context)
-            {
-                return Task.CompletedTask;
-            }
+            public Task Handle(MyEventWithNoRouting message, IMessageHandlerContext context) => Task.CompletedTask;
         }
 
         public class CommandMessageHandler : IHandleMessages<MyCommand>
         {
-            public Task Handle(MyCommand message, IMessageHandlerContext context)
-            {
-                return Task.CompletedTask;
-            }
+            public Task Handle(MyCommand message, IMessageHandlerContext context) => Task.CompletedTask;
         }
     }
 
-    public class MyMessage : IMessage
-    {
-    }
+    public class MyMessage : IMessage;
 
-    public class MyCommand : ICommand
-    {
-    }
+    public class MyCommand : ICommand;
 
-    public class MyEvent : IEvent
-    {
-    }
+    public class MyEvent : IEvent;
 
-    public class MyEventWithNoRouting : IEvent
-    {
-    }
+    public class MyEventWithNoRouting : IEvent;
 
-    public class MyEventWithNoHandler : IEvent
-    {
-    }
+    public class MyEventWithNoHandler : IEvent;
 }
