9defdef25c9a9a887ccf9eac62adac126ee4b149
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-12-16 13:43:00 +0100

    Show combination (attention the params are not deduped)

--- src/NServiceBus.AcceptanceTests.WithAnalyzers/Handlers/When_using_interface_less_handlers.cs
+++ src/NServiceBus.AcceptanceTests.WithAnalyzers/Handlers/When_using_interface_less_handlers.cs
@@ -1,5 +1,6 @@
 namespace NServiceBus.AcceptanceTests.Handlers;
 
+using System;
 using System.Threading.Tasks;
 using EndpointTemplates;
 using NServiceBus;
@@ -14,17 +15,19 @@ public class When_using_interface_less_handlers : NServiceBusAcceptanceTest
         var context = await Scenario.Define<Context>()
             .WithEndpoint<EndpointUsingInterfaceLessHandlers>(b =>
                 b.When(async (session, _) => await session.SendLocal(new ComplexMessage())))
-            .Done(c => c is { WithCtorDependencyReceived: true, WithMethodDependencyReceived: true })
+            .Done(c => c is { WithCtorDependencyReceived: true, WithMethodDependencyReceived: true, WithCtorAndMethodDependencyReceived: true })
             .Run();
 
         Assert.That(context.WithCtorDependencyReceived, Is.True);
         Assert.That(context.WithMethodDependencyReceived, Is.True);
+        Assert.That(context.WithCtorAndMethodDependencyReceived, Is.True);
     }
 
     public class Context : ScenarioContext
     {
         public bool WithCtorDependencyReceived;
         public bool WithMethodDependencyReceived;
+        public bool WithCtorAndMethodDependencyReceived;
     }
 
     public class EndpointUsingInterfaceLessHandlers : EndpointConfigurationBuilder
@@ -50,6 +53,20 @@ public class When_using_interface_less_handlers : NServiceBusAcceptanceTest
             public static Task Handle(ComplexMessage message, IMessageHandlerContext context, Context testContext)
             {
                 testContext.WithMethodDependencyReceived = true;
+                return Task.CompletedTask;
+            }
+        }
+
+        [Handler("EndpointUsingInterfaceLessHandlers")]
+#pragma warning disable CS9113 // Parameter is unread.
+        public class InterfaceLessHandlerWithCtorAndMethodDependency(IServiceProvider provider)
+#pragma warning restore CS9113 // Parameter is unread.
+        {
+#pragma warning disable CA1822
+            public Task Handle(ComplexMessage message, IMessageHandlerContext context, Context testContext)
+#pragma warning restore CA1822
+            {
+                testContext.WithCtorAndMethodDependencyReceived = true;
                 return Task.CompletedTask;
             }
         }
