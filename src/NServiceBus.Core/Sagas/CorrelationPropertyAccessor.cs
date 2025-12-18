cf2d4213204d033793cc6e1a1f300ca623d54467
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-12-18 13:40:40 +0100

    Documentation

--- src/NServiceBus.Core/Sagas/CorrelationPropertyAccessor.cs
+++ src/NServiceBus.Core/Sagas/CorrelationPropertyAccessor.cs
@@ -8,16 +8,16 @@ namespace NServiceBus;
 public abstract class CorrelationPropertyAccessor
 {
     /// <summary>
-    ///
+    /// Writes the value to the saga data's correlation property.
     /// </summary>
-    /// <param name="sagaData"></param>
-    /// <param name="value"></param>
+    /// <param name="sagaData">The saga data to write correlation property value to.</param>
+    /// <param name="value">The correlation property value to be written.</param>
     public abstract void WriteTo(IContainSagaData sagaData, object value);
 
     /// <summary>
-    ///
+    /// Accesses the value from the saga data.
     /// </summary>
-    /// <param name="sagaData"></param>
-    /// <returns></returns>
+    /// <param name="sagaData">The saga data to access the correlation property value from.</param>
+    /// <returns>The value of the saga data correlation property or null.</returns>
     public abstract object? AccessFrom(IContainSagaData sagaData);
 }
