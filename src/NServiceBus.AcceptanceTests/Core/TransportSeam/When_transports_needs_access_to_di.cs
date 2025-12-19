e2998d4c7f34674625b2351c01548949b77d9fc0
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-12-19 15:52:37 +0100

    Improve service collection and dependency injection access

---
+++ src/NServiceBus.AcceptanceTests/Core/TransportSeam/When_transports_needs_access_to_di.cs
@@ -0,0 +1,51 @@
+﻿namespace NServiceBus.AcceptanceTests.Core.TransportSeam;
+
+using System.Threading;
+using System.Threading.Tasks;
+using AcceptanceTesting;
+using EndpointTemplates;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Transport;
+
+public class When_transport_needs_access_to_di : NServiceBusAcceptanceTest
+{
+    [Test]
+    public async Task Should_be_able_to_resolve_dependencies()
+    {
+        var context = await Scenario.Define<Context>()
+            .WithEndpoint<EndpointWithFeature>()
+            .Done(c => c.EndpointsStarted)
+            .Run();
+
+        Assert.That(context.TransportDependencyResolved, Is.True);
+    }
+
+    class Context : ScenarioContext
+    {
+        public bool TransportDependencyResolved { get; set; }
+    }
+
+    class EndpointWithFeature : EndpointConfigurationBuilder
+    {
+        public EndpointWithFeature() => EndpointSetup<DefaultServer>(c => c.UseTransport(new CustomAcceptanceTestingTransport()));
+    }
+
+    class CustomAcceptanceTestingTransport : AcceptanceTestingTransport
+    {
+        protected override void ConfigureServicesCore(IServiceCollection services) => services.AddSingleton<TransportService>();
+
+        public override Task<TransportInfrastructure> Initialize(HostSettings hostSettings, ReceiveSettings[] receivers, string[] sendingAddresses, CancellationToken cancellationToken = default)
+        {
+            if (hostSettings.SupportsDependencyInjection)
+            {
+                var serviceProvider = hostSettings.ServiceProvider;
+                var context = serviceProvider.GetRequiredService<Context>();
+                context.TransportDependencyResolved = serviceProvider.GetService<TransportService>() is not null;
+            }
+            return base.Initialize(hostSettings, receivers, sendingAddresses, cancellationToken);
+        }
+
+        class TransportService;
+    }
+}
--- src/NServiceBus.Core/Transports/HostSettings.cs
+++ src/NServiceBus.Core/Transports/HostSettings.cs
@@ -3,6 +3,7 @@
 namespace NServiceBus.Transport;
 
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using Settings;
 
@@ -15,6 +16,25 @@ using Settings;
 public class HostSettings(string name, string hostDisplayName, StartupDiagnosticEntries startupDiagnostic, Action<string, Exception, CancellationToken> criticalErrorAction, bool setupInfrastructure, IReadOnlySettings? coreSettings = null)
 {
     /// <summary>
+    /// Indicates whether the transport is running in an NServiceBus endpoint.
+    /// </summary>
+    /// <remarks>
+    /// This property returns <c>true</c> if the transport is running in an NServiceBus endpoint; otherwise, <c>false</c> indicating the transports is running in the raw mode.</remarks>
+    [MemberNotNullWhen(true, nameof(CoreSettings), nameof(ServiceProvider))]
+    public bool IsHosted => CoreSettings is not null && ServiceProvider is not null;
+
+    /// <summary>
+    /// Indicates whether the transport supports dependency injection.
+    /// </summary>
+    /// <remarks>
+    /// This property returns <c>true</c> if the associated service provider is initialized; otherwise, <c>false</c>. When the endpoint is running in an NServiceBus endpoint, this property will always return <c>true</c>.
+    /// In raw mode, this property will return <c>false</c> unless the hosting infrastructure has initialized the service provider and assigned it to the <see cref="ServiceProvider"/> property before the transport definition is initialized.
+    /// Transport implementations can use this property to determine whether they can resolve dependencies from the service provider during initialization.
+    /// </remarks>
+    [MemberNotNullWhen(true, nameof(ServiceProvider))]
+    public bool SupportsDependencyInjection => ServiceProvider is not null;
+
+    /// <summary>
     /// Settings available only when running hosted in an NServiceBus endpoint; Otherwise, <c>null</c>.
     /// Transports can use these settings to validate the hosting endpoint settings.
     /// </summary>
@@ -24,7 +44,7 @@ public class HostSettings(string name, string hostDisplayName, StartupDiagnostic
     /// Service provider available only when running hosted in an NServiceBus endpoint; Otherwise, <c>null</c>.
     /// Transports can use the provider in hosted scenarios to resolve dependencies from the provider.
     /// </summary>
-    public IServiceProvider? ServiceProvider { get; internal set; }
+    public IServiceProvider? ServiceProvider { get; set; }
 
     /// <summary>
     /// A name that describes the host (e.g. the endpoint name).
