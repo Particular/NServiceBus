5526068556de72c7b8476fb8179588f2fc699904
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-01-06 12:15:50 +0100

    Improve documentation

--- src/NServiceBus.Core/OpenTelemetry/OpenTelemetryConfigurationExtensions.cs
+++ src/NServiceBus.Core/OpenTelemetry/OpenTelemetryConfigurationExtensions.cs
@@ -10,8 +10,20 @@ using System;
 public static partial class OpenTelemetryConfigurationExtensions
 {
     /// <summary>
-    /// Disables the OpenTelemetry instrumentation in NServiceBus.
+    /// Prevents NServiceBus from creating OpenTelemetry traces for messages.
+    /// This opts out of activity creation; it does not change whether an `ActivitySource` has listeners.
     /// </summary>
+    /// <remarks>
+    /// By default, NServiceBus endpoints generate OpenTelemetry traces for incoming and outgoing messages when the `NServiceBus.Core`
+    /// activity source is configured. Disabling here stops creating Activity instances regardless of whether the `NServiceBus.Core` activity source
+    /// is configured in the OpenTelemetry tracer. To capture traces, add the `NServiceBus.Core` activity source:
+    /// <code>
+    /// var tracingProviderBuilder = Sdk.CreateTracerProviderBuilder()
+    ///     .AddSource("NServiceBus.Core")
+    ///     // ... Add other trace sources and exporters
+    ///     .Build();
+    /// </code>
+    /// </remarks>
     public static void DisableOpenTelemetry(this EndpointConfiguration endpointConfiguration)
     {
         ArgumentNullException.ThrowIfNull(endpointConfiguration);
