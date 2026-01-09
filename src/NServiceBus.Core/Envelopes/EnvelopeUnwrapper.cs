5e0aded59b191d3d36e372522a2fb7a4cfa7b8c1
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-01-09 10:48:54 +0100

    Pooling only needs to happen when an unwrapper actually does something

--- src/NServiceBus.Core/Envelopes/EnvelopeUnwrapper.cs
+++ src/NServiceBus.Core/Envelopes/EnvelopeUnwrapper.cs
@@ -12,14 +12,11 @@ class EnvelopeUnwrapper(IEnvelopeHandler[] envelopeHandlers, IncomingPipelineMet
 
     internal IncomingMessageHandle UnwrapEnvelope(MessageContext messageContext)
     {
-        ArrayPoolBufferWriter<byte>? bufferWriter = null;
-
+        var bufferWriter = new LazyArrayPoolBufferWriter<byte>(messageContext.Body.Length);
         foreach (var envelopeHandler in envelopeHandlers)
         {
             try
             {
-                bufferWriter ??= new ArrayPoolBufferWriter<byte>();
-
                 if (Log.IsDebugEnabled)
                 {
                     Log.Debug(
@@ -68,14 +65,14 @@ class EnvelopeUnwrapper(IEnvelopeHandler[] envelopeHandlers, IncomingPipelineMet
             Log.Debug(envelopeHandlers.Length > 0 ? $"No envelope handler unwrapped the current message (NativeID: {messageContext.NativeMessageId}, assuming the default NServiceBus format" : $"No envelope handler found for the current message (NativeID: {messageContext.NativeMessageId}, assuming the default NServiceBus format");
         }
 
-        return new IncomingMessageHandle(GetDefaultIncomingMessage(messageContext));
+        return new IncomingMessageHandle(GetDefaultIncomingMessage(messageContext), bufferWriter);
     }
 
-    internal readonly struct IncomingMessageHandle(IncomingMessage message, IDisposable? disposable = null) : IDisposable
+    internal readonly struct IncomingMessageHandle(IncomingMessage message, IDisposable disposable) : IDisposable
     {
         public IncomingMessage Message { get; } = message;
 
-        public void Dispose() => disposable?.Dispose();
+        public void Dispose() => disposable.Dispose();
 
         public static implicit operator IncomingMessage(IncomingMessageHandle handle) => handle.Message;
     }
