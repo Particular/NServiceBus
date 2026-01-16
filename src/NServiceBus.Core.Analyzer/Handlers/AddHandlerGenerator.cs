3d53dc6f65385ad309335b697c77defacc2639df
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-01-16 15:09:10 +0100

    Brute forced emitter and parser

---
+++ src/NServiceBus.Core.Analyzer/Handlers/AddHandlerGenerator.cs
@@ -0,0 +1,61 @@
+#nullable enable
+
+namespace NServiceBus.Core.Analyzer.Handlers;
+
+using Microsoft.CodeAnalysis;
+
+[Generator(LanguageNames.CSharp)]
+public sealed partial class AddHandlerGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var addHandlers = context.SyntaxProvider
+            .ForAttributeWithMetadataName("NServiceBus.HandlerAttribute",
+                predicate: static (node, _) => true, // TODO sensible check
+                transform: Parser.Parse)
+            .Where(static spec => spec is not null)
+            .Select(static (spec, _) => spec!)
+            .WithTrackingName("HandlerSpec");
+
+        var collected = addHandlers.Collect()
+            .Select((handlers, _) => new HandlerSpecs(handlers.ToImmutableEquatableArray()))
+            .WithTrackingName("HandlerSpecs");
+
+        context.RegisterSourceOutput(collected,
+            static (productionContext, spec) =>
+            {
+                var emitter = new Emitter(productionContext);
+                emitter.Emit(spec);
+            });
+    }
+
+    internal sealed record HandlerSpec(
+        string Name,
+        string HandlerType,
+        string HandlerNamespace,
+        string AssemblyName,
+        ImmutableEquatableArray<RegistrationSpec> Registrations,
+        ImmutableEquatableArray<InterfaceLessHandlerSpec> InterfaceLessHandlers);
+
+    internal readonly record struct HandlerSpecs(ImmutableEquatableArray<HandlerSpec> Handlers);
+
+    internal enum RegistrationType
+    {
+        MessageHandler,
+        StartMessageHandler,
+        TimeoutHandler,
+    }
+
+    internal readonly record struct RegistrationSpec(RegistrationType RegistrationType, string MessageType, ImmutableEquatableArray<string> MessageHierarchy, string HandlerType);
+
+    internal sealed record InterfaceLessHandlerSpec(
+        bool IsStatic,
+        string GeneratedHandlerName,
+        string GeneratedHandlerType,
+        string DeclaringHandlerType,
+        string MessageType,
+        ImmutableEquatableArray<string> MessageHierarchy,
+        ImmutableEquatableArray<ParameterSpec> ConstructorParameters);
+
+    internal readonly record struct ParameterSpec(string Type, string Name, string FieldName);
+}
