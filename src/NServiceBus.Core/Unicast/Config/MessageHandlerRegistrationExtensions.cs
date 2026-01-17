a713407412b282b12576da8d56f77e630150c936
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-01-17 14:46:51 +0100

    Show additional approach

--- src/NServiceBus.Core/Unicast/Config/MessageHandlerRegistrationExtensions.cs
+++ src/NServiceBus.Core/Unicast/Config/MessageHandlerRegistrationExtensions.cs
@@ -3,7 +3,6 @@ namespace NServiceBus;
 
 using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
 using Unicast;
 
 /// <summary>
@@ -14,16 +13,10 @@ public static class MessageHandlerRegistrationExtensions
     /// <summary>
     /// Registers a message handler.
     /// </summary>
-    [UnconditionalSuppressMessage("Trimming", "IL2026", Justification = "When trimming, this method will either be intercepted or throw an exception.")]
-    [UnconditionalSuppressMessage("Trimming", "IL2091", Justification = "When trimming, this method will either be intercepted or throw an exception.")]
+    [RequiresUnreferencedCode("Uses reflection to inspect handler types.")]
     public static void AddHandler<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THandler>(this EndpointConfiguration config) where THandler : IHandleMessages
     {
         ArgumentNullException.ThrowIfNull(config);
-
-        if (!RuntimeFeature.IsDynamicCodeSupported)
-        {
-            throw new InvalidOperationException("This call requires a source generator. Add the [NServiceBusRegistrations] attribute to the calling method or class to enable the generator.");
-        }
 
         var messageHandlerRegistry = config.Settings.GetOrCreate<MessageHandlerRegistry>();
         messageHandlerRegistry.AddHandler<THandler>();
