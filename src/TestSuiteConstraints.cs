9ced90b18417d05ef6e9137e98fa6abeb02d5ea3
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-01-20 12:21:51 +0100

    WithAnalyzer tests

---
+++ src/TestSuiteConstraints.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus.AcceptanceTests;
+
+using AcceptanceTesting.Support;
+
+public partial class TestSuiteConstraints
+{
+    public bool SupportsDtc => false;
+
+    public bool SupportsCrossQueueTransactions => true;
+
+    public bool SupportsNativePubSub => true;
+
+    public bool SupportsDelayedDelivery => true;
+
+    public bool SupportsOutbox => false;
+
+    public bool SupportsPurgeOnStartup => true;
+
+    public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointAcceptanceTestingTransport(SupportsNativePubSub, SupportsDelayedDelivery, enforcePublisherMetadata: false);
+
+    public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointAcceptanceTestingPersistence();
+}
