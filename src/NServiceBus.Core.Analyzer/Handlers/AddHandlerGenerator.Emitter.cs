b15b300261050179e2fcede6e719925bf55f66b7
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-01-20 20:48:09 +0100

    Xml documentation please less fancy name replacement

--- src/NServiceBus.Core.Analyzer/Handlers/AddHandlerGenerator.Emitter.cs
+++ src/NServiceBus.Core.Analyzer/Handlers/AddHandlerGenerator.Emitter.cs
@@ -3,6 +3,7 @@ namespace NServiceBus.Core.Analyzer.Handlers;
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Utility;
@@ -31,21 +32,28 @@ public sealed partial class AddHandlerGenerator
             EmitHandlers(sourceWriter, handlers);
             sourceWriter.CloseCurlies();
 
-            context.AddSource("Handlers.g.cs", sourceWriter.ToSourceText());
+            context.AddSource("HandlerRegistrations.g.cs", sourceWriter.ToSourceText());
         }
 
         static void EmitHandlers(SourceWriter sourceWriter, ImmutableEquatableArray<HandlerSpec> handlers)
         {
+            Debug.Assert(handlers.Count > 0);
+
             var root = BuildNamespaceTree(handlers);
-            var assemblyId = GetAssemblyIdentifier(handlers);
+            var assemblyName = handlers[0].AssemblyName;
+            var assemblyId = SanitizeIdentifier(assemblyName);
+
             var rootRegistryName = $"{assemblyId}RootRegistry";
 
+            sourceWriter.WriteLine("/// <summary>");
+            sourceWriter.WriteLine($"/// Provides extensions to register message handlers found in the <i>{assemblyName}</i> assembly.");
+            sourceWriter.WriteLine("/// </summary>");
             sourceWriter.WithGeneratedCodeAttribute();
             sourceWriter.WriteLine($"public static class {assemblyId}HandlerRegistryExtensions");
             sourceWriter.WriteLine("{");
             sourceWriter.Indentation++;
 
-            EmitExtensionProperties(sourceWriter, assemblyId, rootRegistryName);
+            EmitExtensionProperties(sourceWriter, assemblyId, assemblyName, rootRegistryName);
 
             sourceWriter.WriteLine();
             EmitNamespaceRegistry(sourceWriter, root, rootRegistryName);
@@ -54,12 +62,15 @@ public sealed partial class AddHandlerGenerator
             sourceWriter.WriteLine("}");
         }
 
-        static void EmitExtensionProperties(SourceWriter sourceWriter, string assemblyId, string rootRegistryName)
+        static void EmitExtensionProperties(SourceWriter sourceWriter, string assemblyId, string assemblyName, string rootRegistryName)
         {
             sourceWriter.WriteLine("extension (global::NServiceBus.HandlerRegistry registry)");
             sourceWriter.WriteLine("{");
             sourceWriter.Indentation++;
 
+            sourceWriter.WriteLine("/// <summary>");
+            sourceWriter.WriteLine($"/// Gets the handler registry for the <i>{assemblyName}</i> assembly.");
+            sourceWriter.WriteLine("/// </summary>");
             sourceWriter.WriteLine($"public {rootRegistryName} {assemblyId}Assembly => new(registry.Configuration);");
 
             sourceWriter.Indentation--;
@@ -68,6 +79,9 @@ public sealed partial class AddHandlerGenerator
 
         static void EmitNamespaceRegistry(SourceWriter sourceWriter, NamespaceNode node, string registryName)
         {
+            sourceWriter.WriteLine("/// <summary>");
+            sourceWriter.WriteLine($"/// The handler registry for the <i>{node.Name ?? registryName.Replace("RootRegistry", string.Empty)}</i> part.");
+            sourceWriter.WriteLine("/// </summary>");
             sourceWriter.WriteLine($"public sealed class {registryName}(global::NServiceBus.EndpointConfiguration configuration)");
             sourceWriter.WriteLine("{");
             sourceWriter.Indentation++;
@@ -81,10 +95,24 @@ public sealed partial class AddHandlerGenerator
 
             foreach (var child in node.Children)
             {
+                sourceWriter.WriteLine("/// <summary>");
+                sourceWriter.WriteLine($"/// Gets the handler registry for the <i>{child.Name}</i> namespace part.");
+                sourceWriter.WriteLine("/// </summary>");
                 sourceWriter.WriteLine($"public {child.RegistryName} {child.Name} => new(_configuration);");
             }
 
             sourceWriter.WriteLine();
+            sourceWriter.WriteLine("/// <summary>");
+            if (node.Children.Count > 0)
+            {
+                sourceWriter.WriteLine($"/// Adds handler registrations from the {string.Join(", ", node.Children.Select(c => $"<i>{c.Name}</i>"))} parts including all the subnamespaces.");
+            }
+
+            if (node.Handlers.Count > 0)
+            {
+                sourceWriter.WriteLine($"/// Adds {string.Join(", ", node.Handlers.Select(c => $"<see cref=\"{c.HandlerType}\"/>"))} handlers from <i>{node.Name}<i/> namespace.");
+            }
+            sourceWriter.WriteLine("/// </summary>");
             sourceWriter.WriteLine("public void AddAll()");
             sourceWriter.WriteLine("{");
             sourceWriter.Indentation++;
@@ -130,6 +158,9 @@ public sealed partial class AddHandlerGenerator
             {
                 var handlerSpec = handlerSpecs[index];
                 var methodName = GetSingleHandlerMethodName(handlerSpec.Name);
+                sourceWriter.WriteLine("/// <summary>");
+                sourceWriter.WriteLine($"""/// Adds the <see cref="{handlerSpec.HandlerType}"/> handler to the registration.""");
+                sourceWriter.WriteLine("/// </summary>");
                 sourceWriter.WriteLine($"public void {methodName}()");
                 sourceWriter.WriteLine("{");
                 sourceWriter.Indentation++;
@@ -164,9 +195,9 @@ public sealed partial class AddHandlerGenerator
         {
             const string HandlerSuffix = "Handler";
 
-            if (handlerName.EndsWith(HandlerSuffix, StringComparison.Ordinal))
+            if (!handlerName.EndsWith(HandlerSuffix, StringComparison.Ordinal))
             {
-                handlerName = handlerName[..^HandlerSuffix.Length];
+                handlerName += HandlerSuffix;
             }
 
             return $"Add{handlerName}";
@@ -219,13 +250,6 @@ public sealed partial class AddHandlerGenerator
                 var hierarchyLiteral = $"[{string.Join(", ", registration.MessageHierarchy.Select(type => $"typeof({type})"))}]";
                 sourceWriter.WriteLine($"messageMetadataRegistry.RegisterMessageTypeWithHierarchy(typeof({registration.MessageType}), {hierarchyLiteral});");
             }
-        }
-
-        static string GetAssemblyIdentifier(ImmutableEquatableArray<HandlerSpec> handlers)
-        {
-            var assemblyName = handlers.Count > 0 ? handlers[0].AssemblyName : "Assembly";
-            var sanitizedAssemblyName = SanitizeIdentifier(assemblyName);
-            return sanitizedAssemblyName;
         }
 
         static string SanitizeIdentifier(string value)
