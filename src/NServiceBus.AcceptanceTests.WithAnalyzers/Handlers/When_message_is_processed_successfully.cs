d1255980e5c566bf47bc2a74cbebca11dade3872
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-01-20 12:21:51 +0100

    WithAnalyzer tests

---
+++ src/NServiceBus.AcceptanceTests.WithAnalyzers/Handlers/When_message_is_processed_successfully.cs
@@ -0,0 +1,51 @@
+namespace NServiceBus.AcceptanceTests.Handlers;
+
+using System.Threading.Tasks;
+using EndpointTemplates;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting;
+using NUnit.Framework;
+
+public class When_manually_registering_handler_with_complex_hierarchy : NServiceBusAcceptanceTest
+{
+    [Test]
+    public async Task Should_handle_message_with_manually_registered_handler()
+    {
+        var context = await Scenario.Define<Context>()
+            .WithEndpoint<EndpointWithComplexMessageHierarchy>(b =>
+                b.When(async (session, _) => await session.SendLocal(new ComplexMessage())))
+            .Run();
+
+        Assert.That(context.ComplexMessageReceived, Is.True);
+    }
+
+    public class Context : ScenarioContext
+    {
+        public bool ComplexMessageReceived;
+    }
+
+    public class EndpointWithComplexMessageHierarchy : EndpointConfigurationBuilder
+    {
+        public EndpointWithComplexMessageHierarchy() => EndpointSetup<NonScanningServer>(config =>
+        {
+            config.Handlers.NServiceBusAcceptanceTestsWithAnalyzersAssembly.AcceptanceTests.Handlers.AddComplexMessage();
+        });
+
+        [Handler]
+        public class ComplexMessageHandler(Context testContext) : IHandleMessages<ComplexMessage>
+        {
+            public Task Handle(ComplexMessage message, IMessageHandlerContext context)
+            {
+                testContext.ComplexMessageReceived = true;
+                testContext.MarkAsCompleted();
+                return Task.CompletedTask;
+            }
+        }
+    }
+
+    public class ComplexMessage : ConcreteParent1, IInterfaceParent1;
+    public class ConcreteParent1 : ConcreteParentBase;
+    public class ConcreteParentBase : IMessage;
+    public interface IInterfaceParent1 : IInterfaceParent1Base;
+    public interface IInterfaceParent1Base : IMessage;
+}
