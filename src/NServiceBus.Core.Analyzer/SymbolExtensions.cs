cc567f438b4d6b434b166ddfbde5dfc15f208abe
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-01-22 20:08:09 +0100

    Remove redundancy

--- src/NServiceBus.Core.Analyzer/SymbolExtensions.cs
+++ src/NServiceBus.Core.Analyzer/SymbolExtensions.cs
@@ -1,63 +1,62 @@
-﻿namespace NServiceBus.Core.Analyzer
+﻿namespace NServiceBus.Core.Analyzer;
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+static class SymbolExtensions
 {
-    using System.Collections.Generic;
-    using System.Collections.Immutable;
-    using System.Linq;
-    using System.Threading;
-    using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.CSharp;
-
-    static class SymbolExtensions
+    extension(ISymbol symbol)
     {
-        extension(ISymbol symbol)
+        public ITypeSymbol GetTypeSymbolOrDefault() => symbol switch
         {
-            public ITypeSymbol GetTypeSymbolOrDefault() => symbol switch
-            {
-                IDiscardSymbol symbolWithType => symbolWithType.Type,
-                IEventSymbol symbolWithType => symbolWithType.Type,
-                IFieldSymbol symbolWithType => symbolWithType.Type,
-                ILocalSymbol symbolWithType => symbolWithType.Type,
-                IMethodSymbol symbolWithType => symbolWithType.ReturnType,
-                INamedTypeSymbol symbolWithType => symbolWithType,
-                IParameterSymbol symbolWithType => symbolWithType.Type,
-                IPointerTypeSymbol symbolWithType => symbolWithType.PointedAtType,
-                IPropertySymbol symbolWithType => symbolWithType.Type,
-                ITypeSymbol symbolWithType => symbolWithType,
-                _ => null,
-            };
+            IDiscardSymbol symbolWithType => symbolWithType.Type,
+            IEventSymbol symbolWithType => symbolWithType.Type,
+            IFieldSymbol symbolWithType => symbolWithType.Type,
+            ILocalSymbol symbolWithType => symbolWithType.Type,
+            IMethodSymbol symbolWithType => symbolWithType.ReturnType,
+            INamedTypeSymbol symbolWithType => symbolWithType,
+            IParameterSymbol symbolWithType => symbolWithType.Type,
+            IPointerTypeSymbol symbolWithType => symbolWithType.PointedAtType,
+            IPropertySymbol symbolWithType => symbolWithType.Type,
+            ITypeSymbol symbolWithType => symbolWithType,
+            _ => null,
+        };
 
-            public IEnumerable<TNode> GetDescendantsAcrossDeclarations<TNode>(CancellationToken cancellationToken = default)
-                where TNode : CSharpSyntaxNode
+        public IEnumerable<TNode> GetDescendantsAcrossDeclarations<TNode>(CancellationToken cancellationToken = default)
+            where TNode : CSharpSyntaxNode
+        {
+            foreach (var syntaxRef in GetAllDeclaringSyntaxReferences(symbol))
             {
-                foreach (var syntaxRef in GetAllDeclaringSyntaxReferences(symbol))
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var declarationNode = syntaxRef.GetSyntax(cancellationToken);
+                foreach (var node in declarationNode.DescendantNodes().OfType<TNode>())
                 {
-                    cancellationToken.ThrowIfCancellationRequested();
-
-                    var declarationNode = syntaxRef.GetSyntax(cancellationToken);
-                    foreach (var node in declarationNode.DescendantNodes().OfType<TNode>())
-                    {
-                        yield return node;
-                    }
+                    yield return node;
                 }
             }
         }
+    }
 
-        static ImmutableArray<SyntaxReference> GetAllDeclaringSyntaxReferences(ISymbol symbol)
+    static ImmutableArray<SyntaxReference> GetAllDeclaringSyntaxReferences(ISymbol symbol)
+    {
+        return symbol switch
         {
-            return symbol switch
-            {
-                ITypeSymbol type => type.DeclaringSyntaxReferences,
-                IMethodSymbol method => MergePartial(method),
-                _ => symbol.DeclaringSyntaxReferences,
-            };
+            ITypeSymbol type => type.DeclaringSyntaxReferences,
+            IMethodSymbol method => MergePartial(method),
+            _ => symbol.DeclaringSyntaxReferences,
+        };
 
-            static ImmutableArray<SyntaxReference> MergePartial(IMethodSymbol method)
-            {
-                var def = method.PartialDefinitionPart?.DeclaringSyntaxReferences ?? [];
-                var impl = method.PartialImplementationPart?.DeclaringSyntaxReferences ?? [];
-                var self = method.DeclaringSyntaxReferences;
-                return [.. ((SyntaxReference[])[.. def, .. impl, .. self]).Distinct()];
-            }
+        static ImmutableArray<SyntaxReference> MergePartial(IMethodSymbol method)
+        {
+            var def = method.PartialDefinitionPart?.DeclaringSyntaxReferences ?? [];
+            var impl = method.PartialImplementationPart?.DeclaringSyntaxReferences ?? [];
+            var self = method.DeclaringSyntaxReferences;
+            return [.. ((SyntaxReference[])[.. def, .. impl, .. self]).Distinct()];
         }
     }
 }
