b7e88df8782e96590ae31924249bdbd22c240990
Andreas Öhlund <andreas.ohlund@particular.net>
2026-01-27 12:55:24 +0100

    use UnconditionalSuppressMessage

--- src/NServiceBus.Core/EndpointCreator.cs
+++ src/NServiceBus.Core/EndpointCreator.cs
@@ -69,7 +69,10 @@ class EndpointCreator
     {
         var receiveSettings = settings.Get<ReceiveComponent.Settings>();
 
-        receiveSettings.MessageHandlerRegistry.AddScannedHandlers(hostingConfiguration.AvailableTypes);
+        if (System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeSupported)
+        {
+            receiveSettings.MessageHandlerRegistry.AddScannedHandlers(hostingConfiguration.AvailableTypes);
+        }
 
         ConfigureMessageTypes(receiveSettings.MessageHandlerRegistry.GetMessageTypes());
 
@@ -173,10 +176,7 @@ class EndpointCreator
 
     public StartableEndpoint CreateStartableEndpoint(IServiceProvider serviceProvider, bool serviceProviderIsExternallyManaged)
     {
-        hostingConfiguration.AddStartupDiagnosticsSection("Container", new
-        {
-            Type = serviceProviderIsExternallyManaged ? "external" : "internal"
-        });
+        hostingConfiguration.AddStartupDiagnosticsSection("Container", new { Type = serviceProviderIsExternallyManaged ? "external" : "internal" });
 
         return new StartableEndpoint(settings,
             featureComponent,
