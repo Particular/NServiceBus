b39eab2bf29ca8e974df819e1ece38cc350bf27a
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-02-02 17:51:11 +0100

    CreateFactory

--- src/NServiceBus.Core/Unicast/MessageHandlerRegistry.cs
+++ src/NServiceBus.Core/Unicast/MessageHandlerRegistry.cs
@@ -311,7 +311,7 @@ public class MessageHandlerRegistry
         public Type MessageType { get; } = messageType ?? throw new ArgumentNullException(nameof(messageType));
 
         public MessageHandler Create() =>
-            new ReflectionMessageHandlerInvoker((provider, _) => ActivatorUtilities.CreateInstance(provider, handlerType), invoker, isTimeoutHandler)
+            new ReflectionMessageHandlerInvoker(ActivatorUtilities.CreateFactory(handlerType, []), invoker, isTimeoutHandler)
             {
                 HandlerType = handlerType
             };
