6e991881bbf09d1ca63bd7e30fa731a1e23380a3
Andreas Öhlund <andreas.ohlund@particular.net>
2026-02-04 12:56:11 +0100

    Refactor DynamicMemberTypeAccess

--- src/NServiceBus.Core/DynamicMemberTypeAccess.cs
+++ src/NServiceBus.Core/DynamicMemberTypeAccess.cs
@@ -4,25 +4,28 @@ using System.Diagnostics.CodeAnalysis;
 
 static class DynamicMemberTypeAccess
 {
-    public const DynamicallyAccessedMemberTypes Handler =
-        DynamicallyAccessedMemberTypes.PublicMethods
-        | DynamicallyAccessedMemberTypes.Interfaces
-        | DynamicallyAccessedMemberTypes.PublicConstructors;
+    public const DynamicallyAccessedMemberTypes Handler = Invokable;
 
     public const DynamicallyAccessedMemberTypes Saga = Handler
-                                                       | DynamicallyAccessedMemberTypes.NonPublicConstructors // Needed since we use RuntimeHelpers.GetUninitializedObject
-                                                       | DynamicallyAccessedMemberTypes.NonPublicMethods; // we need this since we are calling the protected ConfigureHowToFindSaga
+                                                       | DynamicallyAccessedMemberTypes.NonPublicConstructors // Needed since we use RuntimeHelpers.GetUninitializedObject when performing saga mapping
+                                                       | DynamicallyAccessedMemberTypes.NonPublicMethods; // Needed since we are calling the protected ConfigureHowToFindSaga
 
     public const DynamicallyAccessedMemberTypes SagaData = DynamicallyAccessedMemberTypes.PublicConstructors
                                                            | DynamicallyAccessedMemberTypes.PublicProperties;
 
-    public const DynamicallyAccessedMemberTypes SagaFinder = Handler;
+    public const DynamicallyAccessedMemberTypes SagaFinder = Invokable;
 
-    public const DynamicallyAccessedMemberTypes SagaNotFoundHandler = Handler;
+    public const DynamicallyAccessedMemberTypes SagaNotFoundHandler = Invokable;
 
-    public const DynamicallyAccessedMemberTypes Installer = Handler;
+    public const DynamicallyAccessedMemberTypes Installer = Invokable;
 
-    public const DynamicallyAccessedMemberTypes Feature = Handler
-                                                          | DynamicallyAccessedMemberTypes.NonPublicConstructors;
-    public const DynamicallyAccessedMemberTypes FeatureStartupTask = Handler;
+    public const DynamicallyAccessedMemberTypes Feature = Invokable
+                                                          | DynamicallyAccessedMemberTypes.NonPublicConstructors; //TODO: Why was this needed?
+
+    public const DynamicallyAccessedMemberTypes FeatureStartupTask = Invokable;
+
+    const DynamicallyAccessedMemberTypes Invokable =
+        DynamicallyAccessedMemberTypes.PublicMethods
+        | DynamicallyAccessedMemberTypes.Interfaces
+        | DynamicallyAccessedMemberTypes.PublicConstructors;
 }
