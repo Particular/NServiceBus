2193bd5cc6afb92602a98887d111f56a760b1473
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-03-12 16:13:27 +0100

    Remove now unnecessary suppressor

--- src/NServiceBus.Core.Analyzer/SupressionIds.cs
+++ src/NServiceBus.Core.Analyzer/SupressionIds.cs
@@ -4,5 +4,4 @@ public static class SupressionIds
 {
     public const string AddHandlerInterceptorSuppression = "NSBS0001";
     public const string AddSagaInterceptorSuppression = "NSBS0002";
-    public const string InterfaceLessHandlerCancellationTokenSuppression = "NSBS0003";
 }
