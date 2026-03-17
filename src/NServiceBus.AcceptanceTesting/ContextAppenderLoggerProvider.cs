563b87f86dddf514f4ce7bf8d283f540947c2ab8
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-03-17 11:35:56 +0100

    Refactor logging implementation by replacing custom logger factories with `ContextAppenderLoggerProvider`

--- src/NServiceBus.AcceptanceTesting/ContextAppenderLoggerProvider.cs
+++ src/NServiceBus.AcceptanceTesting/ContextAppenderLoggerProvider.cs
@@ -3,16 +3,15 @@ namespace NServiceBus.AcceptanceTesting;
 using System.Collections.Concurrent;
 using Microsoft.Extensions.Logging;
 
-sealed class ContextAppenderMicrosoftLoggerFactory(ScenarioContext scenarioContext) : ILoggerFactory
+[ProviderAlias("ScenarioContext")]
+sealed class ContextAppenderLoggerProvider(ScenarioContext scenarioContext) : ILoggerProvider, ISupportExternalScope
 {
     readonly ConcurrentDictionary<string, ContextAppenderMicrosoftLogger> loggers = new();
-    readonly IExternalScopeProvider scopeProvider = new LoggerExternalScopeProvider();
+    IExternalScopeProvider scopeProvider = new LoggerExternalScopeProvider();
 
     public void Dispose() => loggers.Clear();
 
     public ILogger CreateLogger(string categoryName) => loggers.GetOrAdd(categoryName, static (name, state) => new ContextAppenderMicrosoftLogger(name, state.scenarioContext, state.scopeProvider), (scenarioContext, scopeProvider));
 
-    public void AddProvider(ILoggerProvider provider)
-    {
-    }
+    public void SetScopeProvider(IExternalScopeProvider externalScopeProvider) => scopeProvider = externalScopeProvider;
 }
