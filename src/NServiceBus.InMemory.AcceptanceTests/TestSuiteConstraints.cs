7e5bb55452601c35aec94da1ab294e41058aea56
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-03-27 11:19:09 +0100

    Add InMemory.AcceptanceTests project with ConfigureEndpointInMemoryTransport

---
+++ src/NServiceBus.InMemory.AcceptanceTests/TestSuiteConstraints.cs
@@ -0,0 +1,22 @@
+namespace NServiceBus.AcceptanceTests;
+
+using AcceptanceTesting.Support;
+
+public partial class TestSuiteConstraints
+{
+    public bool SupportsDtc => false;
+
+    public bool SupportsCrossQueueTransactions => true;
+
+    public bool SupportsNativePubSub => true;
+
+    public bool SupportsDelayedDelivery => true;
+
+    public bool SupportsOutbox => true;
+
+    public bool SupportsPurgeOnStartup => true;
+
+    public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointInMemoryTransport();
+
+    public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointAcceptanceTestingPersistence();
+}
