fc4e823ac934f3d5f095641ee3f1713e2e618472
Andreas Bednarz <andreas.bednarz@particular.net>
2026-03-31 16:26:13 +1100

    Fixed outbox test for multiple subscribers handling the same event

--- src/NServiceBus.AcceptanceTests/Outbox/When_subscribers_handles_the_same_event.cs
+++ src/NServiceBus.AcceptanceTests/Outbox/When_subscribers_handles_the_same_event.cs
@@ -1,6 +1,5 @@
 namespace NServiceBus.AcceptanceTests.Outbox;
 
-using System.Threading;
 using System.Threading.Tasks;
 using AcceptanceTesting;
 using AcceptanceTesting.Customization;
@@ -9,28 +8,21 @@ using EndpointTemplates;
 using Features;
 using NUnit.Framework;
 
-public class When_subscribers_handles_the_same_event : NServiceBusAcceptanceTest
+public class When_outbox_is_used_by_multiple_subscribers_for_the_same_event : NServiceBusAcceptanceTest
 {
-    [Test, CancelAfter(10_000)]
-    public async Task Should_be_processed_by_all_subscribers(CancellationToken cancellationToken = default)
+    [Test]
+    public async Task Each_subscriber_should_dispatch_its_own_transport_operations()
     {
-        Requires.OutboxPersistence();
-
         var context = await Scenario.Define<Context>()
-            .WithEndpoint<Publisher>(b =>
-                b.When(c => c.Subscriber1Subscribed && c.Subscriber2Subscribed, session => session.Publish(new MyEvent()))
-            )
+            .WithEndpoint<Publisher>(b => b.When(
+                c => c.Subscriber1Subscribed && c.Subscriber2Subscribed,
+                session => session.Publish(new MyEvent())))
             .WithEndpoint<Subscriber1>(b => b.When(async (session, ctx) =>
             {
                 await session.Subscribe<MyEvent>();
                 if (ctx.HasNativePubSubSupport)
                 {
                     ctx.Subscriber1Subscribed = true;
-                    ctx.AddTrace("Subscriber1 is now subscribed (at least we have asked the broker to be subscribed)");
-                }
-                else
-                {
-                    ctx.AddTrace("Subscriber1 has now asked to be subscribed to MyEvent");
                 }
             }))
             .WithEndpoint<Subscriber2>(b => b.When(async (session, ctx) =>
@@ -39,51 +31,42 @@ public class When_subscribers_handles_the_same_event : NServiceBusAcceptanceTest
                 if (ctx.HasNativePubSubSupport)
                 {
                     ctx.Subscriber2Subscribed = true;
-                    ctx.AddTrace("Subscriber2 is now subscribed (at least we have asked the broker to be subscribed)");
-                }
-                else
-                {
-                    ctx.AddTrace("Subscriber2 has now asked to be subscribed to MyEvent");
                 }
             }))
-            .Run(cancellationToken);
+            .WithEndpoint<Collector>()
+            .Run();
 
-        using (Assert.EnterMultipleScope())
+        Assert.Multiple(() =>
         {
-            Assert.That(context.Subscriber1GotTheEvent, Is.True);
-            Assert.That(context.Subscriber2GotTheEvent, Is.True);
-        }
+            Assert.That(context.FailedMessages.IsEmpty, Is.True);
+            Assert.That(context.Subscriber1ProcessedConfirmed, Is.True);
+            Assert.That(context.Subscriber2ProcessedConfirmed, Is.True);
+        });
     }
 
     public class Context : ScenarioContext
     {
         public bool Subscriber1Subscribed { get; set; }
         public bool Subscriber2Subscribed { get; set; }
+        public bool Subscriber1ProcessedConfirmed { get; set; }
+        public bool Subscriber2ProcessedConfirmed { get; set; }
 
-        public bool Subscriber1GotTheEvent { get; set; }
-        public bool Subscriber2GotTheEvent { get; set; }
-
-        public void MaybeCompleted() => MarkAsCompleted(Subscriber1GotTheEvent, Subscriber2GotTheEvent);
+        public void MaybeCompleted() => MarkAsCompleted(Subscriber1ProcessedConfirmed, Subscriber2ProcessedConfirmed);
     }
 
     public class Publisher : EndpointConfigurationBuilder
     {
         public Publisher() => EndpointSetup<DefaultPublisher>(c =>
         {
-            c.OnEndpointSubscribed<Context>((s, context) =>
+            c.OnEndpointSubscribed<Context>((s, ctx) =>
             {
-                var subscriber1 = Conventions.EndpointNamingConvention(typeof(Subscriber1));
-                if (s.SubscriberEndpoint.Contains(subscriber1))
+                if (s.SubscriberEndpoint.Contains(Conventions.EndpointNamingConvention(typeof(Subscriber1))))
                 {
-                    context.Subscriber1Subscribed = true;
-                    context.AddTrace($"{subscriber1} is now subscribed");
+                    ctx.Subscriber1Subscribed = true;
                 }
-
-                var subscriber2 = Conventions.EndpointNamingConvention(typeof(Subscriber2));
-                if (s.SubscriberEndpoint.Contains(subscriber2))
+                if (s.SubscriberEndpoint.Contains(Conventions.EndpointNamingConvention(typeof(Subscriber2))))
                 {
-                    context.Subscriber2Subscribed = true;
-                    context.AddTrace($"{subscriber2} is now subscribed");
+                    ctx.Subscriber2Subscribed = true;
                 }
             });
         }, metadata => metadata.RegisterSelfAsPublisherFor<MyEvent>(this));
@@ -91,42 +74,63 @@ public class When_subscribers_handles_the_same_event : NServiceBusAcceptanceTest
 
     public class Subscriber1 : EndpointConfigurationBuilder
     {
-        public Subscriber1() =>
-            EndpointSetup<DefaultServer>(c =>
-            {
-                c.DisableFeature<AutoSubscribe>();
+        public Subscriber1() => EndpointSetup<DefaultServer>(c =>
+        {
+            c.DisableFeature<AutoSubscribe>();
+            c.ConfigureTransport().TransportTransactionMode = TransportTransactionMode.ReceiveOnly;
+            c.EnableOutbox();
+        }, metadata => metadata.RegisterPublisherFor<MyEvent>(typeof(Publisher)));
 
-                c.ConfigureTransport().TransportTransactionMode = TransportTransactionMode.ReceiveOnly;
-                c.EnableOutbox();
-            }, metadata => metadata.RegisterPublisherFor<MyEvent>(typeof(Publisher)));
-
-        public class MyHandler(Context testContext) : IHandleMessages<MyEvent>
+        class MyEventHandler : IHandleMessages<MyEvent>
         {
             public Task Handle(MyEvent message, IMessageHandlerContext context)
             {
-                testContext.Subscriber1GotTheEvent = true;
-                testContext.MaybeCompleted();
-                return Task.CompletedTask;
+                var options = new SendOptions();
+                options.SetDestination(Conventions.EndpointNamingConvention(typeof(Collector)));
+                return context.Send(new Subscriber1Processed(), options);
             }
         }
     }
 
     public class Subscriber2 : EndpointConfigurationBuilder
     {
-        public Subscriber2() =>
-            EndpointSetup<DefaultServer>(c =>
-            {
-                c.DisableFeature<AutoSubscribe>();
-
-                c.ConfigureTransport().TransportTransactionMode = TransportTransactionMode.ReceiveOnly;
-                c.EnableOutbox();
-            }, metadata => metadata.RegisterPublisherFor<MyEvent>(typeof(Publisher)));
-
-        public class MyHandler(Context testContext) : IHandleMessages<MyEvent>
+        public Subscriber2() => EndpointSetup<DefaultServer>(c =>
         {
-            public Task Handle(MyEvent messageThatIsEnlisted, IMessageHandlerContext context)
+            c.DisableFeature<AutoSubscribe>();
+            c.ConfigureTransport().TransportTransactionMode = TransportTransactionMode.ReceiveOnly;
+            c.EnableOutbox();
+        }, metadata => metadata.RegisterPublisherFor<MyEvent>(typeof(Publisher)));
+
+        class MyEventHandler : IHandleMessages<MyEvent>
+        {
+            public Task Handle(MyEvent message, IMessageHandlerContext context)
             {
-                testContext.Subscriber2GotTheEvent = true;
+                var options = new SendOptions();
+                options.SetDestination(Conventions.EndpointNamingConvention(typeof(Collector)));
+                return context.Send(new Subscriber2Processed(), options);
+            }
+        }
+    }
+
+    public class Collector : EndpointConfigurationBuilder
+    {
+        public Collector() => EndpointSetup<DefaultServer>();
+
+        public class Subscriber1ProcessedHandler(Context testContext) : IHandleMessages<Subscriber1Processed>
+        {
+            public Task Handle(Subscriber1Processed message, IMessageHandlerContext context)
+            {
+                testContext.Subscriber1ProcessedConfirmed = true;
+                testContext.MaybeCompleted();
+                return Task.CompletedTask;
+            }
+        }
+
+        public class Subscriber2ProcessedHandler(Context testContext) : IHandleMessages<Subscriber2Processed>
+        {
+            public Task Handle(Subscriber2Processed message, IMessageHandlerContext context)
+            {
+                testContext.Subscriber2ProcessedConfirmed = true;
                 testContext.MaybeCompleted();
                 return Task.CompletedTask;
             }
@@ -134,4 +138,6 @@ public class When_subscribers_handles_the_same_event : NServiceBusAcceptanceTest
     }
 
     public class MyEvent : IEvent;
+    public class Subscriber1Processed : IMessage;
+    public class Subscriber2Processed : IMessage;
 }
