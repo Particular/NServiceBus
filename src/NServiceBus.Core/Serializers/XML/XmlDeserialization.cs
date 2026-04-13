89db50b8ed96fdcce563d3ef20947af5fc5fea81
Rhys Bevilaqua <rhys.bevilaqua@particular.net>
2026-04-13 15:12:04 +0800

    Remove unused usings

--- src/NServiceBus.Core/Serializers/XML/XmlDeserialization.cs
+++ src/NServiceBus.Core/Serializers/XML/XmlDeserialization.cs
@@ -3,9 +3,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
