adde26a216e60b795e1d28e705e9920d5048d25e
SzymonPobiega <szymon.pobiega@gmail.com>
2026-04-20 11:49:14 +0200

    Add a test for propagating the receive to dispatch properties

--- src/NServiceBus.Learning.AcceptanceTests/When_message_sent_with_LearningTransport.cs
+++ src/NServiceBus.Learning.AcceptanceTests/When_message_sent_with_LearningTransport.cs
@@ -3,6 +3,7 @@ namespace NServiceBus.AcceptanceTests;
 using System;
 using System.Threading.Tasks;
 using AcceptanceTesting;
+using AcceptanceTesting.Customization;
 using EndpointTemplates;
 using NUnit.Framework;
 using Transport;
@@ -24,15 +25,33 @@ public class When_message_sent_with_LearningTransport : NServiceBusAcceptanceTes
         }
     }
 
+    [Test]
+    public async Task Should_preserve_file_created_time_property_on_dispatched_copies()
+    {
+        var context = await Scenario.Define<Context>()
+            .WithEndpoint<EndpointWithAuditOn>(b => b.When(session => session.SendLocal(new TestMessage())))
+            .WithEndpoint<AuditSpy>()
+            .Run();
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(context.MessageAudited, Is.True, "Message was not audited");
+        }
+    }
+
     class Context : ScenarioContext
     {
         public bool MessageReceived { get; set; }
         public string FileCreatedAt { get; set; }
+        public bool MessageAudited { get; set; }
     }
 
     class Endpoint : EndpointConfigurationBuilder
     {
-        public Endpoint() => EndpointSetup<DefaultServer>();
+        public Endpoint() => EndpointSetup<DefaultServer>(endpointConfiguration =>
+        {
+            endpointConfiguration.AuditProcessedMessagesTo(Conventions.EndpointNamingConvention(typeof(AuditSpy)));
+        });
 
         class TestMessageHandler(Context testContext) : IHandleMessages<TestMessage>
         {
@@ -53,6 +72,59 @@ public class When_message_sent_with_LearningTransport : NServiceBusAcceptanceTes
                 }
 
                 testContext.MarkAsCompleted(testContext.MessageReceived, testContext.FileCreatedAt != null);
+                return Task.CompletedTask;
+            }
+        }
+    }
+
+    class EndpointWithAuditOn : EndpointConfigurationBuilder
+    {
+        public EndpointWithAuditOn() => EndpointSetup<DefaultServer>(endpointConfiguration =>
+        {
+            endpointConfiguration.AuditProcessedMessagesTo(Conventions.EndpointNamingConvention(typeof(AuditSpy)));
+        });
+
+        class TestMessageHandler(Context testContext) : IHandleMessages<TestMessage>
+        {
+            public Task Handle(TestMessage message, IMessageHandlerContext context)
+            {
+                testContext.MessageReceived = true;
+
+                if (context.Extensions.TryGet<ReceiveProperties>(out var receiveProperties))
+                {
+                    if (receiveProperties.TryGetValue("LearningTransport.FileCreatedAt", out var fileCreatedAt))
+                    {
+                        testContext.FileCreatedAt = fileCreatedAt;
+                    }
+                }
+                return Task.CompletedTask;
+            }
+        }
+    }
+
+    class AuditSpy : EndpointConfigurationBuilder
+    {
+        public AuditSpy() =>
+            EndpointSetup<DefaultServer>();
+
+        public class AuditMessageHandler(Context testContext) : IHandleMessages<TestMessage>
+        {
+            public Task Handle(TestMessage message, IMessageHandlerContext context)
+            {
+                testContext.MessageAudited = true;
+
+                if (context.Extensions.TryGet<ReceiveProperties>(out var receiveProperties))
+                {
+                    if (receiveProperties.TryGetValue("LearningTransport.FileCreatedAt", out var fileCreatedAt))
+                    {
+                        testContext.MarkAsCompleted(testContext.MessageAudited, testContext.FileCreatedAt == fileCreatedAt);
+                    }
+                }
+                else
+                {
+                    testContext.MarkAsFailed(new Exception("Failed to propagate receive properties from the original message."));
+                }
+
                 return Task.CompletedTask;
             }
         }
