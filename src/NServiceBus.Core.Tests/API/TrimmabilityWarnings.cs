64b1b6e0c54566b489189662f16c197d1d27881e
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-06-04 11:23:21 +0200

    Remove verbosity

--- src/NServiceBus.Core.Tests/API/TrimmabilityWarnings.cs
+++ src/NServiceBus.Core.Tests/API/TrimmabilityWarnings.cs
@@ -49,8 +49,6 @@ public partial class TrimmabilityWarnings
         startInfo.ArgumentList.Add("-p:TreatWarningsAsErrors=false");
         startInfo.ArgumentList.Add("-p:IsPackable=false");
         startInfo.ArgumentList.Add("-bl:out.binlog");
-        startInfo.ArgumentList.Add("-v");
-        startInfo.ArgumentList.Add("minimal");
         startInfo.ArgumentList.Add("--no-incremental");
 
         using var process = Process.Start(startInfo)!;
