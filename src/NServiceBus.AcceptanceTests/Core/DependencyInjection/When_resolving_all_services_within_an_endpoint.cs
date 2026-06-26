e3b0c1ccebc4a12b02ee1b130b87aacc1705f24f
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-06-26 11:12:00 +0200

    Cleanup usings

--- src/NServiceBus.AcceptanceTests/Core/DependencyInjection/When_resolving_all_services_within_an_endpoint.cs
+++ src/NServiceBus.AcceptanceTests/Core/DependencyInjection/When_resolving_all_services_within_an_endpoint.cs
@@ -3,7 +3,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AcceptanceTesting;
-using AcceptanceTesting.Support;
 using EndpointTemplates;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
