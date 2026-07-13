ba353c1b5e9b1db89214a0ed22585fffaa0f404e
Irina Dominte <scurtuirina@gmail.com>
2026-07-13 14:41:55 +0300

    Fixed approval file

--- src/NServiceBus.Core/OpenTelemetry/Tracing/ActivitySources.cs
+++ src/NServiceBus.Core/OpenTelemetry/Tracing/ActivitySources.cs
@@ -8,7 +8,7 @@ static class ActivitySources
 {
     public static readonly ActivitySource Main =
         new("NServiceBus.Core",
-            "0.1.0");
+            "0.2.0");
 
     public static readonly ActivitySource Handler =
         new("NServiceBus.Core.Handler",
