021179ff18cce2c2fd6801cdc2444c6751281f0b
Michelle Chamberlin <michelle.chamberlin@particular.net>
2026-07-17 16:16:51 -0700

    testing nullable operators in interfaces

---
+++ src/NServiceBus.Core.Tests/API/Infra/NullableInterface/ITestInterface.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+
+namespace NServiceBus.Core.Tests.API.Infra.NullableInterface
+{
+    interface ITestInterface
+    {
+        string WriteAMessage(string message);
+
+        string WriteNullableMessage(string? message);
+
+        string? ReturnNullableMessage(string? message);
+    }
+}
---
+++ src/NServiceBus.Core.Tests/API/Infra/NullableInterface/TestInterface.cs
@@ -0,0 +1,24 @@
+﻿namespace NServiceBus.Core.Tests.API.Infra.NullableInterface
+{
+    using System;
+
+    class TestInterface : ITestInterface
+    {
+        public string ReturnNullableMessage(string message)
+        {
+            Console.WriteLine(message);
+            return message;
+        }
+
+        public string WriteAMessage(string message)
+        {
+            Console.WriteLine(message);
+            return message;
+        }
+        public string WriteNullableMessage(string message)
+        {
+            Console.WriteLine(message);
+            return message;
+        }
+    }
+}
