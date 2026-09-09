44414c21b365fce5470638f26323c7fde37d430b
Mike Minutillo <mike.minutillo@particular.net>
2026-09-09 12:51:08 +0800

    Add ServicePlatform infrastructure

---
+++ src/NServiceBus.Core/ServicePlatform/ServicePlatformSettings.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+
+namespace NServiceBus;
+
+using NServiceBus.Configuration.AdvancedExtensibility;
+using NServiceBus.Settings;
+
+/// <summary>
+/// Exposes the service platform configuration settings.
+/// </summary>
+public class ServicePlatformSettings : ExposeSettings
+{
+    internal ServicePlatformSettings(SettingsHolder settings) : base(settings)
+    {
+    }
+
+    /// <summary>
+    /// Gets or sets the primary ServiceControl queue.
+    /// </summary>
+    public string ServiceControlQueue
+    {
+        get => Settings.Get<string>(ServicePlatform.ServiceControlQueueSettingKey);
+        set => Settings.Set(ServicePlatform.ServiceControlQueueSettingKey, value);
+    }
+}
