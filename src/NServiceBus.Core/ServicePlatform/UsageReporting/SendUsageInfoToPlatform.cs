359048c5dd54f5fe3189f838d51b3dd3d62facb5
Mike Minutillo <mike.minutillo@particular.net>
2026-09-09 12:52:48 +0800

    Add usage reporting feature

---
+++ src/NServiceBus.Core/ServicePlatform/UsageReporting/SendUsageInfoToPlatform.cs
@@ -0,0 +1,37 @@
+﻿#nullable enable
+
+namespace NServiceBus;
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus.Features;
+
+class SendUsageInfoToPlatform : Feature
+{
+    public SendUsageInfoToPlatform()
+    {
+        DependsOn<ServicePlatformFeature>();
+        Prerequisite(context => !context.Settings.GetOrDefault<bool>("Endpoint.SendOnly"),
+            "Usage Information is only relevant for endpoints receiving messages.");
+        Defaults(settings => settings.SetDefault(ReportingIntervalSettingKey, TimeSpan.FromDays(1)));
+    }
+
+    protected override void Setup(FeatureConfigurationContext context)
+        => context.RegisterStartupTask(
+            serviceProvider =>
+            {
+                var servicePlatformSender = serviceProvider.GetRequiredService<ServicePlatform>();
+                var endpointUsageReportSender = servicePlatformSender.CreatePrimarySender(UsageReportingMessagesJsonContext.Default.EndpointUsageReport);
+
+                var settings = new UsageReporterSettings
+                {
+                    EndpointName = context.Settings.EndpointName(),
+                    ReportingInterval = context.Settings.Get<TimeSpan>(ReportingIntervalSettingKey)
+                };
+
+                return new UsageReporter(endpointUsageReportSender, settings);
+            }
+        );
+
+    public const string ReportingIntervalSettingKey = "UsageReporting.ReportingInterval";
+}
---
+++ src/NServiceBus.Core/ServicePlatform/UsageReporting/UsageReporterSettings.cs
@@ -0,0 +1,11 @@
+﻿#nullable enable
+
+namespace NServiceBus;
+
+using System;
+
+class UsageReporterSettings
+{
+    public required string EndpointName { get; init; }
+    public required TimeSpan ReportingInterval { get; init; }
+}
