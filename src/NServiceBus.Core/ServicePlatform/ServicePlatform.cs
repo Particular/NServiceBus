11b018dfd414d718cc13c01b064a55f94ae0ee08
Mike Minutillo <mike.minutillo@particular.net>
2026-09-09 12:51:08 +0800

    Add ServicePlatform infrastructure

---
+++ src/NServiceBus.Core/ServicePlatform/ServicePlatform.cs
@@ -0,0 +1,38 @@
+﻿#nullable enable
+
+namespace NServiceBus;
+
+using System.Text.Json.Serialization.Metadata;
+using NServiceBus.Routing;
+using NServiceBus.Settings;
+using NServiceBus.Transport;
+
+class ServicePlatform(
+    ServicePlatform.Configuration platformConfiguration,
+    IMessageDispatcher messageDispatcher,
+    ReceiveAddresses? receiveAddresses = null
+)
+{
+    const string DefaultPrimaryServiceControlQueue = "Particular.ServiceControl";
+
+    readonly UnicastAddressTag primaryInstanceAddress = new(platformConfiguration.ServiceControlQueue);
+
+    public ServicePlatformSender<TMessage> CreatePrimarySender<TMessage>(JsonTypeInfo<TMessage> jsonTypeInfo)
+        => new(jsonTypeInfo, messageDispatcher, primaryInstanceAddress, receiveAddresses);
+
+
+    internal static void Defaults(SettingsHolder settings)
+        => settings.SetDefault(ServiceControlQueueSettingKey, DefaultPrimaryServiceControlQueue);
+
+    internal static Configuration GetConfiguration(IReadOnlySettings settings) => new()
+    {
+        ServiceControlQueue = settings.Get<string>(ServiceControlQueueSettingKey)
+    };
+
+    public class Configuration
+    {
+        public required string ServiceControlQueue { get; init; }
+    }
+
+    public const string ServiceControlQueueSettingKey = "ServicePlatform.ServiceControlQueue";
+}
