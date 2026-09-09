84bea4cc8db01cda5e376aca13cc16b5f6271142
Mike Minutillo <mike.minutillo@particular.net>
2026-09-09 12:51:08 +0800

    Add ServicePlatform infrastructure

---
+++ src/NServiceBus.Core/ServicePlatform/ServicePlatformFeature.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+
+namespace NServiceBus;
+
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus.Features;
+
+class ServicePlatformFeature : Feature
+{
+    public ServicePlatformFeature()
+        => Defaults(ServicePlatform.Defaults);
+
+    protected override void Setup(FeatureConfigurationContext context)
+        => context.Services.AddSingleton(
+                serviceProvider => ActivatorUtilities.CreateInstance<ServicePlatform>(
+                    serviceProvider,
+                    ServicePlatform.GetConfiguration(context.Settings)
+                )
+            );
+}
